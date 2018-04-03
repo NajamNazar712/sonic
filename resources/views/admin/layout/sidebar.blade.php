86cf32081a67c0744948520abd28948dd6217562
Muhammad Waqas <muhammad.waqas@trax.pk>
2018-04-03 18:24:59 +0500

    Models and controllers plus migrations

---
+++ resources/views/admin/layout/sidebar.blade.php
@@ -0,0 +1,35 @@
+<div class="main-menu menu-fixed menu-light menu-accordion menu-shadow menu-border"
+     data-scroll-to-active="true">
+    <div class="main-menu-content">
+        <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
+
+
+            <!-- official -->
+            <li class=" nav-item"><a href="/admin/dashboard"><i class="la la-home"></i><span class="menu-title" data-i18n="nav.dash.main">Dashboard</span></a></li>
+            <li class=" nav-item"><a href="#"><i class="la la-home"></i><span class="menu-title" data-i18n="nav.dash.main">Order Management</span></a>
+                <ul class="menu-content">
+                    <li><a class="menu-item" href="/admin/order/management" data-i18n="nav.dash.ecommerce">Order Management</a>
+                    </li>
+                    <li class=""><a class="menu-item" href="/admin/order/pending" data-i18n="nav.dash.crypto">Pending Booked Orders</a>
+                    </li>
+                    <li><a class="menu-item" href="/admin/sheet/receiving" data-i18n="nav.dash.sales">Booked Receiving Sheets</a>
+                    </li>
+                    <li><a class="menu-item" href="#" data-i18n="nav.dash.sales">Printed Receiving Sheets</a>
+                    </li>
+                </ul>
+            </li>
+            <li class=" nav-item"><a href="#"><i class="la la-home"></i><span class="menu-title" data-i18n="nav.dash.main">Book A Service</span></a>
+                <ul class="menu-content">
+                    <li><a class="menu-item" href="/RegularDelivery" data-i18n="nav.dash.ecommerce">Regular Delivery</a></li>
+                    <li><a class="menu-item" href="/RegularDelivery" data-i18n="nav.dash.ecommerce">Replacement Delivery</a></li>
+                    <li><a class="menu-item" href="/RegularDelivery" data-i18n="nav.dash.ecommerce">Try & Buy Delivery</a></li>
+                </ul>
+            </li>
+            <li class=" nav-item"><a href="#"><i class="la la-home"></i><span class="menu-title" data-i18n="nav.dash.main">Profile Update</span></a></li>
+            <li class=" nav-item"><a href="#"><i class="la la-home"></i><span class="menu-title" data-i18n="nav.dash.main">Excel Upload</span></a></li>
+            <li class=" nav-item"><a href="#"><i class="la la-home"></i><span class="menu-title" data-i18n="nav.dash.main">Finance</span></a></li>
+            <li class=" nav-item"><a href="#"><i class="la la-home"></i><span class="menu-title" data-i18n="nav.dash.main">Reports</span></a></li>
+            <li class=" nav-item"><a href="#"><i class="la la-home"></i><span class="menu-title" data-i18n="nav.dash.main">Tracking</span></a></li>
+        </ul>
+    </div>
+</div>
