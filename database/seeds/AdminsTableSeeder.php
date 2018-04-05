ea553f9a1dc0fd821aca4a4903d3dc69a828591f
Qasim Naseer <qasimnaseer@Qasims-MacBook-Pro.local>
2018-04-05 11:43:04 +0500

    Adding Admin Seeder File

---
+++ database/seeds/AdminsTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Http\Models\Admin\Admin;
+
+class AdminsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = Admin::create([
+            'name' => 'admin',
+            'email' => 'admin@email.com',
+            'username' => 'admin',
+            'department' => 'Admin Department',
+            'password' => bcrypt('admin123')
+        ]);
+
+    }
+}
+
+
+
