c713bee8be29b4bb46a3df3b4ee758e0d1648f8d
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2018-05-08 12:43:46 +0500

    Missing Seeder Additions
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,9 +15,12 @@ class DatabaseSeeder extends Seeder
         //$this->call(UsersTableSeeder::class);
         $this->call(AdminsTableSeeder::class);
         $this->call(CityTableSeeder::class);
+        $this->call(CityPickupTableSeeder::class);
         $this->call(ProductsTableSeeder::class);
         $this->call(BookingTableSeeder::class);
         $this->call(PickupTypeTableSeeder::class);
         $this->call(ShippingModeTableSeeder::class);
+        $this->call(ShippingModeSameDayTimingTableSeeder::class);
+        $this->call(PaymentModeTableSeeder::class);
     }
 }
