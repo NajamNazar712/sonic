c93e0d0ec61f83fc55fd144f403b5bf20a5de1d7
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2018-05-21 12:44:31 +0500

    Popup Blocker Alert
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- resources/views/client/shipment/receiving_sheet/index.blade.php
+++ resources/views/client/shipment/receiving_sheet/index.blade.php
@@ -106,16 +106,24 @@
 					}
 				})
 				.done(function(data) {
-					var tab = window.open('', '_blank');
+					var tab = window.open('','_blank');
 
-					if (tab) {
+					if(!tab || tab.outerHeight === 0) {
+						swal({
+							title: 'Popup Blocker Enabled!',
+							text: 'Please add this site to your exception list.',
+							icon: 'error',
+							closeOnClickOutside: false,
+							closeOnEsc: false
+						});
+					}
+					else {
 						tab.document.write(data);
 						tab.document.close();
 						tab.focus();
 					}
 				});
 			}
-
 			var selected_rows = [];
 
 			var table = $('.datatable').DataTable({
