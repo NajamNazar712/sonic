f76a3f0f22278f921797990e2a193ad9ed0cccab
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2018-05-24 16:33:52 +0500

    Name Fixes and Ids through Function
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- app/Http/Controllers/ShipmentsJourneyController.php
+++ app/Http/Controllers/ShipmentsJourneyController.php
@@ -9,16 +9,17 @@ use App\Http\Models\ShipmentsJourney;
 
 use Auth;
 
-class ShipmentJourneyController extends Controller
+class ShipmentsJourneyController extends Controller
 {
-    static public function add($shipment_id, $shipper_status_id, $consignee_status_id, $remarks) {
+    static public function add($shipment_id, $shipper_status_id, $consignee_status_id, $remarks, $user_id, $admin_id) {
       $shipment_journey = new ShipmentsJourney();
 
       $shipment_journey->shipment_id = $shipment_id;
       $shipment_journey->shipper_status_id = $shipper_status_id;
       $shipment_journey->consignee_status_id = $consignee_status_id;
       $shipment_journey->remarks = $remarks;
-      $shipment_journey->user_id = Auth::id();
+      $shipment_journey->user_id = $user_id;
+      $shipment_journey->admin_id = $admin_id;
 
       $shipment_journey->save();
     }
--- app/Http/Controllers/Shippers/ShipperShipmentBookController.php
+++ app/Http/Controllers/Shippers/ShipperShipmentBookController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Shippers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Http\Controllers\ShipmentJourneyController;
+use App\Http\Controllers\ShipmentsJourneyController;
 
 use App\Http\Models\BookingType;
 use App\Http\Models\Shipper\User;
@@ -83,7 +83,7 @@ class ShipperShipmentBookController extends Controller
 
       $shipment_id = $shipment->id;
 
-      ShipmentJourneyController::add($shipment_id, 1, 1, 'Shipment has been Booked!');
+      ShipmentsJourneyController::add($shipment_id, 1, 1, 'Shipment has been Booked!', Auth::id(), NULL);
 
       return $shipment_id;
     }
