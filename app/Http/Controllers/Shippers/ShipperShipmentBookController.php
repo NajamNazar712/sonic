6a4972c4087422da032b5b13625b227d47234389
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2018-05-24 16:40:24 +0500

    Fix
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- app/Http/Controllers/Shippers/ShipperShipmentBookController.php
+++ app/Http/Controllers/Shippers/ShipperShipmentBookController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers\Shippers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Http\Controllers\ShipmentsJourneyController;
 use App\Http\Controllers\Admins\AdminPickupsController;
+use App\Http\Controllers\ShipmentsJourneyController;
 
 use App\Http\Models\BookingType;
 use App\Http\Models\Shipper\User;
@@ -83,6 +83,8 @@ class ShipperShipmentBookController extends Controller
       $shipment->save();
 
       $shipment_id = $shipment->id;
+
+      AdminPickupsController::generate($shipment_id);
 
       ShipmentsJourneyController::add($shipment_id, 1, 1, 'Shipment has been Booked!', Auth::id(), NULL);
 
