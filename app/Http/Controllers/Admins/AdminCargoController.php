c31c777ce0918fd8bcf279fba43bd54dc019a0b5
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2018-06-19 01:46:45 +0500

    Pending Shipments for Cargo
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

---
+++ app/Http/Controllers/Admins/AdminCargoController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Admins;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use Auth;
+
+use Yajra\Datatables\Datatables;
+use Carbon\Carbon;
+
+class AdminCargoController extends Controller
+{
+    public function __construct() {
+      $this->middleware('auth:admin');
+    }
+
+    public function pending_index() {
+      return view('admin.cargo.pending');
+    }
+
+    public function pending_list(Request $request) {
+      $shipments = Shipment::join('booking_types as bt', 'shipments.booking_type_id', '=', 'bt.id')
+      ->join('shipment_status as ss', 'shipments.shipper_status_id', '=', 'ss.id')
+      ->join('user_shipping_infos as usi', function($join) {
+        $join->on('shipments.pickup_address_id', '=', 'usi.id')
+        ->on('shipments.consignee_city_id', '!=', 'usi.city_id');
+      })
+      ->join('users as u', 'shipments.user_id', '=', 'u.id')
+      ->join('cities as oc', 'usi.city_id', '=', 'oc.id')
+      ->join('cities as dc', 'shipments.consignee_city_id', '=', 'dc.id')
+      ->join('shipping_modes as sm', 'shipments.shipping_mode_id', '=', 'sm.id')
+      ->join('shipments_journey', function ($join) {
+        $join->on('shipments_journey.id', '=', 'shipments.id')
+        ->on('shipments_journey.shipper_status_id', '=', 2);
+      })
+      ->select('shipments.tracking_number', 'shipments.order_id', 'bt.name as service_type', 'ss.name as status', 'oc.name as origin', 'dc.name as destination', 'u.name as shipper', 'shipments.amount', 'shipping_modes.name as shipping_mode', 'shipments.created_at as booked_at', 'shipments_journey.created_at as arrival_at')
+      ->whereIn('shipments.shipper_status_id', [2, 14, 17, 20, 26]);
+
+      $datatables = Datatables::of($pickup_requests);
+
+      return $datatables->make(true);
+    }
+
+}
--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -39,6 +39,12 @@
               </ul>
             </li>
 
+            <li class=" nav-item"><a href="#"><i class="la la-home"></i><span class="menu-title">Cargo</span></a>
+              <ul class="menu-content">
+                <li><a class="menu-item" href="{{ route('admin.cargo.pending.index') }}">Pending</a></li>
+              </ul>
+            </li>
+
             <li class=" nav-item"><a href="#"><i class="la la-home"></i><span class="menu-title" data-i18n="nav.dash.main">Profile Update</span></a></li>
             <li class=" nav-item"><a href="#"><i class="la la-home"></i><span class="menu-title" data-i18n="nav.dash.main">Excel Upload</span></a></li>
             <li class=" nav-item"><a href="#"><i class="la la-home"></i><span class="menu-title" data-i18n="nav.dash.main">Finance</span></a></li>
