056c43294ad93245aa16a313ba6a974f91ef80e0
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2018-07-09 16:58:42 +0500

    Fixes
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- app/Http/Models/Admin/DeliveryNoteShipment.php
+++ app/Http/Models/Admin/DeliveryNoteShipment.php
@@ -6,9 +6,36 @@ use Illuminate\Database\Eloquent\Model;
 
 class DeliveryNoteShipment extends Model
 {
-    protected $primaryKey = 'shipment_id';
-    public $timestamps = FALSE;
-    protected $fillable = [
-        'delivery_note_id','shipment_id'
-    ];
+	protected $primaryKey = ['delivery_note_id', 'shipment_id'];
+	public $incrementing = FALSE;
+	public $timestamps = FALSE;
+	protected $fillable = [
+		'delivery_note_id','shipment_id'
+	];
+
+	protected function setKeysForSaveQuery(Builder $query) {
+		$keys = $this->getKeyName();
+
+		if (!is_array($keys)) {
+			return parent::setKeysForSaveQuery($query);
+		}
+
+		foreach ($keys as $keyName) {
+			$query->where($keyName, '=', $this->getKeyForSaveQuery($keyName));
+		}
+
+		return $query;
+	}
+
+	protected function getKeyForSaveQuery($keyName = null) {
+		if (is_null($keyName)){
+			$keyName = $this->getKeyName();
+		}
+
+		if (isset($this->original[$keyName])) {
+			return $this->original[$keyName];
+		}
+
+		return $this->getAttribute($keyName);
+	}
 }
