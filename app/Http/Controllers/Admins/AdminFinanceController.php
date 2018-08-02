456b7e3146b7581125092486386714d37b2d2f74
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2018-08-02 10:33:37 +0500

    Outstanding SDN and Outstanding Shipments Fix
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- app/Http/Controllers/Admins/AdminFinanceController.php
+++ app/Http/Controllers/Admins/AdminFinanceController.php
@@ -288,7 +288,7 @@ class AdminFinanceController extends Controller
     }
 
     public function outstanding_shipments_resolved(Request $request) {
-        $delivery_note_shipment = DeliveryNoteShipment::where('shipment_id', $request->id)->where('status', 6);
+        $delivery_note_shipment = DeliveryNoteShipment::where('shipment_id', $request->id)->where('status', 7);
 
         if ($delivery_note_shipment->exists()) {
             $delivery_note_shipment = $delivery_note_shipment->first();
@@ -305,7 +305,7 @@ class AdminFinanceController extends Controller
     }
 
     public function outstanding_shipments_adjust_in_payment(Request $request) {
-        $delivery_note_shipment = DeliveryNoteShipment::where('shipment_id', $request->id)->where('status', 6);
+        $delivery_note_shipment = DeliveryNoteShipment::where('shipment_id', $request->id)->where('status', 7);
 
         if ($delivery_note_shipment->exists()) {
             $delivery_note_shipment = $delivery_note_shipment->first();
