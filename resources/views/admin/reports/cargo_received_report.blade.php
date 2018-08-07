f9e863201916c03cd2f9f131240af7c04e511098
Muhammad Waqas <muhammad.waqas@trax.pk>
2018-08-07 10:12:48 +0500

    Pickup Note updated by also leadtime report done

--- resources/views/admin/reports/cargo_received_report.blade.php
+++ resources/views/admin/reports/cargo_received_report.blade.php
@@ -272,7 +272,7 @@
                         d.search_origin = $('#search_origin').val();
                         d.search_destination = $('#search_destination').val();
                         d.search_shippimg_modes = $('#search_shippimg_modes').val();
-                        d.search_transit_date = $('#transit_date').val();
+                        d.search_transit_date = $('input[name="transit_date_formatted"]').val();
                         d.search_received_date = $('input[name="received_date_formatted"]').val();
                     }
                 },
