8203da1079f37dc04e099a5f104e9336a2425f45
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2018-08-27 12:44:29 +0500

    Fix
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- app/Http/Controllers/Admins/AdminFinanceController.php
+++ app/Http/Controllers/Admins/AdminFinanceController.php
@@ -101,7 +101,7 @@ class AdminFinanceController extends Controller
         ->join('cities as h', 'dn.hub_id', '=', 'h.id')
         ->join('riders as ri', 'dn.rider_id', '=', 'ri.id')
         ->join('admins as a', 'dn.admin_id', '=', 'a.id')
-        ->join('admins as au', 'dn.updated_by', '=', 'a.id')
+        ->join('admins as au', 'dn.updated_by', '=', 'au.id')
         ->select('dn.id', 'dn.id as delivery_note_number', 'h.name as hub', 'ri.name as rider', 'dn.shipments_count as shipments', 'dn.delivered_shipments', 'a.name as assigned_by', 'dn.created_at as assigned_at', 'au.name as updated_by', 'dn.updated_at', 'dn.received_cod_amount as dncc_amount', 'dn.expense');
 
         if ($request->has('id')) {
