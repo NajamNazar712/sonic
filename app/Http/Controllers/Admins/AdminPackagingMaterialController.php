ab290112ad8a6f268b8af11299bc04a36aa95188
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2018-08-27 13:30:59 +0500

    Fix
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- app/Http/Controllers/Admins/AdminPackagingMaterialController.php
+++ app/Http/Controllers/Admins/AdminPackagingMaterialController.php
@@ -245,14 +245,20 @@ class AdminPackagingMaterialController extends Controller
             ->addColumn('action',function ($packaging) {
                 if (($packaging->status == 0) && (session('role_id') == 1 || in_array(80, session('permissions')))) {
                     $dropdown = '
-                      <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-success dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Actions</button>
-                        <div class="dropdown-menu dropdown-menu-sm">
-                            <div class="dropdown-menu open-left arrow">
-                                <a class="dropdown-item dispatch"><i class="ft-fast-forward primary"> Dispatch</a>
-                            </div>
+                      <span class="dropdown">
+                        <button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false"><i class="ft-settings"></i></button>
+                        <div class="dropdown-menu open-left arrow">
+                            <a class="dropdown-item dispatch"><i class="ft-fast-forward primary"> Dispatch</a>
                         </div>
-                      </div>
+                      </span>
+
+                        <button type='button' class='btn btn-success dropdown-toggle' data-toggle='dropdown'
+                        aria-haspopup='true' aria-expanded='false'><i class='ft-settings'></i></button>
+
+                        <div class='dropdown-menu open-left arrow'>
+                            <a href='javascript:void(0);' class='dropdown-item' data-target-id='{$result->id}' data-toggle='modal' data-target='#BankInfoModal'><i class='ft-plus-circle primary'></i> View Bank Info</a>
+                            <a href='javascript:void(0);' class='dropdown-item' data-target-id='{$result->id}' data-toggle='modal' data-target='#ShippingInfoModal'><i class='ft-plus-circle primary'></i> View Shipping Info</a>
                     ';
 
                     return $dropdown;
