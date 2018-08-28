3b4fa09b82a6f40373b7467ef592314a47a227a3
Muhammad Waqas <muhammad.waqas@trax.pk>
2018-08-28 11:02:38 +0500

    editing

---
+++ resources/views/admin/reports/completed_delivery_notes_report.blade.php
@@ -0,0 +1,169 @@
+@extends('admin.layout.master')
+
+@section('content')
+    <h1 class="mb-1">
+        Completed Deliveries
+    </h1>
+
+    <div class="card">
+        <div class="card-content" aria-expanded="true">
+            <div class="card-body">
+                @include('admin.inc.messages')
+
+                <div class="row mb-2 justify-content-center">
+
+                </div>
+                <table class="table table-bordered datatable" id="datatable" style="z-index: 3;">
+                    <thead>
+                    <tr role="row" class="bg-primary white">
+                        <th class="border-primary border-darken-1">S. No.</th>
+                        <th class="border-primary border-darken-1">Delivery Note No.</th>
+                        <th class="border-primary border-darken-1">Hub</th>
+                        <th class="border-primary border-darken-1">Rider</th>
+                        <th class="border-primary border-darken-1">Route</th>
+                        <th class="border-primary border-darken-1">No. Of Shipments</th>
+                        <th class="border-primary border-darken-1">No. Of Shipments Delivered</th>
+                        <th class="border-primary border-darken-1">Assigned By</th>
+                        <th class="border-primary border-darken-1">Assigned Date</th>
+                        <th class="border-primary border-darken-1">Updated By</th>
+                        <th class="border-primary border-darken-1">Update Date</th>
+                        <th class="border-primary border-darken-1">DNCC Amount</th>
+                    </tr>
+                    </thead>
+                </table>
+
+            </div>
+        </div>
+    </div>
+
+
+@endsection
+
+@section('css')
+    <link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/forms/selects/select2.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/extensions/toastr.css')}}">
+
+    <style>
+        table.dataTable {
+            font-size: 12px;
+        }
+
+        table.dataTable thead tr th {
+            padding-left: 0.5em;
+            white-space: normal;
+            word-wrap: break-word;
+        }
+
+        table.dataTable thead tr th:before,
+        table.dataTable thead tr th:after {
+            height: 20px;
+            margin-bottom: -10px;
+            bottom: 50% !important;
+        }
+
+        table.dataTable tbody tr td {
+            padding-left: 0.5em;
+            padding-right: 0.5em;
+        }
+
+        table.dataTable tbody tr td.select-checkbox:before {
+            top: 50%;
+            border-color: #666EE8;
+        }
+
+        table.dataTable tbody tr.selected td.select-checkbox:after {
+            top: 50%;
+            text-shadow: none;
+        }
+
+        .btn-group .dropdown-menu .dropdown-item {
+            white-space: normal;
+        }
+
+        #toast-bottom-center.toast-container {
+            text-align: center;
+        }
+
+        #toast-bottom-center.toast-container .toast {
+            display: table;
+            width: auto !important;
+            text-align: left;
+        }
+    </style>
+@endsection
+
+@section('js')
+    <script src="{{asset('app-assets/vendors/js/forms/select/select2.full.min.js')}}" type="text/javascript"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+            var table = $('#datatable').DataTable({
+                dom: 'ltipr',
+                fixedHeader: {
+                    header: true,
+                    headerOffset: $('.header-navbar').height()
+                },
+                select: {
+                    info: false,
+                    style: 'multi',
+                    selector: 'td.select-checkbox',
+                    className: 'selected bg-primary bg-lighten-5 primary'
+                },
+                lengthMenu: [[25, 50, 100], [25, 50, 100]],
+                pageLength: 25,
+                stateSave: true,
+                pagingType: 'full_numbers',
+                processing: true,
+                serverSide: true,
+                ajax: '{{ route('admin.reports.completed_delivery_notes.list') }}',
+                order: [[2, 'asc']],
+                columns: [
+                    {orderable: false, searchable: false, name: 'serial_number', class: 'align-middle serial_number', targets: 0, render: function (data, type, row) {return '';}},
+                    { data:'delivery_note' ,name: 'delivery_note_id', class: 'align-middle text-center delivery_note'},
+                    { data:'hub' ,name: 'hub', class: 'align-middle hub'},
+                    { data:'rider' ,name: 'rider', class: 'align-middle rider'},
+                    { data:'route' ,name: 'route', class: 'align-middle route'},
+                    { data:'shipments_count' ,name: 'shipments_count', class: 'align-middle shipments_count'},
+                    { data:'delivered_shipments' ,name: 'delivered_shipments', class: 'align-middle delivered_shipments'},
+                    { data:'assignee' ,name: 'assignee', class: 'align-middle assignee'},
+                    { data:'created_at' ,name: 'created_at', class: 'align-middle created_at'},
+                    { data:'updated_by' ,name: 'updated_by', class: 'align-middle updated_by'},
+                    { data:'updated_at' ,name: 'updated_at', class: 'align-middle updated_at'},
+                    { data:'amount' ,name: 'amount', class: 'align-middle amount'},
+                ],
+                rowCallback: function(row, data, index) {
+                    var info = table.page.info();
+
+                    $('td:eq(0)', row).html(index + 1 + info.page * info.length);
+                },
+                initComplete: function() {
+                    var search = $('<tr role="row" class="bg-primary bg-lighten-1 search"></tr>').appendTo(this.api().table().header());
+
+                    var td = '<td style="padding:5px;" class="border-primary border-lighten-2"><fieldset class="form-group m-0 position-relative has-icon-right"></fieldset></td>';
+                    var input = '<input type="text" class="form-control form-control-sm input-sm primary">';
+                    var icon = '<div class="form-control-position primary"><i class="la la-search"></i></div>';
+
+                    this.api().columns().every(function(column_id) {
+                        var column = this;
+                        var header = column.header();
+
+                        if ($(header).is('.serial_number')) {
+                            $(td).appendTo($(search));
+                        }
+                        else {
+                            var current = $(input).appendTo($(search)).on('change', function() {
+                                column.search($(this).val(), false, false, true).draw();
+                            }).wrap(td).after(icon);
+
+                            if (column.search()) {
+                                current.val(column.search());
+                            }
+                        }
+                    });
+                }
+            });
+
+
+        });
+    </script>
+@endsection
