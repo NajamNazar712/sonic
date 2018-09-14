9fc1c2aa9f7e1c4d5b8be68d8bc5e1544008ce8b
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2018-09-14 18:17:47 +0500

    Fix
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- app/Http/Controllers/Admins/AdminPickupsController.php
+++ app/Http/Controllers/Admins/AdminPickupsController.php
@@ -666,7 +666,7 @@ class AdminPickupsController extends Controller
                           <tr>
                             <td class="color secondary"><strong>Rider Name</strong></td>
                             <td>' . $rider->name . '</td>
-                            <td rowspan="7" class="text-center align-middle">
+                            <td rowspan="7" class="text-center align-middle pl-1 pr-1">
                               <img src="data:image/png;base64,' . base64_encode($generator->getBarcode($id, $generator::TYPE_CODE_128, 2, 60)) . '" class="d-block mx-auto">
                               <span><strong>' . str_pad($id, 12, '0', STR_PAD_LEFT) . '</strong></span>
                             </td>
