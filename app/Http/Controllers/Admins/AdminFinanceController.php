f2fdc7e3c0e369710b06081661cce0e6ca7e0517
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2018-09-17 11:55:06 +0500

    Fix
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- app/Http/Controllers/Admins/AdminFinanceController.php
+++ app/Http/Controllers/Admins/AdminFinanceController.php
@@ -388,7 +388,7 @@ class AdminFinanceController extends Controller
         if ($shipment->exists()) {
             $shipment = $shipment->first();
 
-            if (!in_array($shipment->shipper_status_id, [39, 40, 41, 42])) {
+            if (!in_array($shipment->shipper_status_id, [14, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 44, 45, 46])) {
                 $details = array();
 
                 $shipper = $shipment->user;
