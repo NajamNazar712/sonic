42f93a826eb3fd50a556dd85d055232dbb86c1e6
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2018-09-28 20:48:10 +0500

    Fix
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- app/Http/Controllers/Admins/AdminCargoController.php
+++ app/Http/Controllers/Admins/AdminCargoController.php
@@ -185,7 +185,7 @@ class AdminCargoController extends Controller
           if (session('role_id') == 1 || (in_array($hub_id, session('hubs')))) {
             if ($shipment->pickup_address->city->hub_id != $shipment->consignee_city->hub_id) {
               if ($request->cargo_type != 0) {
-                if ($shipment->shipper_status_id != 2) {
+                if ($shipment->shipper_status_id == 2) {
                   $hub_id = $shipment->consignee_city->hub_id;
                 }
                 else {
