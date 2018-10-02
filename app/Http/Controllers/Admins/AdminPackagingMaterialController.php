8734bb4b5a9a472b8fc4b4f7d7a0ce70b0d4940c
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2018-10-02 17:26:59 +0500

    Fix
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- app/Http/Controllers/Admins/AdminPackagingMaterialController.php
+++ app/Http/Controllers/Admins/AdminPackagingMaterialController.php
@@ -296,39 +296,41 @@ class AdminPackagingMaterialController extends Controller
                    $shipment_consignee_name = "Packaging Material to $shipper_details->name";
                 $pickup_address = $pickup_address->first();
                }
-               $now = Carbon::today();
-               if($request_details->packaging_payment_mode_id == 1){
-                  $shipment = $this->book($request_details->user_id,1,$pickup_address->id,1,$request_details->city_id,$shipment_consignee_name,$request_details->address,$request_details->phone,null,null,null,0,$now,null,1,1,null,$request_details->amount,1,2,2);
-               }else{
-                 $shipment = $this->book($request_details->user_id,1,$pickup_address->id,1,$request_details->city_id,$shipment_consignee_name,$request_details->address,$request_details->phone,null,null,null,0,$now,null,1,1,null,0,1,2,2);
-               }
-               $new_tracking_number = $this->generate_tracking_number($shipment->id, $pickup_address->city_id, $request_details->city_id);
 
-               $item_description = '';
+               $now = Carbon::today();
+
+               $details = '';
 
                if ($request_details->small_flyers != 0) {
-                $item_description .= $request_details->small_flyers . ' Small Flyers, ';
+                $details .= $request_details->small_flyers . ' Small Flyers, ';
                }
 
                if ($request_details->medium_flyers != 0) {
-                $item_description .= $request_details->medium_flyers . ' Medium Flyers, ';
+                $details .= $request_details->medium_flyers . ' Medium Flyers, ';
                }
 
                if ($request_details->large_flyers != 0) {
-                $item_description .= $request_details->large_flyers . ' Large Flyers, ';
+                $details .= $request_details->large_flyers . ' Large Flyers, ';
                }
 
                if ($request_details->small_flyers != 0) {
-                $item_description .= $request_details->small_flyers . ' Small Flyers, ';
+                $details .= $request_details->small_flyers . ' Small Flyers, ';
                }
 
                if ($request_details->boxes != 0) {
-                $item_description .= $request_details->boxes . ' Boxes, ';
+                $details .= $request_details->boxes . ' Boxes, ';
                }
 
-               $item_description = substr($item_description, 0, -2);
+               $details = substr($details, 0, -2);
 
-               $this->add_item($shipment->id,24,$item_description,1,null,0,0);
+               if($request_details->packaging_payment_mode_id == 1){
+                  $shipment = $this->book($request_details->user_id,1,$pickup_address->id,1,$request_details->city_id,$shipment_consignee_name,$request_details->address,$request_details->phone,null,null,null,0,$now,null,1,1,null,$request_details->amount,1,2,2);
+               }else{
+                 $shipment = $this->book($request_details->user_id,1,$pickup_address->id,1,$request_details->city_id,$shipment_consignee_name,$request_details->address,$request_details->phone,null,null,null,0,$now,$details,1,1,null,0,1,2,2);
+               }
+               $new_tracking_number = $this->generate_tracking_number($shipment->id, $pickup_address->city_id, $request_details->city_id);
+
+               $this->add_item($shipment->id,24,$details,1,null,0,0);
 
                PackagingMaterialRequest::where('id',$request_id)->update([
                     'tracking_number'=>$new_tracking_number
