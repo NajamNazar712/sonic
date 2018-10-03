85d6ce074a312e02725cbef53329b9e7c8cef85d
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2018-10-03 18:07:52 +0500

    Fix
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- resources/views/admin/user_management/user/index.blade.php
+++ resources/views/admin/user_management/user/index.blade.php
@@ -43,8 +43,10 @@
 @endsection
 
 @section('css')
+	<link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/forms/selects/select2.min.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/extensions/toastr.css')}}">
-	</style>@endsection
+	</style>
+@endsection
 
 @section('js')
 	<script src="{{asset('app-assets/vendors/js/forms/select/select2.full.min.js')}}" type="text/javascript"></script>
