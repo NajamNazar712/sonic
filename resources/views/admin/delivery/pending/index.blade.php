52c4be60ba52f5d078f267f6bffe2889eab62e66
Muhammad Waqas <muhammad.waqas@trax.pk>
2018-10-03 17:27:28 +0500

    Fix

--- resources/views/admin/delivery/pending/index.blade.php
+++ resources/views/admin/delivery/pending/index.blade.php
@@ -42,8 +42,6 @@
                 </div>
 
 
-            </div>
-
 @endsection
 
 @section('css')
@@ -219,7 +217,7 @@
                 {data: 'status', name: 'status', class: 'align-middle status'},
                 {data: 'reason', name: 'ssr.name', class: 'align-middle reason'},
                 {data: 'remarks', name: 'shipments_journey.remarks', class: 'align-middle remarks'},
-                {data: 'arrival', name: 'arrival', class: 'align-middle arrival'},
+                {data: 'arrival', name: 'sj.created_at', class: 'align-middle arrival'},
                 {data: 'status_date', name: 'shipments_journey.created_at', class: 'align-middle status_date'},
                 {data: 'action', name: 'action', class: 'text-center align-middle action p-1', orderable: false, searchable: false}
 
