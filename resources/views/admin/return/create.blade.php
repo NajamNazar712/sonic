83c6a65c04b731265470eea8c968177a0063be64
Muhammad Waqas <muhammad.waqas@trax.pk>
2018-10-04 17:19:07 +0500

    Fix

--- resources/views/admin/return/create.blade.php
+++ resources/views/admin/return/create.blade.php
@@ -11,6 +11,7 @@
         <div class="card-content" aria-expanded="true">
             <div class="card-body">
                 @include('admin.inc.messages')
+                <form action="#" id="return_note_form"></form>
                 <div class="row justify-content-center mb-2">
                     <div class="col-3">
                         <fieldset>
@@ -222,8 +223,9 @@
                 $('#route').val(route).trigger('change');
             });
             $('input#scan_tracking').focus();
-            $('#scan_tracking').on('change',function () {
-                var scan = $(this);
+            $('#return_note_form').on('submit',function (e) {
+                e.preventDefault();
+                var scan = $('#scan_tracking');
                 var tracking = scan.val();
                 var hub_id = $('#hub_id').val();
 
