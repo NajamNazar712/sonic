2af5ec128fe3750eb69a08a87c1d8c7e63f888aa
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2018-10-12 10:24:22 +0500

    Completed
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- app/Http/Controllers/Admins/AdminFinanceController.php
+++ app/Http/Controllers/Admins/AdminFinanceController.php
@@ -940,11 +940,15 @@ class AdminFinanceController extends Controller
     public function make_payments_export_bank_order(Request $request) {
         $done_payment_ids = explode(',', $request->done_payment_ids);
 
+        $filename = 'sonic_bank_order';
+
         $details = array();
 
         $details[] = ['Payment ID', 'Client', 'Account Title', 'IBAN', 'Bank', 'Payable'];
 
         foreach ($done_payment_ids as $done_payment_id) {
+            $filename .= '_' . $done_payment_id;
+
             $done_payment = DonePayment::find($done_payment_id);
 
             $shipper = User::find($done_payment->user_id);
@@ -963,13 +967,18 @@ class AdminFinanceController extends Controller
             $details[] = $row;
         }
 
+        $filename .= '.xlsx';
+
         $spreadsheet = new Spreadsheet();
+
+        $spreadsheet->getActiveSheet()->getStyle('F')->getNumberFormat()->setFormatCode('#,##0');
+
         $spreadsheet->getActiveSheet()->fromArray($details);
 
         $writer = new Xlsx($spreadsheet);
 
         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        header('Content-Disposition: attachment;filename="sonic_bank_order.xlsx"');
+        header('Content-Disposition: attachment;filename="' . $filename .'"');
         header('Cache-Control: max-age=0');
 
         $writer->save('php://output');
@@ -1643,6 +1652,8 @@ class AdminFinanceController extends Controller
     public function done_payments_export_to_excel(Request $request) {
         $done_payment = DonePayment::find($request->id);
 
+        $filename = 'sonic_payment_details_' . $request->id . '.xlsx';
+
         $details = array();
 
         $details[] = ['S. No.', 'Tracking No.', 'Order ID', 'Consignee Name', 'Consignee Phone', 'Destination', 'Service Type', 'Actual Weight', 'Amount', 'Charges', 'Payable'];
@@ -1683,7 +1694,7 @@ class AdminFinanceController extends Controller
         $writer = new Xlsx($spreadsheet);
 
         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        header('Content-Disposition: attachment;filename="sonic_payment_details.xlsx"');
+        header('Content-Disposition: attachment;filename="' . $filename .'"');
         header('Cache-Control: max-age=0');
 
         $writer->save('php://output');
