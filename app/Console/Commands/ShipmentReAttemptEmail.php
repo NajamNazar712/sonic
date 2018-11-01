164432614b42c39dbd5db55ad6612ada43c009aa
salman-jafri <syed.salman@trax.pk>
2018-11-01 10:44:28 +0500

    Conflict

---
+++ app/Console/Commands/ShipmentReAttemptEmail.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use App\Http\Controllers\NotificationsController;
+
+class ShipmentReAttemptEmail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'email:shipmentreattempt';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send Shipment Re-Attempt Email';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        NotificationsController::send(25, 0);
+    }
+}
