a5089e5c4a08254f811f2c631a0ebdd8b50c2b90
Muhammad Waqas <muhammad.waqas@trax.pk>
2018-11-09 15:41:56 +0500

    Fix

--- database/seeds/UpdateMisroutedPermissionsTableSeeder.php
+++ database/seeds/UpdateMisroutedPermissionsTableSeeder.php
@@ -12,8 +12,7 @@ class UpdateMisroutedPermissionsTableSeeder extends Seeder
     public function run()
     {
         DB::table('module_permissions')->insert(array(
-            array('id' => 114, 'name' => 'Pending - View Rates', 'module_id' => 2),
-            array('id' => 115, 'name' => 'Active - View Rates', 'module_id' => 2),
+            array('id' => 119, 'name' => 'Misrouted History - View', 'module_id' => 6),
         ));
     }
 }
