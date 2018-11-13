ec321877657f143d55a20127beef4adbebfc34b8
salman-jafri <syed.salman@trax.pk>
2018-11-13 17:22:50 +0500

    search removed

--- resources/views/admin/cargo/in_transit.blade.php
+++ resources/views/admin/cargo/in_transit.blade.php
@@ -537,7 +537,7 @@
 						var column = this;
 						var header = column.header();
 
-						if ($(header).is('.serial_number') || $(header).is('.action')) {
+						if ($(header).is('.serial_number') || $(header).is('.aging') || $(header).is('.action')) {
 							$(td).appendTo($(search));
 						}else if($(header).is('.shipping_mode')){
                             $(mode_drop_select).appendTo($(search))
