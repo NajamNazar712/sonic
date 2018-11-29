d8ce1287153e71b448366b76e27892e343b7ce93
Muhammad Waqas <muhammad.waqas@trax.pk>
2018-11-29 16:44:21 +0500

    Fix

--- resources/views/admin/delivery/misroute/update.blade.php
+++ resources/views/admin/delivery/misroute/update.blade.php
@@ -84,7 +84,7 @@
                 scrollX: true,
                 "autoWidth": false,
                 paging:false,
-                orderable:false,
+                ordering:false,
                 columns: [
                     {orderable: false, searchable: false, name: 'serial_number', class: 'align-middle serial_number', targets: 0, render: function (data, type, row) {return '';}},
                     {name: 'tracking_number', class: 'align-middle tracking_number'},
