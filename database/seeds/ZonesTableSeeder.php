f8c1b3ca4c444e4da214d80f63d3d0842f056ef1
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2018-12-07 17:43:32 +0500

    Removing Zone Words
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- database/seeds/ZonesTableSeeder.php
+++ database/seeds/ZonesTableSeeder.php
@@ -16,9 +16,9 @@ class ZonesTableSeeder extends Seeder
         $timestamp = Carbon::now()->format('Y-m-d H:i:s');
         DB::table('zones')->truncate();
         DB::table('zones')->insert(array(
-            array('id' => 1, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'South Zone', 'gst' => 0.13),
-            array('id' => 2, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Central Zone', 'gst' => 0.16),
-            array('id' => 3, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'North Zone', 'gst' => 0.16)
+            array('id' => 1, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'South', 'gst' => 0.13),
+            array('id' => 2, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Central', 'gst' => 0.16),
+            array('id' => 3, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'North', 'gst' => 0.16)
         ));
     }
 }
