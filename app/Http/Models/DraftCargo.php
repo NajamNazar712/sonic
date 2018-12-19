7712ff797500262003c52438b0ddd43d9ed065c9
Anas_Anwer <anas.anwer@trax.pk>
2018-12-19 11:46:54 +0500

    migration and models

--- app/Http/Models/DraftCargo.php
+++ app/Http/Models/DraftCargo.php
@@ -1,10 +1,16 @@
 <?php
 
-namespace App;
+namespace App\Http\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class DraftCargo extends Model
 {
+    public function draft_cargo_shipment() {
+        return $this->hasMany('App\Http\Models\DraftCargoShipment');
+    }
     //
+    protected $fillable = [
+        'origin_id','destination_id','shipments_count','cargo_type','added_by'
+    ];
 }
--- app/Http/Models/DraftCargoShipment.php
+++ app/Http/Models/DraftCargoShipment.php
@@ -1,10 +1,13 @@
 <?php
 
-namespace App;
+namespace App\Http\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class DraftCargoShipment extends Model
 {
+    public function draft_cargo() {
+        return $this->belongsTo('App\Http\Models\DraftCargo');
+    }
     //
 }
