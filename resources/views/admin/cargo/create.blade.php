03dfa1f83b6f2d602e4382df26a06d87aa6aa382
Muhammad Waqas <muhammad.waqas@trax.pk>
2018-12-20 17:20:08 +0500

    Fix

--- resources/views/admin/cargo/create.blade.php
+++ resources/views/admin/cargo/create.blade.php
@@ -191,6 +191,7 @@
 			var table = $('#datatable').DataTable({
 				dom: 'ltipr',
 				scrollX: true,
+                "autoWidth": false,
                 paging:false,
                 columns: [
 					{name: 'serial_number', orderable: false, searchable: false, class: 'align-middle serial_number', targets: 1, render: function (data, type, row) {return '';}},
