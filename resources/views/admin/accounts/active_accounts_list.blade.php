0510652f3a87cf73e992a9edd73381e9ed51732b
Muhammad Waqas <muhammad.waqas@trax.pk>
2018-12-27 14:09:45 +0500

    Fix

--- resources/views/admin/accounts/active_accounts_list.blade.php
+++ resources/views/admin/accounts/active_accounts_list.blade.php
@@ -368,6 +368,7 @@
                         }
                         $('#saletag').val('').trigger('change');
                         $('#SalesTagModal').modal('hide');
+                        table.draw(true);
                     });
             }else{
                 var error = "Sales Person Not Selected!";
