1259e2d3f1432484eef97c65cd84847128133f37
Anas_Anwer <anas.anwer@trax.pk>
2018-12-28 18:13:01 +0500

    done

--- app/Http/Controllers/Admins/AdminDashboardController.php
+++ app/Http/Controllers/Admins/AdminDashboardController.php
@@ -873,7 +873,7 @@ class AdminDashboardController extends Controller
             $discount = DiscountCharge::all()->where('user_id', $id)->groupBy('shipping_mode_id');
             $rate_status = $user['rate_status'];
         }
-        elseif(($user['rate_status']>=1) && $user['status']==3){
+        elseif(($user['rate_status']>=0) && $user['status']==3){
             $switches = PendingRateStatus::all()->where('user_id', $id)->groupBy('shipping_mode_id');
 //        return $switches;
 //        var_dump(empty($switches));exit();
