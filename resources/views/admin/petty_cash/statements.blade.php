8d3575cce6a07d439df2687b6141e38b0be4893c
Muhammad Waqas <muhammad.waqas@trax.pk>
2019-01-03 10:47:26 +0500

    Fix

--- resources/views/admin/petty_cash/statements.blade.php
+++ resources/views/admin/petty_cash/statements.blade.php
@@ -29,15 +29,13 @@
                         <th class="border-primary border-darken-1">Statement No.</th>
                         <th class="border-primary border-darken-1">Hub</th>
                         <th class="border-primary border-darken-1">Statement Reference No.</th>
-                        <th class="border-primary border-darken-1">Date</th>
+                        <th class="border-primary border-darken-1">Date(From - To)</th>
                         <th class="border-primary border-darken-1">Created By</th>
                         <th class="border-primary border-darken-1">Created At</th>
                         <th class="border-primary border-darken-1">Station Approved By</th>
                         <th class="border-primary border-darken-1">Station Approved At</th>
                         <th class="border-primary border-darken-1">Operation Approved By</th>
                         <th class="border-primary border-darken-1">Operation Approved At</th>
-                        <th class="border-primary border-darken-1">Finance Approved By</th>
-                        <th class="border-primary border-darken-1">Finance Approved At</th>
                         <th class="border-primary border-darken-1">Status</th>
                         <th class="border-primary border-darken-1"></th>
 
@@ -89,8 +87,6 @@
                             head.push('Station Approved At');
                             head.push('Operation Approved By');
                             head.push('Operation Approved At');
-                            head.push('Finance Approved By');
-                            head.push('Finance Approved At');
                             head.push('Status');
 
 
@@ -109,8 +105,6 @@
                                 row.push(values.station_approved_at);
                                 row.push(values.operation_approved_by);
                                 row.push(values.operation_approved_at);
-                                row.push(values.finance_approved_by);
-                                row.push(values.finance_approved_at);
                                 row.push(values.status);
 
                                 body.push(row);
@@ -145,15 +139,13 @@
                     {data: 'statement_id', name: 'petty_cash_statements.id', class: 'align-middle statement_id'},
                     {data: 'hub_name', name: 'h.name', class: 'align-middle hub_name'},
                     {data: 'reference_no', name: 'petty_cash_statements.reference_no', class: 'align-middle reference_no'},
-                    {data: 'date', name: 'date', class: 'align-middle date'},
+                    {data: 'date', name: 'date', class: 'align-middle date', orderable:false},
                     {data: 'created_by', name: 'cb.name', class: 'align-middle created_by'},
                     {data: 'created_at', name: 'petty_cash_statements.created_at', class: 'align-middle created_at'},
                     {data: 'station_approved_by', name: 'sab.name', class: 'align-middle station_approved_by'},
                     {data: 'station_approved_at', name: 'petty_cash_statements.station_approved_at', class: 'align-middle station_approved_at'},
                     {data: 'operation_approved_by', name: 'oab.name', class: 'align-middle operation_approved_by'},
                     {data: 'operation_approved_at', name: 'petty_cash_statements.operation_approved_at', class: 'align-middle operation_approved_at'},
-                    {data: 'finance_approved_by', name: 'fab.name', class: 'align-middle finance_approved_by'},
-                    {data: 'finance_approved_at', name: 'petty_cash_statements.finance_approved_at', class: 'align-middle finance_approved_at'},
                     {data: 'status', name: 'petty_cash_statements.status', class: 'align-middle status'},
                     {data: 'action', name: 'action', class: 'text-center align-middle action p-1', orderable: false, searchable: false}
 
@@ -174,7 +166,6 @@
                         '<option value="0">Created</option>' +
                         '<option value="1">Station Approved</option>' +
                         '<option value="2">Operation Approved</option>' +
-                        '<option value="3">Finance Approved</option>' +
                         '</select>';
                     this.api().columns().every(function(column_id) {
                         var column = this;
