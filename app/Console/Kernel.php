a77668d1d019f7a28b10335f57f8a962dd0ca0a9
Muhammad Waqas <muhammad.waqas@trax.pk>
2019-01-03 12:01:07 +0500

    Working

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -31,7 +31,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('email:returnconfirmationpending')->dailyAt('10:00')->runInBackground();
         $schedule->command('email:returnconfirm')->dailyAt('15:00')->runInBackground();
         $schedule->command('email:shipmentreattempt')->dailyAt('08:00')->runInBackground();
-        $schedule->command('email:dailypickupsalescron')->dailyAt('08:00')->runInBackground();
+        $schedule->command('email:dailypickupsalescron')->dailyAt('12:00')->runInBackground();
         $schedule->command('shipment:cancel')->dailyAt('00:00')->runInBackground();
         $schedule->command('shipper:disable')->dailyAt('00:00')->runInBackground();
     }
