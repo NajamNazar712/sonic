b4bb5058bb722fa814c31eb50fb5982a750c3e9e
Anas_Anwer <anas.anwer@trax.pk>
2019-01-10 13:02:46 +0500

    in progress

--- resources/views/client/shipment/book/index.blade.php
+++ resources/views/client/shipment/book/index.blade.php
@@ -865,8 +865,8 @@
 					var present = [];
 					for(k=0;k<strArray.length;k++) {
 						for (i = 0; i < check.length; i++) {
-							if(JSON.stringify(strArray[k])=== JSON.stringify(check[i])){
-								present.push(check[i]);
+							if(JSON.stringify(strArray[k]).toLowerCase()=== JSON.stringify(check[i]).toLowerCase()){
+								present.push(strArray[k]);
 							}
 						}
 					}
@@ -894,6 +894,9 @@
 										);
 										form.submit();
 								});
+					}
+					else {
+						
 					}
 				}
 			});
