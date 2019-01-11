4267d8de90df073f9fe20b6efefa4f95927e12d7
Muhammad Waqas <muhammad.waqas@trax.pk>
2019-01-11 12:21:17 +0500

    Fixed

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -380,7 +380,7 @@
                 <li class=" nav-item"><a href="{{ route('admin.notifications.index') }}"><span class="menu-title">Notifications</span></a></li>
             @endif
 
-            @if (session('role_id') == 1 || count(array_intersect([104, 116, 152], session('permissions'))) !== 0)
+            @if (session('role_id') == 1 || count(array_intersect([104, 116, 151, 152], session('permissions'))) !== 0)
             <li class=" nav-item"><a href="#"><span class="menu-title" data-i18n="nav.dash.main">Settings</span></a>
                 <ul class="menu-content">
                     @if (session('role_id') == 1 || in_array(104, session('permissions')))
