375175fadc0e7b418f669e840a54d9858e43036c
Anas_Anwer <anas.anwer@trax.pk>
2019-01-11 13:25:24 +0500

    fixed

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -391,7 +391,7 @@
                         <li><a class="menu-item" href="{{route('admin.settings.shipment_cancellation_cut_off_days.index')}}">Shipment Cancellation Cut-Off Days</a></li>
                     @endif
 
-                    @if (session('role_id') == 1 || in_array(116, session('permissions')))
+                    @if (session('role_id') == 1 || in_array(149, session('permissions')))
                             <li><a class="menu-item" href="{{route('admin.settings.auto_account_disabled_days.auto_index')}}">Auto Account Disabled Days</a></li>
                     @endif
 
