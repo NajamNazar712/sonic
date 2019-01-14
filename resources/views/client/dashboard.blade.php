cde8d64ae9014fb98b67b067d46704f900d20f57
Anas_Anwer <anas.anwer@trax.pk>
2019-01-14 14:29:43 +0500

    Done

--- resources/views/client/dashboard.blade.php
+++ resources/views/client/dashboard.blade.php
@@ -42,6 +42,7 @@
                                 <th class="border-primary border-darken-1">Consignee Address</th>
                                 <th class="border-primary border-darken-1">Collection Amount</th>
                                 <th class="border-primary border-darken-1">Product Type</th>
+                                <th class="border-primary border-darken-1">Product Description</th>
                                 <th class="border-primary border-darken-1">Booking Date</th>
                                 <th class="border-primary border-darken-1">Instructions</th>
                                 <th class="border-primary border-darken-1"></th>
@@ -332,6 +333,7 @@
                     {data: 'consignee_address', name: 'shipments.consignee_address', class: 'align-middle consignee_address'},
                     {data: 'amount', name: 'shipments.amount', class: 'align-middle amount'},
                     {data: 'product_type', name: 'product_type', class: 'align-middle product_type'},
+                    {data: 'product_description', name: 'si.description', class: 'align-middle product_description'},
                     {data: 'booking_date', name: 'shipments.created_at', class: 'align-middle booking_date'},
                     {data: 'instructions', name: 'shipments.special_instructions', class: 'align-middle instructions'},
                     {data: 'action', name: 'action', class: 'text-center align-middle action p-1', orderable: false, searchable: false}
