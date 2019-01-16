321cfa5e187c3d110bd4b289320bb4fdc0a58f0e
Anas_Anwer <anas.anwer@trax.pk>
2019-01-16 11:09:39 +0500

    done

--- app/Http/Controllers/Admins/AdminReportsController.php
+++ app/Http/Controllers/Admins/AdminReportsController.php
@@ -3621,7 +3621,7 @@ class AdminReportsController extends Controller
             ->leftjoin('shipments_journey as sj', function($join){
                 $join->on('sj.shipment_id', '=', 's.id')
                     ->where('sj.id','=',
-                        DB::raw('(select max(id) from shipments_journey where shipments_journey.shipment_id = s.id and shipments_journey.verification = 1 and sj.reference_1_id = delivery_notes.id)'));
+                        DB::raw('(select max(id) from shipments_journey where shipments_journey.shipment_id = s.id and sj.reference_1_id = delivery_notes.id)'));
             })
             ->leftjoin('shipment_status as ss','ss.id','=','sj.shipper_status_id')
             ->select('s.tracking_number as tracking_no','s.tracking_number as tracking_number','delivery_notes.id as delivery_note_id','ss.name as status','ad.name as status_verified_by','delivery_notes.status_verified_at as status_verified_at','dns.call_verification as call_verification_status');
