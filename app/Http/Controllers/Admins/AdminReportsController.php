7558a7ddf08094b5a8454e60408c9100cdead92e
Anas_Anwer <anas.anwer@trax.pk>
2019-01-16 12:46:07 +0500

    done status

--- app/Http/Controllers/Admins/AdminReportsController.php
+++ app/Http/Controllers/Admins/AdminReportsController.php
@@ -3619,12 +3619,12 @@ class AdminReportsController extends Controller
             ->leftjoin('shipments as s', 's.id', '=', 'dns.shipment_id')
             ->leftjoin('admins as ad','ad.id','=','delivery_notes.verified_by')
             ->leftjoin('shipments_journey as sj', function($join){
-                $join->on('sj.shipment_id', '=', 's.id')
+                $join->on('sj.shipment_id', '=', 'dns.shipment_id')
                     ->where('sj.id','=',
-                        DB::raw('(select max(id) from shipments_journey where shipments_journey.shipment_id = s.id and sj.reference_1_id = delivery_notes.id)'));
+                        DB::raw('(select max(id) from shipments_journey where shipments_journey.shipment_id = dns.shipment_id and shipments_journey.reference_1_id = dns.delivery_note_id and shipments_journey.verification = 1)'));
             })
             ->leftjoin('shipment_status as ss','ss.id','=','sj.shipper_status_id')
-            ->select('s.tracking_number as tracking_no','s.tracking_number as tracking_number','delivery_notes.id as delivery_note_id','ss.name as status','ad.name as status_verified_by','delivery_notes.status_verified_at as status_verified_at','dns.call_verification as call_verification_status');
+            ->select('sj.shipper_status_id as shipper_status_id','s.id as ship_id','s.tracking_number as tracking_no','s.tracking_number as tracking_number','delivery_notes.id as delivery_note_id','ss.name as status','ad.name as status_verified_by','delivery_notes.status_verified_at as status_verified_at','dns.call_verification as call_verification_status');
         $datatable = Datatables::of($call_verification_report)
             ->editcolumn('call_verification_status', function ($data){
                 if($data->call_verification_status==0){
