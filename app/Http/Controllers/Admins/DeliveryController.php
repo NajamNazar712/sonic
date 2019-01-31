b0dc02ab366c0cf4b690187ba2b8c640f540cb3c
Muhammad Waqas <muhammad.waqas@trax.pk>
2019-01-31 12:36:54 +0500

    Fiux

--- app/Http/Controllers/Admins/DeliveryController.php
+++ app/Http/Controllers/Admins/DeliveryController.php
@@ -1444,8 +1444,8 @@ class DeliveryController extends Controller
         $delivery_note_id = $request->delivery_note_id;
         $shipment_count = 0;
         $dispute_shipments = array();
-        $delivered_status_array = array(14, 16, 30, 36, 37);
-        $return_status_array = array(21, 22, 23, 24, 25, 44);
+        $delivered_status_array = array(14, 26, 27, 28, 29, 30, 31, 32, 33, 34,35, 36, 37, 45, 46);
+        $return_status_array = array(21, 22, 23, 24, 25, 44, 47, 48);
         if ($delivery_note_id != '') {
             foreach ($shipments as $shipment) {
                 $in_new_delivery_note = DeliveryNoteShipment::where('delivery_note_id', '>', $delivery_note_id)->where('shipment_id', $shipment)->exists();
