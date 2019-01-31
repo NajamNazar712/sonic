8af838890e7e3209e6c0a754e98850f3cd2bad69
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2019-01-31 21:44:39 +0500

    Cargo Weight Increase
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- resources/views/admin/cargo/create.blade.php
+++ resources/views/admin/cargo/create.blade.php
@@ -390,7 +390,7 @@
 						'allowPlus': false,
 						'digits': 2,
 						'min': 0.1,
-						'max': 10000
+						'max': 100000
 					});
 
 					$.each(data.shipping_modes, function(index, shipping_mode) {
