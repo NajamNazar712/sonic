b95cfcf3802f297c626b54c8b7d168a4de5c563a
Anas_Anwer <anas.anwer@trax.pk>
2019-02-01 20:41:28 +0500

    done

--- resources/views/admin/reports/lead_time_report.blade.php
+++ resources/views/admin/reports/lead_time_report.blade.php
@@ -437,7 +437,7 @@
                     }
                 },
                 rowId: 'shipment_id',
-                order: [[10, 'asc']],
+                order: [[12, 'asc']],
                 columns: [
                     {orderable: false, searchable: false, name: 'serial_number', class: 'align-middle serial_number', targets: 0, render: function (data, type, row) {return '';}},
                     {data: 'tracking_number_link', name: 'shipments.tracking_number', class: 'align-middle tracking_number_link'},
