15957530862f7acb7365f0f69974e60e3b234fe0
Anas_Anwer <anas.anwer@trax.pk>
2019-02-01 09:42:06 +0500

    done

--- app/Http/Middleware/Permission.php
+++ app/Http/Middleware/Permission.php
@@ -300,8 +300,8 @@ class Permission
             'reports.petty_cash.index' => 156,
             'reports.petty_cash.list' => 156,
 
-            'reports.fake_status.index' => 156,
-            'reports.fake_status.list' => 156,
+            'reports.fake_status.index' => 169,
+            'reports.fake_status.list' => 169,
             'reports.fake_status.shipments.total' => 169,
             'reports.fake_status.shipments.undelivered' => 169,
             'reports.fake_status.fake_status_shipment' => 169,
