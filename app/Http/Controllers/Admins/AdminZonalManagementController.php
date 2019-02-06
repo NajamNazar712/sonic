0be95241dead6bbdd2741bd7da924756f7aa119a
Anas_Anwer <anas.anwer@trax.pk>
2019-02-06 14:25:54 +0500

    in progress

--- app/Http/Controllers/Admins/AdminZonalManagementController.php
+++ app/Http/Controllers/Admins/AdminZonalManagementController.php
@@ -19,9 +19,9 @@ use Carbon\Carbon;
 class AdminZonalManagementController extends Controller
 {
     public function __construct() {
-      $this->middleware('auth:admin');
+        $this->middleware('auth:admin');
 
-      $this->middleware('Permission');
+        $this->middleware('Permission');
     }
 
     public function index() {
@@ -32,29 +32,29 @@ class AdminZonalManagementController extends Controller
         $zones = Zone::select('zones.id', 'zones.created_at', 'zones.updated_at', 'zones.name', 'zones.gst');
 
         $datatables = Datatables::of($zones)
-        ->addColumn('action', function($zone) {
-            $edit_button = '<button type="button" class="dropdown-item edit"><div class="row no-gutters align-items-center"><div class="col-2"><i class="ft-edit"></i></div><div class="col-9 offset-1">Edit</div></button>';
-            $view_cities_button = '<button type="button" class="dropdown-item view_cities"><div class="row no-gutters align-items-center"><div class="col-2"><i class="ft-file-text"></i></div><div class="col-9 offset-1">View Cities</div></button>';
+            ->addColumn('action', function($zone) {
+                $edit_button = '<button type="button" class="dropdown-item edit"><div class="row no-gutters align-items-center"><div class="col-2"><i class="ft-edit"></i></div><div class="col-9 offset-1">Edit</div></button>';
+                $view_cities_button = '<button type="button" class="dropdown-item view_cities"><div class="row no-gutters align-items-center"><div class="col-2"><i class="ft-file-text"></i></div><div class="col-9 offset-1">View Cities</div></button>';
 
-            $dropdown = '
+                $dropdown = '
                 <div class="btn-group">
                   <button type="button" class="btn btn-sm btn-success dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Actions</button>
                   <div class="dropdown-menu dropdown-menu-sm">
             ';
 
-            if (session('role_id') == 1 || in_array(133, session('permissions'))) {
-                $dropdown .= $edit_button;
-            }
+                if (session('role_id') == 1 || in_array(133, session('permissions'))) {
+                    $dropdown .= $edit_button;
+                }
 
-            $dropdown .= $view_cities_button;
+                $dropdown .= $view_cities_button;
 
-            $dropdown .= '
+                $dropdown .= '
                   </div>
                 </div>
             ';
 
-            return $dropdown;
-        });
+                return $dropdown;
+            });
 
         return $datatables->make(true);
     }
@@ -79,6 +79,18 @@ class AdminZonalManagementController extends Controller
             $zone_class_city->zone_id = $zone->id;
             $zone_class_city->city_id = $city_id;
             $zone_class_city->class = $class;
+            $zone_class_city->account_type = 1;
+
+            $zone_class_city->save();
+        }
+
+        foreach ($request->city_class_cor as $city_id_cor => $class_cor) {
+            $zone_class_city = new ZoneClassCity();
+
+            $zone_class_city->zone_id = $zone->id;
+            $zone_class_city->city_id = $city_id_cor;
+            $zone_class_city->class = $class_cor;
+            $zone_class_city->account_type = 2;
 
             $zone_class_city->save();
         }
