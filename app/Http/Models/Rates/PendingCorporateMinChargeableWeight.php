407bc9d33abd32180325a1e21cc647f3198bd1ce
Anas_Anwer <anas.anwer@trax.pk>
2019-02-11 13:29:41 +0500

    fixed

--- app/Http/Models/Rates/PendingCorporateMinChargeableWeight.php
+++ app/Http/Models/Rates/PendingCorporateMinChargeableWeight.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class PendingCorporateMinChargeableWeight extends Model
 {
     //
+    protected $fillable = [
+        'user_id','shipping_mode_id','delivery_type_id','min_chargeable_weight'
+    ];
 }
--- resources/views/admin/accounts/corporate/edit_rates.blade.php
+++ resources/views/admin/accounts/corporate/edit_rates.blade.php
@@ -3028,6 +3028,7 @@
                             <div class="text-center mt-2">
                                 <div class="form-group">
                                     <input type="hidden" id="authorize" name="authorize">
+                                    <input type="hidden" name="approve" id="approve">
                                     <button id="addRatesSubmit" type="submit" class="btn btn-outline-success round btn-min-width mr-1 mb-1">Update Rates</button>
 
                                     @if ($shipper->status == 1 && (session('role_id') == 1 || in_array(8, session('permissions'))))
