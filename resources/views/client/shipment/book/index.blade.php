8cc80ff2974fc70eee64b9b709ea0f33da5270dd
Anas_Anwer <anas.anwer@trax.pk>
2019-02-12 14:58:57 +0500

    done

--- resources/views/client/shipment/book/index.blade.php
+++ resources/views/client/shipment/book/index.blade.php
@@ -105,7 +105,7 @@
 										</div>
 
 										<div class="form-group">
-											<textarea id="consignee_address" name="consignee_address" class="form-control" placeholder="Address*" data-rule-required="true" data-msg-required="Address is required" data-rule-maxlength="190" data-msg-maxlength="Address can be maximum 190 characters"></textarea>
+											<textarea id="consignee_address" name="consignee_address" class="form-control" rows="6" placeholder="Address*" data-rule-required="true" data-msg-required="Address is required" data-rule-maxlength="190" data-msg-maxlength="Address can be maximum 190 characters"></textarea>
 										</div>
 
 										<div class="form-group">
