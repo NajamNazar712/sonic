b9efd08c284dffcd23b1713ebb5e1a43ebe591d6
Anas_Anwer <anas.anwer@trax.pk>
2019-02-13 10:14:53 +0500

    fixed

--- app/Http/Controllers/Shippers/ShipperDashboardController.php
+++ app/Http/Controllers/Shippers/ShipperDashboardController.php
@@ -70,7 +70,7 @@ class ShipperDashboardController extends Controller
             })
             ->leftjoin('products as p','p.id','=','si.product_type_id')
             ->leftjoin('shipment_payment_status as sps', 'shipments.payment_status_id', '=' , 'sps.id')
-            ->select(['shipments_journey.remarks as cancellation_remarks', 'si.description as product_description','shipments.id as shipment_id','shipments.tracking_number as tracking_number','shipments.order_id','bt.booking_type as service_type','ss.name as status','oc.name as origin','dc.name as destination','shipments.consignee_name','shipments.consignee_phone_number_1 as phone1','shipments.consignee_phone_number_2 as phone2','shipments.consignee_address','shipments.amount','p.product_name as product_type','shipments.created_at as booking_date','shipments.special_instructions as instructions','shipments.shipper_status_id', 'sps.name as payment_status','ssr.name as reason'])
+            ->select(['shipments_journey.remarks as cancellation_remarks', 'si.description as product_description','shipments.id as shipment_id','shipments.tracking_number as tracking_number','shipments.order_id','bt.booking_type as service_type','ss.name as status','oc.name as origin','dc.name as destination','shipments.consignee_name','shipments.consignee_phone_number_1 as phone1','shipments.consignee_phone_number_2 as phone2','shipments.consignee_address','shipments.amount','p.product_name as product_type','shipments.created_at as booking_date','shipments.special_instructions as instructions','shipments.shipper_status_id', 'sps.name as payment_status','ssr.name as reason', 'shipments_journey.shipper_status_id as status_id'])
             ->where('shipments.user_id', session('user_id'))
             ->groupBy('shipments.id');
 
@@ -83,11 +83,11 @@ class ShipperDashboardController extends Controller
                 return $shipments->phone1."<br>".$shipments->phone2;
             })
             ->editColumn('cancellation_remarks',function ($shipments){
-                if($shipments->cancellation_remarks == null){
-                    return '-';
+                if($shipments->cancellation_remarks != null && $shipments->status_id == 17){
+                    return $shipments->cancellation_remarks;
                 }
                 else{
-                    return $shipments->cancellation_remarks;
+                    return '-';
                 }
             })
             ->filterColumn('phone', function ($query, $keyword) {
