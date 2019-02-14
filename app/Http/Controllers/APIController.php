cc4e9e98a87250b76714f2073dc1f971aceabd08
Anas_Anwer <anas.anwer@trax.pk>
2019-02-14 10:53:01 +0500

    done

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -545,10 +545,10 @@ class APIController extends Controller
         foreach ($shipment->items as $item) {
           $item_details = array();
 
+          $item_details['order_id'] = $shipment->order_id;
           $item_details['product_type'] = $item->product->product_name;
           $item_details['description'] = $item->description;
           $item_details['quantity'] = $item->quantity;
-          $item_details['order_id'] = $shipment->order_id;
 
           $details['order_information']['items'][] = $item_details;
         }
