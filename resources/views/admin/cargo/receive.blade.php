099baba50b9bcdecae2312d9033b3119a5d326b3
Muhammad Waqas <muhammad.waqas@trax.pk>
2019-02-16 00:51:08 +0500

    revert

--- resources/views/admin/cargo/receive.blade.php
+++ resources/views/admin/cargo/receive.blade.php
@@ -3,95 +3,95 @@
 @section('title', 'Receive Cargo')
 
 @section('content')
-	<div class="app-content content">
-		<div class="content-wrapper">
-			<div class="content-header row">
-			</div>
-			<div class="content-body">
-				<h1 class="mb-1">
-					Receive Cargo
-				</h1>
+    <div class="app-content content">
+        <div class="content-wrapper">
+            <div class="content-header row">
+            </div>
+            <div class="content-body">
+                <h1 class="mb-1">
+                    Receive Cargo
+                </h1>
 
-				<div class="card">
-					<div class="card-content" aria-expanded="true">
-						<div class="card-body">
-							@include('admin.inc.messages')
+                <div class="card">
+                    <div class="card-content" aria-expanded="true">
+                        <div class="card-body">
+                            @include('admin.inc.messages')
 
-							<div id="camera_scan" class="d-none">
-								<div id="camera_view" class="camera_view"></div>
-							</div>
+                            <div id="camera_scan" class="d-none">
+                                <div id="camera_view" class="camera_view"></div>
+                            </div>
 
-							<form id="add_shipment_form" class="form-inline mb-1 justify-content-center" novalidate="novalidate">
-								<div class="form-group">
-									<input type="text" name="tracking_number" class="form-control tracking_number" placeholder="Tracking Number*" data-rule-required="true" data-msg-required="Tracking Number is required">
+                            <form id="add_shipment_form" class="form-inline mb-1 justify-content-center" novalidate="novalidate">
+                                <div class="form-group">
+                                    <input type="text" name="tracking_number" class="form-control tracking_number" placeholder="Tracking Number*" data-rule-required="true" data-msg-required="Tracking Number is required">
 
-									<div class="d-inline-block ml-1">
-										<a href="#" id="camera_scan_initiate" tabindex="-1">
-											<i class="ft-camera h1"></i>
-										</a>
-									</div>
-								</div>
+                                    <div class="d-inline-block ml-1">
+                                        <a href="#" id="camera_scan_initiate" tabindex="-1">
+                                            <i class="ft-camera h1"></i>
+                                        </a>
+                                    </div>
+                                </div>
 
-								<div class="form-group ml-1">
-									<button type="submit" name="add" class="btn btn-primary" value="Add">Add</button>
-								</div>
-							</form>
+                                <div class="form-group ml-1">
+                                    <button type="submit" name="add" class="btn btn-primary" value="Add">Add</button>
+                                </div>
+                            </form>
 
-							<div id="information" class="information text-center">
-								Cargo No #{{ str_pad(session('cargo_consignment_id'), 6, '0', STR_PAD_LEFT) }} | Scanned: <span class="scanned">0</span>/<span class="total">{{ $total }}</span>
-							</div>
+                            <div id="information" class="information text-center">
+                                Cargo No #{{ str_pad(session('cargo_consignment_id'), 6, '0', STR_PAD_LEFT) }} | Scanned: <span class="scanned">0</span>/<span class="total">{{ $total }}</span>
+                            </div>
 
-							<table class="table table-bordered datatable" id="datatable" style="z-index: 3;">
-								<thead>
-								<tr role="row" class="bg-primary white">
-									<th class="border-primary border-darken-1">S. No.</th>
-									<th class="border-primary border-darken-1">Tracking Number</th>
-									<th class="border-primary border-darken-1">Origin</th>
-									<th class="border-primary border-darken-1">Destination</th>
-									<th class="border-primary border-darken-1">Hub</th>
-									<th class="border-primary border-darken-1">Consignee</th>
-									<th class="border-primary border-darken-1">Amount</th>
-									<th class="border-primary border-darken-1">Shipping Mode</th>
-									<th class="border-primary border-darken-1">Service Type</th>
-								</tr>
-								</thead>
-							</table>
+                            <table class="table table-bordered datatable" id="datatable" style="z-index: 3;">
+                                <thead>
+                                <tr role="row" class="bg-primary white">
+                                    <th class="border-primary border-darken-1">S. No.</th>
+                                    <th class="border-primary border-darken-1">Tracking Number</th>
+                                    <th class="border-primary border-darken-1">Origin</th>
+                                    <th class="border-primary border-darken-1">Destination</th>
+                                    <th class="border-primary border-darken-1">Hub</th>
+                                    <th class="border-primary border-darken-1">Consignee</th>
+                                    <th class="border-primary border-darken-1">Amount</th>
+                                    <th class="border-primary border-darken-1">Shipping Mode</th>
+                                    <th class="border-primary border-darken-1">Service Type</th>
+                                </tr>
+                                </thead>
+                            </table>
 
-							<form id="receive_form" class="form-horizontal text-center" method="POST" action="{{ route('admin.cargo.receive.store') }}" novalidate="novalidate">
-								{{ csrf_field() }}
+                            <form id="receive_form" class="form-horizontal text-center" method="POST" action="{{ route('admin.cargo.receive.store') }}" novalidate="novalidate">
+                                {{ csrf_field() }}
 
-								<input type="hidden" name="cargo_consignment_id" class="cargo_consignment_id" value="{{ session('cargo_consignment_id') }}">
+                                <input type="hidden" name="cargo_consignment_id" class="cargo_consignment_id" value="{{ session('cargo_consignment_id') }}">
 
-								<input type="hidden" name="short_received" class="short_received">
+                                <input type="hidden" name="short_received" class="short_received">
 
-								<input type="hidden" name="shipment_ids" class="shipment_ids">
+                                <input type="hidden" name="shipment_ids" class="shipment_ids">
 
-								<div class="form-group ml-1">
-									<button type="submit" name="receive" class="btn btn-primary receive" value="Confirm" disabled="disabled">Receive</button>
-								</div>
-							</form>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+                                <div class="form-group ml-1">
+                                    <button type="submit" name="receive" class="btn btn-primary receive" value="Confirm" disabled="disabled">Receive</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @section('css')
-	<link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/forms/selects/select2.min.css')}}">
-	<link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/extensions/toastr.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/forms/selects/select2.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/extensions/toastr.css')}}">
 @endsection
 
 @section('js')
-	<script src="{{asset('app-assets/vendors/js/forms/select/select2.full.min.js')}}" type="text/javascript"></script>
-	<script src="{{asset('app-assets/vendors/js/forms/extended/inputmask/jquery.inputmask.bundle.min.js')}}" type="text/javascript"></script>
-	<script src="{{asset('app-assets/vendors/js/forms/validation/jquery.validate.min.js')}}" type="text/javascript"></script>
-	<script src="{{asset('app-assets/vendors/js/extensions/toastr.min.js')}}" type="text/javascript"></script>
-	<script src="{{asset('app-assets/vendors/js/quagga/quagga.min.js')}}" type="text/javascript"></script>
-	<script src="{{asset('js/custom.js')}}" type="text/javascript"></script>
+    <script src="{{asset('app-assets/vendors/js/forms/select/select2.full.min.js')}}" type="text/javascript"></script>
+    <script src="{{asset('app-assets/vendors/js/forms/extended/inputmask/jquery.inputmask.bundle.min.js')}}" type="text/javascript"></script>
+    <script src="{{asset('app-assets/vendors/js/forms/validation/jquery.validate.min.js')}}" type="text/javascript"></script>
+    <script src="{{asset('app-assets/vendors/js/extensions/toastr.min.js')}}" type="text/javascript"></script>
+    <script src="{{asset('app-assets/vendors/js/quagga/quagga.min.js')}}" type="text/javascript"></script>
+    <script src="{{asset('js/custom.js')}}" type="text/javascript"></script>
 
-	<script>
+    <script>
         $(document).ready(function() {
             var cargo_consignment_id = {{ session('cargo_consignment_id') }};
 
@@ -100,23 +100,23 @@
             var table = $('#datatable').DataTable({
                 dom: 'ltipr',
                 scrollX: true,
-                autoWidth: false,
+                "autoWidth": false,
                 paging:false,
                 columns: [
-                    {name: 'serial_number', orderable: false, searchable: false, class: 'align-middle serial_number'},
-                    {name: 'tracking_number', class: 'align-middle tracking_number', orderable: false, searchable: false},
-                    {name: 'origin', class: 'align-middle origin', orderable: false, searchable: false},
-                    {name: 'destination', class: 'align-middle destination', orderable: false, searchable: false},
-                    {name: 'hub', class: 'align-middle hub', orderable: false, searchable: false},
-                    {name: 'consignee', class: 'align-middle consignee', orderable: false, searchable: false},
-                    {name: 'amount', class: 'align-middle amount', orderable: false, searchable: false},
-                    {name: 'shipping_mode', class: 'align-middle shipping_mode', orderable: false, searchable: false},
-                    {name: 'service_type', class: 'align-middle service_type', orderable: false, searchable: false}
+                    {name: 'serial_number', orderable: false, searchable: false, class: 'align-middle serial_number', targets: 1, render: function (data, type, row) {return '';}},
+                    {name: 'tracking_number', class: 'align-middle tracking_number'},
+                    {name: 'origin', class: 'align-middle origin'},
+                    {name: 'destination', class: 'align-middle destination'},
+                    {name: 'hub', class: 'align-middle hub'},
+                    {name: 'consignee', class: 'align-middle consignee'},
+                    {name: 'amount', class: 'align-middle amount'},
+                    {name: 'shipping_mode', class: 'align-middle shipping_mode'},
+                    {name: 'service_type', class: 'align-middle service_type'}
                 ],
                 rowCallback: function(row, data, index) {
-                    // var info = table.page.info();
-                    //
-                    // $('td:eq(0)', row).html(index + 1 + info.page * info.length);
+                    var info = table.page.info();
+
+                    $('td:eq(0)', row).html(index + 1 + info.page * info.length);
                 },
                 initComplete: function() {
                     this.api().table().columns.adjust();
@@ -159,10 +159,9 @@
                                     var index = $.inArray(id, shipment_ids);
 
                                     if (index === -1) {
-                                        var rowNo = table.rows().count();
-                                        table.row.add([rowNo + 1, data.details.tracking_number, data.details.origin, data.details.destination, data.details.hub, data.details.consignee, data.details.amount, data.details.shipping_mode, data.details.service_type]);
+                                        table.row.add([0, data.details.tracking_number, data.details.origin, data.details.destination, data.details.hub, data.details.consignee, data.details.amount, data.details.shipping_mode, data.details.service_type]);
                                         table.draw(false);
-                                        table.order([0, 'desc']).draw();
+
                                         shipment_ids.push(data.details.id);
 
                                         $('#information .scanned').html(shipment_ids.length);
@@ -280,5 +279,5 @@
 
             $('#add_shipment_form').trigger('submit');
         }
-	</script>
+    </script>
 @endsection
