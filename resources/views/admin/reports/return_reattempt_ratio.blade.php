d28539083b0ed123b0b1b9d8a825b9334166b82c
Muhammad Waqas <muhammad.waqas@trax.pk>
2019-02-15 12:09:25 +0500

    change filter

--- resources/views/admin/reports/return_reattempt_ratio.blade.php
+++ resources/views/admin/reports/return_reattempt_ratio.blade.php
@@ -267,7 +267,7 @@
                     }
                 },
                 rowId: 'shId',
-                order: [[6, 'asc']],
+                order: [[9, 'asc']],
                 columns: [
                     {orderable: false, searchable: false, name: 'serial_number', class: 'align-middle serial_number', targets: 0, render: function (data, type, row) {return '';}},
                     {data: 'tracking_number_link', name: 'shipments.tracking_number', class: 'align-middle tracking_number_link'},
