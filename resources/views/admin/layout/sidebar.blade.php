d14d4c6c7f161543a35357cfeeb1a1a8646d5389
Anas_Anwer <anas.anwer@trax.pk>
2019-02-20 16:13:41 +0500

    done

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -275,7 +275,7 @@
                 </li>
             @endif
 
-            @if (session('role_id') == 1 || count(array_intersect([64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 138, 148, 153, 156, 169, 170], session('permissions'))) !== 0)
+            @if (session('role_id') == 1 || count(array_intersect([64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 138, 148, 153, 156, 169, 170, 172], session('permissions'))) !== 0)
                 <li class=" nav-item"><a href="#"><span class="menu-title">Reports</span></a>
 
                     <ul class="menu-content">
@@ -350,7 +350,7 @@
                             <li><a class="menu-item" href="{{ route('admin.reports.debriefing.index') }}">Debriefing</a></li>
                         @endif
 
-                        @if (session('role_id') == 1 || in_array(170, session('permissions')))
+                        @if (session('role_id') == 1 || in_array(172, session('permissions')))
                             <li><a class="menu-item" href="{{ route('admin.reports.cargo_returns_shipment.index') }}">Cargo Returns Shipment</a></li>
                         @endif
 
