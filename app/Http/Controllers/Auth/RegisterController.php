a1fb9439d274eccfd1f8847280c916ead2e723bf
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2019-02-25 13:11:04 +0500

    Fix
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -97,6 +97,7 @@ class RegisterController extends Controller
                 'iban_no'=>'required|string|max:255',
                 'cycle_of_payment'=>'required|string|max:255',
                 'g-recaptcha-response' => 'required|captcha'
+            ]);
         }else{
             return Validator::make($data, [
                 'name' => 'required|string|max:255|unique:users',
@@ -130,8 +131,6 @@ class RegisterController extends Controller
                 'billing_person_email' => 'required|string|email|max:255',
                 'billing_address' => 'required|string|max:255',
                 'g-recaptcha-response' => 'required|captcha'
-
-        ]);
             ]);
         }
 
