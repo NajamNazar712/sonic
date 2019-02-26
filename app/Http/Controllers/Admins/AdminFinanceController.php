3965ffff338dd5b149c9044d77102b66fe672e64
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2019-02-26 14:01:50 +0500

    Fix
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- app/Http/Controllers/Admins/AdminFinanceController.php
+++ app/Http/Controllers/Admins/AdminFinanceController.php
@@ -3312,7 +3312,7 @@ class AdminFinanceController extends Controller
             }
 
             if ($generate) {
-                $pending_invoice_shipments = PendingInvoiceShipment::whereHas('shipment', function ($query) {
+                $pending_invoice_shipments = PendingInvoiceShipment::whereHas('shipment', function ($query) use ($user_id) {
                     $query->where('user_id', $user_id);
                 });
 
