e7e93667f6c4459aaba76f02f8e9079999cb37b1
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2019-02-26 13:58:34 +0500

    Fix

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,17 +12,11 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        'http://sonic.pk/admin/cargo/create/shipment_details',
-        'http://sonic.pk/admin/cargo/create/consignment_details',
-        'http://sonic.pk/admin/cargo/create/store',
-        'http://sonic.pk/admin/cargo/receive/shipment_details',
-        'http://sonic.pk/admin/cargo/receive/short_received',
-        'http://sonic.pk/admin/cargo/receive/store',
-        'http://sonic.pk/admin/delivery/note/shipment/info',
-        'http://sonic.pk/admin/delivery/note/create',
-        'http://sonic.pk/admin/return/create/shipment_details',
-        'http://sonic.pk/admin/return/create/note/submit',
-        'http://sonic.pk/admin/finance/make_payments/verify',
-        'http://sonic.pk/admin/finance/make_payments/store'
+        'admin/cargo/create/*',
+        'admin/cargo/receive/*',
+        'admin/delivery/note/shipment/info',
+        'admin/delivery/note/create',
+        'admin/return/create/*',
+        'admin/finance/make_payments/*'
     ];
 }
