bf6c5fec038e09379f8b8d2a689f40a1cbc1761a
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2019-03-01 10:07:46 +0500

    Fix
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- app/Http/Controllers/Admins/AdminWalkInBookShipmentController.php
+++ app/Http/Controllers/Admins/AdminWalkInBookShipmentController.php
@@ -106,12 +106,12 @@ class AdminWalkInBookShipmentController extends Controller
 
         $shipment_id = $shipment->id;
 
-        ShipmentsJourneyController::add($shipment_id, 1, 1, NULL, NULL, Auth::id(), NULL);
+        ShipmentsJourneyController::add($shipment_id, 1, 1, NULL, NULL, NULL, Auth::id());
 
-        ShipmentsJourneyController::add($shipment_id, 2, 2, NULL, NULL, Auth::id(), NULL);
+        ShipmentsJourneyController::add($shipment_id, 2, 2, NULL, NULL, NULL, Auth::id());
 
         if ($self_collection) {
-            ShipmentsJourneyController::add($shipment_id, 15, 15, NULL, NULL, Auth::id(), NULL);
+            ShipmentsJourneyController::add($shipment_id, 15, 15, NULL, NULL, NULL, Auth::id());
         }
 
         return $shipment_id;
