64af6650038137ffb4a95e80a54c3b229851180e
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2019-03-06 13:45:15 +0500

    Fix
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- app/Http/Middleware/Permission.php
+++ app/Http/Middleware/Permission.php
@@ -493,14 +493,6 @@ class Permission
             }
         }
         else if (Auth::guard('substitute_users')->check()) {
-            if (!session('account_type')) {
-                  Auth::guard('substitute_users')->logout();
-
-                  $request->session()->invalidate();
-
-                  return redirect()->route('cod.login');
-            }
-
             $action = str_replace('cod.', '', $request->route()->getName());
 
             if (session('user_type') == 1 || !isset($this->actions['shipper'][$action]) || in_array($this->actions['shipper'][$action], session('permissions'))) {
@@ -508,15 +500,6 @@ class Permission
             }
             else {
                 return redirect()->route('cod.access_denied');
-            }
-        }
-        else if (Auth::guard('web')->check()) {
-            if (!session('account_type')) {
-                  Auth::guard('web')->logout();
-
-                  $request->session()->invalidate();
-
-                  return redirect()->route('cod.login');
             }
         }
         else {
