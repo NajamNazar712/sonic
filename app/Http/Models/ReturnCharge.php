1b1d46b0f53cd99c239c50780f6924b02374c029
Muhammad Waqas <muhammad.waqas@trax.pk>
2019-03-13 10:07:52 +0500

    in progress

--- app/Http/Models/ReturnCharge.php
+++ app/Http/Models/ReturnCharge.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class ReturnCharge extends Model
 {
     protected $fillable = [
-        'user_id','shipping_mode_id','local','national'
+        'user_id','shipping_mode_id','local','national_charges_class_0','national_charges_class_1','national_charges_class_2','national_charges_class_3'
     ];
 }
