57d8584f40a6fa02ebdb5f66bf7ca59bc72a9dd1
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2019-03-18 13:03:15 +0500

    TO-834
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- app/Http/Controllers/Admins/ShipmentChargesController.php
+++ app/Http/Controllers/Admins/ShipmentChargesController.php
@@ -143,6 +143,10 @@ class ShipmentChargesController extends Controller
                         }
                     }
 
+                    if ($account_type_id == 2) {
+                        $charges = $charges * ROUND($shipment->actual_weight, 0);
+                    }
+
                     if ($charges < $discount) {
                         $shipment->weight_charges = ROUND($charges, 0, PHP_ROUND_HALF_DOWN);
                     }
@@ -667,6 +671,10 @@ class ShipmentChargesController extends Controller
                         else {
                             $charges = $weight_charge->national_charges_class_0;
                         }
+                    }
+
+                    if ($account_type_id == 2) {
+                        $charges = $charges * ROUND($shipment->actual_weight, 0);
                     }
 
                     $charges = ($charges * $replacement_multiplier);
