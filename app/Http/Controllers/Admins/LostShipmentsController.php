021bcc7359665bb27f3b9eb7425615bcff7067bf
Muhammad Waqas <muhammad.waqas@trax.pk>
2019-03-21 16:15:55 +0500

    fix

--- app/Http/Controllers/Admins/LostShipmentsController.php
+++ app/Http/Controllers/Admins/LostShipmentsController.php
@@ -56,8 +56,10 @@ class LostShipmentsController extends Controller
 //                ->whereRaw('IF (shipments.payment_status_id != NULL, (shipments.payment_status_id > 1), TRUE)')
                 ->where('shipments.shipper_status_id', 18)
                 ->where(function ($sub_query) {
-                    $sub_query->where('shipments.payment_status_id', '=', null)
-                        ->orWhere('shipments.payment_status_id', '>', 1);
+                    $sub_query->where('shipments.payment_status_id', '=', null);
+                })
+                ->orWhere(function ($sub_query) {
+                    $sub_query->where('shipments.payment_status_id', '>', 1);
                 });
 
             if (session('role_id') != 1) {
