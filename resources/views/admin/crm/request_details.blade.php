25b91881b04c4517a3fc2cf147f9492653c1cd86
Anas_Anwer <anas.anwer@trax.pk>
2019-03-28 12:22:32 +0500

    in progress

--- resources/views/admin/crm/request_details.blade.php
+++ resources/views/admin/crm/request_details.blade.php
@@ -69,6 +69,19 @@
                                             </tr>
                                             </tbody>
                                         </table>
+                                        @if($crm_details->agent['id'] == Auth::id() || in_array(184, session('permissions')))
+                                        <div class="text-center">
+                                            <form id="valid_invalid">
+                                                <input type="hidden" id="req_id" value="{{$crm_details->id}}">
+                                                <button id="valid" type="submit" class="btn btn-success mr-1 width-20-per" >
+                                                    <span class="d-none d-lg-block">Valid</span>
+                                                </button>
+                                                <button id="invalid" type="submit" class="btn btn-danger width-20-per" >
+                                                    <span class="d-none d-lg-block">Invalid</span>
+                                                </button>
+                                            </form>
+                                        </div>
+                                            @endif
                                     </div>
                                     <div class="col-6">
                                         <div class="content-body chat-application">
@@ -210,6 +223,50 @@
 
     <script type="text/javascript">
         $(document).ready(function() {
+
+            $('#valid').on('click', function (e) {
+                e.preventDefault();
+                $.ajax({
+                    url: '{!! route('admin.crm.valid') !!}',
+                    method: 'POST',
+                    data: {
+                        '_token': '{{ csrf_token() }}',
+                        'id': $('#req_id').val(),
+                        'status' : 2
+                    }
+                })
+                    .done(function(data) {
+                        if (data.status == 0) {
+                            toastr.success(data.success, 'Marked!', {positionClass: 'toast-top-center', containerId: 'toast-top-center'});
+                        }
+                        else {
+                            toastr.error(data.error, 'Error!', {positionClass: 'toast-top-center', containerId: 'toast-top-center'});
+                        }
+                    });
+                });
+
+            $('#invalid').on('click', function (e) {
+                e.preventDefault();
+                $.ajax({
+                    url: '{!! route('admin.crm.invalid') !!}',
+                    method: 'POST',
+                    data: {
+                        '_token': '{{ csrf_token() }}',
+                        'id': $('#req_id').val(),
+                        'status' : 4
+                    }
+                })
+                    .done(function(data) {
+                        if (data.status == 0) {
+                            toastr.success(data.success, 'Marked!', {positionClass: 'toast-top-center', containerId: 'toast-top-center'});
+                        }
+                        else {
+                            toastr.error(data.error, 'Error!', {positionClass: 'toast-top-center', containerId: 'toast-top-center'});
+                        }
+                    });
+                });
+
+
             $('#chat_form').on('submit',function (e) {
                 e.preventDefault();
             });
--- routes/web.php
+++ routes/web.php
@@ -1028,6 +1028,8 @@ Route::prefix('admin')->name('admin.')->group(function () {
             Route::get('list', 'Admins\AdminCRMController@closed_list')->name('list');
         });
         Route::post('assign', 'Admins\AdminCRMController@assign')->name('assign');
+        Route::post('valid', 'Admins\AdminCRMController@valid')->name('valid');
+        Route::post('invalid', 'Admins\AdminCRMController@invalid')->name('invalid');
         Route::prefix('comment')->name('comment.')->group(function(){
             Route::post('add', 'Admins\AdminCRMController@add_comment')->name('add');
             Route::post('get', 'Admins\AdminCRMController@get_latest_comment')->name('get');
