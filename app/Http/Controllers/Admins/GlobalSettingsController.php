47760f491e2665664972fdb41601b441e7a13b9b
Anas_Anwer <anas.anwer@trax.pk>
2019-04-10 18:32:18 +0500

    revert

--- app/Http/Controllers/Admins/GlobalSettingsController.php
+++ app/Http/Controllers/Admins/GlobalSettingsController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Admins;
 
-use App\Http\Models\Admin\FuelFactorHistory;
 use App\Http\Models\Admin\GlobalSettings;
 use App\Http\Models\Admin\NonServiceArea;
 use App\Http\Models\Admin\PettyCashAccountHead;
@@ -10,15 +9,9 @@ use App\Http\Models\Admin\PettyCashAccountHeadAccountTitle;
 use App\Http\Models\Admin\PettyCashAccountTitle;
 use App\Http\Models\Admin\WalkInStandardWeightCharge;
 
-use App\Http\Models\FuelSurcharge;
-use App\Http\Models\Rates\HistoryFuelSurcharge;
-use App\Http\Models\RateStatus;
-use App\Http\Models\Shipper\User;
-use App\Http\Models\ShippingMode;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Auth;
 use Yajra\Datatables\Datatables;
 
 class GlobalSettingsController extends Controller
@@ -227,7 +220,7 @@ class GlobalSettingsController extends Controller
                     return '';
                 }
             })
-        
+
         ->make(true);
     }
 
@@ -518,7 +511,7 @@ class GlobalSettingsController extends Controller
         return redirect()->back()->with('success', 'Settings Updated!');
     }
 
-	
+
 
 	public function debriefing_report_cut_off_time_index() {
         $settings = GlobalSettings::where('type', 'debriefing_report_arrival_cut_off_time')->first();
@@ -593,77 +586,5 @@ class GlobalSettingsController extends Controller
         $settings_due_date->save();
 
         return redirect()->back()->with('success', 'Settings Updated!');
-    }
-
-    public function fuel_factor_index(){
-        return view('admin.settings.fuel_factor');
-    }
-
-    public function fuel_factor_store(Request $request)
-    {
-        $fuel_factor = $request->fuel_factor;
-        if ($fuel_factor != null) {
-            $shipping_modes = ShippingMode::all();
-            $users = User::where('status', 3)->select('id')->get();
-            if (!$users->isEmpty()) {
-                foreach ($users as $user) {
-                    foreach($shipping_modes as $shipping_mode){
-                        $rate_status = RateStatus::where('user_id', $user->id)->where('shipping_mode_id',$shipping_mode->id);
-                        if($rate_status->exists()) {
-                            $rate_status = $rate_status->first();
-
-                            $fuel_surcharge = FuelSurcharge::where('user_id', $user->id)->where('shipping_mode_id',$shipping_mode->id);
-
-                            if ($fuel_surcharge->exists()) {
-                                $fuel_surcharge = $fuel_surcharge->first();
-
-                                if ($rate_status->fuel_charges == 1) {
-                                    $update_fuel_surcharge = $fuel_surcharge->fuel_surcharge + $fuel_factor;
-                                }
-                                else {
-                                    $update_fuel_surcharge = $fuel_factor;
-
-                                    $rate_status->fuel_charges = 1;
-                                    $rate_status->save();
-                                }
-
-                                if ($update_fuel_surcharge >= 0) {
-                                    $fuel_surcharge->fuel_surcharge = $update_fuel_surcharge;
-                                } else {
-                                    $fuel_surcharge->fuel_surcharge = 0;
-                                }
-                                $fuel_surcharge->save();
-
-                                $fuel_surcharge_history = new HistoryFuelSurcharge();
-                                $fuel_surcharge_history->user_id = $user->id;
-                                $fuel_surcharge_history->shipping_mode_id = $shipping_mode->id;
-                                $fuel_surcharge_history->fuel_surcharge = $update_fuel_surcharge;
-                                $fuel_surcharge_history->save();
-
-                            }
-                            else {
-                                $rate_status->fuel_charges = 1;
-                                $rate_status->save();
-
-                                $fuel_surcharge = new FuelSurcharge();
-                                $fuel_surcharge->user_id = $user->id;
-                                $fuel_surcharge->shipping_mode_id = $shipping_mode->id;
-                                $fuel_surcharge->fuel_surcharge = $fuel_factor;
-                                $fuel_surcharge->save();
-                            }
-                        }
-
-                    }
-                }
-                $fuel_factor_history = new FuelFactorHistory();
-                $fuel_factor_history->fuel_factor = $fuel_factor;
-                $fuel_factor_history->admin_id = Auth::id();
-                $fuel_factor_history->save();
-
-                return redirect()->back()->with('success', 'Fuel Factor Updated!');
-            } else {
-                return redirect()->back()->with('error', 'Fuel Factor failed to update!');
-            }
-        }
     }
 }
