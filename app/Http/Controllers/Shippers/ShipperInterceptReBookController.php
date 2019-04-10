7dc8f39b4c3ce433438f070b483bc3292291adde
Anas_Anwer <anas.anwer@trax.pk>
2019-04-10 12:51:06 +0500

    done

--- app/Http/Controllers/Shippers/ShipperInterceptReBookController.php
+++ app/Http/Controllers/Shippers/ShipperInterceptReBookController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Shippers;
 
+use App\Http\Controllers\ShipmentsJourneyController;
 use App\Http\Models\City;
 use App\Http\Models\InterceptReBookRequest;
 use App\Http\Models\Shipment;
@@ -34,25 +35,7 @@ class ShipperInterceptReBookController extends Controller
         if($shipment['consignee_city_id'] != $request->consignee_city || $shipment['consignee_name'] != $request->consignee_name || $shipment['consignee_address'] != $request->consignee_address  || $shipment['consignee_phone_number_1'] != $request->consignee_phone_number_1 || $shipment['consignee_phone_number_2'] != $request->consignee_phone_number_2 || $shipment['consignee_email'] != $request->consignee_email || $shipment['amount'] != $request->amount) {
             $intercept_request = InterceptReBookRequest::where(['shipment_id' => $request->shipment_id, 'status' => 0]);
             if($intercept_request->exists()) {
-
-                $intercept_request = $intercept_request->first();
-                if($intercept_request['consignee_city_id'] != $request->consignee_city || $intercept_request['consignee_name'] != $request->consignee_name || $intercept_request['consignee_address'] != $request->consignee_address  || $intercept_request['consignee_phone_number_1'] != $request->consignee_phone_number_1 || $shipment['consignee_phone_number_2'] != $request->consignee_phone_number_2 || $shipment['consignee_email'] != $request->consignee_email || $intercept_request['amount'] != $request->amount) {
-                    $intercept_request->update([
-                        'consignee_city_id' => $request->consignee_city,
-                        'consignee_name' => $request->consignee_name,
-                        'consignee_address' => $request->consignee_address,
-                        'consignee_phone_number_1' => $request->consignee_phone_number_1,
-                        'consignee_phone_number_2' => $request->consignee_phone_number_2,
-                        'consignee_email' => $request->consignee_email,
-                        'amount' => $request->amount,
-                        'shipper_id' => $user_id
-                    ]);
-
-                    return redirect()->route('cod.return.pending.index')->with('success', 'Intercept/Re-Book request updated for Tracking Number: ' . $shipment['tracking_number']);
-                }
-                else{
-                    return redirect()->back()->with('error', 'Intercept/Re-Book request is already requested with same details');
-                }
+                return redirect()->back()->with('error', 'Intercept/Re-Book is already requested against Tracking Number: ' . $shipment['tracking_number']);
             }
             else{
                 $new_intercept_request = InterceptReBookRequest::create([
@@ -67,11 +50,13 @@ class ShipperInterceptReBookController extends Controller
                     'shipper_id' => $user_id
                 ]);
 
-                return redirect()->route('cod.return.pending.index')->with('success', 'Intercept/Re-Book request submitted for Tracking Number: ' .$shipment['tracking_number']);
+                ShipmentsJourneyController::add($request->shipment_id, 53, 53, NULL, NULL, $user_id, NULL);
+
+                return redirect()->route('cod.return.pending.index')->with('success', 'Intercept/Re-Book request submitted against Tracking Number: ' . $shipment['tracking_number']);
             }
         }
         else{
-            return redirect()->back()->with('error', 'Shipment is already book with same details');
+            return redirect()->back()->with('error', 'Shipment is already book with same details against Tracking Number: ' . $shipment['tracking_number']);
         }
     }
 }
