c28eeddff7856f56e14a6265759f0161ea22a78f
Muhammad Waqas <muhammad.waqas@trax.pk>
2019-04-12 18:11:07 +0500

    fix

--- resources/views/admin/delivery/intercept/index.blade.php
+++ resources/views/admin/delivery/intercept/index.blade.php
@@ -504,14 +504,14 @@
                                     table.rows().deselect();
                                 }
                                 else{
-                                    toastr.error(error, 'Error!', {positionClass: 'toast-top-center', containerId: 'toast-top-center'});
+                                    toastr.error(data.error, 'Error!', {positionClass: 'toast-top-center', containerId: 'toast-top-center'});
                                 }
                             });
                     }
                 });
             }
 
-            function reject(selected_rows) {
+            function reject(ids) {
                 swal({
                     title: 'Are You Sure?',
                     text: 'Select confirm to Reject Intercept Request!',
@@ -539,19 +539,20 @@
                             url: '{!! route('admin.delivery.intercept.reject') !!}',
                             method: 'POST',
                             data: {
-                                'ids[]': selected_rows,
+                                'ids[]': ids,
                                 '_token': '{{ csrf_token() }}'
                             }
                         })
                             .done(function(data) {
                                 if(data.status == 0){
                                     toastr.success(data.success, 'Success!', {positionClass: 'toast-bottom-center', containerId: 'toast-bottom-center'});
-                                    setTimeout(function(){
-                                        window.location.reload();
-                                    },2000);
+                                    table.draw(false);
+                                    selected_rows = [];
+
+                                    table.rows().deselect();
                                 }
                                 else{
-                                    toastr.error(error, 'Error!', {positionClass: 'toast-top-center', containerId: 'toast-top-center'});
+                                    toastr.error(data.error, 'Error!', {positionClass: 'toast-top-center', containerId: 'toast-top-center'});
                                 }
                             });
                     }
