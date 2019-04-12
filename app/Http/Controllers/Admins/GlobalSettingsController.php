9421c3128093cd499b83a88f6bc6e403a431043d
Anas_Anwer <anas.anwer@trax.pk>
2019-04-12 15:58:03 +0500

    done

--- app/Http/Controllers/Admins/GlobalSettingsController.php
+++ app/Http/Controllers/Admins/GlobalSettingsController.php
@@ -650,10 +650,10 @@ class GlobalSettingsController extends Controller
     }
 
     public function cod_cap_zones_index(){
-        $class_a = GlobalSettings::where('type', 'cod_cap_for_zone_class_0')->first()();
-        $class_b = GlobalSettings::where('type', 'cod_cap_for_zone_class_1')->first()();
-        $class_c = GlobalSettings::where('type', 'cod_cap_for_zone_class_2')->first()();
-        $class_d = GlobalSettings::where('type', 'cod_cap_for_zone_class_3')->first()();
+        $class_a = GlobalSettings::where('type', 'cod_cap_for_zone_class_0')->first();
+        $class_b = GlobalSettings::where('type', 'cod_cap_for_zone_class_1')->first();
+        $class_c = GlobalSettings::where('type', 'cod_cap_for_zone_class_2')->first();
+        $class_d = GlobalSettings::where('type', 'cod_cap_for_zone_class_3')->first();
 
         return view('admin.settings.cod_cap_zone')->with(['class_a' => $class_a, 'class_b' => $class_b ,'class_c' => $class_c, 'class_d' => $class_d]);
     }
@@ -661,16 +661,16 @@ class GlobalSettingsController extends Controller
     public function cod_cap_zones_update(Request $request){
         if($request->class_a != null && $request->class_b != null && $request->class_c != null && $request->class_d != null)
         {
-            GlobalSettings::where('class', 0)->update([
+            GlobalSettings::where('type', 'cod_cap_for_zone_class_0')->update([
                 'setting_value' =>  $request->class_a
             ]);
-            GlobalSettings::where('class', 1)->update([
+            GlobalSettings::where('type', 'cod_cap_for_zone_class_1')->update([
                 'setting_value' =>  $request->class_b
             ]);
-            GlobalSettings::where('class', 2)->update([
+            GlobalSettings::where('type', 'cod_cap_for_zone_class_2')->update([
                 'setting_value' =>  $request->class_c
             ]);
-            GlobalSettings::where('class', 3)->update([
+            GlobalSettings::where('type', 'cod_cap_for_zone_class_3')->update([
                 'setting_value' =>  $request->class_d
             ]);
             return redirect()->back()->with('success', 'Settings Updated!');
--- database/seeds/UpdateGlobalSettingsTableCodCapForZone.php
+++ database/seeds/UpdateGlobalSettingsTableCodCapForZone.php
@@ -13,9 +13,9 @@ class UpdateGlobalSettingsTableCodCapForZone extends Seeder
     {
         $timestamp = \Carbon\Carbon::now()->format('Y-m-d H:i:s');
         DB::table('global_settings')->insert(array(
-            array('setting_value' => 30000, 'type' => 'cod_cap_for_zone_class_0', 'created_at' => $timestamp, 'updated_at' => $timestamp),
-            array('setting_value' => 30000, 'type' => 'cod_cap_for_zone_class_1', 'created_at' => $timestamp, 'updated_at' => $timestamp),
-            array('setting_value' => 30000, 'type' => 'cod_cap_for_zone_class_2', 'created_at' => $timestamp, 'updated_at' => $timestamp),
+            array('setting_value' => 1000000, 'type' => 'cod_cap_for_zone_class_0', 'created_at' => $timestamp, 'updated_at' => $timestamp),
+            array('setting_value' => 1000000, 'type' => 'cod_cap_for_zone_class_1', 'created_at' => $timestamp, 'updated_at' => $timestamp),
+            array('setting_value' => 1000000, 'type' => 'cod_cap_for_zone_class_2', 'created_at' => $timestamp, 'updated_at' => $timestamp),
             array('setting_value' => 30000, 'type' => 'cod_cap_for_zone_class_3', 'created_at' => $timestamp, 'updated_at' => $timestamp),
         ));
     }
