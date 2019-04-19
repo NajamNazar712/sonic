b24dc0ec97f195b56c91c1b19a372d6f0137061b
Anas_Anwer <anas.anwer@trax.pk>
2019-04-19 10:06:46 +0500

    sorter

--- resources/views/admin/reports/gst_report.blade.php
+++ resources/views/admin/reports/gst_report.blade.php
@@ -221,7 +221,6 @@
                         d.search_date_to = $('input[name="search_date_to_formatted"]').val();
                     }
                 },
-                order: [[4, 'desc']],
                 columns: [
                     {orderable: false, searchable: false, name: 'serial_number', class: 'align-middle serial_number', targets: 0, render: function (data, type, row) {return '';}},
                     {data: 'account_no', name: 'u.id', class: 'align-middle account_no'},
