7ecfc10300d5508e229ab77f13c70736ba9543c2
Anas_Anwer <anas.anwer@trax.pk>
2019-04-23 15:59:51 +0500

    done

--- resources/views/client/shipment/book/index.blade.php
+++ resources/views/client/shipment/book/index.blade.php
@@ -64,6 +64,14 @@
 											</div>
 
 											<div class="form-group">
+												<select name="new_pickup_city" class="select2" id="new_pickup_city" data-rule-required="true" data-msg-required="City is required">
+													@foreach($cities as $city)
+														<option value="{{ $city->id }}">{{ $city->name }}</option>
+													@endforeach
+												</select>
+											</div>
+
+											<div class="form-group">
 												<input type="text" name="new_pickup_person_of_contact" class="form-control" placeholder="Person of Contact*" data-rule-required="true" data-msg-required="Person of Contact is required" data-rule-maxlength="100" data-msg-maxlength="Person of Contact can be maximum 100 characters">
 											</div>
 
@@ -75,13 +83,6 @@
 												<input type="email" name="new_pickup_email_address" class="form-control" placeholder="Email Address*" data-rule-required="true" data-msg-required="Email Address is required">
 											</div>
 
-											<div class="form-group">
-												<select name="new_pickup_city" class="select2" id="new_pickup_city" data-rule-required="true" data-msg-required="City is required">
-													@foreach($cities as $city)
-														<option value="{{ $city->id }}">{{ $city->name }}</option>
-													@endforeach
-												</select>
-											</div>
 										</div>
 
 										<div id="info_display" class="form-group text-center p-1 border border-light rounded">
