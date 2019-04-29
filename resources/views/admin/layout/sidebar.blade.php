6b454bd2b39657a10656f3f40b11c656b93ded09
Anas_Anwer <anas.anwer@trax.pk>
2019-04-29 14:24:35 +0500

    done

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -11,7 +11,7 @@
 
             <li class=" nav-item"><a href="{{ route('admin.quick_tracking.index') }}"><span class="menu-title">Quick Tracking</span></a>
             </li>
-            @if(session('role_id') == 1 || session('department_id') == 3)
+            @if(session('role_id') == 1 || in_array(204, session('permissions')))
                 <li class=" nav-item"><a href="{{ route('admin.cx_quick_tracking.cx_index') }}"><span class="menu-title">CX Quick Tracking</span></a>
                 </li>
             @endif
