91de3fa2a4d36c4e00a1054c7ca932e216c175bd
Anas_Anwer <anas.anwer@trax.pk>
2019-04-30 12:23:48 +0500

    done

--- resources/views/admin/crm/request_details.blade.php
+++ resources/views/admin/crm/request_details.blade.php
@@ -828,26 +828,26 @@
                             if (user == 0) {
                                 if (data.comment.comment_type == 0) {
 
-                                    var html = '<div class="chat admin internal"><div class="chat-avatar"><div class="badge block badge-admin"><i class="la la-user font-medium-2"></i>You</div></div><div class="chat-body"><div class="chat-content"><p>' + data.comment.comment + '</p></div></div></div>';
+                                    var html = '<div class="chat admin internal"><div class="chat-avatar"><div class="badge block badge-admin"><i class="la la-user font-medium-2"></i>You</div></div><div class="chat-body"><div class="chat-content"><p>' + data.comment.comment + '</p><small>just now</small></div></div></div>';
                                 } else {
-                                    var html = '<div class="chat admin internal"><div class="chat-avatar"><div class="badge block badge-admin"><i class="la la-user font-medium-2"></i>' + name + '</div></div><div class="chat-body"><div class="chat-content"><p>' + data.comment.comment + '</p></div></div></div>';
+                                    var html = '<div class="chat admin internal"><div class="chat-avatar"><div class="badge block badge-admin"><i class="la la-user font-medium-2"></i>' + name + '</div></div><div class="chat-body"><div class="chat-content"><p>' + data.comment.comment + '</p><small>just now</small></div></div></div>';
                                 }
                                 $('section.chat-app-window .chats').append(html);
 
                             } else if (user == 1) {
                                 if ($('div.chat:last-child').hasClass('shipper')) {
-                                    var html = '<div class="chat-content"><p>' + data.comment.comment + '</p></div>';
+                                    var html = '<div class="chat-content"><p>' + data.comment.comment + '</p><small>just now</small></div>';
                                     $('div.chat:last-child').find('.chat-body').append(html);
                                 } else {
-                                    var html = '<div class="chat chat-left shipper"><div class="chat-avatar"><div class="badge block badge-info"><i class="la la-user font-medium-2"></i>' + name + '</div></div><div class="chat-body"><div class="chat-content"><p>' + data.comment.comment + '</p></div></div></div>';
+                                    var html = '<div class="chat chat-left shipper"><div class="chat-avatar"><div class="badge block badge-info"><i class="la la-user font-medium-2"></i>' + name + '</div></div><div class="chat-body"><div class="chat-content"><p>' + data.comment.comment + '</p><small>just now</small></div></div></div>';
                                     $('section.chat-app-window .chats').append(html);
                                 }
                             } else {
                                 if ($('div.chat:last-child').hasClass('substitute-user')) {
-                                    var html = '<div class="chat-content"><p>' + data.comment.comment + '</p></div>';
+                                    var html = '<div class="chat-content"><p>' + data.comment.comment + '</p><small>just now</small></div>';
                                     $('div.chat:last-child').find('.chat-body').append(html);
                                 } else {
-                                    var html = '<div class="chat chat-left substitute-user"><div class="chat-avatar"><div class="badge block badge-substitute-user"><i class="la la-user font-medium-2"></i>' + name + '</div></div><div class="chat-body"><div class="chat-content"><p>' + data.comment.comment + '</p></div></div></div>';
+                                    var html = '<div class="chat chat-left substitute-user"><div class="chat-avatar"><div class="badge block badge-substitute-user"><i class="la la-user font-medium-2"></i>' + name + '</div></div><div class="chat-body"><div class="chat-content"><p>' + data.comment.comment + '</p><small>just now</small></div></div></div>';
                                     $('section.chat-app-window .chats').append(html);
                                 }
 
--- resources/views/client/crm/details.blade.php
+++ resources/views/client/crm/details.blade.php
@@ -316,18 +316,18 @@
                         if(data.status){
                             if(data.comment.comment_by === 0){
                                 if($('div.chat:last-child').hasClass('admin')){
-                                    var html = '<div class="chat-content mr-3"><p>'+ data.comment.comment +'</p></div>';
+                                    var html = '<div class="chat-content mr-3"><p>'+ data.comment.comment +'</p><small>just now</small></div>';
                                     $('div.chat:last-child').find('.chat-body').append(html);
                                 }else{
-                                    var html = '<div class="chat chat-left admin"><div class="chat-avatar"><div class="badge block badge-admin"><i class="la la-user font-medium-2"></i>Agent</div></div><div class="chat-body"><div class="chat-content"><p>' + data.comment.comment + '</p></div></div></div>';
+                                    var html = '<div class="chat chat-left admin"><div class="chat-avatar"><div class="badge block badge-admin"><i class="la la-user font-medium-2"></i>Agent</div></div><div class="chat-body"><div class="chat-content"><p>' + data.comment.comment + '</p><small>just now</small></div></div></div>';
                                     $('section.chat-app-window .chats').append(html);
                                 }
                             }else if(data.comment.comment_by === 2){
                                 if($('div.chat:last-child').hasClass('substitute-user')) {
-                                    var html = '<div class="chat-content"><p>' + data.comment.comment + '</p></div>';
+                                    var html = '<div class="chat-content"><p>' + data.comment.comment + '</p><small>just now</small></div>';
                                     $('div.chat:last-child').find('.chat-body').append(html);
                                 }else{
-                                    var html = '<div class="chat substitute-user"><div class="chat-avatar"><div class="badge block badge-substitute-user"><i class="la la-user font-medium-2"></i>'+ data.name +'</div></div><div class="chat-body"><div class="chat-content"><p>' + data.comment.comment + '</p></div></div></div>';
+                                    var html = '<div class="chat substitute-user"><div class="chat-avatar"><div class="badge block badge-substitute-user"><i class="la la-user font-medium-2"></i>'+ data.name +'</div></div><div class="chat-body"><div class="chat-content"><p>' + data.comment.comment + '</p><small>just now</small></div></div></div>';
                                     $('section.chat-app-window .chats').append(html);
                                 }
                             }
