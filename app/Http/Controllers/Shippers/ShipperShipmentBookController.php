3152a27286b3d5a91845d1066f810ddabf1187c6
Muhammad Waqas <muhammad.waqas@trax.pk>
2019-05-09 14:29:47 +0500

    fix

--- app/Http/Controllers/Shippers/ShipperShipmentBookController.php
+++ app/Http/Controllers/Shippers/ShipperShipmentBookController.php
@@ -534,6 +534,7 @@ class ShipperShipmentBookController extends Controller
 //    }
 
     public static function air_waybill($user_type, $user_id, $ids, $twice = FALSE) {
+
         $generator = new \Picqer\Barcode\BarcodeGeneratorPNG();
 
         if ($user_type == 3) {
@@ -543,7 +544,7 @@ class ShipperShipmentBookController extends Controller
             $user_name = User::find($user_id)->name . ' (Shipper)';
         }
         else if ($user_type == 2) {
-            $user_name = SubstituteUser::find($user_id)->name . ' (Sub-Shipper)';
+            $user_name = SubstituteUser::where('user_id',$user_id)->select('name')->first() . ' (Sub-Shipper)';
         }
         else if ($user_type == 4) {
             $user_name = User::find($user_id)->name . ' (API)';
@@ -986,6 +987,7 @@ class ShipperShipmentBookController extends Controller
     }
 
     public function print_air_waybill(Request $request) {
+
         $user_type = NULL;
         $user_id = NULL;
 
