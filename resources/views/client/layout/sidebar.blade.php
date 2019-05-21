c9099a4b3f36ebe221b8a746fae72296334d12bc
Muhammad Waqas <muhammad.waqas@trax.pk>
2019-05-21 09:27:54 +0500

    remove_link

--- resources/views/client/layout/sidebar.blade.php
+++ resources/views/client/layout/sidebar.blade.php
@@ -44,7 +44,7 @@
                 <li class=" nav-item"><a href="{{ route('cod.finance.payments.index') }}"><span class="menu-title" data-i18n="nav.dash.main">Finance Payments</span></a></li>
             @endif
 
-            <li class=" nav-item"><a href="https://form.jotform.me/81993400128456" target="_blank"><span class="menu-title" data-i18n="nav.dash.main">Complain Form</span></a></li>
+            {{--<li class=" nav-item"><a href="https://form.jotform.me/81993400128456" target="_blank"><span class="menu-title" data-i18n="nav.dash.main">Complain Form</span></a></li>--}}
 
             <li class=" nav-item"><a href="https://form.jotform.me/83261529672462" target="_blank"><span class="menu-title" data-i18n="nav.dash.main">Claims Form</span></a></li>
 
