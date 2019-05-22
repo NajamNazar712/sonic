202ed9bb17ee17d7744a456dbe3afe9103622cc3
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2019-05-22 13:25:57 +0500

    Done
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Http\Request;
 */
 
 Route::middleware('APIToken')->name('api.')->group(function () {
+	Route::post('validate', 'APIController@validate')->name('validate');
+
 	Route::get('pickup_addresses', 'APIController@pickup_addresses')->name('pickup_addresses');
 	Route::post('pickup_address/add', 'APIController@pickup_address_add')->name('pickup_address.add');
 
