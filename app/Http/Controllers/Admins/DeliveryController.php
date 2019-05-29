c9e18f65840b305f6e22b2a88b9b45f273cddcab
Muhammad Waqas <muhammad.waqas@trax.pk>
2019-05-29 10:59:43 +0500

    fix

--- app/Http/Controllers/Admins/DeliveryController.php
+++ app/Http/Controllers/Admins/DeliveryController.php
@@ -920,7 +920,12 @@ class DeliveryController extends Controller
             ->join('cities AS oc', 'shipments.consignee_city_id', '=', 'oc.id')
             ->join('booking_types as bt', 'bt.id', '=', 'shipments.booking_type_id')
             ->join('shipment_status as ss', 'ss.id', '=', 'shipments.shipper_status_id')
-            ->select(['delivery_notes.id as delivery_note', 'shipments.tracking_number', 'shipments.id as shId', 'oc.name as destination', 'shipments.consignee_name', 'shipments.consignee_address as address', 'shipments.amount', 'users.name as shipper', 'bt.booking_type as service_type', 'ss.name as current_status', 'ss.id as current_status_id', 'shipments.booking_type_id', 'usi.poc','shipments.shipper_status_id'])
+            ->leftjoin('crm_requests as crm', function ($join) {
+                $join->on('crm.shipment_id', '=', 'shipments.id')
+                    ->whereIn('crm.status_id', [2, 3, 5])
+                    ->where('crm.case_nature_id', 1);
+            })
+            ->select(['delivery_notes.id as delivery_note', 'shipments.tracking_number', 'shipments.id as shId', 'oc.name as destination', 'shipments.consignee_name', 'shipments.consignee_address as address', 'shipments.amount', 'users.name as shipper', 'bt.booking_type as service_type', 'ss.name as current_status', 'ss.id as current_status_id', 'shipments.booking_type_id', 'usi.poc','shipments.shipper_status_id','crm.id as complaint'])
             ->where('delivery_notes.id', $id);
 
         if (session('role_id') != 1) {
@@ -934,14 +939,13 @@ class DeliveryController extends Controller
                         $delivered_statuses = array(14, 30, 36);
                         if (in_array($deliveries->current_status_id, $delivered_statuses)) {
                             return 'statusDelivered';
-                        }
-                        else if($deliveries->current_status_id==12) {
+                        } else if ($deliveries->current_status_id == 12) {
                             return 'statusReturn';
-                        }else if($deliveries->current_status_id == 5){
-                            return 'complaint_row';
-                        }else {
+                        } else {
                             return 'statusUpdated';
                         }
+                    }else if($deliveries->complaint != null){
+                        return 'complaint_row';
                     } else {
                         return '';
                     }
