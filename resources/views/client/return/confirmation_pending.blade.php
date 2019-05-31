bfd541f21f6963169057880eb191d3ade6e8e445
Anas Anwer <anas.anwer95"gmail.com>
2019-05-31 16:19:05 +0500

    done

--- resources/views/client/return/confirmation_pending.blade.php
+++ resources/views/client/return/confirmation_pending.blade.php
@@ -862,8 +862,8 @@
                             }).done(function (data) {
                                 if(data.status == 1){
                                     var html = '';
-                                    html += 'OSA/NSA Status Shipment against Tracking Number: '+ data.nsa_shipment +'<br/>';
-                                    html += 'Estimated Charges will be: '+ data.estimated_charge +'<br/>';
+                                    html += 'Out of Service Area / Non Service Area Shipment against Tracking Number: '+ data.nsa_shipment +'<br/><br/>';
+                                    html += 'Estimated Charges will be: '+ data.estimated_charge +'<br/><br/>';
                                     html += 'Are you sure you want to deliver shipment with additional charges?';
                                     content = document.createElement('div');
                                     content.innerHTML = html;
