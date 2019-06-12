afc2d6a42ebccd10b251d59340d2e28fad82538c
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2019-06-12 12:34:38 +0500

    Fix
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- database/seeds/UpdateModulePermissionTableReplacementToRegularSeeder.php
+++ database/seeds/UpdateModulePermissionTableReplacementToRegularSeeder.php
@@ -14,7 +14,7 @@ class UpdateModulePermissionTableReplacementToRegularSeeder extends Seeder
         DB::table('module_permissions')->insert(array(
             array('id' => 206, 'name' => 'Replacement Not Collected', 'module_id' => 6),
             array('id' => 207, 'name' => 'Replacement Collected', 'module_id' => 6),
-            array('id' => 208, 'name' => 'Replacement to Regular logs', 'module_id' => 6)
+            array('id' => 208, 'name' => 'Replacement to Regular Logs', 'module_id' => 6)
         ));
     }
 }
