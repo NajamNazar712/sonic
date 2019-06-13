fa2f28fe1f8bb121e4ca6a5c4aad8dbe4f515cbe
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2019-06-13 10:53:42 +0500

    TO-1107
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- resources/views/admin/return/receive_status.blade.php
+++ resources/views/admin/return/receive_status.blade.php
@@ -170,6 +170,7 @@
                                     if (confirm) {
                                         var submit_all_status_flag = true;
                                         var not_updated_shipments = [];
+                                        var not_updated_shipment_ids = [];
 
                                         table.rows().nodes().each(function(index) {
                                             var row = table.row(index);
@@ -181,7 +182,8 @@
                                                 var receiver_name =  $(row.node()).find('td.received_or_refused_by input').val();
 
                                                 if ($.trim(receiver_name) == '') {
-                                                    not_updated_shipments.push($(row.node()).find('td.tracking_number').text()) ;
+                                                    not_updated_shipments.push($(row.node()).find('td.tracking_number').text());
+                                                    not_updated_shipment_ids.push(id);
                                                     submit_all_status_flag = false;
                                                 }
                                                 else {
@@ -191,31 +193,60 @@
                                             }
                                         });
                                         if (submit_all_status_flag == false) {
-                                            var html = '';
-
-                                            $.each(not_updated_shipments, function(index, tracking_number) {
-                                                html += tracking_number + '<br/>';
-                                            });
-
-                                            html += '<br/>Update Received By for all Returned Shipment(s)!';
-
-                                            content = document.createElement('div');
-                                            content.innerHTML = html;
                                             swal({
-                                                title: 'Names Not Updated',
-                                                content: content,
+                                                title: 'Enter Receiver Name for remaining Returned Shipment(s)',
+                                                content: {
+                                                    element: 'input',
+                                                    attributes: {
+                                                        placeholder: 'Receiver Name',
+                                                        type: 'text'
+                                                    }
+                                                },
                                                 icon: 'warning',
                                                 buttons: {
                                                     cancel: {
-                                                        text: 'Close',
+                                                        text: 'Cancel',
                                                         value: null,
                                                         visible: true,
                                                         closeModal: true,
                                                     },
+                                                    confirm: {
+                                                        text: 'Update',
+                                                        value: true,
+                                                        visible: true,
+                                                        closeModal: true
+                                                    }
                                                 },
                                                 closeOnClickOutside: false,
                                                 closeOnEsc: false,
                                                 dangerMode: true
+                                            }).then(function (receiver_name) {
+                                                if (receiver_name) {
+                                                    $.each(not_updated_shipment_ids, function(index, id) {
+                                                        shipment_received_refused_obj[id] = receiver_name;
+                                                    });
+
+                                                    $.ajax({
+                                                        url: '{!! route('admin.return.receive.status.delivered') !!}',
+                                                        method: 'POST',
+                                                        data: {
+                                                            'shipment_ids': selected_rows,
+                                                            'return_note_id': note_id,
+                                                            'remarks': shipment_remarks_obj,
+                                                            'received_or_refused_by': shipment_received_refused_obj,
+                                                            '_token': '{{ csrf_token() }}'
+                                                        }
+                                                    }).done(function (data) {
+                                                        if(data.status === 0){
+                                                            toastr.success(data.success, 'Success!', {positionClass: 'toast-bottom-center', containerId: 'toast-bottom-center'});
+
+                                                        }else{
+                                                            toastr.error(data.error, 'Error!', {positionClass: 'toast-top-center', containerId: 'toast-top-center'});
+
+                                                        }
+                                                        location.reload();
+                                                    });
+                                                }
                                             });
                                         }
                                         else {
