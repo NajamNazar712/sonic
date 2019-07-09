216d72ca6d360789e520978e818def0a1b39685d
Anas Anwer <anas.anwer95"gmail.com>
2019-07-09 15:04:58 +0500

    done

--- resources/views/admin/materials/warehouses/index.blade.php
+++ resources/views/admin/materials/warehouses/index.blade.php
@@ -462,7 +462,9 @@
                 }).done(function (data) {
                     if(data.status === 1){
                         toastr.success(data.success, 'Success!', {positionClass: 'toast-bottom-center', containerId: 'toast-bottom-center'});
-                        table.draw();
+                        setTimeout(function() {
+                            location.reload()
+                        }, 2500);
                     }else{
                         toastr.error(data.error, 'Error!', {positionClass: 'toast-top-center', containerId: 'toast-top-center'});
                     }
@@ -481,7 +483,9 @@
                 }).done(function (data) {
                     if(data.status === 1){
                         toastr.success(data.success, 'Success!', {positionClass: 'toast-bottom-center', containerId: 'toast-bottom-center'});
-                        table.draw();
+                        setTimeout(function() {
+                            location.reload()
+                        }, 2500);
                     }else{
                         toastr.error(data.error, 'Error!', {positionClass: 'toast-top-center', containerId: 'toast-top-center'});
                     }
