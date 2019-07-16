861a55fc5f5ff395ab2af8ff47d0dfc77f168db4
Anas Anwer <anas.anwer95"gmail.com>
2019-07-16 18:29:53 +0500

    done

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -603,7 +603,7 @@ class APIController extends Controller
 
           if ($msg_string != null) {
               NotificationsController::send(32, $shipment_id, $msg_string);
-              $msg_string = "A possible address anomaly: " . $msg_string . " Detected!";
+              $msg_string = "A Possible Address Anomaly: " . $msg_string . " Detected!";
               return response()->json(['status' => 0, 'message' => 'Shipment has been Booked!', 'tracking_number' => $tracking_number, 'non_service_area' => $msg_string . ' In case of, Out Of Service Area: Additional charges may apply and Non Service Area: Shipment may be returned. For assistance, Call: 021-38772222.']);
           }
           else{
