8fcb7dfbc06a9a1fd8ff77a6122905a5481822a6
Muhammad Waqas <muhammad.waqas@trax.pk>
2019-07-18 10:38:10 +0500

    unlink reasons

--- database/seeds/UpdateShipmentReasonConsigneeNotCooperating.php
+++ database/seeds/UpdateShipmentReasonConsigneeNotCooperating.php
@@ -13,5 +13,6 @@ class UpdateShipmentReasonConsigneeNotCooperating extends Seeder
     public function run()
     {
         DB::table('shipment_status_shipment_status_reason')->whereIn('shipment_status_reason_id', [21, 24])->delete();
+        DB::table('shipment_status_shipment_status_reason')->where('shipment_status_reason_id', 19)->where('shipment_status_id', 8)->delete();
     }
 }
