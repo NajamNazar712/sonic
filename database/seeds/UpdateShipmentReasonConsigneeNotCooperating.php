326e20118bf04450466bd26c7f7b25b46bad3cf1
Muhammad Waqas <muhammad.waqas@trax.pk>
2019-07-19 09:59:18 +0500

    done

--- database/seeds/UpdateShipmentReasonConsigneeNotCooperating.php
+++ database/seeds/UpdateShipmentReasonConsigneeNotCooperating.php
@@ -13,7 +13,15 @@ class UpdateShipmentReasonConsigneeNotCooperating extends Seeder
     public function run()
     {
         DB::table('shipment_status_shipment_status_reason')->whereIn('shipment_status_reason_id', [21, 24])->delete();
+
         DB::table('shipment_status_shipment_status_reason')->where('shipment_status_reason_id', 19)->where('shipment_status_id', 8)->delete();
+
+
         DB::table('shipment_status_shipment_status_reason')->where('shipment_status_reason_id', 9)->where('shipment_status_id', 8)->delete();
+
+        DB::table('shipment_status_shipment_status_reason')->whereIn('shipment_status_reason_id', [15, 17, 18])->where('shipment_status_id', 9)->delete();
+
+        DB::table('shipment_status_shipment_status_reason')->whereIn('shipment_status_reason_id', [15, 17, 18])->where('shipment_status_id', 15)->delete();
+
     }
 }
