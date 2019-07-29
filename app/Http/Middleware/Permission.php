c8396223fca8206641ab7633925cc039de7d8a33
Muhammad Waqas <muhammad.waqas@trax.pk>
2019-07-29 10:32:37 +0500

    fix

--- app/Http/Middleware/Permission.php
+++ app/Http/Middleware/Permission.php
@@ -464,7 +464,8 @@ class Permission
             'settings.cod_cap_zones.update' => 197,
 
 			'settings.stock_movement.index' => 228,
-            'settings.stock_movement.update' => 228,            'settings.delivery_call_verification_ratio.index' => 231,
+            'settings.stock_movement.update' => 228,
+            'settings.delivery_call_verification_ratio.index' => 231,
             'settings.delivery_call_verification_ratio.update' => 231,
 
 
