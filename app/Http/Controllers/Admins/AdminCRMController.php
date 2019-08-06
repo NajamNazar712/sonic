f21b12e984fe41ea1c0e261fd9d7b531d364eb36
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2019-08-06 18:32:04 +0500

    Fix
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- app/Http/Controllers/Admins/AdminCRMController.php
+++ app/Http/Controllers/Admins/AdminCRMController.php
@@ -307,8 +307,8 @@ class AdminCRMController extends Controller
             ->leftjoin('shipment_status as ss', 'ss.id', '=', 's.shipper_status_id')
             ->leftJoin('crm_request_agent_histories as res', function ($join) {
                 $join->on('res.crm_request_id', '=', 'crm_requests.id')
-                    ->where('res.created_at','=',
-                        DB::raw('(select max(created_at) from crm_request_agent_histories where crm_request_agent_histories.crm_request_id = crm_requests.id and crm_request_agent_histories.agent_id = crm_requests.agent_id)'));
+                    ->where('res.id','=',
+                        DB::raw('(select max(id) from crm_request_agent_histories where crm_request_agent_histories.crm_request_id = crm_requests.id and crm_request_agent_histories.agent_id = crm_requests.agent_id)'));
             })
             ->leftjoin('crm_comments as ccs', function($join){
                 $join->on('ccs.crm_request_id', '=', 'crm_requests.id')
@@ -534,8 +534,8 @@ class AdminCRMController extends Controller
             })
             ->leftJoin('crm_request_status_histories as res', function ($join) {
                 $join->on('res.crm_request_id', '=', 'crm_requests.id')
-                    ->where('res.created_at','=',
-                        DB::raw('(select max(created_at) from crm_request_status_histories where crm_request_status_histories.crm_request_id = crm_requests.id and crm_request_status_histories.status_id = 2)'));
+                    ->where('res.index','=',
+                        DB::raw('(select max(index) from crm_request_status_histories where crm_request_status_histories.crm_request_id = crm_requests.id and crm_request_status_histories.status_id = 2)'));
             })
             ->leftjoin('crm_comments as ccs', function($join){
                 $join->on('ccs.crm_request_id', '=', 'crm_requests.id')
