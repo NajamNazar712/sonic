af63f08ec164f99f9cdac284f6480569e9258041
Muhammad Waqas <muhammad.waqas@trax.pk>
2019-08-08 12:10:59 +0500

    fix

--- resources/views/admin/petty_cash/approved.blade.php
+++ resources/views/admin/petty_cash/approved.blade.php
@@ -140,7 +140,7 @@
                 serverSide: true,
                 ajax: '{{ route('admin.petty_cash.approved.list') }}',
                 rowId: 'statement_id',
-                order: [1, 'asc'],
+                order: [1, 'desc'],
                 columns: [
                     {orderable: false, searchable: false, name: 'serial_number', class: 'align-middle serial_number', targets: 0, render: function (data, type, row) {return '';}},
                     {data: 'statement_link', name: 'petty_cash_statements.id', class: 'align-middle statement_link'},
