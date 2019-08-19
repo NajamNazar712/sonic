6408edfdae2982f2321ef23e6241262c3d0b1856
Muhammad Waqas <muhammad.waqas@trax.pk>
2019-08-19 12:43:24 +0500

    delivery cound change

--- app/Http/Controllers/Admins/DeliveryController.php
+++ app/Http/Controllers/Admins/DeliveryController.php
@@ -423,7 +423,7 @@ class DeliveryController extends Controller
 
                         if(DeliveryNote::where('id', $old_delivery_note_id->delivery_note_id)->where('status',0)->exists()){
                             $journey = ShipmentsJourney::where('shipment_id',$shipment)->where('verification',0)->latest()->first();
-                            if($journey->count() > 0){
+                            if($journey){
                                 ShipmentsJourneyController::add($journey->shipment_id,$journey->shipper_status_id,$journey->consignee_status_id,$journey->status_reason_id,$journey->remarks,$journey->user_id,Auth::id(),$journey->reference_1_id,NULL,1,$journey->received_or_refused_by);
                             }
                         }
