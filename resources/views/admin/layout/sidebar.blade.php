7e06eb98846b83634e878e83e57d20a6b70075e7
Muhammad Waqas <muhammad.waqas@trax.pk>
2019-08-22 16:09:19 +0500

    fix

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -757,8 +757,10 @@
                                 </ul>
                             </li>
                         @endif
-                        @endif
+                    @endif
                     </ul>
+                </li>
+            @endif
                 <li class=" nav-item"><a href="{{route('admin.tracking.index')}}"><i class="la la-crosshairs"></i><span class="menu-title"
                                                                                                                         data-i18n="nav.dash.main">Tracking</span></a>
         </ul>
