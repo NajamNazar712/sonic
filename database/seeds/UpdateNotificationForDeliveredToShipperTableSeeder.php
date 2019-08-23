565392135b75f88ef4e58e0600ce6a730c62bd59
Anas Anwer <anas.anwer95"gmail.com>
2019-08-24 01:24:35 +0500

    working

---
+++ database/seeds/UpdateNotificationForDeliveredToShipperTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UpdateNotificationForDeliveredToShipperTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $timestamp = \Carbon\Carbon::now()->format('Y-m-d H:i:s');
+        DB::table('notifications')->insert(array(
+            array('id' => 32, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Return Delivered To Shipper', 'type_id' => 1, 'subject' => 'Shipment Return Delivered to Shipper [tracking_number]', 'body' => 'Tracking ID [tracking_number] has been returned to you on [status_updated_at] and received by [receiver_name]', 'updated_by' => 3, 'status' => 0)
+        ));
+    }
+}
