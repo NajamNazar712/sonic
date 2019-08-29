c2f30720099d8cbe4cc70ddbd3fc266862c25ce5
Muhammad Waqas <muhammad.waqas@trax.pk>
2019-08-29 12:27:00 +0500

    done

--- app/Http/Controllers/Admins/AdminFinanceController.php
+++ app/Http/Controllers/Admins/AdminFinanceController.php
@@ -3693,6 +3693,8 @@ class AdminFinanceController extends Controller
                             $total_adjusted_shipments++;
                         }
 
+                        self::adjustment_logs_done(2, $pending_invoice_shipment, $invoice_shipment->id);
+
                         $total_charges = $total_charges + $pending_invoice_shipment->charges;
                         $total_gst = $total_gst + $pending_invoice_shipment->gst;
                         $total_invoice_amount = $total_invoice_amount + $pending_invoice_shipment->invoice_amount;
