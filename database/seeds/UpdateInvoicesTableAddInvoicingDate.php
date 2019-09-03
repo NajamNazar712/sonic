486105e765ccb4d2aa53e489818f09e41ad66391
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2019-09-03 12:36:53 +0500

    Seeder Fix
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- database/seeds/UpdateInvoicesTableAddInvoicingDate.php
+++ database/seeds/UpdateInvoicesTableAddInvoicingDate.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Database\Seeder;
 
-use Carbon\Carbon;
-
 class UpdateInvoicesTableAddInvoicingDate extends Seeder
 {
     /**
@@ -13,21 +11,19 @@ class UpdateInvoicesTableAddInvoicingDate extends Seeder
      */
     public function run()
     {
-        var_dump(Carbon::now()->subMonth()->day(28)->startOfDay()->toDateString());
-        // var_dump(Carbon::now()->day(1)->subDay()->day(15)->startOfDay()->toDateString());
-     //    $invoices = DB::table('invoices');
+        $invoices = DB::table('invoices');
 
-     //    if ($invoices->exists()) {
-     //    	$invoices = $invoices->whereDate('invoicing_date', '0000-00-00')->get();
+        if ($invoices->exists()) {
+        	$invoices = $invoices->whereDate('invoicing_date', '0000-00-00')->get();
 
-     //    	foreach ($invoices as $invoice) {
-     //    		$date = Carbon::parse($invoice->billing_period_to_date)->subDay()->startOfDay()->toDateString();
+        	foreach ($invoices as $invoice) {
+        		$date = Carbon::parse($invoice->billing_period_to_date)->subDay()->startOfDay()->toDateString();
 
-     //    		$invoice->invoicing_date = $date;
-     //    		$invoice->billing_period_to_date = $date;
+        		$invoice->invoicing_date = $date;
+        		$invoice->billing_period_to_date = $date;
 
-     //    		$invoice->save();
-     //    	}
-    	// }
+        		$invoice->save();
+        	}
+    	}
     }
 }
