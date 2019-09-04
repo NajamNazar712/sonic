2f4bc8d1d87c90482ccc926c92c4d2dd95385379
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2019-09-04 16:21:33 +0500

    Fix
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- database/seeds/UpdateInvoicesTableAddInvoicingDate.php
+++ database/seeds/UpdateInvoicesTableAddInvoicingDate.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 
+use App\Http\Models\Invoice;
+
 use Carbon\Carbon;
 
 class UpdateInvoicesTableAddInvoicingDate extends Seeder
@@ -13,10 +15,10 @@ class UpdateInvoicesTableAddInvoicingDate extends Seeder
      */
     public function run()
     {
-        $invoices = DB::table('invoices');
+        $invoices = Invoice::whereDate('invoicing_date', '0000-00-00');
 
         if ($invoices->exists()) {
-        	$invoices = $invoices->whereDate('invoicing_date', '0000-00-00')->get();
+        	$invoices = $invoices->get();
 
         	foreach ($invoices as $invoice) {
         		$date = Carbon::parse($invoice->billing_period_to_date)->subDay()->startOfDay()->toDateString();
