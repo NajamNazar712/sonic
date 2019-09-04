192059d3f45ca5dacb32a27a111bbbcbf3599cf7
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2019-09-04 16:44:16 +0500

    Fix
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- app/Http/Controllers/Admins/AdminDashboardController.php
+++ app/Http/Controllers/Admins/AdminDashboardController.php
@@ -5849,7 +5849,7 @@ class AdminDashboardController extends Controller
 
                 $merged = MergedSisterAccount::where('user_id', $result->id);
                 if(!$merged->exists()){
-                    if(session('role_id') == 1 || session('role_id') == 4 || (($sale_check->user_id == Auth::id())|| in_array(241, session('permissions'))))
+                    if(session('role_id') == 1 || session('role_id') == 4 || (($sale_check) && ($sale_check->user_id == Auth::id()) || in_array(241, session('permissions'))))
                     {
                         $dropdown .= '<button onclick="location.href=\'' . route('admin.accounts.sister_account.add.account', ['id'=> $result->id]) . '\'" type="button" class="dropdown-item"><div class="row no-gutters align-items-center"><div class="col-2"><i class="ft-plus-circle"></i></div><div class="col-9 offset-1">Add Sister Account</div></button>';
                     }
