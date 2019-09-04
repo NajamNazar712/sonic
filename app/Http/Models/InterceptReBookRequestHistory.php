6792ca9f19d8ebc41f12ba49152569dd0a424eff
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2019-09-04 16:16:33 +0500

    TO-1471
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- app/Http/Models/InterceptReBookRequestHistory.php
+++ app/Http/Models/InterceptReBookRequestHistory.php
@@ -10,4 +10,11 @@ class InterceptReBookRequestHistory extends Model
     protected $fillable = [
         'shipment_id','old_consignee_city_id','new_consignee_city_id','old_consignee_name','new_consignee_name','old_consignee_address','new_consignee_address','old_consignee_phone_number_1','new_consignee_phone_number_1','old_consignee_phone_number_2','new_consignee_phone_number_2','old_consignee_email','new_consignee_email','old_amount','new_amount','shipper_id'
     ];
+
+    public function old_consignee_city() {
+        return $this->belongsTo('App\Http\Models\City', 'old_consignee_city_id', 'id');
+    }
+    public function new_consignee_city() {
+        return $this->belongsTo('App\Http\Models\City', 'new_consignee_city_id', 'id');
+    }
 }
