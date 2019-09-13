cf3ea30b91ee85f45a2fb362fb6e5c0f24f1f784
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2019-09-13 13:14:07 +0500

    TO-1502
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- app/Http/Controllers/Admins/AdminPickupsController.php
+++ app/Http/Controllers/Admins/AdminPickupsController.php
@@ -1998,7 +1998,9 @@ class AdminPickupsController extends Controller
             foreach ($pickup_request_all_booked_shipments as $all_shipments) {
                 $shipment = $all_shipments->shipment_id;
                 $shipment_details = Shipment::find($shipment);
-                $bookings[] = $shipment_details->tracking_number;
+                if ($shipment_details->shipment->shipper_status_id == 1) {
+                  $bookings[] = $shipment_details->tracking_number;
+                }
             }
 
             return ['status' => 0, 'success' => 'Booked Shipments', 'booked' => $bookings];
@@ -2019,7 +2021,9 @@ class AdminPickupsController extends Controller
             foreach ($pickup_request_all_booked_shipments as $all_shipments) {
                 $shipment = $all_shipments->shipment_id;
                 $shipment_details = Shipment::find($shipment);
-                $bookings[] = $shipment_details->tracking_number;
+                if ($shipment_details->shipment->shipper_status_id == 1) {
+                  $bookings[] = $shipment_details->tracking_number;
+                }
             }
 
             return ['status' => 0, 'success' => 'Pending Booked Shipments', 'booked' => $bookings];
