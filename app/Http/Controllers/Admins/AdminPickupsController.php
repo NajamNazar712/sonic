f35674ca08704b6d9c5df3d472499d1a51037973
Anas Anwer <anas.anwer95"gmail.com>
2019-09-16 15:05:31 +0500

    done

--- app/Http/Controllers/Admins/AdminPickupsController.php
+++ app/Http/Controllers/Admins/AdminPickupsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admins;
 
 use App\Http\Models\City;
+use App\Http\Models\ConsolidationShipments;
 use App\Http\Models\RiderCategory;
 use App\Http\Models\Shipper\User;
 use Illuminate\Http\Request;
@@ -1278,6 +1279,42 @@ class AdminPickupsController extends Controller
           $shipment->save();
 
           ShipmentsJourneyController::add($shipment_id, 2, 2, NULL, NULL, NULL, Auth::id(), $reference_1_id, $reference_2_id);
+
+            //Consolidated Shipments
+            $consolidated_shipment = ConsolidationShipments::where('shipment_id', $shipment_id)->first();
+            if($consolidated_shipment){
+                $check_all_consolidation_shipments = true;
+
+                $shipment->shipper_status_id = 58;
+                $shipment->consignee_status_id = 58;
+                $shipment->save();
+
+                ShipmentsJourneyController::add($shipment_id, 58, 58, NULL, NULL, NULL, Auth::id());
+
+                $consolidation_id = $consolidated_shipment->consolidation_id;
+                $remaining_consolidated_shipments = ConsolidationShipments::where('consolidation_id', $consolidation_id)->get();
+
+                foreach ($remaining_consolidated_shipments as $remaining_consolidated_shipment){
+                    $check_remaining_consolidated_shipment = Shipment::find($remaining_consolidated_shipment->shipment_id);
+                    if($check_remaining_consolidated_shipment->shipper_status_id != 58){
+                        $check_all_consolidation_shipments = false;
+                    }
+                }
+
+                if($check_all_consolidation_shipments == true){
+                    foreach ($remaining_consolidated_shipments as $update_remaining_consolidated_shipment){
+                        $update_all_consolidated_shipment = Shipment::find($update_remaining_consolidated_shipment->shipment_id);
+
+                        $update_all_consolidated_shipment->shipper_status_id = 59;
+                        $update_all_consolidated_shipment->consignee_status_id = 59;
+
+                        $update_all_consolidated_shipment->save();
+
+                        ShipmentsJourneyController::add($update_remaining_consolidated_shipment->shipment_id, 59, 59, NULL, NULL, NULL, Auth::id());
+                    }
+                }
+            }
+            //Consolidated Shipments
 
           NotificationsController::send(3, $shipment_id);
 
--- app/Http/Controllers/Shippers/ShipperConsolidatedController.php
+++ app/Http/Controllers/Shippers/ShipperConsolidatedController.php
@@ -45,15 +45,25 @@ class ShipperConsolidatedController extends Controller
                     $consolidated_shipment[$index] = $shipment->tracking_number;
                 }
                 else{
-                   if ($shipment->shipper_status_id == 1 && $first_shipment->booking_type_id == $shipment->booking_type_id && $first_shipment->consignee_name == $shipment->consignee_name && $first_shipment->consignee_address == $shipment->consignee_address && $first_shipment->consignee_phone_number_1 == $shipment->consignee_phone_number_1 && $first_shipment->consignee_city_id == $shipment->consignee_city_id) {
+                   if ($shipment->shipper_status_id == 1 && $shipment->booking_type_id == 1 && $first_shipment->booking_type_id == $shipment->booking_type_id && $first_shipment->consignee_name == $shipment->consignee_name && $first_shipment->consignee_address == $shipment->consignee_address && $first_shipment->consignee_phone_number_1 == $shipment->consignee_phone_number_1 && $first_shipment->consignee_city_id == $shipment->consignee_city_id) {
                         $shipments_info[$index] = $shipment;
                     }
                     else{
                         if($shipment->shipper_status_id == 1){
-                            return response()->json(['status'=>0, 'error'=>'Different Consignee Shipments selected']);
+                            if($shipment->booking_type_id == 1){
+                                return response()->json(['status'=>0, 'error'=>'Different Consignee Shipments selected']);
+                            }
+                            else{
+                                return response()->json(['status'=>0, 'error'=>'Service type must be Regular']);
+                            }
                         }
                         else{
-                            return response()->json(['status'=>0, 'error'=>'Shipment Status must be Shipment - Booked']);
+                            if($shipment->booking_type_id == 1){
+                                return response()->json(['status'=>0, 'error'=>'Service type must be Regular']);
+                            }
+                            else{
+                                return response()->json(['status'=>0, 'error'=>'Shipment Status must be Shipment - Booked']);
+                            }
                         }
                     }
                 }
