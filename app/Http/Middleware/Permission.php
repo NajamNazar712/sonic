ed98eeed5f356a66692584ab086a46fd23a503dd
Anas Anwer <anas.anwer95"gmail.com>
2019-09-20 14:55:56 +0500

    done

--- app/Http/Middleware/Permission.php
+++ app/Http/Middleware/Permission.php
@@ -516,6 +516,8 @@ class Permission
             'settings.crm_case_nature_types.index' => 260,
             'settings.crm_case_nature_types.list' => 260,
             'settings.crm_case_nature_types.store' => 260,
+            'settings.return_confirmation_pending_shipment_selection_time.index.list' => 253,
+            'settings.return_confirmation_pending_shipment_selection_time.store' => 253,
 
 
 
