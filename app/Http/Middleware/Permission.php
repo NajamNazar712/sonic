d9c23f6522569a6f3369cfa920f915bc62a24906
Anas Anwer <anas.anwer95"gmail.com>
2019-09-23 12:37:27 +0500

    done

--- app/Http/Middleware/Permission.php
+++ app/Http/Middleware/Permission.php
@@ -42,6 +42,8 @@ class Permission
 
             'daily_visit.index' => 265,
             'daily_visit.store' => 265,
+            'daily_visit.business_card' => 264,
+            'daily_visit.location_photo' => 264,
 
 
             'pickups.pending.index' => 17,
@@ -376,8 +378,6 @@ class Permission
             'reports.fake_status_shipments.list' => 263,
             'reports.daily_visit.index' => 264,
             'reports.daily_visit.list' => 264,
-            'reports.daily_visit.business_card' => 264,
-            'reports.daily_visit.location_photo' => 264,
 
             'packaging.index' => 76,
             'packaging.list' => 76,
