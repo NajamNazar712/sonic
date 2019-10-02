dcc9e5db4a88b1f33e0b25c9760b0ec3761d3a87
Anas Anwer <anas.anwer95"gmail.com>
2019-10-02 15:38:38 +0500

    done

--- app/Http/Controllers/Admins/AdminFinanceController.php
+++ app/Http/Controllers/Admins/AdminFinanceController.php
@@ -3217,7 +3217,7 @@ class AdminFinanceController extends Controller
 
         $rules = [
             'payment_id' => ['required', 'integer', 'digits_between:1,10', Rule::exists('done_payments', 'id')],
-            'status' => ['required', 'string', 'in:paid,Paid,Reverted,reverted'],
+            'status' => ['required', 'string', 'in:paid,Paid,Reverted,reverted,PAID,REVERTED'],
         ];
 
         $fields = [0 => 'payment_id', 1 => 'status'];
