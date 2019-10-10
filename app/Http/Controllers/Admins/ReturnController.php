84b88ecbcad92bfb3e459597d43f8c6890a2face
Muhammad Waqas <muhammad.waqas@trax.pk>
2019-10-10 12:06:09 +0500

    removed emails

--- app/Http/Controllers/Admins/ReturnController.php
+++ app/Http/Controllers/Admins/ReturnController.php
@@ -1602,7 +1602,7 @@ class ReturnController extends Controller
                         Shipment::where('id', $shipment)->update(['shipper_status_id' => 25, 'consignee_status_id' => 25]);
                         ReturnNoteShipment::where(['return_note_id' => $request->return_note_id, 'shipment_id' => $shipment])->update(['status' => 1]);
 
-                        NotificationsController::send(39, $shipment);
+                        // NotificationsController::send(39, $shipment);
 
 
                         $packaging_material_shipment = PackagingMaterialRequest::where('tracking_number', $parcel->tracking_number)->first();
@@ -1658,7 +1658,7 @@ class ReturnController extends Controller
 
                         Shipment::where('id', $shipment)->update(['shipper_status_id' => 25, 'consignee_status_id' => 25]);
                         ReturnNoteShipment::where(['return_note_id' => $request->return_note_id, 'shipment_id' => $shipment])->update(['status' => 1]);
-                        NotificationsController::send(39, $shipment);
+                        // NotificationsController::send(39, $shipment);
                     }
                 }
             }
@@ -1701,7 +1701,7 @@ class ReturnController extends Controller
                             ShipmentsJourneyController::add($shipment_id, 25, 25, NULL, ($request->has('remarks') ? $request->remarks[$shipment_id] : null), NULL, Auth::id(), $request->return_note_id, NULL, 1, ($request->has('received_or_refused_by') ? $request->received_or_refused_by[$shipment_id] : null));
 
                             Shipment::where('id', $shipment_id)->update(['shipper_status_id' => 25, 'consignee_status_id' => 25]);
-                            NotificationsController::send(39, $shipment_id);
+                            // NotificationsController::send(39, $shipment_id);
                         }
                         ReturnNoteShipment::where(['return_note_id' => $request->return_note_id, 'shipment_id' => $shipment_id])->update(['status' => 1]);
                     }
