274f4e322b6f2f3d888285383e2d1cee21a326cd
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2019-10-16 17:07:51 +0500

    Fix
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- database/seeds/UpdateModulePermissionTableAddRiderPickupAndRiperPickupActionLog.php
+++ database/seeds/UpdateModulePermissionTableAddRiderPickupAndRiperPickupActionLog.php
@@ -12,7 +12,7 @@ class UpdateModulePermissionTableAddRiderPickupAndRiperPickupActionLog extends S
     public function run()
     {
         DB::table('module_permissions')->insert(array(
-            array('id' => 271, 'name' => 'Rider - View', 'module_id' => 3)
+            array('id' => 271, 'name' => 'Rider - View', 'module_id' => 3),
             array('id' => 272, 'name' => 'Rider Action Log - View', 'module_id' => 3)
         ));
     }
