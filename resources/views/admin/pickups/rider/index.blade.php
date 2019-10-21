122fcf002d794dd6c9f3b36a228daf554817d3be
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2019-10-21 15:26:44 +0500

    Fixes
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- resources/views/admin/pickups/rider/index.blade.php
+++ resources/views/admin/pickups/rider/index.blade.php
@@ -204,7 +204,7 @@
 						var tracking_numbers = '';
 
 						$.each(data.tracking_numbers, function (index, tracking_number) {
-							tracking_numbers += '<a href="' + route + '?tracking_number=' + tracking_number + '" target="_blank">';
+							tracking_numbers += '<u><a href="' + route + '?tracking_number=' + tracking_number + '" target="_blank"></u><br/>';
 						});
 
 						$('#shipments_modal .modal-body').html(tracking_numbers);
