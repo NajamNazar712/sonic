113adebd5a47cc06ea7776a3ac05700e9c18c707
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2019-10-26 20:12:14 +0500

    Operations Forecast Resume
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -65,7 +65,7 @@ class Kernel extends ConsoleKernel
             $schedule->command('invoice:generate')->dailyAt($time)->runInBackground();
         }
 
-        // $schedule->command('hourlyupdate:operationforecast')->cron('0 */2 * * *')->withoutOverlapping()->runInBackground();
+        $schedule->command('hourlyupdate:operationforecast')->cron('0 */2 * * *')->withoutOverlapping()->runInBackground();
 
         $schedule->command('archive:returnnoteimage')->dailyAt('00:00')->runInBackground();
 
