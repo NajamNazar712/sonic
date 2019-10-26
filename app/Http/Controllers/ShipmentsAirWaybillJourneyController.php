5b1e82e449a2ad9dd8561fef2a77671fad98765a
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2019-10-26 19:55:06 +0500

    Removing Dependency on AWS SQS
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- app/Http/Controllers/ShipmentsAirWaybillJourneyController.php
+++ app/Http/Controllers/ShipmentsAirWaybillJourneyController.php
@@ -5,17 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-use App\Jobs\ProcessShipmentsAirWaybillJourney;
+use App\Http\Models\ShipmentsAirWaybillJourney;
 
 class ShipmentsAirWaybillJourneyController extends Controller
 {
     static public function add($shipment_id, $user_type, $user_id) {
-      $entry = array();
+		$shipment_air_waybill_journey = new ShipmentsAirWaybillJourney();
 
-      $entry['shipment_id'] = $shipment_id;
-      $entry['user_type'] = $user_type;
-      $entry['user_id'] = $user_id;
+		$shipment_air_waybill_journey->shipment_id = $shipment_id;
+		$shipment_air_waybill_journey->user_type = $user_type;
+		$shipment_air_waybill_journey->user_id = $user_id;
 
-      dispatch(new ProcessShipmentsAirWaybillJourney($entry));
+		$shipment_air_waybill_journey->save();
     }
 }
