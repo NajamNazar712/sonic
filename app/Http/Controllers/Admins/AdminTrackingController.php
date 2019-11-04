54b42b407c03c315a88493d0df83437e539420bc
Anas Anwer <anas.anwer95"gmail.com>
2019-11-04 11:16:44 +0500

    doned

--- app/Http/Controllers/Admins/AdminTrackingController.php
+++ app/Http/Controllers/Admins/AdminTrackingController.php
@@ -50,7 +50,7 @@ class AdminTrackingController extends Controller
                 $check = false;
                 if(session('department_id') == 7){
                     if(session('role_id') != 4 ){
-                        if(in_array($shipment->user->id, session('tagged_shippers'))){
+                        if(in_array($shipment->user->id, session('tagged_shippers')) || in_array(273, session('permissions'))){
                             $check = true;
                         }
                     }
