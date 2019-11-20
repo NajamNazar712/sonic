27bbdc5aa5216454e925ee54238820b1ce328080
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2019-11-20 16:01:45 +0500

    Command Changes
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -65,7 +65,7 @@ class Kernel extends ConsoleKernel
             $schedule->command('invoice:generate')->dailyAt($time)->runInBackground();
         }
 
-        $schedule->command('hourlyupdate:operationforecast')->cron('0 */2 * * *')->withoutOverlapping()->runInBackground();
+        // $schedule->command('hourlyupdate:operationforecast')->cron('0 */2 * * *')->withoutOverlapping()->runInBackground();
 
         $schedule->command('archive:returnnoteimage')->dailyAt('00:00')->runInBackground();
 
@@ -82,8 +82,8 @@ class Kernel extends ConsoleKernel
             $schedule->command('email:returndeliveredtoshipper')->dailyAt($rdts_time)->runInBackground();
         }
 
-        $schedule->command('pickuprequest:clear')->everyFiveMinutes()->withoutOverlapping()->runInBackground();
-        $schedule->command('pickupnote:clear')->everyFiveMinutes()->withoutOverlapping()->runInBackground();
+        $schedule->command('pickuprequest:clear')->everyFifteenMinutes()->withoutOverlapping()->runInBackground();
+        $schedule->command('pickupnote:clear')->everyThirtyMinutes()->withoutOverlapping()->runInBackground();
     }
 
     /**
