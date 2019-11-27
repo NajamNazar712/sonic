f57db29a8727f2a5c1a944d1bee2edddfbb55dfc
Anas Anwer <anas.anwer95"gmail.com>
2019-11-27 12:08:37 +0500

    done

--- app/Http/Controllers/Admins/AdminReportsEmailController.php
+++ app/Http/Controllers/Admins/AdminReportsEmailController.php
@@ -235,6 +235,7 @@ class AdminReportsEmailController extends Controller
                     ->where('sj.shipper_status_id', 2)
                     ->whereBetween('sj.created_at', [$new_date_from, $new_date_to]);
                     })
+                ->leftjoin('shipments as s', 's.id', '=', 'sj.shipment_id')
                 ->select('cities.id as origin_id', 'cities.name as origin', DB::raw('count(s.id) as shipment_count'), DB::raw('sum(s.weight_charges) as weight_charges'), DB::raw('sum(s.cash_handling_charges) as cash_handling_charges'), DB::raw('sum(s.insurance_charges) as insurance_charges'), DB::raw('sum(s.return_charges) as return_charges'), DB::raw('sum(s.fuel_surcharge) as fuel_surcharge'), DB::raw('sum(s.replacement_charges) as replacement_charges'), DB::raw('sum(s.try_and_buy_charges) as try_and_buy_charges'), DB::raw('sum(s.packaging_material_charges) as packaging_material_charges'), DB::raw('sum(s.intercept_charges) as intercept_charges'), DB::raw('sum(s.nsa_osa_charges) as nsa_osa_charges'))
                 ->groupBy('cities.id')
                 ->where('cities.pickup', 1)
