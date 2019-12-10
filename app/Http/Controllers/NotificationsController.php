434e5da95427c074c75e4cf9b796264fdef04797
Muhammad Waqas <muhammad.waqas@trax.pk>
2019-12-10 12:06:42 +0500

    email id added

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -2401,7 +2401,7 @@ class NotificationsController extends Controller
 
               $to = array();
 
-              $admins = Admin::whereIn('role_id', [2, 3, 4, 6, 20])->where('status', 1);
+              $admins = Admin::whereIn('role_id', [2, 3, 4, 6, 20, 22])->where('status', 1);
 
               if ($admins->exists()) {
                   $to = array_merge($to, $admins->pluck('email')->toArray());
