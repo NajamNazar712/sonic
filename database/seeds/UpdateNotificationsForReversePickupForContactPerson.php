6d81733b666c3ac22bafcd052c02974ba4e1cc78
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2019-12-16 17:12:29 +0500

    Fix
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- database/seeds/UpdateNotificationsForReversePickupForContactPerson.php
+++ database/seeds/UpdateNotificationsForReversePickupForContactPerson.php
@@ -14,7 +14,7 @@ class UpdateNotificationsForReversePickupForContactPerson extends Seeder
         $timestamp = \Carbon\Carbon::now()->format('Y-m-d H:i:s');
 
         DB::table('notifications')->insert(array(
-            array('id' => 52, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Reverse Pickup for Contact Person', 'type_id' => 2, 'subject' => NULL, 'body' => 'Dear [contact_person],' . PHP_EOL . 'A rider is on the way for pickup on behalf of [company_name].' . PHP_EOL . 'Rider: [rider_name], [rider_phone]' . PHP_EOL . 'Trax: 0213-8772222', 'updated_by' => 3, 'status' => 0)
+            array('id' => 52, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Reverse Pickup for Contact Person', 'type_id' => 2, 'subject' => NULL, 'body' => 'Dear [contact_person],' . PHP_EOL . 'A rider is on the way for pickup on behalf of [company_name].' . PHP_EOL . 'Rider: [rider_name], [rider_phone_number]' . PHP_EOL . 'Trax: 0213-8772222', 'updated_by' => 3, 'status' => 0)
         ));
     }
 }
