f75c44b6e739b6b0ddba30952ab12908618116bd
Anas Anwer <anas.anwer95"gmail.com>
2019-12-17 14:03:02 +0500

    reports

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -3334,44 +3334,44 @@ class NotificationsController extends Controller
                     $body = str_replace('[preview]', $html, $body);
                 }
 
-                $to = array();
-
-                $admins = Admin::whereIn('role_id', [2, 3, 4, 6, 20])->where('status', 1);
-
-                if ($admins->exists()) {
-                    $to = array_merge($to, $admins->pluck('email')->toArray());
-                }
-
-                $admins = Admin::join('admin_roles', 'admins.role_id', '=', 'admin_roles.id')->where('admin_roles.department_id', 7);
-
-                if ($admins->exists()) {
-                    $to = array_merge($to, $admins->pluck('admins.email')->toArray());
-                }
-
-                $ceo = Admin::find(8);
-
-                if ($ceo) {
-                    $to[] = $ceo->email;
-                }
-                $sup_admin = Admin::find(7);
-
-                if ($sup_admin) {
-                    $to[] = $sup_admin->email;
-                }
-
-                self::email($subject, $body, $to);
-
-
 //                $to = array();
-//                $cc = array();
 //
-//                $admins = Admin::whereIn('id', [36, 7])->where('status', 1);
+//                $admins = Admin::whereIn('role_id', [2, 3, 4, 6, 20])->where('status', 1);
 //
 //                if ($admins->exists()) {
 //                    $to = array_merge($to, $admins->pluck('email')->toArray());
 //                }
 //
+//                $admins = Admin::join('admin_roles', 'admins.role_id', '=', 'admin_roles.id')->where('admin_roles.department_id', 7);
+//
+//                if ($admins->exists()) {
+//                    $to = array_merge($to, $admins->pluck('admins.email')->toArray());
+//                }
+//
+//                $ceo = Admin::find(8);
+//
+//                if ($ceo) {
+//                    $to[] = $ceo->email;
+//                }
+//                $sup_admin = Admin::find(7);
+//
+//                if ($sup_admin) {
+//                    $to[] = $sup_admin->email;
+//                }
+//
 //                self::email($subject, $body, $to);
+
+
+                $to = array();
+                $cc = array();
+
+                $admins = Admin::whereIn('id', [36, 7])->where('status', 1);
+
+                if ($admins->exists()) {
+                    $to = array_merge($to, $admins->pluck('email')->toArray());
+                }
+
+                self::email($subject, $body, $to);
             }
             else if ($id == 48) {
                 if (strpos($subject, '[date]') !== FALSE) {
@@ -3434,35 +3434,35 @@ class NotificationsController extends Controller
 
                 $to = array();
 
-                $cc = array();
-
-                $admins = Admin::whereIn('role_id', [2, 3, 4, 6, 20])->where('status', 1);
-
-                if ($admins->exists()) {
-                    $cc = array_merge($to, $admins->pluck('email')->toArray());
-                }
-
-                $admins = Admin::join('admin_roles', 'admins.role_id', '=', 'admin_roles.id')->where('admin_roles.department_id', 7);
-
-                if ($admins->exists()) {
-                    $to = array_merge($to, $admins->pluck('admins.email')->toArray());
-                }
-
-                $ceo = Admin::find(8);
-                if ($ceo) {
-                    $cc[] = $ceo->email;
-                }
-
-                self::email($subject, $body, $to, $cc);
 //                $cc = array();
 //
-//                $admins = Admin::whereIn('id', [36, 7])->where('status', 1);
+//                $admins = Admin::whereIn('role_id', [2, 3, 4, 6, 20])->where('status', 1);
 //
 //                if ($admins->exists()) {
-//                    $to = array_merge($to, $admins->pluck('email')->toArray());
+//                    $cc = array_merge($to, $admins->pluck('email')->toArray());
 //                }
 //
-//                self::email($subject, $body, $to);
+//                $admins = Admin::join('admin_roles', 'admins.role_id', '=', 'admin_roles.id')->where('admin_roles.department_id', 7);
+//
+//                if ($admins->exists()) {
+//                    $to = array_merge($to, $admins->pluck('admins.email')->toArray());
+//                }
+//
+//                $ceo = Admin::find(8);
+//                if ($ceo) {
+//                    $cc[] = $ceo->email;
+//                }
+//
+//                self::email($subject, $body, $to, $cc);
+                $cc = array();
+
+                $admins = Admin::whereIn('id', [36, 7])->where('status', 1);
+
+                if ($admins->exists()) {
+                    $to = array_merge($to, $admins->pluck('email')->toArray());
+                }
+
+                self::email($subject, $body, $to);
             }
             else if ($id == 49) {
                 if (strpos($subject, '[date]') !== FALSE) {
@@ -3527,76 +3527,37 @@ class NotificationsController extends Controller
                     $body = str_replace('[preview]', $html, $body);
                 }
 
-                $to = array();
-
-                $admins = Admin::whereIn('role_id', [2, 3, 4, 6, 20])->where('status', 1);
-
-                if ($admins->exists()) {
-                    $to = array_merge($to, $admins->pluck('email')->toArray());
-                }
-
-                $admins = Admin::join('admin_roles', 'admins.role_id', '=', 'admin_roles.id')->where('admin_roles.department_id', 7);
-
-                if ($admins->exists()) {
-                    $to = array_merge($to, $admins->pluck('admins.email')->toArray());
-                }
-
-                $ceo = Admin::find(8);
-
-                $cc = array();
-                $cc = [$ceo->email, 'asad@trax.pk'];
-
-                self::email($subject, $body, $to, $cc);
-
 //                $to = array();
-//                $cc = array();
 //
-//                $admins = Admin::whereIn('id', [36, 7])->where('status', 1);
+//                $admins = Admin::whereIn('role_id', [2, 3, 4, 6, 20])->where('status', 1);
 //
 //                if ($admins->exists()) {
 //                    $to = array_merge($to, $admins->pluck('email')->toArray());
 //                }
 //
-//                self::email($subject, $body, $to);
-            }
-            else if ($id == 52) {
-              $shipment_fields = ['order_id' => 'order_id', 'tracking_number' => 'tracking_number'];
+//                $admins = Admin::join('admin_roles', 'admins.role_id', '=', 'admin_roles.id')->where('admin_roles.department_id', 7);
+//
+//                if ($admins->exists()) {
+//                    $to = array_merge($to, $admins->pluck('admins.email')->toArray());
+//                }
+//
+//                $ceo = Admin::find(8);
+//
+//                $cc = array();
+//                $cc = [$ceo->email, 'asad@trax.pk'];
+//
+//                self::email($subject, $body, $to, $cc);
 
-              $pickup_note = PickupNote::find($reference_1_id);
+                $to = array();
+                $cc = array();
 
-              $rider = $pickup_note->rider;
+                $admins = Admin::whereIn('id', [36, 7])->where('status', 1);
 
-              $shipment = Shipment::find($reference_2_id);
-
-              $shipper = $shipment->user;
-
-              $pickup_address = $shipment->pickup_address;
-
-              $to = $pickup_address->phone;
-
-              foreach ($shipment_fields as $key => $field) {
-                if (strpos($body, '[' . $key . ']') !== FALSE) {
-                  $body = str_replace('[' . $key . ']', $shipment[$field], $body);
+                if ($admins->exists()) {
+                    $to = array_merge($to, $admins->pluck('email')->toArray());
                 }
-              }
 
-              if (strpos($body, '[company_name]') !== FALSE) {
-                $body = str_replace('[company_name]', $shipper->name, $body);
-              }
-
-              if (strpos($body, '[contact_person]') !== FALSE) {
-                $body = str_replace('[contact_person]', $pickup_address->poc, $body);
-              }
-
-              if (strpos($body, '[rider_name]') !== FALSE) {
-                $body = str_replace('[rider_name]', $rider->name, $body);
-              }
-
-              if (strpos($body, '[rider_phone_number]') !== FALSE) {
-                $body = str_replace('[rider_phone_number]', $rider->phone, $body);
-              }
-
-              self::sms($body, $to);
+                self::email($subject, $body, $to);
             }
         }
       }
