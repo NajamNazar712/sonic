a8707e97058f85ce6fa4b7b78e12fea134ff2ff0
Anas Anwer <anas.anwer95"gmail.com>
2019-12-26 11:31:52 +0500

    done

--- app/Http/Controllers/Admins/AdminFinanceController.php
+++ app/Http/Controllers/Admins/AdminFinanceController.php
@@ -1438,6 +1438,7 @@ class AdminFinanceController extends Controller
         $change_shipment_amount->old_amount = $shipment->amount;
         $change_shipment_amount->new_amount = $amount;
         $change_shipment_amount->admin_id = Auth::id();
+        $change_shipment_amount->remarks = $request->remarks;
         $change_shipment_amount->save();
 
         $shipment->amount = $amount;
