92a22fc7ce6bab79fea45d7f6c4844481856cfc3
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2020-01-01 16:46:16 +0500

    Fix
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- resources/views/admin/cargo/mapping.blade.php
+++ resources/views/admin/cargo/mapping.blade.php
@@ -263,7 +263,7 @@
                         text: '<i class="la la-plus-circle"></i> Add',
                         className: 'btn btn-primary add',
                         action: function (e, dt, node, config) {
-                            $('#add_mapping').modal('show')
+                            $('#add_mapping').modal('show');
                         }
                     },
                     {
@@ -309,12 +309,7 @@
                     var td = '<td style="padding:5px;" class="border-primary border-lighten-2"><fieldset class="form-group m-0 position-relative has-icon-right"></fieldset></td>';
                     var input = '<input type="text" class="form-control form-control-sm input-sm primary">';
                     var icon = '<div class="form-control-position primary"><i class="la la-search"></i></div>';
-                    var drop_select = '<select name="status_select" id="status_select" class="select2 form-control">' +
-                        '</select>';
-                    var service_drop_select = '<select name="service_select" id="service_select" class="select2 form-control">' +
-                        '</select>';
-                    var mode_drop_select = '<select name="mode_select" id="mode_select" class="select2 form-control">' +
-                        '</select>';
+
                     this.api().columns().every(function(column_id) {
                         var column = this;
                         var header = column.header();
