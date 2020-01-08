d6b2a61b4600616081ef3d508284bfaf15e777ab
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2020-01-08 18:12:16 +0500

    Close
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- app/Http/Controllers/Admins/OrderManagementController.php
+++ app/Http/Controllers/Admins/OrderManagementController.php
@@ -64,20 +64,21 @@ class OrderManagementController extends Controller
             })
             ->join('shipment_status as ss', 'ss.id', '=', 'sj.shipper_status_id')
             ->leftjoin('shipment_payment_status as sps', 'shipments.payment_status_id', '=' , 'sps.id')
-            ->select(['shipments.id as shipment_id','shipments.tracking_number as tracking_number','shipments.tracking_number as tracking','shipments.order_id','u.name as shipper','bt.booking_type as service_type','ss.name as status','oc.name as origin','dc.name as destination','shipments.consignee_name','shipments.consignee_phone_number_1 as phone1','shipments.consignee_phone_number_2 as phone2','shipments.consignee_address','shipments.amount','shipments.created_at as booking_date','shipments.shipper_status_id', 'sps.name as payment_status', 'shipments.booking_type_id', 'usi.poc','sj.shipper_status_id as status_id']);
+            ->select(['shipments.id as shipment_id','shipments.tracking_number as tracking_number','shipments.tracking_number as tracking','shipments.order_id','u.name as shipper','bt.booking_type as service_type','ss.name as status','oc.name as origin','dc.name as destination','shipments.consignee_name','shipments.consignee_phone_number_1 as phone1','shipments.consignee_phone_number_2 as phone2','shipments.consignee_address','shipments.amount','shipments.created_at as booking_date','shipments.shipper_status_id', 'sps.name as payment_status', 'shipments.booking_type_id', 'usi.poc','sj.shipper_status_id as status_id'])
+        ->whereRaw('false');
 
-        if(session('department_id') == 7){
-            if(session('role_id') != 4 ){
-                $shipments = $shipments->where(function ($query) {
-                    $query->whereIn('u.id', session('tagged_shippers'));
-                });
-            }
-        }
-        else if (session('role_id') != 1) {
-            $shipments = $shipments->where(function ($query) {
-                $query->whereIn('oc.hub_id', session('hubs'))->orWhereIn('dc.hub_id', session('hubs'));
-            });
-        }
+        // if(session('department_id') == 7){
+        //     if(session('role_id') != 4 ){
+        //         $shipments = $shipments->where(function ($query) {
+        //             $query->whereIn('u.id', session('tagged_shippers'));
+        //         });
+        //     }
+        // }
+        // else if (session('role_id') != 1) {
+        //     $shipments = $shipments->where(function ($query) {
+        //         $query->whereIn('oc.hub_id', session('hubs'))->orWhereIn('dc.hub_id', session('hubs'));
+        //     });
+        // }
 
         $datatable = Datatables::of($shipments)
             ->editColumn('tracking_number', function ($shipments) {
