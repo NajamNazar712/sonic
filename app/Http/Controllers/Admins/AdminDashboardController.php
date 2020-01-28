0f48663663a37f91ccdc0ba185c3229ce7a7951f
Muhammad Waqas <muhammad.waqas@trax.pk>
2020-01-28 16:36:32 +0500

    fix

--- app/Http/Controllers/Admins/AdminDashboardController.php
+++ app/Http/Controllers/Admins/AdminDashboardController.php
@@ -8028,12 +8028,12 @@ if(session('department_id') == 7){
 
     public function userDocuments($id){
         $documents = UserDocumentAttachment::where('user_id', $id)->first();
-        if($documents){
-            $user = User::find($id);
-            return view('admin.profile.documents')->with(['id' => $id, 'documents' => $documents, 'document_status' => $user->documents_status]);
-        }else{
-            return redirect()->back()->with(['error' => 'Documents not found!']);
+
+        $user = User::find($id);
+        if(!$documents){
+            $documents = false;
         }
+        return view('admin.profile.documents')->with(['id' => $id, 'documents' => $documents, 'document_status' => $user->documents_status]);
     }
 
     public function viewUserDocuments($id, $check, $pdf){
