85ceb642a2dad285b13cd522adea9924f39b9188
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2020-02-19 15:06:45 +0500

    TTS-3003
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- resources/views/client/dashboard.blade.php
+++ resources/views/client/dashboard.blade.php
@@ -633,7 +633,7 @@
                     selector: 'td.select-checkbox',
                     className: 'selected bg-primary bg-lighten-5 primary'
                 },
-                lengthMenu: [[10, 50, 100], [10, 50, 100]],
+                lengthMenu: [[10, 50, 100, 500, 1000], [10, 50, 100, 500, 1000]],
                 pageLength: 10,
                 pagingType: 'full_numbers',
                 processing: true,
