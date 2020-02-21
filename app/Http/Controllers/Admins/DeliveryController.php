578ef048e77e3b1e3b62d8b8b018ad493e37372b
Anas Anwer <anas.anwer95"gmail.com>
2020-02-21 11:44:06 +0500

    done

--- app/Http/Controllers/Admins/DeliveryController.php
+++ app/Http/Controllers/Admins/DeliveryController.php
@@ -5062,7 +5062,7 @@ class DeliveryController extends Controller
         $rider = Rider::leftjoin('cities as c', 'c.id', '=', 'riders.city_id')->select('c.hub_id as hub_id')->where('riders.id', $rider_id)->first();
         $delivery_note = DeliveryNote::find($delivery_note_id);
         if($delivery_note){
-            if($delivery_note->city_id == $rider->hub_id){
+            if($delivery_note->hub_id == $rider->hub_id){
                 $delivery_note->rider_id = $rider;
                 $delivery_note->save();
                 return response()->json(['status' => 0, 'success' => 'Rider updated successfully']);
