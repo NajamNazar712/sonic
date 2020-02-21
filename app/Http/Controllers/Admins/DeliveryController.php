5855bde3ba66f2793dee48d393845ba194e26c42
Anas Anwer <anas.anwer95"gmail.com>
2020-02-21 12:36:50 +0500

    done

--- app/Http/Controllers/Admins/DeliveryController.php
+++ app/Http/Controllers/Admins/DeliveryController.php
@@ -645,11 +645,11 @@ class DeliveryController extends Controller
 
                         $dropdown .= $print_undelivered_performa_button;
                     }
-//                    if (($result->created_at->diffInMinutes(Carbon::now()) <= 15) && (session('role_id') == 1 || in_array(304, session('permissions')))) {
+                    if (($result->created_at->diffInMinutes(Carbon::now()) <= 15) && (session('role_id') == 1 || in_array(304, session('permissions')))) {
                         if (!$updatedstatusCheck) {
                             $dropdown .= $reassign_rider_button;
                         }
-//                    }
+                    }
 
                     $dropdown .= '
                         </div>
