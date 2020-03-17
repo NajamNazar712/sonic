505999439c4746182885dfa89e3484a8839461ab
abimwaqas <muhammad.waqas@trax.pk>
2020-03-17 18:29:54 +0500

    fix

--- resources/views/admin/accounts/active_accounts_list.blade.php
+++ resources/views/admin/accounts/active_accounts_list.blade.php
@@ -101,7 +101,7 @@
     <script src="{{asset('app-assets/vendors/js/extensions/toastr.min.js')}}" type="text/javascript"></script>
     <script src="{{asset('app-assets/vendors/js/forms/select/select2.full.min.js')}}" type="text/javascript"></script>
     <script src="{{asset('js/datatable_buttons.js')}}" type="text/javascript"></script>
-    <script src="{{asset('app-adessets/vendors/js/forms/select/selectize.min.js')}}" type="text/javascript"></script>
+    <script src="{{asset('app-assets/vendors/js/forms/select/selectize.min.js')}}" type="text/javascript"></script>
 
 
     <script type="text/javascript">
