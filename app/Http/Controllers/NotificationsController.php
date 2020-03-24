63bb80f10d1b33d1d5fa5816b2dcff33dbc0ac49
Anas Anwer <anas.anwer95"gmail.com>
2020-03-24 11:48:27 +0500

    nitification

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -4386,7 +4386,7 @@ class NotificationsController extends Controller
                     if (strpos($body, '[link]') !== FALSE) {
                         $body = str_replace('[link]', $link, $body);
                     }
-                    $admins =Admin::whereIn('id', [55, 37])->where('status', 1);
+                    $admins =Admin::whereIn('id', [7, 55, 37])->where('status', 1);
 
 
 //                    $cc_admins = Admin::whereIn('id', [3, 20, 8, 9]);
