53440077e78c396ce58e16503967e9b24e8b3fa1
Anas Anwer <anas.anwer95"gmail.com>
2020-04-02 15:49:07 +0500

    done

--- resources/views/client/shipment/book/index.blade.php
+++ resources/views/client/shipment/book/index.blade.php
@@ -275,12 +275,12 @@
 												<p class="border-bottom border-light text-center font-medium-1 text-bold-600" id="total_price">Total Product(s) Value: Rs <span>0</span></p>
 											</div>
 
-											<div class="form-group">
-												<div class="form-group text-center p-1 border border-light rounded">
-													<label class="d-block">Type of Package</label>
-													<input type="checkbox" name="package_type" class="switch hidden package_type" id="package_type" checked="checked" data-off-label="Partial" data-on-label="Complete">
-												</div>
-											</div>
+{{--											<div class="form-group">--}}
+{{--												<div class="form-group text-center p-1 border border-light rounded">--}}
+{{--													<label class="d-block">Type of Package</label>--}}
+{{--													<input type="checkbox" name="package_type" class="switch hidden package_type" id="package_type" checked="checked" data-off-label="Partial" data-on-label="Complete">--}}
+{{--												</div>--}}
+{{--											</div>--}}
 										</div>
 
 										<div class="form-group input-group">
@@ -1010,7 +1010,7 @@
 
 			$('#try_and_buy .insurance').checkboxpicker();
 
-			$('#package_type').checkboxpicker();
+			// $('#package_type').checkboxpicker();
 
 			var current_date = '{{$date}}';
 			$('#pickup_date').pickadate({
