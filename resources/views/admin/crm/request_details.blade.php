09588eba213ac33a844ee688aafad11913b8691b
abimwaqas <muhammad.waqas@trax.pk>
2020-04-09 18:52:11 +0500

    all done

--- resources/views/admin/crm/request_details.blade.php
+++ resources/views/admin/crm/request_details.blade.php
@@ -1351,7 +1351,7 @@
                                     }
                                 }else{
                                     var html = '<div class="chat admin rider"><div class="chat-avatar"><div class="badge block badge-admin"><i class="la la-user font-medium-2"></i>' + name + '</div></div><div class="chat-body"><div class="chat-content text-left"><p>' + data.comment.comment + '</p><small>just now ({{Carbon\Carbon::now()}})</small></div></div></div>';
-                                    console.log(name);
+
                                     $('section.chat-app-window .chats').append(html);
                                 }
 
