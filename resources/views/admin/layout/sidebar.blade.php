140c6278ad0ee56ed7186b13867ecb61c9577088
abimwaqas <muhammad.waqas@trax.pk>
2020-04-18 13:49:01 +0500

    fix

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -607,7 +607,7 @@
                         @if (session('role_id') == 1 || in_array(200, session('permissions')))
                             <li><a class="menu-item" href="{{ route('admin.reports.crm.index') }}">CRM</a></li>
                         @endif
-                        @if (session('role_id') == 1 || in_array(206, session('permissions')))
+                        @if (session('role_id') == 1 || in_array(210, session('permissions')))
                             <li><a class="menu-item" href="{{ route('admin.reports.summary.index') }}">Summary</a></li>
                         @endif
                         @if (session('role_id') == 1 || in_array(246, session('permissions')))
