df0e0de384acab000140bbae9efa2a7eefd08208
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2020-04-21 14:45:07 +0500

    Load Changes
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- resources/views/admin/crm/closed.blade.php
+++ resources/views/admin/crm/closed.blade.php
@@ -284,6 +284,7 @@
                         d.tracking_numbers = $('#track_form .tracking_numbers').val();
                     }
                 },
+                deferRender: true,
                 rowId: 'id',
                 order: [[15, 'desc']],
                 columns: [
