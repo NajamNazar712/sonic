96ad8e6dcc95b8b4369f569dfb5050ff812601e7
Anas Anwer <anas.anwer95"gmail.com>
2020-04-21 12:23:44 +0500

    done

--- app/Http/Controllers/Shippers/ShipperShipmentBookController.php
+++ app/Http/Controllers/Shippers/ShipperShipmentBookController.php
@@ -1926,26 +1926,6 @@ class ShipperShipmentBookController extends Controller
 
     static public function corporate_book($user_id, $service_type_id, $pickup_address_id, $information_display, $consignee_city_id, $consignee_name, $consignee_address, $consignee_phone_number_1, $consignee_phone_number_2, $consignee_email_address, $order_id, $package_type, $pickup_date, $special_instructions, $estimated_weight, $shipping_mode_id, $delivery_type_id, $same_day_timing_id, $charges_mode_id, $amount, $payment_mode_id) {
 
-        //Existing Coordinates
-        $coordinates = Shipment::where(function ($sub_query) use ($consignee_phone_number_1, $consignee_phone_number_2) {
-            $sub_query->where('consignee_phone_number_1', $consignee_phone_number_1)
-                ->orwhere('consignee_phone_number_2', $consignee_phone_number_1)
-                ->orwhere('consignee_phone_number_1', $consignee_phone_number_2)
-                ->orwhere(function ($sub_sub_query) use ($consignee_phone_number_2) {
-                    $sub_sub_query->whereNotNull('consignee_phone_number_2')
-                        ->where('consignee_phone_number_2', $consignee_phone_number_2);
-                });
-        })->whereNotNull('lat')->whereNotNull('long');
-        if($coordinates->exists()){
-            $coordinates = $coordinates->first();
-            $lat = $coordinates->lat;
-            $long = $coordinates->long;
-        }
-        else{
-            $lat = NULL;
-            $long = NULL;
-        }
-        //Existing Coordinates
 
         $shipment = new Shipment();
 
@@ -1996,6 +1976,23 @@ class ShipperShipmentBookController extends Controller
             $reference_1_id = null;
         }
         ShipmentsJourneyController::add($shipment_id, 1, 1, NULL, NULL, $user_id, NULL, $reference_1_id);
+
+
+        //Existing Coordinates
+        $coordinates = ConsigneeLocation::where(function ($sub_query) use ($consignee_phone_number_1, $consignee_phone_number_2) {
+            $sub_query->where('phone_number', $consignee_phone_number_1)
+                ->orwhere('phone_number', $consignee_phone_number_2);
+        })->where('address', $consignee_address);
+        if($coordinates->exists()){
+            $coordinates = $coordinates->latest()->first();
+
+            $shipment_coordinates = new ConsigneeShipmentLocation();
+            $shipment_coordinates->shipment_id = $shipment_id;
+            $shipment_coordinates->previous_location_id = $coordinates->id;
+            $shipment_coordinates->current_location_id = NULL;
+            $shipment_coordinates->save();
+        }
+        //Existing Coordinates
 
         return $shipment_id;
     }
