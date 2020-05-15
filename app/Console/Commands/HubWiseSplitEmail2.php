fc6eda92e61e6450f9e20e04226911482e90288f
abimwaqas <muhammad.waqas@trax.pk>
2020-05-15 06:40:52 +0500

    remove import

--- app/Console/Commands/HubWiseSplitEmail2.php
+++ app/Console/Commands/HubWiseSplitEmail2.php
@@ -2,7 +2,6 @@
 
 namespace App\Console\Commands;
 
-use App\Http\Controllers\Admins\AdminReportsEmailController;
 use App\Http\Controllers\NotificationsController;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
