ca39e3e570c4d7c5764bf283730c434fdf25ef3d
Anas Anwer <anas.anwer95"gmail.com>
2020-05-22 11:24:54 +0500

    done

--- database/migrations/2020_05_20_110639_create_v2_pickup_requests_table.php
+++ database/migrations/2020_05_20_110639_create_v2_pickup_requests_table.php
@@ -20,9 +20,8 @@ class CreateV2PickupRequestsTable extends Migration
             $table->integer('pickup_address_id');
             $table->integer('booked');
             $table->integer('received')->nullable();
-            $table->integer('total_estimated_weight')->nullable();
+            $table->decimal('total_estimated_weight', 16, 2)->nullable();
             $table->integer('pickup_type');
-            $table->integer('received')->nullable();
             $table->integer('city_id');
             $table->integer('status_id')->default(1);
             $table->integer('rider_status')->default(1);
--- routes/web.php
+++ routes/web.php
@@ -300,6 +300,16 @@ Route::prefix('cod')->name('cod.')->group(function () {
         });
     });
 
+
+    Route::prefix('pickup')->name('pickup.')->group(function () {
+        Route::get('', 'Shippers\ShipperPickupController@pickup_index')->name('index');
+        Route::get('list', 'Shippers\ShipperPickupController@pickup_list')->name('list');
+        Route::post('shipments', 'Shippers\ShipperPickupController@shipments')->name('shipments');
+        Route::post('add_remarks', 'Shippers\ShipperPickupController@add_remarks')->name('add_remarks');
+        Route::post('cancel', 'Shippers\ShipperPickupController@cancel')->name('cancel');
+        Route::post('renew', 'Shippers\ShipperPickupController@renew')->name('renew');
+    });
+
 });
 //Admin Routes Start
 Route::prefix('admin')->name('admin.')->group(function () {
