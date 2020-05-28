f763cca3f43744211c01ee43f5c1bb6c952bc451
ShaheryarKhan <shaheryar.khan@trax.pk>
2020-05-28 09:48:28 +0500

    pickup actionlog

--- app/Http/Controllers/Rider/RiderPickupsController.php
+++ app/Http/Controllers/Rider/RiderPickupsController.php
@@ -13,6 +13,7 @@ use App\Http\Models\RiderPickup;
 use App\Http\Models\RiderPickupShipment;
 use App\Http\Models\PickupAction;
 use App\Http\Models\RiderPickupActionLog;
+use App\Http\Models\V2Pickup\V2RiderPickupActionLog;
 
 use Auth;
 
@@ -151,6 +152,43 @@ class RiderPickupsController extends Controller {
         ->editColumn('pickup_note_id', function ($rider_pickup_action_log) {
             return str_pad($rider_pickup_action_log->pickup_note_id, 6, '0', STR_PAD_LEFT);
         })
+        ->editColumn('pickup_request_id', function ($rider_pickup_action_log) {
+            return str_pad($rider_pickup_action_log->pickup_request_id, 6, '0', STR_PAD_LEFT);
+        });
+
+        if($pn_no = $request->get('search_pn_no')){
+            $rider_pickup_action_logs->where('rider_pickup_action_logs.pickup_note_id','=',$pn_no);
+        }
+        if($assigned_by = $request->get('search_assigned_by')){
+            $rider_pickup_action_logs->where('pn.assigned_by_user_id','=',$assigned_by);
+        }
+        if($rider = $request->get('search_rider')){
+            $rider_pickup_action_logs->where('pn.rider_id','=',$rider);
+        }
+        if($city = $request->get('search_city')){
+            $rider_pickup_action_logs->where('pn.city_id','=',$city);
+        }
+        if ($request->get('search_date_from') && $request->get('search_date_to')) {
+            $from = $request->get('search_date_from');
+            $to = $request->get('search_date_to');
+            $rider_pickup_action_logs->whereBetween('pn.created_at', [$from,$to]);
+        }
+
+    	return $datatables->make(true);
+    }
+    public function pickups_action_log_list_v2(Request $request) {
+    	$rider_pickup_action_logs = V2RiderPickupActionLog::join('pickup_actions as pa', 'v2_rider_pickup_action_logs.type_id', 'pa.id')
+        ->join('v2_pickup_requests as pr', 'v2_rider_pickup_action_logs.pickup_request_id', 'pr.id')
+        ->join('v2_pickup_request_attempts as pra', 'pra.pickup_request_id', 'v2_rider_pickup_action_logs.id')
+    	->join('users as u', 'pr.shipper_id', 'u.id')
+    	->join('user_shipping_infos as usi', 'pr.pickup_address_id', 'usi.id')
+    	->join('cities as c', 'usi.city_id', 'c.id')
+    	->select('v2_rider_pickup_action_logs.id', 'v2_rider_pickup_action_logs.logged_at', 'r.name as rider', 'u.name as shipper', 'usi.pickup_address', 'c.name as city', 'pa.name as type', 'v2_rider_pickup_action_logs.pickup_request_id','pr.created_at','pra.assigned_by_user_id','pr.city_id as city_id','pr.current_rider_id');
+
+    	$datatables = Datatables::of($rider_pickup_action_logs)
+        // ->editColumn('pickup_note_id', function ($rider_pickup_action_log) {
+        //     return str_pad($rider_pickup_action_log->pickup_note_id, 6, '0', STR_PAD_LEFT);
+        // })
         ->editColumn('pickup_request_id', function ($rider_pickup_action_log) {
             return str_pad($rider_pickup_action_log->pickup_request_id, 6, '0', STR_PAD_LEFT);
         });
