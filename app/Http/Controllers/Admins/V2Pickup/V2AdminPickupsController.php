7a683e17288b54932a9e3d48d75cd33e60a3bbe8
abimwaqas <muhammad.waqas@trax.pk>
2020-06-01 20:13:12 +0500

    fix

--- app/Http/Controllers/Admins/V2Pickup/V2AdminPickupsController.php
+++ app/Http/Controllers/Admins/V2Pickup/V2AdminPickupsController.php
@@ -47,7 +47,7 @@ class V2AdminPickupsController extends Controller
             $setting = $setting->first();
             $cut_off_time = $setting->setting_value;
         }
-
+        $rider_cut_off_time = NULL;
         $rider_settings = GlobalSettings::where('type', 'rider_assignment_cut_off_time');
         if($rider_settings->exists()){
             $rider_settings = $rider_settings->first();
