841df26bab7a769cab7f3dd33d3b691cf2187d74
abimwaqas <muhammad.waqas@trax.pk>
2020-06-09 13:59:24 +0500

    fix print

--- app/Http/Controllers/Shippers/ShipperShipmentBookController.php
+++ app/Http/Controllers/Shippers/ShipperShipmentBookController.php
@@ -1418,6 +1418,7 @@ class ShipperShipmentBookController extends Controller
                         </div>';
                             }
                             $logo_invoice .= $invoice_items;
+                            $logo_invoice .= '</div>';
                             $shipment_details .= $logo_invoice;
                         }
 
