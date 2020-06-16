4f602f779ad8b5672623c313bcb0722e9322c1c2
Anas Anwer <anas.anwer95"gmail.com>
2020-06-16 13:32:04 +0500

    fixed

--- resources/views/admin/return/create.blade.php
+++ resources/views/admin/return/create.blade.php
@@ -673,7 +673,7 @@
                     var tracking_number = $(form).find('input.scan_piece_tracking_number').val();
                     if(hub_id == null || hub_id == ''){
                         $.ajax({
-                            url: '{!! route('admin.return.create.shipment.info') !!}',
+                            url: '{!! route('admin.return.create.shipment.piece_details') !!}',
                             method: 'POST',
                             data: {
                                 'tracking': tracking_number,
