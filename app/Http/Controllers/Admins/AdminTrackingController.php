5df8619c7a753f47ae318c62f90a65b15229475d
ShaheryarKhan <shaheryar.khan@trax.pk>
2020-06-16 12:15:31 +0500

    fixed

--- app/Http/Controllers/Admins/AdminTrackingController.php
+++ app/Http/Controllers/Admins/AdminTrackingController.php
@@ -853,11 +853,13 @@ class AdminTrackingController extends Controller
                     }
 
                     $crm_requests = CrmRequest::leftjoin('crm_request_status_histories as crsh', 'crsh.crm_request_id', '=', 'crm_requests.id')
-                        ->leftjoin('admins as a', 'a.id', '=', 'crsh.agent_id')
+                        ->leftjoin('admins as a', 'a.id', '=', 'crm_requests.agent_id')
                         ->leftjoin('users as u', 'u.id', '=', 'crm_requests.launched_by_id')
                         ->leftjoin('substitute_users as su', 'su.id', '=', 'crm_requests.launched_by_id')
                         ->leftjoin('crm_request_statuses as crs', 'crs.id', '=', 'crsh.status_id')
-                        ->select('crm_requests.id as id', 'crs.name as status', 'a.id as admin_id','a.name as created_by_admin', 'u.name as created_by_user', 'su.name as created_by_sub_user', 'crsh.created_at as created_at', 'crsh.status_id as status_id', 'crm_requests.launched_by as launched_added_by')
+                        ->select('crm_requests.id as id', 'crs.name as status', 'a.id as admin_id',
+                        'a.name as created_by_admin', 'u.name as created_by_user', 'su.name as created_by_sub_user',
+                         'crsh.created_at as created_at', 'crsh.status_id as status_id', 'crm_requests.launched_by as launched_added_by')
                         ->where('crm_requests.shipment_id', $shipment->id);
 
                     if($crm_requests->exists()){
