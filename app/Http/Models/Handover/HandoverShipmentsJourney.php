a14cab017830768cac5abf08f4a0203a26cbba9c
ShaheryarKhan <shaheryar.khan@trax.pk>
2020-06-17 23:55:53 +0500

    in progress

--- app/Http/Models/Handover/HandoverShipmentsJourney.php
+++ app/Http/Models/Handover/HandoverShipmentsJourney.php
@@ -5,12 +5,9 @@ namespace App\Http\Models\Handover;
 use Illuminate\Database\Eloquent\Model;
 
 class HandoverShipmentsJourney extends Model
-{
-
-    protected $table = 'handover_shipments_journeys';
-    protected $fillable = ['shipment_id', 'status', 'handover_id'];
+{   
     
     public function status() {
-    	return $this->belongsTo('App\Http\Models\Handover\HandoverShipmentStatus', 'status');
+    	return $this->belongsTo('App\Http\Models\Handover\HandoverShipmentStatus', 'status', 'id');
     }
 }
