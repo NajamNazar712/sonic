036ca45c1dae3fa3ef379bda628d0cc0fc9d2e9b
ShaheryarKhan <shaheryar.khan@trax.pk>
2020-06-18 13:06:38 +0500

    icon

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -998,7 +998,7 @@
 
             <!-- Handover -->
             @if (session('role_id') == 1 || count(array_intersect([339, 340,341,342], session('permissions'))) !== 0)
-                <li class=" nav-item"><a href="#"><span class="menu-title"><i class="la-hands-helping"></i>Shipment Handover</span></a>
+                <li class=" nav-item"><a href="#"><span class="menu-title"><i class="la la-hand-o-right"></i>Shipment Handover</span></a>
                     <ul class="menu-content">
                             @if (session('role_id') == 1 || in_array(339, session('permissions')))
                                 <li><a class="menu-item" href="{{ route('admin.handover.create.index')}}">Create</a></li>
