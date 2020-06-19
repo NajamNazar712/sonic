5778a5b5d40405df1cb6079f79ea39dc6645f04f
abimwaqas <muhammad.waqas@trax.pk>
2020-06-19 16:12:25 +0500

    fix

--- app/Http/Controllers/Admins/AdminFinanceController.php
+++ app/Http/Controllers/Admins/AdminFinanceController.php
@@ -4018,16 +4018,13 @@ class AdminFinanceController extends Controller
       $total_payable = 0;
         foreach ($done_payment->done_payment_shipments as $done_payment_shipment) {
             $shipment = $done_payment_shipment->shipment;
-
-            if($done_payment_shipment->type == 2){
-
-            }
-            $change_shipment_weight_log = ChangeShipmentWeightLog::where('shipment_id', $shipment->user_id)->first();
-            if($change_shipment_weight_log){
-                $shipment_weight=$change_shipment_weight_log->old_weight;
-            }
-            else{
-               $shipment_weight= $shipment->actual_weight;
+            $shipment_weight= $shipment->actual_weight;
+            if($done_payment_shipment->type == 0){
+                $change_shipment_weight_log = ChangeShipmentWeightLog::where('shipment_id', $shipment->user_id);
+                if($change_shipment_weight_log->exists()){
+                    $change_shipment_weight_log = $change_shipment_weight_log->first();
+                    $shipment_weight = $change_shipment_weight_log->old_weight;
+                }
             }
 
             if ($done_payment_shipment->type == 0) {
