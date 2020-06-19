e9b8b9ea70a46d66d5cded6dcb04807b835fbde4
abimwaqas <muhammad.waqas@trax.pk>
2020-06-19 12:22:43 +0500

    fix

--- app/Http/Models/Shipment.php
+++ app/Http/Models/Shipment.php
@@ -101,7 +101,4 @@ class Shipment extends Model
 	public function handover_shipments_journeys(){
 	    return $this->hasMany('App\Http\Models\Handover\HandoverShipmentsJourney')->orderBy('id', 'DESC');
     }
-	public function handover_shipments_journeys(){
-	    return $this->hasMany('App\Http\Models\Handover\HandoverShipmentsJourney')->orderBy('id', 'DESC');
-    }
 }
