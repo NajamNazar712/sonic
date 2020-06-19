b385cb0200f8158774b42082bd6857b6a10ae450
abimwaqas <muhammad.waqas@trax.pk>
2020-06-19 10:08:23 +0500

    few changes

--- app/Http/Controllers/Admins/V2Pickup/V2PickupCronController.php
+++ app/Http/Controllers/Admins/V2Pickup/V2PickupCronController.php
@@ -28,6 +28,7 @@ class V2PickupCronController extends Controller
             $rider_id = $setting->setting_value;
         }
         $today = Carbon::today()->toDateString();
+        return $today;
         $pickup_requests = V2PickupRequest::where('status_id', 1)->whereDate('created_at', '<=', $today)->whereTime('created_at', '<=', $arrival_time);
         if($pickup_requests->exists()){
             $pickup_requests = $pickup_requests->get();
