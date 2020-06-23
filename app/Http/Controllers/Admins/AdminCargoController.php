ed91d5553309c302da5d0b86adca25b090e6a974
abimwaqas <muhammad.waqas@trax.pk>
2020-06-23 15:57:41 +0500

    fix

--- app/Http/Controllers/Admins/AdminCargoController.php
+++ app/Http/Controllers/Admins/AdminCargoController.php
@@ -1932,9 +1932,13 @@ class AdminCargoController extends Controller
         $file_name_without_path = "/reports/cargo_consignment_excel_" . $cargo_consignment_excel->id . ".xlsx";
         $file_name = public_path() . "/reports/cargo_consignment_excel_" . $cargo_consignment_excel->id . ".xlsx";
         $writer->save($file_name);
-        $file = File::glob(public_path().$file_name_without_path);
+        $files = File::glob(public_path().$file_name_without_path);
 //        $file = Storage::disk('public')->url('/reports/cargo_consignment_excel_'. $cargo_consignment_excel->id .'.xlsx');
-        Storage::disk('s3')->put( 'cargo_consignment_excels/'.'cargo_consignment_excel_'. $cargo_consignment_excel->id .'.xlsx', file_get_contents($file));
+        foreach ($files as $file){
+            if (is_file($file)) {
+                Storage::disk('s3')->put( 'cargo_consignment_excels/'.'cargo_consignment_excel_'. $cargo_consignment_excel->id .'.xlsx', file_get_contents($file));
+            }
+        }
 //        Storage::disk('public')->delete('reports/cargo_consignment_excel_'. $cargo_consignment_excel->id .'.xlsx');
 
         $cargo_consignment_excel->cargoes = count($cargo_consignment_ids);
