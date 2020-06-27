8ca1ea785e8c56f2a8cee8ff36065cec343fa9c2
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2020-06-27 22:22:31 +0500

    Debriefing Report Changes
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -96,7 +96,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('archive:stationdepositnoteimage')->dailyAt('00:00')->runInBackground();
 
         $schedule->command('archive:pettycashimage')->dailyAt('00:00')->runInBackground();
-		$schedule->command('email:debriefingemail')->dailyAt('00:00')->runInBackground();
+		$schedule->command('email:debriefingemail')->dailyAt('01:00')->runInBackground();
 
         $settings = GlobalSettings::where('type', 'return_delivered_to_shipper_cut_off_time');
 
