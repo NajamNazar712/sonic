54569ccdff5d68260fe5df5f6027a3aafa56a61f
abimwaqas <muhammad.waqas@trax.pk>
2020-07-02 23:39:28 +0500

    change

--- app/Http/Controllers/Admins/V2Pickup/V2PickupCronController.php
+++ app/Http/Controllers/Admins/V2Pickup/V2PickupCronController.php
@@ -116,6 +116,7 @@ class V2PickupCronController extends Controller
         }
         $attempt_ids = array();
         foreach ($pickup_requests as $pickup_request) {
+            return $pickup_request->id;
             foreach ($shipments as $date){
                 $attempt = V2PickupRequestAttempt::where('pickup_request_id', $pickup_request->id)->where('attempt_date', $date)->whereNull('reason_id')->latest('id')->first();
                 $to_delete_attempts = V2PickupRequestAttempt::where('pickup_request_id', $pickup_request->id)->where('attempt_date', $date)->whereNull('reason_id')->where('id', '!=', $attempt)->pluck('id')->toArray();
