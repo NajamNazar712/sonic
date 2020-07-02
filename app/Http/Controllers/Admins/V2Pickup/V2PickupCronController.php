3614f298d853239d5b7cb1b17211e3c6600100af
abimwaqas <muhammad.waqas@trax.pk>
2020-07-02 23:55:43 +0500

    fix

--- app/Http/Controllers/Admins/V2Pickup/V2PickupCronController.php
+++ app/Http/Controllers/Admins/V2Pickup/V2PickupCronController.php
@@ -104,9 +104,9 @@ class V2PickupCronController extends Controller
 //            }
 //        }
 
-        $pickup_requests = V2PickupRequest::where('attempts', '>',1)->whereIn('status_id',[1,3])->select('id')->get();
-        $current = Carbon::now()->day(20)->month(6)->startOfDay();
-        $past = Carbon::now()->day(19)->month(6)->startOfDay();
+        $pickup_requests = V2PickupRequest::where('attempts', '>',1)->whereIn('status_id',[1,2,3])->select('id')->get();
+        $current = Carbon::now()->day(2)->month(7)->startOfDay();
+        $past = Carbon::now()->day(18)->month(6)->startOfDay();
 
         $shipments = array();
         $shipments['dates'] = array();
