6e8338a5ccf543442121f10d0e7f9b3a0a01c38c
abimwaqas <muhammad.waqas@trax.pk>
2020-07-03 16:06:34 +0500

    fix

---
+++ database/seeds/UpdateModulePermissionForDeleteReturnNoteImage.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UpdateModulePermissionForDeleteReturnNoteImage extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('module_permissions')->insert(array(
+            array('id' => 354, 'name' => 'Delete Image - Return Note', 'module_id' => 7)
+        ));
+    }
+}
