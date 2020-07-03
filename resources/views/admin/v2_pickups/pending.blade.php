5e3f493c2ed80e36b65640c86f5b03b33f44cf9e
abimwaqas <muhammad.waqas@trax.pk>
2020-07-04 01:24:20 +0500

    swal on pickup

--- resources/views/admin/v2_pickups/pending.blade.php
+++ resources/views/admin/v2_pickups/pending.blade.php
@@ -578,36 +578,62 @@
                     error.addClass('w-100').appendTo(element.parent('.form-group'));
                 },
                 submitHandler: function(form) {
-                    var rider_id = parseInt($(form).find('select.rider').val());
 
-                    $.ajax({
-                        url: '{!! route('admin.v2_pickups.pending.assign') !!}',
-                        method: 'PUT',
-                        data: {
-                            'pickup_request_ids': selected_rows,
-                            'rider_id': rider_id,
-                            '_token': '{{ csrf_token() }}'
+                    swal({
+                        text: 'Are you sure, you want to assign rider to the following pickup(s)?',
+                        icon: 'info',
+                        buttons: {
+                            cancel: {
+                                text: 'No',
+                                value: null,
+                                visible: true,
+                                closeModal: true,
+                            },
+                            confirm: {
+                                text: 'Yes',
+                                value: true,
+                                visible: true,
+                                closeModal: true
+                            }
+                        },
+                        closeOnClickOutside: false,
+                        closeOnEsc: false,
+                        dangerMode: true
+                    }).then(function(confirm) {
+                        if(confirm){
+                            var rider_id = parseInt($(form).find('select.rider').val());
+
+                            $.ajax({
+                                url: '{!! route('admin.v2_pickups.pending.assign') !!}',
+                                method: 'PUT',
+                                data: {
+                                    'pickup_request_ids': selected_rows,
+                                    'rider_id': rider_id,
+                                    '_token': '{{ csrf_token() }}'
+                                }
+                            })
+                                .done(function(data) {
+                                    if (data.status == 0) {
+                                        toastr.success(data.success, 'Success!', {positionClass: 'toast-bottom-center', containerId: 'toast-bottom-center'});
+                                    }
+                                    else {
+                                        toastr.error(data.error, 'Error!', {positionClass: 'toast-top-center', containerId: 'toast-top-center'});
+                                    }
+
+                                    table.rows().deselect();
+
+                                    selected_rows = [];
+
+                                    table.button('.assign').disable();
+                                    table.button('.update').disable();
+
+                                    table.draw('false');
+
+                                    $('#assign_to_rider').modal('hide');
+                                });
                         }
-                    })
-                    .done(function(data) {
-                        if (data.status == 0) {
-                            toastr.success(data.success, 'Success!', {positionClass: 'toast-bottom-center', containerId: 'toast-bottom-center'});
-                        }
-                        else {
-                            toastr.error(data.error, 'Error!', {positionClass: 'toast-top-center', containerId: 'toast-top-center'});
-                        }
-
-                        table.rows().deselect();
-
-                        selected_rows = [];
-
-                        table.button('.assign').disable();
-                        table.button('.update').disable();
-
-                        table.draw('false');
-
-                        $('#assign_to_rider').modal('hide');
                     });
+
                 }
             });
 
