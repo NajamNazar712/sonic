e2cec754d15889e0322717483e796d921416f600
abimwaqas <muhammad.waqas@trax.pk>
2020-07-10 15:38:57 +0500

    fix

--- resources/views/admin/return/receive.blade.php
+++ resources/views/admin/return/receive.blade.php
@@ -262,7 +262,7 @@
                     { data:'rider' ,name: 'riders.name', class: 'align-middle rider'},
                     { data:'shipments_count_link' ,name: 'return_notes.shipments_count', class: 'align-middle shipments_count_link text-center'},
                     // { data:'shipments_count_link' ,name: 'return_notes.shipments_count', class: 'align-middle shipments_count_link text-center'},
-                    { data:'shipments_unverified_link' ,name: 'return_notes.shipments_unverified_count', class: 'align-middle shipments_unverified_link text-center'},
+                    { data:'shipments_unverified_link' ,name: 'shipments_unverified_count', class: 'align-middle shipments_unverified_link text-center',orderable: false, searchable: false},
                     { data:'assignee' ,name: 'admins.name', class: 'align-middle assignee'},
                     { data:'created_at' ,name: 'return_notes.created_at', class: 'align-middle created_at'},
                     { data:'return_note_status' ,name: 'return_notes.status', class: 'align-middle return_note_status'},
@@ -299,7 +299,7 @@
                         var column = this;
                         var header = column.header();
 
-                        if ($(header).is('.serial_number')) {
+                        if ($(header).is('.serial_number') || $(header).is('.shipments_unverified_link')) {
                             $(td).appendTo($(search));
                         }else if($(header).is('.action')){
                             $(td).appendTo($(search));
