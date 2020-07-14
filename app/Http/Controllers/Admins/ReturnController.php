7c8fb783ca6657b0d0e89abb89c60e1dcd496452
abimwaqas <muhammad.waqas@trax.pk>
2020-07-14 15:45:28 +0500

    in p

--- app/Http/Controllers/Admins/ReturnController.php
+++ app/Http/Controllers/Admins/ReturnController.php
@@ -1305,6 +1305,7 @@ class ReturnController extends Controller
             return ['status' => 1, 'error' => 'No Shipment Item with given Item ID is present'];
         }
     }
+
     public function return_note_create(Request $request)
     {
         $trackings = explode(',', $request->shipment_ids);
--- app/Http/Controllers/Reports/StationRecoveryController.php
+++ app/Http/Controllers/Reports/StationRecoveryController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Reports;
 
+use App\Http\Models\Admin\StationDepositNote;
+use App\Http\Models\City;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -15,6 +17,11 @@ class StationRecoveryController extends Controller
     }
     static public function station_recovery_data(){
         $yesterday = Carbon::yesterday();
-        
+        $hubs = City::where('hub', 1)->where('status', 1)->pluck('id')->toArray();
+        if(count($hubs) > 0){
+            foreach ($hubs as $hub_id){
+                $station_notes = StationDepositNote::where('hub_id', $hub_id)->where('dncc_status',0)->get();
+            }
+        }
     }
 }
