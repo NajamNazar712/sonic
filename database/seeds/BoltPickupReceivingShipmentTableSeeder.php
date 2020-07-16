1367125d9da2e7a95e38a57420b9e26cd5d0212f
abimwaqas <muhammad.waqas@trax.pk>
2020-07-16 16:17:15 +0500

    done

--- database/seeds/BoltPickupReceivingShipmentTableSeeder.php
+++ database/seeds/BoltPickupReceivingShipmentTableSeeder.php
@@ -13,7 +13,7 @@ class BoltPickupReceivingShipmentTableSeeder extends Seeder
     {
         $timestamp = \Carbon\Carbon::now()->format('Y-m-d H:i:s');
         DB::table('notifications')->insert(array(
-            array('id' => 73, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Pickup Request Done/Not Done', 'type_id' => 1, 'subject' => "Pickup Request [status]", 'body' => 'Dear [vendor],'. PHP_EOL . PHP_EOL .'Your pickup on behalf of [shipper_name] is [status]' . PHP_EOL . PHP_EOL . 'Please contact Trax Logistics for further details' . PHP_EOL . PHP_EOL . 'info@trax.pk' . PHP_EOL . PHP_EOL . '0213-877-22-22', 'updated_by' => 3, 'status' => 0)
+            array('id' => 73, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Shipment Rider Picked', 'type_id' => 1, 'subject' => 'Shipment(s) Picked by Rider', 'body' => 'Dear [company_name],' . PHP_EOL . 'Following of your shipments have been picked by TRAX Logistics Rider:' . PHP_EOL . '[tracking_numbers]' . PHP_EOL . PHP_EOL . PHP_EOL . 'Please contact at info@trax.pk or 0213-877-22-22 for further details.', 'updated_by' => 3, 'status' => 0)
         ));
     }
 }
