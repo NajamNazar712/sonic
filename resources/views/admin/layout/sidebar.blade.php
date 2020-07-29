1bd35cef9d94818f47960dff166b7f9c8902ac84
Anas Anwer <anas.anwer@trax.pk>
2020-07-29 13:18:12 +0500

    done

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -172,13 +172,13 @@
                         @if (session('role_id') == 1 || count(array_intersect([25, 26, 27, 124], session('permissions'))) !== 0)
                             <li class=" nav-item"><a href="#"><span class="menu-title">Cargo</span></a>
                                 <ul class="menu-content">
-{{--                                    @if (session('role_id') == 1 || in_array(25, session('permissions')))--}}
-{{--                                        <li><a class="menu-item" href="{{ route('admin.cargo.pending.index') }}">Pending</a></li>--}}
-{{--                                    @endif--}}
+                                    @if (session('role_id') == 1 || in_array(25, session('permissions')))
+                                        <li><a class="menu-item" href="{{ route('admin.cargo.pending.index') }}">Pending</a></li>
+                                    @endif
 
-{{--                                    @if (session('role_id') == 1 || in_array(26, session('permissions')))--}}
-{{--                                        <li><a class="menu-item" href="{{ route('admin.cargo.create.index') }}">Create</a></li>--}}
-{{--                                    @endif--}}
+                                    @if (session('role_id') == 1 || in_array(26, session('permissions')))
+                                        <li><a class="menu-item" href="{{ route('admin.cargo.create.index') }}">Create</a></li>
+                                    @endif
 
                                     @if (session('role_id') == 1 || in_array(26, session('permissions')))
                                         <li><a class="menu-item" href="{{ route('admin.cargo.draft.index') }}">Draft</a></li>
