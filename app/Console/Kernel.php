e490a95ce6ce64fb0b75ae3db25ea4cb7fd6d578
Anas Anwer <anas.anwer@trax.pk>
2020-08-11 18:38:11 +0500

    fixed

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -35,8 +35,8 @@ class Kernel extends ConsoleKernel
         '\App\Console\Commands\DailyFakeStatusReportEmail',
         '\App\Console\Commands\NegativeBalanceShipperSalesPerson',
         '\App\Console\Commands\ClearDefaultBankDuration',
-        '\App\Console\Commands\OvernightCargoReport',
-        '\App\Console\Commands\OverlandCargoReport',
+//        '\App\Console\Commands\OvernightCargoReport',
+//        '\App\Console\Commands\OverlandCargoReport',
 		'\App\Console\Commands\AccountReconciliationReportFromStart',
         '\App\Console\Commands\AccountReconciliationReportCurrent',
 		'\App\Console\Commands\BusinessProjectionAndRetention',
@@ -135,8 +135,8 @@ class Kernel extends ConsoleKernel
         $schedule->command('email:negativebalanceshippersalesperson')->weeklyOn(1, '8:00')->runInBackground();
         $schedule->command('email:weeklyincompletedocumentsshipper')->weeklyOn(1, '8:00')->runInBackground();
 
-        $schedule->command('overnight:cargo_report')->dailyAt('12:00')->runInBackground();
-        $schedule->command('overland:cargo_report')->dailyAt('16:00')->runInBackground();
+//        $schedule->command('overnight:cargo_report')->dailyAt('12:00')->runInBackground();
+//        $schedule->command('overland:cargo_report')->dailyAt('16:00')->runInBackground();
 
 //		$schedule->command('accounts:reconciliationcurrent')->monthly()->days([1,14,28])->runInBackground();
 //      $schedule->command('accounts:reconciliationcurrent')->cron('0 0 1,14,28 * *'); //another solution
