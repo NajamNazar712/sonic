84203154fb4b14a2689816c742c1b6daa21db6f0
Nabeel973 <nabeelsiddiqui324@gmail.com>
2020-08-19 02:18:14 -0700

    created ledger

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,8 @@ Route::prefix('cod')->name('cod.')->group(function () {
     Route::get('/email/verified/{id?}','Auth\RegisterController@email_verified')->name('email.verified');
 
     Route::get('access_denied', 'Shippers\ShipperDashboardController@access_denied')->name('access_denied');
+    Route::get('ledger', 'Shippers\ShipperDashboardController@ledger_index')->name('ledger');
+    Route::get('ledger/list', 'Shippers\ShipperDashboardController@ledger_list')->name('ledger.list');
 
     Route::get('/welcome', 'Shippers\ShipperDashboardController@welcome_index')->name('welcome');
     Route::get('/dashboard', 'Shippers\ShipperDashboardController@orders_index')->name('dashboard');
