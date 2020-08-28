98e536b59aebb3ee6f506f874da4efd9c7dc59f1
abimwaqas <muhammad.waqas@trax.pk>
2020-08-28 14:41:23 +0500

    seeder change

--- database/seeds/UpdateNotificationModuleForReversePickup.php
+++ database/seeds/UpdateNotificationModuleForReversePickup.php
@@ -14,7 +14,7 @@ class UpdateNotificationModuleForReversePickup extends Seeder
         $timestamp = \Carbon\Carbon::now()->format('Y-m-d H:i:s');
 
         DB::table('notifications')->insert(array(
-            array('id' => 77, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Reverse Pickup Receiving', 'type_id' => 2, 'subject' => null, 'body' => 'Dear Shipper,' . PHP_EOL . PHP_EOL .'Rider [rider_name] is on the way for pickup from your address. We have sent address label to your email address, please paste it on packed shipment.'. PHP_EOL . PHP_EOL .'Rider Phone No.  [rider_phone]', 'updated_by' => 6, 'status' => 0)
+            array('id' => 77, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Reverse Pickup Receiving', 'type_id' => 2, 'subject' => null, 'body' => 'Dear Customer,' . PHP_EOL . PHP_EOL .'Rider [rider_name] is on the way for pickup from your address. We have sent address label to your email address, please paste it on packed shipment.'. PHP_EOL . PHP_EOL .'Rider Phone No.  [rider_phone]', 'updated_by' => 6, 'status' => 0)
         ));
     }
 }
