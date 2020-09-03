190e28aa6c84cb4e16ac5cde1b70f36ee97ad62d
Anas Anwer <anas.anwer@trax.pk>
2020-09-03 13:27:41 +0500

    fixed

--- app/Http/Controllers/Shippers/ShipperShipmentBookController.php
+++ app/Http/Controllers/Shippers/ShipperShipmentBookController.php
@@ -3059,7 +3059,7 @@ class ShipperShipmentBookController extends Controller
             'pickup_address_id' => ['required', 'integer', 'digits_between:1,10', Rule::exists('user_shipping_infos', 'id')->where(function($query) use($user_id) {
                 $query->where('user_id', $user_id);
             })],
-            'delivery_type_id' => ['required_if:service_type_id,1,2', 'integer', 'digits_between:1,10', Rule::exists('delivery_types', 'id')],
+            'delivery_type_id' => ['required_if:service_type_id,1,2', 'nullable', 'integer', 'digits_between:1,10', Rule::exists('delivery_types', 'id')],
             'charges_mode_id' => ['nullable', 'integer', 'digits_between:1,10', Rule::exists('charges_modes', 'id')->where(function($query) {
                 $query->whereIn('id', [2, 3]);
             })],
