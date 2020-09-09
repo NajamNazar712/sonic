266b34e1c13080305ba168442fdbb1123f0da23e
abimwaqas <muhammad.waqas@trax.pk>
2020-09-09 18:18:39 +0500

    fix

--- app/Http/Controllers/Shippers/ShipperShipmentPieceController.php
+++ app/Http/Controllers/Shippers/ShipperShipmentPieceController.php
@@ -187,7 +187,7 @@ class ShipperShipmentPieceController extends Controller
             ->join('cities AS dc', 'shipments.consignee_city_id', '=', 'dc.id')
             ->leftjoin('shipment_pieces_request_statuses as ss', 'ss.id', '=', 'shipment_pieces_requests.request_status_id')
             ->select('shipments.id as shId', 'shipments.tracking_number as tracking_number_link', 'shipments.tracking_number', 'shipments.booking_type_id', 'u.name as shipper', 'oc.name as origin', 'dc.name as destination','shipments.amount', 'ss.name as request_status', 'shipment_pieces_requests.created_at', 'shipment_pieces_requests.request_status_id', 'shipment_pieces_requests.status')
-            ->where('shipments.shipper_status_id', 62)->where('shipment_pieces_requests.status', 2);
+            ->where('shipment_pieces_requests.status', 2);
 
         $datatables = Datatables::of($shipments)
             ->editColumn('tracking_number_link', function ($shipments) {
