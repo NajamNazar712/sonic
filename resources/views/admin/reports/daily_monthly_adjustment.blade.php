e4244e1dc10e67f31174d28f49a8c0102ca0f8ee
Anas Anwer <anas.anwer@trax.pk>
2020-09-09 18:11:44 +0500

    done

--- resources/views/admin/reports/daily_monthly_adjustment.blade.php
+++ resources/views/admin/reports/daily_monthly_adjustment.blade.php
@@ -20,7 +20,7 @@
                             </span>
                             </div>
 
-                            <input type="text" name="search_date_from" class="form-control pickadate bg-primary border-primary white rounded-right" id="search_date_from" placeholder="Date (From)">
+                            <input type="text" name="search_date_from" class="form-control pickadate bg-primary border-primary white rounded-right" id="search_date_from" placeholder="Date (From)" data-value="{{ Carbon\Carbon::today() }}">
                         </div>
                     </div>
                     <div class="col-3 ">
@@ -31,7 +31,7 @@
                             </span>
                             </div>
 
-                            <input type="text" name="search_date_to" class="form-control pickadate bg-primary border-primary white rounded-right" id="search_date_to" placeholder="Date (To)">
+                            <input type="text" name="search_date_to" class="form-control pickadate bg-primary border-primary white rounded-right" id="search_date_to" placeholder="Date (To)" data-value="{{ Carbon\Carbon::today() }}">
                         </div>
 
                     </div>
