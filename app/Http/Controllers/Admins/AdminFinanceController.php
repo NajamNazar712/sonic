e90aceac97ffb8f2544b68bf4d9db8898180d1dc
abimwaqas <muhammad.waqas@trax.pk>
2020-09-10 17:15:14 +0500

    fix

--- app/Http/Controllers/Admins/AdminFinanceController.php
+++ app/Http/Controllers/Admins/AdminFinanceController.php
@@ -1870,10 +1870,10 @@ class AdminFinanceController extends Controller
         $charges = 0;
         $gst = 0;
 
-        if ($payable > 0) {
-            $payment_type = 0;
-        }
-        else {
+//        if ($payable > 0) {
+//            $payment_type = 0;
+//        }
+//        else {
             $account_type_id = $shipment->user->account_type_id;
 
             if ($account_type_id == 1) {
@@ -1882,7 +1882,7 @@ class AdminFinanceController extends Controller
             else {
                 $payment_type = 1;
             }
-        }
+//        }
 
         if ($payment_type == 0) {
             $pending_payment = PendingPayment::where('user_id', $shipment->user_id);
