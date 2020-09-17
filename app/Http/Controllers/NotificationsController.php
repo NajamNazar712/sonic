3c59d553a1df6ea23ad02ee596ce6d06f9f3b974
muhammadwaseem786 <muhammad.waseem@trax.pk>
2020-09-17 12:04:28 +0500

    change

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -5008,7 +5008,7 @@ class NotificationsController extends Controller
                   self::email($subject, $body, $to);
               }
           else if($id == 72){
-                  $pending_cash_collection_agings = PendingCashCollectionAgingReport::get();
+                  $pending_cash_collection_agings = PendingCashCollectionAgingReport::orderBy('count','desc')->get();
 
                   $subject = $notification->subject;
                   $body = $notification->body;
