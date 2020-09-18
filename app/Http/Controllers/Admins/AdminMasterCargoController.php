35393a3d3cf562a483c5de10abb56a326fcbbf81
Anas Anwer <anas.anwer@trax.pk>
2020-09-18 12:23:52 +0500

    changes

--- app/Http/Controllers/Admins/AdminMasterCargoController.php
+++ app/Http/Controllers/Admins/AdminMasterCargoController.php
@@ -1700,10 +1700,13 @@ class AdminMasterCargoController extends Controller
             $cargo_consignment_junction_receival->save();
 
             $cargo_consignment = MasterCargo::find($cargo_consignment_id);
+
             $master_bags = $cargo_consignment->master_bags;
 
             if ($cargo_consignment->junction_hub_1_id == $request->junction) {
                 foreach ($master_bags as $master_bag){
+                    $master_bag->status = 1;
+                    $master_bag->save();
                     $bag = $master_bag->bag;
                     $bag->status_id = 5;
                     $bag->save();
@@ -1711,6 +1714,8 @@ class AdminMasterCargoController extends Controller
             }
             else if ($cargo_consignment->junction_hub_2_id == $request->junction) {
                 foreach ($master_bags as $master_bag){
+                    $master_bag->status = 1;
+                    $master_bag->save();
                     $bag = $master_bag->bag;
                     $bag->status_id = 6;
                     $bag->save();
@@ -1718,6 +1723,8 @@ class AdminMasterCargoController extends Controller
             }
             else {
                 foreach ($master_bags as $master_bag){
+                    $master_bag->status = 1;
+                    $master_bag->save();
                     $bag = $master_bag->bag;
                     $bag->status_id = 3;
                     $bag->save();
@@ -1725,6 +1732,8 @@ class AdminMasterCargoController extends Controller
             }
 
             $cargo_consignment->status_id = 2;
+            $cargo_consignment->received_at = Carbon::now();
+            $cargo_consignment->received_by = Auth::id();
             $cargo_consignment->save();
         }
 
