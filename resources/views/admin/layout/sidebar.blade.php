e68bfe5e9276084bdc064795a54d7ea9a2154cd0
Anas Anwer <anas.anwer@trax.pk>
2020-09-22 15:58:00 +0500

    changes

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -934,6 +934,10 @@
                                     @if (session('role_id') == 1 || in_array(355, session('permissions')))
                                         <li><a class="menu-item" href="{{ route('admin.settings.station_recovery_cron.index') }}">Station Recovery Cron</a></li>
                                     @endif
+
+                                    @if (session('role_id') == 1 || in_array(205, session('permissions')))
+                                        <li><a class="menu-item" href="{{route('admin.settings.restrict_parcels_attempt.index')}}">Restrict Shipper Parcels Attempts</a></li>
+                                    @endif
                                 </ul>
                             </li>
                         @endif
