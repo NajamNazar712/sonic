6661581dc54609d906193a80ad77f465a0498028
muhammadwaseem786 <muhammad.waseem@trax.pk>
2020-09-24 16:43:17 +0500

    changed slidebar rout and show rides name on sms_rider_history screen

--- resources/views/admin/management/riders/sms_history.blade.php
+++ resources/views/admin/management/riders/sms_history.blade.php
@@ -134,7 +134,7 @@
                     {orderable: false, searchable: false, name: 'serial_number', class: 'align-middle serial_number', targets: 0, render: function (data, type, row) {return '';}},
                     {data: 'body', name: 'sms_histories.body', class: 'align-middle msg_body'},
                     {data: 'send_by', name: 'admins.name', class: 'align-middle send_by'},
-                    {data: 'riders_count', name: 'riders_count', class: 'align-middle riders_count'},
+                    {data: 'riders_count', name: 'riders_count', class: 'align-middle riders_count', orderable: false, searchable: false},
                     {data: 'created_at', name: 'sms_histories.created_at', class: 'align-middle created_at'},
                 ],
                 rowCallback: function(row, data, index) {
