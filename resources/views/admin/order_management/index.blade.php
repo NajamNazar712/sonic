48e2e8251b2fbc9ae8b850583a2d06ef0be957e5
abimwaqas <muhammad.waqas@trax.pk>
2020-09-24 17:00:28 +0500

    done filtering

--- resources/views/admin/order_management/index.blade.php
+++ resources/views/admin/order_management/index.blade.php
@@ -578,7 +578,6 @@
                     }
                 });
             }
-
             var selected_rows = [];
             var table = $('#datatable').DataTable({
                 scrollX: true, scrollY: '500px',
@@ -873,22 +872,22 @@
                         }else if($(header).is('.status')){
                             $(drop_select).appendTo($(search))
                                 .on( 'change', function () {
-                                    column.search($(this).val(), false, false, true).draw();
+                                    column.search($(this).val(), false, false, true);
                                 } ).wrap(td);
                         }else if($(header).is('.service_type')){
                             $(service_drop_select).appendTo($(search))
                                 .on( 'change', function () {
-                                    column.search($(this).val(), false, false, true).draw();
+                                    column.search($(this).val(), false, false, true);
                                 } ).wrap(td);
                         }else if($(header).is('.payment_status')){
                             $(payment_select).appendTo($(search))
                                 .on( 'change', function () {
-                                    column.search($(this).val(), false, false, true).draw();
+                                    column.search($(this).val(), false, false, true);
                                 } ).wrap(td);
                         }
                         else {
                             var current = $(input).appendTo($(search)).on('change', function() {
-                                column.search($(this).val(), false, false, true).draw();
+                                    column.search($(this).val(), false, false, true);
                             }).wrap(td).after(icon);
 
                             if (column.search()) {
