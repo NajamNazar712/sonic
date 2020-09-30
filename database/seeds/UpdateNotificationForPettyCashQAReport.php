40a7cae28fd4b51945ae80d9d534f0eb4a9f2858
muhammadwaseem786 <muhammad.waseem@trax.pk>
2020-09-30 22:16:55 +0500

    Petty Cash QA Report mail body text updated

--- database/seeds/UpdateNotificationForPettyCashQAReport.php
+++ database/seeds/UpdateNotificationForPettyCashQAReport.php
@@ -15,7 +15,7 @@ class UpdateNotificationForPettyCashQAReport extends Seeder
         $timestamp = Carbon::now()->format('Y-m-d H:i:s');
 
         DB::table('notifications')->insert(array(
-            array('id' => 89, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Petty Cash QA Report', 'type_id' => 1, 'subject' => 'Petty Cash QA Report', 'body' =>'Please find below the link to download Petty Cash QA Report' . PHP_EOL .'[link]', 'updated_by' => 3, 'status' => 0)
+            array('id' => 89, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Petty Cash QA Report', 'type_id' => 1, 'subject' => 'Petty Cash QA Report', 'body' => 'Dear Concern' . PHP_EOL . 'Please find below the link to download Petty Cash QA Report' . PHP_EOL .'[link]', 'updated_by' => 3, 'status' => 0)
         ));
     }
 }
