e6e21726f2c817bc7fa26c4691b5acf3fc8f2e92
muhammadwaseem786 <muhammad.waseem@trax.pk>
2020-10-01 13:26:30 +0500

    Shipper IBN Number Add

---
+++ database/seeds/UpdateNotificationForShipperAddIBANNumber.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UpdateNotificationForShipperAddIBANNumber extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $timestamp = Carbon::now()->format('Y-m-d H:i:s');
+        DB::table('notifications')->insert(array(
+            array('id' => 91, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Add IBN Number', 'type_id' => 2, 'subject' => NULL, 'body' => 'Dear Concern,' . PHP_EOL . 'Your account [IBN] changed.', 'updated_by' => 3, 'status' => 0)
+        ));
+    }
+}
