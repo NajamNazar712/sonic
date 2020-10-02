ca6cbaed12a95120ba34d84a3e0eaac6b90b0b84
abimwaqas <muhammad.waqas@trax.pk>
2020-10-02 18:21:44 +0500

    fix

--- app/Http/Controllers/Admins/AdminReportsEmailController.php
+++ app/Http/Controllers/Admins/AdminReportsEmailController.php
@@ -8,9 +8,13 @@ use App\Http\Models\Admin\Admin;
 use App\Http\Models\Admin\DeliveryNote;
 use App\Http\Models\Admin\DeliveryNoteShipment;
 use App\Http\Models\Admin\GlobalSettings;
+use App\Http\Models\Admin\MasterCargo\Bag;
+use App\Http\Models\Admin\MasterCargo\BagShipment;
+use App\Http\Models\Admin\MasterCargo\MasterCargoBag;
 use App\Http\Models\Admin\PettyCashStatement;
 use App\Http\Models\Admin\SalePersonTag;
 use App\Http\Models\Admin\SalePersonTarget;
+use App\Http\Models\Admin\StationDepositNote;
 use App\Http\Models\CargoConsignment;
 use App\Http\Models\CargoConsignmentShipment;
 use App\Http\Models\City;
@@ -162,7 +166,7 @@ class AdminReportsEmailController extends Controller
         } else {
             $total_avg_revenue_count = 0;
         }
-        
+
         $cell_st =[
             'font' =>['bold' => true],
             'alignment' =>['horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER],
@@ -227,29 +231,31 @@ class AdminReportsEmailController extends Controller
                 $join->on('s.consignee_city_id', '=', 'cities.id')
                     ->where('s.packaging_material_request', 0);
             })
+            ->leftjoin('user_shipping_infos as usi', 'usi.id','=', 's.pickup_address_id')
+            ->leftjoin('cities as oc', 'usi.city_id', '=', 'oc.id')
             ->leftjoin('shipments_journey as sj', 'sj.shipment_id', '=', 's.id')
-            ->select('h.id as hub_id', 'h.name as hub', DB::raw('count(s.id) as shipment_count'), DB::raw('sum(s.actual_weight) as actual_weight'))
+            ->select('h.id as hub_id', 'h.name as hub', DB::raw('count(s.id) as shipment_count'), DB::raw('sum(s.actual_weight) as actual_weight'), 'oc.name as origin', 'oc.id as origin_id')
             ->where('sj.shipper_status_id', 2)
             ->whereBetween('sj.created_at', [$date_from, $date_to])
-            ->groupBy('h.id')
+            ->groupBy('oc.id')
             ->get();
         foreach ($hub_wise_splits as $hub_wise_split) {
             $total_shipments = $total_shipments + $hub_wise_split->shipment_count;
         }
         foreach ($hub_wise_splits as $hub_wise_split) {
             if ($total_shipments != 0) {
-                $ratio[$hub_wise_split->hub_id] = $hub_wise_split->shipment_count / $total_shipments;
+                $ratio[$hub_wise_split->origin_id][$hub_wise_split->hub_id] = $hub_wise_split->shipment_count / $total_shipments;
             } else {
-                $ratio[$hub_wise_split->hub_id] = 0;
+                $ratio[$hub_wise_split->origin_id][$hub_wise_split->hub_id] = 0;
             }
             if ($hub_wise_split->shipment_count != 0) {
-                $avg_actual_weight[$hub_wise_split->hub_id] = $hub_wise_split->actual_weight / $hub_wise_split->shipment_count;
+                $avg_actual_weight[$hub_wise_split->origin_id][$hub_wise_split->hub_id] = $hub_wise_split->actual_weight / $hub_wise_split->shipment_count;
             } else {
-                $avg_actual_weight[$hub_wise_split->hub_id] = 0;
+                $avg_actual_weight[$hub_wise_split->origin_id][$hub_wise_split->hub_id] = 0;
             }
         }
 
-        $hub_wise_split_array['header'] = ['S. No.', 'Hub', 'Count of Parcels', 'Ratio', 'Actual Weight', 'Avg Actual Weight/Shipment'];
+        $hub_wise_split_array['header'] = ['S. No.', 'Origin', 'Hub', 'Count of Parcels', 'Ratio', 'Actual Weight', 'Avg Actual Weight/Shipment'];
         $serial = 1;
 
         $total_shipments_count = 0;
@@ -263,19 +269,20 @@ class AdminReportsEmailController extends Controller
             } else {
                 $actual_weight = 0;
             }
-            $hub_wise_split_array[] = ['serial' => $serial, 'Hub' => $hub_wise_split->hub, 'Count of Parcels' => $hub_wise_split->shipment_count, 'Ratio' => round($ratio[$hub_wise_split->hub_id] * 100, 2), 'Actual Weight' => round($actual_weight, 2), 'Avg Actual Weight/Shipment' => round($avg_actual_weight[$hub_wise_split->hub_id], 2)];
+            $hub_wise_split_array[] = ['serial' => $serial, 'Origin' => $hub_wise_split->origin, 'Hub' => $hub_wise_split->hub, 'Count of Parcels' => $hub_wise_split->shipment_count, 'Ratio' => round($ratio[$hub_wise_split->hub_id] * 100, 2), 'Actual Weight' => round($actual_weight, 2), 'Avg Actual Weight/Shipment' => round($avg_actual_weight[$hub_wise_split->hub_id], 2)];
             $hub_wise_split_entry = new HubWiseSplit();
+            $hub_wise_split_entry->origin = $hub_wise_split->origin_id;
             $hub_wise_split_entry->hub_id = $hub_wise_split->hub_id;
             $hub_wise_split_entry->shipments = $hub_wise_split->shipment_count;
-            $hub_wise_split_entry->ratio = $ratio[$hub_wise_split->hub_id] * 100;
+            $hub_wise_split_entry->ratio = $ratio[$hub_wise_split->origin_id][$hub_wise_split->hub_id] * 100;
             $hub_wise_split_entry->actual_weight = $actual_weight;
-            $hub_wise_split_entry->avg_actual_weight = $avg_actual_weight[$hub_wise_split->hub_id];
+            $hub_wise_split_entry->avg_actual_weight = $avg_actual_weight[$hub_wise_split->origin_id][$hub_wise_split->hub_id];
             $hub_wise_split_entry->save();
             $serial++;
 
 
             $total_shipments_count = $total_shipments_count + $hub_wise_split->shipment_count;
-            $total_avg_ratio_count = $total_avg_ratio_count + $ratio[$hub_wise_split->hub_id];
+            $total_avg_ratio_count = $total_avg_ratio_count + $ratio[$hub_wise_split->origin_id][$hub_wise_split->hub_id];
             $total_actual_weight_count = $total_actual_weight_count + $hub_wise_split->actual_weight;
         }
         if($total_shipments_count <= 0){
@@ -284,8 +291,8 @@ class AdminReportsEmailController extends Controller
         else{
             $total_avg_actual_weight_count = $total_actual_weight_count / $total_shipments_count;
         }
-        $hub_wise_split_array[] = ['serial' => '', 'Hub' => '', 'Count of Parcels' => '', 'Ratio' => '', 'Actual Weight' => '', 'Avg Actual Weight/Shipment' => ''];
-        $hub_wise_split_array[] = ['serial' => 'Total', 'Hub' => '', 'Count of Parcels' => $total_shipments_count, 'Ratio' => $total_avg_ratio_count * 100, 'Actual Weight' => round($total_actual_weight_count, 2), 'Avg Actual Weight/Shipment' => round($total_avg_actual_weight_count, 2)];
+        $hub_wise_split_array[] = ['serial' => '', 'Origin' => '', 'Hub' => '', 'Count of Parcels' => '', 'Ratio' => '', 'Actual Weight' => '', 'Avg Actual Weight/Shipment' => ''];
+        $hub_wise_split_array[] = ['serial' => 'Total', 'Origin' => '', 'Hub' => '', 'Count of Parcels' => $total_shipments_count, 'Ratio' => $total_avg_ratio_count * 100, 'Actual Weight' => round($total_actual_weight_count, 2), 'Avg Actual Weight/Shipment' => round($total_avg_actual_weight_count, 2)];
         $cell_st = [
             'font' => ['bold' => true],
             'alignment' => ['horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER],
@@ -1190,10 +1197,11 @@ class AdminReportsEmailController extends Controller
                 $writer->save($file_name);
 
                 NotificationsController::send(76, $hub->id, url('/') . '/' . $file_name_without_path);
+
             }
         }
     }
-	
+
 	static public function done_payment($date){
         $done_payments = DonePaymentCalculation::whereDate('created_at', $date);
         DonePaymentsReport::truncate();
@@ -1297,4 +1305,197 @@ class AdminReportsEmailController extends Controller
         }
     }
 
+    static public function short_received_report_hub_wise($hub_ids){
+        $today = Carbon::today();
+        foreach ($hub_ids as $hub_id){
+            $hub = City::find($hub_id);
+            $serial = 1;
+            $short_received_array = array();
+            $short_received_array['header'] = ['S No.', 'Shipments', 'Bag No.', 'Master Cargo No.', 'Origin', 'Destination', 'Short Received Date'];
+            $short_received_array[] = ['S No.' => '', 'Shipments' => '', 'Bag No.' => '', 'Master Cargo No.' => '', 'Origin' => '', 'Destination' => '', 'Short Received Date' => ''];
+            $short_received_bags = Bag::where('origin_hub_id', $hub_id)->where('status_id', 7)->where('short_received', '>', 0)->whereDate('received_at', $today);
+            if($short_received_bags->exists()){
+                $short_received_bags = $short_received_bags->get();
+                foreach ($short_received_bags as $bag) {
+                    $bag_shipments = BagShipment::where('bag_id', $bag->id)->where('status', 0);
+                    if($bag_shipments->exists()){
+                        $bag_shipments = $bag_shipments->get();
+                        foreach ($bag_shipments as $bag_shipment){
+                            $master_cargo_bag = MasterCargoBag::where('bag_id', $bag->id)->latest()->first();
+                            $tracking_number = $bag_shipment->shipment->tracking_number;
+                            $seal_number = $bag->seal_number;
+                            $origin_hub = $bag->origin_hub->name;
+                            $destination_hub = $bag->destination_hub->name;
+                            $received_at = $bag->received_at;
+                            $short_received_array[] = ['S No.' => $serial, 'Shipments' => $tracking_number, 'Bag No.' => $seal_number, 'Master Cargo No.' => str_pad($master_cargo_bag->master_cargo_id, 6, '0', STR_PAD_LEFT), 'Origin' => $origin_hub, 'Destination' => $destination_hub, 'Short Received Date' => $received_at];
+                            $serial++;
+                        }
+                    }
+                }
+                if($serial > 1){
+
+                    $cell_st = [
+                        'font' => ['bold' => true],
+                        'alignment' => ['horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER],
+                        'borders' => ['bottom' => ['style' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_MEDIUM]]
+                    ];
+                    $spreadsheet = new Spreadsheet();
+                    $sheet = $spreadsheet->getActiveSheet();
+                    $sheet->getDefaultColumnDimension()->setWidth(20);
+                    $sheet->getStyle("B2:B4000")->getNumberFormat()
+                        ->setFormatCode(
+                            \PHPExcel_Style_NumberFormat::FORMAT_NUMBER
+                        );
+                    $sheet->fromArray($short_received_array, NULL, 'A2', true);
+                    $sheet->getStyle("A2:G2")->applyFromArray($cell_st);
+                    $title = 'Short Received Shipments ' . $hub->name;
+                    if(strlen($title) > 31){
+                        $title = substr($title, 0, 28);
+                        $title = $title . '...';
+                    }
+                    $sheet->setTitle($title);
+                    $writer = new Xlsx($spreadsheet);
+                    header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+                    header('Content-Disposition: attachment;filename="outstanding_shipment_report.xlsx"');
+                    header('Cache-Control: max-age=0');
+                    $date_file_name = Carbon::today()->format('Y_m_d');
+                    $file_name_without_path = "reports/short_received_shipment_report_" . strtolower($hub->name) . "_" . $date_file_name . ".xlsx";
+                    $file_name = public_path() . "/reports/short_received_shipment_report_" . strtolower($hub->name) . "_"  . $date_file_name . ".xlsx";
+                    $writer->save($file_name);
+
+                    NotificationsController::send(86, $hub->id, url('/') . '/' . $file_name_without_path);
+                }
+            }
+        }
+    }
+
+    static public function outstanding_sdn($date)
+    {
+        $outstanding_sdn_report_array[] = ['Trax Online Private Limited'];
+        $outstanding_sdn_report_array[] = ['Outstanding SDN Report'];
+        $outstanding_sdn_report_array['header'] = ['S. No.', 'Hub Name', 'Completed >2days'];
+        $outstanding_sdn_report_array[] = ['S. No.' => '', 'Hub Name' => '', 'Completed >2days' => ''];
+
+        $now = Carbon::now();
+        $total_number = 0;
+        $total = 0;
+        $serial = 0;
+        $hubs = City::where('hub', 1)->where('status', 1)->get();
+        if (count($hubs) > 0) {
+            foreach ($hubs as $hub) {
+                $total_count = 0;
+                $sdn_data[$hub->id]['name'] = $hub->name;
+                $sdn_data[$hub->id]['count'] = 0;
+                $outstanding_sdn = StationDepositNote::where('hub_id', $hub->id)->whereIn('status', [0,1])->get();
+                foreach ($outstanding_sdn as $sdn) {
+                    $start = Carbon::parse($sdn->created_at);
+                    $difference = $start->diffInDays($now);
+                    if ($difference > 2) {
+                        $total_count++;
+                    }
+                }
+                $sdn_data[$hub->id]['count'] += $total_count;
+            }
+
+            foreach ($sdn_data as $index => $sdn){
+                if($sdn['count'] != 0){
+                    $serial++;
+                    $total_number += $sdn['count'];
+                    $outstanding_sdn_report_array[] = ['S. No.' => $serial, 'Hub Name' =>  $sdn['name'], 'Completed >2days' =>  $sdn['count']] ;
+                }
+            }
+            $outstanding_sdn_report_array[] = ['S. No.' => 'Total Number', 'Hub Name' =>  '', 'Completed >2days' =>  $total_number] ;
+
+            $cell_st = [
+                'font' => ['bold' => true],
+                'alignment' => ['horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER],
+                'borders' => ['bottom' => ['style' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_MEDIUM]]
+            ];
+            $serial = $serial + 6;
+            $spreadsheet = new Spreadsheet();
+            $sheet = $spreadsheet->getActiveSheet();
+            $sheet->getDefaultColumnDimension()->setWidth(20);
+            $sheet->fromArray($outstanding_sdn_report_array, NULL, 'A2', true);
+            $sheet->getStyle("A2:C2")->applyFromArray($cell_st);
+            $sheet->getStyle("A3:C3")->applyFromArray($cell_st);
+            $sheet->getStyle("A" . $serial . ":C" . $serial)->applyFromArray($cell_st);
+            $sheet->setTitle('Outstanding SDN Report');
+            $sheet->mergeCells('A2:C2');
+            $sheet->mergeCells('A3:C3');
+            $sheet->mergeCells('A' . $serial . ':B' . $serial);
+            $writer = new Xlsx($spreadsheet);
+            header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+            header('Content-Disposition: attachment;filename="outstanding_sdn_report.xlsx"');
+            header('Cache-Control: max-age=0');
+            $date_file_name = Carbon::today()->format('Y_m_d');
+            $file_name_without_path = "reports/outstanding_sdn_report_" . $date_file_name . ".xlsx";
+            $file_name = public_path() . "/reports/outstanding_sdn_report_" . $date_file_name . ".xlsx";
+            $writer->save($file_name);
+            return url('/') . '/' . $file_name_without_path;
+
+        }
+    }
+
+    static public function petty_cash_qa_report()
+    {
+        $hubs = City::where('hub', 1)->where('status', 1)->get();
+        $qa_report_petty_cash_array['header'] = ['S. No.', 'Hub Name', 'Station Approval', 'Operation Approval', 'Finance Approval'];
+        $qa_report_petty_cash_array[] = ['S. No.' => '', 'Hub Name' => '', 'Station Approval' => '', 'Operation Approval' => '', 'Finance Approval' => ''];
+        $serial=1;
+        foreach ($hubs as $hub){
+            $hub_approvals[$hub->id]['name'] = $hub->name;
+            $hub_approvals[$hub->id]['station_approval'] = 0;
+            $hub_approvals[$hub->id]['operation_approval'] = 0;
+            $hub_approvals[$hub->id]['finance_approval'] = 0;
+            $petty_cash_statements = PettyCashStatement::where('hub_id', $hub->id)->get();
+            foreach ($petty_cash_statements as $petty_cash_statement){
+                if($petty_cash_statement->station_approved_by == null){
+                    $hub_approvals[$hub->id]['station_approval']++;
+                }
+                if($petty_cash_statement->operation_approved_by == null){
+                    $hub_approvals[$hub->id]['operation_approval']++;
+                }
+                if($petty_cash_statement->finance_approved_by == null){
+                    $hub_approvals[$hub->id]['finance_approval']++;
+                }
+
+            }
+        }
+        QaReportPettyCash::truncate();
+
+        foreach ($hub_approvals as $index => $hub_approval){
+            $qa_report_petty_cash = new QaReportPettyCash();
+            $qa_report_petty_cash->hub_id = $index;
+            $qa_report_petty_cash->hub_name = $hub_approval['name'];
+            $qa_report_petty_cash->station_approval = $hub_approval['station_approval'];
+            $qa_report_petty_cash->operation_approval = $hub_approval['operation_approval'];
+            $qa_report_petty_cash->finance_approval = $hub_approval['finance_approval'];
+            $qa_report_petty_cash->save();
+            $qa_report_petty_cash_array[] = ['S. No.' => $serial, 'Hub Name' => $hub_approval['name'], 'Station Approval' => $hub_approval['station_approval'], 'Operation Approval' => $hub_approval['operation_approval'], 'Finance Approval' => $hub_approval['finance_approval']];
+            $serial++;
+        }
+
+        $cell_st = [
+            'font' => ['bold' => true],
+            'alignment' => ['horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER],
+            'borders' => ['bottom' => ['style' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_MEDIUM]]
+        ];
+
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+        $sheet->getDefaultColumnDimension()->setWidth(20);
+        $sheet->fromArray($qa_report_petty_cash_array, NULL, 'A2', true);
+        $sheet->getStyle("A2:E2")->applyFromArray($cell_st);
+        $sheet->setTitle('QA Report Petty Cash');
+        $writer = new Xlsx($spreadsheet);
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment;filename="qa_report_petty_cash.xlsx"');
+        header('Cache-Control: max-age=0');
+        $date_file_name = Carbon::today()->format('Y_m_d');
+        $file_name_without_path = "reports/qa_report_petty_cash_" . $date_file_name . ".xlsx";
+        $file_name = public_path() . "/reports/qa_report_petty_cash_" . $date_file_name . ".xlsx";
+        $writer->save($file_name);
+
+        return url('/') . '/' . $file_name_without_path;
+    }
 }
