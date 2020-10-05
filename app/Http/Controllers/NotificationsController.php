1131951f38dd3ce20eb721dce9369effab320582
Nabeel973 <nabeelsiddiqui324@gmail.com>
2020-10-05 17:08:08 +0500

    Email Fixed

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -5793,6 +5793,7 @@ class NotificationsController extends Controller
           else if($id == 85){
               $shipments = Shipment::find($reference_1_id);
               $booking_person = $reference_2_id;
+              $admin = Admin::find($booking_person);
               $subject = $notification->subject;
               $body = $notification->body;
 
@@ -5831,8 +5832,8 @@ class NotificationsController extends Controller
               $finance = Admin::whereIn('id', [12, 60, 49])->where('status', 1);
 
               $to = array();
-              if($booking_person->email){
-                  $to[] = $booking_person->email;
+              if($admin->exists()){
+                  $to[] = $admin->email;
               }
 
               if ($finance->exists()) {
