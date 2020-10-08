e9644c6a3c13c0089b914b7057c6a8c3229ea81b
Anas Anwer <anas.anwer@trax.pk>
2020-10-08 14:44:08 +0500

    changes

--- app/Http/Controllers/Admins/AdminDashboardController.php
+++ app/Http/Controllers/Admins/AdminDashboardController.php
@@ -9003,7 +9003,7 @@ if(session('department_id') == 7){
         elseif ($check == 'blank_cheque_image'){
             $file = $user_documents->blank_cheque_image;
         }
-        $url = Storage::url('users_attached_documents/' . $id . '/'. $file .'.' . $pdf_image);
+        $url = Storage::url('users_attached_documents/' . $id . '/'. $file);
         return view('admin.profile.documents_view')->with(['url' => $url, 'pdf' => $pdf]);
     }
     public function approveDocuments($id, $approve, $reason){
