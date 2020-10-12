976266fc01b26cc8fd795eb152e2edfcb27e201c
Anas Anwer <anas.anwer@trax.pk>
2020-10-12 13:47:58 +0500

    done

--- app/Jobs/ProcessShipmentBookingDBPriority.php
+++ app/Jobs/ProcessShipmentBookingDBPriority.php
@@ -3,6 +3,7 @@
 namespace App\Jobs;
 
 use App\Http\Models\Shipment;
+use App\http\Models\ShipmentOrderDate;
 use App\Http\Models\Shipper\User;
 use App\http\Models\SubstituteUserShipment;
 use Illuminate\Bus\Queueable;
@@ -169,6 +170,13 @@ class ProcessShipmentBookingDBPriority implements ShouldQueue
             $substitute_user_shipment->save();
         }
         $tracking_number = ShipperShipmentBookController::generate_tracking_number($shipment_id, $pickup_city_id, $consignee_city_id);
+
+        if($this->booking['order_date'] != null){
+            $order_date = new ShipmentOrderDate();
+            $order_date->shipment_id = $shipment_id;
+            $order_date->order_date = $this->booking['order_date'];
+            $order_date->save();
+        }
 
         if ($service_type_id == 1 || $service_type_id == 5) {
             $item_product_type_id = $this->booking['item_product_type_id'];
