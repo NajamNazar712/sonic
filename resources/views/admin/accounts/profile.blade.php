f7ce7be22867883926e47e785e0e9992b6bfbca0
Anas Anwer <anas.anwer@trax.pk>
2020-10-15 17:06:27 +0500

    changes

--- resources/views/admin/accounts/profile.blade.php
+++ resources/views/admin/accounts/profile.blade.php
@@ -49,6 +49,12 @@
                                     <td><b>Company Name</b></td>
                                     <td>{{$user->name}}</td>
                                 </tr>
+                                @if($user->brand_name != null)
+                                    <tr>
+                                        <td><b>Brand Name</b></td>
+                                        <td>{{$user->brand_name}}</td>
+                                    </tr>
+                                @endif
                                 <tr>
                                     <td><b>Email Address</b></td>
                                     <td>{{$user->email}}</td>
@@ -111,12 +117,6 @@
                                     <tr>
                                         <td><b>Reference</b></td>
                                         <td>{{$reference->name}}</td>
-                                    </tr>
-                                @endif
-                                @if($user->brand_name != null)
-                                    <tr>
-                                        <td><b>Brand Name</b></td>
-                                        <td>{{$user->brand_name}}</td>
                                     </tr>
                                 @endif
                                 <tr>
