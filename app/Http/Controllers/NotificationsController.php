a4d968b08319ab1b8b99b5099aa47c823b278050
abimwaqas <muhammad.waqas@trax.pk>
2020-10-23 15:28:12 +0500

    fix

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -4922,7 +4922,7 @@ class NotificationsController extends Controller
                         $body = str_replace('[preview]', $html, $body);
                     }
 
-                    $admins = Admin::whereIn('id', [12, 60, 79, 216]);
+                    $admins = Admin::whereIn('id', [12, 49, 216]);
 
                     if ($admins->exists()) {
                         $to = $admins->pluck('email')->toArray();
@@ -5851,7 +5851,7 @@ class NotificationsController extends Controller
                 }
                 else if($id == 90){
                     $to = array();
-                    $finance = Admin::whereIn('id', [12, 60,13])->where('status', 1);
+                    $finance = Admin::whereIn('id', [12, 60, 79, 216])->where('status', 1);
                     if ($finance->exists()) {
                         $to = array_merge($to, $finance->pluck('email')->toArray());
                     }
