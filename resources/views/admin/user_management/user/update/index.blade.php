ec96747fdfa7476d16e27e088f4cea2228bc9dfa
Nabeel973 <nabeelsiddiqui324@gmail.com>
2020-10-23 17:56:42 +0500

    changes

--- resources/views/admin/user_management/user/update/index.blade.php
+++ resources/views/admin/user_management/user/update/index.blade.php
@@ -79,12 +79,12 @@
 									</div>
 									<div class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
 										<div class="form-group">
-											<input type="text" name="designation" class="form-control" placeholder="Designation*" data-rule-required="true" data-msg-required="Designation is required" data-rule-required="true" value="{{ $user->designation }}">
+											<input type="text" name="designation" class="form-control" placeholder="Designation" value="{{ $user->designation }}">
 										</div>
 									</div>
 									<div class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
 										<div class="form-group">
-											<input type="text" name="trax_id" class="form-control" placeholder="Trax Id*" data-rule-required="true" data-msg-required="Trax Id is required" data-rule-required="true" value="{{ $user->trax_id }}">
+											<input type="text" name="trax_id" class="form-control" placeholder="Trax Id" value="{{ $user->trax_id }}">
 										</div>
 									</div>
 
