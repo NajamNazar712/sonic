f35c4581a95323b85fff77830527f5ad253dfa78
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2020-10-27 16:03:52 +0500

    Sapphire Fix
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- resources/views/client/layout/sidebar.blade.php
+++ resources/views/client/layout/sidebar.blade.php
@@ -50,7 +50,7 @@
                 </li>
             @endif
 
-            @if (session('user_type') == 1)
+            @if (session('user_type') == 1 && session('user_id') != 7762)
                 <li class=" nav-item"><a href="#"><span class="menu-title" data-i18n="nav.dash.main"><i class="la la-list-ul"></i>Rates</span></a>
                     <ul class="menu-content">
                         <li><a class="menu-item" href="{{ route('cod.rates.view.index') }}">View</a></li>
