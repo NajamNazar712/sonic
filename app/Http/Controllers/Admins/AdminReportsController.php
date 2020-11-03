7faa73982f554daf2049c57214ac677c339d1f56
Nabeel973 <nabeelsiddiqui324@gmail.com>
2020-11-03 12:18:48 +0500

    Chnages in report

--- app/Http/Controllers/Admins/AdminReportsController.php
+++ app/Http/Controllers/Admins/AdminReportsController.php
@@ -12,6 +12,7 @@ use App\Http\Models\BanksList;
 use App\Http\Models\CargoConsignment;
 use App\Http\Models\City;
 use App\Http\Models\Excel_reports\Debriefing;
+use App\Http\Models\Rider;
 use App\Http\Models\ShipmentsJourney;
 use App\Http\Models\StationRecoveryReport;
 use App\Http\Models\StationRecoveryReportDeposit;
@@ -6769,7 +6770,8 @@ use Yajra\Datatables\Datatables;
         }
 
         public function app_efficiency_index(){
-            return view('admin.reports.app_efficiency');
+            $riders = Rider::select('id','name')->get();
+            return view('admin.reports.app_efficiency')->with(['riders' => $riders]);
         }
         public function app_efficiency_list(Request $request){
 
@@ -6780,9 +6782,13 @@ use Yajra\Datatables\Datatables;
                ->join('cities as c','c.id','=','vpr.city_id')
                ->join('cities as ci','c.id','=','ci.hub_id')
                ->join('v2_pickup_request_statuses as vprs','vprs.id','=','vpr.status_id')
-               ->select('v2_rider_pickups.pickup_request_id as request_id','v2_rider_pickups.pickup_note_id as note_id','u.name as shipper_name','r.name as rider','usi.vendor as vendor','usi.pickup_address as address','c.name as city','ci.name as hub','v2_rider_pickups.created_at','vprs.name as status');
+               ->select('v2_rider_pickups.pickup_request_id as request_id','v2_rider_pickups.pickup_note_id as note_id','u.name as shipper_name','r.name as rider','usi.vendor as vendor','usi.pickup_address as address','c.name as city','ci.name as hub','v2_rider_pickups.created_at','vprs.name as status','v2_rider_pickups.created_at as created_at','r.id as rider_id');
 
            $datatable = Datatables::of($v2_rider_pickups);
+
+            if($rider_id = $request->get('rider')){
+                $v2_rider_pickups = $v2_rider_pickups->where('vpr.current_rider_id', '=', $rider_id);
+            }
            if($request->get('search_date_from') && $request->get('search_date_to')){
                $from = $request->get('search_date_from');
                $to = $request->get('search_date_to');
