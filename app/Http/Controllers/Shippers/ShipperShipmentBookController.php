edef0de776dd0cd4d041612b64efe3e7562d19f6
Anas Anwer <anas.anwer@trax.pk>
2020-11-04 16:42:05 +0500

    changes

--- app/Http/Controllers/Shippers/ShipperShipmentBookController.php
+++ app/Http/Controllers/Shippers/ShipperShipmentBookController.php
@@ -2529,6 +2529,7 @@ class ShipperShipmentBookController extends Controller
                 if($service_type_id == 1){
                     $pieces_quantity = $request->pieces_quantity;
                 }
+                $business_category_id = 1;
                 $shipment_id = $this->corporate_book($user_id, $service_type_id, $pickup_address_id, $information_display, $consignee_city_id, $consignee_name, $consignee_address, $consignee_phone_number_1, $consignee_phone_number_2, $consignee_email_address, $order_id, $package_type, $special_instructions, $estimated_weight, $shipping_mode_id, $delivery_type_id, $same_day_timing_id, $charges_mode_id, $amount, $payment_mode_id, $pieces_quantity, $self_collection, $business_category_id);
                 if(session('user_type') == 2){
                     $substitute_user_shipment = new SubstituteUserShipment();
