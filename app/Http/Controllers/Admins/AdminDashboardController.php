090e6630054e77293966132589191118fdc1ed33
Nabeel973 <nabeelsiddiqui324@gmail.com>
2020-11-08 00:12:39 +0500

    Set Segment

--- app/Http/Controllers/Admins/AdminDashboardController.php
+++ app/Http/Controllers/Admins/AdminDashboardController.php
@@ -11,6 +11,7 @@ use App\Http\Models\Admin\AdminHub;
 use App\Http\Models\Admin\GlobalSettings;
 use App\Http\Models\Admin\HistoryShipperBankAccount;
 use App\Http\Models\Admin\SalePersonTag;
+use App\Http\Models\Admin\Segment;
 use App\Http\Models\Admin\WalkInStandardWeightCharge;
 use App\Http\Models\AdminLogs;
 use App\Http\Models\AverageShipmentCycle;
@@ -1374,7 +1375,8 @@ class AdminDashboardController extends Controller
         $salesperson = Admin::join('admin_roles as ar', 'admins.role_id', '=', 'ar.id')->select(['admins.name','admins.id'])->where('status', 1)->where('ar.department_id',7)->get();
         $products = Product::select('id','product_name')->get();
         $payment_cycles = PaymentCycle::all();
-        return view('admin.accounts.active_accounts_list')->with(['products'=>$products,'sale_name'=>$salesperson, 'shippers' => $shippers, 'payment_cycles' => $payment_cycles]);
+        $segments = Segment::all();
+        return view('admin.accounts.active_accounts_list')->with(['products'=>$products,'sale_name'=>$salesperson, 'shippers' => $shippers, 'payment_cycles' => $payment_cycles,'segments' => $segments]);
 
     }
     public function blockAccountsList(){
@@ -9510,6 +9512,20 @@ if(session('department_id') == 7){
             City::where('id',$city->id)->update(['hub_id'=>$city->id]);
             return redirect()->back()->with('success','Hub city added successfully');
         }
+    }
+
+    public function setSegmentBulk(Request $request){
+    $segment_id = $request->segment_id;
+    $shipper_ids = $request->shipper_ids;
+    if($shipper_ids){
+        foreach($shipper_ids as $shipper_id){
+            $user = User::find($shipper_id);
+            $user->segment_id = $segment_id;
+            $user->save();
+        }
+        return ['success'=>"Segment has been set fot the following Shippers!"];
+    }
+        return ['error' => "Select some shippers first!"];
     }
 }
 
--- routes/web.php
+++ routes/web.php
@@ -443,6 +443,7 @@ Route::prefix('admin')->name('admin.')->group(function () {
         Route::put('status', 'Admins\AdminDashboardController@UserStatus')->name('status');
         Route::post('tag/submit','Admins\AdminDashboardController@tagSubmit')->name('tag.submit');
         Route::post('tag/submit/bulk','Admins\AdminDashboardController@tagSubmitBulk')->name('tag.submit.bulk');
+        Route::post('set_segment/bulk','Admins\AdminDashboardController@setSegmentBulk')->name('set.segment_bulk');
         Route::post('reject/submit','Admins\AdminDashboardController@rejectReasonSubmit')->name('rejectreason.submit');
         Route::post('auto_shipment_cancel_days/submit','Admins\AdminShipmentCancelController@auto_shipment_cancel_days')->name('auto_shipment_cancel_days.submit');
 
