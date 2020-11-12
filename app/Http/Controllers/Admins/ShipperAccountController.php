b8037cc31bae5487428c749d87b214cb66d1e479
abimwaqas <muhammad.waqas@trax.pk>
2020-11-12 15:39:15 +0500

    fix

--- app/Http/Controllers/Admins/ShipperAccountController.php
+++ app/Http/Controllers/Admins/ShipperAccountController.php
@@ -27,8 +27,6 @@ class ShipperAccountController extends Controller
             if(count($active_users) > 0){
                 $shipments = Shipment::where('created_at', '>', $date)->groupBy('user_id')->pluck('user_id')->toArray();
                 $result = array_diff($active_users,$shipments);
-                dd($result);
-
                 if (count($result) > 0)
                 {
                     foreach ($result as $status) {
