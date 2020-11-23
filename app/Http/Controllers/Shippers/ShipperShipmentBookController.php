c8b918fa43a38a0d58a553af10bb4244b930be20
abimwaqas <muhammad.waqas@trax.pk>
2020-11-23 12:38:15 +0500

    fix

--- app/Http/Controllers/Shippers/ShipperShipmentBookController.php
+++ app/Http/Controllers/Shippers/ShipperShipmentBookController.php
@@ -1553,7 +1553,7 @@ class ShipperShipmentBookController extends Controller
                             $logo_invoice = '<div class="invoice p-1" style="page-break-before: always;">
                         <div class="row"><div class="col-3"><h2>Invoice ' . $invoice_id . '</h2></div></div>
                         <div class="row"><div class="col-6 text-center">
-                        <img src="' . Storage::url('shippers_logo/' . $logo) . '" width="100" class="d-block mb-1">
+                        <img src="' . asset('storage/shippers_logo/' . $logo) . '" width="100" class="d-block mb-1">
     </div><div class="col-6 text-right"><img src="' . asset('img/trax_logo_new.png') . '" width="100" class="d-block mb-1" style="margin: 0 auto;"></div></div>
                         
                         <div class="row align-items-start justify-content-between p-2">
