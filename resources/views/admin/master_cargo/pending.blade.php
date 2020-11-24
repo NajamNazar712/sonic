5eb99f2ee7d66042ca09eae82888357acc0229bf
Anas Anwer <anas.anwer@trax.pk>
2020-11-24 11:18:17 +0500

    changes

--- resources/views/admin/master_cargo/pending.blade.php
+++ resources/views/admin/master_cargo/pending.blade.php
@@ -229,7 +229,7 @@
                     }
                 },
                 rowId: 'id',
-                order: [[15, 'desc']],
+                order: [[16, 'desc']],
                 columns: [
                     {data: 'serial_number', orderable: false, searchable: false, name: 'serial_number', class: 'align-middle serial_number', targets: 1, render: function (data, type, row) {return '';}},
                     {data: 'seal_number', name: 'bags.seal_number', class: 'align-middle seal_number'},
