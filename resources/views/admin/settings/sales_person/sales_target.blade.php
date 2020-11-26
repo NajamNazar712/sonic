95ed28b88945d8c9afae14da6ccebbd16a188a3b
Nabeel973 <nabeelsiddiqui324@gmail.com>
2020-11-26 10:55:32 +0500

    changes

--- resources/views/admin/settings/sales_person/sales_target.blade.php
+++ resources/views/admin/settings/sales_person/sales_target.blade.php
@@ -22,9 +22,10 @@
                                     <form id="settings_form" class="form-horizontal text-center" method="POST" action="{{ route('admin.settings.sales.targets.update') }}" novalidate="novalidate">
                                         {{ csrf_field() }}
                                         <div class="form-group">
-                                            <input id="daterange" type="text" name="daterange" class="form-control" data-msg-required="Date Range is required" data-rule-required="true" required="required" value="" />
+                                           {{-- <input id="daterange" type="text" name="daterange" class="form-control" data-msg-required="Date Range is required" data-rule-required="true" required="required" value="" />
                                             <input type="hidden" name="start_date" id="start_date">
-                                            <input type="hidden" name="end_date" id="end_date">
+                                            <input type="hidden" name="end_date" id="end_date">--}}
+                                            <input type="text" name="search_date_from"  class="form-control bg-primary border-primary white rounded-right pickadate" id="search_date_from" placeholder="Receiving Sheet From">
                                         </div>
                                         
 
@@ -104,16 +105,21 @@
 
 @section('css')
     <link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/forms/selects/select2.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('app-assets/vendors/css/pickers/pickadate/pickadate.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/plugins/pickers/daterange/daterange.min.css')}}">
 @endsection
 
 @section('js')
     <script src="{{asset('app-assets/vendors/js/forms/extended/inputmask/jquery.inputmask.bundle.min.js')}}" type="text/javascript"></script>
     <script src="{{asset('app-assets/vendors/js/forms/validation/jquery.validate.min.js')}}" type="text/javascript"></script>
+    <script src="{{asset('app-assets/vendors/js/pickers/pickadate/picker.js')}}" type="text/javascript"></script>
+    <script src="{{asset('app-assets/vendors/js/pickers/pickadate/picker.date.js')}}" type="text/javascript"></script>
+    <script src="{{asset('app-assets/vendors/js/pickers/pickadate/legacy.js')}}" type="text/javascript"></script>
     <script src="{{asset('app-assets/vendors/js/forms/select/select2.full.min.js')}}" type="text/javascript"></script>
 <script src="{{asset('js/datatable_buttons.js')}}" type="text/javascript"></script>
     <script>
         $(document).ready(function() {
-            $('#daterange').daterangepicker({
+           /* $('#daterange').daterangepicker({
                 opens: 'left',
                 timePicker:false,
                 todayHighlight: true,
@@ -122,6 +128,19 @@
                 console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
                 $('#start_date').val(start.format('YYYY-MM-DD'));
                 $('#end_date').val(end.format('YYYY-MM-DD'));
+            });*/
+            var search_date_from = $('#search_date_from').pickadate({
+                firstDay: 1,
+                clear: '',
+                selectYears: true,
+                selectMonths: true,
+                formatSubmit: 'yyyy-mm-dd 00:00:00',
+                hiddenSuffix: '_formatted',
+                onSet: function(context) {
+                    if (context.select) {
+                        $('#search_date_to').pickadate('picker').set('min', $('#search_date_from').pickadate('picker').get('select'));
+                    }
+                }
             });
 
 
