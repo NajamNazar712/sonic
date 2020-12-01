30d17957e416a0bd51ad0309474d26267ddc30d7
umairbadar <umair.badar@trax.pk>
2020-12-01 16:06:52 +0500

    changes

--- app/Http/Controllers/Rider/RiderAPIController.php
+++ app/Http/Controllers/Rider/RiderAPIController.php
@@ -1426,7 +1426,9 @@ class RiderAPIController extends Controller {
 
                 $rider_pickup->shipments = $request->shipments;
 
-                $picture_path = 'rider_pickup/' . $rider_pickup->pickup_request_id . '.png';
+                $rider_pickup->save();
+
+                $picture_path = 'rider_pickup/' . $rider_pickup->id . '.png';
                 Storage::disk('public')->put($picture_path, file_get_contents($request->picture));
                 $rider_pickup->picture_path = $picture_path;
 
