766655daba2ea171d2af065906c70f094a8d5a12
Nabeel973 <nabeelsiddiqui324@gmail.com>
2020-12-07 12:08:03 +0500

    runner report

--- resources/views/admin/runner/index.blade.php
+++ resources/views/admin/runner/index.blade.php
@@ -21,10 +21,28 @@
                         <th class="border-primary border-darken-1">Contact No.</th>
                         <th class="border-primary border-darken-1">Created At</th>
                         <th class="border-primary border-darken-1">Created By</th>
+                        <th class="border-primary border-darken-1">Status</th>
                         <th class="border-primary border-darken-1">Action</th>
                     </tr>
                     </thead>
                 </table>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="ViewDetailsModal" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="ViewDetailsModal"
+         aria-hidden="true">
+        <div class="modal-dialog modal-xl" role="document">
+            <div class="modal-content">
+                <div class="modal-header text-center">
+                    <h4 class="modal-title w-100 font-weight-bold">View Details</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+
+                </div>
+
             </div>
         </div>
     </div>
@@ -147,6 +165,7 @@
                             head.push('Contact No.');
                             head.push('Created At');
                             head.push('Created By');
+                            head.push('Status');
 
                             $.each(result.data, function(index, values) {
                                 row = [];
@@ -159,6 +178,7 @@
                                 row.push(values.contact_no);
                                 row.push(values.created_at);
                                 row.push(values.created_by);
+                                row.push(values.status);
 
                                 body.push(row);
                             });
@@ -209,6 +229,7 @@
                     {data: 'contact_no', name: 'runner_details.contact_no', class: 'align-middle text-center contact_no'},
                     {data: 'created_at', name: 'runner_details.created_at', class: 'align-middle text-center created_at'},
                     {data: 'created_by', name: 'a.name', class: 'align-middle text-center created_by'},
+                    {data: 'status', name: 'runner_details.status', class: 'align-middle text-center status'},
                     {data: 'action', name: 'action', class: 'align-middle text-center action', orderable: false, searchable: false}
 
                 ],
@@ -222,6 +243,10 @@
                     var td = '<td style="padding:5px;" class="border-primary border-lighten-2"><fieldset class="form-group m-0 position-relative has-icon-right"></fieldset></td>';
                     var input = '<input type="text" class="form-control form-control-sm input-sm primary">';
                     var icon = '<div class="form-control-position primary"><i class="la la-search"></i></div>';
+                    var status_select = '<select name="status_select" id="status_select" class="select2 form-control">' +
+                        '<option value="1">Completed</option>' +
+                        '<option value="0">Update</option>' +
+                        '</select>';
 
                     this.api().columns().every(function(column_id) {
                         var column = this;
@@ -230,6 +255,12 @@
 
                         if ($(header).is('.serial_number') || $(header).is('.action')) {
                             $(td).appendTo($(search));
+                        }
+                        else if($(header).is('.status')){
+                            $(status_select).appendTo($(search))
+                                .on( 'change', function () {
+                                    column.search($(this).val(), false, false, true).draw();
+                                } ).wrap(td);
                         }
                         else {
                             var current = $(input).appendTo($(search)).on('change', function() {
@@ -241,6 +272,12 @@
                             }
                         }
                     });
+                    $("#status_select").prepend('<option value="" selected></option>').select2({
+                        placeholder: "Select Status",
+                        width:'100%',
+                        containerCssClass: 'select-xs',
+                        dropdownCssClass: 'form-control-sm p-0'
+                    });
                     this.api().table().columns.adjust();
                 }
             });
@@ -250,6 +287,54 @@
 
                 window.location = link.substr(0, link.lastIndexOf('/')) + '/' + id;
             });
+
+            $('#datatable tbody').on('click', 'tr td.action .btn-group .dropdown-menu .dropdown-item', function() {
+
+                var id = table.row( $(this).parents('tr') ).data().id;
+
+                if ($(this).hasClass('details')) {
+                    $.ajax({
+                        url: '{!! route('admin.runner.view_details') !!}',
+                        method: 'POST',
+                        data: {
+                            'id': id,
+                            '_token': '{{ csrf_token() }}'
+                        }
+                    }).done(function (data) {
+                        console.log(id);
+                        if (data.runner_detail_times.length != 0) {
+                            var html = '';
+                            html += '<table class="table table-sm datatable text-center">';
+                            html += '<thead><tr><th>S No.</th><th>Origin</th><th>Destination</th><th>Departure Date</th><th>Departure Time</th><th>Departure Time</th><th>Arrival Date</th><th>Arrival Time</th><th>Comment</th></tr></thead>';
+                            html += '<tbody>';
+                            $.each(data.runner_detail_times, function (index, value) {
+                                var ind = index + 1;
+                                html += '<tr class=""><td>' + ind + '</td>';
+                                html += '<td>' + value.origin + '</td>';
+                                html += '<td>' + value.destination + '</td>';
+                                html += '<td>' + value.departure_date + '</td>';
+                                html += '<td>' + value.departure_time + '</td>';
+                                html += '<td>' + value.arrival_date + '</td>';
+                                html += '<td>' + value.arrival_time + '</td>';
+                                html += '<td>' + value.stay_time + '</td>';
+                                if(value.comment !== null){
+                                    html += '<td>' + value.comment + '</td>';
+                                }
+                                else{
+                                    html += '<td>-</td>';
+                                }
+                            });
+                            html += '</tbody></table>';
+
+                            $('#ViewDetailsModal .modal-body').html(html);
+                            $('#ViewDetailsModal').modal('show');
+
+                        }
+                    })
+                }
+            });
+
+
             $('#runner_select_form').validate({
                 errorClass: 'danger',
                 successClass: 'success',
