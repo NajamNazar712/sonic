d6588e44b7e71d4aaf971bc8cf724f66f370fa2e
Hammad Khan <hammad.khan@trax.pk>
2020-12-07 15:37:57 +0500

    Changes

--- resources/views/admin/petty_cash/statements.blade.php
+++ resources/views/admin/petty_cash/statements.blade.php
@@ -466,7 +466,7 @@
                     }
                 },
                 rowId: 'statement_id',
-                order: [1, 'desc'],
+                order: [8, 'desc'],
                 columns: [
                     {data: 'statement_id', orderable: false, searchable: false, class: 'text-center align-middle select select-checkbox p-1', targets: 0, render: function (data, type, row) {return '';}},
                     {orderable: false, searchable: false, name: 'serial_number', class: 'align-middle serial_number', targets: 0, render: function (data, type, row) {return '';}},
