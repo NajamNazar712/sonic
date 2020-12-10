db2c4d9bd4c5cc98330d8e43b7aa7f575723af19
Hammad Khan <hammad.khan@trax.pk>
2020-12-10 15:23:00 +0500

    Changes

--- database/seeds/PendingDeliveriesReport.php
+++ database/seeds/PendingDeliveriesReport.php
@@ -14,7 +14,7 @@ class PendingDeliveriesReport extends Seeder
         $timestamp = \Carbon\Carbon::now()->format('Y-m-d H:i:s');
         DB::table('notifications')->where('id', 110)->delete();
         DB::table('notifications')->insert(array(
-            array('id' => 110, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Pending Deliveries Report Email', 'type_id' => 1, 'subject' => '[date] Pending Deliveries', 'body' => 'Please check daily report dated [date],' . PHP_EOL . '[link]', 'updated_by' => 3, 'status' => 0)
+            array('id' => 110, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Pending Deliveries Report Email', 'type_id' => 1, 'subject' => 'Pending Deliveries', 'body' => 'Please find below the link to download Pending Deliveries Report' . PHP_EOL . '[link]', 'updated_by' => 3, 'status' => 0)
         ));
     }
 }
