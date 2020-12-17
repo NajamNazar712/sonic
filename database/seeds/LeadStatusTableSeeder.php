2537e0b29c63ca206cf35590de55caf818a1ee4f
Anas Anwer <anas.anwer@trax.pk>
2020-12-17 14:13:11 +0500

    working

--- database/seeds/LeadStatusTableSeeder.php
+++ database/seeds/LeadStatusTableSeeder.php
@@ -15,9 +15,11 @@ class LeadStatusTableSeeder extends Seeder
 
         DB::table('lead_statuses')->insert(array(
             array('id' => 1, 'name' => 'Lead Received'),
-            array('id' => 2, 'name' => 'Follow-Up'),
-            array('id' => 3, 'name' => 'Proposal Sent'),
-            array('id' => 4, 'name' => 'Unresponsive'),
+            array('id' => 2, 'name' => 'Unresponsive'),
+            array('id' => 3, 'name' => 'Irrelevant'),
+            array('id' => 4, 'name' => 'Not Interested'),
+            array('id' => 2, 'name' => 'Data Gathered'),
+            array('id' => 2, 'name' => 'Proposal Sent'),
             array('id' => 5, 'name' => 'Proceed for Account Activation'),
             array('id' => 6, 'name' => 'Account Activated')
         ));
