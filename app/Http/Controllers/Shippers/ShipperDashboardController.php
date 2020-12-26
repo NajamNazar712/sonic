153df5f47b1b3d9d8865cc5d0f82e4ef246d7c84
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2020-12-26 16:26:25 +0500

    Order Management
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- app/Http/Controllers/Shippers/ShipperDashboardController.php
+++ app/Http/Controllers/Shippers/ShipperDashboardController.php
@@ -154,12 +154,19 @@ class ShipperDashboardController extends Controller
       return view('client.dashboard')->with(['cities'=>$cities,'dispute_types'=>$dispute_types,'shipment_status'=>$shipment_status,'service_type'=>$service_type,'products'=>$products,'payment_status'=>$payment_status, 'case_nature' => $case_nature, 'case_nature_complaints' => $case_nature_type_complaints, 'case_nature_service_requests' => $case_nature_type_service_requests, 'case_nature_type_claims' => $case_nature_type_claims, 'business_categories' => $business_categories]);
     }
     public function orders_list(Request $request) {
-        $count = Shipment::where(function ($query) {
+        if (!in_array(session('user_id'), [167, 1159, 2035, 3324, 4740, 4758])) {
+            $connection = 'reports';
+        }
+        else {
+            $connection = 'mysql';
+        }
+
+        $count = DB::connection($connection)->table('shipments')->where(function ($query) {
             $query->where('shipments.user_id', session('user_id'))
                 ->orwhereIn('shipments.user_id', session('sister_users'));
             })->count();
 
-        $shipments = DB::connection('reports')->table('shipments')->join('users as u', 'shipments.user_id', '=', 'u.id')
+        $shipments = DB::connection($connection)->table('shipments')->join('users as u', 'shipments.user_id', '=', 'u.id')
             ->join('user_shipping_infos AS usi', 'shipments.pickup_address_id', '=', 'usi.id')
             ->join('cities AS oc', 'usi.city_id', '=', 'oc.id')
             ->join('cities AS dc', 'shipments.consignee_city_id', '=', 'dc.id')
