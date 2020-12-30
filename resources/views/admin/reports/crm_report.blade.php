df8789a4dd4a55fd1178e3633a9d90b62dfdf97b
Nabeel973 <nabeelsiddiqui324@gmail.com>
2020-12-30 12:32:14 +0500

    cod

--- resources/views/admin/reports/crm_report.blade.php
+++ resources/views/admin/reports/crm_report.blade.php
@@ -150,6 +150,7 @@
                         <th class="border-primary border-darken-1">Arrival Date</th>
                         <th class="border-primary border-darken-1">Channel</th>
                         <th class="border-primary border-darken-1">Agent</th>
+                        <th class="border-primary border-darken-1">COD Adjusted Amount</th>
                         <th class="border-primary border-darken-1">Request Status</th>
                         <th class="border-primary border-darken-1">Launched By</th>
                         <th class="border-primary border-darken-1">Launched By User Type</th>
@@ -344,6 +345,7 @@
                             head.push('Arrival Date');
                             head.push('Channel');
                             head.push('Agent');
+                            head.push('COD Adjusted Amount');
                             head.push('Request Status');
                             head.push('Launched By');
                             head.push('Launched By User Type');
@@ -376,6 +378,7 @@
                                 row.push(values.arrival_date);
                                 row.push(values.channel);
                                 row.push(values.agent);
+                                row.push(values.cod_adjusted);
                                 row.push(values.request_status);
                                 row.push(values.launched_by_name);
                                 row.push(values.launched_by_type);
@@ -458,6 +461,7 @@
                     {data: 'arrival_date', name: 'sj.created_at', class: 'align-middle arrival_date'},
                     {data: 'channel', name: 'crc.id', class: 'align-middle channel'},
                     {data: 'agent', name: 'a.name', class: 'align-middle agent'},
+                    {data: 'cod_adjusted', name: 'dps.amount', class: 'align-middle cod_adjusted'},
                     {data: 'request_status', name: 'crs.name', class: 'align-middle request_status'},
                     {data: 'launched_by_name', name: 'launched_by_name', class: 'align-middle launched_by_name'},
                     {data: 'launched_by_type', name: 'crm_requests.launched_by', class: 'align-middle launched_by_type'},
