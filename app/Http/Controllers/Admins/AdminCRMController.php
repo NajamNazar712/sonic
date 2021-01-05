89905a66c5a2b2996b3803e6122f53eeb0a7773c
Nabeel973 <nabeelsiddiqui324@gmail.com>
2021-01-05 08:52:52 +0500

    changes

--- app/Http/Controllers/Admins/AdminCRMController.php
+++ app/Http/Controllers/Admins/AdminCRMController.php
@@ -82,7 +82,7 @@ class AdminCRMController extends Controller
         $channel_id = $request->channel_id;
         $receiving_sheet_id = $request->receiving_sheet_id;
         if($complaint_id == 17 && $receiving_sheet_id != null){
-            $description_text = $request->description;
+            $description_text = $request->description ;
             $description = '<strong>' .'Receiving Sheet No: ' .$receiving_sheet_id. '</strong>'. PHP_EOL. $description_text;
         }
         else{
