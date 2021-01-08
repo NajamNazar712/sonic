cbcbcd2229f29b17fed260ba33f6d1a705c1981b
Hammad Khan <hammad.khan@trax.pk>
2021-01-08 12:30:03 +0500

    Finance Approved Button

--- app/Http/Controllers/Admins/AdminPettyCashController.php
+++ app/Http/Controllers/Admins/AdminPettyCashController.php
@@ -1597,17 +1597,18 @@ class AdminPettyCashController extends Controller
                             $flag = TRUE;
                         }
                     }
-                    else if ($petty->finance_received_statement_by == null) {
-                        if (session('role_id') == 1 || in_array(191, session('permissions'))) {
-                            $petty->finance_received_statement_by = Auth::id();
-                            $petty->finance_received_statement_at = Carbon::now();
-                            $petty->status = 7;
-                            $petty->save();
-                            $this->petty_cash_statement_details_auto_approve($request->statement_id);
-                            $flag = TRUE;
-                        }
-                    }
-                    else if ($petty->finance_approved_by == null) {
+                    else if ($petty->finance_approved_by == null && $petty->operation_approved_by != null) {
+                        return response()->json(['status' => 1, 'error' => 'Previous status is not updated yet!']);
+//                        if (session('role_id') == 1 || in_array(191, session('permissions'))) {
+//                            $petty->finance_approved_by = Auth::id();
+//                            $petty->finance_approved_at = Carbon::now();
+//                            $petty->status = 3;
+//                            $petty->save();
+//                            $this->petty_cash_statement_details_auto_approve($request->statement_id);
+//                            $flag = TRUE;
+//                        }
+                        /*tum condition likh do*/
+                    } else if ($petty->finance_approved_by == null) {
                         if (session('role_id') == 1 || in_array(191, session('permissions'))) {
                             $petty->finance_approved_by = Auth::id();
                             $petty->finance_approved_at = Carbon::now();
