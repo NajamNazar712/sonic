d0fb588eb02830a47152c6d61189a1cf54ea12c6
Hammad Khan <hammad.khan@trax.pk>
2021-01-11 16:08:38 +0500

    Finance Approved

--- resources/views/admin/petty_cash/statements.blade.php
+++ resources/views/admin/petty_cash/statements.blade.php
@@ -224,7 +224,7 @@
             var table = $('#datatable').DataTable({
                 dom: '<"d-inline-block"l><"pull-right"B>tipr',
                 buttons:[
-                        @if (session('role_id') == 1 || in_array(190, session('permissions')))
+                        @if (session('role_id') == 1 || in_array(173, session('permissions')))
                     {
                         className: 'btn btn-primary finance',
                         text: 'Finance Approved',
