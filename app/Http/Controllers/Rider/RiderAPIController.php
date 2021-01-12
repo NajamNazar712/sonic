2aca8685cbf7026cebb99ef856e766c773f69fba
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-01-12 10:51:27 +0500

    changes in delivery history api

--- app/Http/Controllers/Rider/RiderAPIController.php
+++ app/Http/Controllers/Rider/RiderAPIController.php
@@ -1953,8 +1953,8 @@ class RiderAPIController extends Controller {
                 ->where('r.id', '=', $rider_id);
 
             if ($from_date != null) {
-                $to_date = str_replace("00:00:00", "23:59:59", $from_date);
-                $rider_pickups = $rider_pickups->whereBetween('v2_rider_pickups.created_at', [$from_date, $to_date]);
+//                $to_date = str_replace("00:00:00", "23:59:59", $from_date);
+                $rider_pickups = $rider_pickups->whereDate('v2_rider_pickups.created_at',$from_date);
             }
             if ($pickup_request_id != null) {
                 $rider_pickups = $rider_pickups->where('v2_rider_pickups.pickup_request_id', $pickup_request_id);
@@ -1996,8 +1996,8 @@ class RiderAPIController extends Controller {
                 ->where('riders.id', '=', $rider_id);
 
             if ($from_date != null) {
-                $to_date = str_replace("00:00:00", "23:59:59", $from_date);
-                $rider_deliveries = $rider_deliveries->whereBetween('delivery_notes.created_at', [$from_date, $to_date])
+//                $to_date = str_replace("00:00:00", "23:59:59", $from_date);
+                $rider_deliveries = $rider_deliveries->whereDate('delivery_notes.created_at', $from_date)
                     ->groupBy('delivery_notes.id');
             }
 
