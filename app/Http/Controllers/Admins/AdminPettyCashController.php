92c48849f9ff34dd12f2c0cfed4f1d01ff8358a8
Hammad Khan <hammad.khan@trax.pk>
2021-01-12 10:07:31 +0500

    approved

--- app/Http/Controllers/Admins/AdminPettyCashController.php
+++ app/Http/Controllers/Admins/AdminPettyCashController.php
@@ -1578,7 +1578,7 @@ class AdminPettyCashController extends Controller
             foreach ($statement_ids as $statement_id) {
                 $petty = PettyCashStatement::find($statement_id);
                 if ($petty) {
-                    if ($petty->station_approved_by == null) {
+                    if ($petty->created_by != null && $petty->station_approved_by == null) {
                         if (session('role_id') == 1 || in_array(190, session('permissions'))) {
                             $petty->station_approved_by = Auth::id();
                             $petty->station_approved_at = Carbon::now();
@@ -1601,8 +1601,14 @@ class AdminPettyCashController extends Controller
                             $flag = TRUE;
                         }
                     }
-                    else if ($petty->station_approved_by != null)  {
+                    else if ($petty->operation_approved_by != null){
                         return response()->json(['status' => 1, 'error' => 'Already Approved!']);
+                    }
+                    else if ($petty->operation_approved_by != null && $petty->finance_approved_by == null) {
+                        return response()->json(['status' => 1, 'error' => 'Previous status is not updated yet!']);
+                    }
+                    else if ($petty->created_by != null && $petty->operation_approved_by == null) {
+                        return response()->json(['status' => 1, 'error' => 'Previous status is not updated yet!']);
                     }
                     else if ($petty->finance_received_statement_by != null && $petty->finance_approved_by == null){
                         if (session('role_id') == 1 || in_array(173, session('permissions'))) {
@@ -1613,15 +1619,6 @@ class AdminPettyCashController extends Controller
                             $this->petty_cash_statement_details_auto_approve($request->statement_id);
                             $flag = TRUE;
                         }
-                    }
-                    else if ($petty->finance_approved_by == null && $petty->operation_approved_by != null) {
-                        return response()->json(['status' => 1, 'error' => 'Previous status is not updated yet!']);
-                    }
-                    if ($flag) {
-                        return response()->json(['status' => 0, 'success' => 'Approved!']);
-                    }
-                    else {
-                        return response()->json(['status' => 1, 'error' => 'Previous status is not updated yet!']);
                     }
                 }
             }
