257fb759abbd707332da072def0d15c6e4b15faf
Hammad Khan <hammad.khan@trax.pk>
2021-01-14 15:15:16 +0500

    Finance Approved

--- app/Http/Controllers/Admins/AdminPettyCashController.php
+++ app/Http/Controllers/Admins/AdminPettyCashController.php
@@ -1599,6 +1599,9 @@ class AdminPettyCashController extends Controller
                             $flag = TRUE;
                         }
                     }
+                    else if ($petty->operation_approved_by != null && $petty->finance_approved_by == null) {
+                        return response()->json(['status' => 1, 'error' => 'Previous status is not updated yet!']);
+                    }
                     else if ($petty->finance_received_statement_by != null && $petty->finance_received_by == null) {
                         if (session('role_id') == 1 || in_array(173, session('permissions'))) {
                             $petty->finance_approved_by = Auth::id();
@@ -1609,14 +1612,8 @@ class AdminPettyCashController extends Controller
                             $flag = TRUE;
                         }
                     }
-                    else if ($petty->station_approved_by != null || $petty->operation_approved_by != null){
+                    else if ($petty->station_approved_by != null || $petty->operation_approved_by != null || $petty->finance_approved_by != null){
                         return response()->json(['status' => 1, 'error' => 'Already Approved!']);
-                    }
-                    else if ($petty->operation_approved_by != null && $petty->finance_approved_by == null){
-                        return response()->json(['status' => 1, 'error' => 'Previous status is not updated yet!']);
-                    }
-                    else if ($petty->finance_received_statement_by != null && $petty->finance_approved_by == null){
-                        return response()->json(['status' => 1, 'error' => 'Previous status is not updated yet!']);
                     }
                 }
 
