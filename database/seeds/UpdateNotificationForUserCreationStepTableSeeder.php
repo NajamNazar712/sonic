3ad0da7751b1e2cfa3a6447d47f288c040249d98
Anas Anwer <anas.anwer@trax.pk>
2021-01-25 19:26:36 +0500

    changes

--- database/seeds/UpdateNotificationForUserCreationStepTableSeeder.php
+++ database/seeds/UpdateNotificationForUserCreationStepTableSeeder.php
@@ -14,7 +14,9 @@ class UpdateNotificationForUserCreationStepTableSeeder extends Seeder
         $timestamp = \Carbon\Carbon::now()->format('Y-m-d H:i:s');
 
         DB::table('notifications')->insert(array(
-            array('id' => 81, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Tagging History', 'type_id' => 1, 'subject' => 'Tagging History', 'body' => 'This is to bring to your kind attention that you have been assigned as a new sales person for the following shipper(s).'. PHP_EOL .'[preview]' , 'updated_by' => 3, 'status' => 0)
+            array('id' => 200, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Admin User Request Created', 'type_id' => 1, 'subject' => 'New user request generated', 'body' => 'Dear [admin],'. PHP_EOL . PHP_EOL .'New email request generated for [admin_user_name].'. PHP_EOL .'[preview]' , 'updated_by' => 7, 'status' => 0),
+            array('id' => 201, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Admin User Request Verified', 'type_id' => 1, 'subject' => 'New user request verified by HR', 'body' => 'Dear [admin],'. PHP_EOL . PHP_EOL .'New email request generated for [admin_user_name].'. PHP_EOL .'[preview]' , 'updated_by' => 7, 'status' => 0),
+            array('id' => 202, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Admin User Request Completed', 'type_id' => 1, 'subject' => '[admin_user_name] successfully created', 'body' => 'Dear [admin],'. PHP_EOL . PHP_EOL .'New email request generated for [admin_user_name].'. PHP_EOL .'[preview]' , 'updated_by' => 7, 'status' => 0)
         ));
     }
 }
