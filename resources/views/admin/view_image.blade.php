4c137278d19eed5b6b5b0917861625afb91d9a7f
umairbadar <umair.badar@trax.pk>
2021-01-29 16:42:51 +0500

    changes

--- resources/views/admin/view_image.blade.php
+++
@@ -1,8 +0,0 @@
-@php
-    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-    header("Cache-Control: post-check=0, pre-check=0", false);
-    header("Pragma: no-cache");
-@endphp
-<div style="width: 300px">
-    <img class="" src="{{asset($url)}}" alt="" title="" style="width: 100%"/>
-</div>
