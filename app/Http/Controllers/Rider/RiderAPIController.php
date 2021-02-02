f0e8de5c70a54eb16141f67e22f0b073864cd714
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-02-02 11:14:27 +0500

    changes

--- app/Http/Controllers/Rider/RiderAPIController.php
+++ app/Http/Controllers/Rider/RiderAPIController.php
@@ -2753,7 +2753,8 @@ class RiderAPIController extends Controller {
         } else {
             $rider_return_deliveries = ReturnNote::join('return_note_shipments as rns', 'return_notes.id', '=', 'rns.return_note_id')
                 ->join('shipments as s', 'rns.shipment_id', '=', 's.id')
-                ->where('return_notes.rider_id', '=', $rider_id);
+                ->where('return_notes.rider_id', '=', $rider_id)
+                ->where('return_notes.status', '=', 3);
 
             if ($from_date != null) {
                 $rider_return_deliveries = $rider_return_deliveries->whereDate('return_notes.created_at', $from_date)
