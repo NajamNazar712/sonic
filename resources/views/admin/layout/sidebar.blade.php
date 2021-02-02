6bd7c1139d7e5abb693e9483398c599c6062beda
Anas Anwer <anas.anwer@trax.pk>
2021-02-02 10:15:24 +0500

    fixed

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -930,10 +930,6 @@
                         @if (session('role_id') == 1 || in_array(401, session('permissions')))
                           <li><a class="menu-item" href="{{ route('admin.reports.app_efficiency.index') }}">App Efficiency Report</a></li>
                         @endif
-                            @if (session('role_id') == 1 || in_array(430, session('permissions')))
-                          <li><a class="menu-item" href="{{ route('admin.reports.confirmation_pending_report.index') }}">Confirmation Pending Shipments Report</a></li>
-                        @endif
-
                         @if (session('role_id') == 1 || in_array(414, session('permissions')))
                           <li><a class="menu-item" href="{{ route('admin.reports.month_closing.individual.index') }}">Month Closing - Individual</a></li>
                         @endif
