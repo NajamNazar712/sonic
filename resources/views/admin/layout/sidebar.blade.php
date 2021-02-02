d41cc841fc9cf99e4dfedf40aac8d3be4d1aa4a9
Anas Anwer <anas.anwer@trax.pk>
2021-02-02 14:53:23 +0500

    changes

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -327,7 +327,6 @@
             @if (session('role_id') == 1 || count(array_intersect([32, 33, 35, 36, 40, 42, 105, 44, 47, 48, 49, 126, 262, 441], session('permissions'))) !== 0)
                 <li class=" nav-item"><a href="#"><span class="menu-title" data-i18n="nav.dash.main"><i class="la la-motorcycle"></i>Last Mile</span></a>
                     <ul class="menu-content">
-
                         @if (session('role_id') == 1 || count(array_intersect([32, 33, 35, 36, 40, 42, 105, 262, 441], session('permissions'))) !== 0)
                             <li class=" nav-item"><a href="#"><span class="menu-title" data-i18n="nav.dash.main">Delivery</span></a>
                                 <ul class="menu-content">
@@ -351,6 +350,7 @@
                                         @if (session('role_id') == 1 || in_array(105, session('permissions')))
                                             <li class=" nav-item"><a href="#"><span class="menu-title" data-i18n="nav.dash.main">Pending
                                                     Cash Collection</span></a>
+                                            </li>
                                         <ul class="menu-content">
                                             <li><a class="menu-item" href="{{ route('admin.delivery.cash_collection.pending.index') }}">Pending
                                                     Cash Collection COD</a>
