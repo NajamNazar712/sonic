e3e722db48a206fc8a682fb52b7b76a7d4336882
Nabeel973 <nabeelsiddiqui324@gmail.com>
2021-02-02 17:05:18 +0500

    Notification

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -6772,15 +6772,24 @@ class NotificationsController extends Controller
                     $body = $notification->body;
                     $sales_person = $reference_1_id;
                     $admin_id = $reference_2_id;
+                    $to = array();
                     foreach ($sales_person as $index => $person) {
                         $shipper = User::find($index);
                         if (strpos($body, '[shipper_name]') !== FALSE) {
                             $body = str_replace('[shipper_name]', $shipper->name, $body);
                         }
+                        else{
+                            $body = str_replace($shipper_name, $shipper->name, $body);
+                        }
                         if (strpos($body, '[new_sale_person]') !== FALSE) {
                             $body = str_replace('[new_sale_person]', $person['new_sale_person']->name, $body);
                         }
+                        else{
+                            $body = str_replace($sale_person, $person['new_sale_person']->name, $body);
+                        }
                         $to = $person['new_sale_person']->phone_number;
+                        $shipper_name = $shipper->name;
+                        $sale_person = $person['new_sale_person']->name;
                         self::sms($body,$to);
                     }
                 }
--- database/seeds/UpdateNotificationForSalesPersonTagSms.php
+++ database/seeds/UpdateNotificationForSalesPersonTagSms.php
@@ -14,7 +14,7 @@ class UpdateNotificationForSalesPersonTagSms extends Seeder
     {
         $timestamp = \Carbon\Carbon::now()->format('Y-m-d H:i:s');
         DB::table('notifications')->insert(array(
-            array('id' => 119, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Tagging History SMS', 'type_id' => 2, 'subject' => null, 'body' => 'The [shipper_name] has been assigned a new sales person [new_sale_person] ' , 'updated_by' => 3, 'status' => 0)
+            array('id' => 119, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Tagging History SMS', 'type_id' => 2, 'subject' => null, 'body' => 'The Account : [shipper_name] has been assigned a new sales person [new_sale_person] ' , 'updated_by' => 3, 'status' => 0)
         ));
     }
 }
