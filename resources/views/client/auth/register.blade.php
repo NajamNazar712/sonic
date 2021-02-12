0c65761a031cebf392b9dd5c286e54c499f9579e
Nabeel973 <nabeelsiddiqui324@gmail.com>
2021-02-12 17:04:25 +0500

    changes

--- resources/views/client/auth/register.blade.php
+++ resources/views/client/auth/register.blade.php
@@ -887,7 +887,7 @@
                         $('#territory').val('').trigger('change');
                         
                     } else {
-                        $('#territory').val('').trigger('change');
+                        $('#territory').empty();
                         var error = 'No Territory found for the selected city';
                         toastr.error(error, 'Error!', {
                             positionClass: 'toast-top-center',
