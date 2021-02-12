1fe53a2a069789132fa54a83280e450c31378f5e
abimwaqas <muhammad.waqas@trax.pk>
2021-02-12 13:32:34 +0500

    fix

--- resources/views/admin/finance/change_shipment_weight.blade.php
+++ resources/views/admin/finance/change_shipment_weight.blade.php
@@ -21,32 +21,39 @@
 								{{ csrf_field() }}
 
 								<div class="row align-items-center justify-content-center">
-									<div class="col">
+									<div class="col-2">
+										<h3 class="heading"><strong>For Bulk Shipment</strong></h3>
+									</div>
+									<div class="col-4">
 										<div class="form-group">
 											<input type="file" name="shipments" class="w-100 p-1 border-primary" title="Select File" data-rule-required="true" data-msg-required="File is required" data-rule-extension="xls|xlsx" data-msg-extension="Only file with extension xls or xlsx allowed" data-rule-accept="application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" data-msg-accept="Only Excel file allowed" data-rule-maxsize="5242880" data-msg-maxsize="File Size must not exceed 5 MB (5120 KB).">
 										</div>
 									</div>
 
-									<div class="col">
-										<div class="form-group text-left">
+									<div class="col-1">
+										<div class="form-group">
 											<button type="submit" name="upload" class="btn btn-primary">Upload</button>
 										</div>
 									</div>
 
-									<div class="col ml-auto">
-										<div class="form-group text-right">
+									<div class="col-2">
+										<div class="form-group">
 											<a href="{{ asset('file/Change Shipment Weight Template.xlsx') }}" class="btn btn-primary"><i class="la la-download"></i> Download Template</a>
 										</div>
 									</div>
 								</div>
 							</form>
 
-							<form id="search_form" class="form-inline mb-1 justify-content-center" novalidate="novalidate">
-								<div class="form-group">
+
+							<form id="search_form" class="row mb-1 justify-content-center mt-2" novalidate="novalidate">
+								<div class="col-2">
+									<h3 class="heading"><strong>Individual Shipment</strong></h3>
+								</div>
+								<div class="col-3 form-group">
 									<input type="text" name="tracking_numbers" class="form-control tracking_number" placeholder="Tracking Number*" data-rule-required="true" data-msg-required="Tracking Number is required">
 								</div>
 
-								<div class="form-group ml-1">
+								<div class="col-2 form-group">
 									<button type="submit" name="search" class="btn btn-primary search" value="Search">Search</button>
 								</div>
 							</form>
@@ -126,7 +133,7 @@
 				errorClass: 'danger',
 				successClass: 'success',
 				errorPlacement: function(error, element) {
-					error.addClass('w-100').appendTo(element.parents('form'));
+					error.addClass('w-100').appendTo(element.parent('.form-group'));
 				},
 				submitHandler: function(form) {
 					$(form).find('button.search').prop('disabled', true);
