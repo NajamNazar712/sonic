1bb9e77ccc2c203992bf00c42c57a7d70f507944
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-02-19 14:30:09 +0500

    Changes

--- app/Http/Controllers/Rider/RiderAPIController.php
+++ app/Http/Controllers/Rider/RiderAPIController.php
@@ -2802,6 +2802,8 @@ class RiderAPIController extends Controller
             $shipment_status = $pickup_requests->shipper_status_id;
             if ($pickup_requests->current_rider_id == $rider_id) {
                 return response()->json(['status' => 1, 'message' => 'Pickup Already Assigned to You']);
+            } else if ($pickup_requests->current_rider_id != null) {
+                return response()->json(['status' => 1, 'message' => 'Pickup Already Assigned']);
             } elseif ($shipment_status != 1 && $shipment_status != 17) {
                 return response()->json(['status' => 1, 'message' => 'Pickup Already Modified']);
             } else {
@@ -2841,6 +2843,8 @@ class RiderAPIController extends Controller
 
             if ($pickup_requests->current_rider_id == $rider_id) {
                 return response()->json(['status' => 1, 'message' => 'Pickup Already Assigned to You']);
+            } else if ($pickup_requests->current_rider_id != null) {
+                return response()->json(['status' => 1, 'message' => 'Pickup Already Assigned']);
             } else {
                 $pickup_request_id = $pickup_requests->pickup_request_id;
                 $riders = array();
