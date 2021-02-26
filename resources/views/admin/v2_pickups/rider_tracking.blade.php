fea78d8957f8072ff0c6d50c35d34c25b2dbf874
Maaz Khan <Maaz.Khan@Trax.pk>
2021-02-26 17:53:23 +0500

    little change

--- resources/views/admin/v2_pickups/rider_tracking.blade.php
+++ resources/views/admin/v2_pickups/rider_tracking.blade.php
@@ -91,7 +91,7 @@
 
                     <div class="row">
                         <div class="col-12">
-                            <div id="googleMap" style="width:100%;height:400px;"></div>
+                            <div id="googleMap" style="width:100%;height:800px;"></div>
                         </div>
                     </div>
 
