b2c7a254226cdcd118c2028d0c0e6ae125dd9c51
abimwaqas <muhammad.waqas@trax.pk>
2021-03-01 12:42:43 +0500

    change

---
+++ app/Http/Controllers/Admins/Fuel/FuelManagementController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Admins\Fuel;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class FuelManagementController extends Controller
+{
+    //
+}
