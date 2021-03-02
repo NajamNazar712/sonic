e45fcde05e3d9ccb84ff3594423448f4184a6620
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-03-02 10:20:45 +0500

    Changes

--- app/Http/Controllers/Rider/RiderAPIController.php
+++ app/Http/Controllers/Rider/RiderAPIController.php
@@ -3068,12 +3068,10 @@ class RiderAPIController extends Controller
                         $datum['received_or_refused_by'] = $delivery_note_shipment->received_or_refused_by;
                         $datum['picture_path'] = $delivery_note_shipment->picture_path;
                         $datum['updated_by'] = $delivery_note_shipment->updated_by;
-                        if ($delivery_note_shipment->delivery_note_shipments_status == 0) {
-                            $datum['status'] = 'Not Attempt';
-                        } elseif ($delivery_note_shipment->delivery_note_shipments_status == 1) {
-                            $datum['status'] = 'Undelivered';
-                        } elseif ($delivery_note_shipment->delivery_note_shipments_status > 1 && in_array($delivery_note_shipment->shipper_status_id, [14, 30, 36, 37])) {
+                        if (in_array($delivery_note_shipment->shipper_status_id, [14, 30, 36, 37])) {
                             $datum['status'] = 'Delivered';
+                        } else {
+                            $datum['status'] = 'Undelivered';
                         }
                         $data[] = $datum;
                     }
@@ -3083,6 +3081,27 @@ class RiderAPIController extends Controller
                 }
 
             } else if ($request->has('return_note_id')) {
+                $return_note_id = $request->return_note_id;
+                /*$return_note_shipments = ReturnNoteShipment::join('shipments as s', 's.id', '=', 'return_note_shipments.shipment_id')
+                    ->leftjoin('shipments_journey', function ($join) {
+                        $join->on('shipments_journey.shipment_id', '=', 'return_note_shipments.shipment_id')
+                            ->where('shipments_journey.id', '=',
+                                DB::raw('(select max(id) from shipments_journey where shipments_journey.shipment_id = return_note_shipments.shipment_id and reference_1_id = return_note_shipments.return_note_id'));
+                    })
+                    ->leftjoin('rider_return_deliveries', function ($join) {
+                        $join->on('return_note_shipments.shipment_id', '=', 'rider_return_deliveries.shipment_id')
+                            ->where('rider_return_deliveries.id', '=',
+                                DB::raw('(select max(id) from rider_return_deliveries as rrd where rrd.shipment_id = return_note_shipments.shipment_id AND rrd.return_note_id = rider_return_deliveries.return_note_id)'));
+                    })
+                    ->leftjoin('admins as ad','ad.id', 'shipments_journey.admin_id')
+                    ->leftjoin('shipment_status as ss', 'ss.id', '=', 'shipments_journey.shipper_status_id')
+                    ->leftJoin('shipment_status_reason as ssr', 'ssr.id', '=', 'shipments_journey.status_reason_id')
+                    ->select('s.tracking_number','ss.name as shipment_status', 'ssr.name as shipment_reason', 'shipments_journey.created_at as update_date_time', 'shipments_journey.received_or_refused_by', 'rider_return_deliveries.picture_path', 'return_note_shipments.status as return_note_shipments_status', 'ad.name as updated_by')
+                    ->where('return_note_shipments.return_note_id', $return_note_id)->get();
+                return response()->json(['status' => 0, 'history_details' => $return_note_shipments]);*/
+
+
+
                 $return_note_shipments = ReturnNoteShipment::join('shipments as s', 'return_note_shipments.shipment_id', 's.id')
                     ->select('s.tracking_number as tacking_no', 'return_note_shipments.status as status')
                     ->where('return_note_id', $request->return_note_id);
