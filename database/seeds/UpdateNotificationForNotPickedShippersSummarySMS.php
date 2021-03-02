6a072bd9aee46a4bf2a98e90c2706b2ee9dbd535
maqib79 <maqib79@gmail.com>
2021-03-02 09:41:32 +0500

    fix

--- database/seeds/UpdateNotificationForNotPickedShippersSummarySMS.php
+++ database/seeds/UpdateNotificationForNotPickedShippersSummarySMS.php
@@ -14,7 +14,7 @@ class UpdateNotificationForNotPickedShippersSummarySMS extends Seeder
         $timestamp = \Carbon\Carbon::now()->format('Y-m-d H:i:s');
         DB::table('notifications')->where('id', 105)->delete();
         DB::table('notifications')->insert(array(
-            array('id' => 105, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'SMS to sales person', 'type_id' => 2, 'subject' => null, 'body' => 'Dear [sales_person],' . PHP_EOL . 'It is to inform that shipments from [shipper_name] were not picked due to [reason]' . PHP_EOL . 'Regards,' . PHP_EOL . 'Team Trax', 'updated_by' => 3, 'status' => 0)
+            array('id' => 105, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'SMS to sales person', 'type_id' => 2, 'subject' => null, 'body' => 'Dear [sales_person],' . PHP_EOL . 'It is to inform that shipments from [shipper_name] were not picked from [pickup_address], [city_name]  due to [reason]' . PHP_EOL . 'Regards,' . PHP_EOL . 'Team Trax', 'updated_by' => 3, 'status' => 0)
         ));
     }
 }
