527aad91f12e8fbdf27bf8b8dbc789bc4914b2f3
abimwaqas <muhammad.waqas@trax.pk>
2021-03-02 13:50:44 +0500

    fux

--- database/migrations/2021_03_01_125017_create_fuel_card_requests_table.php
+++ database/migrations/2021_03_01_125017_create_fuel_card_requests_table.php
@@ -15,7 +15,8 @@ class CreateFuelCardRequestsTable extends Migration
     {
         Schema::create('fuel_card_requests', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('card_number')->index();
+            $table->string('card_number')->nullable()->index();
+            $table->integer('card_holder_id')->index();
             $table->integer('card_holder_type_id')->index()->nullable();
             $table->integer('card_request_type_id')->index();
             $table->integer('fuel_deduction_type_id')->nullable();
@@ -24,6 +25,7 @@ class CreateFuelCardRequestsTable extends Migration
             $table->integer('requested_by')->index();
             $table->integer('approved_by')->index()->nullable();
             $table->timestamp('approved_at')->nullable();
+            $table->integer('status')->default(0);
             $table->timestamps();
         });
     }
