41ba209e2ffe388c11aef44469426642aa153d58
Anas Anwer <anas.anwer@trax.pk>
2021-03-03 15:48:16 +0500

    fixed

--- app/Http/Controllers/Shippers/ShipperShipmentBookController.php
+++ app/Http/Controllers/Shippers/ShipperShipmentBookController.php
@@ -3336,7 +3336,7 @@ class ShipperShipmentBookController extends Controller
             if ($value) {
                 $value = $this->phone_number($value);
 
-                if (preg_match('/^((\+92)|(92|(0092))-{0,1}\d{3}-{0,1}\d{7}$|^\d{3}-{1}\d{7}$|^\d{11}$|^\d{4}-\d{7}$|^\d{3}-\d{7}$/', $value)) {
+                if (preg_match('/^((\+92)|(92)|(0092))-{0,1}\d{3}-{0,1}\d{7}$|^\d{3}-{1}\d{7}$|^\d{11}$|^\d{4}-\d{7}$|^\d{3}-\d{7}$|^\d{10}$/', $value)) {
                     return TRUE;
                 }
                 else {
