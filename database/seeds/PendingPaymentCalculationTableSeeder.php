777631a7e97045f8023303c43b7c59c869331cc8
abimwaqas <muhammad.waqas@trax.pk>
2021-03-15 11:47:51 +0500

    finance seeder

--- database/seeds/PendingPaymentCalculationTableSeeder.php
+++ database/seeds/PendingPaymentCalculationTableSeeder.php
@@ -17,11 +17,13 @@ class PendingPaymentCalculationTableSeeder extends Seeder
     public function run()
     {
         DB::table('pending_payment_calculations')->truncate();
-        $pending_payments = PendingPayment::pluck('id')->toArray();
+//        $pending_payments = PendingPayment::pluck('id')->toArray();
+        $pending_payments = [15198,34327,26271,13982,20395,41558,35302,5746,41782,33016,5421,5746,13855,41627];
         if(count($pending_payments) > 0){
             foreach ($pending_payments as $payment_id){
                 $pending_payment_shipments = PendingPaymentShipment::where('pending_payment_id', $payment_id)->selectRaw('SUM(amount) as total_amount, SUM(charges) as total_charges, SUM(gst) as total_gst, SUM(payable) as total_payable')->first();
                 if($pending_payment_shipments){
+                    PendingPaymentCalculation::where('pending_payment_id', $payment_id)->delete();
                     $pending_payment_calculation = new PendingPaymentCalculation();
                     $pending_payment_calculation->pending_payment_id = $payment_id;
                     $pending_payment_calculation->amount = $pending_payment_shipments->total_amount;
