635688a6c742767da681a3be11aff7a407b8353b
Anas Anwer <anas.anwer@trax.pk>
2021-03-22 00:04:26 -0700

    chnages

--- app/Http/Controllers/Admins/AdminMasterCargoController.php
+++ app/Http/Controllers/Admins/AdminMasterCargoController.php
@@ -1763,7 +1763,7 @@ class AdminMasterCargoController extends Controller
                               <td class="color primary"><strong>Route Information</strong></td>
                             </tr>
                             <tr>
-                              <td class="text-center">' . $master_cargo->origin_hub->name . ' - ' . $master_cargo->junction_hub_1->name . ' - ' . (($master_cargo->junction_hub_2_id) ? ($master_cargo->junction_hub_2->name . ' - ') : '') . $master_cargo->destination_hub->name . '</td>
+                              <td class="text-center">' . $master_cargo->origin_hub->name . ' - ' . (($master_cargo->junction_hub_1_id) ? ($master_cargo->junction_hub_1->name . ' - ') : '') . ' - ' . (($master_cargo->junction_hub_2_id) ? ($master_cargo->junction_hub_2->name . ' - ') : '') . $master_cargo->destination_hub->name . '</td>
                             </tr>
                           </tbody>
                         </table>
