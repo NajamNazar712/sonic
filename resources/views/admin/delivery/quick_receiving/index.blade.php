7b4bc3dfdff3b7cca1b5fa1864df976e572d28ca
Anas Anwer <anas.anwer@trax.pk>
2021-03-25 00:20:26 -0700

    changess

--- resources/views/admin/delivery/quick_receiving/index.blade.php
+++ resources/views/admin/delivery/quick_receiving/index.blade.php
@@ -208,6 +208,7 @@
                         var rowNo = table.rows().count();
                         var tracking_number_column = `<input type="hidden" form="submit_form" name="tracking_number[]" value="${data.details.tracking_number}">${data.details.tracking_number}`
                         var rowNode = table.row.add([rowNo + 1, tracking_number_column , data.details.status,data.details.reason,data.details.remarks,data.details.status_date, data.details.origin, data.details.destination, data.details.amount, data.details.shipper_name]).node().id = data.details.row_id;
+                        console.log(rowNode);
                         $(rowNode).addClass(data.details.class);
                         table.draw(false);
                         table.order([0, 'asc']).draw();
