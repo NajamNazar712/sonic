3da4ca815b3621e0bad8d4320a597eed09c3f6fb
Maaz Khan <Maaz.Khan@Trax.pk>
2021-03-26 12:05:22 +0500

    permission name change

--- database/seeds/UpdateModulePermissionForPettyCashConsigneeSettings.php
+++ database/seeds/UpdateModulePermissionForPettyCashConsigneeSettings.php
@@ -12,7 +12,7 @@ class UpdateModulePermissionForPettyCashConsigneeSettings extends Seeder
     public function run()
     {
         DB::table('module_permissions')->insert(array(
-            array('id' => 462, 'name' => 'Petty Cash Consignee Setting', 'module_id' => 14)
+            array('id' => 462, 'name' => 'Petty Cash Hub Assigning - View', 'module_id' => 14)
         ));
     }
 }
