8839965453a6c3cfe83a0ea594ade348ecc2233c
Nabeel973 <nabeelsiddiqui324@gmail.com>
2021-04-02 12:38:11 +0500

    changes

--- database/seeds/UpdateSearchSonicSeederForMasterCargoReports.php
+++ database/seeds/UpdateSearchSonicSeederForMasterCargoReports.php
@@ -12,7 +12,7 @@ class UpdateSearchSonicSeederForMasterCargoReports extends Seeder
     public function run()
     {
         $timestamp = \Carbon\Carbon::now()->format('Y-m-d H:i:s');
-        DB::table('admins_screen_list')->insert( array('created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Reports  > In Transit Report Bag Wise', 'url'=>'admin.reports.master_cargo.bag.in_transit.index', 'permission_id' => 421));
+        DB::table('admins_screen_list')->insert( array('created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Reports  > In Transit Report Bag Wise', 'url'=>'admin.reports.master_cargo.bag.in_transit.index', 'permission_id' => 472));
         DB::table('admins_screen_list')->insert( array('created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Reports  > Master Cargo Short Received Shipments', 'url'=>'admin.reports.master_cargo.short_received_shipments.index', 'permission_id' => 319));
 
     }
