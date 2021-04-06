cd1f2acbbe09aacf909e9109f3e514a96e0453c7
Nabeel973 <nabeelsiddiqui324@gmail.com>
2021-04-06 16:15:04 +0500

    changes

--- app/Http/Controllers/ShipperAgreementController.php
+++ app/Http/Controllers/ShipperAgreementController.php
@@ -702,7 +702,7 @@ otherwise it will be rejected</li>
 
                     $intl_box .= '<div class="row"><div class="col-12 border"> <table class="table color secondary table-sm table-bordered mb-0 mt-0"><thead class="color secondary text-center">
                     <tr><td><strong>International Rate(s)</strong></td></tr></thead></table>';
-                    $margin_1 = 0;
+                  /*  $margin_1 = 0;
                     $margin_2 = 0;
                     $margin_3 = 0;
                     $margin_4 = 0;
@@ -723,7 +723,20 @@ otherwise it will be rejected</li>
                     $margin_8 = (float)$international_rate_status->margin_8;
                     $margin_9 = (float)$international_rate_status->margin_9;
                     $margin_10 = (float)$international_rate_status->margin_10;
-                    $margin_11 = (float)$international_rate_status->margin_11;
+                    $margin_11 = (float)$international_rate_status->margin_11;*/
+                  $margin = array('margin_1' => 0,'margin_2' => 0,'margin_3' => 0,'margin_4' => 0,'margin_5' => 0,'margin_6' => 0,'margin_7' => 0,'margin_8' => 0,'margin_9' => 0,'margin_10' => 0,'margin_11' => 0,);
+                    $margin['margin_1']= (float)$international_rate_status->margin_1;
+                    $margin['margin_2']= (float)$international_rate_status->margin_2;
+                    $margin['margin_3']= (float)$international_rate_status->margin_3;
+                    $margin['margin_4']= (float)$international_rate_status->margin_4;
+                    $margin['margin_5']= (float)$international_rate_status->margin_5;
+                    $margin['margin_6']= (float)$international_rate_status->margin_6;
+                    $margin['margin_7']= (float)$international_rate_status->margin_7;
+                    $margin['margin_8']= (float)$international_rate_status->margin_8;
+                    $margin['margin_9']= (float)$international_rate_status->margin_9;
+                    $margin['margin_10'] = (float)$international_rate_status->margin_10;
+                    $margin['margin_11'] = (float)$international_rate_status->margin_11;
+
                     $fuel_charges = 0;
                     $fuel_surcharge = GlobalSettings::where('type', 'international_fuel_surcharge');
                     if($fuel_surcharge->exists()){
@@ -763,17 +776,18 @@ otherwise it will be rejected</li>
                         $intl_weight_charges_details .= '<table class="table table-sm table-bordered mb-0"><thead><tr><th>Range Up</th><th>Range Down</th><th>Zone 1</th><th>Zone 2</th><th>Zone 3</th><th>Zone 4</th><th>Zone 5</th><th>Zone 6</th><th>Zone 7</th><th>Zone 8</th><th>Zone 9</th><th>Zone 10</th><th>Zone 11</th></tr></thead><tbody>';
 
                         foreach ($intl_weight_charges as $weight_charge) {
-                            $zone_1_charges = self::international_charges_calculate($weight_charge->zone_1, $fuel_surcharge_flat, $exchange_rate_charges, $margin_1, $gst_flat);
-                            $zone_2_charges = self::international_charges_calculate($weight_charge->zone_2, $fuel_surcharge_flat, $exchange_rate_charges, $margin_2, $gst_flat);
-                            $zone_3_charges = self::international_charges_calculate($weight_charge->zone_3, $fuel_surcharge_flat, $exchange_rate_charges, $margin_3, $gst_flat);
-                            $zone_4_charges = self::international_charges_calculate($weight_charge->zone_4, $fuel_surcharge_flat, $exchange_rate_charges, $margin_4, $gst_flat);
-                            $zone_5_charges = self::international_charges_calculate($weight_charge->zone_5, $fuel_surcharge_flat, $exchange_rate_charges, $margin_5, $gst_flat);
-                            $zone_6_charges = self::international_charges_calculate($weight_charge->zone_6, $fuel_surcharge_flat, $exchange_rate_charges, $margin_6, $gst_flat);
-                            $zone_7_charges = self::international_charges_calculate($weight_charge->zone_7, $fuel_surcharge_flat, $exchange_rate_charges, $margin_7, $gst_flat);
-                            $zone_8_charges = self::international_charges_calculate($weight_charge->zone_8, $fuel_surcharge_flat, $exchange_rate_charges, $margin_8, $gst_flat);
-                            $zone_9_charges = self::international_charges_calculate($weight_charge->zone_9, $fuel_surcharge_flat, $exchange_rate_charges, $margin_9, $gst_flat);
-                            $zone_10_charges = self::international_charges_calculate($weight_charge->zone_10, $fuel_surcharge_flat, $exchange_rate_charges, $margin_10, $gst_flat);
-                            $zone_11_charges = self::international_charges_calculate($weight_charge->zone_11, $fuel_surcharge_flat, $exchange_rate_charges, $margin_11, $gst_flat);
+                           
+                            $zone_1_charges = self::international_charges_calculate($weight_charge->zone_1, $fuel_surcharge_flat, $exchange_rate_charges,  $margin['margin_1'], $gst_flat);
+                            $zone_2_charges = self::international_charges_calculate($weight_charge->zone_2, $fuel_surcharge_flat, $exchange_rate_charges,  $margin['margin_2'], $gst_flat);
+                            $zone_3_charges = self::international_charges_calculate($weight_charge->zone_3, $fuel_surcharge_flat, $exchange_rate_charges,  $margin['margin_3'], $gst_flat);
+                            $zone_4_charges = self::international_charges_calculate($weight_charge->zone_4, $fuel_surcharge_flat, $exchange_rate_charges, $margin['margin_4'], $gst_flat);
+                            $zone_5_charges = self::international_charges_calculate($weight_charge->zone_5, $fuel_surcharge_flat, $exchange_rate_charges,  $margin['margin_5'], $gst_flat);
+                            $zone_6_charges = self::international_charges_calculate($weight_charge->zone_6, $fuel_surcharge_flat, $exchange_rate_charges,  $margin['margin_6'], $gst_flat);
+                            $zone_7_charges = self::international_charges_calculate($weight_charge->zone_7, $fuel_surcharge_flat, $exchange_rate_charges,  $margin['margin_7'], $gst_flat);
+                            $zone_8_charges = self::international_charges_calculate($weight_charge->zone_8, $fuel_surcharge_flat, $exchange_rate_charges,  $margin['margin_8'], $gst_flat);
+                            $zone_9_charges = self::international_charges_calculate($weight_charge->zone_9, $fuel_surcharge_flat, $exchange_rate_charges,  $margin['margin_9'], $gst_flat);
+                            $zone_10_charges = self::international_charges_calculate($weight_charge->zone_10, $fuel_surcharge_flat, $exchange_rate_charges,  $margin['margin_10'], $gst_flat);
+                            $zone_11_charges = self::international_charges_calculate($weight_charge->zone_11, $fuel_surcharge_flat, $exchange_rate_charges,  $margin['margin_11'], $gst_flat);
 
                             $intl_weight_charges_details .= '<tr><td>' . $weight_charge->range_up . '</td><td>' . $weight_charge->range_down . '</td><td>' . $zone_1_charges . '</td><td>' . $zone_2_charges . '</td><td>' . $zone_3_charges . '</td><td>' . $zone_4_charges . '</td><td>' . $zone_5_charges . '</td><td>' . $zone_6_charges . '</td><td>' . $zone_7_charges . '</td><td>' . $zone_8_charges . '</td><td>' . $zone_9_charges . '</td><td>' . $zone_10_charges . '</td><td>' . $zone_11_charges . '</td></tr>';
                         }
@@ -850,6 +864,7 @@ otherwise it will be rejected</li>
     }
 
     static public function international_charges_calculate($zone_charge, $fsc, $er, $margin, $gst){
+        
         $zone_charge = (float)$zone_charge;
         $overall_charges = 0;
         $overall_charges = $zone_charge * $fsc;
