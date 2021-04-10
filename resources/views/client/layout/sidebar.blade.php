6ab00c47a4f765d3a087153b7231d94f2daca8ad
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2021-04-10 14:59:46 +0500

    Allow
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- resources/views/client/layout/sidebar.blade.php
+++ resources/views/client/layout/sidebar.blade.php
@@ -111,7 +111,7 @@
                         <li><a class="menu-item" href="{{ route('cod.reports.summary.index') }}">Summary</a></li>
                         <li><a class="menu-item" href="{{ route('cod.reports.adjustments.index') }}">Adjustments</a></li>
 
-                            @if (session('user_type') == 1 || in_array(13, session('permissions')))
+                            @if (in_array(13, session('permissions')))
                         <li><a class="menu-item" href="{{ route('cod.reports.confirmation_pending_report.index') }}">Confirmation Pending Shipment Report</a></li>
                             @endif
 
