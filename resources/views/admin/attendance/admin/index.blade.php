64ec52fae96603540d8ca4e4cd5b9cb1f4076696
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-04-19 17:31:34 +0500

    Changes

--- resources/views/admin/attendance/admin/index.blade.php
+++ resources/views/admin/attendance/admin/index.blade.php
@@ -111,12 +111,12 @@
                                     <th class="border-primary border-darken-1">Designation</th>
                                     <th class="border-primary border-darken-1">Department</th>
                                     <th class="border-primary border-darken-1">Date</th>
-                                    <th class="border-primary border-darken-1">Clock In Time</th>
-                                    <th class="border-primary border-darken-1">Clock In Radius</th>
-                                    <th class="border-primary border-darken-1">Clock In Location</th>
-                                    <th class="border-primary border-darken-1">Clock Out Time</th>
-                                    <th class="border-primary border-darken-1">Clock Out Radius</th>
-                                    <th class="border-primary border-darken-1">Clock Out Location</th>
+                                    <th class="border-primary border-darken-1">Clock-in Time</th>
+                                    <th class="border-primary border-darken-1">Clock-in Radius</th>
+                                    <th class="border-primary border-darken-1">Clock-in Location</th>
+                                    <th class="border-primary border-darken-1">Clock-out Time</th>
+                                    <th class="border-primary border-darken-1">Clock-out Radius</th>
+                                    <th class="border-primary border-darken-1">Clock-out Location</th>
                                 </tr>
                                 </thead>
                             </table>
