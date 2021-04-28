0888816e6be2b7e77886bc6877df8a0dce2f2fce
Maaz Khan <Maaz.Khan@Trax.pk>
2021-04-29 02:45:05 +0500

    bug fix little changes

--- resources/views/admin/delivery/receive/add_status.blade.php
+++ resources/views/admin/delivery/receive/add_status.blade.php
@@ -1839,7 +1839,7 @@
                                             if(data.not_suitable_for_rcp) {
                                                 var html = '<p>Since it is the first attempt for following Shipment(s):</p>';
                                                 $.each(data.not_suitable_for_rcp, function (index, shipment_id) {
-                                                    html += '<ul><li>' + shipment_id + '</li></ul><br>';
+                                                    html += '<ol><li>' + shipment_id + '</li></ol><br>';
                                                 });
                                                 html += '<p>Therefore can not apply Shipment - Return Confirmation Pending status on following Shipment(s).</p>';
                                                 content = document.createElement('div');
