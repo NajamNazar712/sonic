2669c1c97f35a19bf14c3f65f3fe9aa3bc6d598e
Nabeel973 <nabeelsiddiqui324@gmail.com>
2021-04-29 12:18:48 +0500

    rates

--- resources/views/admin/accounts/corporate/zone_wise/edit_rates.blade.php
+++ resources/views/admin/accounts/corporate/zone_wise/edit_rates.blade.php
@@ -4030,7 +4030,7 @@
                                     @if ($shipper->new_rate_type_id != null && $shipper->status == 3 && $shipper->rate_type_id_status == 1 && (session('role_id') == 1 || in_array(140, session('permissions'))))
                                         <button id="accountApproveChangeSubmit" type="submit" class="btn btn-outline-primary round btn-min-width mr-1 mb-1">Approve Change</button>
                                     @endif
-                                    @if (($shipper->new_rate_type_id != null && ($shipper->status == 3 && $shipper->rate_type_id_status == 1) && (session('role_id') == 1 || in_array(8, session('permissions')))) && (session('role_id') == 1 || in_array(140, session('permissions')))))
+                                    @if (($shipper->new_rate_type_id != null && ($shipper->status == 3 && $shipper->rate_type_id_status == 1) && (session('role_id') == 1 || in_array(8, session('permissions')))) && (session('role_id') == 1 || in_array(140, session('permissions'))))
                                     <button id="accountRejectActiveSubmit" type="button" class="btn btn-outline-danger round btn-min-width mr-1 mb-1">Reject Change</button>
                                     @endif
                                 </div>
