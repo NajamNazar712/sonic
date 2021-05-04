f109ec41f073e4e2ec9add0b84f435c63ed8076e
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-05-04 22:47:07 +0500

    Changes

--- app/Http/Controllers/Rider/RiderAPIController.php
+++ app/Http/Controllers/Rider/RiderAPIController.php
@@ -6532,6 +6532,87 @@ class RiderAPIController extends Controller
                     }
                     return response()->json(['status' => 0, 'data' => $experience_certificate_array]);
                 }
+            } else {
+                $attachment_type = $request->attachment_type;
+                if ($attachment_type == 'cv') {
+                    $cv_array = [];
+                    for ($x = 0; $x <= 3; $x++) {
+                        $cv_array[$x]['cv_' . ($x + 1)] = "";
+                    }
+                    return response()->json(['status' => 0, 'data' => $cv_array]);
+                } elseif ($attachment_type == 'cnic') {
+                    $cnic_array = [];
+                    for ($x = 0; $x <= 3; $x++) {
+                        $cnic_array[$x]['cnic_' . ($x + 1)] = "";
+                    }
+                    return response()->json(['status' => 0, 'data' => $cnic_array]);
+                } elseif ($attachment_type == 'photo') {
+                    $photo_array = [];
+                    for ($x = 0; $x <= 3; $x++) {
+                        $photo_array[$x]['photo_' . ($x + 1)] = "";
+                    }
+                    return response()->json(['status' => 0, 'data' => $photo_array]);
+                } elseif ($attachment_type == 'academic') {
+                    $academic_array = [];
+                    for ($x = 0; $x <= 3; $x++) {
+                        $academic_array[$x]['academic_' . ($x + 1)] = "";
+                    }
+                    return response()->json(['status' => 0, 'data' => $academic_array]);
+                } elseif ($attachment_type == 'cheque') {
+                    $cheque_array = [];
+                    for ($x = 0; $x <= 3; $x++) {
+                        $cheque_array[$x]['cheque_' . ($x + 1)] = "";
+                    }
+                    return response()->json(['status' => 0, 'data' => $cheque_array]);
+                } elseif ($attachment_type == 'affidavit') {
+                    $affidavit_array = [];
+                    for ($x = 0; $x <= 3; $x++) {
+                        $affidavit_array[$x]['affidavit_' . ($x + 1)] = "";
+                    }
+                    return response()->json(['status' => 0, 'data' => $affidavit_array]);
+                } elseif ($attachment_type == 'utility_bill') {
+                    $utility_bill_array = [];
+                    for ($x = 0; $x <= 3; $x++) {
+                        $utility_bill_array[$x]['utility_bill_' . ($x + 1)] = "";
+                    }
+                    return response()->json(['status' => 0, 'data' => $utility_bill_array]);
+                } elseif ($attachment_type == 'cnic_nominee') {
+                    $cnic_nominee_array = [];
+                    for ($x = 0; $x <= 3; $x++) {
+                        $cnic_nominee_array[$x]['cnic_nominee_' . ($x + 1)] = "";
+                    }
+                    return response()->json(['status' => 0, 'data' => $cnic_nominee_array]);
+                } elseif ($attachment_type == 'child_b_form') {
+                    $child_b_form_array = [];
+                    for ($x = 0; $x <= 3; $x++) {
+                        $child_b_form_array[$x]['child_b_form_' . ($x + 1)] = "";
+                    }
+                    return response()->json(['status' => 0, 'data' => $child_b_form_array]);
+                } elseif ($attachment_type == 'cnic_spouse') {
+                    $cnic_spouse_array = [];
+                    for ($x = 0; $x <= 3; $x++) {
+                        $cnic_spouse_array[$x]['cnic_spouse_' . ($x + 1)] = "";
+                    }
+                    return response()->json(['status' => 0, 'data' => $cnic_spouse_array]);
+                } elseif ($attachment_type == 'last_pay_slip') {
+                    $last_pay_slip_array = [];
+                    for ($x = 0; $x <= 3; $x++) {
+                        $last_pay_slip_array[$x]['last_pay_slip_' . ($x + 1)] = "";
+                    }
+                    return response()->json(['status' => 0, 'data' => $last_pay_slip_array]);
+                } elseif ($attachment_type == 'nikkah_nama') {
+                    $nikkah_nama_array = [];
+                    for ($x = 0; $x <= 3; $x++) {
+                        $nikkah_nama_array[$x]['nikkah_nama_' . ($x + 1)] = "";
+                    }
+                    return response()->json(['status' => 0, 'data' => $nikkah_nama_array]);
+                } elseif ($attachment_type == 'experience_certificate') {
+                    $experience_certificate_array = [];
+                    for ($x = 0; $x <= 3; $x++) {
+                        $experience_certificate_array[$x]['experience_certificate_' . ($x + 1)] = "";
+                    }
+                    return response()->json(['status' => 0, 'data' => $experience_certificate_array]);
+                }
             }
 
         }
