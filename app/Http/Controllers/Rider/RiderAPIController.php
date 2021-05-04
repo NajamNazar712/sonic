ece279716ea716cc370a1221d175eef8b831a9e3
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-05-05 02:49:09 +0500

    changes

--- app/Http/Controllers/Rider/RiderAPIController.php
+++ app/Http/Controllers/Rider/RiderAPIController.php
@@ -7168,7 +7168,8 @@ class RiderAPIController extends Controller
 
     public function shipment_delivered_v2(Request $request)
     {
-        return response()->json([$request->trybuy_id_list]);
+        $res = str_replace( array( '[', ']'), '', $request->trybuy_id_list);
+        return response()->json([$res]);
         $message = '';
 
         $rules = [
