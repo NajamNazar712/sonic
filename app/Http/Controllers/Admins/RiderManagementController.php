2476834d495c75119c08661eeb19e226edee8bd9
abimwaqas <muhammad.waqas@trax.pk>
2021-05-06 13:07:24 +0500

    fix

--- app/Http/Controllers/Admins/RiderManagementController.php
+++ app/Http/Controllers/Admins/RiderManagementController.php
@@ -803,10 +803,10 @@ class RiderManagementController extends Controller
                         $shipment_weight_type_1_count = Shipment::whereIn('id', $cod_shipments)->whereBetween('actual_weight' , [0.11,1.50])->count();
 //                        $test = [$cod_shipments, $non_cod_shipments, $verified_shipments];
 
-                        if($rider->id == 297){
-                            dd($shipment_weight_type_1_count);
-                        }
                         $shipment_weight_type_2_count = Shipment::whereIn('id', $cod_shipments)->where('actual_weight' , '=>', 1.51)->count();
+                        if($rider->id == 297){
+                            dd($shipment_weight_type_2_count);
+                        }
                         $shipment_weight_type_3_count = Shipment::whereIn('id', $cod_shipments)->where('actual_weight' , '=<', 0.10)->count();
                         $test = [$shipment_weight_type_1_count, $shipment_weight_type_2_count, $shipment_weight_type_2_count];
                         if($shipment_weight_type_1_count > 0){
