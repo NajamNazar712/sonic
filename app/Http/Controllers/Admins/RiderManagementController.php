6d135a7e46bcf6494d241a2148a2e496b64b9f5e
abimwaqas <muhammad.waqas@trax.pk>
2021-05-06 12:56:33 +0500

    fix

--- app/Http/Controllers/Admins/RiderManagementController.php
+++ app/Http/Controllers/Admins/RiderManagementController.php
@@ -796,15 +796,16 @@ class RiderManagementController extends Controller
                     $cod_shipments = Shipment::whereIn('id', $pickup_shipment_ids)->where('amount', '>', 0)->whereNotIn('user_id', [3324,10104])->pluck('id')->toArray();
                     $non_cod_shipments = Shipment::whereIn('id', $pickup_shipment_ids)->where('amount', '=', 0)->whereNotIn('user_id', [3324,10104])->pluck('id')->toArray();
                     $verified_shipments = Shipment::whereIn('id', $pickup_shipment_ids)->whereIn('user_id', [3324,10104])->pluck('id')->toArray();
-                    $test = [$cod_shipments, $non_cod_shipments, $verified_shipments];
 
-                    if($rider->id == 297){
-                        dd($test);
-                    }
                     if(count($cod_shipments) > 0){
 
-                        $shipment_weight_type_1_count = Shipment::whereIn('id', $cod_shipments)->where('actual_weight' , '>', 0.10)->where('actual_weight' , '=<', 1.50)->count();
+//                        $shipment_weight_type_1_count = Shipment::whereIn('id', $cod_shipments)->where('actual_weight' , '>', 0.10)->where('actual_weight' , '=<', 1.50)->count();
+                        $shipment_weight_type_1_count = Shipment::whereIn('id', $cod_shipments)->count();
+//                        $test = [$cod_shipments, $non_cod_shipments, $verified_shipments];
 
+                        if($rider->id == 297){
+                            dd($shipment_weight_type_1_count);
+                        }
                         $shipment_weight_type_2_count = Shipment::whereIn('id', $cod_shipments)->where('actual_weight' , '=>', 1.51)->count();
                         $shipment_weight_type_3_count = Shipment::whereIn('id', $cod_shipments)->where('actual_weight' , '=<', 0.10)->count();
                         $test = [$shipment_weight_type_1_count, $shipment_weight_type_2_count, $shipment_weight_type_2_count];
