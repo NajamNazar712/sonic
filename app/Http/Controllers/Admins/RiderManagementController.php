a257aa4b30e794f883c9d5bf6accc22aa539e091
abimwaqas <muhammad.waqas@trax.pk>
2021-05-06 12:10:31 +0500

    fix

--- app/Http/Controllers/Admins/RiderManagementController.php
+++ app/Http/Controllers/Admins/RiderManagementController.php
@@ -775,8 +775,8 @@ class RiderManagementController extends Controller
         $date_to = Carbon::createFromFormat("Y-m-d H:i:s",$next_day)->format('Y-m-d 05:59A');
         $riders = Rider::where('status', 1)->select('id', 'rider_category_id')->get();
         if(count($riders) > 0){
+                dd($riders);
             foreach ($riders as $rider){
-//                dd($date_to);
                 $rider_category_id = $rider->rider_category_id;
                 $pickup_shipment_ids = array();
                 $delivery_shipment_ids = array();
