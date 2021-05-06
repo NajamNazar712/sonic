8d3befc8268a9f95f2f7587870f51e12695b6353
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-05-06 14:11:02 +0500

    changes

--- app/Http/Controllers/Rider/RiderAPIController.php
+++ app/Http/Controllers/Rider/RiderAPIController.php
@@ -5094,8 +5094,7 @@ class RiderAPIController extends Controller
             $rider_request = RiderRequest::where('phone_no', $request->input('phone_number'))
                 ->orWhere('cnic', $request->input('cnic_no'));
 
-            $employee = Employee::where('employee_type_id', 2)
-                ->where('phone_number', $request->input('phone_number'))
+            $employee = Employee::where('phone_number', $request->input('phone_number'))
                 ->orWhere('cnic', $request->input('cnic_no'));
 
             $rider = Rider::where('phone', $request->input('phone_number'))
