a8c119d978ee3a3d169d6e5d950f11471a88ed4e
abimwaqas <muhammad.waqas@trax.pk>
2021-05-06 13:45:17 +0500

    fix

--- app/Http/Controllers/Admins/RiderManagementController.php
+++ app/Http/Controllers/Admins/RiderManagementController.php
@@ -874,10 +874,10 @@ class RiderManagementController extends Controller
                 if($delivery_notes->exists()){
                     $delivery_note_ids = array();
                     $delivery_note_ids = $delivery_notes->pluck('id')->toArray();
-                    dd($delivery_note_ids);
                     $delivery_note_ids_count = count($delivery_note_ids);
                     if($delivery_note_ids_count > 0){
                         $delivery_shipment_ids = ShipmentsJourney::whereIn('reference_1_id', $delivery_note_ids)->whereIn('shipper_status_id', [14, 30, 36,37])->where('verification', 1)->pluck('shipment_id')->toArray();
+                        dd($delivery_shipment_ids);
                         array_unique($delivery_shipment_ids);
 
                         $delivered_shipment_count = count($delivery_shipment_ids);
