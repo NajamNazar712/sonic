6867dd7f0c84a007eb3f9b00678f1bfac411dc3c
abimwaqas <muhammad.waqas@trax.pk>
2021-05-06 11:49:42 +0500

    fix

--- app/Console/Commands/RiderIncentiveCalculate.php
+++ app/Console/Commands/RiderIncentiveCalculate.php
@@ -39,7 +39,8 @@ class RiderIncentiveCalculate extends Command
      */
     public function handle()
     {
-        $date = Carbon::yesterday()->format('Y-m-d');
+        $date = Carbon::today()->subDays(3)->format('Y-m-d');
+        dd($date);
         RiderManagementController::riders_incentives_calculation($date. ' 00:00:00');
     }
 }
