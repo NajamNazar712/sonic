eba0b15e9cb1ca9acfb81876118c3c12b33209c6
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2021-05-10 23:25:54 +0500

    Done
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- routes/web.php
+++ routes/web.php
@@ -395,6 +395,11 @@ Route::prefix('cod')->name('cod.')->group(function () {
             Route::post('', 'Shippers\ShipperTelenorController@data_conversion_store')->name('store');
         });
     });
+
+    Route::prefix('quick_search')->name('quick_search.')->group(function(){
+        Route::get('','Shippers\ShipperDashboardController@quick_search_index')->name('index');
+        Route::get('list','Shippers\ShipperDashboardController@quick_search_list')->name('list');
+    });
 });
 //Admin Routes Start
 Route::prefix('admin')->name('admin.')->group(function () {
