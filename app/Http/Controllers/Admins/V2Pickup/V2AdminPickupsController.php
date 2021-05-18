04d8aa3e8701ee4ffacc9a24359f70c56c97a40e
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-05-18 10:54:04 +0500

    Changes

--- app/Http/Controllers/Admins/V2Pickup/V2AdminPickupsController.php
+++ app/Http/Controllers/Admins/V2Pickup/V2AdminPickupsController.php
@@ -401,8 +401,8 @@ class V2AdminPickupsController extends Controller
                     if ($rider_device_token->exists()) {
                         $rider_device_token = $rider_device_token->first();
                         $device_token = $rider_device_token->device_token;
-                        $title = "Pickup Request Removed";
-                        $message = "Dear Rider Pickup Request : " . $pickup_request->id . " Removed From Your Pickups";
+                        $title = "Pickup Request Reassigned";
+                        $message = "Dear Rider Pickup of : " . $pickup_request->shipper->name . " has been reassigned to";
                         NotificationsController::bolt_app_notification($previous_rider_id, 2,$device_token, $title, $message);
                     }
                 }
@@ -415,7 +415,7 @@ class V2AdminPickupsController extends Controller
                         $rider_device_token = $rider_device_token->first();
                         $device_token = $rider_device_token->device_token;
                         $title = "Pickup Request Assigned";
-                        $message = "Dear Rider Pickup Request : " . $pickup_request->id . " Assigned To You";
+                        $message = "Dear Rider Pickup Request : " . $pickup_request->shipper->name . " Assigned To You";
                         NotificationsController::bolt_app_notification($rider_id, 2,$device_token, $title, $message);
                     }
                 }
