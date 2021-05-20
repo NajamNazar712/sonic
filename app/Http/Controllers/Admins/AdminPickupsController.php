6ecd04f3dac1e8313f51eef62c2bf59198a2db9d
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-05-20 19:34:25 +0500

    Changes

--- app/Http/Controllers/Admins/AdminPickupsController.php
+++ app/Http/Controllers/Admins/AdminPickupsController.php
@@ -67,6 +67,7 @@ class AdminPickupsController extends Controller
 
     static public function generate($shipment_id) {
               $shipment = Shipment::find($shipment_id);
+              dd($shipment);
               if(in_array($shipment->shipper_status_id, [1, 17])){
 
                   if($shipment->shipper_status_id == 17){
