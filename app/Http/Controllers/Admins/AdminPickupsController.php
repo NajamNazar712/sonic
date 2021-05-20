6019ff7c4657988c07149986e6dc0fc6d136bdfe
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-05-20 11:32:12 +0500

    Changes

--- app/Http/Controllers/Admins/AdminPickupsController.php
+++ app/Http/Controllers/Admins/AdminPickupsController.php
@@ -207,7 +207,7 @@ class AdminPickupsController extends Controller
                         $retail_pickup_note_create = new RetailPickupNote();
                         $retail_pickup_note_create->pickup_address_id = $shipment->pickup_address_id;
                         $retail_pickup_note_create->hub_id = $shipment->pickup_address->city->hub_id;
-                        $retail_pickup_note_create->retail_user_id = $retail_shipment->retail_user_id;
+                        $retail_pickup_note_create->retail_user_id = ($retail_shipment->retail_user_id) ? $retail_shipment->retail_user_id : $retail_shipment->admin_id;
                         $retail_pickup_note_create->pickup_request_id = $pickup_request_id;
                         $retail_pickup_note_create->shipments = 1;
                         $retail_pickup_note_create->amount = $retail_shipment->total_charges;
