db70e90247d2864cecdee55b77d8c01c967836ea
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-05-21 16:05:02 +0500

    Changes

--- routes/api.php
+++ routes/api.php
@@ -214,6 +214,21 @@ Route::name('api.')->group(function () {
 
     });
 
+    Route::prefix('retail_user')->name('retail_user.')->group(function() {
+        Route::post('login_v2', 'Retail\RetailAPIController@login')->name('login_v2');
+        Route::get('slider', 'Rider\RiderAPIController@rider_ticker_images')->name('slider');
+
+        Route::middleware('RetailUserAPIToken')->group(function () {
+            Route::prefix('retail')->name('retail.')->group(function () {
+                Route::get('retail_data', 'Retail\RetailAPIController@retail_index')->name('retail_data');
+                Route::post('retail_bank_info', 'Retail\RetailAPIController@retail_bank_info')->name('retail_bank_info');
+                Route::post('retail_shipment_store', 'Retail\RetailAPIController@retail_shipment_store')->name('retail_shipment_store');
+            });
+
+        });
+
+    });
+
 
 
 });
