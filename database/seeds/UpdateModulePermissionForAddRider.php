1477edf2c2a89dd571ee3c7f0856f09d76efe728
Nabeel973 <nabeelsiddiqui324@gmail.com>
2021-05-24 13:24:52 +0500

    change

--- database/seeds/UpdateModulePermissionForAddRider.php
+++ database/seeds/UpdateModulePermissionForAddRider.php
@@ -12,7 +12,7 @@ class UpdateModulePermissionForAddRider extends Seeder
     public function run()
     {
         DB::table('module_permissions')->insert(array(
-            array('id' => 500, 'name' => 'Incentive Screen Add-Rider', 'module_id' => 12),
+            array('id' => 500, 'name' => 'Incentive Screen Add-Rider', 'module_id' => 11),
         ));
         DB::table('module_permissions')->where('id',93)->update(['name' => 'Permanent Screen Add-Rider']);
         
