e31ce236d26a51936be2fc58aa1d5d5b8da74083
Nabeel973 <nabeelsiddiqui324@gmail.com>
2021-05-24 11:21:24 +0500

    permission added

---
+++ database/seeds/UpdateModulePermissionForAddRider.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UpdateModulePermissionForAddRider extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('module_permissions')->insert(array(
+            array('id' => 500, 'name' => 'Incentive Screen Add-Rider', 'module_id' => 12),
+        ));
+    }
+}
