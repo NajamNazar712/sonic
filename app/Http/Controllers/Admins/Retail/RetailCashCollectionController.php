8827d9561cb3c3e8889bb742192bdc20a7362884
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-05-25 11:08:06 +0500

    ReailCashCollection Changes

--- app/Http/Controllers/Admins/Retail/RetailCashCollectionController.php
+++ app/Http/Controllers/Admins/Retail/RetailCashCollectionController.php
@@ -36,7 +36,7 @@ class RetailCashCollectionController extends Controller
         $deliveries = RetailPickupNote::join('cities AS oc', 'retail_pickup_notes.hub_id', '=', 'oc.id')
             ->leftjoin('riders as r', 'retail_pickup_notes.rider_id', '=', 'r.id')
             ->leftjoin('admins as a', 'a.id', '=', 'retail_pickup_notes.assigned_by')
-            ->join('retail_users as ru', 'ru.id', '=', 'retail_pickup_notes.retail_user_id')
+            ->leftjoin('retail_users as ru', 'ru.id', '=', 'retail_pickup_notes.retail_user_id')
             ->leftjoin('retail_franchises as rf', 'rf.id', '=', 'ru.category_id')
             ->leftjoin('retail_trax_centers as rc', 'rc.id', '=', 'ru.category_id')
             ->select(['retail_pickup_notes.id', 'retail_pickup_notes.id as retail_pickup_note_id', 'oc.id as hub_id', 'oc.name as hub','r.name as rider','a.name as assignee',  'retail_pickup_notes.assigned_at', 'retail_pickup_notes.shipments as shipments_count', 'retail_pickup_notes.amount as amount','rf.name as franchise','rf.code as franchise_code','rc.name as center','rc.code as center_code','ru.category', 'retail_pickup_notes.status'])
