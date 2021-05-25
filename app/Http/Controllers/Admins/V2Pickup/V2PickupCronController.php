a2fe564a3884ca6950cec6cb0c068512830349cb
abimwaqas <muhammad.waqas@trax.pk>
2021-05-25 22:16:01 +0500

    fix

--- app/Http/Controllers/Admins/V2Pickup/V2PickupCronController.php
+++ app/Http/Controllers/Admins/V2Pickup/V2PickupCronController.php
@@ -38,7 +38,7 @@ class V2PickupCronController extends Controller
 
         $now = Carbon::now();
         V2PickupNote::where('status', 0)->update(['status' => 1]);
-        V2PickupRequest::whereIn('status_id', [1,3])->where('rider_status', 2)->whereBetween('updated_at', [$yesterday,$today])->update(['last_rider_id' => DB::raw('current_rider_id'), 'current_rider_id' => NULL, 'rider_status' => 1]);
+        V2PickupRequest::whereIn('status_id', [1,3])->where('rider_status', 2)->whereDate('created_at', '<', $today)->update(['last_rider_id' => DB::raw('current_rider_id'), 'current_rider_id' => NULL, 'rider_status' => 1]);
 
         $pickup_requests = V2PickupRequest::whereIn('status_id', [1,3])->whereBetween('created_at', [$yesterday,$today]);
 
@@ -118,7 +118,7 @@ class V2PickupCronController extends Controller
     }
 
     static public function remove_riders($today){
-        V2PickupRequest::whereIn('status_id', [1,3])->where('rider_status', 2)->where('updated_at', '<', $today)->update(['last_rider_id' => DB::raw('current_rider_id'), 'current_rider_id' => NULL, 'rider_status' => 1]);
+        V2PickupRequest::whereIn('status_id', [1,3])->where('rider_status', 2)->whereDate('created_at', '<', $today)->update(['last_rider_id' => DB::raw('current_rider_id'), 'current_rider_id' => NULL, 'rider_status' => 1]);
     }
     static public function cancel_if_not_valid(){
         $pickup_requests = V2PickupRequest::where('status_id', 1);
