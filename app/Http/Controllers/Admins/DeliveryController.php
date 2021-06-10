149cbed77f010d2e7c58a3aab30cb8fb8251af4b
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-06-10 18:47:23 +0500

    CHnages

--- app/Http/Controllers/Admins/DeliveryController.php
+++ app/Http/Controllers/Admins/DeliveryController.php
@@ -780,8 +780,11 @@ class DeliveryController extends Controller
                 foreach ($valid_shipments as $index => $shipment) {
                     NotificationsController::send(10, $note->id, $shipment);
                     NotificationsController::send(11, $note->id, $shipment);
-
                     if($notifications[$index]) {
+                        NotificationsController::send(12, $note->id, $shipment);
+                    }
+
+                    /*if($notifications[$index]) {
                         if($shipment_details->amount == 0){
                             //English
                             NotificationsController::send(132, $note->id, $shipment);
@@ -790,7 +793,7 @@ class DeliveryController extends Controller
                         }else{
                             NotificationsController::send(12, $note->id, $shipment);
                         }
-                    }
+                    }*/
                 }
                 NotificationsController::send(40, $note->id);
             }
