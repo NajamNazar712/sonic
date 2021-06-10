02bf4803e54fa0a95fc2517b722c49767afb7e9b
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-06-10 18:46:26 +0500

    CHnages

--- app/Http/Controllers/Admins/DeliveryController.php
+++ app/Http/Controllers/Admins/DeliveryController.php
@@ -782,7 +782,7 @@ class DeliveryController extends Controller
                     NotificationsController::send(11, $note->id, $shipment);
 
                     if($notifications[$index]) {
-                        if($shipment->amount == 0){
+                        if($shipment_details->amount == 0){
                             //English
                             NotificationsController::send(132, $note->id, $shipment);
                             //Urdu
