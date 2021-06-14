620f9c55a6f5378eca8f53baab1deb725cde52aa
Maaz Khan <Maaz.Khan@Trax.pk>
2021-06-14 13:31:54 +0500

    in progress

--- database/seeds/UpdateModuleTableBookShipments.php
+++ database/seeds/UpdateModuleTableBookShipments.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class UpdateModuleTableBookShipments extends Seeder
+    class UpdateModuleTableBookShipments extends Seeder
 {
     /**
      * Run the database seeds.
