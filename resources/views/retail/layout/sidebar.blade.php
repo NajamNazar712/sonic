d740d38bda364d37eebcf3b48545ee1989b1729b
abimwaqas <muhammad.waqas@trax.pk>
2021-06-14 13:24:20 +0500

    fix

--- resources/views/retail/layout/sidebar.blade.php
+++ resources/views/retail/layout/sidebar.blade.php
@@ -6,7 +6,7 @@
 {{--            </li>--}}
             <li><a class="menu-item" href="{{route('retail.shipment.book.index')}}"><i class="la la-cart-plus"></i>Booking</a></li>
             <li><a class="menu-item" href="{{route('retail.parcel_receiving.index')}}"><i class="la la-dropbox"></i>Parcel Receiving</a></li>
-            <li class="menu-item"><a href="{{ route('retail.cancelled_shipments.add.index') }}"><i class="la la-trash"></i>Cancelled</a>
+            <li class="menu-item"><a href="{{ route('retail.cancelled_shipments.index') }}"><i class="la la-trash"></i>Cancelled</a>
             </li>
             <li><a class="menu-item" href="{{route('retail.cash_deposit.index')}}"><i class="la la-money"></i>Cash Deposit</a></li>
             <li><a class="menu-item" href="{{route('retail.shipment.tracking_slip.index')}}"><i class="la la-image"></i>Tracking Slip</a></li>
