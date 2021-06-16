c89cdb4ae9a792be8fdf52f60cf513546b659bcd
Anas Anwer <anas.anwer@trax.pk>
2021-06-16 17:49:00 +0500

    changes

--- app/Http/Controllers/Retail/RetailShipmentBookController.php
+++ app/Http/Controllers/Retail/RetailShipmentBookController.php
@@ -1512,7 +1512,7 @@ class RetailShipmentBookController extends Controller
     public function excel_index() {
         $products = Product::all();
         $business_categories = BusinessCategory::where('id', 1)->get();
-        $shipping_modes = RetailShippingMode::all();
+        $shipping_modes = RetailShippingMode::where('id', '!=', 3)->get();
         $domestic_cities = City::where('business_category_id', 1)->where('status', 1)->get();
         $international_cities = City::where('business_category_id', 2)->where('status', 1)->get();
         $domestic_overland_cities = CityDelivery::join('cities as c', 'c.id', '=', 'city_deliveries.city_id')->where('city_deliveries.booking_type_id', 1)->where('city_deliveries.shipping_mode_id', 2)->where('c.business_category_id', 1)->where('c.status', 1)->select('c.id', 'c.name')->get();
