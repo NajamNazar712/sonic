f5368275c23c521616270c0460d05d414b404ec0
Muhammad Aqib <muhammad.aqib@trax.pk>
2021-06-17 10:02:48 +0500

    fix

--- app/Http/Models/Admin/FtlRequest.php
+++ app/Http/Models/Admin/FtlRequest.php
@@ -10,4 +10,16 @@ class FtlRequest extends Model
     {
         return $this->hasMany(FtlRequestAdditionalCost::class);
     }
+
+    public function origin() {
+        return $this->belongsTo('App\Http\Models\City', 'origin_id', 'id');
+    }
+
+    public function destination() {
+        return $this->belongsTo('App\Http\Models\City', 'destination_id', 'id');
+    }
+
+    public function shipment() {
+        return $this->belongsTo('App\Http\Models\Shipment', 'shipment_id', 'id');
+    }
 }
--- app/Http/Models/Admin/WalkinFtlInvoice.php
+++ app/Http/Models/Admin/WalkinFtlInvoice.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class WalkinFtlInvoice extends Model
 {
-    //
+    public function ftl_request(){
+        return $this->belongsTo('App\Http\Models\Admin\FtlRequest', 'ftl_request_id', 'id');
+
+    }
+
+   
 }
