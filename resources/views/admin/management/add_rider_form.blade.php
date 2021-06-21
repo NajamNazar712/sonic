1e758c56dff5d5218d8867140cc92f82d4cb7416
Anas Anwer <anas.anwer@trax.pk>
2021-06-21 13:15:56 +0500

    working

--- resources/views/admin/management/add_rider_form.blade.php
+++ resources/views/admin/management/add_rider_form.blade.php
@@ -7,12 +7,20 @@
 <form action="{{route('admin.management.riders.add')}}" method="post" class="mt-1" id="addRiderForm" novalidate="novalidate">
     {{csrf_field()}}
     <div class="row justify-content-center">
-        <div class="col-3">
+        <div class="col text-center">
             <label class="font-medium-2 font-weight-bold block">Special Rider</label>
             <div class="form-group">
                 <label for="all_shippers_checkbox" class="font-medium-2 text-bold-600 mr-1">No</label>
                 <input type="checkbox" name="special_rider_checkbox" id="special_rider_checkbox" class="switchery special_rider_checkbox" data-size="sm" data-switchery="true">
                 <label for="special_rider_checkbox" class="font-medium-2 text-bold-600 ml-1">Yes</label>
+            </div>
+        </div>
+        <div class="col text-center">
+            <label class="font-medium-2 font-weight-bold block">Credit Card on Delivery-CCD</label>
+            <div class="form-group">
+                <label for="ccd_rider_checkbox" class="font-medium-2 text-bold-600 mr-1">No</label>
+                <input type="checkbox" name="ccd_rider_checkbox" id="ccd_rider_checkbox" class="switchery ccd_rider_checkbox" data-size="sm" data-switchery="true">
+                <label for="ccd_rider_checkbox" class="font-medium-2 text-bold-600 ml-1">Yes</label>
             </div>
         </div>
     </div>
@@ -146,6 +154,9 @@
         console.log('working')
 
         var elem = document.querySelector('.special_rider_checkbox');
+        var switchery = new Switchery(elem);
+
+        var elem = document.querySelector('.ccd_rider_checkbox');
         var switchery = new Switchery(elem);
 
         $('#city_list').prepend('<option value="" selected="selected"></option>').select2({
