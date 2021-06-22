189397bb6d1df491f1fcf2c43530b0cde4cbdf51
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-06-22 15:39:15 +0500

    Otp Work done

--- app/Http/Controllers/Admins/DeliveryController.php
+++ app/Http/Controllers/Admins/DeliveryController.php
@@ -730,6 +730,9 @@ class DeliveryController extends Controller
                 }
             }
             if ($note) {
+                $otp_pin = rand(1000, 9999);
+                $note->otp = $otp_pin;
+                $note->save();
                 if (!$order) {  //Default
                     sort($valid_shipments); //sort_valid_shipments;
                 }
@@ -812,6 +815,7 @@ class DeliveryController extends Controller
                         }
                     }
                 }
+                NotificationsController::send(137,$rider->id, $note->id);
                 NotificationsController::send(40, $note->id);
             }
 
@@ -6248,6 +6252,12 @@ class DeliveryController extends Controller
                 if($delivery_note->hub_id == $rider->hub_id){
                     $delivery_note->rider_id = $rider_id;
                     $delivery_note->save();
+                    if($delivery_note->otp == null){
+                        $otp_pin = rand(1000, 9999);
+                        $delivery_note->otp = $otp_pin;
+                        $delivery_note->save();
+                    }
+                    NotificationsController::send(137,$rider_id, $delivery_note_id);
                     return response()->json(['status' => 0, 'success' => 'Rider updated successfully']);
                 }
                 else{
