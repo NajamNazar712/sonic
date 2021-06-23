020d6b3e15f7b016caa86ce324321b536f4cb8ea
Nabeel973 <nabeelsiddiqui324@gmail.com>
2021-06-23 15:42:03 +0500

    invoice

--- routes/web.php
+++ routes/web.php
@@ -243,8 +243,8 @@ Route::prefix('cod')->name('cod.')->group(function () {
             Route::get('list', 'Shippers\ShipperFinanceController@invoice_list')->name('list');
             Route::post('print', 'Shippers\ShipperFinanceController@invoices_print')->name('print');
             Route::get('export_to_excel', 'Shippers\ShipperFinanceController@invoices_export_to_excel')->name('export_to_excel');
-            Route::put('email_reminder', 'Admins\AdminFinanceController@invoices_email_reminder')->name('email_reminder');
-            Route::post('print_origin_wise', 'Admins\AdminFinanceController@invoices_print_origin_wise')->name('print_origin_wise');
+            Route::put('email_reminder', 'Shippers\ShipperFinanceController@invoices_email_reminder')->name('email_reminder');
+            Route::post('print_origin_wise', 'Shippers\ShipperFinanceController@invoices_print_origin_wise')->name('print_origin_wise');
 
 
         });
