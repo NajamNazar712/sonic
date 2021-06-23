45b6337e070611effba2cb9ed5168a10b7fc3891
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-06-23 15:00:33 +0500

    Temp changes

--- app/Http/Controllers/Admins/DeliveryController.php
+++ app/Http/Controllers/Admins/DeliveryController.php
@@ -295,8 +295,11 @@ class DeliveryController extends Controller
 
     public function check_rider_dncc_status(Request $request)
     {
-         $datetime = Carbon::createFromFormat('Y-m-d H:i:s', '2021-05-18 23:59:00');
-         $delivery_note =  DeliveryNote::where([['rider_id',$request->rider_id],['dncc_status',0]])
+        $datetime = Carbon::createFromFormat('Y-m-d H:i:s', '2021-05-18 23:59:00');
+        $rider = Rider::find($request->rider_id);
+        $ccd_rider = $rider->ccd;
+        return response()->json(['status'=> 1, 'ccd_rider' => $ccd_rider]);
+         /*$delivery_note =  DeliveryNote::where([['rider_id',$request->rider_id],['dncc_status',0]])
              ->whereDate('created_at','>',$datetime);
          if($delivery_note->exists())
          {
@@ -306,7 +309,7 @@ class DeliveryController extends Controller
             $rider = Rider::find($request->rider_id);
             $ccd_rider = $rider->ccd;
             return response()->json(['status'=> 1, 'ccd_rider' => $ccd_rider]);
-         }
+         }*/
     }
 
 	public function note_consolidation_check(Request $request){
