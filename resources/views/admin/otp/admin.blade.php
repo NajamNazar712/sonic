c10481f02f50fc41c329856dda33107c3e379608
Anas Anwer <anas.anwer@trax.pk>
2021-06-25 22:11:38 +0500

    changes

--- resources/views/admin/otp/admin.blade.php
+++ resources/views/admin/otp/admin.blade.php
@@ -154,6 +154,7 @@
             scrollX: true, scrollY: '500px',
             lengthMenu: [[50, 100, 500, 1000, -1], [50, 100, 500, 1000, 'All']],
             pageLength: 50,
+            autoWidth: false,
             pagingType: 'full_numbers',
             processing: true,
             language: {
