ef3191e6f9dbc2b5beca8a0243ef83a1f025804e
Muhammad Aqib <muhammad.aqib@trax.pk>
2021-06-25 11:13:43 +0500

    changes

--- resources/views/admin/reports/crm_report.blade.php
+++ resources/views/admin/reports/crm_report.blade.php
@@ -408,9 +408,16 @@
                 if ( this.context.length ) {
                     body = [];
                     var params = table.ajax.params();
+                    if(params !== undefined){
                     params.start = 0;
                     params.length = -1;
                     params.excel = true;
+                }
+                else{
+                    params = {
+                        'excel':true,
+                    }
+                }
                     var jsonResult = $.ajax({
                         url: '{{ route('admin.reports.crm.list') }}',
                         method: 'POST',
