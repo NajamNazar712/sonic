d829a3ba3c56a0d8fc4e7378b42e3265c5eeeaa7
Anas Anwer <anas.anwer@trax.pk>
2021-06-26 06:02:27 +0500

    changes

--- routes/web.php
+++ routes/web.php
@@ -3030,8 +3030,8 @@ Route::prefix('admin')->name('admin.')->group(function () {
 
     Route::prefix('otp')->name('otp.')->group(function () {
         Route::prefix('admin')->name('admin.')->group(function () {
-            Route::get('', 'Admins\AdminOTPController@admin_otp_index')->name('index');
-            Route::get('list', 'Admins\AdminOTPController@admin_otp_list')->name('list');
+            Route::get('', 'Admins\UserManagementController@admin_otp_index')->name('index');
+            Route::get('list', 'Admins\UserManagementController@admin_otp_list')->name('list');
         });
     });
 
