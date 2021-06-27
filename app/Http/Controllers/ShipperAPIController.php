731c262e38ebbdcaf357816e6b16000a533b85da
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-06-27 18:39:13 +0500

    Changes

--- app/Http/Controllers/ShipperAPIController.php
+++ app/Http/Controllers/ShipperAPIController.php
@@ -7,9 +7,63 @@ use App\Http\Models\ShipmentsJourney;
 use App\Http\Models\Shipper\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Validator;
 
 class ShipperAPIController extends Controller
 {
+    private $names = [
+        'email_address' => 'Email Address',
+        'password' => 'Password',
+
+        'rider_location_latitude' => 'Rider Location Latitude',
+        'rider_location_longtidue' => 'Rider Location Longitude',
+
+        'added_at' => 'Added At',
+        'pickup_note_id' => 'Pickup Note ID',
+        'pickup_request_id' => 'Pickup Request ID',
+        'actual_location_latitude' => 'Location Latitude',
+        'actual_location_longtidue' => 'Location Longitude',
+        'start_location_latitude' => 'Location Latitude',
+        'start_location_longitude' => 'Location Longitude',
+
+        'shipments' => 'Shipments',
+        'tracking_no' => 'Tracking Number',
+
+        'reason_id' => 'Reason ID',
+        'picture' => 'Picture',
+
+        'delivery_note_id' => 'Delivery Note ID',
+        'receiver_name' => 'Receiver Name',
+        'cnic' => 'CNIC',
+
+        'shipper_status_id' => 'Shipper Status ID',
+        'status_reason_id' => 'Status Reason ID',
+        'remarks' => 'Remarks',
+
+        'actions' => 'Actions',
+        'actions.*' => 'Action',
+        'actions.*.logged_at' => 'Logged At',
+        'actions.*.type_id' => 'Type ID',
+        'actions.*.pickup_note_id' => 'Pickup Note ID',
+        'actions.*.pickup_request_id' => 'Pickup Request ID',
+
+        'from_date' => 'From Date'
+    ];
+
+    private $messages = [
+        'phone_number.regex' => ':attribute format is Invalid, required Format is: 0300-0000000.',
+        'integer' => ':attribute must be an Integer.',
+        'digits' => ':attribute must be of :digits Digits.',
+        'exists' => 'Given :attribute is of Invalid ID.',
+        'actual_location_latitude.regex' => ':attribute is Invalid Latitude Coordinates.',
+        'actual_location_longitude.regex' => ':attribute is Invalid Longitude Coordinates.',
+        'start_location_latitude.regex' => ':attribute is Invalid Latitude Coordinates.',
+        'start_location_longitude.regex' => ':attribute is Invalid Longitude Coordinates.',
+        'image' => ':attribute must be an Image.',
+        'rider_location_latitude.regex' => ':attribute is Invalid Latitude Coordinates.',
+        'rider_location_longitude.regex' => ':attribute is Invalid Longitude Coordinates.',
+    ];
+
     public function login(Request $request)
     {
         $rules = [
