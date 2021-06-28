42020c12d50d3572660f98bf9a221bf7d0c08346
Muhammad Aqib <muhammad.aqib@trax.pk>
2021-06-28 13:32:03 +0500

    changes

--- resources/views/admin/settings/CRM/escalation/launched/index.blade.php
+++ resources/views/admin/settings/CRM/escalation/launched/index.blade.php
@@ -99,7 +99,7 @@
                 serverSide: true,
                 ajax: '{{ route('admin.settings.escalation.launched.list') }}',
                 rowId: 'id',
-                order: [[8, 'asc']],
+                order: [[8, 'desc']],
                 columns: [
                     {data: 'serial_number', orderable: false, searchable: false, name: 'serial_number', class: 'align-middle serial_number', targets: 1, render: function (data, type, row) {return '';}},
                     {data: 'case_nature', name: 'crcn.name', class: 'align-middle case_nature'},
