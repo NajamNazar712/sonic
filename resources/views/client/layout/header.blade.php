ab21771d75db4da07e76d8f490d66d32b9b745ca
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2021-06-28 14:14:17 +0500

    Trax Logistics to Trax
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- resources/views/client/layout/header.blade.php
+++ resources/views/client/layout/header.blade.php
@@ -1,8 +1,8 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-<meta name="description" content="Trax Shippers Dashboard">
-<meta name="keywords" content="Trax logistics,trax,trax logistics,delivery">
+<meta name="description" content="Trax, Sonic">
+<meta name="keywords" content="Trax">
 <meta name="author" content="Trax IT">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <title>@yield('title') - Sonic | Trax</title>
