7c0d6dcbe6225829fc30fbeae4ca297ca1cc3855
abimwaqas <muhammad.waqas@trax.pk>
2021-07-01 18:22:03 +0500

    fix

--- resources/views/admin/delivery/receive/update.blade.php
+++ resources/views/admin/delivery/receive/update.blade.php
@@ -349,7 +349,7 @@
                 rowId: 'shId',
                 order: [[1, 'desc']],
                 columns: [
-                    {data: 'shId', orderable: false, searchable: false, class: 'text-center align-middle select-checkbox p-1', targets: 0, render: function (data, type, row) {return '';}},
+                    {data: 'shId', orderable: false, searchable: false, class: 'text-center align-middle select select-checkbox p-1', targets: 0, render: function (data, type, row) {return '';}},
                     {orderable: false, searchable: false, name: 'serial_number', class: 'align-middle serial_number', targets: 0, render: function (data, type, row) {return '';}},
                     {data:'tracking_number',name: 'shipments.tracking_number', class: 'align-middle tracking_number'},
                     {data:'destination',name: 'oc.name', class: 'align-middle destination'},
