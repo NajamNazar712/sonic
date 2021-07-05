328008e1e83c9b9459b2192a7c95f0c5a678f5a3
Muhammad Aqib <muhammad.aqib@trax.pk>
2021-07-05 14:05:57 +0500

    fix

---
+++ database/migrations/2021_07_05_131825_create_incidence_monitorings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIncidenceMonitoringsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('incidence_monitorings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('incidence_monitorings');
+    }
+}
