83b3e6e9de9610e441f7c80a76c9ebea6948f913
abimwaqas <muhammad.waqas@trax.pk>
2021-07-06 17:01:58 +0500

    fix

--- resources/views/client/shipment/book/errors.blade.php
+++ resources/views/client/shipment/book/errors.blade.php
@@ -438,11 +438,7 @@
                                 @else
                                     <td>{!! Form::text('form[' . $no . '][open_shipment]', $ro['open_shipment'],['class' => 'form-control','style'=>'width:60px','readonly' => 'readonly']) !!}</td>
                                 @endif
-                                {{--@if(isset($errors[$no]['return_address_id']))
-                                <td>{!! Form::text('form[' . $no . '][return_address_id]', $ro['return_address_id'],['class' => 'form-control is-invalid','style'=>'width:auto']) !!}<font color="red">{{$errors[$no]['return_address_id']}}</font></td>
-                                @else
-                                    <td>{!! Form::text('form[' . $no . '][return_address_id]', $ro['return_address_id'],['class' => 'form-control','style'=>'width:60px','readonly' => 'readonly']) !!}</td>
-                                @endif--}}
+
 
                                     <td><button type="button" class="btn btn-icon btn-danger cancel_shipment"><i class="la la-close"></i> </button></td>
 
