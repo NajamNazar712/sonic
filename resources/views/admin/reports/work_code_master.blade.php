669621e68b16c856bb783da4c0b9f8bbb4ae563f
abimwaqas <muhammad.waqas@trax.pk>
2021-07-06 12:02:38 +0500

    fix

--- resources/views/admin/reports/work_code_master.blade.php
+++ resources/views/admin/reports/work_code_master.blade.php
@@ -294,6 +294,11 @@
                         params.length = -1;
                         params.excel = true;
                     }
+                    else{
+                        params = {
+                            'excel':false,
+                        }
+                    }
                     
                     var jsonResult = $.ajax({
                         url: '{{ route('admin.reports.work_code_master.list') }}',
@@ -323,7 +328,7 @@
                         },
                         async: false
                     });
-                    UnblockPagePermanently();
+
 
                     return {body: body, header: head};
                 }
@@ -338,7 +343,7 @@
                     {
                         extend: 'excel',
                         title: 'Work Code Master Report',
-                        className: 'btn btn-primary',
+                        className: 'btn btn-primary excel',
                         text: '<i class="la la-file-excel-o"></i> Excel',
                     },
                 ],
@@ -361,8 +366,6 @@
                         d.status_marked = $('#status_marked').val();
                         d.search_rider = $('#search_rider').val();
                         d.search_admin = $('#search_admin').val();
-
-                        
                         
                     }
                 },
@@ -382,7 +385,7 @@
                     $('td:eq(0)', row).html(index + 1 + info.page * info.length);
                 },
                 initComplete: function() {
-                    
+
                     this.api().table().columns.adjust();
                 }
             });
