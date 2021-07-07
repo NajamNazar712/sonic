419e60dad407c32fd5439cb602cb5becc9c8e69b
Maaz Khan <Maaz.Khan@Trax.pk>
2021-07-07 10:12:56 +0500

    delivery note not creating bug fix

--- app/Http/Controllers/Admins/DeliveryController.php
+++ app/Http/Controllers/Admins/DeliveryController.php
@@ -298,7 +298,7 @@ class DeliveryController extends Controller
     public function check_rider_dncc_status(Request $request)
     {
         $datetime = Carbon::createFromFormat('Y-m-d H:i:s', '2021-05-18 23:59:00');
-        $delivery_note = DeliveryNote::where([['rider_id', $request->rider_id], ['dncc_status', 0]])
+        $delivery_note = DeliveryNote::where([['rider_id', $request->rider_id], ['dncc_status', 0]])->where('status','!=',4)
             ->whereDate('created_at', '>', $datetime);
         if ($delivery_note->exists()) {
             $delivery_note_request = DeliveryNoteRequests::where('rider_id',$request->rider_id)->where('status',2)->where('completed',0)->latest()->first();
