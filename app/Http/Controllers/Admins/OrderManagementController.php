1d8e0d88634da0d76a946fbb859d0288cb886ea4
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2021-07-09 12:19:20 +0500

    Fix
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- app/Http/Controllers/Admins/OrderManagementController.php
+++ app/Http/Controllers/Admins/OrderManagementController.php
@@ -68,7 +68,7 @@ class OrderManagementController extends Controller
             ->join('cities as h', 'dc.hub_id', '=', 'h.id')
             ->join('shipping_modes as sm', 'sm.id', '=', 'shipments.shipping_mode_id')
             ->join('booking_types as bt', 'bt.id', '=', 'shipments.booking_type_id')
-            ->join('payment_modes as pm', 'sm.id', '=', 'shipments.payment_mode_id')
+            ->join('payment_modes as pm', 'pm.id', '=', 'shipments.payment_mode_id')
             ->leftJoin('shipments_journey', function ($join) {
                 $join->on('shipments_journey.shipment_id', '=', 'shipments.id')
                     ->where('shipments_journey.id', '=',
