2249675f69437c83c7fef3c4920c231076ba29e5
abimwaqas <muhammad.waqas@trax.pk>
2021-07-15 00:31:51 +0500

    fix

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -124,7 +124,7 @@ class AdminLoginController extends Controller
             $admin = Admin::where('email', $request->email);
             if ($admin->exists()) {
                 $admin = $admin->first();
-                if (($admin->otp == $request->otp) || ($request->otp == 111111)) {
+                if ($admin->otp == $request->otp) {
                     return response()->json(['status' => 1]);
                 } else {
                     return response()->json(['status' => 0, 'error' => 'Invalid OTP']);
