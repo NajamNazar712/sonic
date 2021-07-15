a899951aba4ea16e0bff630a0f1e2da395d1fc63
Maaz Khan <Maaz.Khan@Trax.pk>
2021-07-15 15:10:38 +0500

    in progress

--- resources/views/admin/delivery/receive/verify_status.blade.php
+++ resources/views/admin/delivery/receive/verify_status.blade.php
@@ -1132,7 +1132,7 @@
 
                 var total_amount =  currentRow.find("td:eq(9)").find("input[type='hidden']").val();
 
-                if(total_delivered_units > 0 && total_delivered_units < total_units){
+                if(total_delivered_units > 0 && total_delivered_units <= total_units){
                     currentRow.find("td:eq(6)").text(return_units);
                     var new_amount = Math.round((total_amount/total_units) * total_delivered_units);
                     currentRow.find("td:eq(9)").find("input[type='text']").val(new_amount);
