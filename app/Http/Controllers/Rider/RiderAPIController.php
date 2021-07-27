3cbe270abe479619dee691ea17f2b15aee91fc70
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-07-27 15:31:35 +0500

    changes

--- app/Http/Controllers/Rider/RiderAPIController.php
+++ app/Http/Controllers/Rider/RiderAPIController.php
@@ -8431,9 +8431,8 @@ class RiderAPIController extends Controller
             foreach ($rider_incentives as $rider_incentive) {
                 $datum = array();
                 $payable = DeliveryNote::where('rider_id', $rider_id)
-                    ->whereDate('created_at', '2021-07-15')
+                    ->whereDate('created_at', $rider_incentive->created_at)
                     ->sum('received_cod_amount');
-                return response()->json(["status" => 1, "message" => $payable]);
                 $datum['pickup_shipments'] = $rider_incentive->pickup_shipments;
                 $datum['pickup_incentive'] = $rider_incentive->pickup_incentive;
                 $datum['delivery_shipments'] = $rider_incentive->delivery_shipments;
