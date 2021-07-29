2895137415f6d3eccdede4a71615e8840f5b06f9
abimwaqas <muhammad.waqas@trax.pk>
2021-07-29 13:39:43 +0500

    fix

--- app/Http/Controllers/Admins/DeliveryController.php
+++ app/Http/Controllers/Admins/DeliveryController.php
@@ -781,13 +781,12 @@ class DeliveryController extends Controller
                     $shipment_data->shipper_status_id = 5;
                     $shipment_data->consignee_status_id = 5;
                     if(in_array($shipment, $open_box_ids)){
-                        ShipmentDetail::where('shipment_id', $shipment)->update(['is_open' => 1]);
-                        
+                        $shipment_detail = ShipmentDetail::where('shipment_id', $shipment)->first();
+                        $shipment_detail->is_open = 1;
+                        $shipment_detail->save();
+
                         $shipment_data->open_box = 1;
                         ShipmentOpenBoxJourneyController::add($shipment,3,Auth::id());
-                    }else{
-                        ShipmentDetail::where('shipment_id', $shipment)->update(['is_open' => 0]);
-                        
                     }
                     $shipment_data->save();
 
