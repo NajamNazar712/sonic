6acdb5a34bc069dd04d32d7780232dfcda0a275a
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-07-30 23:28:49 +0500

    change

--- app/Http/Controllers/Rider/RiderAPIController.php
+++ app/Http/Controllers/Rider/RiderAPIController.php
@@ -8797,9 +8797,7 @@ class RiderAPIController extends Controller
                         }
 
                         if (DeliveryNote::where('id', $request->delivery_note_id)->where('pending_status', 0)->exists()) {
-                            dd($request->distribution);
-                            if ($request->get("distribution") == 1) {
-
+                            if ($request->distribution == 1) {
                                 if ($request->has('distribution_items_list')) {
                                     $distribution_items = json_decode($request->distribution_items_list, true);
                                     foreach ($distribution_items as $distribution_item) {
