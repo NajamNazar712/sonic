8a04362cd1d46e658e690891216aaad1cd3ad9e6
Nabeel973 <nabeelsiddiqui324@gmail.com>
2021-08-03 09:19:19 +0500

    print

--- app/Http/Models/CargoManifest.php
+++ app/Http/Models/CargoManifest.php
@@ -6,5 +6,48 @@ use Illuminate\Database\Eloquent\Model;
 
 class CargoManifest extends Model
 {
-    //
+
+    public function manifest_bags() {
+        return $this->hasMany('App\Http\Models\ManifestBags')->orderBy('bag_id');
+    }
+
+    public function origin_hub() {
+        return $this->belongsTo('App\Http\Models\City', 'origin_hub_id', 'id');
+    }
+
+    public function destination_hub() {
+        return $this->belongsTo('App\Http\Models\City', 'destination_hub_id', 'id');
+    }
+
+    public function shipping_mode() {
+        return $this->belongsTo('App\Http\Models\ShippingMode');
+    }
+
+    public function transport_mode() {
+        return $this->belongsTo('App\Http\Models\TransportMode');
+    }
+
+   /* public function transport_mode_vendor() {
+        return $this->belongsTo('App\Http\Models\TransportModeVendor');
+    }*/
+
+   /* public function status() {
+        return $this->belongsTo('App\Http\Models\Admin\MasterCargo\MasterCargoStatus', 'status_id', 'id');
+    }*/
+
+    public function sender() {
+        return $this->belongsTo('App\Http\Models\Admin\Admin', 'created_by', 'id');
+    }
+
+    public function receiver() {
+        return $this->belongsTo('App\Http\Models\Admin\Admin', 'received_by', 'id');
+    }
+
+   /* public function route_management() {
+        return $this->belongsTo('App\Http\Models\Admin\RouteManagement');
+    }
+
+    public function fleet() {
+        return $this->belongsTo('App\Http\Models\Admin\Fleet');
+    }*/
 }
