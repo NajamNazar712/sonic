f7028d988f749950c1de08ecb048968f764e8940
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-08-05 16:49:02 +0500

    change

--- app/Http/Controllers/AdminAPIController.php
+++ app/Http/Controllers/AdminAPIController.php
@@ -2850,14 +2850,12 @@ class AdminAPIController extends Controller
         $admin_id = $request->admin_id;
         $bag_no = $request->bag_no;
         $admin = Admin::find($admin_id);
-        return response()->json(['status' => 1, 'message' => $admin->default_hub_id]);
         $cargos = CargoManifest::join('cities as oh', 'cargo_manifests.origin_hub_id', '=', 'oh.id')
             ->join('cities as dh', 'cargo_manifests.destination_hub_id', '=', 'dh.id')
             ->where('cargo_manifests.status_id',1)
-//            ->where('cargo_manifests.destination_hub_id', $admin->default_hub_id)
-            /*->where(function ($query) use ($admin) {
+            ->where(function ($query) use ($admin) {
                 $query->where('cargo_manifests.destination_hub_id', $admin->default_hub_id);
-            })*/
+            })
             ->select('cargo_manifests.id as cargo_id', 'cargo_manifests.bags as bags', 'cargo_manifests.shipments as shipments', 'dh.name as destination', 'oh.name as origin', 'cargo_manifests.vehicle_number as vehicle_no');
 
         if ($bag_no != null) {
