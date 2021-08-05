fe29254132916f591eef9554b73d5f378bcb37c6
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-08-05 15:39:44 +0500

    change

--- app/Http/Controllers/AdminAPIController.php
+++ app/Http/Controllers/AdminAPIController.php
@@ -2918,11 +2918,10 @@ class AdminAPIController extends Controller
         $admin_id = $request->admin_id;
         $admin = Admin::find($admin_id);
         $bag_ids = explode(',', $request->bags);
-        return response()->json(['status' => 0, 'data' => $request->bags]);
         $bags = CargoManifestBag::join('manifest_bags as mb', 'cargo_manifest_bags.id', '=', 'mb.cargo_manifest_bag_id')
-            ->join('v2_junction_mappings as jm', 'cargo_manifest_bags.junction_mapping_id', '=', 'jm.id')
             ->join('cities as oh', 'cargo_manifest_bags.origin_hub_id', '=', 'oh.id')
             ->join('cities as dh', 'cargo_manifest_bags.destination_hub_id', '=', 'dh.id')
+            ->leftjoin('v2_junction_mappings as jm', 'cargo_manifest_bags.junction_mapping_id', '=', 'jm.id')
             ->leftjoin('v2_junctions as j', 'cargo_manifest_bags.junction_mapping_id', 'j.junction_mapping_id')
             ->whereIn('cargo_manifest_bags.status_id', [2, 4, 6, 8, 9, 10])
             ->whereIn('cargo_manifest_bags.id',$bag_ids)
