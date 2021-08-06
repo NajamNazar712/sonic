406881fe82a501f5664aab1894d23a73ca979fdd
Nabeel973 <nabeelsiddiqui324@gmail.com>
2021-08-06 19:19:26 +0500

    changes

--- app/Http/Controllers/Admins/AdminCargoManifestController.php
+++ app/Http/Controllers/Admins/AdminCargoManifestController.php
@@ -1861,7 +1861,8 @@ class AdminCargoManifestController extends Controller
              ->leftjoin('admins as ah', 'cargo_manifest_bags.updated_by', '=', 'ah.id')
              ->join('transport_modes as tm', 'cargo_manifest_bags.transport_mode_id', '=', 'tm.id')
              ->join('cargo_manifest_bag_statuses as bs', 'cargo_manifest_bags.status_id', '=', 'bs.id')
-             ->select('cargo_manifest_bags.id', 'cargo_manifest_bags.status_id as status_id', 'oh.id as origin_id', 'oh.name as origin', 'dh.id as destination_id', 'dh.name as destination', 'cargo_manifest_bags.shipments', 'cargo_manifest_bags.quantity','tm.name as transport_mode','cargo_manifest_bags.shipments_weight', 'cargo_manifest_bags.actual_weight', 'cargo_manifest_bags.shipments as shipments', 'a.name as transitted_by','oh.hub_id as origin_hub_id', 'dh.hub_id as destination_hub_id', 'cargo_manifest_bags.type as bag_type','cargo_manifest_bags.seal_number','bs.name as status','sm.mode as shipping_mode','cm.id as manifest_id','cargo_manifest_bags.seal_number','cm.created_at as manifest_created_at','cargo_manifest_bags.origin_hub_id as origin_hub_id','cargo_manifest_bags.destination_hub_id as destination_hub_id','cm.id as manifest','ah.name as updated_by','cargo_manifest_bags.created_at as transitted_date','cargo_manifest_bags.short_received_shipments as short_received_shipments');
+
+             ->select('cargo_manifest_bags.id', 'cargo_manifest_bags.status_id as status_id', 'oh.id as origin_id', 'oh.name as origin', 'dh.id as destination_id', 'dh.name as destination', 'cargo_manifest_bags.shipments', 'cargo_manifest_bags.quantity','tm.name as transport_mode','cargo_manifest_bags.shipments_weight', 'cargo_manifest_bags.actual_weight', 'cargo_manifest_bags.shipments as bag_shipments', 'a.name as transitted_by','oh.hub_id as origin_hub_id', 'dh.hub_id as destination_hub_id', 'cargo_manifest_bags.type as bag_type','cargo_manifest_bags.seal_number','bs.name as status','sm.mode as shipping_mode','cm.id as manifest_id','cargo_manifest_bags.seal_number','cm.created_at as manifest_created_at','cargo_manifest_bags.origin_hub_id as origin_hub_id','cargo_manifest_bags.destination_hub_id as destination_hub_id','cm.id as manifest','ah.name as updated_by','cargo_manifest_bags.created_at as transitted_date','cargo_manifest_bags.short_received_shipments as short_received_shipments',DB::raw('(select count(bag_shipments.id) from cargo_manifest_bag_shipments as bag_shipments where bag_shipments.cargo_manifest_bag_id =cargo_manifest_bags.id AND status = 1 ) as arrived_shipments_count' ) )->where('cargo_manifest_bags.shipments','!=','arrived_shipments_count');
 
 
         $datatables = Datatables::of($bags)
@@ -1893,8 +1894,8 @@ class AdminCargoManifestController extends Controller
                     return '';
                 }
             })
-            ->addColumn('shipments', function ($bag) {
-                if($bag->shipments != 0){
+            ->addColumn('bag_shipments', function ($bag) {
+                if($bag->bag_shipments != 0){
                     return '<button class="btn btn-sm btn-outline-info align-middle">' . $bag->shipments . '</button>';
                 }
                 else{
@@ -1946,8 +1947,8 @@ class AdminCargoManifestController extends Controller
         ;
 
         if ($tracking_number = $request->get('tracking_number')) {
-            $datatables->join('cargo_manifest_bag_shipments as bssh', 'cargo_manifest_bags.id', '=', 'bssh.cargo_manifest_bag_id')
-                ->join('shipments as s', 'bssh.shipment_id', '=', 's.id')
+            $datatables/*->join('cargo_manifest_bag_shipments as bssh', 'cargo_manifest_bags.id', '=', 'bssh.cargo_manifest_bag_id')
+                ->join('shipments as s', 'bssh.shipment_id', '=', 's.id')*/
                 ->where('s.tracking_number', '=', $tracking_number);
         }
 
