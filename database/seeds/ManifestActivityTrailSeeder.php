9f94fe68dfe73d6e53d3fa4b1fe7447cd0e1876e
Anas Anwer <anas.anwer@trax.pk>
2021-08-10 17:15:51 +0500

    changes

--- database/seeds/ManifestActivityTrailSeeder.php
+++ database/seeds/ManifestActivityTrailSeeder.php
@@ -11,20 +11,19 @@ class ManifestActivityTrailSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('activity_trail_actions')->insert(array(
-            array('id' => 404, 'screen_name' => 'Cargo Manifest', 'action'=> 'View'),
-            array('id' => 405, 'screen_name' => 'Cargo Manifest', 'action'=> 'Excel Download'),
-            array('id' => 406, 'screen_name' => 'Cargo Manifest Short Received Report', 'action'=> 'View'),
-            array('id' => 407, 'screen_name' => 'Cargo Manifest Short Received Report', 'action'=> 'Excel Download'),
-            array('id' => 413, 'screen_name' => 'Cargo Manifest Open Box', 'action'=> 'View'),
-
-        ));
-
-        DB::table('module_permissions')->insert(array(
-            array('id' => 564, 'name' => 'Manifest - View', 'module_id' => 32),
-            array('id' => 555, 'name' => 'Cargo Manifest Short Received Report - View', 'module_id' => 32),
-            array('id' => 555, 'name' => 'Cargo Manifest Short Received Report - View', 'module_id' => 32),
-        ));
+//        DB::table('activity_trail_actions')->insert(array(
+//            array('id' => 404, 'screen_name' => 'Cargo Manifest', 'action'=> 'View'),
+//            array('id' => 405, 'screen_name' => 'Cargo Manifest', 'action'=> 'Excel Download'),
+//            array('id' => 406, 'screen_name' => 'Cargo Manifest Short Received Report', 'action'=> 'View'),
+//            array('id' => 407, 'screen_name' => 'Cargo Manifest Short Received Report', 'action'=> 'Excel Download'),
+//            array('id' => 413, 'screen_name' => 'Cargo Manifest Open Box', 'action'=> 'View'),
+//
+//        ));
+//
+//        DB::table('module_permissions')->insert(array(
+//            array('id' => 564, 'name' => 'Manifest - View', 'module_id' => 32),
+//            array('id' => 555, 'name' => 'Cargo Manifest Short Received Report - View', 'module_id' => 32),
+//        ));
         
         $timestamp = \Carbon\Carbon::now()->format('Y-m-d H:i:s');
         DB::table('admins_screen_list')->insert(array(
