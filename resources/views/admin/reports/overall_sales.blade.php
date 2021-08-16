9bee4098e6b6e70bba57b835a10d60a412abbca7
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2021-08-16 21:49:45 +0500

    Overall Sales removed Category and Description
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- resources/views/admin/reports/overall_sales.blade.php
+++ resources/views/admin/reports/overall_sales.blade.php
@@ -144,8 +144,6 @@
                         <th class="border-primary border-darken-1">Class</th>
                         <th class="border-primary border-darken-1">Attempts</th>
                         <th class="border-primary border-darken-1">Shipping Mode</th>
-                        <th class="border-primary border-darken-1">Category</th>
-                        <th class="border-primary border-darken-1">Description</th>
                         <th class="border-primary border-darken-1">International Tracking No.</th>
                         <th class="border-primary border-darken-1">Collection Amount</th>
                         <th class="border-primary border-darken-1">Actual Weight</th>
@@ -375,8 +373,6 @@
                             head.push('Class');
                             head.push('Attempts');
                             head.push('Shipping Mode');
-                            head.push('Category');
-                            head.push('Description');
                             head.push('International Tracking No.');
                             head.push('Collection Amount');
                             head.push('Actual Weight');
@@ -421,8 +417,6 @@
                                 row.push(values.class);
                                 row.push(values.attempts);
                                 row.push(values.shipping_mode);
-                                row.push(values.category);
-                                row.push(values.description);
                                 row.push(values.international_tracking_number);
                                 row.push(values.p_collection_amount);
                                 row.push(values.actual_weight);
@@ -516,8 +510,6 @@
                     { data:'class' ,name: 'zcc.class', class: 'align-middle class'},
                     { data:'attempts' ,name: 'attempts', class: 'align-middle attempts',sortable:false},
                     { data:'shipping_mode' ,name: 'sm.mode', class: 'align-middle shipping_mode'},
-                    { data:'category' ,name: 'p.product_name', class: 'align-middle category'},
-                    { data:'description' ,name: 'si.description', class: 'align-middle description'},
                     { data:'international_tracking_number' ,name: 'ibs.international_tracking_number', class: 'align-middle international_tracking_number'},
                     { data:'p_collection_amount' ,name: 'pps.amount', class: 'align-middle collection_amount'},
                     { data:'actual_weight' ,name: 'shipments.actual_weight', class: 'align-middle actual_weight'},
