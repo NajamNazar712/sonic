4f819fa6192508d25840ab564c2059c968d0cb82
abimwaqas <muhammad.waqas@trax.pk>
2021-08-20 23:15:43 +0500

    fix

--- database/seeds/UpdateActivityTrailActionForLastMileSettingSeeder.php
+++ database/seeds/UpdateActivityTrailActionForLastMileSettingSeeder.php
@@ -12,9 +12,9 @@ class UpdateActivityTrailActionForLastMileSettingSeeder extends Seeder
     public function run()
     {
 
-        /*DB::table('module_permissions')->insert(array(
+        DB::table('module_permissions')->insert(array(
             array('id' => 565, 'name' => 'Last Mile Status Cron Time - View', 'module_id' => 14),
-        ));*/
+        ));
 
         DB::table('activity_trail_actions')->insert(array(
             array('id' => 431, 'screen_name' => 'Last Mile Status Cron Time', 'action'=> 'View'),
