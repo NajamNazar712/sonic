01058a09501a0c83b5d0e1067b28d069f824bf12
Nabeel973 <nabeelsiddiqui324@gmail.com>
2021-08-20 15:57:14 +0500

    fix

--- database/seeds/UpdateNotificationsForFnf.php
+++ database/seeds/UpdateNotificationsForFnf.php
@@ -15,7 +15,7 @@ class UpdateNotificationsForFnf extends Seeder
         $timestamp = Carbon::now()->format('Y-m-d H:i:s');
 
         DB::table('notifications')->insert(array(
-            array('id' => 146, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'FNF Request', 'type_id' => 1, 'subject' => 'FNF Request [emp_id]', 'body' => 'The final settlement of FNF request of below staff awaits your approval.'. PHP_EOL . '[link]'. PHP_EOL . '[emp_id]'. PHP_EOL . '[name]' . PHP_EOL . '[designation]' , 'updated_by' => 3, 'status' => 1)
+            array('id' => 146, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'FNF Request', 'type_id' => 1, 'subject' => 'FNF Request [emp_id]', 'body' => 'The final settlement of FNF request of below staff awaits your approval.'. PHP_EOL .  PHP_EOL . '[link]'. PHP_EOL .  PHP_EOL . ' Trax Id : [emp_id]'. PHP_EOL . 'Name : [name]' . PHP_EOL . 'Designation : [designation]' , 'updated_by' => 3, 'status' => 1)
         ));
     }
 }
