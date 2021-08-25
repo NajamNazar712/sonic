9ade632ecaeb79c278f3ccfde3f9790494230aef
abimwaqas <muhammad.waqas@trax.pk>
2021-08-25 22:15:03 +0500

    fix

--- resources/views/admin/debriefing/caller_agent.blade.php
+++ resources/views/admin/debriefing/caller_agent.blade.php
@@ -220,20 +220,20 @@
                                                             <td><strong>Attempt Count :</strong></td>
                                                             <td>{{$reattempt_count->reattempts}}</td>
                                                             <td><strong>Rider Status :</strong></td>
-                                                            @if($rider_status)
+                                                            @if($rider_status != NULL)
                                                             <td>{{$rider_status->shipment_status_shipper->name}}</td>
                                                             @else
                                                                 <td></td>
                                                             @endif
                                                             <td><strong>Rider Status Reason :</strong></td>
-                                                            @if($rider_status)
+                                                            @if($rider_status != NULL)
                                                             <td>{{($rider_status->status_reason_id) ? $rider_status->shipment_status_reason->name : '-'}}</td>
                                                             @else
                                                                 <td></td>
                                                             @endif
                                                             
                                                             <td><strong>Remarks :</strong></td>
-                                                            @if($rider_status)
+                                                            @if($rider_status != NULL)
                                                             <td>{{($rider_status->remarks) ? $rider_status->remarks : '-'}}</td>
                                                             @else
                                                                 <td></td>
