c399a57893f7cb1bb024783ac83eb913f88254f0
Taha Habib <taha.habib@trax.pk>
2021-08-27 01:57:02 +0500

    changes1

--- resources/views/client/auth/login.blade.php
+++ resources/views/client/auth/login.blade.php
@@ -47,7 +47,7 @@
                 <div class="col-12 d-flex align-items-center justify-content-end">
                     <div class="col-md-4 col-10 p-0">
                         <div class="mb-2 text-center">
-                            <p class="white bold" style="font-size: 17px !important;">For Help Dial : 021-111-118-729 or email us info@trax.pk</p>
+                            <p class="white bold" style="font-size: 17px !important;">For Help Dial : <a href="tel:021-111-118-729" style="color:#ffffff;text-decoration:underline;">021-111-118-729</a>  or email us <a href="mailto:info@trax.pk" style="color:#ffffff;text-decoration:underline;">info@trax.pk</a></p>
                         </div>
                         <div class="card box-shadow-1 border-grey border-lighten-3 px-1 py-1 m-0">
                             <div class="card-header border-0">
