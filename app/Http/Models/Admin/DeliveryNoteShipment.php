18402a17cad9cde958e49d18b2793715b045f587
abimwaqas <muhammad.waqas@trax.pk>
2021-08-29 16:10:00 +0500

    migrations

--- app/Http/Models/Admin/DeliveryNoteShipment.php
+++ app/Http/Models/Admin/DeliveryNoteShipment.php
@@ -9,7 +9,6 @@ class DeliveryNoteShipment extends Model
 {
 	protected $primaryKey = ['delivery_note_id', 'shipment_id'];
 	public $incrementing = FALSE;
-	public $timestamps = FALSE;
 	protected $fillable = [
 		'delivery_note_id','shipment_id','status','call_verification','notification','rider_information','ordering','update_type'
 	];
