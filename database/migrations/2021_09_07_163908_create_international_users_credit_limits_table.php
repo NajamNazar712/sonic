a1e059f25c17b6c05aa6ee45916d8b83bb77e26a
abimwaqas <muhammad.waqas@trax.pk>
2021-09-08 11:44:34 +0500

    fix

---
+++ database/migrations/2021_09_07_163908_create_international_users_credit_limits_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInternationalUsersCreditLimitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('international_users_credit_limits', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->index();
+            $table->integer('limit');
+            $table->integer('limit_usage')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('international_users_credit_limits');
+    }
+}
