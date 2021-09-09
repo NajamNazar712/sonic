893ed8935ccf9153ee60c6f97e3869cf7c93b0ef
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-09-09 14:52:37 +0500

    Employee Attendance Controller Change

--- app/Http/Controllers/Admins/Attendance/AdminAttendanceController.php
+++ app/Http/Controllers/Admins/Attendance/AdminAttendanceController.php
@@ -29,8 +29,8 @@ class AdminAttendanceController extends Controller
         $cities = City::select('id','name')->get();
         $departments = AdminDepartment::select('id','name')->get();
         $users = Admin::where('status', 1)->select('id','name')->get();
-        $trax_id = Admin::wherenotnull('trax_id')->where('status', 1)->pluck('trax_id')->toArray();
-        $rider_trax_id = Rider::wherenotnull('trax_id')->where('status', 1)->pluck('trax_id')->toArray();
+        $trax_id = Admin::wherenotnull('trax_id')->pluck('trax_id')->toArray();
+        $rider_trax_id = Rider::wherenotnull('trax_id')->pluck('trax_id')->toArray();
         $trax_ids = array_merge($trax_id, $rider_trax_id);
         $admin_cnic = Admin::wherenotnull('cnic')->where('status', 1)->pluck('cnic')->toArray();
         $rider_cnic = Rider::wherenotnull('cnic')->where('status', 1)->pluck('cnic')->toArray();
@@ -52,9 +52,7 @@ class AdminAttendanceController extends Controller
             ->leftjoin('riders as r', 'r.id', 'employee_attendances.employee_id')
             ->leftjoin('cities as rc', 'rc.id', 'r.city_id')
             ->leftjoin('rider_types as rt', 'rt.id', 'r.rider_type_id')
-            ->select('a.name as admin_name', 'a.trax_id as trax_id', 'c.name as city_name', 'c.id as city_id', 'a.designation as designation', 'r.name as rider_name', 'r.trax_id as rider_trax_id', 'rc.name as rider_city_name', 'rc.id as rider_city_id', 'rt.name as rider_type', 'rt.id as rider_type_id', 'employee_attendances.attendance_date as attendance_date', 'employee_attendances.clock_in as clock_in', 'employee_attendances.clock_out as clock_out', 'employee_attendances.clock_in_latitude as clock_in_latitude', 'employee_attendances.clock_in_longitude as clock_in_longitude', 'employee_attendances.clock_out_latitude', 'employee_attendances.clock_out_longitude', 'ad.name as department', 'ad.id as department_id', 'employee_attendances.employee_type', 'employee_attendances.clock_in_location as clock_in_status', 'employee_attendances.clock_out_location as clock_out_status', 'r.cnic as rider_cnic', 'a.cnic as admin_cnic')
-            ->where('r.status', 1)
-            ->where('a.status', 1);
+            ->select('a.name as admin_name', 'a.trax_id as trax_id', 'c.name as city_name', 'c.id as city_id', 'a.designation as designation', 'r.name as rider_name', 'r.trax_id as rider_trax_id', 'rc.name as rider_city_name', 'rc.id as rider_city_id', 'rt.name as rider_type', 'rt.id as rider_type_id', 'employee_attendances.attendance_date as attendance_date', 'employee_attendances.clock_in as clock_in', 'employee_attendances.clock_out as clock_out', 'employee_attendances.clock_in_latitude as clock_in_latitude', 'employee_attendances.clock_in_longitude as clock_in_longitude', 'employee_attendances.clock_out_latitude', 'employee_attendances.clock_out_longitude', 'ad.name as department', 'ad.id as department_id', 'employee_attendances.employee_type', 'employee_attendances.clock_in_location as clock_in_status', 'employee_attendances.clock_out_location as clock_out_status', 'r.cnic as rider_cnic', 'a.cnic as admin_cnic');
 
         if (session('role_id') != 1) {
             $attendances = $attendances->whereIn('c.hub_id', session('hubs'));
