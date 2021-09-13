5cbfb9ee2c073e58a1c291eb20ba80da91563e91
Muhammad Aqib <muhammad.aqib@trax.pk>
2021-09-13 16:19:15 +0500

    changes

--- app/Http/Controllers/Admins/DeliveryController.php
+++ app/Http/Controllers/Admins/DeliveryController.php
@@ -892,6 +892,7 @@ class DeliveryController extends Controller
                 // $rider_attendance_action->longitude = $request->longitude;
                 $rider_attendance_action->save();
             }
+            //rider attendance end
 
 
             return redirect()->back()->with(['success'=>'Delivery note has been created successfully','print'=>$note->id]);
@@ -6525,6 +6526,35 @@ ActivityTrailController::createActivityTrailLog(Auth::id(),303);
                     if($ccd_flag == false || ($ccd_flag == true && $rider->ccd == 1)){
                         $delivery_note->rider_id = $rider_id;
                         $delivery_note->save();
+
+                        //rider attendance
+                        $attendance_datetime = Carbon::now()->format('Y-m-d H:i:s');
+                        $attendance_date = Carbon::now()->format('Y-m-d');
+                        $attendance_time = Carbon::now()->format('H:i:s');
+
+                        $rider_attendance = EmployeeAttendance::where('employee_id', $rider_id)
+                            ->whereDate('attendance_date', $attendance_date)
+                            ->where('employee_type', 2);
+                        if (!$rider_attendance->exists()) {
+                            $rider_attendance = new EmployeeAttendance();
+                            $rider_attendance->employee_id = $rider_id;
+                            $rider_attendance->employee_type = 2;
+                            $rider_attendance->attendance_date = $attendance_date;
+                            $rider_attendance->clock_in = $attendance_time;
+                            // $rider_attendance->clock_in_latitude = $request->latitude;
+                            // $rider_attendance->clock_in_longitude = $request->longitude;
+                            $rider_attendance->save();
+                            
+                            $rider_attendance_action = new EmployeeAttendanceActionLog();
+                            $rider_attendance_action->employee_id = $rider_id;
+                            $rider_attendance_action->employee_type = 2;
+                            $rider_attendance_action->action_id = 1;
+                            $rider_attendance_action->action_date = $attendance_datetime;
+                            // $rider_attendance_action->latitude = $request->latitude;
+                            // $rider_attendance_action->longitude = $request->longitude;
+                            $rider_attendance_action->save();
+                        }
+                        //rider attendance end
                         return response()->json(['status' => 0, 'success' => 'Rider updated successfully']);
                     }
                     else{
