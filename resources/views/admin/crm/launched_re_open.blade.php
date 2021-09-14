b84d9df7409a64263484483a3cb0ab5edd006352
Taha Habib <taha.habib@trax.pk>
2021-09-14 15:09:31 +0500

    changes

--- resources/views/admin/crm/launched_re_open.blade.php
+++ resources/views/admin/crm/launched_re_open.blade.php
@@ -32,6 +32,7 @@
                             </form>
 
                             <table class="table table-bordered datatable" id="datatable" style="z-index: 3;">
+                                <label class="ml-1" id="count"></label>
                                 <thead>
                                 <tr role="row" class="bg-primary white">
                                     <th class="border-primary border-darken-1"></th>
@@ -663,6 +664,7 @@
                 lengthMenu: [[50, 100, 500, 1000, -1], [50, 100, 500, 1000, 'All']],
                 pageLength: 50,
                 pagingType: 'full_numbers',
+                paging: true,
                 processing: true,
                 serverSide: true,
                 language: {
@@ -926,6 +928,16 @@
                     this.api().table().columns.adjust();
                 }
             });
+            table.on( 'select', function ( e, dt, type, indexes ) {
+                var count = table.rows( { selected: true } ).count();
+                var lblcount= document.getElementById('count');
+                lblcount.textContent =count +' Row(s) selected';
+            } );
+            table.on( 'deselect', function ( e, dt, type, indexes ) {
+                var count = table.rows( { selected: true } ).count();
+                var lblcount= document.getElementById('count');
+                lblcount.textContent =count +' Row(s) selected';
+            } );
             $('.closebutton').on('click',function(){
                 $("#BulkExternalCommentModal").on("hidden.bs.modal", function() {
                     $("#BulkExternalCommentModal #bulk_comment").val("");
