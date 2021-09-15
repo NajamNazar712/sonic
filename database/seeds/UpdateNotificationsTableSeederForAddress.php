2909c83f21c643ad8f90d12ede0b199b6a82b937
Nabeel973 <nabeelsiddiqui324@gmail.com>
2021-09-15 14:05:23 +0500

    notification

---
+++ database/seeds/UpdateNotificationsTableSeederForAddress.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Models\Notification;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UpdateNotificationsTableSeederForAddress extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+            Notification::where('id',31)->update(['body' => '[shipper_name][email][phone][destination][channel][case_nature][case_nature_type][tracking_number][details][status]'. PHP_EOL . PHP_EOL . PHP_EOL.'Regards,'.PHP_EOL.'TRAX Logistics'.PHP_EOL.'Address: Plot 105, Sector 7-A, Mehran Town, Korangi, Karachi
+For Help Dial: 021-111-118-729']);
+
+        Notification::where('id',33)->update(['body' => 'Dear Shipper,'. PHP_EOL . PHP_EOL .'You have approved for additional charges of shipment which has been updated as Out of Service Area.'. PHP_EOL .'Tracking No. = [tracking_number]'. PHP_EOL . 'Destination = [destination]'. PHP_EOL . 'Out of Service Area Charges = [nsa_osa_estimated_charges]' . PHP_EOL .'Remarks = [remarks]'. PHP_EOL . PHP_EOL . PHP_EOL .  'Regards,'.PHP_EOL.'TRAX Logistics'.PHP_EOL.'Address: Plot 105, Sector 7-A, Mehran Town, Korangi, Karachi
+For Help Dial: 021-111-118-729']);
+
+
+        Notification::where('id',36)->update(['body' => 'Dear [company_name_a],'.PHP_EOL.'Please note that the account [company_name_b] bearing account ID [account_id] has been merged with you as a sister account.'.PHP_EOL . PHP_EOL . PHP_EOL . PHP_EOL.'Regards,'.PHP_EOL.'TRAX Logistics'.PHP_EOL.'[trax_logo]'.PHP_EOL.'Address: Plot 105, Sector 7-A, Mehran Town, Korangi, Karachi,'.PHP_EOL.'For Help Dial: 021-111-118-729']);
+
+        Notification::where('id',37)->update(['body' => 'Dear [company_name_a],'.PHP_EOL.'Please note that the account [company_name_b] bearing account ID [account_id] has been removed from your group of sister account.'.PHP_EOL . PHP_EOL . PHP_EOL . PHP_EOL.'Regards,'.PHP_EOL.'TRAX Logistics'.PHP_EOL.'[trax_logo]'.PHP_EOL.'Address: Plot 105, Sector 7-A, Mehran Town, Korangi, Karachi,'.PHP_EOL.'For Help Dial: 021-111-118-729']);
+
+        Notification::where('id',63)->update(['body' => 'Dear Shipper [shipper],' . PHP_EOL . PHP_EOL .'Please note that Pickup Request# [pickup_request_ID] is updated as Cancelled on [date].' . PHP_EOL . PHP_EOL . PHP_EOL .'Regards,'.PHP_EOL. PHP_EOL .'Trax Logistics' . PHP_EOL . PHP_EOL . '[trax_logo]' . PHP_EOL . PHP_EOL . 'Address: Plot 105, Sector 7-A, Mehran Town, Korangi, Karachi,' . PHP_EOL . PHP_EOL . 'For Help Dial: 021-111-118-729']);
+
+        Notification::where('id',64)->update(['body' => 'Dear Concerns,' . PHP_EOL . PHP_EOL .'The rates for the account [account_id] [name] have been rejected by finance department. Please contact for resolution.' . PHP_EOL . PHP_EOL . PHP_EOL .'Regards,'.PHP_EOL. PHP_EOL .'Trax Logistics' . PHP_EOL . PHP_EOL . '[trax_logo]' . PHP_EOL . PHP_EOL . 'Address: Plot 105, Sector 7-A, Mehran Town, Korangi, Karachi,' . PHP_EOL . PHP_EOL . 'Helpline: +92-21-3-877-22-22']);
+
+    }
+}
