41190a9da5eb4086f29fd5729d7577de5ddfa590
Wajahat Ali Farooq <wajahat.farooq@trax.pk>
2021-09-16 16:53:43 +0500

    Email And Deactivation Work Done

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -98,7 +98,8 @@ class Kernel extends ConsoleKernel
         'App\Console\Commands\NotPickedShipmentsJourney',
         'App\Console\Commands\LastMileStatusReport',
 		'App\Console\Commands\ShipperPaymentCalculation',
-		'App\Console\Commands\ReturnSheetReceive',
+        'App\Console\Commands\ReturnSheetReceive',
+        'App\Console\Commands\RiderDeactivateAutomatically',
     ];
 
     /**
@@ -333,6 +334,7 @@ class Kernel extends ConsoleKernel
 
         $schedule->command('sms:retry_otp')->everyMinute()->withoutOverlapping()->runInBackground();
         $schedule->command('Reset:AdminPasswordMonthly')->monthlyOn(1, '06:00')->runInBackground();
+        $schedule->command('email:RiderDeactivateAutomaticallyAndGenerateEmail')->dailyAt('03:30')->runInBackground();
 
         $settings = GlobalSettings::where('type', 'last_mile_cron_time');
         if ($settings->exists()) {
