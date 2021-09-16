8ea6e11f564d8e3b53f493cd2f4a6c32477561ed
Taha Habib <taha.habib@trax.pk>
2021-09-16 16:47:18 +0500

    changes

--- database/seeds/UpdateMailNotificationForBookingAfterCutofftime.php
+++ database/seeds/UpdateMailNotificationForBookingAfterCutofftime.php
@@ -14,7 +14,7 @@ class UpdateMailNotificationForBookingAfterCutofftime extends Seeder
         $timestamp = \Carbon\Carbon::now()->format('Y-m-d H:i:s');
         DB::table('notifications')->where('id', 153)->delete();
         DB::table('notifications')->insert(array(
-            array('id' => 153, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Booking After Cut Off Time', 'type_id' => 1, 'subject' => 'Shipment Booked after Cut Off Time' , 'body' => 'Dear [shipper_name], You have booked shipment after cut off time so we cannot commit pick up today but will try to arrange it or it will be done tomorrow. Thanks and Regards, Team TRAX', 'updated_by' => 3, 'status' => 1)
+            array('id' => 153, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Booking After Cut Off Time', 'type_id' => 1, 'subject' => 'Shipment Booked after Cut Off Time' , 'body' => 'Dear [shipper_name],' . PHP_EOL. 'You have booked shipment after cut off time so we cannot commit pick up today but will try to arrange it or it will be done tomorrow.' . PHP_EOL. 'Thanks and Regards,' . PHP_EOL. 'Team TRAX', 'updated_by' => 3, 'status' => 1)
         ));
     }
 }
