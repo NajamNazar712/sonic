0e9acd23aebf34c2c2d9cffc1d020c5558a361fc
Wajahat Ali Farooq <wajahat.farooq@trax.pk>
2021-09-23 10:48:32 +0500

    Multiple pieces work done

--- app/Console/Commands/RiderDeactivateAutomatically.php
+++ app/Console/Commands/RiderDeactivateAutomatically.php
@@ -51,7 +51,7 @@ class RiderDeactivateAutomatically extends Command
         $v2_pickups  = V2PickupNote::whereBetween('created_at', [$date_week_age, $today])->pluck('rider_id')->toArray();
         $rider_active = array_unique(array_merge($deliveries,$v2_pickups));
         $data = array_diff($rider_ids, $rider_active);
-        if ($data != null){
+        if ($data !t= null){
         NotificationsController::send(155, $data);
         }
     }
