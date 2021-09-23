b8a01a96e4a3b88a42f60e165d3fd2062bbeb786
umairbadar <umair.badar@trax.pk>
2021-09-23 15:43:54 +0500

    changes

--- database/seeds/Sprint77QuickSearchAndActivityTrailSeeder.php
+++ database/seeds/Sprint77QuickSearchAndActivityTrailSeeder.php
@@ -11,19 +11,19 @@ class Sprint77QuickSearchAndActivityTrailSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('activity_trail_actions')->insert(array(
-            array('id' => 439, 'screen_name' => 'Carrefour Bulk Arrival', 'action'=> 'View'),
-            array('id' => 440, 'screen_name' => 'Rider Tracking', 'action'=> 'View'),
-            array('id' => 441, 'screen_name' => 'International Shipments Status', 'action'=> 'View'),
-            array('id' => 442, 'screen_name' => 'Opertaion Service Level Report', 'action'=> 'View'),
-            array('id' => 443, 'screen_name' => 'Opertaion Service Level Report', 'action'=> 'Excel Download'),
-            array('id' => 444, 'screen_name' => 'Carrefour Accounts', 'action'=> 'View'),
-            array('id' => 445, 'screen_name' => 'Debriefing Time Settings', 'action'=> 'View'),
-            array('id' => 446, 'screen_name' => 'Last Mile Status Cron Time', 'action'=> 'View'),
-            array('id' => 447, 'screen_name' => 'DHL International Shipment Sync Time', 'action'=> 'View'),
-            array('id' => 448, 'screen_name' => 'International User', 'action'=> 'View'),
-            array('id' => 449, 'screen_name' => 'Telenor Shipment Status ETA', 'action'=> 'View'),
-        ));
+//        DB::table('activity_trail_actions')->insert(array(
+//            array('id' => 439, 'screen_name' => 'Carrefour Bulk Arrival', 'action'=> 'View'),
+//            array('id' => 440, 'screen_name' => 'Rider Tracking', 'action'=> 'View'),
+//            array('id' => 441, 'screen_name' => 'International Shipments Status', 'action'=> 'View'),
+//            array('id' => 442, 'screen_name' => 'Opertaion Service Level Report', 'action'=> 'View'),
+//            array('id' => 443, 'screen_name' => 'Opertaion Service Level Report', 'action'=> 'Excel Download'),
+//            array('id' => 444, 'screen_name' => 'Carrefour Accounts', 'action'=> 'View'),
+//            array('id' => 445, 'screen_name' => 'Debriefing Time Settings', 'action'=> 'View'),
+//            array('id' => 446, 'screen_name' => 'Last Mile Status Cron Time', 'action'=> 'View'),
+//            array('id' => 447, 'screen_name' => 'DHL International Shipment Sync Time', 'action'=> 'View'),
+//            array('id' => 448, 'screen_name' => 'International User', 'action'=> 'View'),
+//            array('id' => 449, 'screen_name' => 'Telenor Shipment Status ETA', 'action'=> 'View'),
+//        ));
 
         $timestamp = \Carbon\Carbon::now()->format('Y-m-d H:i:s');
         DB::table('admins_screen_list')->insert(
