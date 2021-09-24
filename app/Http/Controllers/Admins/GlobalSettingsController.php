192defa5914974e2d69d5a6354cd25332848653a
Anas Anwer <anas.anwer@trax.pk>
2021-09-24 18:00:33 +0500

    changes

--- app/Http/Controllers/Admins/GlobalSettingsController.php
+++ app/Http/Controllers/Admins/GlobalSettingsController.php
@@ -928,13 +928,12 @@ class GlobalSettingsController extends Controller
                                     $fuel_surcharge_history->save();
 
                                 } else {
-                                    if($fuel_factor > 0) {
-                                        $rate_status->fuel_charges = 1;
-                                        $rate_status->save();
-                                    }
-                                    else{
+                                    if($fuel_factor < 0) {
                                         $fuel_factor = 0;
                                     }
+
+                                    $rate_status->fuel_charges = 1;
+                                    $rate_status->save();
                                     if ($user->account_type_id == 1) {
                                         $fuel_surcharge = new FuelSurcharge();
                                         $fuel_surcharge->user_id = $user->id;
@@ -1041,13 +1040,12 @@ class GlobalSettingsController extends Controller
                                         $fuel_surcharge_history->save();
 
                                     } else {
-                                        if($fuel_factor > 0) {
-                                            $rate_status->fuel_charges = 1;
-                                            $rate_status->save();
-                                        }
-                                        else{
+                                        if($fuel_factor < 0) {
                                             $fuel_factor = 0;
                                         }
+
+                                        $rate_status->fuel_charges = 1;
+                                        $rate_status->save();
                                         if ($user->account_type_id == 1) {
                                             $fuel_surcharge = new FuelSurcharge();
                                             $fuel_surcharge->user_id = $user->id;
