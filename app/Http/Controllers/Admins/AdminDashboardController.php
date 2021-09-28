6524099992d0ecaa9598ed38c969c193b48abdae
Nabeel973 <nabeelsiddiqui324@gmail.com>
2021-09-28 13:22:54 +0500

    rates changes

--- app/Http/Controllers/Admins/AdminDashboardController.php
+++ app/Http/Controllers/Admins/AdminDashboardController.php
@@ -7971,7 +7971,7 @@ class AdminDashboardController extends Controller
 
         //dd($overnight_changes,$overland_changes,$detain_changes,$sameday_changes);
 
-        if($overnight_changes == 0 || $overland_changes == 0 || $detain_changes == 0 || $sameday_changes == 0){
+        if($overnight_changes == 0 && $overland_changes == 0 && $detain_changes == 0 && $sameday_changes == 0){
 
             User::where('id',$id)->update(['rate_status'=>0,'status' => 2,'rates_authorized_by'=> 32,'rates_approved_at'=>Carbon::now()]);
         }
