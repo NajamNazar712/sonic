aa4754ab3a48a68a2a86f4605bb890be890ea346
Nabeel973 <nabeelsiddiqui324@gmail.com>
2021-09-28 19:58:42 +0500

    changes reverted

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -380,7 +380,7 @@ class APIController extends Controller
             if ($value) {
                 $value = $this->phone_number($value);
 
-                if (preg_match('/^((\+92)|(92)|(0092))-{0,1}\d{3}-{0,1}\d{7}$|^\d{3}-{1}\d{7}$|^\d{11}$|^\d{4}-\d{7}$|^\d{3}-\d{7}$|^\d{11}$/', $value)) {
+                if (preg_match('/^((\+92)|(92)|(0092))-{0,1}\d{3}-{0,1}\d{7}$|^\d{3}-{1}\d{7}$|^\d{11}$|^\d{4}-\d{7}$|^\d{3}-\d{7}$|^\d{10}$/', $value)) {
                     return true;
                 } else {
                     return false;
