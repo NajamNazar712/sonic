67cdbb9a8c412edd3eebaccb16ab07c4c4a83785
Taha Habib <taha.habib@trax.pk>
2021-09-28 16:51:28 +0500

    changes of sorter

--- resources/views/admin/crm/resolved.blade.php
+++ resources/views/admin/crm/resolved.blade.php
@@ -603,7 +603,7 @@
                     }
                 },
                 rowId: 'id',
-                order: [[21, 'desc']],
+                order: [[23, 'desc']],
                 columns: [
                     {data: 'id', orderable: false, searchable: false, class: 'text-center align-middle select p-1', targets: 0, render: function (data, type, row) {return '';}},
                     {orderable: false, searchable: false, name: 'serial_number', class: 'align-middle serial_number', targets: 0, render: function (data, type, row) {return '';}},
