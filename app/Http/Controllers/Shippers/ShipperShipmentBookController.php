24435319ebded562c2faf224375e40e9f1bd66cc
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2021-10-01 13:40:06 +0500

    Fix
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- app/Http/Controllers/Shippers/ShipperShipmentBookController.php
+++ app/Http/Controllers/Shippers/ShipperShipmentBookController.php
@@ -4615,7 +4615,7 @@ class ShipperShipmentBookController extends Controller
               if(in_array($user_id,$ccd_account_tags))
               {
                 $rules['payment_mode_id']  = ['required_if:service_type_id,1,2,3', 'nullable', 'integer', 'digits_between:1,10', Rule::exists('payment_modes', 'id')->where(function ($query) {
-                query->whereNotIn('id', [3]);
+                $query->whereNotIn('id', [3]);
                 })];
               }
               else
