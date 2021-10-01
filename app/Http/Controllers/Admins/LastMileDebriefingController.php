44d8a80ca8442411a52e32146936b0de85c7a72c
abimwaqas <muhammad.waqas@trax.pk>
2021-10-01 14:50:48 +0500

    fix

--- app/Http/Controllers/Admins/LastMileDebriefingController.php
+++ app/Http/Controllers/Admins/LastMileDebriefingController.php
@@ -89,7 +89,7 @@ class LastMileDebriefingController extends Controller
         $delivery_note_shipments = $delivery_note_details->delivery_note_shipments;
         if(count($delivery_note_shipments) > 0){
             foreach ($delivery_note_shipments as $delivery_note_shipment){
-                $agent_call_monitor = AgentCallMonitoring::where('shipment_id', $delivery_note_shipment->shipment_id)->where('delivery_note_id', $delivery_note_shipment->delivery_note_id);
+                $agent_call_monitor = AgentCallMonitoring::where('shipment_id', $delivery_note_shipment->shipment_id)->where('delivery_note_id', $delivery_note_shipment->delivery_note_id)->where('completed', 0);
                 if($agent_call_monitor->exists()){
                     $agent_call_monitor = $agent_call_monitor->first();
                     $agent_call_monitor->agent_id = $request->agent_id;
