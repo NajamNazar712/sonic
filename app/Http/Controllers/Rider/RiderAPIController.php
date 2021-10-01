136223985de2f1b51ac9fc2fcd27362313ba54bd
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-10-02 01:30:16 +0500

    change in return undelivered api

--- app/Http/Controllers/Rider/RiderAPIController.php
+++ app/Http/Controllers/Rider/RiderAPIController.php
@@ -8128,11 +8128,19 @@ class RiderAPIController extends Controller
                             }
 
                             $updated_shipments_count = ReturnNoteShipment::where('return_note_id', $request->return_note_id)->where('status', 0)->count();
+                            $total_shipments_count = ReturnNoteShipment::where('return_note_id', $request->return_note_id)->count();
+                            $undelivered_shipments_count = ReturnNoteShipment::where('return_note_id', $request->return_note_id)->where('status', 1)->count();
 
-                            if ($updated_shipments_count == 0) {
-                                $return_note_data->status = 3;
+                            if ($total_shipments_count == $undelivered_shipments_count){
+                                $return_note_data->status = 1;
                                 $return_note_data->updated_at = Carbon::now();
                                 $return_note_data->save();
+                            }else{
+                                if ($updated_shipments_count == 0) {
+                                    $return_note_data->status = 3;
+                                    $return_note_data->updated_at = Carbon::now();
+                                    $return_note_data->save();
+                                }
                             }
                             $message = 'Shipment is marked as Undelivered Successfully';
                         } else {
