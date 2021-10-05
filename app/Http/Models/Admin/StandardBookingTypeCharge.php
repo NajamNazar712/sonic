a3090f0473b10d4bb924562efb610e049846d354
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-10-05 16:20:03 +0500

    change

--- app/Http/Models/Admin/StandardBookingTypeCharge.php
+++ app/Http/Models/Admin/StandardBookingTypeCharge.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class StandardBookingTypeCharge extends Model
 {
+    public $timestamps = FALSE;
     protected $fillable = [
         'shipping_mode_id', 'replacement_charges', 'try_and_buy_charges',
     ];
