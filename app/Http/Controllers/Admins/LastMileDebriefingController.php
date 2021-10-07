2cae2dfb43d3386c4f80e689232290faef0b0650
Taha Habib <taha.habib@trax.pk>
2021-10-07 15:29:05 +0500

    changes

--- app/Http/Controllers/Admins/LastMileDebriefingController.php
+++ app/Http/Controllers/Admins/LastMileDebriefingController.php
@@ -75,7 +75,14 @@ class LastMileDebriefingController extends Controller
         $admin_ids = AdminHub::where('hub_id',$request->hub_id)->pluck('admin_id')->toArray();
         if(count($admin_ids) > 0){
 
-            $agents = Admin::whereIn('id', $admin_ids)->where('role_id', 18)->where('status',1)->get();
+            //$agents = Admin::whereIn('id', $admin_ids)->where('role_id', 18)->where('status',1)->get();
+
+            $agents = Admin::join('employee_attendances as ea','ea.employee_id','=','admins.id')
+            ->whereIn('admins.id', $admin_ids)
+            ->where('admins.role_id', 18)
+            ->where('admins.status',1)
+            ->where('ea.clock_out_datetime','=',null)
+            ->where('ea.attendance_date','=',Carbon::now()->format('Y-m-d'))->get();
            
             return response()->json(['status' => 1, 'agents' => $agents]);
         }
--- resources/views/admin/debriefing/supervisor.blade.php
+++ resources/views/admin/debriefing/supervisor.blade.php
@@ -268,6 +268,7 @@
                         .done(function (data) {
 
                             if(data.status){
+                                $('#agent_id').empty().append('<option selected="selected" placeholder="Select Hub *" value="">text</option>');
                                 $('#agend_input').removeClass('d-none');
                                 $.each(data.agents, function (index, agent) {
                                     $('#agent_id').append('<option value="'+agent.id+'" >'+agent.name+'</option>')
