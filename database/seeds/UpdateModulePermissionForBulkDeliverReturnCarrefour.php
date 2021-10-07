faf3f73bcf7fb150fcee436e117d9874f512c977
Anas Anwer <anas.anwer@trax.pk>
2021-10-07 11:35:26 +0500

    changes

--- database/seeds/UpdateModulePermissionForBulkDeliverReturnCarrefour.php
+++ database/seeds/UpdateModulePermissionForBulkDeliverReturnCarrefour.php
@@ -11,6 +11,9 @@ class UpdateModulePermissionForBulkDeliverReturnCarrefour extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('module_permissions')->insert(array(
+            array('id' => 606, 'name' => 'Carrefour Bulk Delivery - View', 'module_id' => 6),
+            array('id' => 607, 'name' => 'Carrefour Bulk Return - View', 'module_id' => 7),
+        ));
     }
 }
