923d10a1b298901ce21d90badd5ef91f020117b4
abimwaqas <muhammad.waqas@trax.pk>
2021-10-13 19:32:25 +0500

    fix

--- app/Http/Controllers/Admins/AdminMasterCargoController.php
+++ app/Http/Controllers/Admins/AdminMasterCargoController.php
@@ -384,17 +384,7 @@ class AdminMasterCargoController extends Controller
                     $hub_id = $city_details->hub_id;
                 }
                 else {
-                    if ($shipment->shipper_status_id == 20) {
-                        if ($shipment->return_address_id != NULL) {
-                            $hub_id = $shipment->return_address->city->hub_id;
-                        }
-                        else {
-                            $hub_id = $shipment->consignee_city->hub_id;
-                        }
-                    }
-                    else {
-                        $hub_id = $shipment->consignee_city->hub_id;
-                    }
+                    $hub_id = $shipment->consignee_city->hub_id;
 
                 }
 
@@ -411,7 +401,13 @@ class AdminMasterCargoController extends Controller
                 }
 
                 if ($allowed) {
-                    if (($shipment->pickup_address->city->hub_id != $shipment->consignee_city->hub_id) || (in_array($shipment->shipper_status_id, [49, 55]) && ($shipment->consignee_city->hub_id != $hub_id)) || ($shipment->shipper_status_id == 20 && $shipment->return_address_id != NULL && $shipment->pickup_address->city->hub_id != $hub_id)) {
+                    if (($shipment->pickup_address->city->hub_id != $shipment->consignee_city->hub_id) || (in_array($shipment->shipper_status_id, [49, 55]) && ($shipment->consignee_city->hub_id != $hub_id)) || ($shipment->shipper_status_id == 20)) {
+                        if($shipment->return_address_id != NULL){
+                            if(($shipment->shipper_status_id == 20) && ($shipment->pickup_address->city->hub_id == $shipment->return_address->city->hub_id)){
+                                return ['status' => 1, 'error' => 'Given Tracking Number\'s Shipment belongs to same Origin and Destination Hub'];
+                            }
+                        }
+
                         if ($request->bag_type != 0) {
                             if (in_array($shipment->shipper_status_id, [2, 49, 55])) {
                                 $hub_id = $shipment->consignee_city->hub_id;
@@ -897,17 +893,8 @@ class AdminMasterCargoController extends Controller
                         $hub_id = $city_details->hub_id;
                     }
                     else {
-                        if(in_array($shipment->shipper_status_id, [20])){
-                            if($shipment->return_address_id != null){
-                                $hub_id = $shipment->return_address->city->hub_id;
-                            }
-                            else{
-                                $hub_id = $shipment->consignee_city->hub_id;
-                            }
-                        }
-                        else{
-                            $hub_id = $shipment->consignee_city->hub_id;
-                        }
+
+                        $hub_id = $shipment->consignee_city->hub_id;
 
                     }
 
@@ -926,7 +913,7 @@ class AdminMasterCargoController extends Controller
                     if ($allowed) {
                         if (($shipment->pickup_address->city->hub_id != $shipment->consignee_city->hub_id) || (in_array($shipment->shipper_status_id, [49, 55]) && ($shipment->consignee_city->hub_id != $hub_id)) || ($shipment->shipper_status_id == 20)) {
                             if($shipment->return_address_id != NULL){
-                                if(($shipment->shipper_status_id == 20) && ($shipment->pickup_address->city->hub_id == $hub_id)){
+                                if(($shipment->shipper_status_id == 20) && ($shipment->pickup_address->city->hub_id == $shipment->return_address->city->hub_id)){
                                     return ['status' => 1, 'error' => 'Given Tracking Number\'s Shipment belongs to same Origin and Destination Hub'];
                                 }
                             }
