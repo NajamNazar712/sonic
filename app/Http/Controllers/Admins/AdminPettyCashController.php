33032510d2479b7592234852b3f8cd45743332dc
Maaz Khan <Maaz.Khan@Trax.pk>
2021-10-21 17:41:35 +0500

    Allow Hub City in Petty Cash Cities

--- app/Http/Controllers/Admins/AdminPettyCashController.php
+++ app/Http/Controllers/Admins/AdminPettyCashController.php
@@ -90,7 +90,7 @@ class AdminPettyCashController extends Controller
     public function make_petty_cash_statement_get_cities(Request $request)
     {
         $hub_id = $request->hub;
-        $cities = City::where('hub_id',$hub_id)->where('hub',0)->where('status',1);
+        $cities = City::where('hub_id',$hub_id)->where('status',1);
         if($cities->exists())
         {
             $data = $cities->select(['id','name'])->get();
