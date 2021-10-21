b43d9e06a6fc3f8ba840ec025a9659adba6ce454
Anas Anwer <anas.anwer@trax.pk>
2021-10-21 20:35:58 +0500

    changes

--- app/Http/Controllers/Admins/AdminCargoManifestController.php
+++ app/Http/Controllers/Admins/AdminCargoManifestController.php
@@ -738,7 +738,13 @@ class AdminCargoManifestController extends Controller
                     }
 
                     if ($allowed) {
-                        if (($shipment->pickup_address->city->hub_id != $shipment->consignee_city->hub_id) || (in_array($shipment->shipper_status_id, [49, 55]) && ($shipment->consignee_city->hub_id != $hub_id)) || ($shipment->shipper_status_id == 20 && $shipment->return_address_id != NULL && $shipment->pickup_address->city->hub_id != $hub_id)) {
+                        if (($shipment->pickup_address->city->hub_id != $shipment->consignee_city->hub_id) || (in_array($shipment->shipper_status_id, [49, 55]) && ($shipment->consignee_city->hub_id != $hub_id)) || ($shipment->shipper_status_id == 20)) {
+                            if($shipment->return_address_id != NULL){
+                                if(($shipment->shipper_status_id == 20) && ($shipment->consignee_city->hub_id == $shipment->return_address->city->hub_id)){
+                                    return ['status' => 1, 'error' => 'Given Tracking Number\'s Shipment belongs to same Origin and Destination Hub'];
+                                }
+                            }
+
                             if ($request->bag_type != 0) {
                                 if (in_array($shipment->shipper_status_id, [2, 49, 55])) {
                                     $hub_id = $shipment->consignee_city->hub_id;
