d71aa09030dfb509a75f069582d172995acd15a1
Nabeel973 <nabeelsiddiqui324@gmail.com>
2021-10-21 20:34:34 +0500

    fix

--- resources/views/admin/cargo/manifest/bags/create.blade.php
+++ resources/views/admin/cargo/manifest/bags/create.blade.php
@@ -442,7 +442,7 @@
 
                            $('#cargo_consignment form input.seal_number').on('change', function () {
                                var seal = this.value;
-                               if (seal.length != 12 && seal.length != 13 && seal.length != 6 && seal.length != 7 &&  && seal.length != 14 &&  && seal.length != 15) {
+                               if (seal.length != 12 && seal.length != 13 && seal.length != 6 && seal.length != 7 &&  seal.length != 14 &&  seal.length != 15) {
                                    this.value = '';
                                }
                            });
