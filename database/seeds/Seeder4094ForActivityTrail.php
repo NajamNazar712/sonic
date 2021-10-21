e39575603fa171b65310f495383ccc3420fec134
Maaz Khan <Maaz.Khan@Trax.pk>
2021-10-21 16:02:02 +0500

    done

--- database/seeds/Seeder4094ForActivityTrail.php
+++ database/seeds/Seeder4094ForActivityTrail.php
@@ -15,5 +15,10 @@ class Seeder4094ForActivityTrail extends Seeder
             array('id' => 453, 'screen_name' => 'Employee Attendance (Horizontal)', 'action'=> 'View'),
             array('id' => 454, 'screen_name' => 'Employee Attendance (Horizontal)', 'action'=> 'Excel Download')
         ));
+
+        $timestamp = \Carbon\Carbon::now()->format('Y-m-d H:i:s');
+        DB::table('admins_screen_list')->insert(
+            array('created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Human Resource > Emploee Attendance (Horizontal)', 'url'=>'admin.attendance.horizontal.index', 'permission_id' => 465)
+        );
     }
 }
