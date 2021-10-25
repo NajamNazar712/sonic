5f9afb7416a7cfa4593810c4110b22f58099dde1
abimwaqas <muhammad.waqas@trax.pk>
2021-10-25 14:13:05 +0500

    fix

--- app/Http/Controllers/Admins/DeliveryController.php
+++ app/Http/Controllers/Admins/DeliveryController.php
@@ -4691,9 +4691,10 @@ class DeliveryController extends Controller
 
         $sdn = StationDepositNote::
             join('cities AS oc', 'station_deposit_notes.hub_id', '=', 'oc.id')
-            ->leftjoin('station_deposit_note_adjustments as sdna', function($join){
+            ->leftjoin('station_deposit_note_adjustments as sdna',function($join){
                 $join->on('sdna.sdn_id', '=','station_deposit_notes.id')
-                    ->latest('id');
+                    ->where('sdna.id', '=',
+                        DB::raw('(select max(id) from station_deposit_note_adjustments where station_deposit_note_adjustments.sdn_id = station_deposit_notes.id.id)'));
             })
             ->join('admins', 'admins.id', '=', 'station_deposit_notes.deposited_by')
             ->leftjoin('banks_lists', 'banks_lists.id', '=', 'station_deposit_notes.banks_list_id')
