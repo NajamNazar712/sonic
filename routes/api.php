ec010e93efb8de09236c80351cc28e1e05492781
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-10-27 20:05:52 +0500

    change

--- routes/api.php
+++ routes/api.php
@@ -276,7 +276,7 @@ Route::name('api.')->group(function () {
             Route::post('payslip', 'AdminAPIController@admin_payslip')->name('payslip');
 
             Route::prefix('leave')->name('leave.')->group(function () {
-                Route::get('index', 'AdminAPIController@leave_index')->name('index');
+                Route::post('index', 'AdminAPIController@leave_index')->name('index');
                 Route::post('apply', 'AdminAPIController@leave_apply')->name('apply');
                 Route::get('list', 'AdminAPIController@employee_leave_list')->name('list');
                 Route::get('approver_list', 'AdminAPIController@approver_leave_list')->name('approver_list');
