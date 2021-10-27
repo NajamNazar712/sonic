b985817cd44768b91573a64e0c7fe887917108fc
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-10-27 22:28:09 +0500

    check

--- app/Http/Controllers/AdminAPIController.php
+++ app/Http/Controllers/AdminAPIController.php
@@ -4170,6 +4170,7 @@ class AdminAPIController extends Controller
                         $shift = EmployeeShift::find('id', $user->shift_id);
                         if($employee_leaves->to) {
                             $dates = $this->generateDateRange($employee_leaves->from, $employee_leaves->to);
+                            dd($dates);
                         }else {
                             $dates[] = $employee_leaves->from;
                         }
