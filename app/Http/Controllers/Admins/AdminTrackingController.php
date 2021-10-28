edcf5f7be53dfa091ec786e1dea38ea957c4c4c2
Anas Anwer <anas.anwer@trax.pk>
2021-10-28 17:18:55 +0500

    changes

--- app/Http/Controllers/Admins/AdminTrackingController.php
+++ app/Http/Controllers/Admins/AdminTrackingController.php
@@ -1350,11 +1350,12 @@ class AdminTrackingController extends Controller
                             ->leftjoin('admins as a', 'a.id', '=', 'crm_requests.agent_id')
                             ->leftjoin('users as u', 'u.id', '=', 'crm_requests.launched_by_id')
                             ->leftjoin('retail_users as ru', 'ru.id', '=', 'crm_requests.launched_by_id')
+                            ->leftjoin('consignee_users as cu', 'cu.id', '=', 'crm_requests.launched_by_id')
                             ->leftjoin('substitute_users as su', 'su.id', '=', 'crm_requests.launched_by_id')
                             ->leftjoin('crm_request_statuses as crs', 'crs.id', '=', 'crsh.status_id')
                             ->select('crm_requests.id as id', 'crs.name as status', 'a.id as admin_id',
                             'a.name as created_by_admin', 'u.name as created_by_user', 'su.name as created_by_sub_user',
-                             'crsh.created_at as created_at', 'crsh.status_id as status_id', 'crm_requests.launched_by as launched_added_by','crm_requests.launched_by_id', 'ru.name as created_by_retail_user')
+                             'crsh.created_at as created_at', 'crsh.status_id as status_id', 'crm_requests.launched_by as launched_added_by','crm_requests.launched_by_id', 'ru.name as created_by_retail_user', 'cu.name as created_by_consignee_user')
                             ->where('crm_requests.shipment_id', $shipment->id);
 
                         if($crm_requests->exists()){
@@ -1368,16 +1369,19 @@ class AdminTrackingController extends Controller
                                 $crm_request_journey['status'] = $crm_request->status;
                                 if($crm_request->status_id == 1){
                                     if($crm_request->launched_added_by == 0){
-                                           $crm_request_journey['created_by'] = Admin::find($crm_request->launched_by_id)->name . ' (Admin)';
+                                        $crm_request_journey['created_by'] = $crm_request->created_by_admin . ' (Admin)';
                                     }
                                     else if($crm_request->launched_added_by == 1){
                                         $crm_request_journey['created_by'] = $crm_request->created_by_user . ' (Shipper)';
                                     }
+                                    else if($crm_request->launched_added_by == 2){
+                                        $crm_request_journey['created_by'] = $crm_request->created_by_sub_user . ' (Substitute Shipper)';
+                                    }
                                     else if($crm_request->launched_added_by == 3){
                                         $crm_request_journey['created_by'] = $crm_request->created_by_retail_user . ' (Retail User)';
                                     }
-                                    else{
-                                        $crm_request_journey['created_by'] = $crm_request->created_by_sub_user . ' (Substitute Shipper)';
+                                    else if($crm_request->launched_added_by == 4){
+                                        $crm_request_journey['created_by'] = $crm_request->created_by_consignee_user . ' (Consignee)';
                                     }
                                 }
                                 else{
