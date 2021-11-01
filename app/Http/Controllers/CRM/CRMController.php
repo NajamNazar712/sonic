52c04adc82718b98bf9282bb5d36da5da4a4285a
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-11-01 16:34:47 +0500

    change

--- app/Http/Controllers/CRM/CRMController.php
+++ app/Http/Controllers/CRM/CRMController.php
@@ -7,6 +7,9 @@ use App\Http\Models\Admin\GlobalSettings;
 use App\Http\Models\CRM\CrmRequest;
 use App\Http\Models\CRM\CrmRequestStatus;
 use App\Http\Models\CRM\CrmRequestStatusHistory;
+use App\Http\Models\CRM\CrmRequestTagging;
+use App\Http\Models\CRM\CrmRequestTaggingHistory;
+use App\Http\Models\SaleTierTag;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
@@ -133,6 +136,51 @@ class CRMController extends Controller
                 $crm_request_status_history->status_id = 2;
 
                 $crm_request_status_history->save();
+
+                if($crm_request->shipper_id){
+                    $sales_tier_tag = SaleTierTag::where('user_id', $crm_request->shipper_id);
+                    if($sales_tier_tag->exists()){
+                        $sales_tier_tag = $sales_tier_tag->first();
+                        $tagged_id = $sales_tier_tag->kam;
+                        if($tagged_id){
+                            $tagged_crm_request = CrmRequestTagging::where('crm_request_id', $crm_request->id)->first();
+                            if(!empty($tagged_crm_request)){
+                                if($tagged_crm_request['tagged_id'] != $tagged_id) {
+                                    CrmRequestTagging::where('crm_request_id', $crm_request->id)->update([
+                                        'crm_request_tagging_type_id' => 2,
+                                        'tagged_id' => $tagged_id
+                                    ]);
+
+                                    CrmRequestTaggingHistory::create([
+                                        'crm_request_id' => $crm_request->id,
+                                        'crm_request_tagging_type_id' => 2,
+                                        'tagged_id' => $tagged_id,
+                                        'agent_id' => $launched_by_id,
+                                        'hub_id' => NULL
+                                    ]);
+                                    NotificationsController::send(31,$crm_request->id);
+                                }
+                            }
+                            else{
+                                CrmRequestTagging::create([
+                                    'crm_request_id' => $crm_request->id,
+                                    'crm_request_tagging_type_id' => 2,
+                                    'tagged_id' => $tagged_id,
+                                    'hub_id' => NULL
+                                ]);
+
+                                CrmRequestTaggingHistory::create([
+                                    'crm_request_id' => $crm_request->id,
+                                    'crm_request_tagging_type_id' => 2,
+                                    'tagged_id' => $tagged_id,
+                                    'agent_id' => $launched_by_id,
+                                    'hub_id' => NULL
+                                ]);
+                                NotificationsController::send(31,$crm_request->id);
+                            }
+                        }
+                    }
+                }
             }
         }
 
