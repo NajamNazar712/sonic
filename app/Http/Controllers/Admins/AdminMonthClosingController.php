f3597c3494e7ed3345264d423a9d3b03a1a215ba
abimwaqas <muhammad.waqas@trax.pk>
2021-11-01 14:09:53 +0500

    fix

--- app/Http/Controllers/Admins/AdminMonthClosingController.php
+++ app/Http/Controllers/Admins/AdminMonthClosingController.php
@@ -396,7 +396,7 @@ class AdminMonthClosingController extends Controller
         $shipment_ids = $request->shipment_ids;
         $date = Carbon::now();
         if(count($shipment_ids) > 0){
-            $status_not_allowed = array(1, 5, 6, 14, 17, 25, 31, 38, 51, 53, 3, 21, 26, 32);
+            $status_not_allowed = array(1, 5, 6, 14, 17, 25, 31, 38, 51, 53);
             $intransit_status_array = array(3, 21, 26, 32);
             $return_revert_statuses = array(20, 21, 22, 23, 24, 44, 47, 48);
             $return_note_statuses = array(23, 24, 28, 29, 34, 35, 44, 45,46, 47, 48, 60);
