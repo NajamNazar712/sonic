adf3afd1ec08bfda5f22bba77e0589cee3ad130e
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-11-01 22:02:54 +0500

    change

--- database/seeds/UpdateAppNotificationForEmployeeLeavesNotification.php
+++ database/seeds/UpdateAppNotificationForEmployeeLeavesNotification.php
@@ -13,9 +13,9 @@ class UpdateAppNotificationForEmployeeLeavesNotification extends Seeder
     {
         $timestamp = \Carbon\Carbon::now()->format('Y-m-d H:i:s');
         DB::table('app_notifications')->insert(array(
-            array('id' => 11, 'name' => 'Leave Request Update - Employee', 'title' => 'Leave Request Update', 'body' => 'Your Leave Request [from]-[to] has been [status].', 'app_id' => 1,'updated_by' => 664,'created_at' => $timestamp, 'updated_at' => $timestamp),
-            array('id' => 12, 'name' => 'Leave Request Update - HOD', 'title' => 'Leave Request Update', 'body' => '[employee_name] ([trax_id]) has submit leave request [from]-[to] is awaiting for your approval.', 'app_id' => 1,'updated_by' => 664,'created_at' => $timestamp, 'updated_at' => $timestamp),
-            array('id' => 13, 'name' => 'Leave Request Update - HR', 'title' => 'Leave Request Update', 'body' => 'Leave request of [employee_name] ([trax_id]) [from]-[to] Approve by HOD, awaiting for your approval.', 'app_id' => 1,'updated_by' => 664,'created_at' => $timestamp, 'updated_at' => $timestamp),
+            array('id' => 11, 'name' => 'Leave Request Update - Employee', 'title' => 'Leave Request Update', 'body' => 'Your Leave Request [from] to [to] has been [status].', 'app_id' => 1,'updated_by' => 664,'created_at' => $timestamp, 'updated_at' => $timestamp),
+            array('id' => 12, 'name' => 'Leave Request Update - HOD', 'title' => 'Leave Request Update', 'body' => '[employee_name] ([trax_id]) has submit leave request [from] to [to] is awaiting for your approval.', 'app_id' => 1,'updated_by' => 664,'created_at' => $timestamp, 'updated_at' => $timestamp),
+            array('id' => 13, 'name' => 'Leave Request Update - HR', 'title' => 'Leave Request Update', 'body' => 'Leave request of [employee_name] ([trax_id]) [from] to [to] has Approve by HOD, awaiting for your approval.', 'app_id' => 1,'updated_by' => 664,'created_at' => $timestamp, 'updated_at' => $timestamp),
         ));
     }
 }
