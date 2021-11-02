1d20beeb230b1ee2d30df2774c27adbc69ce52ee
Muhammad Aqib <muhammad.aqib@trax.pk>
2021-11-02 12:10:10 +0500

    fix

--- app/Console/Commands/AutoAssignCrmAgent.php
+++ app/Console/Commands/AutoAssignCrmAgent.php
@@ -59,6 +59,21 @@ class AutoAssignCrmAgent extends Command
                                     ->where('crm_requests.case_nature_id','<>',3)
                                     ->get();
         foreach ($crm_requests as $value) {
+            if($value->case_nature_id == 4){
+                $agent_log = CrmAgentLog::where('case_nature_id',$value->case_nature_id)
+                ->where('assinged_requests','<',90)
+                ->whereDate('assigned_date',Carbon::today()->toDateString())
+                ->orderBy('assinged_requests', 'asc')->get()->first();
+
+               if($agent_log){
+                $agent_log->assinged_requests = $agent_log->assinged_requests+1;
+                $agent_log->save();
+                
+                $value->agent_id = $agent_log->admin_id;
+                $value->save();
+                }
+            }else{
+
                 $agent_log = CrmAgentLog::where('zone_id',$value->zone_id)
                 ->where('case_nature_id',$value->case_nature_id)
                 ->where('assinged_requests','<',90)
@@ -71,7 +86,9 @@ class AutoAssignCrmAgent extends Command
                 
                 $value->agent_id = $agent_log->admin_id;
                 $value->save();
-               }
+                }
+
+            }
 
                
         }
