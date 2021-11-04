6d84d4150ca900bd14d85ef1e792b6f96ea0077a
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-11-04 18:35:24 +0500

    Fix in AdminAttendanceController

--- app/Http/Controllers/Admins/Attendance/AdminAttendanceController.php
+++ app/Http/Controllers/Admins/Attendance/AdminAttendanceController.php
@@ -109,9 +109,13 @@ class AdminAttendanceController extends Controller
             ->select('a.name as admin_name', 'a.trax_id as trax_id', 'c.name as city_name', 'c.id as city_id', 'a.designation as designation', 'r.name as rider_name', 'r.trax_id as rider_trax_id', 'rc.name as rider_city_name', 'rc.id as rider_city_id', 'rt.name as rider_type', 'rt.id as rider_type_id', 'employee_attendances.attendance_date as attendance_date', 'employee_attendances.clock_in as clock_in', 'employee_attendances.clock_out as clock_out', 'employee_attendances.clock_in_latitude as clock_in_latitude', 'employee_attendances.clock_in_longitude as clock_in_longitude', 'employee_attendances.clock_out_latitude', 'employee_attendances.clock_out_longitude', 'ad.name as department', 'ad.id as department_id', 'employee_attendances.employee_type', 'employee_attendances.clock_in_location as clock_in_status', 'employee_attendances.clock_out_location as clock_out_status', 'r.cnic as rider_cnic', 'a.cnic as admin_cnic', 'employee_attendances.clock_in_datetime as clock_in_datetime', 'employee_attendances.clock_out_datetime as clock_out_datetime', 'aes.name as admin_shift', 'res.name as rider_shift');
 
         if(session('role_id') != 1 && session('role_id') != 63 && session('role_id') != 70){
-            $attendances->where('ad.id', session('department_id'));
             if(session('department_id') != 6){
-                $attendances->where('employee_attendances.employee_type', 1);
+                $attendances->where('employee_attendances.employee_type', 1)
+                    ->where('ad.id', session('department_id'));
+            }
+            else{
+                $attendances->where('employee_attendances.employee_type', 2)
+                    ->orWhere('ad.id', session('department_id'));
             }
             $attendances = $attendances->whereIn('c.hub_id', session('hubs'));
         }
@@ -317,9 +321,13 @@ class AdminAttendanceController extends Controller
             ->select('employee_attendances.employee_id','a.name as admin_name', 'a.trax_id as trax_id', 'a.designation as designation','ed.name as designation_name', 'r.name as rider_name', 'r.trax_id as rider_trax_id', 'rt.name as rider_type', 'rt.id as rider_type_id', 'ad.name as department', 'ad.id as department_id', 'employee_attendances.employee_type');
 
         if(session('role_id') != 1 && session('role_id') != 63 && session('role_id') != 70){
-            $attendances->where('ad.id', session('department_id'));
             if(session('department_id') != 6){
-                $attendances->where('employee_attendances.employee_type', 1);
+                $attendances->where('employee_attendances.employee_type', 1)
+                    ->where('ad.id', session('department_id'));
+            }
+            else{
+                $attendances->where('employee_attendances.employee_type', 2)
+                    ->orWhere('ad.id', session('department_id'));
             }
             $attendances = $attendances->whereIn('c.hub_id', session('hubs'));
         }
