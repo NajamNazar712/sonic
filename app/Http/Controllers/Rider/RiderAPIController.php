fadfdd90e71ff020797c82ff954bf4fdbef69e3c
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-11-04 16:03:21 +0500

    Changes

--- app/Http/Controllers/Rider/RiderAPIController.php
+++ app/Http/Controllers/Rider/RiderAPIController.php
@@ -4441,7 +4441,7 @@ class RiderAPIController extends Controller
         $banks = BanksList::select('id', 'name')->where('status', 1)->get();
         $rider_type = RiderType::select('id', 'name')->get();
         $staff_categories = StaffCategory::select('id', 'name')->get();
-        $shifts = EmployeeShift::where('name', '!=', 'Default')->select('id', 'name', 'start_time', 'end_time')->get();
+        $shifts = EmployeeShift::select('id', 'name', 'start_time', 'end_time')->get();
         $shift_data = array();
         foreach($shifts as $shift){
             $datum = array();
