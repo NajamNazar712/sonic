a9cec053ea673ba097d3a3a3109a208c58cc040b
Taha Habib <taha.habib@trax.pk>
2021-11-04 13:54:35 +0500

    changes

--- database/seeds/UpdateScreenListforSupport.php
+++ database/seeds/UpdateScreenListforSupport.php
@@ -12,13 +12,13 @@ class UpdateScreenListforSupport extends Seeder
      */
     public function run()
     {
-        AdminsScreenList::where('id', 85)->delete();
-        AdminsScreenList::where('id', 86)->delete();
-        
-        $timestamp = \Carbon\Carbon::now()->format('Y-m-d H:i:s');
-        DB::table('admins_screen_list')->insert(array(
-            array('created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Support  >  Order Management', 'url'=>'admin.orders.index', 'permission_id' => 621),
-            array('created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Support  >  Self Collection Shipments', 'url'=>'admin.orders.self_collection.index', 'permission_id' => 622),
-        ));
+        //For Support > Order Management 
+        $orderid=AdminsScreenList::where('id', 84)->first();
+        $orderid->permission_id=621;
+        $orderid->save();
+        //For Support > Self Collection Shipments 
+        $selfcollectionid=AdminsScreenList::where('id', 86)->first();
+        $selfcollectionid->permission_id=622;
+        $selfcollectionid->save();
     }
 }
