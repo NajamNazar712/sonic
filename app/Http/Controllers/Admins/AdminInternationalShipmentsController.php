2f862c1becbdc62d2425d0899f90a7ee2ae0f692
Taha Habib <taha.habib@trax.pk>
2021-11-09 01:35:42 +0500

    changes

--- app/Http/Controllers/Admins/AdminInternationalShipmentsController.php
+++ app/Http/Controllers/Admins/AdminInternationalShipmentsController.php
@@ -116,7 +116,7 @@ class AdminInternationalShipmentsController extends Controller
             })],
             'international_tracking_number' => ['required'],
             'actual_weight' => ['nullable', 'numeric', 'between:0.1,100000'],
-            'service_provider_id' => ['required', 'numeric', 'between:1,6', 'exists:international_shipment_service_providers,id']
+            'service_provider_id' => ['required', 'numeric', 'between:1,7', 'exists:international_shipment_service_providers,id']
         ];
 
 
