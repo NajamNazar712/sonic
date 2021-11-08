049844d97cc3e14401b739b818d7d2186cf2227e
Taha Habib <taha.habib@trax.pk>
2021-11-09 01:34:18 +0500

    changesrevert

--- database/seeds/InternationalShipmentServiceProviderSeeder.php
+++ database/seeds/InternationalShipmentServiceProviderSeeder.php
@@ -19,7 +19,6 @@ class InternationalShipmentServiceProviderSeeder extends Seeder
             array('id' => 4, 'name'=>'Aramex', 'code' => 'T004'),
             array('id' => 5, 'name'=>'SkyNet', 'code' => 'T005'),
             array('id' => 6, 'name'=>'Nice Express AE', 'code' => 'T006'),
-            array('id' => 7, 'name'=>'Others', 'code' => 'T007'),
 
         ));
     }
