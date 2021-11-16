430f1d6b1bf2b0ef916506d8951097fa6744c362
Nabeel973 <nabeelsiddiqui324@gmail.com>
2021-11-16 12:43:36 +0500

    fix

--- app/Http/Controllers/Admins/AdminCargoManifestController.php
+++ app/Http/Controllers/Admins/AdminCargoManifestController.php
@@ -994,6 +994,7 @@ class AdminCargoManifestController extends Controller
             $bag->type = $request->input('bag_type');
             $bag->transport_mode_id = 2;
             $bag->status_id = 1;
+            $bag->completed = 0;
             $bag->save();
           
             CargoManifestBagJourneyController::add($bag->id,$bag->seal_number, $bag->status_id, Auth::id(), NULL, NULL);
@@ -1942,7 +1943,13 @@ class AdminCargoManifestController extends Controller
              ->join('transport_modes as tm', 'cargo_manifest_bags.transport_mode_id', '=', 'tm.id')
              ->join('cargo_manifest_bag_statuses as bs', 'cargo_manifest_bags.status_id', '=', 'bs.id')
              ->select('cargo_manifest_bags.id', 'cargo_manifest_bags.status_id as status_id', 'oh.id as origin_id', 'oh.name as origin', 'dh.id as destination_id', 'dh.name as destination', 'cargo_manifest_bags.shipments', 'cargo_manifest_bags.quantity','tm.name as transport_mode','cargo_manifest_bags.shipments_weight', 'cargo_manifest_bags.actual_weight', 'cargo_manifest_bags.shipments as bag_shipments', 'a.name as transitted_by','oh.hub_id as origin_hub_id', 'dh.hub_id as destination_hub_id', 'cargo_manifest_bags.type as bag_type','cargo_manifest_bags.seal_number','bs.name as status','sm.mode as shipping_mode','cm.id as manifest_id','cargo_manifest_bags.seal_number','cm.created_at as manifest_created_at','cargo_manifest_bags.origin_hub_id as origin_hub_id','cargo_manifest_bags.destination_hub_id as destination_hub_id','cm.id as manifest','ah.name as updated_by','cargo_manifest_bags.created_at as transitted_date','cargo_manifest_bags.short_received_shipments as short_received_shipments','cargo_manifest_bags.short_received_shipments as short_shipments','cargo_manifest_bags.received_shipments','cmbj.created_at as status_updated_at','status_editor.name as status_updated_by','sedh.name as status_location')
-            ->where('cargo_manifest_bags.shipments','!=',DB::raw('(select(received_shipments) from cargo_manifest_bags as cmb where cmb.id =cargo_manifest_bags.id)'));
+         /*   ->where('cargo_manifest_bags.shipments','!=',DB::raw('(select(received_shipments) from cargo_manifest_bags as cmb where cmb.id =cargo_manifest_bags.id)'))
+            ->orWhere('cargo_manifest_bags.completed',0);*/
+         ->where(function ($query) {
+             $query->where('cargo_manifest_bags.shipments','!=',DB::raw('(select(received_shipments) from cargo_manifest_bags as cmb where cmb.id =cargo_manifest_bags.id)'))
+                     ->orWhere('cargo_manifest_bags.completed', 0);
+                     });
+
 
 
         $datatables = Datatables::of($bags)
@@ -2823,6 +2830,7 @@ class AdminCargoManifestController extends Controller
             else {
                 $bag->short_received_shipments = 0;
                 $status_id = 7;
+                $bag->completed = 1;
             }
             $bag->status_id = $status_id;
             $bag->received_at = Carbon::now();
