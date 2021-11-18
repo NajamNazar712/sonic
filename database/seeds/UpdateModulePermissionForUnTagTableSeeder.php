cab6e9f47560de80ad29fcd39e8b3cc5ad2e5881
Muhammad Aqib <muhammad.aqib@trax.pk>
2021-11-18 15:52:07 +0500

    changes

--- database/seeds/UpdateModulePermissionForUnTagTableSeeder.php
+++ database/seeds/UpdateModulePermissionForUnTagTableSeeder.php
@@ -11,7 +11,7 @@ class UpdateModulePermissionForUnTagTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('module_permissions')->where('module_id',309)->delete();
+        DB::table('module_permissions')->where('id',309)->delete();
         
         DB::table('module_permissions')->insert(array(
             array('id' => 309, 'name' => 'Tag / Un Tag', 'module_id' => 18),
