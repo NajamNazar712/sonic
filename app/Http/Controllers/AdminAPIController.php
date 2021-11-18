cfe3fc14adbceeaf8b884182cf698addb38b417d
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-11-18 13:14:50 +0500

    change

--- app/Http/Controllers/AdminAPIController.php
+++ app/Http/Controllers/AdminAPIController.php
@@ -4507,6 +4507,7 @@ class AdminAPIController extends Controller
                 ->whereMonth('payroll_month', Carbon::parse($request->date)->format("m"))
                 ->whereYear('payroll_month', Carbon::parse($request->date)->format("Y"));
             if ($payslip) {
+                $payslip_obj = $payslip->get();
                 $payslip = $payslip->first();
                 $payroll_month = Carbon::parse($payslip->payroll_month)->format('F Y');
                 $payroll_cut_off_date = Carbon::parse($payslip->payroll_cut_off_date)->toDateString();
@@ -4863,7 +4864,6 @@ class AdminAPIController extends Controller
                     $payslip_pdf->save();
                     $file_url = $payslip_pdf->file_path;
                 }
-                $payslip_obj = $payslip->get();
                 return response()->json(['status' => 0, 'payroll_month' => $payroll_month, 'data' => $payslip_obj, 'file_url' => $file_url]);
             } else {
                 return response()->json(['status' => 1, 'message' => "Payslip not found"]);
