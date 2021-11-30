9627056d88f3d1c786a3035da7c590ee3d352573
abimwaqas <muhammad.waqas@trax.pk>
2021-11-30 16:19:15 +0500

    fix

--- resources/views/admin/tracking.blade.php
+++ resources/views/admin/tracking.blade.php
@@ -1472,7 +1472,6 @@
             $('#tracking').on('click','.intercept', function () {
                 id = $(this).attr('id');
                 status_id = $(this).attr('data-tracking');
-                debugger;
                 if(id != ''){
                     var redirect = '{!! route('admin.intercept.index', ':id') !!}';
                     if(status_id==12 ||status_id==52)
