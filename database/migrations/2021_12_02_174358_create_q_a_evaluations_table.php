68b4847e49a73db8da068cffacfe70d3acda94df
Muhammad Aqib <muhammad.aqib@trax.pk>
2021-12-07 13:13:09 +0500

    fix

---
+++ database/migrations/2021_12_02_174358_create_q_a_evaluations_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQAEvaluationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('q_a_evaluations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('agent_id')->index();
+            $table->integer('campaign_id')->index();
+            $table->integer('evaluated_by')->index();
+            $table->string('evaluation_date');
+            $table->integer('nature_id')->index();
+            $table->string('call_duration')->nullable();
+            $table->date('date_time')->nullable();
+            $table->integer('query_by')->index();
+            $table->string('caller_contact');
+            $table->tinyInteger('status')->default(0);
+            $table->integer('score')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('q_a_evaluations');
+    }
+}
---
+++ database/seeds/EvaluationCampaignNatureSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class EvaluationCampaignNatureSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('evaluation_campaigns')->insert(array(
+            array('id' => 1, 'campaign_id' => 1, 'campaign' => 'Incoming'),
+            array('id' => 2, 'campaign_id' => 1, 'campaign' => 'RCP'),
+            array('id' => 3, 'campaign_id' => 2, 'campaign' => 'Complains'),
+            array('id' => 4, 'campaign_id' => 2, 'campaign' => 'Claim'),
+            array('id' => 5, 'campaign_id' => 3, 'campaign' => 'Email Live Chat'),
+        ));
+
+        DB::table('evaluation_natures')->insert(array(
+            array('id' => 1, 'nature' => 'Complain'),
+            array('id' => 2, 'nature' => 'Inquiry'),
+            array('id' => 3, 'nature' => 'Lead'),
+            array('id' => 4, 'nature' => 'Followup'),
+            array('id' => 5, 'nature' => 'Feedback'),
+            array('id' => 6, 'nature' => 'Service Request'),
+        ));
+    }
+}
