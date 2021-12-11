59aa3cbcfe4d08f7a7f7a853c38c3c2c6f394ad2
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2021-12-11 11:30:16 +0500

    SMS Timeout Increase
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- app/Jobs/ProcessOTPSMSITS.php
+++ app/Jobs/ProcessOTPSMSITS.php
@@ -57,7 +57,7 @@ class ProcessOTPSMSITS implements ShouldQueue
 
     private function its($sms, $name, $otp) {
         try {
-            $client = new Client(['base_uri' => 'https://gateway.its.com.pk/api/otp', 'http_errors' => FALSE, 'connect_timeout' => 60, 'timeout' => 60]);
+            $client = new Client(['base_uri' => 'https://gateway.its.com.pk/api/otp', 'http_errors' => FALSE, 'connect_timeout' => 120, 'timeout' => 120]);
 
             $response = $client->get('', [
                 'query' => [
