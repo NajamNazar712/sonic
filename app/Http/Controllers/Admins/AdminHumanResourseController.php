9b9a3da46f127f2511ce6f331967371e005fbb40
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2021-12-13 13:56:55 +0500

    change

--- app/Http/Controllers/Admins/AdminHumanResourseController.php
+++ app/Http/Controllers/Admins/AdminHumanResourseController.php
@@ -972,6 +972,7 @@ class AdminHumanResourseController extends Controller
 
     public function employee_directory_profile_update(Employee $employee, Request $request)
     {
+        dd($request);
 
 //        return $request;
         $request->validate([
@@ -1001,7 +1002,7 @@ class AdminHumanResourseController extends Controller
         $employee->cnic_expiry_date = $request->cnic_expiry_date_formatted;
         $employee->designation_id = $request->designation;
         $employee->city_id = $request->city;
-        $employee->department_id = $request->department;
+        $employee->department_id = ($request->has('department')) ? $request->department : 6;
         $employee->zone_id = $request->zone;
         $employee->official_email = $request->official_email;
         $employee->official_phone_number = $request->official_number;
