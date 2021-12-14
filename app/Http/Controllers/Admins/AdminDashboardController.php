61a406e2ae026b5c85fa4d97733ee58a50283795
Muhammad Aqib <muhammad.aqib@trax.pk>
2021-12-14 18:38:04 +0500

    changes

--- app/Http/Controllers/Admins/AdminDashboardController.php
+++ app/Http/Controllers/Admins/AdminDashboardController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Admins;
 
-use App\Http\Controller\Admins\DwsWeightChargesController;
+use App\Http\Controllers\Admins\DwsWeightChargesController;
 use App\Http\Controllers\Admins\ActivityTrailController;
 use App\Http\Controllers\NotificationsController;
 use App\Http\Controllers\ShipmentsJourneyController;
