325f47c3f9fef15ee53e15fbd13a814e86c0b390
Maaz Khan <Maaz.Khan@Trax.pk>
2021-12-20 15:59:44 +0500

    department added in usermanagement dropdown

--- app/Http/Models/HR/EmployeeDesignation.php
+++ app/Http/Models/HR/EmployeeDesignation.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Models\HR;
 
+use App\Http\Models\Admin\AdminDepartment;
 use Illuminate\Database\Eloquent\Model;
 
 class EmployeeDesignation extends Model
@@ -9,5 +10,10 @@ class EmployeeDesignation extends Model
     public function hubs()
     {
         return $this->hasMany(EmployeeDesignationHub::class,'designation_id');
+    }
+
+    public function department()
+    {
+        return $this->belongsTo(AdminDepartment::class,'department_id');
     }
 }
