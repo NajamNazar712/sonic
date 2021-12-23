8b0bc1c01efbcee62e7771c2d869243a2b67e5cf
Muhammad Aqib <muhammad.aqib@trax.pk>
2021-12-23 15:47:00 +0500

    changes

--- app/Http/Controllers/Admins/QAEvaluationController.php
+++ app/Http/Controllers/Admins/QAEvaluationController.php
@@ -283,8 +283,9 @@ class QAEvaluationController extends Controller
         $handlings = EvaluationHandling::where('campaign_id',$campaign__id)->pluck('id')->toArray();
         $activity_score = EvaluationActivity::whereIn('evaluation_handling_id',$handlings)->whereNotIn('id',$activities_id)->sum('weightage');
         if($score+$activity_score != 100){
+            $total_score = $score+$activity_score;
             return redirect()->back()->with('error', 'Cannot Update Activities ! <br>
-            Current Weightage is "'.$score+$activity_score.'". Weightage should not be greater than 100.');
+            Current Weightage is "'.$total_score.'". Weightage should not be greater than 100.');
         }else{
             foreach ($activities_id as $key => $value) {
                 if($value == 0){
