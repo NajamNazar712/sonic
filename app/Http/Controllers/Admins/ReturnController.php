0aeb425cba14667113d1211b6dea2a053c93aa4d
Taha Habib <taha.habib@trax.pk>
2021-12-30 22:19:18 +0500

    changes

--- app/Http/Controllers/Admins/ReturnController.php
+++ app/Http/Controllers/Admins/ReturnController.php
@@ -4431,9 +4431,10 @@ class ReturnController extends Controller
                 $total_assigning = ReturnAssignedShipments::join('return_assigned_shipment_logs as rasl','rasl.return_assign_shipment_id','=','return_assigned_shipments.id')
                 ->where('return_assigned_shipments.admin_id',$agent_productivity->agent_id)
                 ->where('rasl.status',0)
-                ->whereDate('rasl.created_at',$agent_productivity->current_date)->count();
+                ->whereDate('rasl.created_at',$agent_productivity->current_date)
+                ->select('return_assigned_shipments.shipment_id as shipment')->distinct()->count('return_assigned_shipments.shipment_id');
 
-
+                
                 return $total_assigning;
 
             })
