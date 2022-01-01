7e00943762793ad189f4a7c825c04999df15241f
Taha Habib <taha.habib@trax.pk>
2022-01-02 02:17:26 +0500

    changes

--- app/Http/Controllers/Admins/V2Pickup/V2AdminPickupsController.php
+++ app/Http/Controllers/Admins/V2Pickup/V2AdminPickupsController.php
@@ -2944,7 +2944,12 @@ class V2AdminPickupsController extends Controller
     {
         ActivityTrailController::createActivityTrailLog(Auth::id(), 10);
         // $users = User::join('user_shipping_infos as usi','usi.user_id','=','users.id')->select('users.id','pickup_address','users.name','usi.id as address_id')->where('usi.status',1)->get();
-        $users = User::select(['id', 'name'])->get();
+        if (session('role_id') != 1) {
+            $users = User::join('cities as c','c.id','=','users.city_id')->whereIn('c.hub_id',session('hubs'))->select(['users.id', 'users.name'])->get();
+        }
+        else{
+            $users = User::select(['id', 'name'])->get();
+        }
         $cities = City::where('business_category_id', 1)->select(['id', 'name'])->get();
         $riders = Rider::where('status', 1)->select(['id', 'name'])->get();
         return view('admin.v2_pickups.pickup_route')->with(['cities' => $cities, 'riders' => $riders, 'users' => $users]);
