e33b77c1e5cd25ddd6edf0a00e96cfdb0e9e5058
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-01-05 15:50:54 +0500

    change

--- database/seeds/UpdateNotificationForShipmentOtpToConsignee.php
+++ database/seeds/UpdateNotificationForShipmentOtpToConsignee.php
@@ -11,10 +11,10 @@ class UpdateNotificationForShipmentOtpToConsignee extends Seeder
      */
     public function run()
     {
-        $timestamp = Carbon::now()->format('Y-m-d H:i:s');
+        $timestamp = \Carbon\Carbon::now()->format('Y-m-d H:i:s');
 
         DB::table('notifications')->insert(array(
-            array('id' => 165, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Shipment Refusal Otp', 'type_id' => 2, 'subject' => null, 'body' => 'Dear customer' . PHP_EOL . 'Trax Rider is on the way.' . PHP_EOL . 'Please Keep your CNIC ready.' . PHP_EOL . 'For verification purpose and record keeping, rider will ask for your CNIC' . PHP_EOL . 'Thank you' . PHP_EOL . 'AWB#[tracking_number].' . PHP_EOL . 'Rider:[rider]' . PHP_EOL . '021-111-118-729', 'updated_by' => 7, 'status' => 1),
+            array('id' => 165, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Shipment Refusal Otp', 'type_id' => 2, 'subject' => null, 'body' => 'Dear [consignee]' . PHP_EOL . 'Please inform the OTP [otp] to rider for the refusal of Shipment [tracking_no].', 'updated_by' => 7, 'status' => 1),
         ));
     }
 }
