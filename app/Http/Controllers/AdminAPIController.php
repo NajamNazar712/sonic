22fccf52b07be4d5294cd9584302cb3e5af0119e
Muhammad Waqas <muhammad.waqas@trax.pk>
2022-01-06 15:53:50 +0500

    fix

--- app/Http/Controllers/AdminAPIController.php
+++ app/Http/Controllers/AdminAPIController.php
@@ -5095,7 +5095,7 @@ class AdminAPIController extends Controller
                         $pickup_request_received_shipment->pickup_request_id = $pickup_request_id;
                         $pickup_request_received_shipment->shipment_id = $shipment->id;
                         $pickup_note_id = NULL;
-                        $pickup_rider_id = NULL;
+
                         $pickup_note_request = V2PickupNoteRequest::where('pickup_request_id', $pickup_request_id)->latest()->first();
                         if ($pickup_note_request) {
                             $pickup_note_id = $pickup_note_request->pickup_note_id;
