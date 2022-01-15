2b0af766935b2ea4cf0022753bd025e81446a742
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-01-15 16:40:15 +0500

    changes

--- app/Http/Models/HR/Employee.php
+++ app/Http/Models/HR/Employee.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Models\HR;
 
+use App\Http\Models\Admin\Admin;
 use App\Http\Models\Rider;
 use App\Http\Models\Rider\RiderRequest;
 use Illuminate\Database\Eloquent\Model;
@@ -54,6 +55,11 @@ class Employee extends Model
     public function rider()
     {
         return $this->belongsTo(Rider::class,'trax_id','trax_id');
+    }
+
+    public function admin()
+    {
+        return $this->belongsTo(Admin::class,'trax_id','trax_id');
     }
 
 }
