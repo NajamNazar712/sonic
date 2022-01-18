1b7bb0c061e0fe3082439ee323630c6993593c04
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-01-18 12:39:36 +0500

    changes

--- app/Http/Controllers/Admins/Attendance/AdminAttendanceController.php
+++ app/Http/Controllers/Admins/Attendance/AdminAttendanceController.php
@@ -1021,7 +1021,8 @@ class AdminAttendanceController extends Controller
                     }
                     if($employee_attendance->clock_out_datetime){
                         $clock_out = Carbon::parse($employee_attendance->clock_out_datetime)->format("H:i:s");
-                        $time_diff_out = Carbon::parse($clock_out)->diffInMinutes(Carbon::parse($shift->end_time), false);
+                        $out_interval = Carbon::parse($clock_out)->diff(Carbon::parse($shift->end_time));
+                        $time_diff_out = $out_interval->format('%r%a');
                         $working_hours = Carbon::parse($employee_attendance->clock_out_datetime)->diff(Carbon::parse($employee_attendance->clock_in_datetime))->format('%H:%I:%S');
                         $time_out = Carbon::parse($employee_attendance->clock_out_datetime)->format("H:i:s");
                         $date_out = Carbon::parse($employee_attendance->clock_out_datetime)->format("Y-m-d");
