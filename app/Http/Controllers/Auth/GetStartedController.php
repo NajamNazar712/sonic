e8543b56b8650db29867e0f221c199d29817a574
Muhammad Waqas <muhammad.waqas@trax.pk>
2022-01-20 16:22:11 +0500

    fix

--- app/Http/Controllers/Auth/GetStartedController.php
+++ app/Http/Controllers/Auth/GetStartedController.php
@@ -24,6 +24,13 @@ class GetStartedController extends Controller
     }
 
     public function getstarted_submit(Request $request){
+
+        $email = $request->email;
+        $phone = $request->phone;
+        if(Lead::where(['phone_number' => $phone, 'email_address' => $email])->exists()){
+            return redirect()->back()->with('error', 'Duplicate entry - You have already provided this Number or Email previously. Kindly fill new details. Thanks');
+        }
+
         $max_lead_id = Lead::max('lead_id');
         $max_lead_id = $max_lead_id + 1;
         $new_lead = new Lead();
@@ -32,8 +39,8 @@ class GetStartedController extends Controller
         $new_lead->city_id = $request->city;
         $new_lead->territory_id = $request->territory;
         $new_lead->territory_area_id = $request->area;
-        $new_lead->phone_number = $request->phone;
-        $new_lead->email_address = $request->email;
+        $new_lead->phone_number = $phone;
+        $new_lead->email_address = $email;
         $new_lead->requested_date = Carbon::now();
         $new_lead->message = $request->message;
         $new_lead->reference_id = $request->reference;
