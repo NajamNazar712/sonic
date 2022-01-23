f70931d7ad3d6a0da46e59cfabe45ee13a125afe
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-01-23 17:18:07 +0500

    changes

--- app/Http/Controllers/Admins/Attendance/AdminAttendanceController.php
+++ app/Http/Controllers/Admins/Attendance/AdminAttendanceController.php
@@ -1027,6 +1027,7 @@ class AdminAttendanceController extends Controller
                     if ($shift){
                         $clock_in = Carbon::parse($employee_attendance->clock_in_datetime)->format("H:i:s");
                         $time_diff = Carbon::parse($clock_in)->diffInMinutes(Carbon::parse($shift->start_time), false);
+                        dd($time_diff);
                         if ($time_diff > $shift->grace_time) {
                             $remarks = 'Late';
                             $late++;
