3612eb4fb578cf10b5d827eb0b0756257ec31e6f
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-01-25 14:51:14 +0500

    changes

--- app/Http/Controllers/AdminAPIController.php
+++ app/Http/Controllers/AdminAPIController.php
@@ -5933,20 +5933,63 @@ class AdminAPIController extends Controller
     }
 
     public function add_remarks(Request $request){
-        $lead_id = $request->lead_id;
+        $rules = [
+            'remarks' => ['required', 'max:500'],
+            'lead_id' => ['required', 'integer', 'digits_between:1,10', 'exists:leads,id'],
+        ];
+
         $admin_id = $request->admin_id;
-        $lead = Lead::find($lead_id);
-        $remarks = $request->remarks;
-        if($remarks != NULL){
-            $lead_remarks = new LeadRemark();
-            $lead_remarks->lead_id = $lead->id;
-            $lead_remarks->remarks = $remarks;
-            $lead_remarks->updated_by = $admin_id;
-            $lead_remarks->save();
-            return response()->json(['status' => 0, 'message' => 'Remarks added Successfully!']);
+        $validate = Validator::make($request->all(), $rules, $this->messages);
+
+        $validate->setAttributeNames($this->names);
+
+        if ($validate->fails()) {
+            return response()->json(['status' => 1, 'message' => 'Error(s) in Input', 'errors' => $validate->errors()]);
+        } else {
+            $lead_id = $request->lead_id;
+            $lead = Lead::find($lead_id);
+            $remarks = $request->remarks;
+            if($remarks != NULL){
+                $lead_remarks = new LeadRemark();
+                $lead_remarks->lead_id = $lead->id;
+                $lead_remarks->remarks = $remarks;
+                $lead_remarks->updated_by = $admin_id;
+                $lead_remarks->save();
+                return response()->json(['status' => 0, 'message' => 'Remarks added Successfully!']);
+            }
+            else{
+                return response()->json(['status' => 1, 'message' => 'Invalid Remarks!']);
+            }
         }
-        else{
-            return response()->json(['status' => 1, 'message' => 'Invalid Remarks!']);
+    }
+
+    public function view_remarks(Request $request){
+        $rules = [
+            'lead_id' => ['required', 'integer', 'digits_between:1,10', 'exists:leads,id'],
+        ];
+
+        $admin_id = $request->admin_id;
+        $validate = Validator::make($request->all(), $rules, $this->messages);
+
+        $validate->setAttributeNames($this->names);
+
+        if ($validate->fails()) {
+            return response()->json(['status' => 1, 'message' => 'Error(s) in Input', 'errors' => $validate->errors()]);
+        } else {
+            $lead_id = $request->lead_id;
+            $lead = Lead::find($lead_id);
+            $remarks = $request->remarks;
+            if($remarks != NULL){
+                $lead_remarks = new LeadRemark();
+                $lead_remarks->lead_id = $lead->id;
+                $lead_remarks->remarks = $remarks;
+                $lead_remarks->updated_by = $admin_id;
+                $lead_remarks->save();
+                return response()->json(['status' => 0, 'message' => 'Remarks added Successfully!']);
+            }
+            else{
+                return response()->json(['status' => 1, 'message' => 'Invalid Remarks!']);
+            }
         }
     }
 
--- routes/api.php
+++ routes/api.php
@@ -329,6 +329,7 @@ Route::name('api.')->group(function () {
             Route::prefix('leads')->name('leads.')->group(function () {
                 Route::get('list', 'AdminAPIController@leads_list')->name('list');
                 Route::post('add_remarks', 'AdminAPIController@add_remarks')->name('add_remarks');
+                Route::post('view_remarks', 'AdminAPIController@view_remarks')->name('view_remarks');
             });
 
         });
