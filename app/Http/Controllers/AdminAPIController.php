4ffdc5b5cf50066d10259635f9e7a91e89315c15
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-01-25 13:41:23 +0500

    leads work

--- app/Http/Controllers/AdminAPIController.php
+++ app/Http/Controllers/AdminAPIController.php
@@ -5332,7 +5332,9 @@ class AdminAPIController extends Controller
                     $information['phone'] = $user->phone_number;
                     $information['cnic'] = $user->cnic;
                     $information['cargo_user'] = (in_array($user->role_id,[11,10, 15, 55, 23, 33, 46])) ? 1 : 0;
-                    $information['sales_person'] = (in_array($user->role_id,[1])) ? 1 : 0;
+                    /*$user_department = $user->Edesignation->department_id;
+                    $information['sales_person'] = ($user_department == 7) ? 1 : 0;*/
+                    $information['sales_person'] = 1;
                     if ($employee->exists()) {
                         $employee = $employee->first();
                         $information['address'] = ($employee->address) ? $employee->address : "" ;
