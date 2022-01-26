e4898030e3191fddbc2ca664756b44f5530c7732
Maaz Khan <Maaz.Khan@Trax.pk>
2022-01-26 20:22:28 +0500

    done

---
+++ database/migrations/2022_01_26_200411_create_pam_lead_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePamLeadItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pam_lead_items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('lead_id')->index();
+            $table->string('item');
+            $table->integer('quantity');
+            $table->float('length');
+            $table->float('width');
+            $table->float('height');
+            $table->float('weight');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pam_lead_items');
+    }
+}
