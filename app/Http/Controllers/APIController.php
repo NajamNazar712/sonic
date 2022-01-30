3853d0af020adce3ffae5e53aa345379f02d492d
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2022-01-30 23:44:07 +0500

    Eager Loading
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -1869,7 +1869,7 @@ class APIController extends Controller
         $cities = City::where('status', 1);
 
         if ($cities->exists()) {
-            $cities = $cities->get();
+            $cities = $cities->with(['hub_city', 'zone', 'deliveries'])->get();
 
             $details = array();
 
