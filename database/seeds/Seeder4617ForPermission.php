8d12e807abd2e46762e72273d4553a63cbef678b
Maaz Khan <Maaz.Khan@Trax.pk>
2022-01-31 14:42:30 +0500

    done

--- database/seeds/Seeder4617ForPermission.php
+++ database/seeds/Seeder4617ForPermission.php
@@ -12,16 +12,16 @@ class Seeder4617ForPermission extends Seeder
     public function run()
     {
         DB::table('module_permissions')->insert(array(
-            array('id' => 669, 'name' => 'Packers & Movers Lead - View', 'module_id' => 25),
+            array('id' => 669, 'name' => 'Movit Lead - View', 'module_id' => 25),
         ));
 
         DB::table('activity_trail_actions')->insert(array(
-            array('id' => 489, 'screen_name' => 'Packers & Movers Lead', 'action'=> 'View'),
-            array('id' => 490, 'screen_name' => 'Packers & Movers Lead', 'action'=> 'Excel Download'),
+            array('id' => 489, 'screen_name' => 'Movit Lead', 'action'=> 'View'),
+            array('id' => 490, 'screen_name' => 'Movit Lead', 'action'=> 'Excel Download'),
         ));
 
         $timestamp = \Carbon\Carbon::now()->format('Y-m-d H:i:s');
         DB::table('admins_screen_list')->insert(
-            array('created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Packers & Movers Lead', 'url'=>'admin.pam_leads.index', 'permission_id' => 669));
+            array('created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Movit Lead', 'url'=>'admin.pam_leads.index', 'permission_id' => 669));
     }
 }
