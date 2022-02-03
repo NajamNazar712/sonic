8cd546d1ffcf44f1c79f9889748be5f92be652dc
Muhammad Aqib <muhammad.aqib@trax.pk>
2022-02-03 11:39:28 +0500

    seeder_fixed

--- database/seeds/UpdateActivityTrailForLeadManagement.php
+++ database/seeds/UpdateActivityTrailForLeadManagement.php
@@ -21,11 +21,11 @@ class UpdateActivityTrailForLeadManagement extends Seeder
         ));
 
         $timestamp = \Carbon\Carbon::now()->format('Y-m-d H:i:s');
-        DB::table('admins_screen_list')->insert( 
+        DB::table('admins_screen_list')->insert(array(
             array('created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Settings > Leads Management > Lead Auto Tagging', 'url'=>'admin.settings.lead_tagging.index', 'permission_id' => 661),
             array('created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Settings > Leads Management > Lead Zone Tagging', 'url'=>'admin.settings.lead_zones.index', 'permission_id' => 664),
             array('created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Settings > Leads Management > Leads Notifications', 'url'=>'admin.settings.lead_notification.index', 'permission_id' => 671),
             array('created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'First Mile > v2 Pickups > Rider Receiving DWS', 'url'=>'v2_pickups.rider_receiving.dws.index', 'permission_id' => 670)
-        );
+        ));
     }
 }
