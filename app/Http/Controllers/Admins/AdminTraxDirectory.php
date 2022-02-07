c0ae55345417aff006da70fd232287a9c9799423
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-02-07 21:47:51 +0500

    done

--- app/Http/Controllers/Admins/AdminTraxDirectory.php
+++ app/Http/Controllers/Admins/AdminTraxDirectory.php
@@ -28,37 +28,14 @@ class AdminTraxDirectory extends Controller
         if ($request->get('excel') && $request->get('excel') == true) {
             ActivityTrailController::createActivityTrailLog(Auth::id(), 112);
         }
-        if($request->search_name){
-            $admin = Admin::join('employees as e', 'admins.trax_id', '=', 'e.trax_id')
-                ->join('employee_designations as d', 'd.id', '=', 'admins.designation_id')
-                ->join('admin_departments as ad', 'd.department_id', '=', 'ad.id')
-                ->join('cities as c', 'c.id', '=', 'e.city_id')
-                ->select('e.trax_id as trax_id', 'e.name as name', 'e.official_email as email', 'e.phone_number as phone', 'd.name as designation', 'ad.name as department_name', 'c.name as city', 'e.official_phone_number as official_phone_number')
-                ->where('admins.status', 1)
-                ->where('admins.role_id', '!=', 1)
-                ->where('e.name', 'like','%' . $request->search_name . '%');
-        }
-        if($request->search_phone_number){
-            $admin = Admin::join('employees as e', 'admins.trax_id', '=', 'e.trax_id')
-                ->join('employee_designations as d', 'd.id', '=', 'admins.designation_id')
-                ->join('admin_departments as ad', 'd.department_id', '=', 'ad.id')
-                ->join('cities as c', 'c.id', '=', 'e.city_id')
-                ->select('e.trax_id as trax_id', 'e.name as name', 'e.official_email as email', 'e.phone_number as phone', 'd.name as designation', 'ad.name as department_name', 'c.name as city', 'e.official_phone_number as official_phone_number')
-                ->where('admins.status', 1)
-                ->where('admins.role_id', '!=', 1)
-                ->where('e.phone_number', substr_replace($request->input('search_phone_number'), '-', 4, 0))
-                ->orwhere('e.official_phone_number', substr_replace($request->input('search_phone_number'), '-', 4, 0));
-        }
-        if($request->search_trax_id){
-            $admin = Admin::join('employees as e', 'admins.trax_id', '=', 'e.trax_id')
-                ->join('employee_designations as d', 'd.id', '=', 'admins.designation_id')
-                ->join('admin_departments as ad', 'd.department_id', '=', 'ad.id')
-                ->join('cities as c', 'c.id', '=', 'e.city_id')
-                ->select('e.trax_id as trax_id', 'e.name as name', 'e.official_email as email', 'e.phone_number as phone', 'd.name as designation', 'ad.name as department_name', 'c.name as city', 'e.official_phone_number as official_phone_number')
-                ->where('admins.status', 1)
-                ->where('admins.role_id', '!=', 1)
-                ->where('e.trax_id', $request->search_trax_id);
-        }
+
+        $admin = Admin::join('employees as e', 'admins.trax_id', '=', 'e.trax_id')
+            ->join('employee_designations as d', 'd.id', '=', 'admins.designation_id')
+            ->join('admin_departments as ad', 'd.department_id', '=', 'ad.id')
+            ->join('cities as c', 'c.id', '=', 'e.city_id')
+            ->select('e.trax_id as trax_id', 'e.name as name', 'e.official_email as email', 'e.phone_number as phone', 'd.name as designation', 'ad.name as department_name', 'c.name as city', 'e.official_phone_number as official_phone_number')
+            ->where('admins.status', 1)
+            ->where('admins.role_id', '!=', 1);
         $datatable = Datatables::of($admin)
             ->addColumn('phone_number', function ($user) {
                 if ($user->official_phone_number != null) {
@@ -67,6 +44,20 @@ class AdminTraxDirectory extends Controller
                     return $user->phone;
                 }
             });
+
+        if($request->search_name){
+            $datatable->where('e.name', 'like','%' . $request->search_name . '%');
+        }
+        if($request->search_phone_number){
+            $phone = $request->search_phone_number;
+            $datatable->where(function($query) use($phone){
+                $query->where('e.phone_number', substr_replace($phone, '-', 4, 0))
+                    ->orWhere('e.official_phone_number', substr_replace($phone, '-', 4, 0));
+            });
+        }
+        if($request->search_trax_id){
+            $datatable->where('e.trax_id', $request->search_trax_id);
+        }
 
         return $datatable->make(true);
     }
