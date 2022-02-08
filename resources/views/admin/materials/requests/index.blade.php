3adfa9dc14ed445fc3839c6db7e5d7580fec4859
Abdul Ghani CHishti <ghani.chisti@trax.pk>
2022-02-08 13:57:02 +0500

    just fixed sorting of shipper cell no coloum in packaging/request

--- resources/views/admin/materials/requests/index.blade.php
+++ resources/views/admin/materials/requests/index.blade.php
@@ -450,7 +450,7 @@
                 serverSide: true,
                 ajax: '{{ route('admin.packaging.requests.list') }}',
                 rowId: 'request_id',
-                order: [[3, 'desc']],
+                order: [[4, 'desc']],
                 columns: [
                     {orderable: false, searchable: false, name: 'serial_number', class: 'align-middle serial_number', targets: 0, render: function (data, type, row) {return '';}},
                     {data: 'tracking_number_link', name: 'packaging_material_requests.tracking_number', class: 'align-middle tracking_number'},
