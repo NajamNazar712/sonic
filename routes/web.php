0b1c0e3c19624b0c4fd9dda31463e7bbe46d65ef
Abdul Ghani CHishti <ghani.chisti@trax.pk>
2022-02-10 17:29:15 +0500

    bulk status update in lead management

--- routes/web.php
+++ routes/web.php
@@ -3447,6 +3447,7 @@ Route::prefix('admin')->name('admin.')->group(function () {
         Route::get('', 'Admins\LeadManagementController@index')->name('index');
         Route::get('list', 'Admins\LeadManagementController@list')->name('list');
         Route::post('add_status', 'Admins\LeadManagementController@add_status')->name('add_status');
+        Route::post('add_bulk_status', 'Admins\LeadManagementController@add_bulk_status')->name('add_bulk_status');
         Route::post('tag_sale_person', 'Admins\LeadManagementController@tag_sale_person_forward_lead')->name('tag_sale_person');
         Route::post('add_remarks', 'Admins\LeadManagementController@add_remarks')->name('add_remarks');
         Route::get('view_remarks/{id}', 'Admins\LeadManagementController@view_remarks_index')->name('view_remarks');
