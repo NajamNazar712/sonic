852ce1a72b4daf5d002b872d81ded340e5855f59
Muhammad Waqas <muhammad.waqas@trax.pk>
2022-02-11 20:52:21 +0500

    fix

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -177,19 +177,23 @@ class NotificationsController extends Controller
                     return false;
                 }
             }
+            if($cc != NULL){
+                if(is_array($cc)){
+                    $cc = array_values(array_filter($cc));
+                    if(empty($cc)){
+                        $cc = NULL;
+                    }
+                }
+            }
+            if($bcc != NULL){
+                if(is_array($bcc)){
+                    $bcc = array_values(array_filter($bcc));
+                    if(empty($bcc)){
+                        $bcc = NULL;
+                    }
+                }
+            }
 
-            if(is_array($cc)){
-                $cc = array_values(array_filter($cc));
-                if(empty($cc)){
-                    $cc = NULL;
-                }
-            }
-            if(is_array($bcc)){
-                $bcc = array_values(array_filter($bcc));
-                if(empty($bcc)){
-                    $bcc = NULL;
-                }
-            }
 
             $mail = Mail::to($to);
 
