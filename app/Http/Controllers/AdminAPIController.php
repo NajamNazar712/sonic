cede2b9bb2d934ae67a022b8e65f9ad940183d9d
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-02-14 15:11:59 +0500

    fix in login apis

--- app/Http/Controllers/AdminAPIController.php
+++ app/Http/Controllers/AdminAPIController.php
@@ -5361,16 +5361,11 @@ class AdminAPIController extends Controller
                     }
                     $information['role'] = 'staff';
 
+                    EmployeeDeviceToken::where('employee_id', $user->id)->where('employee_type_id', 1)->delete();
                     if($request->has('device_token')){
-                        $employee_device_token = EmployeeDeviceToken::where('employee_id', $user->id)
-                            ->where('employee_type_id', 1);
-                        if ($employee_device_token->exists()) {
-                            $employee_device_token = $employee_device_token->first();
-                        } else {
-                            $employee_device_token = new EmployeeDeviceToken();
-                            $employee_device_token->employee_id = $user->id;
-                            $employee_device_token->employee_type_id = 1;
-                        }
+                        $employee_device_token = new EmployeeDeviceToken();
+                        $employee_device_token->employee_id = $user->id;
+                        $employee_device_token->employee_type_id = 1;
                         $employee_device_token->device_token = $request->get('device_token');
                         $employee_device_token->save();
                     }
