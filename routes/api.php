c43294f8db78dae4ef799f91e754fa0c47ee554d
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-02-15 17:03:02 +0500

    done

--- routes/api.php
+++ routes/api.php
@@ -339,7 +339,7 @@ Route::name('api.')->group(function () {
 
             Route::prefix('leads')->name('leads.')->group(function () {
                 Route::post('list', 'AdminAPIController@leads_list')->name('list');
-                Route::get('status_list', 'AdminAPIController@lead_statuses')->name('status_list');
+                Route::post('status_list', 'AdminAPIController@lead_statuses')->name('status_list');
                 Route::post('status_update', 'AdminAPIController@lead_status_update')->name('status_update');
                 Route::post('add_remarks', 'AdminAPIController@add_remarks')->name('add_remarks');
                 Route::post('view_remarks', 'AdminAPIController@view_remarks')->name('view_remarks');
