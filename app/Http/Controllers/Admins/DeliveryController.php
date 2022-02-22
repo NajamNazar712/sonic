92c421059a53f348b330b4602d2ef33c551673d2
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-02-23 02:33:35 +0500

    changes

--- app/Http/Controllers/Admins/DeliveryController.php
+++ app/Http/Controllers/Admins/DeliveryController.php
@@ -1757,15 +1757,19 @@ class DeliveryController extends Controller
                     }
                     if ($flag == true) {
                         if ($deliveries->packaging_material_request == 1 && $deliveries->packaging_material_charges == '') {
-                            $where = array(7, 8, 9, 15, 18, 56);
+                            $where = array(7, 8, 9, 15, 18);
                         } else {
-                            $where = array(7, 8, 9, 12, 15, 18, 56);
+                            $where = array(7, 8, 9, 12, 15, 18);
                         }
                     } else {
                         $where = array(12);
                     }
-                } else {
-                    $where = array(7, 8, 9, 15, 18, 56);
+                    if($deliveries->booking_type_id == 2){
+                        array_push($where,56);
+                    }
+                }
+                else {
+                    $where = array(7, 8, 9, 15, 18);
                 }
 
                 $statuses = ShipmentStatus::whereIn('id', $where)->get();
@@ -1899,9 +1903,9 @@ class DeliveryController extends Controller
 
     public function receive_delivery_status_submit_all(Request $request)
     {
-        $open_box_ids = array();
         $received_shipments = array();
         $first_attempt_shipments = array();
+        $regular_type_shipments = array();
         $restrict_status_shipments = array();
         $now = Carbon::now();
         $end_of_the_day = Carbon::today()->endOfDay()->addMinute(2);
@@ -1938,7 +1942,12 @@ class DeliveryController extends Controller
                     if (!in_array($shipment, $restrict_status_shipments)) {
                         array_push($restrict_status_shipments, $shipment);
                     }
-                } elseif (ShipmentsJourney::where('shipment_id', $shipment)->where('shipper_status_id', 5)->count() == 1) {
+                } elseif ($selected_status == 56 && $shipment_details->booking_type_id != 2){
+                    if (!in_array($shipment_details->tracking_number, $regular_type_shipments)) {
+                        array_push($regular_type_shipments, $shipment_details->tracking_number);
+                    }
+                }
+                elseif (ShipmentsJourney::where('shipment_id', $shipment)->where('shipper_status_id', 5)->count() == 1) {
 
                     if ($selected_status == 12 && !in_array($selected_reason, $reason_for_first_attempt)) {
                         if (!in_array($shipment_details->tracking_number, $first_attempt_shipments)) {
@@ -2152,9 +2161,18 @@ class DeliveryController extends Controller
             $delivery_note_data->status_updated_at = Carbon::now();
             $delivery_note_data->updated_by = Auth::id();
             $delivery_note_data->save();
-
-            if (count($invalid_reason_shipments) > 0) {
-                return response()->json(['status' => 2, 'success' => 'Statuses updated successfully!', 'invalid_shipments' => $invalid_reason_shipments, 'first_attempt_shipments' => $first_attempt_shipments]);
+            $response = array();
+            if(count($invalid_reason_shipments) > 0){
+                $response['invalid_shipments'] = $invalid_reason_shipments;
+            }
+            if(count($regular_type_shipments) > 0){
+                $response['not_replacement_shipments'] = $regular_type_shipments;
+            }
+            if($response){
+                $response['status'] = 2;
+                $response['success'] = 'Statuses updated successfully!';
+                $response['first_attempt_shipments'] = $first_attempt_shipments;
+                return response()->json($response);
             } else {
                 return response()->json(['status' => 1, 'success' => 'Statuses updated successfully!', 'first_attempt_shipments' => $first_attempt_shipments]);
             }
