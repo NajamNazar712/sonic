471fb59388d28018a72c5b0968070a5d9e56bded
Anas_Anwer <anas.anwer@trax.pk>
2022-02-22 11:56:48 -0800

    chagnes

--- resources/views/admin/leads/index.blade.php
+++ resources/views/admin/leads/index.blade.php
@@ -190,25 +190,6 @@
                             </div>
                         </div>
                     </div>
-                <div class="row justify-content-center">
-                        <div class="col-3" id="dormant_div">
-                            <div class="card bg-gradient-directional-pending_confirmation pull-up">
-                                <div class="card-content">
-                                    <div class="card-body">
-                                        <div class="media d-flex">
-                                            <div class="align-self-center">
-                                                <i class="la la-hourglass text-white font-large-2 float-left"></i>
-                                            </div>
-                                            <div class="media-body text-white text-right">
-                                                <h3 class="text-white"><p id="dormant" class="d-inline">{{$leads['dormant']}} </p>(<p id="active_percentage" class="d-inline">{{$leads['accounts_activated_percentage']}}</p>%)</h3>
-                                                <span>Dormant</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
 
                 <table class="table table-bordered datatable" id="datatable" style="z-index: 3;">
                     <thead>
