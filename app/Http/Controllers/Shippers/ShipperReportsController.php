5702704d154b00c562ddf4e51c131b3ca5f9de76
Nabeel973 <nabeelsiddiqui324@gmail.com>
2022-02-23 20:40:52 +0500

    reattempt count

--- app/Http/Controllers/Shippers/ShipperReportsController.php
+++ app/Http/Controllers/Shippers/ShipperReportsController.php
@@ -988,7 +988,7 @@ class ShipperReportsController extends Controller
                 return "<u><a href='{$route}?tracking_number=$shipment->tracking_number' class='tracking' target='_blank'>$shipment->tracking_number</a></u>";
             })
             ->editColumn('shipper_name', function ($shipment) {
-                if($shipment->shipper_id == 7306){
+                if($shipment->shipper_id == 1091){
                     return '-';
                 }else{
                     return $shipment->shipper_name;
