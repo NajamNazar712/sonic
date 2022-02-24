05c88ece3e840d2dbdae142cbdb82937746cdb88
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-02-24 16:57:16 +0500

    changes

--- app/Http/Controllers/AdminAPIController.php
+++ app/Http/Controllers/AdminAPIController.php
@@ -6358,9 +6358,10 @@ class AdminAPIController extends Controller
     public function pending_pick_list(Request $request){
         $admin_id = $request->admin_id;
         $pending_picklist = DB::table('wms_picklists')->join('admins', 'admins.id', '=', 'wms_picklists.created_by')
+            ->join('wms_pickers as wp', 'wp.id', '=', 'wms_picklists.picker_id')
             ->select('wms_picklists.id as picklist_id', 'admins.name as created_by','wms_picklists.created_at as picking_date','wms_picklists.sku_count', 'wms_picklists.tracking_count','wms_picklists.quantity')
             ->where('wms_picklists.status', 0)
-            ->where('wms_picklists.picker_id', $admin_id);
+            ->where('wp.admin_id', $admin_id);
 
         if($pending_picklist->exists()){
             $pending_picklist = $pending_picklist->get();
