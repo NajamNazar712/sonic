a04d6ac0638a97386f2f9a0d5455ec3cea88d44a
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-02-25 10:54:27 +0500

    changes

--- app/Http/Controllers/AdminAPIController.php
+++ app/Http/Controllers/AdminAPIController.php
@@ -6455,6 +6455,7 @@ class AdminAPIController extends Controller
 
     public function pick_list_receive(Request $request)
     {
+        dd($request->picklist_id);
         $rules = [
             'picklist_id' => ['required'],
             'barcode_list' => ['required'],
