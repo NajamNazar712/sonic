53ce6c2cbbdaf941f7930bb554b55c6267b4a177
Nabeel973 <nabeelsiddiqui324@gmail.com>
2022-02-28 18:05:15 +0500

    sorter set

--- resources/views/client/finance/invoice/index.blade.php
+++ resources/views/client/finance/invoice/index.blade.php
@@ -192,7 +192,7 @@
                     }
                 },
                 rowId: 'id',
-                order: [[0, 'desc']],
+                order: [[6, 'desc']],
                 columns: [
                     {data: 'serial_number', orderable: false, searchable: false, name: 'serial_number', class: 'align-middle serial_number', targets: 1, render: function (data, type, row) {return '';}},
                     {data:'invoice_number_button', name: 'invoices.invoice_number', class: 'align-middle text-center invoice_number'},
