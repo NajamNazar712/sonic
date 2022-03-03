a6666eb970d5148d0939ea7302db35f7a4651a09
Muhammad Waqas <muhammad.waqas@trax.pk>
2022-03-03 14:29:26 +0500

    fix

--- app/Http/Controllers/Admins/DeliveryController.php
+++ app/Http/Controllers/Admins/DeliveryController.php
@@ -6936,7 +6936,7 @@ class DeliveryController extends Controller
                     $sorted_array[$slip->id]['bank'] = BanksList::find($slip->bank_id)->name;
                     $sorted_array[$slip->id]['amount'] = $slip->amount;
                     $sorted_array[$slip->id]['created_at'] = Carbon::parse($slip->created_at)->toDateTimeString();
-                    $sorted_array[$slip->id]['uploaded_by'] = ($slip->uploaded_by_admin->name) ? $slip->uploaded_by_admin->name : '';
+                    $sorted_array[$slip->id]['uploaded_by'] = ($slip->uploaded_by_admin->name != '') ? $slip->uploaded_by_admin->name : '';
                     $img_url = 'uploads/sdn/'. $slip->created_at;
                     if(file_exists($img_url)){
                         $sorted_array[$slip->id]['image'] = '<a class="btn btn-sm btn-outline-info align-middle" href="' . asset('uploads/sdn/' . $slip->image) . '" target="_blank"><i class="la la-lg la-image align-middle"></i> <span class="align-middle">View</span></a>';
