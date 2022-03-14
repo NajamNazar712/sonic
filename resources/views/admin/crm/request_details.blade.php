8dbb689c982328e9b41f1cbf5800242a3751b9d1
Muhammad Waqas <muhammad.waqas@trax.pk>
2022-03-14 11:51:15 +0500

    fix

--- resources/views/admin/crm/request_details.blade.php
+++ resources/views/admin/crm/request_details.blade.php
@@ -1266,6 +1266,10 @@
             filter: grayscale(0);
             font-size: 4rem;
         }
+        .feedback .radio:hover ~ span {
+            filter: grayscale(0);
+            font-size: 4rem;
+        }
     </style>
 @endsection
 
