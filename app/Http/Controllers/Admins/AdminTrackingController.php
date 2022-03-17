c085cf9be61e6162de188ce0712d048afe4b2c14
Anas_Anwer <anas.anwer@trax.pk>
2022-03-17 14:55:02 +0500

    changes

--- app/Http/Controllers/Admins/AdminTrackingController.php
+++ app/Http/Controllers/Admins/AdminTrackingController.php
@@ -611,13 +611,13 @@ class AdminTrackingController extends Controller
                     $details['destination'] = $shipment->consignee_city->name;
 
                     //                    yahan p join lgana h shipment and delivery note shipment ka our delivery note id uthaleni h
-                    $shipment = Shipment::leftjoin('delivery_note_shipments', 'delivery_note_shipments.shipment_id', '=', 'shipments.id')
-                        ->select('delivery_note_shipments.delivery_note_id')
+                    $delivery_note_shipment = Shipment::leftjoin('delivery_note_shipments', 'delivery_note_shipments.shipment_id', '=', 'shipments.id')
+                        ->select('delivery_note_shipments.delivery_note_id as delivery_note_id')
                         ->where('delivery_note_shipments.shipment_id', '=', $shipment->id)
                         ->orderBy('delivery_note_shipments.shipment_id', 'desc');
-                    if($shipment->exists()){
-                        $shipment = $shipment->first();
-                        $dn = str_pad($shipment->delivery_note_id, 6, '0', STR_PAD_LEFT);;
+                    if($delivery_note_shipment->exists()){
+                        $delivery_note_shipment = $delivery_note_shipment->first();
+                        $dn = str_pad($delivery_note_shipment->delivery_note_id, 6, '0', STR_PAD_LEFT);;
                     }
                     else{
                         $dn = '-';
