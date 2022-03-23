72da00ba53234a167ea748bb5f5722196eabc08c
Muhammad Aqib <muhammad.aqib@trax.pk>
2022-03-23 14:07:34 +0500

    changes

--- app/Console/Commands/AutoComplaintHighAging.php
+++ app/Console/Commands/AutoComplaintHighAging.php
@@ -67,7 +67,7 @@ class AutoComplaintHighAging extends Command
                     
                     $crm_req = CrmRequest::where('shipment_id',$shipment->id)->where('case_nature_type_id',34);
                     if(!$crm_req->exists()){
-                        CRMController::add($nature_id, $complaint_id, 1, 1, $user_id, $launched_by, $shipment->id, $shipment->shipper_id, null, $description);
+                        CRMController::add($nature_id, $complaint_id, 1, 1, $user_id, $launched_by, $shipment->id, $shipment->user_id, null, $description);
                     }
                       
                 }elseif($shipment->shipper_status_id == 2){
