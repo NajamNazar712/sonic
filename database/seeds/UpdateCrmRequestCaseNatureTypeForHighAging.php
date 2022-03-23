1e30e43afb8b659ccdce00ba739cb47a1a8e77a3
Muhammad Aqib <muhammad.aqib@trax.pk>
2022-03-23 13:17:59 +0500

    changes

--- database/seeds/UpdateCrmRequestCaseNatureTypeForHighAging.php
+++ database/seeds/UpdateCrmRequestCaseNatureTypeForHighAging.php
@@ -12,7 +12,7 @@ class UpdateCrmRequestCaseNatureTypeForHighAging extends Seeder
     public function run()
     {
         DB::table('crm_request_case_nature_types')->insert(array(
-            array('id' => 34, 'nature_id' => 1, 'type' => 'High Aging', 'status' => 0)
+            array('id' => 34, 'nature_id' => 1, 'type' => 'High Aging', 'status_id' => 0)
         ));
     }
 }
