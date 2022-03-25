74122d5f687a3aff36d2ebfc6bd8cb9a9298cab0
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-03-25 15:46:43 +0500

    changes

--- app/Http/Controllers/Rider/RiderAPIController.php
+++ app/Http/Controllers/Rider/RiderAPIController.php
@@ -4984,9 +4984,8 @@ class RiderAPIController extends Controller
                         $information['cargo_user'] = 0;
 
                         if($request->has('device_token')){
-                            EmployeeDeviceToken::where('device_token', $request->get('device_token'))
-                                ->orWhere(['employee_type_id' => 2, 'employee_id' => $rider->id])
-                                ->delete();
+                            EmployeeDeviceToken::where('device_token', $request->get('device_token'))->delete();
+                            EmployeeDeviceToken::where('employee_type_id', 2)->where('employee_id',$rider->id)->delete();
                             $employee_device_token = new EmployeeDeviceToken();
                             $employee_device_token->employee_id = $rider->id;
                             $employee_device_token->employee_type_id = 2;
