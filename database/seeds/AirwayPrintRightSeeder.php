fb8019152236991fa0dbb42928a23963037fa043
Abdul Ghani CHishti <ghani.chisti@trax.pk>
2022-03-28 10:01:47 +0500

    changes

--- database/seeds/AirwayPrintRightSeeder.php
+++ database/seeds/AirwayPrintRightSeeder.php
@@ -12,7 +12,7 @@ class AirwayPrintRightSeeder extends Seeder
     public function run()
     {
         DB::table('module_permissions')->insert(array(
-            array('id' => 699, 'name' => 'Airway Bill Print Rights','module_id' => 15),
+            array('id' => 699, 'name' => 'Airway Bill Print Rights','module_id' => 33),
         ));
     }
 }
