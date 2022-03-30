b669cbd20a4f3e83adacbfae4942c654d77fe9ed
Abdul Ghani CHishti <ghani.chisti@trax.pk>
2022-03-30 11:17:19 +0500

    changes

--- app/Http/Controllers/Admins/AdminFinanceController.php
+++ app/Http/Controllers/Admins/AdminFinanceController.php
@@ -5083,6 +5083,9 @@ class AdminFinanceController extends Controller
             ->join('users as u', 'done_payments.user_id', '=', 'u.id')
             ->join('cities as c', 'u.city_id', '=', 'c.id')
             ->leftjoin('done_payment_calculations as dpc','dpc.done_payment_id', '=', 'done_payments.id')
+            ->leftJoin('admins as ad', function ($join) {
+                $join->on('ad.id', '=', 'done_payments.status_updated_by');
+            })
             ->leftJoin('user_bank_infos as ubi', function ($join) {
                 $join->on('ubi.id', '=', 'done_payments.user_bank_info_id');
             })
@@ -5100,8 +5103,7 @@ class AdminFinanceController extends Controller
                 });
             })
             ->leftjoin('banks_lists as b', 'done_payments.company_bank_id', '=', 'b.id')
-            ->join('admins', 'admins.id', '=', 'done_payments.status_updated_by')
-            ->select('done_payments.user_id as user_id','done_payments.id as id','done_payments.id as payment_id', 'u.name as shipper', 'c.name as city', 'u.phone', 'u.phone2', 'u.address', 'done_payments.total_shipments', 'done_payments.delivered_shipments', 'done_payments.delivered_shipments as delivered_shipments_count', 'done_payments.returned_shipments', 'done_payments.returned_shipments as returned_shipments_count', 'done_payments.adjusted_shipments', 'done_payments.adjusted_shipments as adjusted_shipments_count', 'dpc.amount as total_amount', 'dpc.charges as total_charges', 'dpc.gst as total_gst', 'dpc.payable as total_payable', 'ub.name as bank', 'done_payments.reference_number', 'done_payments.created_at as done_at', 'b.name as company_bank', 'done_payments.status', 'done_payments.ibft_charges', 'dpc.packaging_charges', 'dpc.adjustment as adjustment_charges', 'done_payments.status_updated_at as status_updated_at','dpc.wht as total_wht','done_payments.created_at as start_date','done_payments.updated_at as end_date','admins.name as admin_name');
+            ->select('done_payments.user_id as user_id','done_payments.id as id','done_payments.id as payment_id', 'u.name as shipper', 'c.name as city', 'u.phone', 'u.phone2', 'u.address', 'done_payments.total_shipments', 'done_payments.delivered_shipments', 'done_payments.delivered_shipments as delivered_shipments_count', 'done_payments.returned_shipments', 'done_payments.returned_shipments as returned_shipments_count', 'done_payments.adjusted_shipments', 'done_payments.adjusted_shipments as adjusted_shipments_count', 'dpc.amount as total_amount', 'dpc.charges as total_charges', 'dpc.gst as total_gst', 'dpc.payable as total_payable', 'ub.name as bank', 'done_payments.reference_number', 'done_payments.created_at as done_at', 'b.name as company_bank', 'done_payments.status', 'done_payments.ibft_charges', 'dpc.packaging_charges', 'dpc.adjustment as adjustment_charges', 'done_payments.status_updated_at as status_updated_at','dpc.wht as total_wht','done_payments.created_at as start_date','done_payments.updated_at as end_date','ad.name as admin_name');
 
         if(session('department_id') == 7){
             if(!in_array(session('id'), session('sale_users_bypass'))){
@@ -5346,6 +5348,7 @@ class AdminFinanceController extends Controller
         }
 
         return $datatables->make(true);
+
     }
 
     public function view_status_history(Request $request)
--- resources/views/admin/finance/done_payments.blade.php
+++ resources/views/admin/finance/done_payments.blade.php
@@ -756,8 +756,8 @@
 					selector: 'td.select-checkbox',
 					className: 'selected bg-primary bg-lighten-5 primary'
 				},
-				lengthMenu: [[50, 100, 500, 1000, -1], [50, 100, 500, 1000, 'All']],
-				pageLength: 50,
+				lengthMenu: [[10,50, 100, 500, 1000, -1], [10,50, 100, 500, 1000, 'All']],
+				pageLength: 10,
 				pagingType: 'full_numbers',
 				processing: true,
                 language: {
