7608a851b7ba8653962eed7422f6a3e019aba1ac
Abdul Ghani CHishti <ghani.chisti@trax.pk>
2022-03-31 18:17:25 +0500

    changes

--- app/Http/Models/Shipper/User.php
+++ app/Http/Models/Shipper/User.php
@@ -33,6 +33,10 @@ class User extends Authenticatable
         return $this->belongsTo('App\Http\Models\City');
     }
 
+    public function city2() {
+        return $this->belongsTo('App\Http\Models\City', 'city_id', 'id');
+    }
+
     public function bank(){
         return $this->hasMany('App\Http\Models\Shipper\UserBankInfo');
     }
