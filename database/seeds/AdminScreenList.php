80dd523363925ac3b57f344b29875eb3e80b5070
Junaid Mazhar <mazhar.junaid@trax.pk>
2022-04-06 16:03:14 +0500

    Cargo Manifest

--- database/seeds/AdminScreenList.php
+++ database/seeds/AdminScreenList.php
@@ -8,6 +8,6 @@ class AdminScreenList extends Seeder
 
     public function run()
     {
-        DB::table('admins_screen_list')->where('id', 396)->update(['name' => 'Supply Chain->Cargo Manifest Draft Setting']);
+        DB::table('admins_screen_list')->where('id', 396)->update(['name' => 'Supply Chain->Cargo Manifest Draft Setting' , 'url' => 'admin.cargo_manifest.draft.setting' , 'permission_id' => 682]);
     }
 }
