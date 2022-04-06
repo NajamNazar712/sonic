9e8bf922052346936e072217d828e4cc66df254b
Junaid Mazhar <mazhar.junaid@trax.pk>
2022-04-06 13:14:26 +0500

    Draft Manifest

--- database/seeds/CargoManifestDraftSetting.php
+++ database/seeds/CargoManifestDraftSetting.php
@@ -12,7 +12,7 @@ class CargoManifestDraftSetting extends Seeder
      */
     public function run()
     {
-        $data = DB::table('module_permissions')->insert([
+        $data = DB::table('module_permissions')->where('module_id', '14')->first()([
             'id' => '682',
             'name' => 'Cargo Manifest',
             'module_id' => '24',
