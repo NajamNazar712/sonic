153f3a233a398aad41619a88b83353cfdf101c76
Abdul Ghani CHishti <ghani.chisti@trax.pk>
2022-04-07 10:17:53 +0500

    changes

--- app/Http/Controllers/Admins/AdminFinanceController.php
+++ app/Http/Controllers/Admins/AdminFinanceController.php
@@ -5227,11 +5227,15 @@ class AdminFinanceController extends Controller
 //            $done_payments = DonePayment::join('shipments_payment_journey as spj', 'spj.payment_id', '=', 'done_payments.id')
 //                ->leftjoin('admins', 'admins.id', '=', 'spj.admin_id')
 //                ->leftjoin('shipment_payment_status as sps', 'spj.status_id', '=', 'sps.id')
-//                ->where('done_payments.id', 5)
+//                ->where('done_payments.id', $request->id)
 //                ->select('done_payments.id as payment_id', 'spj.id', 'spj.status_id as status_id', 'spj.admin_id', 'spj.updated_at as updated_at', 'admins.name as admin', 'sps.name as payment_status')
-//                ->latest('updated_at')->get();
+//                ->groupBy('shipment_id')
+//                ->get();
 
-//
+//Shipments::select('id', 'shipment_id', DB::raw('MAX(date) as latest_date'))
+//                            ->where('is_active', 1)
+//                            ->groupBy('shipment_id')
+//                            ->get();
             if (!empty($done_payments)) {
                 foreach ($done_payments as $key => $done_payment) {
                     $payment_id = $done_payment->payment_id;
