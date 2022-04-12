e660c5c2d28930e66837c2d0201ece33559e3f50
Maaz Khan <Maaz.Khan@Trax.pk>
2022-04-12 15:59:20 +0500

    done

--- app/Http/Controllers/Admins/AdminFinanceController.php
+++ app/Http/Controllers/Admins/AdminFinanceController.php
@@ -3082,7 +3082,7 @@ class AdminFinanceController extends Controller
             }
 
             if ($valid) {
-                if ($account_type_id == 1 || ($account_type_id == 2 && !$shipment->packaging_material_request && $amount != 0) || $crs) {
+                if ($account_type_id == 1 || ($account_type_id == 2 && !$shipment->packaging_material_request) || $crs) {
                     $pending_payment = PendingPayment::where('user_id', $shipment->user_id);
 
                     if ($pending_payment->exists()) {
