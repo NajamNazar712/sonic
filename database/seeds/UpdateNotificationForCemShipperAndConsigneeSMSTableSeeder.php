cacf8dfb63eb2387b3bd22a7573c47d737348219
Anas_Anwer <anas.anwer@trax.pk>
2022-04-12 15:21:05 +0500

    changes

--- database/seeds/UpdateNotificationForCemShipperAndConsigneeSMSTableSeeder.php
+++ database/seeds/UpdateNotificationForCemShipperAndConsigneeSMSTableSeeder.php
@@ -14,7 +14,7 @@ class UpdateNotificationForCemShipperAndConsigneeSMSTableSeeder extends Seeder
         $timestamp =  \Carbon\Carbon::now()->format('Y-m-d H:i:s');
 
         DB::table('notifications')->insert(array(
-            array('id' => 176, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'CRM Comment SMS', 'type_id' => 2, 'subject' => '', 'body' => 'Dear [name],' . PHP_EOL . 'Comment against CRM Request ID [crm_request_id] '. PHP_EOL .' [comment].', 'updated_by' => 7, 'status' => 0),
+            array('id' => 176, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'CRM Comment SMS', 'type_id' => 2, 'subject' => '', 'body' => 'Dear [name],' . PHP_EOL . 'Comment against CRM Request ID [crm_request_id]: '. PHP_EOL . PHP_EOL .' [comment].', 'updated_by' => 7, 'status' => 0),
         ));
     }
 }
