0ccc9c7b60271a2d4817aa9c9ab4373fc6fb273f
Muhammad Waqas <muhammad.waqas@trax.pk>
2022-04-13 13:05:44 +0500

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -319,8 +319,8 @@ class Kernel extends ConsoleKernel
         $schedule->command('email:pendingdeliveryreport')->dailyAt('01:00')->runInBackground();
         $schedule->command('email:receivedeliveryreport')->dailyAt('01:00')->runInBackground();
 
-//        $schedule->command('website:leads')->everyFiveMinutes()->runInBackground();
-        $schedule->command('website:pamleads')->hourly()->runInBackground();
+        $schedule->command('website:leads')->everyFiveMinutes()->runInBackground();
+//        $schedule->command('website:pamleads')->hourly()->runInBackground();
 
         $schedule->command('generate:usersotp')->monthlyOn(1, '00:00')->runInBackground();
 //        $schedule->command('email:revenuereport')->monthlyOn(1, '00:00')->runInBackground();
