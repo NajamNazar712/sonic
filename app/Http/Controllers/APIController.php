f933e83fc4611ea1448e52680f47f02be6b9278f
Anas_Anwer <anas.anwer@trax.pk>
2022-04-19 12:03:30 +0500

    changes

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -4882,7 +4882,13 @@ class APIController extends Controller
                 $delivery_note = DeliveryNote::where('id', $delivery_note_id);
                 if($delivery_note->exists()){
                     $delivery_note = $delivery_note->first();
-                    $net_amount = $delivery_note->total_cod_amount - $delivery_note->received_cod_amount;
+                    $hbl_konnect_transaction_delivery_note = HblKonnectTransactionDeliveryNote::where('delivery_note_id', $delivery_note->id);
+                    $transactions_amount = 0;
+                    if($hbl_konnect_transaction_delivery_note->exists()){
+                        $hbl_konnect_transaction_delivery_note = $hbl_konnect_transaction_delivery_note->first();
+                        $transactions_amount = $hbl_konnect_transaction_delivery_note->transactions_amount;
+                    }
+                    $net_amount = $delivery_note->received_cod_amount - $transactions_amount;
 
                     return response()->json(['status' => 1, 'delivery_note_id' =>  str_pad($delivery_note->id, 6, '0', STR_PAD_LEFT), 'amount' => $net_amount]);
                 }
