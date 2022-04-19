37907c54acc95a1b508bcc01cd562207ebf5169f
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-04-19 17:11:49 +0500

    changes

--- app/Http/Controllers/AdminAPIController.php
+++ app/Http/Controllers/AdminAPIController.php
@@ -5325,7 +5325,8 @@ class AdminAPIController extends Controller
                     ->where('employee_type', 1)
                     ->whereDate('attendance_date', Carbon::parse($date)->format("Y-m-d"));
                 if ($attendance->exists()) {
-                    $attendance = $attendance->first();
+                    $attendance = $attendance->get();
+                    dd($attendance);
                     if ($shift_exists == 1) {
                         if ($attendance->clock_in_datetime) {
                             $clock_in_date = Carbon::parse($attendance->clock_in_datetime)->format("Y-m-d");
