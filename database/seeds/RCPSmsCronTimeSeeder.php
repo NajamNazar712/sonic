dda5820497dcd2b33b9c4f6dade16e96f978bb0e
Junaid Mazhar <syed.junaidmazhar@trax.pk>
2022-04-25 14:32:08 +0500

    Time Change Setting Require

--- database/seeds/RCPSmsCronTimeSeeder.php
+++ database/seeds/RCPSmsCronTimeSeeder.php
@@ -2,19 +2,17 @@
 
 use Illuminate\Database\Seeder;
 
-
-class rcp_sms_cron_time extends Seeder
+class RCPSmsCronTimeSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      *
      * @return void
      */
-
-        public function run()
+    public function run()
     {
         DB::table('global_settings')->insert(array(
-            array('id' => 74, 'setting_value' => 0, 'type' => 'rcp_sms_cron_time', 'text' => '09')
+            array('setting_value' => 0, 'type' => 'rcp_sms_cron_time', 'text' => '12:00')
         ));
     }
 }
