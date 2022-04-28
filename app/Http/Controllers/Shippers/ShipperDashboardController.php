8c9a425a88769714a8363c8b2049919f41ea7563
Anas_Anwer <anas.anwer@trax.pk>
2022-04-28 14:49:05 +0500

    changes

--- app/Http/Controllers/Shippers/ShipperDashboardController.php
+++ app/Http/Controllers/Shippers/ShipperDashboardController.php
@@ -216,7 +216,7 @@ class ShipperDashboardController extends Controller
         $permission = session('permissions');
 
         $case_nature = CrmRequestCaseNature::get();
-
+        $row = array();
         if(session('user_type') !== 1){
             foreach($case_nature as $nature) {
                 if (in_array(16,$permission) && ($nature->id == 1)) {
