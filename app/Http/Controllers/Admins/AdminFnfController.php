339539e585d8e9986420377f3a86f88b10d06159
Nabeel973 <nabeelsiddiqui324@gmail.com>
2022-05-20 18:57:33 +0500

    changes

--- app/Http/Controllers/Admins/AdminFnfController.php
+++ app/Http/Controllers/Admins/AdminFnfController.php
@@ -51,7 +51,7 @@ class AdminFnfController extends Controller
          ->join('employee_designations as ed','ed.id','=','employees.designation_id')
          ->join('cities as c','c.id','=','employees.city_id')
          ->join('fnf_statuses as fs','fs.id','=','fnf.status_id')
-         ->select(['fnf.id as id','fnf.id as fnf_id','employees.name','employees.city_id','employees.phone_number','a.name as line_manager','ah.name as hod','d.name as department','ed.name  as designation','employees.id as employee_id','employees.trax_id','c.id as city_id','c.name as city','employees.name as employee_name','fnf.status_id as status_id','fnf.joining_date','fnf.resign_date','fnf.created_at','h.name as created_by','employees.id as employee','fs.name as status','fnf.hod as hod_id','fnf.line_manager as reporting_manager']);
+         ->select(['fnf.id as id','fnf.id as fnf_id','employees.name','employees.city_id','employees.phone_number','a.name as line_manager','ah.name as hod','d.name as department','ed.name  as designation','employees.id as employee_id','employees.trax_id','c.id as city_id','c.name as city','employees.name as employee_name','fnf.status_id as status_id','fnf.joining_date','fnf.resign_date','fnf.created_at','h.name as created_by','employees.id as employee','fs.name as status','fnf.hod as hod_id','fnf.line_manager as reporting_manager','c.hub_id']);
 
         if(session('role_id') != 1)
         {
