06e7b199f1b1e4166ea44e786010611f2378ff08
Anas_Anwer <anas.anwer@trax.pk>
2022-05-25 12:16:19 +0500

    changes

---
+++ database/migrations/2022_05_25_120023_create_shipments_estimated_weights_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShipmentsEstimatedWeightsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shipments_estimated_weights', function (Blueprint $table) {
+            $table->increments('id');
+            $table->decimal('estimated_weight', 8, 2);
+            $table->decimal('actual_weight', 8, 2);
+            $table->decimal('length', 8, 2)->nullable()->default(NULL);
+            $table->decimal('breadth', 8, 2)->nullable()->default(NULL);
+            $table->decimal('height', 8, 2)->nullable()->default(NULL);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shipments_estimated_weights');
+    }
+}
