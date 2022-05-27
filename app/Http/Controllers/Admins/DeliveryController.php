2aefb9bb189ac4057a7a3ab69946fa9af859e2e5
Junaid Mazhar <syed.junaidmazhar@trax.pk>
2022-05-27 15:33:32 +0500

    sdn_resolved_status

--- app/Http/Controllers/Admins/DeliveryController.php
+++ app/Http/Controllers/Admins/DeliveryController.php
@@ -7993,17 +7993,21 @@ class DeliveryController extends Controller
         if ($station_deposit_notes->exists()) {
             $station_deposit_notes = $station_deposit_notes->get();
             foreach($station_deposit_notes as $station_deposit_note){
-                if (($station_deposit_note->sdn_amount - ($station_deposit_note->sdn_deposit_amount + $station_deposit_note->adjustment_amount)) == 0 && $station_deposit_note->status == 1) {
-                    $station_deposit_note->status = 3;
-                    $station_deposit_note->closed_at = Carbon::now();
-                    $station_deposit_note->save();
+                if (($station_deposit_note->sdn_amount - ($station_deposit_note->sdn_deposit_amount + $station_deposit_note->adjustment_amount)) == 0)
+                {
+                    if($station_deposit_note->status == 1) {
+                        $station_deposit_note->status = 3;
+                        $station_deposit_note->closed_at = Carbon::now();
+                        $station_deposit_note->save();
+                        return response()->json(['status'=> 1,'success'=>"Station Deposit Notes Status Updated To Closed"]);
+                    }else{
+                        return response()->json(['status'=> 0,'error'=>"Status Should Be Deposited First"]);
+                    }
+                }
+                else{
+                    return response()->json(['status'=> 0,'error'=>"Difference Amount is pending"]);
                 }
             }
-            return response()->json(['status'=> 1,'success'=>"Station Deposit Notes Status Updated To Closed"]);
-
-        } else {
-            return response()->json(['status'=> 0,'error'=>"Station Deposit Notes Not Found"]);
-
         }
     }
 
