44f7b85d1ee248ba775302145486dc5a6d4cff05
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-05-27 13:54:29 +0500

    changes

--- app/Http/Controllers/Rider/RiderAPIController.php
+++ app/Http/Controllers/Rider/RiderAPIController.php
@@ -11794,10 +11794,7 @@ class RiderAPIController extends Controller
                 ->leftjoin('cities as h', 'h.id', 'c.hub_id')
                 ->where('is_line_manager', 1)
                 ->where('department_id', 6)
-                ->get();
-            return response()->json(['status' => 0, 'line_managers' => $line_managers]);
-
-//                ->select(['employees.name', 'employees.trax_id', 'employees.id', 'h.name as hub']);
+                ->select(['employees.name', 'employees.trax_id', 'employees.id', 'h.name as hub']);
             if($line_managers->exists()){
                 $line_managers = $line_managers->get();
                 $data = array();
