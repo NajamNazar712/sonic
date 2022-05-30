d935ab923a01d6ebc589b026b155043baa405f0a
Abdul Ghani CHishti <ghani.chisti@trax.pk>
2022-05-30 16:37:51 +0500

    chages

--- app/Http/Middleware/Permission.php
+++ app/Http/Middleware/Permission.php
@@ -1224,6 +1224,7 @@ class Permission
             'settings.lost_shipment_shippers.index' => 708,
             'settings.lost_shipment_admins.index' => 710,
 
+            'settings.invoice_against_return_delivered_shipper.index' => 716,
             
 
             
