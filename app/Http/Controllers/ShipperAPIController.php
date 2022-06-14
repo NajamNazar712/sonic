79972f3feb1a983761b2e477a379eb96cb4f078c
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-06-14 12:08:43 +0500

    changes

--- app/Http/Controllers/ShipperAPIController.php
+++ app/Http/Controllers/ShipperAPIController.php
@@ -786,8 +786,8 @@ class ShipperAPIController extends Controller
 
             $shipment_status = $shipment->status_shipper->name;
 
-            if ($shipment['shipper_status_id'] == 12) {
-                if ($shipment['consignee_city_id'] != $request->consignee_city || $shipment['consignee_name'] != $request->consignee_name || $shipment['consignee_address'] != $request->consignee_address || $shipment['consignee_phone_number_1'] != $request->consignee_phone_number_1 || $shipment['consignee_phone_number_2'] != $request->consignee_phone_number_2 || $shipment['consignee_email'] != $request->consignee_email || $shipment['amount'] != $amount) {
+            if ($shipment->shipper_status_id == 12) {
+                if ($shipment->consignee_city_id != $request->consignee_city || $shipment->consignee_name != $request->consignee_name || $shipment->consignee_address != $request->consignee_address || $shipment->consignee_phone_number_1 != $request->consignee_phone_number_1 || $shipment->consignee_phone_number_2 != $request->consignee_phone_number_2 || $shipment->consignee_email != $request->consignee_email || $shipment->amount != $amount) {
                     if ($shipment['intercepted'] == 1) {
                         return response()->json(['status' => 1, 'message' => 'Intercept/Re-Book is already requested against Tracking Number: ' . $shipment['tracking_number']]);
                     }
