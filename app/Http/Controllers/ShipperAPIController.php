73333b2909b11410ec55fd98405f2d1a367f1e49
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-06-14 14:33:53 +0500

    changes

--- app/Http/Controllers/ShipperAPIController.php
+++ app/Http/Controllers/ShipperAPIController.php
@@ -788,6 +788,7 @@ class ShipperAPIController extends Controller
 
             if ($shipment->shipper_status_id == 12) {
                 if ($shipment->consignee_city_id != $request->consignee_city || $shipment->consignee_name != $request->consignee_name || $shipment->consignee_address != $request->consignee_address || $shipment->consignee_phone_number_1 != $request->consignee_phone_number_1 || $shipment->consignee_phone_number_2 != $request->consignee_phone_number_2 || $shipment->consignee_email != $request->consignee_email || $shipment->amount != $amount) {
+                    dd($shipment);
                     if ($shipment['intercepted'] == 1) {
                         return response()->json(['status' => 1, 'message' => 'Intercept/Re-Book is already requested against Tracking Number: ' . $shipment['tracking_number']]);
                     }
