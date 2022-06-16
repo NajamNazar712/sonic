a94f524318ef3d79048d71050f9355d7d9c9485c
Shariq Raees <shariq.raees@trax.pk>
2022-06-16 19:00:09 +0500

    work done 2 screens pending to apply

--- app/Http/Controllers/ShipmentsJourneyController.php
+++ app/Http/Controllers/ShipmentsJourneyController.php
@@ -28,7 +28,7 @@ use Auth;
 
 class ShipmentsJourneyController extends Controller
 {
-    static public function add($shipment_id, $shipper_status_id, $consignee_status_id, $status_reason_id, $remarks, $user_id, $admin_id, $reference_1_id = NULL, $reference_2_id = NULL,$verification = 1, $received_or_refused_by = NULL, $rider_id = NULL) {
+    static public function add($shipment_id, $shipper_status_id, $consignee_status_id, $status_reason_id, $remarks, $user_id, $admin_id, $reference_1_id = NULL, $reference_2_id = NULL,$verification = 1, $received_or_refused_by = NULL, $rider_id = NULL , $remarks_id = NULL) {
       $shipment_journey = new ShipmentsJourney();
 
       $shipment_journey->shipment_id = $shipment_id;
@@ -37,6 +37,7 @@ class ShipmentsJourneyController extends Controller
       $shipment_journey->consignee_status_id = $consignee_status_id;
       $shipment_journey->status_reason_id = $status_reason_id;
       $shipment_journey->remarks = $remarks;
+      $shipment_journey->status_sub_reason_id = $remarks_id;
       $shipment_journey->user_id = $user_id;
       $shipment_journey->admin_id = $admin_id;
       $shipment_journey->rider_id = $rider_id;
