547840760fd8b9d99abdde62ac17e8b3915b72ce
Abdul Ghani CHishti <ghani.chisti@trax.pk>
2022-06-17 10:35:49 +0500

    changes

--- app/Http/Controllers/Admins/DeliveryController.php
+++ app/Http/Controllers/Admins/DeliveryController.php
@@ -2237,6 +2237,8 @@ class DeliveryController extends Controller
 
     public function receive_delivery_status_submit(Request $request)
     {
+        dd($request->cnic,$request->relation);
+        
         $now = Carbon::now();
         $end_of_the_day = Carbon::today()->endOfDay()->addMinute(2);
 
