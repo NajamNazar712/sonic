4dc2bc301d526accefa1895e4f5c2fe36709ca31
Maaz Khan <Maaz.Khan@Trax.pk>
2022-06-18 15:57:14 +0500

    done

--- resources/views/admin/otp/rider.blade.php
+++ resources/views/admin/otp/rider.blade.php
@@ -17,8 +17,8 @@
                     @csrf
                     <div class="row justify-content-center">
                         <div class="input-group col-3">
-                            <label class="mr-2 font-medium-3"><b>Rider OTP:</b></label>
-                            <div class="form-group ml-1">
+                            <label class="mr-2 font-medium-3"><b>Rider Login OTP:</b></label>
+                            <div class="form-group">
                                 <input type="checkbox" name="rider_otp_toggle" id="rider_otp_toggle" class="switchery rider_otp_toggle" data-size="sm" data-switchery="true" @if(isset($setting->setting_value) && $setting->setting_value == 1) checked @endif>
                             </div>
                         </div>
