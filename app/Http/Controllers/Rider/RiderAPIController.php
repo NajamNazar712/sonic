2cf039bf78b2d0634cfd586abf9a4b66777cae0c
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-06-21 16:49:06 +0500

    changes

--- app/Http/Controllers/Rider/RiderAPIController.php
+++ app/Http/Controllers/Rider/RiderAPIController.php
@@ -8536,7 +8536,7 @@ class RiderAPIController extends Controller
         $to_date = $request->get('to_date');
         $rider_incentives = RidersIncentive::where('rider_id', $rider_id);
         if ($to_date) {
-            $rider_incentives = $rider_incentives->whereBetween('created_at', [$from_date . ' 00:00:00', $to_date . ' 23:59:59']);
+            $rider_incentives = $rider_incentives->whereBetween('date', [$from_date . ' 00:00:00', $to_date . ' 23:59:59']);
 
             $total_payable = DeliveryNote::where('rider_id', $rider_id)
                 ->whereBetween('created_at', [$from_date . ' 00:00:00', $to_date . ' 23:59:59'])
@@ -8546,7 +8546,7 @@ class RiderAPIController extends Controller
                 ->whereBetween('date', [$from_date . ' 00:00:00', $to_date . ' 23:59:59'])
                 ->where('rider_id', $rider_id)->first();
         } else {
-            $rider_incentives = $rider_incentives->whereDate('created_at', $from_date);
+            $rider_incentives = $rider_incentives->whereDate('date', $from_date);
 
             $total_payable = DeliveryNote::where('rider_id', $rider_id)
                 ->whereDate('created_at', $from_date)
@@ -8563,7 +8563,7 @@ class RiderAPIController extends Controller
             foreach ($rider_incentives as $rider_incentive) {
                 $datum = array();
                 $payable = DeliveryNote::where('rider_id', $rider_id)
-                    ->whereDate('created_at', date('Y-m-d',strtotime($rider_incentive->created_at)))
+                    ->whereDate('created_at', date('Y-m-d',strtotime($rider_incentive->date)))
                     ->sum('received_cod_amount');
                 $datum['created_at'] = date('Y/m/d',strtotime($rider_incentive->created_at));
                 $datum['pickup_shipments'] = $rider_incentive->pickup_shipments;
