eb2c84589bc02a48a6be29f9e79c17aeaab42765
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-06-29 12:32:46 +0500

    changes

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -135,7 +135,7 @@ class NotificationsController extends Controller
         $notification_history->message = $body;
         $notification_history->screen_id = $screen;
         $notification_history->save();
-        dd($notification_history->get());
+        dd($notification_history->employee_id);
         dispatch(new ProcessPushNotification($notification_history));
     }
 
