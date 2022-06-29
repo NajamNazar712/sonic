65167448e418c67ed2516b52994fdf13cbfb9030
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-06-29 17:31:40 +0500

    changes

--- app/Http/Controllers/AdminAPIController.php
+++ app/Http/Controllers/AdminAPIController.php
@@ -6123,7 +6123,8 @@ class AdminAPIController extends Controller
         }
         if($leads->exists()){
             $leads->orderBy('leads.requested_date', "DESC");
-            $leads = $leads->paginate(10);
+            $leads = $leads->get();
+//            $leads = $leads->paginate(10);
             $data = array();
             foreach ($leads as $lead){
                 $datum = array();
