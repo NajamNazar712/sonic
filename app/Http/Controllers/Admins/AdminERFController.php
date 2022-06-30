caf1babe726f235ad5a78d8c8d21a6dae380b3c6
Abdul Ghani CHishti <ghani.chisti@trax.pk>
2022-06-30 12:22:58 +0500

    changes

--- app/Http/Controllers/Admins/AdminERFController.php
+++ app/Http/Controllers/Admins/AdminERFController.php
@@ -164,7 +164,7 @@ class AdminERFController extends Controller
                     $ids = EmployeeRequisitionReplacement::where('er_id',$erf->erf_id)->select('trax_id')->get();
                     foreach ($ids as $id){
                         $name = Admin::where('trax_id',$id->trax_id)->select('name')->first();
-                        $trax_id.= $name->name.",<br>";
+                        $trax_id.= $name->name.",";
                     }
                     return $trax_id;
                 }
