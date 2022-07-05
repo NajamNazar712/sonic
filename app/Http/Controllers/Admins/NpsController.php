9f54979c8f23b601049a00b651b32c9a4a8ff3c7
mohsin sajjad <mohsin.sajjad@trax.pk>
2022-07-05 12:41:49 +0500

    percenatge fix

--- app/Http/Controllers/Admins/NpsController.php
+++ app/Http/Controllers/Admins/NpsController.php
@@ -380,7 +380,7 @@ class NpsController extends Controller
             $promoter_per = (!empty($promoter_per) ? ($promoter_per/$sum)*100 : 0 ) ;
             $passive_per = (!empty($passive_per) ? ($passive_per/$sum)*100 : 0 ) ;
             $detractors_per = (!empty($detractors_per) ? ($detractors_per/$sum)*100 : 0 ) ;
-            $nps_percentage = (!empty($promoter_per) && !empty($detractors_per)) ? $promoter_per - $detractors_per  : 0;
+            $nps_percentage = (!empty($promoter_per) || !empty($detractors_per)) ? $promoter_per - $detractors_per  : 0;
 
             $data['sum_question_and_response'] = $sum_question_and_response;
             $data['total_question'] = $total_question;
