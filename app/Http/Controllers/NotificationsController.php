3c70d236561bb0a584f1fee47e5f8259b00a4208
Shariq Raees <shariq.raees@trax.pk>
2022-07-06 15:07:25 +0500

    changes

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -9443,7 +9443,7 @@ class NotificationsController extends Controller
                                 $body = str_replace('[link]', $link, $body);
                             }
 
-                            self::email($subject, $body,$val->email);
+                            self::sms($body, $val->phone);
                         }
                     }
                 }
