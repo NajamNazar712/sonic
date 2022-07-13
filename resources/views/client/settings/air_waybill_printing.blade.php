fe13ba13a7a1f96d2232809a342ca6adc1ffb4c1
mohsin sajjad <mohsin.sajjad@trax.pk>
2022-07-13 12:08:28 +0500

    waybill label and width fixes

--- resources/views/client/settings/air_waybill_printing.blade.php
+++ resources/views/client/settings/air_waybill_printing.blade.php
@@ -12,47 +12,65 @@
                 @include('client.inc.messages')
                 <form id="settings_form" class="form-horizontal text-center" method="POST" action="{{ route('cod.settings.air_waybill_printing.store') }}" novalidate="novalidate">
                     {{ csrf_field() }}
-                    <div class="row justify-content-center">
-                        <div class="col-5 col-sm-4 col-md-3 col-lg-2">
+
+                        <div class="row justify-content-center">
+                            <div class="col-4">
                                 <div class="form-group">
                                     <div class="input-group">
                                         @if($air_waybill != null)
-                                            <div id="info_display" class="form-group text-center p-1 border border-light rounded">
-                                                <label class="d-block">Show Information on Air Waybill for all Shipments</label>
+                                            <div id="info_display" class="form-group text-center p-1" style="display: contents">
+                                                <label class="d-block">Show Information on Air Waybill (Default)</label>
                                                 @if($air_waybill->information == 1)
                                                     <input type="checkbox" name="information_display" class="switch hidden" id="information_display" checked="checked">
                                                 @else
                                                     <input type="checkbox" name="information_display" class="switch hidden" id="information_display">
                                                 @endif
                                             </div>
-                                            <input type="text" name="air_waybill_printing_count" class="form-control air_waybill_printing_count" placeholder="Air Waybill*" data-rule-required="true" data-msg-required="Air Waybill Print Count is required" value="{{ $air_waybill->print_count }}" data-rule-min="1" data-msg-min="Air Waybill Print Count can not be less than 1" data-rule-max="3" data-msg-min="Air Waybill Print Count can not be more than 3">
+{{--                                            <input type="text" name="air_waybill_printing_count" class="form-control air_waybill_printing_count" placeholder="Air Waybill*" data-rule-required="true" data-msg-required="Air Waybill Print Count is required" value="{{ $air_waybill->print_count }}" data-rule-min="1" data-msg-min="Air Waybill Print Count can not be less than 1" data-rule-max="3" data-msg-min="Air Waybill Print Count can not be more than 3">--}}
                                         @else
-                                            <div id="info_display" class="form-group text-center p-1 border border-light rounded">
+                                            <div id="info_display" class="form-group text-center p-1" style="display: contents">
                                                 <label class="d-block">Show Information on Air Waybill for all Shipments</label>
                                                 <input type="checkbox" name="information_display" class="switch hidden" id="information_display" checked="checked">
                                             </div>
-                                            <input type="text" name="air_waybill_printing_count" class="form-control air_waybill_printing_count" placeholder="Air Waybill*" data-rule-required="true" data-msg-required="Air Waybill Print Count is required" value="1" data-rule-min="1" data-msg-min="Air Waybill Print Count can not be less than 1" data-rule-max="3" data-msg-min="Air Waybill Print Count can not be more than 3">
+{{--                                            <input type="text" name="air_waybill_printing_count" class="form-control air_waybill_printing_count" placeholder="Air Waybill*" data-rule-required="true" data-msg-required="Air Waybill Print Count is required" value="1" data-rule-min="1" data-msg-min="Air Waybill Print Count can not be less than 1" data-rule-max="3" data-msg-min="Air Waybill Print Count can not be more than 3">--}}
                                         @endif
-                                        <div class="input-group-append">
-                                            <span class="input-group-text">No. of Prints</span>
-                                        </div>
+{{--                                        <div class="input-group-append">--}}
+{{--                                            <span class="input-group-text">No. of Prints</span>--}}
+{{--                                        </div>--}}
                                     </div>
                                 </div>
+                            </div>
                         </div>
-                        <div class="col-5 col-sm-4 col-md-3 col-lg-2">
+                        <div class="row justify-content-center">
+                            <div class="col-4">
                                 <div class="form-group">
                                     <div class="input-group">
-                                            <div id="info_display" class="form-group text-center p-1 border border-light rounded">
-                                                <label class="d-block">Page Brake After Each Item &nbsp;</label>
+                                            <div id="info_display" class="form-group text-center p-1" style="display: contents">
+                                                <label class="d-block">Air Waybill Page Break &nbsp;</label>
                                                 <input type="checkbox" name="page_breaker"  class="switch hidden" id="page_breaker" @if(isset($air_waybill->page_breaker)) @if($air_waybill->page_breaker == 1)   checked="checked" @endif @endif >
                                             </div>
                                     </div>
                                 </div>
-
-
+                            </div>
                         </div>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Update</button>
+                        <div class="row justify-content-center">
+                            <div class="col-4">
+                            <div class="form-group" style="width: 88%">
+                                <div class="input-group">
+                                        <label class="d-block">No of Air Waybill Print &nbsp;</label>
+                                        @if($air_waybill != null)
+                                            <input type="text" name="air_waybill_printing_count" class="form-control air_waybill_printing_count" placeholder="Air Waybill*" data-rule-required="true" data-msg-required="Air Waybill Print Count is required" value="{{ $air_waybill->print_count }}" data-rule-min="1" data-msg-min="Air Waybill Print Count can not be less than 1" data-rule-max="3" data-msg-max="Air Waybill Print Count can not be more than 3">
+                                            @else
+                                            <input type="text" name="air_waybill_printing_count" class="form-control air_waybill_printing_count" placeholder="Air Waybill*" data-rule-required="true" data-msg-required="Air Waybill Print Count is required" value="1" data-rule-min="1" data-msg-min="Air Waybill Print Count can not be less than 1" data-rule-max="3" data-msg-max="Air Waybill Print Count can not be more than 3">
+                                        @endif
+
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Update</button>
+
                 </form>
             </div>
         </div>
@@ -84,6 +102,13 @@
             width: auto !important;
             text-align: left;
         }
+        .btn-group{
+            margin-left: 20px;
+        }
+        .d-block{
+            margin-top: 10px;
+        }
+
     </style>
 @endsection
 
