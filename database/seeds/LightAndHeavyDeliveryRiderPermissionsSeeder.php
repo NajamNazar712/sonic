5df5078880ba2b94798eff376875c5213d132da2
Abdul Ghani CHishti <ghani.chisti@trax.pk>
2022-07-13 16:53:12 +0500

    changes in permission seeder

--- database/seeds/LightAndHeavyDeliveryRiderPermissionsSeeder.php
+++ database/seeds/LightAndHeavyDeliveryRiderPermissionsSeeder.php
@@ -21,9 +21,9 @@ class LightAndHeavyDeliveryRiderPermissionsSeeder extends Seeder
         ));
 
         DB::table('activity_trail_actions')->insert(array(
-            array('id' => 552, 'screen_name' => 'Vendor Invoice', 'action'=> 'View'),
-            array('id' => 553, 'screen_name' => 'Vendors Accounts List', 'action'=> 'View'),
-            array('id' => 554, 'screen_name' => 'Vendors Accounts List', 'action'=> 'View'),
+            array('id' => 552, 'screen_name' => 'Rider Category ByPass Request', 'action'=> 'View'),
+            array('id' => 553, 'screen_name' => 'Rider Category ByPass Request Excel', 'action'=> 'View'),
+            array('id' => 554, 'screen_name' => 'Rider Category ByPass Weight', 'action'=> 'View'),
         ));
 
 
