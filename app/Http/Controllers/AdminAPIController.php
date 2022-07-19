ffa1fa0b47e8f56e865061a8565e40cfde885721
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-07-19 16:33:39 +0500

    changes

--- app/Http/Controllers/AdminAPIController.php
+++ app/Http/Controllers/AdminAPIController.php
@@ -8580,7 +8580,7 @@ class AdminAPIController extends Controller
 
     public function return_note_create(Request $request)
     {
-        return response()->json(['status'=>0, 'trackings' => gettype($request->tracking), 'open_box' => gettype($request->open_box)]);
+        return response()->json(['status'=>0, 'trackings' => $request->tracking, 'open_box' => gettype($request->open_box)]);
         $tracking_numbers = explode(',', $request->shipment_ids);
         $open_box_ids = explode(',',$request->open_box_ids);
         $trackings = Shipments::whereIn('tracking_number', $tracking_numbers)->pluck('id')->toArray();
