c5a8fab748a2561a370d25cbc407f9a1ab8da934
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-07-19 18:22:30 +0500

    changes

--- app/Http/Controllers/AdminAPIController.php
+++ app/Http/Controllers/AdminAPIController.php
@@ -7863,6 +7863,7 @@ class AdminAPIController extends Controller
                         if ($leave->exists()) {
                             return response()->json(['status' => 1, 'message' => 'Leave Request Already Submitted & Pending for Approval']);
                         }
+                        dd($employee->line_manager->admin->id);
                         $leave_request = new EmployeeLeave();
                         $leave_request->employee_id = $employee->id;;
                         $leave_request->employee_type_id = 1;
