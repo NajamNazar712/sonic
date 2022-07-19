c3fc377e93bf962d55a2824fc4fd80f12d4da843
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-07-19 11:28:18 +0500

    changes

--- app/Http/Controllers/AdminAPIController.php
+++ app/Http/Controllers/AdminAPIController.php
@@ -8575,4 +8575,27 @@ class AdminAPIController extends Controller
             return ['status' => 1, 'error' => 'No Shipment Item with given Item ID is present'];
         }
     }
+
+    public function get_return_note_list(Request $request){
+
+        $role_id = $request->role_id;
+        $admin_id = $request->admin_id;
+        $admin_hubs = AdminHub::where('admin_id',$admin_id)->pluck('hub_id')->toArray();
+
+        $deliveries = ReturnNote::join('cities AS oc', 'return_notes.hub_id', '=', 'oc.id')
+            ->join('riders', 'return_notes.rider_id', '=', 'riders.id')
+            ->join('admins','admins.id','=','return_notes.admin_id')
+            ->select(['return_notes.id as return_note', 'return_notes.id','return_notes.id as return_note_id','oc.name as hub','riders.name as rider','admins.name as assignee','return_notes.created_at','return_notes.shipments_count','return_notes.shipments_count as shipments_count_link','return_notes.status',DB::raw('(SELECT COUNT(shipment_id) FROM return_note_shipments WHERE return_note_id = return_notes.id AND status = 0) AS shipments_unverified_count'), DB::raw('(SELECT COUNT(id) FROM shipments_journey where shipper_status_id in (25, 31, 38) and reference_1_id = return_notes.id and verification = 1 ) as delivered_to_shipper_count')])
+            ->where('return_notes.status',0);
+
+        if ($role_id != 1) {
+            $deliveries = $deliveries->whereIn('oc.hub_id', $admin_hubs);
+        }
+
+        if($deliveries->exists()){
+            $deliveries = $deliveries->get();
+            return response()->json(['status' => 0, "return_notes" => $deliveries]);
+        }
+        return response()->json(['status' => 1, 'message' => 'No Return Note Found!']);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -413,6 +413,7 @@ Route::name('api.')->group(function () {
                 Route::get('index', 'AdminAPIController@return_create_index')->name('index');
                 Route::post('get_riders', 'AdminAPIController@get_riders_by_hub')->name('get_riders');
                 Route::post('get_shipment_details', 'AdminAPIController@get_shipment_details')->name('get_shipment_details');
+                Route::get('receive_list', 'AdminAPIController@get_return_note_list')->name('receive_list');
             });
 
         });
