b908a24f317c30d4f3048198d5429a7f28dd2468
Anas_Anwer <anas.anwer@trax.pk>
2022-07-19 14:43:15 +0500

    changes

--- database/migrations/2022_07_04_144459_create_shipment_journey_consignee_refused_sub_reasons_table.php
+++ database/migrations/2022_07_04_144459_create_shipment_journey_consignee_refused_sub_reasons_table.php
@@ -16,7 +16,7 @@ class CreateShipmentJourneyConsigneeRefusedSubReasonsTable extends Migration
         Schema::create('shipment_journey_consignee_refused_sub_reasons', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('shipment_journey_id')->index('shipment_journey_id');
-            $table->integer('status_sub_reason_id')->index();
+            $table->integer('status_sub_reason_id')->index('status_sub_reason_id');
             $table->integer('status')->default(1);
             $table->integer('updated_by')->nullable();
             $table->timestamps();
