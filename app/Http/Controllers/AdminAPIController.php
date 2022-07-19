cdc942736915c77f46fc52f2a2f4544f8dc5364a
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-07-19 18:15:57 +0500

    changes

--- app/Http/Controllers/AdminAPIController.php
+++ app/Http/Controllers/AdminAPIController.php
@@ -7751,7 +7751,6 @@ class AdminAPIController extends Controller
                         $leave_types = LeaveType::whereIn('id', [1, 2, 5, 6])->select('id', 'name')->get();
                     }
                 }
-                dd($employee->line_manager_id." ".$employee->id);
                 if ($employee->line_manager_id != null) {
                     $data = array();
                     $data['trax_id'] = $employee->trax_id;
