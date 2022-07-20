a7d9f3a24da0278ef8590aa82bc23311d9ca2031
Abdul Ghani CHishti <ghani.chisti@trax.pk>
2022-07-20 10:45:01 +0500

    changes in migration

--- database/migrations/2022_07_14_140751_create_employee_logs_table.php
+++ database/migrations/2022_07_14_140751_create_employee_logs_table.php
@@ -18,10 +18,8 @@ class CreateEmployeeLogsTable extends Migration
             $table->string('employee_id')->index();
             $table->integer('employee_type_id')->index()->nullable(); // rider or staff
             $table->integer('staff_category_id')->index()->nullable(); //
-            $table->integer('request_status_id')->index()->nullable(); // blocked ki 2 h (activate/deactivate)
             $table->integer('status_id')->index()->nullable(); // active,inactive n active no info ()
             $table->integer('rider_type_id')->index()->nullable(); // rider incentive (2)/rider perment (1)
-            $table->integer('first_inactive')->nullable();
             $table->integer('blacklist')->nullable();
             $table->integer('update_pin')->nullable();
             $table->integer('updated_by')->index();
