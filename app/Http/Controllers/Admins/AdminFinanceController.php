16028e4ef72dc4153e138faa3cf2abc4e330603b
Muhammad Waqas <muhammad.waqas@trax.pk>
2022-07-26 02:21:23 +0500

    fix

--- app/Http/Controllers/Admins/AdminFinanceController.php
+++ app/Http/Controllers/Admins/AdminFinanceController.php
@@ -1724,7 +1724,7 @@ class AdminFinanceController extends Controller
                                         ShipmentsPaymentJourneyController::add($shipment_id, 4, Auth::id());
                                     }
 
-                                    ShipmentsJourneyController::add($shipment_id, 13, 13, NULL, NULL, NULL, Auth::id());
+                                    ShipmentsJourneyController::add($shipment_id, 13, 13, NULL, "Shipment Reverted", NULL, Auth::id());
 
                                     if ($shipment->packaging_material_request == 1) {
                                         $packaging_material_shipment = PackagingMaterialRequest::where('tracking_number', $shipment->tracking_number)->first();
@@ -2471,7 +2471,7 @@ class AdminFinanceController extends Controller
                                     ShipmentsPaymentJourneyController::add($request->id, 4, Auth::id(), '', NULL, 1);
                                 }
 
-                                ShipmentsJourneyController::add($request->id, 13, 13, NULL, NULL, NULL, Auth::id());
+                                ShipmentsJourneyController::add($request->id, 13, 13, NULL, "Shipment Reverted", NULL, Auth::id());
 
                                 if ($shipment->packaging_material_request == 1) {
                                     $packaging_material_shipment = PackagingMaterialRequest::where('tracking_number', $shipment->tracking_number)->first();
