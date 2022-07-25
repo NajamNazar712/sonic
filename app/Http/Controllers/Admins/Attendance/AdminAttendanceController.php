2747dc4d027312294d334444c82e73cc39b81cc2
Muhammad Waqas <muhammad.waqas@trax.pk>
2022-07-25 21:14:08 +0500

    fix

--- app/Http/Controllers/Admins/Attendance/AdminAttendanceController.php
+++ app/Http/Controllers/Admins/Attendance/AdminAttendanceController.php
@@ -335,12 +335,11 @@ class AdminAttendanceController extends Controller
         }
 
         $attendances = EmployeeAttendance::leftjoin('employees as a', 'a.id', 'employee_attendances.employee_id')
-            ->leftjoin('cities as c', 'c.id', 'a.default_hub_id')
+            ->leftjoin('cities as c', 'c.id', 'a.city_id')
             ->leftjoin('riders as r', 'r.id', 'employee_attendances.employee_id')
             ->leftjoin('cities as rc','rc.id','=','r.city_id')
-            ->leftjoin('admin_roles as ar', 'ar.id', 'a.role_id')
-            ->leftjoin('admin_departments as ad', 'ad.id', 'ar.department_id')
-            ->leftjoin('employee_designations as ed', 'ed.id', 'a.designation_id')
+            ->leftjoin('admin_departments as ad', 'ad.id', 'a.department_id')
+            ->leftjoin('employee_designations as ed', 'ed.id', 'a.designation')
             ->leftjoin('rider_types as rt', 'rt.id', 'r.rider_type_id')
             ->select('employee_attendances.employee_id','a.name as admin_name', 'a.trax_id as trax_id','ed.name as designation_name', 'r.name as rider_name', 'r.trax_id as rider_trax_id', 'rt.name as rider_type', 'rt.id as rider_type_id', 'ad.name as department', 'ad.id as department_id', 'employee_attendances.employee_type','c.hub_id');
 
