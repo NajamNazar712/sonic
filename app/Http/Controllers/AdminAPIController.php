9f21be9aaabbebf1d28b59bdcc588abd586fea22
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-07-25 15:15:31 +0500

    changes

--- app/Http/Controllers/AdminAPIController.php
+++ app/Http/Controllers/AdminAPIController.php
@@ -8592,7 +8592,7 @@ class AdminAPIController extends Controller
             $invalid_shipments = NULL;
             if (!empty($trackings)) {
                 foreach ($trackings as $shipment_id) {
-                    $shipment_details = Shipment::whereIn('tracking_number', $shipment_id);
+                    $shipment_details = Shipment::where('tracking_number', $shipment_id);
                     if ($shipment_details->exists()) {
                         $shipment_details = $shipment_details->first();
                         if (in_array($shipment_details->shipper_status_id, $return_statuses)) {
