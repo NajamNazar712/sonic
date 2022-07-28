41710f63f5c3c61b0f005cc8367c734ddb93910a
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-07-28 12:40:11 +0500

    changes

--- resources/views/admin/reports/mms.blade.php
+++ resources/views/admin/reports/mms.blade.php
@@ -119,7 +119,6 @@
                         <th class="border-primary border-darken-1">Zone</th>
                         <th class="border-primary border-darken-1">Delivered/Returned Date</th>
                         <th class="border-primary border-darken-1">Received/Refused By</th>
-                        <th class="border-primary border-darken-1">CNIC</th>
                         <th class="border-primary border-darken-1">Aging (Days)</th>
                     </tr>
                     </thead>
@@ -348,7 +347,6 @@
                             head.push('Zone');
                             head.push('Delivered/Returned Date');
                             head.push('Received/Refused By');
-                            head.push('CNIC');
                             head.push('Aging (Days)');
                             $.each(result.data, function(index, values) {
                                 row = [];
@@ -365,7 +363,6 @@
                                 row.push(values.zone);
                                 row.push(values.delivered_or_returned);
                                 row.push(values.received_or_refused_by);
-                                row.push(values.cnic);
                                 row.push(values.aging);
 
                                 body.push(row);
@@ -429,7 +426,6 @@
                     { data:'zone' ,name: 'z.name', class: 'align-middle zone'},
                     { data: 'delivered_or_returned' ,name: 'dr.created_at', class: 'align-middle delivered_or_returned'},
                     { data: 'received_or_refused_by' ,name: 'dr.received_or_refused_by', class: 'align-middle received_or_refused_by'},
-                    { data: 'cnic' ,name: 'dr.cnic', class: 'align-middle cnic'},
                     { data: 'aging' ,name: 'aging', class: 'align-middle aging', orderable: false, searchable: false},
                 ],
                 rowCallback: function(row, data, index) {
