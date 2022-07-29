0fa3584050468d8e305111eee337cb870e0d8e87
Nabeel973 <nabeelsiddiqui324@gmail.com>
2022-07-29 13:54:49 +0500

    seeder

--- database/seeds/UpdateManifestBagsStatusForInternationalShipmentsSeeder.php
+++ database/seeds/UpdateManifestBagsStatusForInternationalShipmentsSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Models\Admin\CargoManifest\CargoManifestBag;
+use App\Http\Models\Admin\CargoManifest\CargoManifestBagShipments;
 use Illuminate\Database\Seeder;
 
 class UpdateManifestBagsStatusForInternationalShipmentsSeeder extends Seeder
@@ -12,7 +13,29 @@ class UpdateManifestBagsStatusForInternationalShipmentsSeeder extends Seeder
      */
     public function run()
     {
-        $bags = CargoManifestBag::join('cities as c','c.id','cargo_manifest_bags.destination_hub_id')
-            ->select('cargo_manifest_bags.seal_number','cargo_manifest_bags.shipments','cargo_manifest_bags.actual_weight','cargo_manifest_bags.')
+        $bags = CargoManifestBag::where('status_id', 1)->whereHas('destination_hub', function ($query) {
+                $query->where('business_category_id',2);
+            });
+        $cargo_manifest_bags = $bags->get();
+        foreach($cargo_manifest_bags as $bags){
+           $bag_shipments = CargoManifestBagShipments::where('cargo_manifest_bag_id',$bags->id);
+           if($bag_shipments->exists()){
+               $bag_shipments = $bag_shipments->get();
+               foreach($bag_shipments as $bag_shipment){
+                    $shipment = Shipment::where('id',$bag_shipment->shipment_id)->first();
+                    if(in_array($shipment->shipper_status_id,[14,25])){
+                        $bag_shipment->status = 1;
+                        $bag_shipment->save();
+                    }
+               }
+               $received_shipments_count = CargoManifestBagShipments::where('cargo_manifest_bag_id',$bags->id)->where('status',1)->count();
+               if($bags->shipments == $received_shipments_count){
+                  $bags->received_shipments = $received_shipments_count;
+                  $bags->completed = 1;
+                  $bags->save();
+               }
+           }
+        }
+
     }
 }
