6692a443662b04d61d04854e809a4cbb707a3709
Nabeel973 <nabeelsiddiqui324@gmail.com>
2022-08-04 15:42:33 +0500

    changes

--- app/Http/Controllers/Admins/AdminHumanResourseController.php
+++ app/Http/Controllers/Admins/AdminHumanResourseController.php
@@ -5082,7 +5082,37 @@ class AdminHumanResourseController extends Controller
     }
 
     public function view_employee_confirmation(Request $request){
-        //
+        $employee = Employee::where('trax_id',$request->trax_id);
+        if($employee->exists()){
+            $employee = $employee->first();
+            $details = array();
+            $details['name'] = $employee->name;
+            $details['trax_id'] = $employee->trax_id;
+            $details['designation'] = $employee->designation->name;
+            $details['department'] = $employee->department->name;
+            $details['city'] = $employee->city->name;
+            $details['manager'] = ($employee->line_manager_id != null) ? $employee->department->name:'-';
+            $employee_confirmation = EmployeeConfirmation::where('employee_id',$employee->id)->first();
+            $details['review_period'] = $employee->joining_date . ' - ' .$employee_confirmation->probation_end_date;
+
+
+            $confirmation = EmployeeConfirmation::where('employee_id',$employee->id);
+            if($confirmation->exists()){
+                $confirmation = $employee_confirmation->latest()->first();
+                $rating = $confirmation->employee_confirmation;
+                dd($rating);
+
+
+            }
+            else{
+                return response()->json(['status' => 0, 'error' => 'Employee not found!']);
+            }
+
+            return response()->json(['status' => 1, 'success' => 'Employee found!','details' => $details]);
+        }
+        else{
+            return response()->json(['status' => 0, 'error' => 'Employee not found!']);
+        }
     }
 
 }
--- app/Http/Models/HR/EmployeeConfirmationRating.php
+++ app/Http/Models/HR/EmployeeConfirmationRating.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class EmployeeConfirmationRating extends Model
 {
-    //
+    public function employee_confirmation() {
+        return $this->belongsTo('App\Http\Models\HR\EmployeeConfirmation');
+    }
+
 }
