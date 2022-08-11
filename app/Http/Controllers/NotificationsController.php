b0821ce87b31689bbe3bad209c6345a77bea8aea
Anas_Anwer <anas.anwer@trax.pk>
2022-08-11 19:24:25 +0500

    changes

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -9531,7 +9531,7 @@ else if ($id == 178) {
                             $subject = str_replace('[name]', $employee->name, $subject);
                         }
 
-                        $body .=  PHP_EOL. PHP_EOL.'<img class="brand-logo trax" alt="Trax" src="' . asset('img/trax_logo_new.png') . '" width="100" height="50">'.PHP_EOL.'</p>
+                        $body .=  PHP_EOL. PHP_EOL.'<img class="brand-logo trax" alt="Trax" src="' . asset('img/trax_logo_new.png') . '" width="100" height="50"></p>
                     </div>
                         <p style="margin-top: 0px; margin-bottom: 0px; vertical-align: middle;">Copyright © ' . now()->year . ' By TRAX, All Rights Reserved.</p>
                     </div>';
@@ -9544,7 +9544,7 @@ else if ($id == 178) {
 //                        }else{
 //                            $to = ['muhammad.sohail@trax.pk','shahzad.ali@trax.pk'];
 //                        }
-                        $to = ['anas.anwer@trax.pk'];
+                        $to = ['anas.anwer95@gmail.com'];
 
 
 
