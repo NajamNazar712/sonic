37f7a839d538d3fdbfd46db7148e3c70845886dd
Anas_Anwer <anas.anwer@trax.pk>
2022-08-19 13:51:28 +0500

    changes

--- app/Http/Controllers/Admins/AdminHumanResourseController.php
+++ app/Http/Controllers/Admins/AdminHumanResourseController.php
@@ -3827,7 +3827,6 @@ class AdminHumanResourseController extends Controller
                 $payslip_pdf = $payslip_pdf->first();
                 $file_url = $payslip_pdf->file_path;
                 $file= public_path(). "/storage/". $file_url;
-                dd($file);
 
                 $headers = array(
                     'Content-Type: application/pdf',
