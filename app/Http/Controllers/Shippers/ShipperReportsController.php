8976b15e54fcd8bca46b1aaf0bace78089ea22ea
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-08-30 17:20:00 +0500

    changes

--- app/Http/Controllers/Shippers/ShipperReportsController.php
+++ app/Http/Controllers/Shippers/ShipperReportsController.php
@@ -1236,7 +1236,7 @@ class ShipperReportsController extends Controller
                 ->leftJoin('shipment_status_reason as ssr', 'ssr.id', '=', 'sjr.status_reason_id')
                 ->select('shipments.id as shipment_id','shipments.tracking_number','shipments.order_id as order_id','shipments.tracking_number as tracking_number_link', 'shipments.consignee_name','u.name as shipper','usi.pickup_address as shipper_address','ss.name as current_status','sj.created_at as arrival_date', 'shipments.created_at as booking_date','dc.name as destination','h.name as hub', 'dr.created_at as delivered_or_returned','z.name as zone', 'dc.id as destination_city_id', 'shipments.shipper_status_id as shipment_status', 'dr.received_or_refused_by', 'dr.cnic', 'dr.relation','ssr.name as reason')
                 ->whereNotIn('shipments.shipper_status_id',[1,17])
-                ->whereIn('u.id', session('user_id'))
+                ->where('u.id', session('user_id'))
                 ->whereBetween('sj.created_at', [$from,$to]);
 
             $from_id = DB::connection($connection)->table('shipments_journey')->select('id')->where('created_at', '>=', $from);
