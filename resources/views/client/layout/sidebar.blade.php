745e6761375ef97cf7a51f390cc16b5cf1d2dc61
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-09-05 14:57:43 +0500

    changes

--- resources/views/client/layout/sidebar.blade.php
+++ resources/views/client/layout/sidebar.blade.php
@@ -160,7 +160,7 @@
                             @endif
                         @endif
 
-                        @if (in_array(session('user_id'), [10358, 15636, 16292, 15587, 17363, 17747, 3324, 1091, 10104]))
+                        @if (in_array(session('user_id'), [15636, 16292, 15587, 17363, 17747, 3324, 1091, 10104]))
                             <li><a class="menu-item" href="{{ route('cod.reports.mms.index') }}">MMS Report</a></li>
                         @endif
 
