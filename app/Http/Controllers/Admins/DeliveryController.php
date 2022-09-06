ce17b66463037029ba3e79818b8f55a0184febec
Anas_Anwer <anas.anwer@trax.pk>
2022-09-06 17:33:18 +0500

    changes

--- app/Http/Controllers/Admins/DeliveryController.php
+++ app/Http/Controllers/Admins/DeliveryController.php
@@ -6342,12 +6342,12 @@ class DeliveryController extends Controller
             $payment_transactions = $payment_transactions->get();
             $payment_transaction_data = array();
             foreach ($payment_transactions as $index => $payment_transaction){
-                $payment_transaction_data['transaction_authentication_id'] = $payment_transaction->transaction_authentication_id;
-                $payment_transaction_data['tracking_number'] = $payment_transaction->tracking_number;
-                $payment_transaction_data['transaction_amount'] = $payment_transaction->transaction_amount;
-                $payment_transaction_data['created_at'] = $payment_transaction->created_at;
-                $payment_transaction_data['transaction_date'] = Carbon::parse($payment_transaction->transaction_date)->format('M d, Y');
-                $payment_transaction_data['transaction_time'] = Carbon::parse($payment_transaction->transaction_time)->format('H:i:s');
+                $payment_transaction_data[$index]['transaction_authentication_id'] = $payment_transaction->transaction_authentication_id;
+                $payment_transaction_data[$index]['tracking_number'] = $payment_transaction->tracking_number;
+                $payment_transaction_data[$index]['transaction_amount'] = $payment_transaction->transaction_amount;
+                $payment_transaction_data[$index]['created_at'] = $payment_transaction->created_at;
+                $payment_transaction_data[$index]['transaction_date'] = Carbon::parse($payment_transaction->transaction_date)->format('M d, Y');
+                $payment_transaction_data[$index]['transaction_time'] = Carbon::parse($payment_transaction->transaction_time)->format('H:i:s');
             }
             return response()->json(['status' => 1, 'transaction_data' => $payment_transaction_data]);
             
