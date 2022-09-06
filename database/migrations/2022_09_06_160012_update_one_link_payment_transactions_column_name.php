77cfa53c06552df9a44e41fdefc481ffadec7ade
Anas_Anwer <anas.anwer@trax.pk>
2022-09-06 16:03:20 +0500

    changes

---
+++ database/migrations/2022_09_06_160012_update_one_link_payment_transactions_column_name.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateOneLinkPaymentTransactionsColumnName extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('one_link_payment_transactions', function (Blueprint $table) {
+            $table->renameColumn('consumer_prefx', 'consumer_prefix');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
