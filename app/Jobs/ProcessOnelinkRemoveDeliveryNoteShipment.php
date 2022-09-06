71bb7eb2bd258feb5a1247e9a9c5c781c8870cfd
Anas_Anwer <anas.anwer@trax.pk>
2022-09-06 18:34:07 +0500

    changes

--- app/Jobs/ProcessOnelinkRemoveDeliveryNoteShipment.php
+++ app/Jobs/ProcessOnelinkRemoveDeliveryNoteShipment.php
@@ -10,20 +10,22 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 
-class ProcessOneLinkExpireDeliveryNote implements ShouldQueue
+class ProcessOnelinkRemoveDeliveryNoteShipment implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     protected $delivery_note_id;
+    protected $shipment_id;
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct(int $delivery_note_id)
+    public function __construct(int $delivery_note_id, int $shipment_id)
     {
-        $this->queue = 'one_link_delivery_note_expire';
+        $this->queue = 'one_link_delivery_note_shipment_remove';
         $this->delivery_note_id = $delivery_note_id;
+        $this->shipment_id = $shipment_id;
     }
 
     /**
@@ -33,14 +35,17 @@ class ProcessOneLinkExpireDeliveryNote implements ShouldQueue
      */
     public function handle()
     {
+
         $delivery_note_id = $this->delivery_note_id;
-        if($delivery_note_id != null){
+        $shipment_id = $this->shipment_id;
+        if($delivery_note_id != null && $shipment_id != null){
 
             try{
                 $client = new Client(['base_uri' => 'https://link1link.trax.pk/api/sonic/', 'http_errors' => FALSE, 'connect_timeout' => 60, 'timeout' => 60]);
-                $response = $client->delete('out_for_delivery_shipments', [
+                $response = $client->put('out_for_delivery_shipments', [
                     'form_params' => [
-                        'delivery_note_id' => $delivery_note_id
+                        'delivery_note_id' => $delivery_note_id,
+                        'shipment_id' => $shipment_id
                     ]
                 ]);
             }
