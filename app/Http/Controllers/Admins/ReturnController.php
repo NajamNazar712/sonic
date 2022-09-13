43dbfc44c45ae2addd68104479648fbe19ea145b
Anas_Anwer <anas.anwer@trax.pk>
2022-09-13 17:29:26 +0500

    changes

--- app/Http/Controllers/Admins/ReturnController.php
+++ app/Http/Controllers/Admins/ReturnController.php
@@ -1175,6 +1175,7 @@ class ReturnController extends Controller
                             continue;
                         }
                         $shipment_details->shipper_status_id = 20; //Confirmation Pending
+                        $shipment_details->consignee_status_id = 20; //Confirmation Pending
                         NotificationsController::send(15, 0, $shipment_details->id);
                         NotificationsController::send(16, 0, $shipment_details->id);
 
