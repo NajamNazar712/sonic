d15dc5b13930c7b4917676394b99ba2181b81f5c
Abdul Ghani CHishti <ghani.chisti@trax.pk>
2022-09-20 13:23:55 +0500

    minor change

--- resources/views/admin/return/index.blade.php
+++ resources/views/admin/return/index.blade.php
@@ -1221,7 +1221,7 @@
                     }
                 },
                 rowId: 'shId',
-                order: [[22, 'desc']],
+                order: [[23, 'desc']],
                 columns: [
                     {data: 'shId', orderable: false, searchable: false, class: 'text-center align-middle select p-1', targets: 0, render: function (data, type, row) {return '';}},
                     {data: 'id',defaultContent:'', orderable: false, searchable: false, class: 'align-middle serial_number'},
