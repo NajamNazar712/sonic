de82df8559fbd626691513f31378fd9b7d278e15
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-09-26 11:11:52 +0500

    changes

--- app/Models/Rider/RiderDeliveryNoteRequest.php
+++ app/Models/Rider/RiderDeliveryNoteRequest.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class RiderDeliveryNoteRequest extends Model
 {
-    //
+    protected $fillable=[
+        'hub_id','rider_id','route_id','shipments_count','updated_by','total_cod_amount','approved_by','approved_at','status','ordering'];
 }
--- app/Models/Rider/RiderDeliveryNoteRequestShipment.php
+++ app/Models/Rider/RiderDeliveryNoteRequestShipment.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class RiderDeliveryNoteRequestShipment extends Model
 {
-    //
+    protected $fillable=[
+        'request_note_id','shipment_id','notification','rider_information','open_box','ordering'];
 }
