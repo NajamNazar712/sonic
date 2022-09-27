f1273277ea408c99ff4d595462f87516ca38f4fc
Shariq Raees <shariq.raees@trax.pk>
2022-09-27 17:28:20 +0500

    changes

--- database/seeds/UpdateNotificationSeederForBankAlfalahEmail.php
+++ database/seeds/UpdateNotificationSeederForBankAlfalahEmail.php
@@ -14,7 +14,7 @@ class UpdateNotificationSeederForBankAlfalahEmail extends Seeder
         //
         $timestamp = \Carbon\Carbon::now()->format('Y-m-d H:i:s');
         DB::table('notifications')->insert(array(
-            array('id' => 191, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Complaint Shippers CRM Close Reason Email', 'type_id' => 1, 'subject' => 'CRM Close Reasons', 'body' => 'Dear [Shipper name],' .PHP_EOL. 'Following is the CRM request with Close Reason.', 'updated_by' => 7, 'status' => 0)
+            array('id' => 191, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Complaint Shippers CRM Close Reason Email', 'type_id' => 1, 'subject' => 'CRM Close Reason', 'body' => 'Dear [Shipper name],' .PHP_EOL. 'Following is the CRM request with Close Reason.', 'updated_by' => 7, 'status' => 0)
         ));
     }
 }
