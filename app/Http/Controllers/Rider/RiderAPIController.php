30a3159c4232bc97429c04216df91677c0ae12ea
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-09-27 10:43:51 +0500

    changes

--- app/Http/Controllers/Rider/RiderAPIController.php
+++ app/Http/Controllers/Rider/RiderAPIController.php
@@ -12789,7 +12789,6 @@ class RiderAPIController extends Controller
             $shipments_count = count($valid_shipments);
             if ($shipments_count != 0) {
                 $valid_shipments = $valid_shipments->toArray();
-                Shipment::whereIn('id', $valid_shipments)->update(['shipper_status_id' => 5, 'consignee_status_id' => 5]);
                 $total_cod_amount = Shipment::whereIn('id', $valid_shipments)->where(function ($query) {
                     $query->where('booking_type_id', '!=', 4)
                         ->orWhere(function ($sub_query) {
--- app/Models/Rider/RiderDeliveryNoteRequest.php
+++ app/Models/Rider/RiderDeliveryNoteRequest.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class RiderDeliveryNoteRequest extends Model
 {
     protected $fillable=[
-        'hub_id','rider_id','route_id','shipments_count','updated_by','total_cod_amount','approved_by','approved_at','status','ordering'];
+        'hub_id','rider_id','route_id','shipment_count','updated_by','total_cod_amount','approved_by','approved_at','status','ordering'];
 }
