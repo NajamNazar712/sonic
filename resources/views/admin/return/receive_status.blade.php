fa8b56e20969eee8ace62fdae64ecb600b5c8e00
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-10-03 17:32:22 +0500

    changes

--- resources/views/admin/return/receive_status.blade.php
+++ resources/views/admin/return/receive_status.blade.php
@@ -856,6 +856,7 @@
                                                     var tracking_numbers = '';
                                                     var route = '{!! route('admin.tracking.index') !!}';
                                                     UnblockPagePermanently();
+                                                    console.log(data.negative_shipper);
                                                     if(data.reason_mandatory_shipments.length > 0) {
                                                         $.each(data.reason_mandatory_shipments, function (index, tracking_number) {
                                                             tracking_numbers += '<u><a href=' + route + '?tracking_number=' + tracking_number + ' target="_blank">' + tracking_number + '</a></u><br>';
