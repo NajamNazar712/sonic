56175dc2c56963c979915d6a58878fa6a494700b
Anas_Anwer <anas.anwer@trax.pk>
2022-10-05 17:21:24 +0500

    changes

--- app/Http/Controllers/Admins/DeliveryController.php
+++ app/Http/Controllers/Admins/DeliveryController.php
@@ -4618,7 +4618,7 @@ class DeliveryController extends Controller
                 }
             })
             ->editColumn('cash_amount', function ($shipment) {
-                if($shipment->cash_amount >= 0){
+                if($shipment->cash_amount){
                     return number_format($shipment->cash_amount);
                 }
                 else{
