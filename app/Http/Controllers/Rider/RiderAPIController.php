a9d8c60f7126b047a8637b23301143697b0f4a6f
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-10-10 10:52:20 +0500

    changes

--- app/Http/Controllers/Rider/RiderAPIController.php
+++ app/Http/Controllers/Rider/RiderAPIController.php
@@ -12098,6 +12098,7 @@ class RiderAPIController extends Controller
                     }
                 }
             }
+            dd($only_shippers, $excluded_shippers, $all_shippers->setting_value, $delivery_otp);
 
             $nodes = array();
 
