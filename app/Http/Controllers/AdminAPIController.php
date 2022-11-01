3ec594d9fa09a576d6f601a2f92eddb6cfcfe3c1
Shariq Raees <shariq.raees@trax.pk>
2022-11-01 15:52:49 +0500

    changes

--- app/Http/Controllers/AdminAPIController.php
+++ app/Http/Controllers/AdminAPIController.php
@@ -7133,7 +7133,8 @@ class AdminAPIController extends Controller
                 $role_id = $request->admin_role_id;
                 if ($role_id == 81) {
                     $data['user_type'] = 2;
-                } elseif (in_array($role_id, [1, 2, 3, 4, 5, 6, 35, 52, 58, 70, 63])) {
+                } elseif ($employee->is_line_manager == 1) {
+                // } elseif (in_array($role_id, [1, 2, 3, 4, 5, 6, 35, 52, 58, 70, 63])) {
                     $data['user_type'] = 1;
                 } else {
                     $data['user_type'] = 0;
