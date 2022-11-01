a200c6394ed984da06be1814c97fd7cb668f0e70
Muzaffar Kareem <muzaffar.kareem@trax.pk>
2022-11-01 18:26:36 +0500

    changes

---
+++ database/migrations/2022_11_01_181404_update_one_link_out_for_delivery_shipment_payments_set_id_primary_key.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateOneLinkOutForDeliveryShipmentPaymentsSetIdPrimaryKey extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('one_link_out_for_delivery_shipment_payments', function (Blueprint $table) {
+            $table->Integer('id')->primary()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('one_link_out_for_delivery_shipment_payments', function (Blueprint $table) {
+            $table->dropPrimary('id');
+        });
+    }
+}
