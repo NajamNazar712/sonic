a610db30722172dd3756675da6a0a76c60541d59
Anas_Anwer <anas.anwer@trax.pk>
2022-11-01 20:15:40 +0500

    changes

---
+++ database/migrations/2022_11_01_181635_update_one_link_out_for_delivery_shipment_payments_add_id.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateOneLinkOutForDeliveryShipmentPaymentsAddId extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('one_link_out_for_delivery_shipment_payments', function (Blueprint $table) {
+            $table->dropColumn('id');
+            $table->increments('id')->first();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('one_link_out_for_delivery_shipment_payments', function (Blueprint $table) {
+//            $table->dropColumn('id');
+        });
+    }
+}
