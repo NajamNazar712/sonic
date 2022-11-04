00fba388131ca5225d2843acf23493c8f29b22fa
Nabeel973 <nabeelsiddiqui324@gmail.com>
2022-11-04 18:21:04 +0500

    adjustment

--- resources/views/admin/finance/add_shipment_adjustment.blade.php
+++ resources/views/admin/finance/add_shipment_adjustment.blade.php
@@ -17,48 +17,102 @@
 						<div class="card-body">
 							@include('admin.inc.messages')
 
-							<form id="search_form" class="form-inline mb-1 justify-content-center" novalidate="novalidate">
-								<div class="form-group">
-									<input type="text" name="tracking_numbers" class="form-control tracking_number" placeholder="Tracking Number*" data-rule-required="true" data-msg-required="Tracking Number is required">
-								</div>
-
-								<div class="form-group ml-1">
-									<button type="submit" name="search" class="btn btn-primary search" value="Search">Search</button>
-								</div>
-							</form>
-
-							<div class="shipment mt-2" id="shipment">
+							<div class="row toggle_row justify-content-center">
+								<h3 class="pr-1">Individual</h3>
+								<input type="checkbox" class="switchery" data-color="success" data-size="md" name="switch" id="switch"/>
+								<h3 class="pl-1">Bulk</h3>
 							</div>
-
-							@if (session('role_id') == 1 || in_array(137, session('permissions')))
-								<form id="add_adjustment_form" class="form-inline mb-1 justify-content-center mt-2 d-none" method="POST" action="{{ route('admin.finance.add_shipment_adjustment.store') }}" novalidate="novalidate">
-									{{ csrf_field() }}
-
-									<input type="hidden" name="shipment_id" class="shipment_id">
-
+							<div class="row individual_adjustemnt mt-4 justify-content-center">
+								<form id="search_form" class="form-inline mb-1 justify-content-center" novalidate="novalidate">
 									<div class="form-group">
-										<input type="text" name="payable" class="form-control payable" placeholder="Payable" data-rule-range="[-500000,500000]" data-msg-range="Payable needs to be from -500000 to 500000" data-rule-not="0" data-msg-not="Payable cannot be 0">
+										<input type="text" name="tracking_numbers" class="form-control tracking_number" placeholder="Tracking Number*" data-rule-required="true" data-msg-required="Tracking Number is required">
 									</div>
 
 									<div class="form-group ml-1">
-										<select name="adjustment_type" id="adjustment_types" class="select2 form-control" data-rule-required="true" data-msg-required="Adjustment Type is required">
-											@foreach($adjustment_types as $adjustment_type)
-												<option value="{{$adjustment_type->id}}">{{$adjustment_type->name}}</option>
-											@endforeach
-										</select>
-									</div>
-
-									<div class="form-group ml-1">
-										<input type="text" name="payable_remarks" class="form-control" placeholder="Remarks" data-rule-required="true" data-msg-required="Remarks is required">
-									</div>
-
-									<div class="form-group ml-1">
-										<button type="submit" name="add" class="btn btn-primary add" value="Add">Add</button>
+										<button type="submit" name="search" class="btn btn-primary search" value="Search">Search</button>
 									</div>
 								</form>
-							@endif
-						</div>
+
+								<div class="shipment mt-2" id="shipment">
+								</div>
+
+								@if (session('role_id') == 1 || in_array(137, session('permissions')))
+									<form id="add_adjustment_form" class="form-inline mb-1 justify-content-center mt-2 d-none" method="POST" action="{{ route('admin.finance.add_shipment_adjustment.store') }}" novalidate="novalidate">
+										{{ csrf_field() }}
+
+										<input type="hidden" name="shipment_id" class="shipment_id">
+
+										<div class="form-group">
+											<input type="text" name="payable" class="form-control payable" placeholder="Payable" data-rule-range="[-500000,500000]" data-msg-range="Payable needs to be from -500000 to 500000" data-rule-not="0" data-msg-not="Payable cannot be 0">
+										</div>
+
+										<div class="form-group ml-1">
+											<select name="adjustment_type" id="adjustment_types" class="select2 form-control" data-rule-required="true" data-msg-required="Adjustment Type is required">
+												@foreach($adjustment_types as $adjustment_type)
+													<option value="{{$adjustment_type->id}}">{{$adjustment_type->name}}</option>
+												@endforeach
+											</select>
+										</div>
+
+										<div class="form-group ml-1">
+											<input type="text" name="payable_remarks" class="form-control" placeholder="Remarks" data-rule-required="true" data-msg-required="Remarks is required">
+										</div>
+
+										<div class="form-group ml-1">
+											<button type="submit" name="add" class="btn btn-primary add" value="Add">Add</button>
+										</div>
+									</form>
+								@endif
+							</div>
+
+							<div class="row bulk_adjustment mt-4 d-none">
+								<form id="bulk_adjustment_form" class="form-horizontal w-100 p-2" method="POST" action="{{ route('admin.finance.add_shipment_adjustment.bulk_store') }}" novalidate="novalidate" enctype="multipart/form-data">
+									@csrf
+									<div class="row">
+										<div class="col-md-6">
+											<div class="form-group">
+												<input type="file" name="shipments" class="w-100 p-1 border-primary" title="Select File" data-rule-required="true" data-msg-required="File is required" data-rule-extension="xls|xlsx" data-msg-extension="Only file with extension xls or xlsx allowed" data-rule-accept="application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" data-msg-accept="Only Excel file allowed" data-rule-maxsize="5242880" data-msg-maxsize="File Size must not exceed 5 MB (5120 KB).">
+											</div>
+										</div>
+										<div class="col-md-3 mt-1">
+											<div class="form-group">
+												<button type="submit" class="btn btn-primary mb-2">Upload</button>
+											</div>
+										</div>
+
+										<div class="col-md-3 justify-content-end">
+											<div class="form-group text-right">
+												<a href="{{ asset('file/Bulk Adjsutment Template.xlsx') }}?v=09_06_2021" class="btn btn-primary btn-block"><i class="la la-download"></i> Download Template</a>
+											</div>
+										</div>
+									</div>
+								</form>
+
+									<div class="col-3">
+										<table class="table table-bordered">
+											<thead>
+											<tr role="row" class="bg-primary white text-center">
+												<th colspan="2" class="border-primary border-darken-1">Adjustment Types</th>
+											</tr>
+											<tr role="row" class="bg-primary bg-lighten-1 white">
+												<th class="text-center border-primary border-lighten-2">ID</th>
+												<th class="border-primary border-lighten-2">Name</th>
+											</tr>
+											</thead>
+											<tbody>
+											@foreach ($adjustment_types as $adjustment)
+												<tr role="row">
+													<td class="text-center">{{ $adjustment->id }}</td>
+													<td>{{ $adjustment->name }}</td>
+												</tr>
+											@endforeach
+											</tbody>
+										</table>
+									</div>
+
+							</div>
 					</div>
+				</div>
 				</div>
 			</div>
 		</div>
@@ -82,6 +136,19 @@
 
 	<script>
 		$(document).ready(function() {
+
+			$("#switch").on('change', function(){
+				if($("#switch").is(":checked")){
+					$('.individual_adjustemnt').addClass('d-none');
+					$('.bulk_adjustment').removeClass('d-none');
+				}else{
+					$('.individual_adjustemnt').removeClass('d-none');
+					$('.bulk_adjustment').addClass('d-none');
+				}
+			});
+
+
+
 			$('#adjustment_types').prepend('<option value="" selected="selected"></option>').select2({
 				width:'100%',
 				placeholder:"Select Adjustment Type",
@@ -234,6 +301,13 @@
 					return false;
 				}
 			});
+			$('#bulk_adjustment_form').validate({
+				errorClass: 'danger',
+				successClass: 'success',
+				errorPlacement: function(error, element) {
+					error.addClass('w-100').appendTo(element.parents('form'));
+				},
+			});
 
 			@if (session('role_id') == 1 || in_array(137, session('permissions')))
 				$.validator.addMethod('not', function(value, element, param) {
@@ -252,10 +326,23 @@
 					errorClass: 'danger',
 					successClass: 'success',
 					normalizer: function(value) {
-						return $.trim(value).replace(/,/g, '');
+						return $.trim(value);
 					},
 					errorPlacement: function(error, element) {
-						error.addClass('w-100').appendTo(element.parents('form'));
+						error.addClass('w-100').appendTo(element.parent('.form-group'));
+					},
+					submitHandler: function(form) {
+
+						swal({
+							title: 'Please Wait!',
+							text: 'Adjustments are being added!',
+							icon: 'info',
+							buttons: false,
+							closeOnClickOutside: false,
+							closeOnEsc: false
+						});
+
+						form.submit();
 					}
 				});
 			@endif
