747738ce711890b75affcc02196bb24148255eb1
Muhammad Waqas <abi.m.waqas@gmail.com>
2022-11-04 15:33:26 +0500

    fix

--- app/Http/Controllers/Admins/DeliveryController.php
+++ app/Http/Controllers/Admins/DeliveryController.php
@@ -1066,7 +1066,7 @@ class DeliveryController extends Controller
             ->join('admins', 'admins.id', '=', 'delivery_notes.admin_id')
             ->join('zones as z','oc.zone_id','=','z.id')
             ->leftjoin('admins as ad', 'ad.id', '=', 'delivery_notes.updated_by')
-            ->join('vigilance_verifications as vv', function ($join) {
+            ->leftjoin('vigilance_verifications as vv', function ($join) {
                 $join->on('vv.delivery_note_id', '=', 'delivery_notes.id')
                     ->where('vv.id', '=',
                         DB::raw('(select max(id) from vigilance_verifications where vigilance_verifications.delivery_note_id = delivery_notes.id)'));
