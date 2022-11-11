f6ea011b7f714436ee26e8e012fde88b4b7f11a6
Shariq Raees <shariq.raees@trax.pk>
2022-11-11 12:25:03 +0500

    changes

---
+++ database/migrations/2022_11_01_180235_update_one_link_out_for_delivery_shipment_payments_add_id.php
@@ -0,0 +1,34 @@
+<?php
+
+// use Illuminate\Support\Facades\Schema;
+// use Illuminate\Database\Schema\Blueprint;
+// use Illuminate\Database\Migrations\Migration;
+
+// class UpdateOneLinkOutForDeliveryShipmentPaymentsAddId extends Migration
+// {
+//     /**
+//      * Run the migrations.
+//      *
+//      * @return void
+//      */
+//     public function up()
+//     {
+//         //
+//         Schema::table('one_link_out_for_delivery_shipment_payments', function (Blueprint $table) {
+//             $table->Integer('id')->nullable()->first();
+//         });
+//     }
+
+//     /**
+//      * Reverse the migrations.
+//      *
+//      * @return void
+//      */
+//     public function down()
+//     {
+//         //
+//         Schema::table('one_link_out_for_delivery_shipment_payments', function (Blueprint $table) {
+//             $table->dropColumn('id');
+//         });
+//     }
+// }
