897868380e6bf605eb7f5cdcdf5ef2fd5a30e506
Shariq Raees <shariq.raees@trax.pk>
2022-11-11 12:35:09 +0500

    changes

--- database/migrations/2022_11_01_181404_update_one_link_out_for_delivery_shipment_payments_set_id_primary_key.php
+++ database/migrations/2022_11_01_181404_update_one_link_out_for_delivery_shipment_payments_set_id_primary_key.php
@@ -1,41 +1,41 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Migrations\Migration;
+// use Illuminate\Support\Facades\Schema;
+// use Illuminate\Database\Schema\Blueprint;
+// use Illuminate\Database\Migrations\Migration;
 
-class UpdateOneLinkOutForDeliveryShipmentPaymentsSetIdPrimaryKey extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        //
-//        Schema::table('one_link_out_for_delivery_shipment_payments', function (Blueprint $table) {
-////            $table->primary('id')->unsigned(true)->nullable(false)->change();
-//
-//        });
+// class UpdateOneLinkOutForDeliveryShipmentPaymentsSetIdPrimaryKey extends Migration
+// {
+//     /**
+//      * Run the migrations.
+//      *
+//      * @return void
+//      */
+//     public function up()
+//     {
+//         //
+// //        Schema::table('one_link_out_for_delivery_shipment_payments', function (Blueprint $table) {
+// ////            $table->primary('id')->unsigned(true)->nullable(false)->change();
+// //
+// //        });
 
 
 
-    }
+//     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        //
-//        Schema::table('one_link_out_for_delivery_shipment_payments', function (Blueprint $table) {
-//            $table->Integer('id',false)->change();
-//            $table->dropPrimary('id');
-//
-//
-//        });
-    }
-}
+//     /**
+//      * Reverse the migrations.
+//      *
+//      * @return void
+//      */
+//     public function down()
+//     {
+//         //
+// //        Schema::table('one_link_out_for_delivery_shipment_payments', function (Blueprint $table) {
+// //            $table->Integer('id',false)->change();
+// //            $table->dropPrimary('id');
+// //
+// //
+// //        });
+//     }
+// }
--- database/migrations/2022_11_01_181635_update_one_link_out_for_delivery_shipment_payments_add_id.php
+++ database/migrations/2022_11_01_181635_update_one_link_out_for_delivery_shipment_payments_add_id.php
@@ -1,37 +1,37 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Migrations\Migration;
+// use Illuminate\Support\Facades\Schema;
+// use Illuminate\Database\Schema\Blueprint;
+// use Illuminate\Database\Migrations\Migration;
 
-class UpdateOneLinkOutForDeliveryShipmentPaymentsAddId extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        //
-        Schema::table('one_link_out_for_delivery_shipment_payments', function (Blueprint $table) {
-            $table->dropColumn('id');
-        });
-        Schema::table('one_link_out_for_delivery_shipment_payments', function (Blueprint $table) {
-            $table->increments('id')->first();
-        });
-    }
+// class UpdateOneLinkOutForDeliveryShipmentPaymentsAddId extends Migration
+// {
+//     /**
+//      * Run the migrations.
+//      *
+//      * @return void
+//      */
+//     public function up()
+//     {
+//         //
+//         Schema::table('one_link_out_for_delivery_shipment_payments', function (Blueprint $table) {
+//             $table->dropColumn('id');
+//         });
+//         Schema::table('one_link_out_for_delivery_shipment_payments', function (Blueprint $table) {
+//             $table->increments('id')->first();
+//         });
+//     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        //
-        Schema::table('one_link_out_for_delivery_shipment_payments', function (Blueprint $table) {
-//            $table->dropColumn('id');
-        });
-    }
-}
+//     /**
+//      * Reverse the migrations.
+//      *
+//      * @return void
+//      */
+//     public function down()
+//     {
+//         //
+//         Schema::table('one_link_out_for_delivery_shipment_payments', function (Blueprint $table) {
+// //            $table->dropColumn('id');
+//         });
+//     }
+// }
