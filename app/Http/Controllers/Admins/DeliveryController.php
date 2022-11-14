7302c891fefccf08b2acfbafe11b2adc2fdfff7a
Shariq Raees <shariq.raees@trax.pk>
2022-11-14 19:21:00 +0500

    changes

--- app/Http/Controllers/Admins/DeliveryController.php
+++ app/Http/Controllers/Admins/DeliveryController.php
@@ -8861,7 +8861,7 @@ class DeliveryController extends Controller
         $admin = Auth::id();
         $request_id = $id;
         $delivery_request = RiderDeliveryNoteRequest::find($request_id);
-        print_r('1', $delivery_request , $request_id);
+        print_r('1');
         if ($delivery_request) {
             $request_shipments = RiderDeliveryNoteRequestShipment::where('request_note_id', $delivery_request->id)->whereIn('status', [0, 4]);
             $shipments = $request_shipments->pluck('shipment_id')->toArray();
@@ -8875,7 +8875,8 @@ class DeliveryController extends Controller
             $pending_status = array(2, 4, 6, 7, 8, 9, 10, 13, 15, 49, 55, 59);
             $valid_shipments = Shipment::whereIn('id', $shipments)->whereIn('shipper_status_id', $pending_status)->pluck('id');
             $shipments_count = count($valid_shipments);
-            print_r('2', $shipments_count);
+            print_r('2');
+            print_r($shipments_count);
             if ($shipments_count != 0) {
                 $valid_shipments = $valid_shipments->toArray();
                 $invalid_shipments = array_diff($shipments, $valid_shipments);
@@ -8907,9 +8908,11 @@ class DeliveryController extends Controller
                     'ordering' => $order,
                     'request_note_id' => $delivery_request->id
                 ]);
-                print_r('3', $note);
+                print_r('3');
+                print_r( $note);
                 if ($note) {
-                    print_r('4 inside note', $note);
+                    print_r('5 inside note');
+                    print_r($note);
                    
                     if (!$order) {  //Default
                         sort($valid_shipments); //sort_valid_shipments;
