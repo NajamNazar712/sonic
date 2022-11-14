664c20965ef0a6549bfde72aa916a5445b493933
Shariq Raees <shariq.raees@trax.pk>
2022-11-14 19:43:16 +0500

    changes

--- app/Http/Controllers/Admins/DeliveryController.php
+++ app/Http/Controllers/Admins/DeliveryController.php
@@ -8868,7 +8868,7 @@ class DeliveryController extends Controller
             $notifications = $request_shipments->where('notification', 1)->pluck('shipment_id')->toArray();
             $rider_informations = $request_shipments->where('rider_information', 1)->pluck('shipment_id')->toArray();
 
-            dd($request_shipments);
+            dd($request_shipments->get());
 
             if (count($shipments) == 0) {
                 return redirect()->back()->with('error',  'Shipments not entered!');
