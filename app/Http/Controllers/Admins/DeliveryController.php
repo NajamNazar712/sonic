1145dce2cc01a2db604a521791404c022374c850
Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>
2022-12-04 02:13:01 +0500

    Misroute Charges Fix
    Signed-off-by: Muhammad Yousuf Fazal <muhammad.yousuf@trax.pk>

--- app/Http/Controllers/Admins/DeliveryController.php
+++ app/Http/Controllers/Admins/DeliveryController.php
@@ -7196,6 +7196,7 @@ class DeliveryController extends Controller
                     $shipment->consignee_status_id = 49;
                     $shipment->save();
                     ShipmentChargesController::weight($shipment->id);
+                    ShipmentChargesController::fuel_surcharge($shipment_id);
                     ShipmentsJourneyController::add($shipment->id, 49, 49, NULL, NULL, NULL, Auth::id());
 
 
