ffaa07624359e2d1440ea62b741bc96f115bb135
Muhammad Waqas <abi.m.waqas@gmail.com>
2022-12-20 14:16:51 +0500

    fix

--- resources/views/admin/accounts/edit_rates.blade.php
+++ resources/views/admin/accounts/edit_rates.blade.php
@@ -4607,7 +4607,7 @@
                                     @if ($shipper->rate_status ==1 && $shipper->status == 3 && (session('role_id') == 1 || in_array(140, session('permissions'))))
                                         <button id="accountApproveActiveSubmit" type="submit" class="btn btn-outline-primary round btn-min-width mr-1 mb-1">Approve</button>
                                     @endif
-                                    @if (($shipper->rate_status ==0 && ($shipper->status == 1 || $shipper->status == 5) && (session('role_id') == 1 || in_array(8, session('permissions'))))|| ($shipper->rate_status ==1 && (session('role_id') == 1 || in_array(140, session('permissions')))))
+                                    @if ((in_array($shipper->rate_status, [0, 1, 2]) && ($shipper->status == 1 || $shipper->status == 5) && (session('role_id') == 1 || in_array(8, session('permissions'))))|| ($shipper->rate_status ==1 && (session('role_id') == 1 || in_array(140, session('permissions')))))
                                         <button id="accountRejectActiveSubmit" type="button" class="btn btn-outline-danger round btn-min-width mr-1 mb-1">Reject Rates</button>
                                     @endif
                                 </div>
