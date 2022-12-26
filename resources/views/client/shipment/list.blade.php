083430fa10953e60b772f28650bc4ea424ee8127
Anas_Anwer <anas.anwer@trax.pk>
2022-12-26 15:20:50 +0500

    changes

--- resources/views/client/shipment/list.blade.php
+++ resources/views/client/shipment/list.blade.php
@@ -18,20 +18,19 @@
 							@include('client.inc.messages')
 
 							<form id="consignee_phone_number_search_form" class="form-inline mb-1 justify-content-center" novalidate="novalidate">
-								<div class="col text-center">
-									<label class="font-medium-2 font-weight-bold block">Print By</label>
+								<div class="row justify-content-center">
 									<div class="form-group">
 										<label for="print_by_checkbox" class="font-medium-2 text-bold-600 mr-1">Consignee Phone Number</label>
 										<input type="checkbox" name="print_by_checkbox" id="print_by_checkbox" class="switchery print_by_checkbox" data-size="sm" data-switchery="true">
 										<label for="print_by_checkbox" class="font-medium-2 text-bold-600 ml-1">Order ID</label>
 									</div>
-								</div>
-								<div class="form-group">
-									<input type="text" name="consignee_phone_number" class="form-control consignee_phone_number" placeholder="Consignee Phone Number/Order ID" data-rule-required="true" data-msg-required="This Field is required">
-								</div>
+									<div class="form-group">
+										<input type="text" name="consignee_phone_number" class="form-control consignee_phone_number" placeholder="Consignee Phone Number/Order ID" data-rule-required="true" data-msg-required="This Field is required">
+									</div>
 
-								<div class="form-group ml-1">
-									<button type="submit" name="add" class="btn btn-primary add" value="Add">Add</button>
+									<div class="form-group ml-1">
+										<button type="submit" name="add" class="btn btn-primary add" value="Add">Add</button>
+									</div>
 								</div>
 							</form>
 
