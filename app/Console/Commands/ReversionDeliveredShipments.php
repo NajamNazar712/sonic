f646c5022ad09d468ed6e860bd82808f1567ad09
Anas_Anwer <anas.anwer@trax.pk>
2022-12-28 17:41:38 +0500

    changes

--- app/Console/Commands/ReversionDeliveredShipments.php
+++ app/Console/Commands/ReversionDeliveredShipments.php
@@ -77,6 +77,7 @@ class ReversionDeliveredShipments extends Command
                         foreach ($destination_shipments as $hub_id => $destination_shipment){
                             if(in_array($hub_id, $assigned_hubs)){
                                 foreach ($destination_shipments as $shipment_id => $shipment){
+                                    dd($shipment);
                                     $shipment_details[$shipment_id]['tracking_number'] = $shipment['tracking_number'];
                                     $shipment_details[$shipment_id]['consignee_name'] = $shipment['consignee_name'];
                                     $shipment_details[$shipment_id]['consignee_phone'] = $shipment['consignee_phone'];
