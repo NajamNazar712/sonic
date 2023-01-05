4bbd278217dc42a3189ee9bd2bfafb6e38924d3a
Anas_Anwer <anas.anwer@trax.pk>
2023-01-05 14:21:59 +0500

    changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -333,11 +333,10 @@ class Kernel extends ConsoleKernel
 
         $schedule->command('generate:usersotp')->monthlyOn(1, '00:00')->runInBackground();
 //        $schedule->command('email:revenuereport')->monthlyOn(1, '00:00')->runInBackground();
-//        $schedule->command('email:revenuereport')->monthlyOn(26, '00:00')->runInBackground();
+        $schedule->command('email:revenuereport')->monthlyOn(26, '00:00')->runInBackground();
         $schedule->command('email:revenuereportbydeliverydate')->monthlyOn(26, '01:00')->runInBackground();
 //        $schedule->command('verify:usersotp')->monthlyOn(15, '00:00')->runInBackground();
         $schedule->command('auto:birthdaymessage')->dailyAt('00:00')->runInBackground();
-        $schedule->command('email:revenuereport')->dailyAt('12:50')->runInBackground();
 
         $settings = GlobalSettings::where('type', 'rider_incentive_cron_time');
         if ($settings->exists()) {
