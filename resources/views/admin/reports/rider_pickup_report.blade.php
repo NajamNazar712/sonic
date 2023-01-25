ffedcfe75f544443d22534bb1b53d1a966d4dfcf
Muhammad Aqib <muhammad.aqib@trax.pk>
2023-01-25 19:26:00 +0500

    changes

--- resources/views/admin/reports/rider_pickup_report.blade.php
+++ resources/views/admin/reports/rider_pickup_report.blade.php
@@ -323,7 +323,7 @@
                     { data:'rider_id' ,name: 'r.trax_id', class: 'align-middle text-center rider_id'},
                     { data:'rider_name' ,name: 'r.name', class: 'align-middle text-center rider_name'},
                     { data:'origin' ,name: 'c.name', class: 'align-middle text-center origin'},
-                    { data:'pickup_note_btn', class: 'align-middle text-center pickup_note_btn'},
+                    { data:'pickup_note', class: 'align-middle text-center pickup_note'},
                     { data:'scanned_shipments_btn', class: 'align-middle scanned_shipments', orderable: false, searchable: false},
                     { data:'arrived_shipments_btn', class: 'align-middle arrived_shipments', orderable: false, searchable: false},
                     { data:'without_scan_shipments_btn', class: 'align-middle without_scan_shipments', orderable: false, searchable: false},
@@ -351,7 +351,7 @@
                     }).every(function() {
                         $(this.footer()).html('Total');
                     });
-                    api.columns('.pickup_note_btn', {
+                    api.columns('.pickup_note', {
                         page: 'current'
                     }).every(function() {
                         pickup_note = this
