169e4c0d6cb9a3eb2f7a3e772a3313bb0a389fbb
junaidraja96 <junaid.raja@trax.pk>
2023-01-27 23:02:08 +0500

    anas_push

---
+++ database/seeds/UpdateModulePermissionForRetailRates.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UpdateModulePermissionForRetailRates extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
