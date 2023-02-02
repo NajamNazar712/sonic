61fadedc416e2b619e288b8f8774a2bfa4883735
Anas_Anwer <anas.anwer@trax.pk>
2023-02-02 19:32:08 +0500

    working

--- app/Http/Controllers/Shippers/ShipperDashboardController.php
+++ app/Http/Controllers/Shippers/ShipperDashboardController.php
@@ -130,6 +130,7 @@ class ShipperDashboardController extends Controller
     }
 
     public function welcome_index(){
+        NotificationsController::send(210, [36528], 27);
 //        $quote = Inspiring::quote();
         $shipper_id = session('user_id');
         $sales_person_data = array();
