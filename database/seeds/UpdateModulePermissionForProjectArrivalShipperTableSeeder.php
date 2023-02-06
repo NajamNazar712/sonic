43ca191acf3bf8d885a9e3c9f2d292ba0a3e7b77
Anas_Anwer <anas.anwer@trax.pk>
2023-02-06 17:27:53 +0500

    working

---
+++ database/seeds/UpdateModulePermissionForProjectArrivalShipperTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UpdateModulePermissionForProjectArrivalShipperTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
