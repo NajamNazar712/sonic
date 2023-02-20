18d3d922887edaf7b0dbc413d15b6fac64ed4185
Syed Umair Badar <umair.badar@trax.pk>
2023-02-20 16:51:04 +0500

    guarded fix

--- app/Http/Models/Rider/RiderReturnNoteRequest.php
+++ app/Http/Models/Rider/RiderReturnNoteRequest.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class RiderReturnNoteRequest extends Model
 {
-    protected $guared=[];
+    protected $guarded=[];
 
     public function rider(){
         return $this->belongsTo('App\Http\Models\Rider','rider_id');
