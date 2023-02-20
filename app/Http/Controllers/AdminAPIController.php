210350e25baffc4d875ee3e216d6a334967aac6b
Syed Umair Badar <umair.badar@trax.pk>
2023-02-20 17:17:41 +0500

    variable name change

--- app/Http/Controllers/AdminAPIController.php
+++ app/Http/Controllers/AdminAPIController.php
@@ -10836,19 +10836,19 @@ class AdminAPIController extends Controller
     public function return_note_requests(Request $request){
         $role_id = $request->admin_role_id;
         $admin_hubs = $request->admin_hubs;
-        $delivery_note_requests = RiderReturnNoteRequest::join('riders as r', 'r.id', '=', 'rider_delivery_note_requests.rider_id')
-            ->join('cities as c', 'c.id', '=', 'rider_delivery_note_requests.hub_id')
-            ->join('routes as ro', 'ro.id', '=', 'rider_delivery_note_requests.route_id')
-            ->whereDate('rider_delivery_note_requests.created_at', Carbon::today())
-            ->where('rider_delivery_note_requests.status', 0)
-            ->select('rider_delivery_note_requests.id as id', 'rider_delivery_note_requests.created_at as date', 'r.name as rider_name', 'c.name as city_name', 'ro.junction as junction', 'ro.start as start', 'ro.end as end')->orderBy('rider_delivery_note_requests.id', 'DESC');
+        $return_note_requests = RiderReturnNoteRequest::join('riders as r', 'r.id', '=', 'rider_return_note_requests.rider_id')
+            ->join('cities as c', 'c.id', '=', 'rider_return_note_requests.hub_id')
+            ->join('routes as ro', 'ro.id', '=', 'rider_return_note_requests.route_id')
+            ->whereDate('rider_return_note_requests.created_at', Carbon::today())
+            ->where('rider_return_note_requests.status', 0)
+            ->select('rider_return_note_requests.id as id', 'rider_return_note_requests.created_at as date', 'r.name as rider_name', 'c.name as city_name', 'ro.junction as junction', 'ro.start as start', 'ro.end as end')->orderBy('rider_return_note_requests.id', 'DESC');
         if($role_id != 1){
-            $delivery_note_requests = $delivery_note_requests->whereIn('rider_delivery_note_requests.hub_id', $admin_hubs);
+            $return_note_requests = $return_note_requests->whereIn('rider_return_note_requests.hub_id', $admin_hubs);
         }
-        if($delivery_note_requests->exists()){
-            $delivery_note_requests = $delivery_note_requests->get();
+        if($return_note_requests->exists()){
+            $return_note_requests = $return_note_requests->get();
             $data = array();
-            foreach ($delivery_note_requests as $delivery_note_request){
+            foreach ($return_note_requests as $delivery_note_request){
                 $datum = array();
                 $shipments = RiderReturnNoteRequestShipment::join('shipments as s', 's.id', '=', 'rider_delivery_note_request_shipments.shipment_id')
                     ->where('request_note_id', $delivery_note_request->id)->pluck('s.tracking_number')->toArray();
