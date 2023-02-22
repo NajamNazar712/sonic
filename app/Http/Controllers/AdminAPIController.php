e600ec5b32541ba61d55c7886a9503ba7f0f8bd4
Anas_Anwer <anas.anwer@trax.pk>
2023-02-22 15:03:39 +0500

    changes

--- app/Http/Controllers/AdminAPIController.php
+++ app/Http/Controllers/AdminAPIController.php
@@ -11307,18 +11307,4 @@ class AdminAPIController extends Controller
         return response()->json(['status' => 0, 'crm_requests' => $crm_requests ]);
     }
 
-    public function get_receiving_sheet(Request $request)
-    {
-        $shipment = Shipment::where('tracking_number',$request->tracking_number)->first();
-        if ($shipment) {
-            $receiving_sheet_id = -1;
-            if ($shipment->receiving_sheet_shipment) {
-                $receiving_sheet_id = $shipment->receiving_sheet_shipment->receiving_sheet_id;
-                return response()->json(['status' => 0, 'receiving_sheet' => [['id' => strval($receiving_sheet_id)]]]);
-            }
-            return response()->json(['status' => 0, 'receiving_sheet' => [['id' => strval($receiving_sheet_id)]], 'error_message' => 'Receiving Sheet does not exists']);
-        }
-        return response()->json(['status' => 1, 'message' => 'No Shipments Found']);
-    }
-
 }
