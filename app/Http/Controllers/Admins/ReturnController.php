bfb5d7b45f61489dbe4d2bce0dd79f6873601da2
Muhammad Waqas <abi.m.waqas@gmail.com>
2023-02-23 13:17:55 +0500

    fix

--- app/Http/Controllers/Admins/ReturnController.php
+++ app/Http/Controllers/Admins/ReturnController.php
@@ -1723,6 +1723,9 @@ class ReturnController extends Controller
                                                 foreach ($pending_payment_shipments as $pending_payment_shipment) {
                                                     $shipper_payable += $pending_payment_shipment->payable;
                                                 }
+                                                if(($shipper_payable < 0) && ($shipper_payable > -1)){
+                                                    $shipper_payable = 0;
+                                                }
                                             }
                                         }
                                         if ($shipper_payable < 0) {
