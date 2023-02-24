d2cb743d07a738e096eb63293e940ca3844e10a0
Anas_Anwer <anas.anwer@trax.pk>
2023-02-24 16:28:15 +0500

    changes

--- app/Http/Controllers/Admins/AdminWalkInBookShipmentController.php
+++ app/Http/Controllers/Admins/AdminWalkInBookShipmentController.php
@@ -1152,9 +1152,9 @@ class AdminWalkInBookShipmentController extends Controller
         $booking_types = BookingType::select('id')->where('id', '=', 4)->first();
         $user_shipping_infos = UserShippingInfo::where('user_id', $user_id)->get();
         $cities = WalkInCities::join('cities as c', 'c.id', '=', 'walk_in_cities.city_id')
-            ->select('c.name as city_name', 'c.id as city_id')->where('walk_in_cities.pickup', 1)->where('c.status', 1)->where('c.hub', 1)->where('c.business_category_id', 2)->whereNotNull('c.zone_id')->get();
+            ->select('c.name as city_name', 'c.id as city_id')->where('walk_in_cities.pickup', 1)->get();
         $allowed_hubs = WalkInInternationalStandardWeightChargeHub::pluck('hub_id')->toArray();
-        $consignee_cities = City::whereIn('hub_id', $allowed_hubs)->where('cities.status', 1)->where('cities.hub', 0)->where('cities.business_category_id', 2)->whereNotNull('cities.zone_id')->groupBy('cities.id')->orderBy('cities.name')->select('cities.name','cities.id')->groupBy('cities.id')->get();
+        $consignee_cities = City::whereIn('hub_id', $allowed_hubs)->where('cities.status', 1)->where('cities.hub', 1)->where('cities.business_category_id', 2)->whereNotNull('cities.zone_id')->groupBy('cities.id')->orderBy('cities.name')->select('cities.name','cities.id')->groupBy('cities.id')->get();
         $products = Product::orderBy('product_name')->get();
         $shipping_mode = ShippingMode::where('id', 2)->get();
         $delivery_type = DeliveryType::orderBy('delivery_type')->get();
