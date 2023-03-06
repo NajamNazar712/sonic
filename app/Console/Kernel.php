5b1e3413f074c77b77bcc3439b220513e6ccee06
TahaKhalid <tahatk20@gmail.com>
2023-03-06 11:55:21 +0500

    test for shift cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -170,7 +170,10 @@ class Kernel extends ConsoleKernel
             foreach($endshifts as $shift)
             {
                 // run 1 hour before from the shift ends, to get save from the next day switch as well
-                $dailyAt = Carbon::parse($shift->end_time)->subHour(1)->format('H:i:s');
+                // $dailyAt = Carbon::parse($shift->end_time)->subHour(1)->format('H:i:s');
+                $dailyAt = Carbon::now();
+
+
                 $schedule->command('employee:attendenceadjustment', [$shift->id])
                 ->dailyAt($dailyAt)
                 ->runInBackground();
