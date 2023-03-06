339c8e24fd8d798a73563c2255f40122196eea6d
TahaKhalid <tahatk20@gmail.com>
2023-03-06 11:57:44 +0500

    revert all

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -170,10 +170,7 @@ class Kernel extends ConsoleKernel
             foreach($endshifts as $shift)
             {
                 // run 1 hour before from the shift ends, to get save from the next day switch as well
-                // $dailyAt = Carbon::parse($shift->end_time)->subHour(1)->format('H:i:s');
-                $dailyAt = Carbon::now()->format('H:i:s');
-
-
+                $dailyAt = Carbon::parse($shift->end_time)->subHour(1)->format('H:i:s');
                 $schedule->command('employee:attendenceadjustment', [$shift->id])
                 ->dailyAt($dailyAt)
                 ->runInBackground();
