e5f7304fce76ed78851f2fb62e9890deaeae9ee7
TahaKhalid <tahatk20@gmail.com>
2023-03-09 01:10:05 +0500

    fix

--- app/Http/Controllers/AdminAPIController.php
+++ app/Http/Controllers/AdminAPIController.php
@@ -8028,43 +8028,48 @@ class AdminAPIController extends Controller
 
     public function calculateToDateLeaves($employee, $toDate)
     {
-        
-        // $now = Carbon::now();
-        $start = Carbon::now()->startOfMonth();
-        $to_date = Carbon::parse($toDate)->startOfMonth();
-        $to_date_month = Carbon::parse($toDate)->month;
-        $difference = $to_date->diffInMonths($start);
-        // dd($difference);
-        // If Leaves apply for 2 or more than 2 days
-        if($difference >= 2) {
-            // If month is june, add 6 as per last months of fiscal year
-            if($to_date_month == 6){
-
-                $nd = $difference - 2;
-                $result = $nd * 2;
-                $result = $result+6;
-            } 
-            // If month is may, add 3 as per second last month of fiscal year
-            else if($to_date_month == 5){
-
-                $nd = $difference - 1;
-                $result = $nd * 2;
-                $result = $result+3;
+        try {
+            //code...
+            // $now = Carbon::now();
+            $start = Carbon::now()->startOfMonth();
+            $to_date = Carbon::parse($toDate)->startOfMonth();
+            $to_date_month = Carbon::parse($toDate)->month;
+            $difference = $to_date->diffInMonths($start);
+            // dd($difference);
+            // If Leaves apply for 2 or more than 2 days
+            if($difference >= 2) {
+                // If month is june, add 6 as per last months of fiscal year
+                if($to_date_month == 6){
+    
+                    $nd = $difference - 2;
+                    $result = $nd * 2;
+                    $result = $result+6;
+                } 
+                // If month is may, add 3 as per second last month of fiscal year
+                else if($to_date_month == 5){
+    
+                    $nd = $difference - 1;
+                    $result = $nd * 2;
+                    $result = $result+3;
+                } else {
+                    $result = $difference * 2;
+                }
+            }
+            else if ($difference == 1){
+                // If month is may or june, add 3 as per last month of fiscal year
+                if($to_date_month == 5 || $to_date_month == 6){
+                    $result = 3;
+                }  else {
+                    $result = 2;
+                }
             } else {
-                $result = $difference * 2;
+                $result = $employee->leave_count;
             }
+            return ['status' => 1, 'data' => $result];
+        } catch (\Throwable $th) {
+            //throw $th;
+            return ['status' => 0, 'msg' => $th->getMessage()];
         }
-        else if ($difference == 1){
-            // If month is may or june, add 3 as per last month of fiscal year
-            if($to_date_month == 5 || $to_date_month == 6){
-                $result = 3;
-            }  else {
-                $result = 2;
-            }
-        } else {
-            $result = $employee->leave_count;
-        }
-        return response()->json(['status' => 1, 'data' => $result]);
 
 
         // dd($result, $difference, $to_date_month);
@@ -8117,7 +8122,7 @@ class AdminAPIController extends Controller
                 if ($diffDays <= 56) {
                     if ($request->leave_type == 1) {
                         $response = $this->calculateToDateLeaves($employee, $to_date);
-                        dd($response['data']['status']);
+                        // dd($response['status']);
                         if($response['status'] == 1){
                             dd($response['data']);
                         } else {
