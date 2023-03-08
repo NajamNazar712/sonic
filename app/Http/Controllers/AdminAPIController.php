80c5229b3f3a5cef8580c17bc30f8da27dfe9863
TahaKhalid <tahatk20@gmail.com>
2023-03-08 13:01:54 +0500

    comment hod logic

--- app/Http/Controllers/AdminAPIController.php
+++ app/Http/Controllers/AdminAPIController.php
@@ -8199,21 +8199,22 @@ class AdminAPIController extends Controller
             if (!$employee) {
                 return response()->json(['status' => 1, 'message' => "Employee profile not found!"]);
             }
-            if (in_array($admin_id, $department_head_ids)) {
-                $employee_leaves = EmployeeLeave::join('leave_statuses as ls', 'employee_leaves.status', '=', 'ls.id')
-                    ->join('employees as emp', 'employee_leaves.employee_id', '=', 'emp.id')
-                    ->join('leave_types as lt', 'employee_leaves.leave_type', '=', 'lt.id')
-                    ->select('employee_leaves.id as id', 'employee_leaves.from as from', 'employee_leaves.to as to', 'employee_leaves.applied_reason as applied_reason', 'employee_leaves.status as status_id', 'ls.name as status', 'employee_leaves.employee_id as employee_id', 'employee_leaves.employee_type_id as type_id', 'employee_leaves.rejected_reason as rejected_reason', 'lt.name as leave_type', 'lt.id as leave_type_id')
-                    ->where(function ($query) use ($admin_id) {
-                        $query->where('employee_leaves.reporter_id', $admin_id);
-                    })
-                    ->orwhere(function ($query) use ($employee) {
-                        $query->where('employee_leaves.status', 6)
-                            ->where('employee_leaves.leave_type', '<>', 1)
-                            ->where('emp.department_id', $employee->department_id);
-                    });
-                $is_hod = true;
-            } elseif ($admin->employee->is_line_manager) {
+            // if (in_array($admin_id, $department_head_ids)) {
+            //     $employee_leaves = EmployeeLeave::join('leave_statuses as ls', 'employee_leaves.status', '=', 'ls.id')
+            //         ->join('employees as emp', 'employee_leaves.employee_id', '=', 'emp.id')
+            //         ->join('leave_types as lt', 'employee_leaves.leave_type', '=', 'lt.id')
+            //         ->select('employee_leaves.id as id', 'employee_leaves.from as from', 'employee_leaves.to as to', 'employee_leaves.applied_reason as applied_reason', 'employee_leaves.status as status_id', 'ls.name as status', 'employee_leaves.employee_id as employee_id', 'employee_leaves.employee_type_id as type_id', 'employee_leaves.rejected_reason as rejected_reason', 'lt.name as leave_type', 'lt.id as leave_type_id')
+            //         ->where(function ($query) use ($admin_id) {
+            //             $query->where('employee_leaves.reporter_id', $admin_id);
+            //         })
+            //         ->orwhere(function ($query) use ($employee) {
+            //             $query->where('employee_leaves.status', 6)
+            //                 ->where('employee_leaves.leave_type', '<>', 1)
+            //                 ->where('emp.department_id', $employee->department_id);
+            //         });
+            //     $is_hod = true;
+            // } else
+            if ($admin->employee->is_line_manager) {
                 $employee_leaves = EmployeeLeave::join('leave_statuses as ls', 'employee_leaves.status', '=', 'ls.id')
                     ->join('leave_types as lt', 'employee_leaves.leave_type', '=', 'lt.id')
                     ->select('employee_leaves.id as id', 'employee_leaves.from as from', 'employee_leaves.to as to', 'employee_leaves.applied_reason as applied_reason', 'employee_leaves.status as status_id', 'ls.name as status', 'employee_leaves.employee_id as employee_id', 'employee_leaves.employee_type_id as type_id', 'employee_leaves.rejected_reason as rejected_reason', 'lt.name as leave_type', 'lt.id as leave_type_id')
