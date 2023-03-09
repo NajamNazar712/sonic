304af33e480a74dfc4f51fd56747ca6981553099
Anas_Anwer <anas.anwer@trax.pk>
2023-03-09 16:30:47 +0500

    changes

--- resources/views/admin/notifications/index.blade.php
+++ resources/views/admin/notifications/index.blade.php
@@ -59,7 +59,7 @@
 															<option value="" selected="selected"></option>
 															<option value="1">General Logistics</option>
 															<option value="2">E-Commerce</option>
-															<option value="3">E-Commerce</option>
+															<option value="3">All</option>
 														</select>
 													</div>
 
