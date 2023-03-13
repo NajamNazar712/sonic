0ef81530c4676418460613e777303bb3209af3c0
Anas_Anwer <anas.anwer@trax.pk>
2023-03-13 12:10:46 +0500

    changes

--- app/Http/Controllers/AdminAPIController.php
+++ app/Http/Controllers/AdminAPIController.php
@@ -7282,7 +7282,7 @@ class AdminAPIController extends Controller
                 $last_action_log = $last_action_log->first();
                 if (($attendance_date->lt(Carbon::now()->format("Y-m-d")) && $last_action_log->action_id == 2) || $attendance_date->format('l') == "Sunday") {
                     $attendance_date = $attendance_date->addDays(1);
-                    return response()->json($attendance_date);
+//                    return response()->json($attendance_date);
                 }
             }
             $admin_attendance = EmployeeAttendance::where('employee_id', $employee_id)
