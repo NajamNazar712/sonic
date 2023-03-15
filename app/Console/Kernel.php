dda351632da148cfdd98594ecf619f5506249679
mohsin sajjad <mohsin.sajjad@trax.pk>
2023-03-15 18:50:44 +0500

    changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -452,7 +452,7 @@ class Kernel extends ConsoleKernel
 
         $schedule->command('comment:dailycrmclaimshipments')->dailyAt('14:00')->runInBackground();
         $schedule->command('rider:fuel_allocation')->dailyAt('04:00')->runInBackground();
-        $schedule->command('api:visionsoftexcel')->dailyAt('04:00')->runInBackground();
+        $schedule->command('api:visionsoftexcel')->dailyAt('07:00')->runInBackground();
 
     }
     /**
