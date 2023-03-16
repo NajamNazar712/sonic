bf9eeb4764d6aaf4db96cb7f91ab38b4d85a08d8
junaidraja96 <junaid.raja@trax.pk>
2023-03-16 17:27:41 +0500

    changes

---
+++ database/seeds/PettyCashApprovedEditButtonSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PettyCashApprovedEditButtonSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('module_permissions')->insert(array(
+            array('id' => 840, 'name' => 'Petty Cash Edit - Action', 'module_id' => 8),
+            array('id' => 841, 'name' => 'Petty Cash Amount Edit - Action', 'module_id' => 8),
+        ));
+    }
+}
