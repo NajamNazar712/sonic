9eabf8674d3984b255fad10b87c2ac99a56d0fe9
mohsin sajjad <mohsin.sajjad@trax.pk>
2023-03-20 12:58:24 +0500

    changes

--- resources/views/admin/crm/launched_re_open.blade.php
+++ resources/views/admin/crm/launched_re_open.blade.php
@@ -618,7 +618,7 @@
                     }
                 },
                 rowId: 'id',
-                order: [[19, 'desc']],
+                order: [[21, 'desc']],
                 columns: [
                     {data: 'id', orderable: false, searchable: false, class: 'text-center align-middle select p-1', targets: 0, render: function (data, type, row) {return '';}},
                     {orderable: false, searchable: false, name: 'serial_number', class: 'align-middle serial_number', targets: 0, render: function (data, type, row) {return '';}},
