cec71ffb4281116306aaad7eb9af0f78af14f413
Muhammad Waqas <abi.m.waqas@gmail.com>
2023-03-21 18:37:54 +0500

    change invoice cron

--- app/Console/Commands/CreateInvoiceOriginWise.php
+++ app/Console/Commands/CreateInvoiceOriginWise.php
@@ -48,7 +48,7 @@ class CreateInvoiceOriginWise extends Command
 
         $weekStartDate = $now->subMonths(6)->startOfMonth()->format('Y-m-d H:i');
         $weekEndDate = Carbon::today()->subWeek()->endOfWeek()->format('Y-m-d H:i');
-        $invoices = Invoice::whereBetween('invoicing_date', [$weekStartDate, $weekEndDate]);
+        $invoices = Invoice::whereBetween('invoicing_date', [$weekStartDate, $weekEndDate])->whereNotIn('id', [3012,3024,3054,3059,3098,3119,3163,3171,3205,3226,3266,3270,3304,3342,3388,3392,3395,3426,3455,3462,3474,3502,3510,3521,3522,3536,3560,3589,3629,3636,3652,3653,3666,3686,3708]);
         if($invoices->exists()){
             $invoices = $invoices->get();
             if(count($invoices) > 0){
