6642f9f8006a7f69add15591e8779295aa9992d9
Abdul Ghani CHishti <ghani.chisti@trax.pk>
2023-03-29 16:22:57 +0500

    minor change in arrivel notification

--- database/seeds/UpdateNotificationTableForDailyRevenueReportSeeder.php
+++ database/seeds/UpdateNotificationTableForDailyRevenueReportSeeder.php
@@ -14,7 +14,7 @@ class UpdateNotificationTableForDailyRevenueReportSeeder extends Seeder
         $timestamp = \Carbon\Carbon::now()->format('Y-m-d H:i:s');
 
         DB::table('notifications')->insert(array(
-            array('id' => 214, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Revenue Daily Report', 'type_id' => 1, 'subject' => ' Revenue Daily Report  | (For Month Of [month] [year])' , 'body' => 'Dear Concern,'. PHP_EOL .PHP_EOL.'Please download the report from the following link: [link].', 'updated_by' => 7, 'status' => 0)
+            array('id' => 214, 'created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Revenue Daily Report', 'type_id' => 1, 'subject' => ' Revenue Daily Report By Arrival Date' , 'body' => 'Dear Concern,'. PHP_EOL .PHP_EOL.'Please download the report from the following link: [link].', 'updated_by' => 7, 'status' => 0)
         ));
     }
 }
