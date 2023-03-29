f546d7f0259e344009bb634aa5eeb6a8c4428d31
Abdul Ghani CHishti <ghani.chisti@trax.pk>
2023-03-29 12:23:34 +0500

    minor change in star shipper table

--- app/Http/Controllers/Admins/GlobalSettingsController.php
+++ app/Http/Controllers/Admins/GlobalSettingsController.php
@@ -7583,6 +7583,7 @@ class GlobalSettingsController extends Controller
             })
             ->addColumn('action', function ($star_shippers) {
                 if (session('role_id') == 1) {
+
                     $dropdown = '<div class="btn-group">
                     <button type="button" class="btn btn-sm btn-success dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Actions</button>
                     <div class="dropdown-menu dropdown-menu-sm">
@@ -7590,7 +7591,8 @@ class GlobalSettingsController extends Controller
                     if ($star_shippers->status == 1) {
 
                         $dropdown .= ' <button type="button" class="dropdown-item enable_disable"><div class="row no-gutters align-items-center"><div class="col-2"><i class="ft-minus-circle"></i></div><div class="col-9 offset-1">Disable</div></button>';
-                    } else {
+                    }
+                    else {
 
                         $dropdown .= ' <button type="button" class="dropdown-item enable_disable"><div class="row no-gutters align-items-center"><div class="col-2"><i class="ft-plus-circle"></i></div><div class="col-9 offset-1">Enable</div></button>';
                     }
@@ -7598,9 +7600,9 @@ class GlobalSettingsController extends Controller
                     $dropdown .= '</div>
                   </div>
           ';
-
                     return $dropdown;
-                } else {
+                }
+                else {
                     return '';
                 }
             });
--- routes/web.php
+++ routes/web.php
@@ -3471,8 +3471,6 @@ Route::prefix('admin')->name('admin.')->group(function () {
             Route::post('add', 'Admins\GlobalSettingsController@star_shippers_add')->name('add');
             Route::post('enable_disable', 'Admins\GlobalSettingsController@star_shippers_enable_disable')->name('enable_disable');
         });
-
-
 	});
 
     Route::prefix('shipment')->name('shipment.')->group(function () {
