5fefa9090ce02c50bc2c21c83c1d63bb54bac650
mohsin sajjad <mohsin.sajjad@trax.pk>
2023-04-03 14:12:23 +0500

    changes

--- app/Http/Controllers/Admins/AdminReportsController.php
+++ app/Http/Controllers/Admins/AdminReportsController.php
@@ -6571,18 +6571,14 @@ class AdminReportsController extends Controller
                 $shipment_id = $requests->shipment_id;
                 $origin_hub = $requests->origin_hub;
                 $destination_hub = $requests->hub;
-                $shipment_journey = ShipmentsJourney::whereIn('shipper_status_id',[12,20,22])
+                $shipment_journey = ShipmentsJourney::whereIn('shipper_status_id',[22])
                     ->where('shipment_id',$shipment_id);
                 if($shipment_journey->exists()){
                     $shipment_journey = $shipment_journey->pluck('shipper_status_id')->toArray();
-                    if(in_array(12, $shipment_journey) && in_array(20, $shipment_journey)) {
-                        $temp = $destination_hub;
-                        $destination_hub = $origin_hub;
-                        $origin_hub = $temp;
-                    }elseif(in_array(12, $shipment_journey) && in_array(20, $shipment_journey) && in_array(22, $shipment_journey)){
-                        $temp = $destination_hub;
-                        $destination_hub = $origin_hub;
-                        $origin_hub = $temp;
+                    if(in_array(22,$shipment_journey)) {
+//                        $temp = $destination_hub;
+//                        $destination_hub = $origin_hub;
+//                        $origin_hub = $temp;
                     }
                 }
                 if($status == 1 || $status == 2 || $status == 17 || $status == 22){
@@ -6629,18 +6625,14 @@ class AdminReportsController extends Controller
 
                 $origin_zone = $requests->origin_zone;
                 $destination_zone = $requests->zone;
-                $shipment_journey = ShipmentsJourney::whereIn('shipper_status_id',[12,20,22])
+                $shipment_journey = ShipmentsJourney::whereIn('shipper_status_id',[22])
                     ->where('shipment_id',$shipment_id);
                 if($shipment_journey->exists()){
                     $shipment_journey = $shipment_journey->pluck('shipper_status_id')->toArray();
-                    if(in_array(12, $shipment_journey) && in_array(20, $shipment_journey) && in_array(22, $shipment_journey)) {
-                        $temp = $destination_zone;
-                        $destination_zone = $origin_zone;
-                        $origin_zone = $temp;
-                    }  elseif(in_array(12, $shipment_journey) && in_array(20, $shipment_journey) && in_array(22, $shipment_journey)){
-                        $temp = $destination_zone;
-                        $destination_zone = $origin_zone;
-                        $origin_zone = $temp;
+                    if(in_array(22,$shipment_journey)) {
+//                        $temp = $destination_zone;
+//                        $destination_zone = $origin_zone;
+//                        $origin_zone = $temp;
                     }
                 }
 
