71c2a04062796e3c1b4dbb5e6fcb8561a13fa359
Abdul Ghani CHishti <ghani.chisti@trax.pk>
2023-04-03 14:40:40 +0500

    minor cahnge in web.php

--- routes/web.php
+++ routes/web.php
@@ -3496,6 +3496,7 @@ Route::prefix('admin')->name('admin.')->group(function () {
             Route::post('add', 'Admins\GlobalSettingsController@star_shippers_add')->name('add');
             Route::post('enable_disable', 'Admins\GlobalSettingsController@star_shippers_enable_disable')->name('enable_disable');
         });
+    });
     Route::prefix('shipment')->name('shipment.')->group(function () {
         Route::prefix('book')->name('book.')->group(function () {
             Route::get('', 'Admins\AdminWalkInBookShipmentController@index')->name('walk_in');
@@ -4253,8 +4254,6 @@ Route::prefix('admin')->name('admin.')->group(function () {
         });
 
     });
-});
-
 });
 
 Route::prefix('retail')->name('retail.')->group(function () {
