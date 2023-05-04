b5cac717c92b64c4c0f993a76f75ce0f684d9033
trazx1 <groupon26095@gmail.com>
2023-05-04 13:33:14 +0500

    to-5818 column add rider name and trax id

--- resources/views/admin/return/confirmed.blade.php
+++ resources/views/admin/return/confirmed.blade.php
@@ -65,6 +65,7 @@
                         <th class="border-primary border-darken-1">Shipping Mode</th>
                         <th class="border-primary border-darken-1">Service Type</th>
                         <th class="border-primary border-darken-1">Status</th>
+                        <th class="border-primary border-darken-1">Rider Trax Id</th>
                         <th class="border-primary border-darken-1">Rider Name</th>
                         <th class="border-primary border-darken-1">Return Pending for</th>
                         <th class="border-primary border-darken-1">Return Confirmed By</th>
@@ -73,6 +74,7 @@
                         <th class="border-primary border-darken-1">Arrival Date</th>
                         <th class="border-primary border-darken-1">Status Date</th>
                         <th class="border-primary border-darken-1">Received By</th>
+
                         <th class="border-primary border-darken-1"></th>
                     </tr>
                     </thead>
@@ -229,6 +231,7 @@
                             head.push('Shipping Mode');
                             head.push('Service Type');
                             head.push('Status');
+                            head.push('Rider Trax Id');
                             head.push('Rider Name');
                             head.push('Return Pending For');
                             head.push('Return Confirmed By');
@@ -259,6 +262,7 @@
                                 row.push(values.mode);
                                 row.push(values.service_type);
                                 row.push(values.status);
+                                row.push(values.rider_id);
                                 row.push(values.rider_name);
                                 row.push(values.return_pending_for);
                                 row.push(values.return_confirmed_by);
@@ -500,6 +504,7 @@
                     {data: 'mode', name: 'sm.id', class: 'align-middle mode'},
                     {data: 'service_type', name: 'bt.id', class: 'align-middle service_type'},
                     {data: 'status', name: 'status', class: 'align-middle status'},
+                    {data: 'rider_id', name: 'rider.trax_id', class: 'align-middle rider_id'},
                     {data: 'rider_name', name: 'rider.name', class: 'align-middle receiver_name'},
                     {data: 'return_pending_for', name: 'return_pending_for', class: 'align-middle return_pending_for', orderable: false},
                     {data: 'return_confirmed_by', name: 'cb.name', class: 'align-middle return_confirmed_by', orderable: false},
@@ -508,7 +513,6 @@
                     {data: 'arrival', name: 'sj.created_at', class: 'align-middle arrival'},
                     {data: 'status_date', name: 'shipments_journey.created_at', class: 'align-middle status_date'},
                     {data: 'receiver_name', name: 'a.name', class: 'align-middle receiver_name'},
-
                     {data: 'action', name: 'action', class: 'text-center align-middle action p-1', orderable: false, searchable: false}
                 ],
                 rowCallback: function(row, data, index) {
