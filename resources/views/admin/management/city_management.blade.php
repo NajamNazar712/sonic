90b76c184ffde5f0ad10bbf990ac46cbdac581dd
unknown <muhammad.ahmed@trax.pk>
2023-05-10 16:33:22 +0500

    changes

--- resources/views/admin/management/city_management.blade.php
+++ resources/views/admin/management/city_management.blade.php
@@ -173,7 +173,7 @@
                 @if (session('role_id') == 1 || in_array(89, session('permissions')))
 
                     buttons: [{
-                       text: '<i class="la la-map-marker"></i> Add Areas',
+                       text: '<i class="la la-map-marker"></i> Add City',
                        className: 'btn btn-primary',
                        enabled: true,
                        action: function (e, dt, node, config) {
