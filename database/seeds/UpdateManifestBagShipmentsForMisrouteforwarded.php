ff1016db80e1ee4cfd405a877ce75d1612a4b451
Nabeel973 <nabeelsiddiqui324@gmail.com>
2023-05-17 13:38:58 +0500

    change

--- database/seeds/UpdateManifestBagShipmentsForMisrouteforwarded.php
+++ database/seeds/UpdateManifestBagShipmentsForMisrouteforwarded.php
@@ -14,21 +14,21 @@ class UpdateManifestBagShipmentsForMisrouteforwarded extends Seeder
      */
     public function run()
     {
-        $shipment_ids = array(23715936,23783648,23794787,23809283,23848970,23894761,23903634,23919513,23919991,23936152,23936399,23956153,23994032,23996315,23998203,24001497,24013273,24014560,24049073,23900538);
+        $shipment_ids = array(23970938,23970938,23970938,23970938,23970938,23970938,23970938,23970938,23970938,23970938,23970938,23970938,23970938,23970938,23970938,23970938,23970938,23970938,23970938,23970938,23970938,23970938,23970938);
 
         foreach ($shipment_ids as $shipment_id) {
 
             $shipment = Shipment::find($shipment_id);
             if ($shipment) {
                 if ($shipment->shipper_status_id == 49 || $shipment->shipper_status_id == 11) {
-                    // $mis_fwd = ShipmentsJourney::where('shipment_id', $shipment_id)->where('shipper_status_id', 49)->orderBy('id', 'desc')->first();
-                    // if ($mis_fwd) {
-                    //     $mis_fwd->delete();
-                    // }
-                    $mis = ShipmentsJourney::where('shipment_id', $shipment_id)->where('shipper_status_id', 11)->orderBy('id', 'desc')->first();
-                    if ($mis) {
-                        $mis->delete();
+                    $mis_fwd = ShipmentsJourney::where('shipment_id', $shipment_id)->where('shipper_status_id', 49)->orderBy('id', 'desc')->first();
+                    if ($mis_fwd) {
+                        $mis_fwd->delete();
                     }
+                    // $mis = ShipmentsJourney::where('shipment_id', $shipment_id)->where('shipper_status_id', 11)->orderBy('id', 'desc')->first();
+                    // if ($mis) {
+                    //     $mis->delete();
+                    // }
                     $journey = ShipmentsJourney::where('shipment_id', $shipment_id)->orderBy('id', 'desc')->first();
                     Shipment::where('id', $shipment_id)->update(['shipper_status_id' => $journey->shipper_status_id, 'consignee_status_id' => $journey->consignee_status_id]);
                 }
