a7521473a41693a609089281aaf3a3666e54fa88
Muhammad Waqas <muhammad.waqas@trax.pk>
2023-05-24 17:57:52 +0500

    fix

--- app/Http/Controllers/Webhook/ShipmentStatusWebhookController.php
+++ app/Http/Controllers/Webhook/ShipmentStatusWebhookController.php
@@ -51,11 +51,14 @@ class ShipmentStatusWebhookController extends Controller
             if($shipper_status_id == 5){
                 $shipment_otp = ShipmentOtp::where('shipment_id', $shipment_id)->first();
                 if($shipment_otp){
-                    $data['opt'] = $shipment_otp->otp;
+                    $data['otp'] = $shipment_otp->otp;
+                }
+                else{
+                    $data['otp'] = NULL;
                 }
             }
             else{
-                $data['opt'] = NULL;
+                $data['otp'] = NULL;
             }
 
             $data['date_time'] = $date;
