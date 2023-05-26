3c32a499851a9871f2cdb0a57819fbb419ba5952
trazx1 <groupon26095@gmail.com>
2023-05-26 14:21:26 +0500

    minor change

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -3333,7 +3333,7 @@ class NotificationsController extends Controller
                                 if (strpos($body, '[return_detail]') !== FALSE) {
                                     $body = str_replace('[return_detail]', $return_detail, $body);
                                 }
-                                dd($body);
+                                dd($body,$return_detail);
 
                                 self::email($subject, $body, $to);
 
