18a2fc4b17f7e29dfe63696cc73520a926139ca7
trazx1 <groupon26095@gmail.com>
2023-05-29 18:45:42 +0500

    some changes in return note ntification no 216

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -3325,7 +3325,7 @@ class NotificationsController extends Controller
                                 $data1 = ReturnDeliveredToShipperSms::join('return_notes as rn', 'rn.id', '=', 'return_delivered_to_shipper_sms.return_note_id')
                                     ->join('shipments', 'shipments.id', '=', 'return_delivered_to_shipper_sms.shipment_id')
                                     ->join('users as u', 'u.id', '=', 'return_delivered_to_shipper_sms.user_id')
-                                    ->where('return_delivered_to_shipper_sms.status', 0)
+//                                    ->where('return_delivered_to_shipper_sms.status', 0)
                                     ->whereBetween('return_delivered_to_shipper_sms.created_at',[$yesterday,$today])
                                     ->select('return_delivered_to_shipper_sms.return_note_id as return_note_id','shipments.id as shipment_id','return_delivered_to_shipper_sms.user_id as user_id',
                                         'u.phone as phone_number',
