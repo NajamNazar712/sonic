caec7e783d9759123b7a525c81ee684c7e8a260e
Abdul Ghani CHishti <ghani.chisti@trax.pk>
2023-05-30 15:27:35 +0500

    just add some more conditions in returned recieved screen

--- app/Http/Controllers/Admins/ReturnController.php
+++ app/Http/Controllers/Admins/ReturnController.php
@@ -2780,6 +2780,24 @@ class ReturnController extends Controller
                         Shipment::where('id', $shipment)->update(['shipper_status_id' => 25, 'consignee_status_id' => 25]);
                         ReturnNoteShipment::where(['return_note_id' => $request->return_note_id, 'shipment_id' => $shipment])->update(['status' => 1]);
 
+                        $users = GlobalSettings::where('type', 'returned_shipment_notification');
+
+                        if($users->exists()) {
+                            $users = $users->first();
+                            $current_users = $users->text;
+                            $c_user = explode(',', $current_users);
+                            if(in_array($parcel->user_id,$c_user)){
+                                $return_delivered_to_shipper = ReturnDeliveredToShipperSms::where(['return_note_id'=>$request->return_note_id,'user_id' => $parcel->user_id,'shipment_id' => $parcel->id,'status' => 0]);
+                                if(!$return_delivered_to_shipper->exists()){
+                                    $return_delivered = new ReturnDeliveredToShipperSms();
+                                    $return_delivered->return_note_id = $request->return_note_id;
+                                    $return_delivered->user_id = $parcel->user_id;
+                                    $return_delivered->shipment_id = $parcel->id;
+                                    $return_delivered->status = 0;
+                                    $return_delivered->save();
+                                }
+                            }
+                        }
 
 
 //                        $packaging_material_shipment = PackagingMaterialRequest::where('tracking_number', $parcel->tracking_number)->first();
@@ -2835,7 +2853,7 @@ class ReturnController extends Controller
 
                         $users = GlobalSettings::where('type', 'returned_shipment_notification');
 
-                        if($users->exists()) { 
+                        if($users->exists()) {
                             $users = $users->first();
                             $current_users = $users->text;
                             $c_user = explode(',', $current_users);
