b6aa0c23b7102826019d6c884da47ad5295594e5
trazx1 <groupon26095@gmail.com>
2023-05-31 13:42:35 +0500

    minor changes done

--- resources/views/admin/return/history.blade.php
+++ resources/views/admin/return/history.blade.php
@@ -317,7 +317,7 @@
                     }
                 },
                 rowId: 'return_note_id',
-                order: [[7, 'desc']],
+                order: [[8, 'desc']],
                 columns: [
                     {orderable: false, searchable: false, name: 'serial_number', class: 'align-middle serial_number', targets: 0, render: function (data, type, row) {return '';}},
                     { data:'return_note' ,name: 'return_notes.id', class: 'align-middle return_note'},
