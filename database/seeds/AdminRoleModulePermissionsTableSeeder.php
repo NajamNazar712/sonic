282380315aec4f8fff469c1e3e1ce94255ce7106
Nabeel973 <nabeelsiddiqui324@gmail.com>
2023-06-01 14:07:43 +0500

    permission seeder fix

--- database/seeds/AdminRoleModulePermissionsTableSeeder.php
+++ database/seeds/AdminRoleModulePermissionsTableSeeder.php
@@ -1314,71 +1314,7 @@ class AdminRoleModulePermissionsTableSeeder extends Seeder
 
             }
 
-            elseif($permission_id[$i]==862)
-            {
-                $roles = array(3,8,9,10,11,15,18,19,23,33,46,55,63,65,70,72,76,78,84,85,89,91,93,95,96,101,104,105,106,116,121,122,123);
-
-                for($j=0;$j<count($roles);$j++)
-                {
-                    DB::table('admin_role_module_permissions')->insert(array(
-                        array('role_id' => $roles[$j], 'permission_id' => $permission_id[$i])
-                    ));
-                }
-
-            }
-
-            elseif($permission_id[$i]==863)
-            {
-                $roles = array(9, 8, 105, 10, 3, 93);
-
-                for($j=0;$j<count($roles);$j++)
-                {
-                    DB::table('admin_role_module_permissions')->insert(array(
-                        array('role_id' => $roles[$j], 'permission_id' => $permission_id[$i])
-                    ));
-                }
-
-            }
-
-            elseif($permission_id[$i]==864)
-            {
-                $roles = array(9, 8, 105, 10, 3, 93);
-
-                for($j=0;$j<count($roles);$j++)
-                {
-                    DB::table('admin_role_module_permissions')->insert(array(
-                        array('role_id' => $roles[$j], 'permission_id' => $permission_id[$i])
-                    ));
-                }
-
-            }
-
-            elseif($permission_id[$i]==865)
-            {
-                $roles = array(9, 8, 105, 10, 3, 93);
-
-                for($j=0;$j<count($roles);$j++)
-                {
-                    DB::table('admin_role_module_permissions')->insert(array(
-                        array('role_id' => $roles[$j], 'permission_id' => $permission_id[$i])
-                    ));
-                }
-
-            }
-
-            elseif($permission_id[$i]==866)
-            {
-                $roles = array(9, 8, 105, 10, 3, 93);
-
-                for($j=0;$j<count($roles);$j++)
-                {
-                    DB::table('admin_role_module_permissions')->insert(array(
-                        array('role_id' => $roles[$j], 'permission_id' => $permission_id[$i])
-                    ));
-                }
-
-            }
-
+            
            
 
         }
