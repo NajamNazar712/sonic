2a8058fd4553f902dbe1a5d91ad56d219980688b
trazx1 <groupon26095@gmail.com>
2023-06-01 13:00:15 +0500

    6037 mms setting shipper done

--- app/Http/Controllers/Admins/Reports/MMSReportController.php
+++ app/Http/Controllers/Admins/Reports/MMSReportController.php
@@ -33,10 +33,8 @@ class MMSReportController extends Controller
         $business_categories = DB::connection('reports')->table('business_categories')->select('id', 'name')->get();
 
         $shippers = GlobalSettings::where('type','mms_setting')->select('text')->first();
-
         $shippers = explode(',',$shippers->text);
         $shippers = User::whereIn('id',$shippers)->select('id','name')->get();
-
 
 
         return view('admin.reports.mms')->with(['shippers' => $shippers, 'cities' => $cities, 'hubs' => $hubs, 'statuses' => $statuses]);
