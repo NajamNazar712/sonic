858489f14dd3573d1a9c76f2c0fd3dc6f8948219
Muhammad Murad <muhammad.murad@trax.pk>
2023-06-07 11:00:00 +0500

    changes

--- resources/views/admin/settings/fintech/form.blade.php
+++ resources/views/admin/settings/fintech/form.blade.php
@@ -454,10 +454,8 @@ function inputValidate(){
                                             data-msg-required="This field is required"   min="1">
                                         </div>
                                     </td>
-                                    <td>
-                                        <div class="col mb-1">
-                                            <button type="button" onclick="$(this).closest('tr').remove();" class="btn btn-outline-danger btm-sm"><i class="la la-trash"></i></button>
-                                        </div>
+                                    <td style="width:1%">
+                                        <button type="button" onclick="$(this).closest('tr').remove();" class="btn btn-outline-danger"><i class="la la-trash"></i></button>
                                     </td>            
                                 </tr> `;
                     $('#row_append_body').append(html);
