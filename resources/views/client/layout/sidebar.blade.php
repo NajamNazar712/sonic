9d61efb184072c8a403fa28fa3d3bb64729b2583
junaidraja96 <junaid.raja@trax.pk>
2023-06-08 16:17:45 +0500

    changes

--- resources/views/client/layout/sidebar.blade.php
+++ resources/views/client/layout/sidebar.blade.php
@@ -116,8 +116,8 @@
             @if (session('user_type') == 1 || in_array(12, session('permissions')))
                 <li class=" nav-item"><a href="#"><span class="menu-title" data-i18n="nav.dash.main"><i class="la la-cubes"></i>Pickup</span></a>
                     <ul class="menu-content">
-                        <li><a href="{{ route('cod.pickup.index') }}">Pickup Request History</a></li>
                         <li><a href="{{ route('cod.pickup.add_pickup') }}">Add Pickup Request</a></li>
+                        <li><a href="{{ route('cod.pickup.index') }}">Pickup Request History</a></li>
                     </ul>
                 </li>
             @endif
