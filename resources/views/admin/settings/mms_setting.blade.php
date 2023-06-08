666a82d37c110710b3233c57ed78e23b808b4f0d
Ahad <groupon26095@gmail.com>
2023-06-08 13:36:20 +0500

    changes has been done

--- resources/views/admin/settings/mms_setting.blade.php
+++ resources/views/admin/settings/mms_setting.blade.php
@@ -89,7 +89,7 @@
                 submitHandler: function (form) {
                     swal({
                         title: 'Are You Sure?',
-                        text: 'Select Yes to update User Shippers.',
+                        text: 'Select Yes to update Shippers.',
                         icon: 'warning',
                         buttons: {
                             cancel: {
