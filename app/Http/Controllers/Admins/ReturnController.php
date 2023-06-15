619e4e6df8baa20d2691a855b82c7448e71932e2
Zain <zain.hyder@trax.pk>
2023-06-16 03:31:50 +0500

    changes

--- app/Http/Controllers/Admins/ReturnController.php
+++ app/Http/Controllers/Admins/ReturnController.php
@@ -5178,6 +5178,7 @@ class ReturnController extends Controller
             
             $total_assigning = ReturnAssignedShipments::join('return_assigned_shipment_logs as rasl','rasl.return_assign_shipment_id','=','return_assigned_shipments.id')
             ->where('return_assigned_shipments.admin_id',$agent_productivity->agent_id)
+            // ->where('rasl.assigned_by','=',$agent_productivity->agent_id)
             ->where('rasl.status',0)
             ->whereDate('rasl.created_at',$agent_productivity->current_date)->count();
         
@@ -6400,14 +6401,16 @@ class ReturnController extends Controller
         'a.id as agent_id', 'return_assigned_shipments.shipment_id as shipment_id',
         'a.name as assigned_to', 's.consignee_name as consignee_name', 's.consignee_address as consignee_address',
         's.consignee_phone_number_1 as consignee_phone_number','sscf.remark as call_findings',
-        'return_assigned_shipments.updated_at as agent_status_date', 'updated_by_user.name as user','updated_by_admin.name as admin'
-        ,'rasl_latest.status as rasl_status')->groupBy('tracking_number');
+        'return_assigned_shipments.updated_at as agent_status_date', 'updated_by_user.id as user_id', 'updated_by_user.name as user', 
+        'updated_by_admin.id as admin_id' ,'updated_by_admin.name as admin', 'rasl_latest.status as rasl_status', 's.user_id as shipment_user_id')
+        
+        ->groupBy('tracking_number');
 
         $datatable = Datatables::of($agent_productivity)
 
 
         ->addColumn('updated_by', function ($agent_productivity){
-            if($agent_productivity->user != null){
+            if($agent_productivity->user != null && $agent_productivity->user_id == $agent_productivity->shipment_user_id){
                 return $agent_productivity->user;
             }
             else if ($agent_productivity->rasl_status != 0){
