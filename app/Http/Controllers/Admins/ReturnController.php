68e353719579410b82db2021538b60e2839dbaab
Zain <zain.hyder@trax.pk>
2023-06-15 15:48:14 +0500

    chnages

--- app/Http/Controllers/Admins/ReturnController.php
+++ app/Http/Controllers/Admins/ReturnController.php
@@ -5031,10 +5031,12 @@ class ReturnController extends Controller
             $total_assigning = ReturnAssignedShipments::join('return_assigned_shipment_logs as rasl','rasl.return_assign_shipment_id','=','return_assigned_shipments.id')
             ->where('return_assigned_shipments.admin_id',$agent_productivity->agent_id)
             ->where('rasl.status',0)
-            ->whereDate('rasl.created_at',$agent_productivity->current_date)->count();
+            ->whereDate('rasl.created_at',$agent_productivity->current_date)
+            ->distinct('return_assign_shipment_id', 'status')
+            ->count();
             return $total_assigning;
-
         })
+
         
         ->addColumn('unresponsive_return',function ($agent_productivity){
             $unresponsive_return = ReturnAssignedShipments::join('return_assigned_shipment_logs as rasl','rasl.return_assign_shipment_id','=','return_assigned_shipments.id')
