c979bbd07a56597638a9f92e6a5a0b009fa016c9
Zain <zain.hyder@trax.pk>
2023-06-16 21:18:59 +0500

    changes

--- app/Http/Controllers/Admins/ReturnController.php
+++ app/Http/Controllers/Admins/ReturnController.php
@@ -5110,16 +5110,24 @@ class ReturnController extends Controller
         })
 
         ->addColumn('total_unresponsive_in_percent',function ($agent_productivity){
-            $total_assigning = ReturnAssignedShipments::join('return_assigned_shipment_logs as rasl','rasl.return_assign_shipment_id','=','return_assigned_shipments.id')
-            ->where('return_assigned_shipments.admin_id',$agent_productivity->agent_id)
-            ->where('rasl.status',0)
-            ->where('rasl.status', '!=', 4)
-            ->whereDate('rasl.created_at',$agent_productivity->current_date)->count();
+            $total_assigning = ReturnAssignedShipments::join('return_assigned_shipment_logs as rasl', 'rasl.return_assign_shipment_id', '=', 'return_assigned_shipments.id')
+            ->where('return_assigned_shipments.admin_id', $agent_productivity->agent_id)
+            ->whereDate('rasl.created_at', $agent_productivity->current_date)
+
+            //checking if agent has unassigned shipment to agent
+            ->whereNotExists(function ($query) {
+                $query->select(DB::raw(1))
+                    ->from('return_assigned_shipment_logs as rasl2')
+                    ->whereColumn('rasl2.return_assign_shipment_id', '=', 'return_assigned_shipments.id')
+                    ->where('rasl2.status', 4);
+            })
+            ->where('rasl.status', 0)
+            ->distinct('return_assigned_shipments.id')
+            ->count();
 
             $total_unresponsive_in_percent = ReturnAssignedShipments::join('return_assigned_shipment_logs as rasl','rasl.return_assign_shipment_id','=','return_assigned_shipments.id')
-            ->leftjoin('status_remarks as sr','sr.shipment_id','return_assigned_shipments.shipment_id')
-            ->leftjoin('sub_status_call_findings as sscf','sscf.id','=','sr.sub_status_call_finding_id')
-            ->where('sscf.remark',4) 
+            ->where('return_assigned_shipments.admin_id',$agent_productivity->agent_id)
+            ->where('rasl.status',8)
             ->where('rasl.assigned_by','=',$agent_productivity->agent_id)
             ->whereDate('rasl.created_at',$agent_productivity->current_date)->count();
 
