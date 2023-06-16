78efdb5a1fdfd61055cd341d04b614ef3f3a66ef
Zain <zain.hyder@trax.pk>
2023-06-16 17:37:29 +0500

    changes

--- app/Http/Controllers/Admins/ReturnController.php
+++ app/Http/Controllers/Admins/ReturnController.php
@@ -5268,7 +5268,7 @@ class ReturnController extends Controller
                 ->where('ras.admin_id',$agent_productivity->agent_id)
                 ->where('return_assigned_shipment_logs.status', '!=', 0)
                 ->where('return_assigned_shipment_logs.status', '!=', 4)
-                ->where('return_assigned_shipment_logs.status', '!=', 3)
+                ->where('return_assigned_shipment_logs.status', '=', 3)
                 ->where('return_assigned_shipment_logs.assigned_by','!=',$agent_productivity->agent_id)
                 ->whereDate('return_assigned_shipment_logs.created_at',$agent_productivity->current_date)
                 ->count();//2
