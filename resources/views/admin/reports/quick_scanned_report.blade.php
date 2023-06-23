198bca15d60c72b8ce85646904980154a8c3107e
junaidraja96 <junaid.raja@trax.pk>
2023-06-23 12:26:11 +0500

    changes

--- resources/views/admin/reports/quick_scanned_report.blade.php
+++ resources/views/admin/reports/quick_scanned_report.blade.php
@@ -306,7 +306,7 @@
                     }
                 },
                 rowId: 'id',
-                order: [[12, 'asc']],
+                order: [[12, 'desc']],
                 columns: [
                     {orderable: false, searchable: false, name: 'serial_number', class: 'align-middle serial_number', targets: 0, render: function (data, type, row) {return '';}},
                     {data: 'tracking_number_hyperlink', name: 'shipments.tracking_number', class: 'align-middle text_center tracking_number'},
