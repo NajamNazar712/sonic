ca2eef1345313426bed02a283ceab1e35c0eaba9
Abdul Ghani CHishti <ghani.chisti@trax.pk>
2023-06-23 18:42:14 +0500

    just add a col in retail book print slip

--- app/Http/Controllers/Retail/RetailShipmentBookController.php
+++ app/Http/Controllers/Retail/RetailShipmentBookController.php
@@ -218,6 +218,7 @@ class RetailShipmentBookController extends Controller
     }
 
     public function store(Request $request){
+
         if($request->ref == 'Others'){
           $ref = $request->ref_name;
         }else{
@@ -295,26 +296,7 @@ class RetailShipmentBookController extends Controller
         }
 
         $rates = RetailRatesCalculationController::rates($shipping_mode_check, $business_category_id, $pickup_city_id, $consignee_city_id, $request->trax_box, $discount, $estimated_weight,$insurance_amount,$packaging);
-
-        if($request->has('admin_discount'))
-        {
-            if ($request->filled('admin_discount') && $request->admin_discount > 0) {
-                if ($request->has('admin_discount_type1') && $request->admin_discount_type1 ==  1)
-                {
-                    $rates['total_charges'] = ($rates['total_charges'] * $request->admin_discount)/100; //todo: for %
-                }
-                if ($request->has('admin_discount_type1') && $request->admin_discount_type1 ==  0)
-                {
-                    $rates['total_charges'] = $rates['total_charges'] - $request->admin_discount; // todo: for flat
-                }
-            }
-        }
-
-        if ($rates['total_charges'] <= 0)
-        {
-            return redirect()->back()->with(['error' => 'Total charges cannot be less than zero !']);
-        }
-
+       
         if( $rates['charges'] == 0 && $rates['charges_with_discount'] == 0) {
             return redirect()->back()->with(['error' => 'Charges should be greater than zero']);
         }
@@ -470,21 +452,6 @@ class RetailShipmentBookController extends Controller
         $retail_shipment->breadth = $breadth;
         $retail_shipment->height = $height;
         $retail_shipment->retail_user_id = Auth::id();
-
-        if($request->has('admin_discount'))
-        {
-            $retail_shipment->admin_discount = $request->admin_discount;
-
-            if ($request->has('admin_discount_type1') && $request->admin_discount_type1 ==  1)
-            {
-                $retail_shipment->admin_discount_type = 1; // todo: for %
-            }
-            elseif ($request->has('admin_discount_type1') && $request->admin_discount_type1 ==  0)
-            {
-                $retail_shipment->admin_discount_type = 0; // todo: for flat
-            }
-        }
-
         $retail_shipment->save();
 
         $shipment = Shipment::find($shipment_id);
@@ -554,26 +521,6 @@ class RetailShipmentBookController extends Controller
         }
 
         $details = RetailRatesCalculationController::rates($request->shipping_mode_id, $request->business_category_id, $pickup_city_id, $request->consignee_city_id, $request->trax_box, $discount, $weight,$insurance_amount,$packaging);
-
-        if($request->has('admin_discount'))
-        {
-            if ($request->filled('admin_discount') && $request->admin_discount > 0) {
-                if ($request->has('admin_discount_type1') && $request->admin_discount_type1 ==  1)
-                {
-                    $details['total_charges'] = ($details['total_charges'] * $request->admin_discount)/100; // todo: for %
-                }
-                if ($request->has('admin_discount_type1') && $request->admin_discount_type1 ==  0)
-                {
-                    $details['total_charges'] = $details['total_charges'] - $request->admin_discount; // todo: for flat
-                }
-            }
-        }
-
-        if ($details['total_charges'] <= 0)
-        {
-            return response()->json(['status' => 0, 'error' => 'Total charges should be greater than zero !', 'details' => $details]);
-        }
-
         return response()->json(['status' => 1, 'success' => 'Rates Calculated!', 'details' => $details]);
     }
 
@@ -855,7 +802,8 @@ class RetailShipmentBookController extends Controller
                                 <td colspan="2" class="color primary"><strong>Pieces</strong></td>
                                 <td colspan="1" class="color primary"><strong>Weight</strong></td>
                                 <td colspan="1" class="color primary"><strong>Service Charges</strong></td>
-                                <td colspan="1" class="color primary"><strong>Discount</strong></td>
+                                <td colspan="1" class="color primary"><strong>Discount(Trax Center)</strong></td>
+                                <td colspan="1" class="color primary"><strong>Discount(Consumer)</strong></td>
                                 <td colspan="1" class="color primary"><strong>Charges With Discount</strong></td>
                                 <td colspan="1" class="color primary border"><strong>GST</strong></td>
                                 <td colspan="1" class="color primary border"><strong>Packaging & Insurance </strong></td>
@@ -867,6 +815,7 @@ class RetailShipmentBookController extends Controller
                                 <td colspan="1" class="border twice-bottom">' . number_format($shipment->estimated_weight) . '</td>
                                 <td colspan="1" class="border twice-bottom">' . number_format($shipment->retail->weight_charges,2) . '</td>
                                 <td colspan="1" class="border twice-bottom">' . number_format($shipment->retail->discount,2) . '</td>
+                                <td colspan="1" class="border twice-bottom">' . number_format($shipment->retail->admin_discount,2) . '</td>
                                 <td colspan="1" class="border twice-bottom">' . number_format($shipment->retail->charges_with_discount,2) . '</td>
                                 <td colspan="1" class="border twice-bottom">' . number_format($gst,2) . '</td>
                                 <td colspan="1" class="border twice-bottom">' . number_format($packaging_and_insurance,2) . '</td>
@@ -1806,8 +1755,6 @@ class RetailShipmentBookController extends Controller
             'account_number' => 'Account Number',
             'bank_id' => 'Bank ID',
             'special_instruction' => 'Special Instruction',
-            'admin_discount' => 'Admin Discount',
-            'admin_discount_type' => 'Admin Discount Type',
         ];
 
         $messages = [
@@ -1861,18 +1808,8 @@ class RetailShipmentBookController extends Controller
             'account_number' => ['nullable', 'numeric'],
             'bank_id' => ['nullable', 'integer', 'between:1,100', Rule::exists('banks_lists', 'id')],
             'special_instruction' => ['nullable', 'between:1,190'],
-            'admin_discount' => ['nullable', 'numeric','between:1,190'],
-            'admin_discount_type' => ['required_with:admin_discount','nullable', 'numeric','between:1,2'],
-
+            
         ];
-
-//        Validator::extend('test', function ($attribute, $value, $parameters, $validator) use ($user_id) {
-//            if(!empty($value)){
-//                return  true;
-//            }else{
-//                return false;
-//            }
-//        });
 
         if($file = $request->file('shipments')) {
             $spreadsheet = IOFactory::createReaderForFile($file);
@@ -1880,17 +1817,13 @@ class RetailShipmentBookController extends Controller
             $spreadsheet = $spreadsheet->load($file)->getActiveSheet()->toArray();
         }
 
-
-
         if (isset($spreadsheet)) {
-                $fields = [0 => 'product_id', 1 => 'business_category_id', 2 => 'shipping_mode_id', 3 => 'destination', 4 => 'volumetric_weight', 5 => 'weight', 6 => 'length', 7 => 'breadth', 8 => 'height', 9 => 'pieces', 10 => 'payment_mode_id', 11 => 'charges_mode_id', 12 => 'shipper_cell_number', 13 => 'shipper_name', 14 => 'shipper_cnic', 15 => 'shipper_address', 16 => 'consignee_cell_number', 17 => 'consignee_name', 18 => 'consignee_cnic', 19 => 'consignee_address', 20 => 'order_id', 21 =>'insurance_offered',22 => 'insurance_value', 23 =>'packaging_charges',24 => 'trax_box_id', 25 => 'iban_number', 26 => 'account_number', 27 => 'bank_id', 28 => 'special_instruction',29 => 'admin_discount', 30 => 'admin_discount_type'];
-            if (count($spreadsheet[0]) != 31){
-                dd($spreadsheet[0]);
+                $fields = [0 => 'product_id', 1 => 'business_category_id', 2 => 'shipping_mode_id', 3 => 'destination', 4 => 'volumetric_weight', 5 => 'weight', 6 => 'length', 7 => 'breadth', 8 => 'height', 9 => 'pieces', 10 => 'payment_mode_id', 11 => 'charges_mode_id', 12 => 'shipper_cell_number', 13 => 'shipper_name', 14 => 'shipper_cnic', 15 => 'shipper_address', 16 => 'consignee_cell_number', 17 => 'consignee_name', 18 => 'consignee_cnic', 19 => 'consignee_address', 20 => 'order_id', 21 =>'insurance_offered',22 => 'insurance_value', 23 =>'packaging_charges',24 => 'trax_box_id', 25 => 'iban_number', 26 => 'account_number', 27 => 'bank_id', 28 => 'special_instruction'];
+            if (count($spreadsheet[0]) != 29){
                 return redirect()->back()->with('error', 'Invalid Columns, Kindly follow the Template provided');
             }
             unset($spreadsheet[0]);
         }
-
 
 
         if (!isset($spreadsheet) || !empty($spreadsheet)) {
@@ -1949,14 +1882,6 @@ class RetailShipmentBookController extends Controller
                     $rows[$key]['insurance_value'] = $row['insurance_value'];
                 }
 
-                if(!isset($row['admin_discount']) || $row['admin_discount'] == null){
-                    $row['admin_discount'] = null;
-                }
-
-                if(!isset($row['admin_discount_type']) || $row['admin_discount_type'] == null){
-                    $row['admin_discount_type'] = null;
-                }
-
 
                 $validate = Validator::make($row, $rules, $messages);
 
@@ -2011,7 +1936,6 @@ class RetailShipmentBookController extends Controller
                 }
             }
 
-//            dd($row);
             if (empty($errors)) {
                 foreach ($rows as $key => $row) {
                     $row['user_id'] = $user_id;
@@ -2019,7 +1943,6 @@ class RetailShipmentBookController extends Controller
                     $row['pickup_address_id'] = $pickup_address_id;
                     $row['category'] = $category;
                     $row['category_id'] = $category_id;
-//                    dd($row);
                     dispatch(new ProcessRetailShipmentBookingDB($row));
                 }
 
