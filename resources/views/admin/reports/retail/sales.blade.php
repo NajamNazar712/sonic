1d9b951e2c1341d8ef0c85a7a12d687bd8837f4d
Abdul Ghani CHishti <ghani.chisti@trax.pk>
2023-06-26 15:40:58 +0500

    just set the sortor in front end of retail sales report

--- resources/views/admin/reports/retail/sales.blade.php
+++ resources/views/admin/reports/retail/sales.blade.php
@@ -426,7 +426,7 @@
                         d.search_date_to = $('input[name="search_date_to_formatted"]').val();
                     }
                 },
-                order: [[10, 'desc']],
+                order: [[11, 'desc']],
                 columns: [
                     {orderable: false, searchable: false, name: 'serial_number', class: 'align-middle serial_number', targets: 0, render: function (data, type, row) {return '';}},
                     { data:'tracking_number_link' ,name: 'shipments.tracking_number', class: 'align-middle text-center tracking_number_link'},
