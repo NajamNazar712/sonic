c8a03fa2defa923f9084edac0295a4a65e1a762b
Shariq Raees <Shariq.raees@trax.pk>
2023-07-05 14:30:08 +0500

    changes

--- app/Http/Controllers/Shippers/ShipperShipmentBookController.php
+++ app/Http/Controllers/Shippers/ShipperShipmentBookController.php
@@ -141,7 +141,7 @@ class ShipperShipmentBookController extends Controller
     static public function book($user_id, $service_type_id, $pickup_address_id, $information_display, $consignee_city_id, $consignee_name, $consignee_address, $consignee_phone_number_1, $consignee_phone_number_2, $consignee_email_address, $order_id, $package_type, $special_instructions, $estimated_weight, $shipping_mode_id, $same_day_timing_id, $amount, $payment_mode_id, $charges_mode_id, $try_and_buy_charges, $pieces, $self_collection, $business_category_id, $open_shipment, $return_address_id,$parcel_value = null)
     {
         
-        $information_display = $this->information_display_check($user_id);
+        $information_display = self::information_display_check($user_id);
 
         $shipment = new Shipment();
         $shipment->user_id = $user_id;
@@ -3439,7 +3439,7 @@ class ShipperShipmentBookController extends Controller
     static public function corporate_book($user_id, $service_type_id, $pickup_address_id, $information_display, $consignee_city_id, $consignee_name, $consignee_address, $consignee_phone_number_1, $consignee_phone_number_2, $consignee_email_address, $order_id, $package_type, $special_instructions, $estimated_weight, $shipping_mode_id, $delivery_type_id, $same_day_timing_id, $charges_mode_id, $amount, $payment_mode_id, $pieces, $self_collection, $business_category_id, $try_and_buy_charges, $open_shipment, $return_address_id,$parcel_value = null)
     {
         
-        $information_display = $this->information_display_check($user_id);
+        $information_display = self::information_display_check($user_id);
 
         $shipment = new Shipment();
 
@@ -3549,7 +3549,7 @@ class ShipperShipmentBookController extends Controller
     }
 
     // this function is used to show information in airwaybill if user_id exist in airway bill setting screen
-    public function information_display_check($user_id)
+    static public function information_display_check($user_id)
     {
         $information_display = 1;
         $settings = GlobalSettings::where('type', 'airway_bill_address_visibility_setting');
