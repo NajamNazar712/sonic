c44d931972f902f00aab88578ecbdefcd491c370
Abdul Ghani CHishti <ghani.chisti@trax.pk>
2023-07-05 19:12:12 +0500

    working on delivery status on receive screen

--- app/Http/Controllers/Admins/DeliveryController.php
+++ app/Http/Controllers/Admins/DeliveryController.php
@@ -2277,7 +2277,6 @@ class DeliveryController extends Controller
 
     public function receive_delivery_status_submit_all(Request $request)
     {
-        dd(1,$request->all());
         $open_box_ids = array();
         $received_shipments = array();
         $first_attempt_shipments = array();
