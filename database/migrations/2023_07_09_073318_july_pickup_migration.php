5242a599ed69e866f8de9a0558887bd4874c0b8d
Anas_Anwer <anas.anwer@trax.pk>
2023-07-09 07:34:33 +0500

    changes

--- database/migrations/2023_07_09_073318_july_pickup_migration.php
+++ database/migrations/2023_07_09_073318_july_pickup_migration.php
@@ -11,26 +11,26 @@ class JulyPickupMigration extends Migration
      *
      * @return void
      */
-//    public function up()
-//    {
-//        Schema::table('v2_pickup_notes', function (Blueprint $table) {
-//            $table->integer('shipments')->default(0);
-//            $table->integer('arrived_shipments')->default(0);
-//            $table->integer('shipments_scanned_by_rider')->default(0);
-//        });
-//    }
-//
-//    /**
-//     * Reverse the migrations.
-//     *
-//     * @return void
-//     */
-//    public function down()
-//    {
-//        Schema::table('v2_pickup_notes', function (Blueprint $table) {
-//            $table->dropColumn('shipments');
-//            $table->dropColumn('arrived_shipments');
-//            $table->dropColumn('shipments_scanned_by_rider');
-//        });
-//    }
+    public function up()
+    {
+        Schema::table('v2_pickup_notes', function (Blueprint $table) {
+            $table->integer('shipments')->default(0);
+            $table->integer('arrived_shipments')->default(0);
+            $table->integer('shipments_scanned_by_rider')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('v2_pickup_notes', function (Blueprint $table) {
+            $table->dropColumn('shipments');
+            $table->dropColumn('arrived_shipments');
+            $table->dropColumn('shipments_scanned_by_rider');
+        });
+    }
 }
