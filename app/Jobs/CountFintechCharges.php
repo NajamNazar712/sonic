0447d6379d26acd5bbe12b700164079183f55463
Anas_Anwer <anas.anwer@trax.pk>
2023-07-13 13:18:37 +0500

    changes

--- app/Jobs/CountFintechCharges.php
+++ app/Jobs/CountFintechCharges.php
@@ -120,12 +120,12 @@ class CountFintechCharges implements ShouldQueue
                         ];
                         $client = new Client();
                         $response = $client->request('Post', $this->url,$options);
-                        Log::channel('trax_pay')->info(json_encode($response));
+                        Log::channel('trax_pay')->info('s ' . json_encode($response->getBody()->getContents()));
                     }
                 }
             }
             catch(Exception $e){
-                Log::channel('trax_pay')->info(json_encode($e));
+                Log::channel('trax_pay')->info('e ' . json_encode($e));
             }
         }
     }
