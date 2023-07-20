1305fac2dc47bcf6df43069ea4690f0aed11592c
Zain <zain.hyder@trax.pk>
2023-07-20 18:39:25 +0500

    chnages

--- resources/views/agent/return_v2/index.blade.php
+++ resources/views/agent/return_v2/index.blade.php
@@ -518,7 +518,6 @@
                         .done(function(data) {
                             if (data.shipment != undefined) {
                                 $('#shipment_id_val').val(data.shipment.id)
-                                console.log(data.rider_details);
                                 var shipment = '';
 
                                 shipment +=
