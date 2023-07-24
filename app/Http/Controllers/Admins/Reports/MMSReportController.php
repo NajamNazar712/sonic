ebe7eaaaeea03e7e319af302adf3298847cc9111
Shariq Raees <Shariq.raees@trax.pk>
2023-07-24 13:44:48 +0500

    admin mms report hotfix for date search

--- app/Http/Controllers/Admins/Reports/MMSReportController.php
+++ app/Http/Controllers/Admins/Reports/MMSReportController.php
@@ -51,16 +51,17 @@ class MMSReportController extends Controller
         if ($request->get('excel') && $request->get('excel') == true) {
             ActivityTrailController::createActivityTrailLog(Auth::id(), 567);
         }
-        $arrival_from = Carbon::parse($request->arrival_time_from)->format('H:i:s');
-        $arrival_to = Carbon::parse($request->arrival_time_to)->format('H:i:s');
+        // $arrival_from = Carbon::parse($request->arrival_time_from)->format('H:i:s');
+        // $arrival_to = Carbon::parse($request->arrival_time_to)->format('H:i:s');
 
         $from = $request->get('search_date_from');
         $from = Carbon::parse($from)->toDateTimeString();
         $to = $request->get('search_date_to');
         $to = Carbon::parse($to)->toDateTimeString();
 
-        $from = str_replace('00:00:00', $arrival_from, $from);
-        $to = str_replace('00:00:00', $arrival_to, $to);
+        // $from = str_replace('00:00:00', $arrival_from, $from);
+        // $to = str_replace('00:00:00', $arrival_to, $to);
+
 
         $sales = DB::connection($connection)->table('shipments')->join('users as u','u.id','=','shipments.user_id')
             ->join('shipment_status as ss','ss.id','=','shipments.shipper_status_id')
@@ -104,20 +105,26 @@ class MMSReportController extends Controller
             ->whereIn('u.id', $special_shippers)
             ->whereBetween('sj.created_at', [$from,$to]);
 
+            
+            
         $from_id = DB::connection($connection)->table('shipments_journey')->select('id')->where('created_at', '>=', $from);
         if ($from_id->exists()) {
             $from_id = $from_id->first()->id;
 
-            $to_id = DB::connection($connection)->table('shipments_journey')->select(DB::raw('MAX(id) as id'))->where('created_at', '>=', $from)->where('created_at', '<=', $to);
+            
 
+            $to_id = DB::connection($connection)->table('shipments_journey')->select(DB::raw('id'))->where('created_at', '>=', $from)->where('created_at', '<=', $to);
+
+            
             if ($to_id->exists()) {
-                $to_id = $to_id->first()->id;
+                $to_id = $to_id->latest()->first()->id;
 
                 $sales->where('sj.id', '>=', $from_id)
-                    ->where('sj.id', '<=', $to_id);
+                ->where('sj.id', '<=', $to_id);    
             }
         }
 
+        
 //        if (!$request->get('search_date_from') && !$request->get('search_date_to')) {
 //            $now = Carbon::now();
 //            $yesterday = Carbon::now()->subDays(3);
