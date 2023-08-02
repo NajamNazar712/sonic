b76201dedd0cc74f03153dc0532cca2eaccf40ce
TahaKhalid <tahatk20@gmail.com>
2023-08-02 12:57:38 +0500

    employee shift seeder

---
+++ database/seeds/AddContractualEmployeeShifts.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Models\EmployeeShift;
+use Illuminate\Database\Seeder;
+
+class AddContractualEmployeeShifts extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $timestamp = \Carbon\Carbon::now()->format('Y-m-d H:i:s');
+        EmployeeShift::create([
+            'shift_type_id' => '2', 'start_time' => '10:00:00', 'end_time' => '14:00:00', 'status' => '1', 'extension_minutes' => '30',  'created_at' => $timestamp, 'updated_at' => $timestamp,
+            'shift_type_id' => '2', 'start_time' => '14:00:00', 'end_time' => '18:00:00', 'status' => '1', 'extension_minutes' => '30',  'created_at' => $timestamp, 'updated_at' => $timestamp,
+            'shift_type_id' => '2', 'start_time' => '18:00:00', 'end_time' => '22:00:00', 'status' => '1', 'extension_minutes' => '30', 'created_at' => $timestamp, 'updated_at'=> $timestamp
+        ]);
+    }
+}
