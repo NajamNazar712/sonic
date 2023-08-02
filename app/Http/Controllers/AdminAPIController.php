c2b0780d089b5e425a0afe52f67dac12885340e8
Syed Umair Badar <umair.badar@trax.pk>
2023-08-02 15:27:02 +0500

    created_new_api_for_employee_shifts

--- app/Http/Controllers/AdminAPIController.php
+++ app/Http/Controllers/AdminAPIController.php
@@ -7502,7 +7502,6 @@ class AdminAPIController extends Controller
             $datum = array();
             $datum['id'] = $shift->id;
             $datum['name'] = $shift->name . ' (' . $shift->start_time . ' - ' . $shift->end_time . ') ';
-            $datum['shift_type_id'] = $shift->shift_type_id;
             $shift_data[] = $datum;
         }
         return response()->json(['status' => 0, "cities" => $cities, "designation" => $designation, "domicile" => $domicile, "marital_status" => $marital_status, "nationality" => $nationality, "religion" => $religion, "gender" => $gender, "zone" => $zone, "department" => $department, "hub" => $hub, "blood_group" => $blood_group, "relationships" => $relationships, 'banks' => $banks, 'rider_type' => $rider_type, 'staff_categories' => $staff_categories, 'shifts' => $shift_data, 'rider_sub_category' => $category, 'rider_main_category' => $main_category]);
@@ -12815,6 +12814,34 @@ class AdminAPIController extends Controller
         }
 
         return $shipment_scanned;
+    }
+
+    public function get_staff_working_shift(Request $request) {
+
+        $rules = [
+            'staff_category_id' => 'required'
+        ];
+
+        $validate = Validator::make($request->all(), $rules, $this->messages);
+
+        $validate->setAttributeNames($this->names);
+
+        if ($validate->fails()) {
+            return response()->json(['status' => 1, 'message' => 'Error(s) in Input', 'errors' => $validate->errors()]);
+        } else {
+            $shifts = EmployeeShift::where('id', '!=', 1);
+            $shifts = $request->staff_category_id == 3 ? $shifts->where('shift_type_id', '2') : $shifts->where('shift_type_id', '!=', '2');
+            $shifts = $shifts->select('id', 'name', 'start_time', 'end_time')->get();
+
+            $shift_data = array();
+            foreach ($shifts as $shift) {
+                $datum = array();
+                $datum['id'] = $shift->id;
+                $datum['name'] = $shift->name . ' (' . $shift->start_time . ' - ' . $shift->end_time . ') ';
+                $shift_data[] = $datum;
+            }
+            return response()->json(['status' => 0, 'shifts' => $shift_data]);
+        }
     }
 
 }
