195a41c037c3b4d2ce45628a702b3092b0fd725b
Zain <zain.hyder@trax.pk>
2023-08-15 13:36:54 +0500

    fixed last rider name and id

--- app/Http/Controllers/Admins/DeliveryController.php
+++ app/Http/Controllers/Admins/DeliveryController.php
@@ -226,7 +226,8 @@ class DeliveryController extends Controller
             ->whereRaw('IF (shipments.shipper_status_id IN (2, 49), (oc.hub_id = dc.hub_id), TRUE)')
             ->whereRaw('IF (shipments.shipper_status_id = 55, (irrh.old_consignee_city_id = irrh.new_consignee_city_id), TRUE)')
             ->whereRaw('IF (shipments.shipper_status_id = 55, (irrh.old_consignee_city_id = irrh.new_consignee_city_id), TRUE)')
-            ->whereIn('shipments.shipper_status_id', $status);
+            ->whereIn('shipments.shipper_status_id', $status)
+            ->groupBy('r.id');
 
         if (session('role_id') != 1) {
             $shipments = $shipments->whereIn('dc.hub_id', session('hubs'));
--- resources/views/admin/delivery/pending/index.blade.php
+++ resources/views/admin/delivery/pending/index.blade.php
@@ -356,8 +356,8 @@
                 {data: 'arrival', name: 'sj.created_at', class: 'align-middle arrival'},
                 {data: 'destination_zone', name: 'z.name ', class: 'align-middle destination_zone', orderable: false, searchable: false},
                 {data: 'destination_arrival', name: 'sjd.created_at ', class: 'align-middle destination_arrival', orderable: false, searchable: false},
-                {data: 'last_rider', name: 'r.name ', class: 'align-middle last_rider', orderable: false, searchable: false},
-                {data: 'rider_trax_id', name: 'r.trax_id ', class: 'align-middle rider_trax_id', orderable: false, searchable: false},
+                {data: 'last_rider', name: 'r.name', class: 'align-middle last_rider', orderable: false},
+                {data: 'rider_trax_id', name: 'r.trax_id ', class: 'align-middle rider_trax_id', orderable: false},
                 {data: 'status_date', name: 'shipments_journey.created_at', class: 'align-middle status_date'},
                 {data: 'action', name: 'action', class: 'text-center align-middle action p-1', orderable: false, searchable: false}
 
