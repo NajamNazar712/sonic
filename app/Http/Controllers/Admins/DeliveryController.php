82438ed6072219d250a31c66e4a0eac751f5ab11
Zain <zain.hyder@trax.pk>
2023-08-25 13:46:58 +0500

    commented ->groupBy('r.id') in pending_list function

--- app/Http/Controllers/Admins/DeliveryController.php
+++ app/Http/Controllers/Admins/DeliveryController.php
@@ -261,8 +261,8 @@ class DeliveryController extends Controller
             ->whereRaw('IF (shipments.shipper_status_id IN (2, 49), (oc.hub_id = dc.hub_id), TRUE)')
             ->whereRaw('IF (shipments.shipper_status_id = 55, (irrh.old_consignee_city_id = irrh.new_consignee_city_id), TRUE)')
             ->whereRaw('IF (shipments.shipper_status_id = 55, (irrh.old_consignee_city_id = irrh.new_consignee_city_id), TRUE)')
-            ->whereIn('shipments.shipper_status_id', $status)
-            ->groupBy('r.id');
+            ->whereIn('shipments.shipper_status_id', $status);
+            // ->groupBy('r.id');
 
 
 
