82fedcaa1f72b78dffe5c6cbdde6cb867ec52144
junaidraja96 <junaid.raja@trax.pk>
2023-08-28 16:15:33 +0500

    changed email view

--- resources/views/email/crm-response-rate-email-template.blade.php
+++ resources/views/email/crm-response-rate-email-template.blade.php
@@ -1,42 +1,29 @@
-<p>Dated: {{ $date }}</p>
-
-<table style="border-collapse: collapse; width: 100%; border: 1px solid #ddd;">
-    <thead>
-        <tr>
-            <th style="border: 1px solid #ddd; text-align: center; padding: 8px; background-color: #f2f2f2;">Sno</th>
-            <th style="border: 1px solid #ddd; text-align: center; padding: 8px; background-color: #f2f2f2;">Responsible Hub</th>
-            <th style="border: 1px solid #ddd; text-align: center; padding: 8px; background-color: #f2f2f2;">Total Tagged</th>
-            <th style="border: 1px solid #ddd; text-align: center; padding: 8px; background-color: #f2f2f2;">Response Rate (%)</th>
-        </tr>
-    </thead>
-    <tbody>
-        @php
-            $sno = 1;
-        @endphp
-        @foreach ($responses as $resp)
-        <tr>
-            <td style="border: 1px solid #ddd; text-align: center; padding: 8px;">{{ $sno }}</td>
-            <td style="border: 1px solid #ddd; text-align: center; padding: 8px;">{{ $resp['responsible_hub'] }}</td>
-            <td style="border: 1px solid #ddd; text-align: center; padding: 8px;">{{ $resp['total_tagged'] }}</td>
-            <td style="border: 1px solid #ddd; text-align: center; padding: 8px;">{{ $resp['response_rate'] }}</td>
-        </tr>
-        @php
-            $sno++;
-        @endphp
-        @endforeach
-    </tbody>
-</table>
-
-<style>
-    .datahead {
-        border: 1px solid #ddd;
-        text-align: center;
-        padding: 8px;
-        background-color: #f2f2f2;
-    }
-    .datarow {
-        border: 1px solid #ddd;
-        padding: 8px;
-        text-align: center;
-    }
-</style>
+<div>
+    <span style="margin-bottom: 10px;">Dated: {{ $date }}</span>
+    <table style="border-collapse: collapse; width: 100%; border: 1px solid #ddd; margin-top: 0;">
+        <thead>
+            <tr>
+                <th style="border: 1px solid #ddd; text-align: center; padding: 8px; background-color: #f2f2f2;">Sno</th>
+                <th style="border: 1px solid #ddd; text-align: center; padding: 8px; background-color: #f2f2f2;">Responsible Hub</th>
+                <th style="border: 1px solid #ddd; text-align: center; padding: 8px; background-color: #f2f2f2;">Total Tagged</th>
+                <th style="border: 1px solid #ddd; text-align: center; padding: 8px; background-color: #f2f2f2;">Response Rate (%)</th>
+            </tr>
+        </thead>
+        <tbody>
+            @php
+                $sno = 1;
+            @endphp
+            @foreach ($responses as $resp)
+            <tr>
+                <td style="border: 1px solid #ddd; text-align: center; padding: 8px;">{{ $sno }}</td>
+                <td style="border: 1px solid #ddd; text-align: center; padding: 8px;">{{ $resp['responsible_hub'] }}</td>
+                <td style="border: 1px solid #ddd; text-align: center; padding: 8px;">{{ $resp['total_tagged'] }}</td>
+                <td style="border: 1px solid #ddd; text-align: center; padding: 8px;">{{ $resp['response_rate'] }}</td>
+            </tr>
+            @php
+                $sno++;
+            @endphp
+            @endforeach
+        </tbody>
+    </table>
+</div>
