b6ea525993ffddbca90b648a46e0e747cce32bdb
junaidraja96 <junaid.raja@trax.pk>
2023-08-28 16:47:33 +0500

    remove comment of html str

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -10403,38 +10403,6 @@ class NotificationsController extends Controller
                         'date' => $date
                         ])->render();
 
-                //     $html = "<div>
-                //     <span style=\"margin-bottom: 10px;\">Dated: $date</span>
-                //     <table style=\"border-collapse: collapse; width: 100%; border: 1px solid #ddd; margin-top: 0;\">
-                //         <thead>
-                //             <tr>
-                //                 <th style=\"border: 1px solid #ddd; text-align: center; padding: 8px; background-color: #f2f2f2;\">Sno</th>
-                //                 <th style=\"border: 1px solid #ddd; text-align: center; padding: 8px; background-color: #f2f2f2;\">Responsible Hub</th>
-                //                 <th style=\"border: 1px solid #ddd; text-align: center; padding: 8px; background-color: #f2f2f2;\">Total Tagged</th>
-                //                 <th style=\"border: 1px solid #ddd; text-align: center; padding: 8px; background-color: #f2f2f2;\">Response Rate (%)</th>
-                //             </tr>
-                //         </thead>
-                //         <tbody>";
-                //     $sno = 1;
-                //     foreach ($responses as $resp) {
-                //         $html.="<tr>
-                //             <td style=\"border: 1px solid #ddd; text-align: center; padding: 8px;\">$sno</td>
-                //             <td style=\"border: 1px solid #ddd; text-align: center; padding: 8px;\">";
-                //         $html.=$resp['responsible_hub'];
-                //         $html.="</td>
-                //             <td style=\"border: 1px solid #ddd; text-align: center; padding: 8px;\">";
-                //         $html.=$resp['total_tagged'];
-                //         $html.="</td>
-                //             <td style=\"border: 1px solid #ddd; text-align: center; padding: 8px;\">";
-                //         $html.=$resp['response_rate'];
-                //         $html.="</td>
-                //         </tr>";
-                //         $sno++;
-                //     }
-                //         $html.="</tbody>
-                //     </table>
-                // </div>";
-
                     $body=str_replace('[preview]', $preview, $body);
 
                     self::email($subject, $body, $to, $cc);
