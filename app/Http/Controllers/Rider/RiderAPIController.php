ef879ab333293f395c846cc015c7976db0ac6230
Abdul Ghani CHishti <ghani.chisti@trax.pk>
2023-08-28 12:38:40 +0500

    working on last mile app report

--- app/Http/Controllers/Rider/RiderAPIController.php
+++ app/Http/Controllers/Rider/RiderAPIController.php
@@ -8939,8 +8939,8 @@ RiderAPIController extends Controller
         } else {
             $rider_id = $request->rider_id;
 
-//            $added_at = Carbon::createFromTimestampMs($request->added_at)->toDateTimeString();
-                $added_at = $request->added_at;
+            $added_at = Carbon::createFromTimestampMs($request->added_at)->toDateTimeString();
+//                $added_at = $request->added_at;
             if (!RiderDelivery::where('delivery_note_id', $request->delivery_note_id)->where('shipment_id', $request->shipment_id)->where('delivered_status', 1)->exists()) {
                 if (DeliveryNoteShipment::join('delivery_notes as dn', 'delivery_note_shipments.delivery_note_id', 'dn.id')->where('dn.id', $request->delivery_note_id)->where('shipment_id', $request->shipment_id)->where('dn.rider_id', $rider_id)->exists()) { {
 
