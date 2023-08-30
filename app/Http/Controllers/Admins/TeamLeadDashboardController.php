5c61fa08844075d0665f18b99016cd10351e7141
Mohammad <muhammad.ahmed@trax.pk>
2023-08-30 18:35:08 +0500

    date_correct

--- app/Http/Controllers/Admins/TeamLeadDashboardController.php
+++ app/Http/Controllers/Admins/TeamLeadDashboardController.php
@@ -63,7 +63,7 @@ class TeamLeadDashboardController extends Controller
         $number_of_available_agents = Employee::where('line_manager_id', Auth::id())->where('employee_type_id', 1)->where('staff_category_id', 3)->where('is_line_manager', 0)->pluck('id')->toArray();
 
         $Attendance = EmployeeAttendance::whereIn('employee_id', $number_of_available_agents)
-            ->whereDate('attendance_date', '=', date('Y-m-d'))
+            ->whereDate('attendance_date', '=', now()->format('Y-m-d'))
             ->whereIn('id', function ($query) {
                 $query->select(DB::raw('MAX(id)'))
                     ->from('employee_attendances')
