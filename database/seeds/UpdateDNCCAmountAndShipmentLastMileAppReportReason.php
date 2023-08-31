597416c1bc72469d292243d2eebc266a207c975e
Anas_Anwer <anas.anwer@trax.pk>
2023-08-31 13:18:58 +0500

    Case by Case Dncc Amount Fixed Seeder

--- database/seeds/UpdateDNCCAmountAndShipmentLastMileAppReportReason.php
+++ database/seeds/UpdateDNCCAmountAndShipmentLastMileAppReportReason.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Models\Admin\DeliveryNoteStationDepositNote;
+use App\Http\Models\Admin\StationDepositNote;
 use App\Http\Models\ShipmentsJourney;
 use Illuminate\Database\Seeder;
 use App\Http\Models\Admin\DeliveryNote;
@@ -15,35 +17,43 @@ class UpdateDNCCAmountAndShipmentLastMileAppReportReason extends Seeder
      */
     public function run()
     {
-        $delivery_note_ids = [1634250, 1634276];
-        foreach ($delivery_note_ids as $delivery_note_id){
-            $delivery_note = DeliveryNote::find($delivery_note_id);
-            if($delivery_note){
-                $delivery_note_shipments = DeliveryNoteShipment::where('delivery_note_id', $delivery_note->id);
-                if($delivery_note_shipments->exists()){
-                    $dncc_amount = 0;
-                    $delivered_count = 0;
-                    $delivery_note_shipments = $delivery_note_shipments->get();
-                    foreach ($delivery_note_shipments as $delivery_note_shipment){
-                        $shipment = Shipment::find($delivery_note_shipment->shipment_id);
-                        if($shipment){
-                            $max_shipment_journey = ShipmentsJourney::where('shipment_id', $shipment->id)->orderBy('id', 'desc')->first();
-                            if($max_shipment_journey){
-                                $shipment->shipper_status_id = $max_shipment_journey->shipper_status_id;
-                                $shipment->consignee_status_id = $max_shipment_journey->shipper_status_id;
+        $first_sdn_number = 115248;
+        $station_deposit_notes = StationDepositNote::where('id', '>=', $first_sdn_number);
+        if($station_deposit_notes->exists()){
+            $station_deposit_notes = $station_deposit_notes->get();
+            foreach ($station_deposit_notes as $station_deposit_note){
+                $delivery_note_ids = DeliveryNoteStationDepositNote::where('station_deposit_note_id', $station_deposit_note->id)->pluck('delivery_note_id')->toArray();
+                dd($delivery_note_ids);
+                foreach ($delivery_note_ids as $delivery_note_id){
+                    $delivery_note = DeliveryNote::find($delivery_note_id);
+                    if($delivery_note){
+                        $delivery_note_shipments = DeliveryNoteShipment::where('delivery_note_id', $delivery_note->id);
+                        if($delivery_note_shipments->exists()){
+                            $dncc_amount = 0;
+                            $delivered_count = 0;
+                            $delivery_note_shipments = $delivery_note_shipments->get();
+                            foreach ($delivery_note_shipments as $delivery_note_shipment){
+                                $shipment = Shipment::find($delivery_note_shipment->shipment_id);
+                                if($shipment){
+                                    $max_shipment_journey = ShipmentsJourney::where('shipment_id', $shipment->id)->orderBy('id', 'desc')->first();
+                                    if($max_shipment_journey){
+                                        $shipment->shipper_status_id = $max_shipment_journey->shipper_status_id;
+                                        $shipment->consignee_status_id = $max_shipment_journey->shipper_status_id;
 
-                                if(in_array($delivery_note_shipment->status, [6,7])){
-                                    $shipment->received_amount = $shipment->amount;
-                                    $dncc_amount = $dncc_amount + $shipment->amount;
-                                    $delivered_count++;
+                                        if(in_array($delivery_note_shipment->status, [6,7])){
+                                            $shipment->received_amount = $shipment->amount;
+                                            $dncc_amount = $dncc_amount + $shipment->amount;
+                                            $delivered_count++;
+                                        }
+                                        $shipment->save();
+                                    }
                                 }
-                                $shipment->save();
                             }
+                            $delivery_note->received_cod_amount = $dncc_amount;
+                            $delivery_note->delivered_shipments = $delivered_count;
+                            $delivery_note->save();
                         }
                     }
-                    $delivery_note->received_cod_amount = $dncc_amount;
-                    $delivery_note->delivered_shipments = $delivered_count;
-                    $delivery_note->save();
                 }
             }
         }
