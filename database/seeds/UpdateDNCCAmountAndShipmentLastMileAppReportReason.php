234bf84b77707c231e1f9bb245bda798b88d9461
Anas_Anwer <anas.anwer@trax.pk>
2023-08-31 17:15:08 +0500

    Case by Case Dncc Amount Fixed Seeder

--- database/seeds/UpdateDNCCAmountAndShipmentLastMileAppReportReason.php
+++ database/seeds/UpdateDNCCAmountAndShipmentLastMileAppReportReason.php
@@ -43,7 +43,7 @@ class UpdateDNCCAmountAndShipmentLastMileAppReportReason extends Seeder
                                         $shipment->shipper_status_id = $max_shipment_journey->shipper_status_id;
                                         $shipment->consignee_status_id = $max_shipment_journey->shipper_status_id;
 
-                                        if(in_array($delivery_note_shipment->status, [2, 6, 7])){
+                                        if(in_array($delivery_note_shipment->status, [6, 7])){
                                             $shipment->received_amount = $shipment->amount;
                                             $dncc_amount = $dncc_amount + $shipment->amount;
                                             $delivered_count++;
