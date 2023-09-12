a87b5585d1508ed6d3f5f75ad9ca2aebc1590678
Mohammad <muhammad.ahmed@trax.pk>
2023-09-12 13:10:16 +0500

    job_changes_fintech

--- app/Http/Controllers/Admins/DeliveryController.php
+++ app/Http/Controllers/Admins/DeliveryController.php
@@ -1119,44 +1119,42 @@ class DeliveryController extends Controller
                     }
                 }
 
-                foreach ($valid_shipments as $index => $shipment) {
+                foreach ($valid_shipments as $index => $shipment_id) {
 
-                    $shipment_otp = ShipmentOtp::where('shipment_id', $shipment);
+                    $shipment_otp = ShipmentOtp::where('shipment_id', $shipment_id);
                     $dbf_otp = mt_rand(100000, 999999);
                     if ($shipment_otp->exists()) {
                         $shipment_otp = $shipment_otp->first();
                     } else {
                         $shipment_otp = new ShipmentOtp();
-                        $shipment_otp->shipment_id = $shipment;
+                        $shipment_otp->shipment_id = $shipment_id;
                     }
                     $shipment_otp->dbf_otp = $dbf_otp;
                     $shipment_otp->rider_id = null;
                     $shipment_otp->latitude = null;
                     $shipment_otp->longitude = null;
 
-                    $pos = array_keys($shipments, $shipment);
+                    $pos = array_keys($shipments, $shipment_id);
                     if ($notifications[$pos[0]]) {
-                        $shipment_obj = Shipment::find($shipment);
+                        $shipment_obj = Shipment::find($shipment_id);
                         $otp = mt_rand(100000, 999999);
                         $shipment_otp->otp = $otp;
                         $shipment_otp->save();
                         if ($shipment_obj->amount == 0) {
                             //English
-                            NotificationsController::send(132, $note->id, $shipment);
+                            NotificationsController::send(132, $note->id, $shipment_id);
                             //Urdu
-                            NotificationsController::send(135, $note->id, $shipment);
+                            NotificationsController::send(135, $note->id, $shipment_id);
                         } else {
                             $environment = config('app.env');
                             if ($environment == 'production' || $environment == 'staging') {
                                 //When Admin Create Delivery Note
-                                $payment_detials = PayfastApiCall::ApiCall($note->id, $shipment);
-                                $rand = $payment_detials['unique_key'];
-                                $payment_link = $payment_detials['payment_link'];
-                                $url = $payment_detials['url'];
-                                $shipments_id = array_wrap($shipment);
-                                Log::channel('trax_pay_test')->info('sh '. json_encode($shipments_id, true));
-
-                                CountFintechCharges::dispatch($shipments_id, $payment_link, $rand, $url);
+                                $payment_details = PayfastApiCall::ApiCall($note->id, $shipment_id);
+                                $rand = $payment_details['unique_key'];
+                                $payment_link = $payment_details['payment_link'];
+                                $url = $payment_details['url'];
+                                Log::channel('trax_pay_test')->info('sh '. json_encode($shipment_id, true));
+                                CountFintechCharges::dispatch($shipment_id, $payment_link, $rand, $url);
                                 NotificationsController::send(12, $note->id, $shipment, $payment_link);
                             }
                         }
@@ -9779,10 +9777,10 @@ class DeliveryController extends Controller
                     }
 
                     foreach ($valid_shipments as $shipment) {
-                        $payment_detials = PayfastApiCall::ApiCall($note->id, $shipment);
-                        $rand = $payment_detials['unique_key'];
-                        $payment_link = $payment_detials['payment_link'];
-                        $url = $payment_detials['url'];
+                        $payment_details = PayfastApiCall::ApiCall($note->id, $shipment);
+                        $rand = $payment_details['unique_key'];
+                        $payment_link = $payment_details['payment_link'];
+                        $url = $payment_details['url'];
                         $shipments_id = array_wrap($shipment);
                         CountFintechCharges::dispatch($shipments_id, $payment_link, $rand, $url);
                         NotificationsController::send(10, $note->id, $shipment);
