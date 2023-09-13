919a1a7582aae2cdaa2d8abb69c0929443a3f547
Mohammad <muhammad.ahmed@trax.pk>
2023-09-13 17:13:07 +0500

    Screen Added RV Shipper Priority

---
+++ database/seeds/ModulePermissionForRvPriorityShipper.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ModulePermissionForRvPriorityShipper extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('module_permissions')->insert(array(
+            array('id' => 906, 'name' => 'Setting - RV Priority Shipper', 'module_id' => 14),
+        ));
+
+
+        $timestamp = \Carbon\Carbon::now()->format('Y-m-d H:i:s');
+        DB::table('admins_screen_list')->insert(array(
+            array('created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Settings > Shippers > RV Priority Shipper', 'url'=>'admin.settings.rv_shipper_priority.index', 'permission_id' => 906),           
+        ));
+
+    }
+}
