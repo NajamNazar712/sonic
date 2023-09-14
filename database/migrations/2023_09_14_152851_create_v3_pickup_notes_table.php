a4a4f4fc06339a456ef7dee8424228821b3e62cc
Muhammad Waqas <muhammad.waqas@trax.pk>
2023-09-14 17:58:53 +0500

    in progress

---
+++ database/migrations/2023_09_14_152851_create_v3_pickup_notes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateV3PickupNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('v3_pickup_notes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('v3_pickup_notes');
+    }
+}
