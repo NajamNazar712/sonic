62179c8c3eaeaf225f1a5e4d1daf2828c22ab132
TahaKhalid <tahatk20@gmail.com>
2023-09-14 12:51:37 +0500

    add shipment ids missing entries for create payments

--- database/seeds/CreatePaymentsForMissingShipmentsSeeder.php
+++ database/seeds/CreatePaymentsForMissingShipmentsSeeder.php
@@ -14,7 +14,7 @@ class CreatePaymentsForMissingShipmentsSeeder extends Seeder
      */
     public function run()
     {
-        $shipment_ids = [27285757];
+        $shipment_ids = [27279080, 26579493];
 
         foreach ($shipment_ids as $shipment_id){
             $shipment = Shipment::find($shipment_id);
