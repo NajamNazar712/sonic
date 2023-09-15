5783ca2b74a59a3a095dc97bc14c5afb16a60031
Mohammad <muhammad.ahmed@trax.pk>
2023-09-16 03:10:52 +0500

    Order Set Final

--- resources/views/admin/leads/team_lead.blade.php
+++ resources/views/admin/leads/team_lead.blade.php
@@ -7,15 +7,11 @@
         Team Leads Management
     </h1>
 
-
     @include('admin.inc.messages')
-
-
 
     {{-- <input type="hidden" class="datepicker"> --}}
     <div class="modal fade" id="joiningDateModal" tabindex="-1" role="dialog" aria-labelledby="joiningDateModalLabel"
         aria-hidden="true">
-
     </div>
 
     <div class="modal fade" id="shipments_modal" data-backdrop="static" role="dialog" aria-labelledby="shipments_modal"
@@ -666,7 +662,6 @@
                 ],
                 rowId: 'employee_id',
                 columns: [
-                    // {data: 'id', orderable: false, searchable: false, class: 'text-center align-middle select p-1', targets: 0, render: function (data, type, row) {return '';}},
                     {
                         orderable: false,
                         searchable: false,
@@ -753,9 +748,7 @@
                     }
                 ],
                 rowCallback: function(row, data, index) {
-                    // $('td:eq(0)', row).addClass('select-checkbox');
                     var info = table.page.info();
-                    console.log(data);
                     $('td:eq(0)', row).html(index + 1 + info.page * info.length);
                 },
                 initComplete: function() {
@@ -833,7 +826,6 @@
                         {
                             $(employee_attendance).appendTo($(search))
                             .on( 'change', function () {
-                                console.log($(this).val());
                                     column.search($(this).val(), false, false, true).draw();
                                 } ).wrap(td);
                         }
@@ -1197,8 +1189,6 @@
                 cities = $(this).attr('data-city');
                 $('#assign_agent_hubs').append('<input type="hidden" name="employee_id" value="' + employeeId + '">');
                 selectedCities = cities.toString().split(',');
-
-                console.log(selectedCities);
                 if (cities != null) {
                     var selectedOptions = [];
                     $('#search_origin option').each(function() {
@@ -1209,7 +1199,6 @@
                         $(this).prop('selected', false);
                     });
 
-                    $('#search_origin').empty();
                     $.each(selectedCities, function(index, value) {
                         var option = selectedOptions.find(function(opt) {
                             return opt.val() === value;
@@ -1229,11 +1218,6 @@
                     $('#AssignHubModal').modal('show');
                 }
             });
-
-            
-
-
-
 
             var $select2 = $('#search_origin').select2({
                 templateSelection: template,
