ea89116ac324857c47c2501feada8d23b5e80185
Shariq Raees <Shariq.raees@trax.pk>
2023-09-19 19:42:23 +0500

    create payments for missing shipments in seeder

--- database/seeds/CreatePaymentsForMissingShipmentsSeeder.php
+++ database/seeds/CreatePaymentsForMissingShipmentsSeeder.php
@@ -15,8 +15,9 @@ class CreatePaymentsForMissingShipmentsSeeder extends Seeder
     public function run()
     {
         $shipment_ids = [
-            27757339,
-            27757314
+            26754299,
+            26754159,
+            26753506
         ];
 
         foreach ($shipment_ids as $shipment_id){
