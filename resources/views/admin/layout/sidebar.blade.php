397fdb14c688079ef12982650fb4fb8d738afd85
junaidraja96 <junaid.raja@trax.pk>
2023-09-21 10:28:02 +0500

    Change rv report name in sidebar & title

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -1693,7 +1693,7 @@
                         <li><a class="menu-item" href="{{ route('admin.reports.overland.index') }}">Overland Report</a></li>
                         @endif
                         @if (session('role_id') == 1 || in_array(905, session('permissions')))
-                        <li><a class="menu-item" href="{{ route('admin.reports.rv_report.index') }}">Rv Report</a></li>
+                        <li><a class="menu-item" href="{{ route('admin.reports.rv_report.index') }}">Overall RV Action Report</a></li>
                         @endif
                     </ul>
                 </li>
