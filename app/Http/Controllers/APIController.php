0a083b507e70e769d686c86b091034bb7bbd40e3
Abdul Ghani CHishti <ghani.chisti@trax.pk>
2023-09-22 11:23:32 +0500

    minor change in booking API

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -647,7 +647,7 @@ class APIController extends Controller
 
                 'same_day_timing_id' => ['required_if:shipping_mode_id,4', 'integer', 'digits_between:1,10', 'exists:shipping_mode_same_day_timings,id'],
                 'amount' => ['required_if:service_type_id,1,2,3', 'nullable', 'numeric', 'between:0,1000000'],
-                'parcel_value' => ['nullable','numeric','between:1,1000000'],
+                'parcel_value' => ['nullable','numeric','digits_between:1,1000000'],
                 // 'payment_mode_id' => ['required_if:service_type_id,1,2,3', 'nullable', 'integer', 'digits_between:1,10', Rule::exists('payment_modes', 'id')->where(function($query) {
                 //     $query->whereNotIn('id', [3]);
                 // })],
