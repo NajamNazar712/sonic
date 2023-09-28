5fe440622b26451711a75028e7e92c7172513ab1
Mohammad <muhammad.ahmed@trax.pk>
2023-09-28 14:07:04 +0500

    Seeder Added.

--- resources/views/admin/return/index.blade.php
+++ resources/views/admin/return/index.blade.php
@@ -1622,7 +1622,7 @@
                                 },
                             @endif,
 
-                            @if (session('role_id') == 1 || in_array(906, session('permissions')))
+                            @if (session('role_id') == 1 || in_array(907, session('permissions')))
                                 {
                                     text: 'Call History',
                                     className: 'btn btn-primary call_history',
