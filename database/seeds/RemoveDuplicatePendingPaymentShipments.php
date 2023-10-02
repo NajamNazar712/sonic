e89d7e70c1d392e6363e8703dd6ffc4a5e6c7935
TahaKhalid <tahatk20@gmail.com>
2023-10-02 16:42:46 +0500

    make seprate decrement for each values

--- database/seeds/RemoveDuplicatePendingPaymentShipments.php
+++ database/seeds/RemoveDuplicatePendingPaymentShipments.php
@@ -39,7 +39,8 @@ class RemoveDuplicatePendingPaymentShipments extends Seeder
                     if($key != 0){
 
                         $value->delete();
-                        PendingPayment::where('id', $value->pending_payment_id)->decrement(['total_shipments' => 1, 'delivered_shipments' => 1]);
+                        PendingPayment::where('id', $value->pending_payment_id)->decrement('total_shipments',1);
+                        PendingPayment::where('id', $value->pending_payment_id)->decrement('delivered_shipments',1);
                         dd($value);
                         
                         
