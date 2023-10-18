a2d8ec71074c840739603989d952feff4fbc0e85
Mohammad <muhammad.ahmed@trax.pk>
2023-10-18 15:55:18 +0500

    fixes_final

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -10512,7 +10512,7 @@ class NotificationsController extends Controller
 
                         $daysDifference = $created_at->diffInDays($updated_at);
                         if ($crm_complaint['status_id'] == 4 && $daysDifference <= 2){
-                            $crm_complaints_2_days_closure[] = $daysDifference;
+                            $crm_complaints_2_days_closure[] = 1;
                         }
 
                     }
@@ -10531,7 +10531,7 @@ class NotificationsController extends Controller
 
                         $daysDifference = $created_at->diffInDays($updated_at);
                         if ($crm_service_request['status_id'] == 4 && $daysDifference <= 2){
-                            $crm_services_2_days_closure[] = $daysDifference;
+                            $crm_services_2_days_closure[] = 1;
                         }
                     }
 
@@ -10549,7 +10549,7 @@ class NotificationsController extends Controller
                         
                         $daysDifference = $created_at->diffInDays($updated_at);
                         if ($crm_claim['status_id'] == 4 && $daysDifference <= 10){
-                            $crm_claims_10_days_closure[] = $daysDifference;
+                            $crm_claims_10_days_closure[] = 1;
                         }
                     }
 
