1f4bc2905bdd63d1f565fb4d0888f4aadc53e786
Zain <zain.hyder@trax.pk>
2023-10-19 15:28:37 +0500

    change connection report r2

--- app/Http/Controllers/Admins/DeliveryController.php
+++ app/Http/Controllers/Admins/DeliveryController.php
@@ -151,8 +151,11 @@ class DeliveryController extends Controller
         }
         // $status = array(2, 4, 6, 10, 13, 15, 49, 55, 59); //new changes for rv
         
+         // $status = array(2, 4, 6, 10, 13, 15, 49, 55, 59); //new changes for rv
+        
         $status = array(2, 4, 6, 7, 8, 9, 10, 13, 15, 49, 55, 59); //for pending deliveries (old)
-        $shipments = Shipment::join('users as u', 'shipments.user_id', '=', 'u.id')
+        $shipments = DB::connection('reports_2')->table('shipments')
+            ->join('users as u', 'shipments.user_id', '=', 'u.id')
             ->join('user_shipping_infos AS usi', 'shipments.pickup_address_id', '=', 'usi.id')
             ->join('cities AS oc', 'usi.city_id', '=', 'oc.id')
             ->join('cities AS dc', 'shipments.consignee_city_id', '=', 'dc.id')
