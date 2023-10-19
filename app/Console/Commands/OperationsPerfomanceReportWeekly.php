4409dd98abd679aa813f581f7da058eabfd50332
Mohammad <muhammad.ahmed@trax.pk>
2023-10-19 14:46:57 +0500

    changes

--- app/Console/Commands/OperationsPerfomanceReportWeekly.php
+++ app/Console/Commands/OperationsPerfomanceReportWeekly.php
@@ -43,6 +43,7 @@ class OperationsPerfomanceReportWeekly extends Command
         $id = 224;
         $currentDate = Carbon::now();
         $from = $currentDate->copy()->previous(Carbon::FRIDAY);
+        $from = $from->copy()->previous(Carbon::FRIDAY);
         $to = $from->copy()->next(Carbon::THURSDAY)->toDateString();
         $from = $from->toDateString();
         $this->operations_performance_export_to_excel($from, $to, $id);
