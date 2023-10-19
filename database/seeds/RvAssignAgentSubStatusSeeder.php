b36a46fb7033a21a2c3a566321c3fd59162f6b45
Zain <zain.hyder@trax.pk>
2023-10-19 14:43:29 +0500

    changes in RvAssignAgentSubStatusSeeder

--- database/seeds/RvAssignAgentSubStatusSeeder.php
+++ database/seeds/RvAssignAgentSubStatusSeeder.php
@@ -16,24 +16,42 @@ class RvAssignAgentSubStatusSeeder extends Seeder
         DB::table('rv_assign_agent_sub_statuses')->truncate();
         
         DB::table('rv_assign_agent_sub_statuses')->insert(array(
+
+
             //Return Confirm Reasons in Shipment Status Reason Table
-            array('name' => 'Consignee Unresponsive on Phone call','shipment_status_reason_id'=> 2,'rv_assign_agent_status_id'=> 1,'is_active'=>1,'created_at' => $timestamp, 'updated_at' => $timestamp),
-            array('name' => 'Consignee Out Of City','shipment_status_reason_id'=> 5,'rv_assign_agent_status_id'=> 1,'is_active'=> 1,'created_at' => $timestamp, 'updated_at' => $timestamp),
+            // array('name' => 'Issue in the Product','shipment_status_reason_id'=> 47,'rv_assign_agent_status_id'=> 1,'is_active'=>1,'created_at' => $timestamp, 'updated_at' => $timestamp),
+            // array('name' => 'Issue in the COD Amount','shipment_status_reason_id'=> 9,'rv_assign_agent_status_id'=> 1,'is_active'=>1,'created_at' => $timestamp, 'updated_at' => $timestamp),
+            // array('name' => 'Consignee Unavailable','shipment_status_reason_id'=> 52,'rv_assign_agent_status_id'=> 1,'is_active'=>1,'created_at' => $timestamp, 'updated_at' => $timestamp),
+            // array('name' => 'No such order/consignee','shipment_status_reason_id'=> 49'rv_assign_agent_status_id'=> 1,'is_active'=>1,'created_at' => $timestamp, 'updated_at' => $timestamp),
+            // array('name' => 'Delay in Delivery','shipment_status_reason_id'=> 39,'rv_assign_agent_status_id'=> 1,'is_active'=>1,'created_at' => $timestamp, 'updated_at' => $timestamp),
+
+
+            //Return Confirm Reasons Not Found in Shipment Status Reason Table
+            // array('name' => 'Consignee is not interested','shipment_status_reason_id'=> Null,'rv_assign_agent_status_id'=> 1,'is_active'=>1,'created_at' => $timestamp, 'updated_at' => $timestamp),
+            // array('name' => 'Consignee wants to open the shipment','shipment_status_reason_id'=> Null,'rv_assign_agent_status_id'=> 1,'is_active'=>1,'created_at' => $timestamp, 'updated_at' => $timestamp),
+            // array('name' => 'Duplicate Order','shipment_status_reason_id'=> Null,'rv_assign_agent_status_id'=> 1,'is_active'=>1,'created_at' => $timestamp, 'updated_at' => $timestamp),
+            // array('name' => 'Number Not Pertain to Consignee','shipment_status_reason_id'=> Null,'rv_assign_agent_status_id'=> 1,'is_active'=>1,'created_at' => $timestamp, 'updated_at' => $timestamp),
+            // array('name' => 'Refused after opening the shipment','shipment_status_reason_id'=> Null,'rv_assign_agent_status_id'=> 1,'is_active'=>1,'created_at' => $timestamp, 'updated_at' => $timestamp),
+
+
+            //Return Confirm Reasons in Shipment Status Reason Table
+            array('name' => 'Consignee Unresponsive on Phone call','shipment_status_reason_id'=> 2,'rv_assign_agent_status_id'=> 0,'is_active'=> 0,'created_at' => $timestamp, 'updated_at' => $timestamp),
+            array('name' => 'Consignee Out Of City','shipment_status_reason_id'=> 5,'rv_assign_agent_status_id'=> 1,'is_active'=> 0,'created_at' => $timestamp, 'updated_at' => $timestamp),
             array('name' => 'Issue in the COD Amount','shipment_status_reason_id'=> 9,'rv_assign_agent_status_id'=> 1,'is_active'=> 1,'created_at' => $timestamp, 'updated_at' => $timestamp),
-            array('name' => 'Consignee is not Responding','shipment_status_reason_id'=> 13,'rv_assign_agent_status_id'=> 1,'is_active'=> 1,'created_at' => $timestamp, 'updated_at' => $timestamp),
-            array('name' => 'Consignee Refused','shipment_status_reason_id'=> 38,'rv_assign_agent_status_id'=> 1,'is_active'=> 1,'created_at' => $timestamp, 'updated_at' => $timestamp),
+            array('name' => 'Consignee is not Responding','shipment_status_reason_id'=> 13,'rv_assign_agent_status_id'=> 1,'is_active'=> 0,'created_at' => $timestamp, 'updated_at' => $timestamp),
+            array('name' => 'Consignee Refused','shipment_status_reason_id'=> 38,'rv_assign_agent_status_id'=> 1,'is_active'=> 0,'created_at' => $timestamp, 'updated_at' => $timestamp),
             array('name' => 'Delay in Delivery','shipment_status_reason_id'=> 39,'rv_assign_agent_status_id'=> 1,'is_active'=> 1,'created_at' => $timestamp, 'updated_at' => $timestamp),
-            array('name' => 'Address Issue','shipment_status_reason_id'=> 46,'rv_assign_agent_status_id'=> 1,'is_active'=> 1,'created_at' => $timestamp, 'updated_at' => $timestamp),
+            array('name' => 'Address Issue','shipment_status_reason_id'=> 46,'rv_assign_agent_status_id'=> 1,'is_active'=> 0,'created_at' => $timestamp, 'updated_at' => $timestamp),
             array('name' => 'Issue in the Product','shipment_status_reason_id'=> 47,'rv_assign_agent_status_id'=> 1,'is_active'=> 1,'created_at' => $timestamp, 'updated_at' => $timestamp),
-            array('name' => 'Shipment Damage','shipment_status_reason_id'=> 48,'rv_assign_agent_status_id'=> 1,'is_active'=> 1,'created_at' => $timestamp, 'updated_at' => $timestamp),
+            array('name' => 'Shipment Damage','shipment_status_reason_id'=> 48,'rv_assign_agent_status_id'=> 1,'is_active'=> 0,'created_at' => $timestamp, 'updated_at' => $timestamp),
             array('name' => 'No such order/consignee','shipment_status_reason_id'=> 49,'rv_assign_agent_status_id'=> 1,'is_active'=> 1,'created_at' => $timestamp, 'updated_at' => $timestamp),
-            array('name' => 'NSA / OSA Parcel','shipment_status_reason_id'=> 50,'rv_assign_agent_status_id'=> 1,'is_active'=> 1,'created_at' => $timestamp, 'updated_at' => $timestamp),
+            array('name' => 'NSA / OSA Parcel','shipment_status_reason_id'=> 50,'rv_assign_agent_status_id'=> 1,'is_active'=> 0,'created_at' => $timestamp, 'updated_at' => $timestamp),
             array('name' => 'Consignee Unavailable','shipment_status_reason_id'=> 52,'rv_assign_agent_status_id'=> 1,'is_active'=> 1,'created_at' => $timestamp, 'updated_at' => $timestamp),
-            array('name' => 'As per Shipper Request','shipment_status_reason_id'=> 53,'rv_assign_agent_status_id'=> 1,'is_active'=> 1,'created_at' => $timestamp, 'updated_at' => $timestamp),
-            array('name' => 'Wants to Open2','shipment_status_reason_id'=> 54,'rv_assign_agent_status_id'=> 1,'is_active'=> 1,'created_at' => $timestamp, 'updated_at' => $timestamp),
-            array('name' => 'Consignee Unresponsive','shipment_status_reason_id'=> 55,'rv_assign_agent_status_id'=> 1,'is_active'=> 1,'created_at' => $timestamp, 'updated_at' => $timestamp),
-            array('name' => 'Delay in Dispatch from Shipper','shipment_status_reason_id'=> 59,'rv_assign_agent_status_id'=> 1,'is_active'=> 1,'created_at' => $timestamp, 'updated_at' => $timestamp),
-            array('name' => 'A OPEN','shipment_status_reason_id'=> 62,'rv_assign_agent_status_id'=> 1,'is_active'=> 1,'created_at' => $timestamp, 'updated_at' => $timestamp),
+            array('name' => 'As per Shipper Request','shipment_status_reason_id'=> 53,'rv_assign_agent_status_id'=> 1,'is_active'=> 0,'created_at' => $timestamp, 'updated_at' => $timestamp),
+            array('name' => 'Wants to Open2','shipment_status_reason_id'=> 54,'rv_assign_agent_status_id'=> 1,'is_active'=> 0,'created_at' => $timestamp, 'updated_at' => $timestamp),
+            array('name' => 'Consignee Unresponsive','shipment_status_reason_id'=> 55,'rv_assign_agent_status_id'=> 1,'is_active'=> 0,'created_at' => $timestamp, 'updated_at' => $timestamp),
+            array('name' => 'Delay in Dispatch from Shipper','shipment_status_reason_id'=> 59,'rv_assign_agent_status_id'=> 1,'is_active'=> 0,'created_at' => $timestamp, 'updated_at' => $timestamp),
+            array('name' => 'A OPEN','shipment_status_reason_id'=> 62,'rv_assign_agent_status_id'=> 1,'is_active'=> 0,'created_at' => $timestamp, 'updated_at' => $timestamp),
 
             //Return Confirm Reasons Not Found in Shipment Status Reason Table
             array('name' => 'Consignee is not interested','shipment_status_reason_id'=> Null,'rv_assign_agent_status_id'=> 1,'is_active'=> 1,'created_at' => $timestamp, 'updated_at' => $timestamp),
