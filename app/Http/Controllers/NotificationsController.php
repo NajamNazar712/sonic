57be7b78a705597c05305568a4edd9ce8454f249
Mohammad <muhammad.ahmed@trax.pk>
2023-10-20 14:48:03 +0500

    Condition Added

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -10510,7 +10510,7 @@ class NotificationsController extends Controller
                             }
     
                             $daysDifference = $created_at->diffInDays($updated_at);
-                            if ($crm_complaint['status_id'] == 4 && $daysDifference <= 2 && $updated_at->diffInDays($now) <= 2 && $updated_at->isToday()){
+                            if ($crm_complaint['status_id'] == 4 && $daysDifference <= 2 && $updated_at->diffInDays($now) <= 2){
                                 $crm_complaints_2_days_closure[] = $daysDifference;
                             }
     
@@ -10530,7 +10530,7 @@ class NotificationsController extends Controller
                             }
     
                             $daysDifference = $created_at->diffInDays($updated_at);
-                            if ($crm_service_request['status_id'] == 4 && $daysDifference <= 2 && $updated_at->diffInDays($now) <= 2 && $updated_at->isToday()){
+                            if ($crm_service_request['status_id'] == 4 && $daysDifference <= 2 && $updated_at->diffInDays($now) <= 2){
                                 $crm_services_2_days_closure[] = $daysDifference;
                             }
                         }
@@ -10549,7 +10549,7 @@ class NotificationsController extends Controller
                             }
                             
                             $daysDifference = $created_at->diffInDays($updated_at);
-                            if ($crm_claim['status_id'] == 4 && $daysDifference <= 10 && $updated_at->diffInDays($now) <= 10 && $updated_at->isToday()){
+                            if ($crm_claim['status_id'] == 4 && $daysDifference <= 10 && $updated_at->diffInDays($now) <= 10){
                                 $crm_claims_10_days_closure[] = $daysDifference;
                             }
                         }
