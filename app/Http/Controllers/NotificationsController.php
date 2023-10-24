00ab6a7e7c4e1739f162157f73a3044ed27eed91
Mohammad <muhammad.ahmed@trax.pk>
2023-10-24 12:38:37 +0500

    Final Correction

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -8579,9 +8579,9 @@ class NotificationsController extends Controller
                         ];
                     })
                     ->toArray();
-                    $hubs = array_values($hubs);
+
+                    $hubs = $hubs[0];
                     $admin_ids = AdminHub::whereIn('hub_id', $hubs)->pluck('admin_id')->toArray();
-                    dd($hubs, $admin_ids);
                     foreach($admin_ids as $admin_id){
                         $admin = Admin::find($admin_id);
                         $roles = [3,81,125]; //Area Operation Manager of Origin & Destination, Regional Director Operations of Origin & Destination, Regional Director of Origin & Destination.
