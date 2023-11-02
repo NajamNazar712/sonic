a5f7c1d0829aa96714078e56335181b050b209f0
Mohammad <muhammad.ahmed@trax.pk>
2023-11-02 13:54:00 +0500

    Working ON SCREEN

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -1828,6 +1828,11 @@
                                                    href="{{ route('admin.settings.mms_report.index') }}">MMS Report
                                                     Setting</a> </li>
                                         @endif
+
+                                        @if (session('role_id') == 1 || in_array(868, session('permissions')))
+                                        <li><a class="menu-item"
+                                               href="{{ route('admin.settings.shippers.bypass_weight.index') }}">Bypassing of weight entry</a> </li>
+                                    @endif
                                 </ul>
 
                             </li>
