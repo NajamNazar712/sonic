7ae7805cf7b463d0a1cc4c3bf7c319b9076e92b6
Mohammad <muhammad.ahmed@trax.pk>
2023-11-02 14:55:25 +0500

    Module Permission Has Been added

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -1829,7 +1829,7 @@
                                                     Setting</a> </li>
                                         @endif
 
-                                        @if (session('role_id') == 1 || in_array(868, session('permissions')))
+                                        @if (session('role_id') == 1 || in_array(909, session('permissions')))
                                         <li><a class="menu-item"
                                                href="{{ route('admin.settings.shippers.bypass_weight.index') }}">Bypassing of weight entry</a> </li>
                                     @endif
