86bb7c4f0089ad7e1960b0b18eba1cacad2d4b4e
Shariq Raees <Shariq.raees@trax.pk>
2023-11-07 13:41:28 +0500

    remove comment

--- app/Http/Controllers/Admins/AdminReportsController.php
+++ app/Http/Controllers/Admins/AdminReportsController.php
@@ -3432,10 +3432,6 @@ class AdminReportsController extends Controller
             }
         }
 
-        // $sales = $sales->get();
-
-        // dd($sales);
-
         $datatable = Datatables::of($sales)
             ->addColumn('attempts', function ($shipment) {
                 $out_for_delivery = DB::connection('reports')->table('shipments_journey')->where('shipment_id', $shipment->shipment_id)->where('shipper_status_id', 5)->count();
