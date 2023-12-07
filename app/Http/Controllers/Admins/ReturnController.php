a4610ba215c43b547cc93846a20c2e966882cd63
Mohammad <muhammad.ahmed@trax.pk>
2023-12-07 13:44:06 +0500

    chnages

--- app/Http/Controllers/Admins/ReturnController.php
+++ app/Http/Controllers/Admins/ReturnController.php
@@ -238,7 +238,11 @@ class ReturnController extends Controller
         $service_type = BookingType::all();
         $rv_tickets = count(RvShipmentAssignAgent::get()) > 0 ? count(RvShipmentAssignAgent::get()) : 1;
         $total_of_shipments = $this->shipments()->get()->count();
-        $oldest_shipments = $total_of_shipments - count(RvShipmentAssignAgent::leftJoin('shipments as sh','rv_shipment_assign_agents.shipment_id','sh.id')->where('sh.shipper_status_id', 12)->get());
+        $oldest_shipments = $total_of_shipments - count(RvShipmentAssignAgent::leftJoin('shipments as sh','rv_shipment_assign_agents.shipment_id','sh.id')->leftJoin('shipments_journey', function ($join) {
+            $join->on('shipments_journey.shipment_id', '=', 'sh.id')
+            ->where('shipments_journey.id','=',
+            DB::raw('(select max(id) from shipments_journey where shipments_journey.shipment_id = sh.id)'));
+        })->whereIn('sh.shipper_status_id', [12,65,66])->whereDate('shipments_journey.created_at', '<', Carbon::today())->get());
         //Average Aging
         $aging = RvShipmentAssignAgent::select('created_at')->get();
         $totalSeconds = 0;
