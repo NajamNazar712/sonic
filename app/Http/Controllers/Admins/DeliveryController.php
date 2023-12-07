2056a2b7178ee18b90c0749b3f619233b8c621d2
Abdul Ghani CHishti <ghani.chisti@trax.pk>
2023-12-07 14:28:49 +0500

    remove lost status from debrifer verify deliveries screen

--- app/Http/Controllers/Admins/DeliveryController.php
+++ app/Http/Controllers/Admins/DeliveryController.php
@@ -3507,18 +3507,18 @@ class DeliveryController extends Controller
                 }
                 if ($flag == true) {
                     if ($deliveries->packaging_material_request == 1 && $deliveries->packaging_material_charges == '') {
-                        $where = array(7, 8, 9, 15, 18, 56);
+                        $where = array(7, 8, 9, 15, 56);
                     } else {
                         if ($deliveries->booking_type_id == 5) {
-                            $where = array(7, 8, 9, 15, 18);
+                            $where = array(7, 8, 9, 15);
                         } else {
                             if ($not_rcp === true) {
-                                $where = array(7, 8, 9, 15, 18);
+                                $where = array(7, 8, 9, 15);
                                 if ($deliveries->booking_type_id == 2) {
                                     array_push($where, 56);
                                 }
                             } else {
-                                $where = array(7, 8, 9, 12, 15, 18);
+                                $where = array(7, 8, 9, 12, 15);
                                 if ($deliveries->booking_type_id == 2) {
                                     array_push($where, 56);
                                 }
