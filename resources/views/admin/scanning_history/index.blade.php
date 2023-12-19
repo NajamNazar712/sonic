d1e6a4b083d0b9edd989b586709887da8eedb4fc
Mohammad <muhammad.ahmed@trax.pk>
2023-12-19 18:27:19 +0500

    half done almost.

--- resources/views/admin/scanning_history/index.blade.php
+++ resources/views/admin/scanning_history/index.blade.php
@@ -129,6 +129,7 @@
                             shipment += '<th><strong>Scanned By</strong></th>';
                             shipment += '<th><strong>City</strong></th>';
                             shipment += '<th><strong>Area</strong></th>';
+                            shipment += '<th><strong>Rider Pick Status</strong></th>';
                             shipment += '<th><strong>Location</strong></th>';
                             shipment += '<th><strong>Scanned Via</strong></th>';
                             shipment += '<th><strong>Scanned At</strong></th>';
@@ -146,6 +147,7 @@
                                 shipment += '<td>' + history.scanned_by + '</td>';
                                 shipment += '<td>' + history.city + '</td>';
                                 shipment += '<td>' + history.area + '</td>';
+                                shipment += '<td>' + history.rider_picked + '</td>';
                                 shipment += '<td>' + (history.area === '-' ? '-' : (history.area_log.location_status == 1 ? 'On-site' : 'Off-site')) + '</td>';
                                 shipment += '<td>' + history.updated_via + '</td>';
                                 shipment += '<td>' + history.scanned_at + '</td>';
