7722b3167d83d8a6db5efd883424f6daa91305af
Mohammad <muhammad.ahmed@trax.pk>
2023-12-20 11:30:53 +0500

    DONE almost.

--- resources/views/admin/tracking/shipment_position.blade.php
+++ resources/views/admin/tracking/shipment_position.blade.php
@@ -64,6 +64,9 @@
                                     <th class="border-primary border-darken-1">Handover Received By</th>
                                     <th class="border-primary border-darken-1">Handover Received At</th>
                                     <th class="border-primary border-darken-1">Last Action Performed</th>
+                                    <th class="border-primary border-darken-1">Rider Picked Status</th>
+                                    <th class="border-primary border-darken-1">Rider Picked At</th>
+
                                 </tr>
                                 </thead>
                             </table>
@@ -203,6 +206,9 @@
                             head.push('Handover Received By');
                             head.push('Handover Received At');
                             head.push('Last Action Performed');
+                            head.push('Rider Picked Status');
+                            head.push('Rider Picked Ait');
+
                             $.each(result.data, function(index, values) {
                                 row = [];
 
@@ -229,6 +235,8 @@
                                 row.push(values.handover_received_by);
                                 row.push(values.handover_received_at);
                                 row.push(values.last_action);
+                                row.push(values.rider_picked_status);
+                                row.push(values.rider_picked_at);
 
                                 body.push(row);
                             });
@@ -289,6 +297,9 @@
                     {data: 'handover_received_by', name: 'shipment_positions.handover_received_by', class: 'align-middle handover_received_by'},
                     {data: 'handover_received_at', name: 'shipment_positions.handover_received_at', class: 'align-middle handover_received_at'},
                     {data: 'last_action', name: 'shipment_positions.last_action', class: 'align-middle action'},
+                    {data: 'rider_picked_status', name: 'shipment_positions.shipment_id', class: 'align-middle rider_picked_status'},
+                    {data: 'rider_picked_at', name: 'shipment_positions.shipment_id', class: 'align-middle rider_picked_at'},
+
                 ],
                 rowCallback: function(row, data, index) {
 
