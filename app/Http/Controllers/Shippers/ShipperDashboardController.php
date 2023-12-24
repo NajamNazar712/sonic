e28ceee69995d44e2cff239b231e2db5129614ad
Muhammad Waqas <muhammad.waqas@trax.pk>
2023-12-24 17:45:17 +0500

    order management change

--- app/Http/Controllers/Shippers/ShipperDashboardController.php
+++ app/Http/Controllers/Shippers/ShipperDashboardController.php
@@ -623,65 +623,6 @@ class ShipperDashboardController extends Controller
              $connection = 'mysql';
          }
 
-        $count = DB::connection($connection)->table('shipments')
-        
-        
-        ->where(function ($query) use ($masp, $request) {
-            if(isset($request->search_account_type) && count($request->search_account_type) > 0){
-                $query->whereIn('shipments.user_id', $request->search_account_type);
-            } else {
-                $query->whereIn('shipments.user_id', $masp);
-            }
-        });
-
-        // ->where(function ($query){ 
-        //     $query->where('shipments.user_id', session('user_id'))
-        //         ->orwhereIn('shipments.user_id', session('sister_users'));
-        // });
-
-      
-
-        if ($request->get('booking_from_date') && $request->get('booking_from_date')) {
-            $from = $request->get('booking_from_date');
-            $to = $request->get('booking_to_date');
-            $count = $count->whereBetween('shipments.created_at', [$from, $to]);
-        }
-
-        $from_id = DB::connection($connection)->table('shipments')->select('id')->where('created_at', '>=', $from);
-
-        $to_id = NULL;
-
-        if ($from_id->exists()) {
-            $from_id = $from_id->first()->id;
-
-            $to_id = DB::connection($connection)->table('shipments')->select('id')->where('created_at', '>=', $from)->where('created_at', '<=', $to)->where('id', '>=', $from_id);
-
-            if ($to_id->exists()) {
-                $to_id = $to_id->orderBy('id', 'DESC')->first()->id;
-            }
-            else {
-                $to_id = NULL;
-            }
-        }
-        else {
-            $from_id = NULL;
-        }
-
-        if ($from_id && $to_id) {
-            $count = $count->where('shipments.id', '>=', $from_id)
-                ->where('shipments.id', '<=', $to_id);
-        }
-
-        $from_sj_id = DB::connection($connection)->table('shipments_journey')->select('id')->where('created_at', '>=', $from);
-
-        if ($from_sj_id->exists()) {
-            $from_sj_id = $from_sj_id->first()->id;
-        }
-        else {
-            $from_sj_id = NULL;
-        }
-
-        $count = $count->count();
 
         $shipments = DB::connection($connection)->table('shipments')
             ->leftJoin('users as u', 'shipments.user_id', '=', 'u.id')
@@ -692,10 +633,10 @@ class ShipperDashboardController extends Controller
             ->leftJoin('shipping_modes as sm','sm.id','=','shipments.shipping_mode_id')
             ->leftJoin('booking_types as bt','bt.id','=','shipments.booking_type_id')
             ->leftJoin('payment_modes as pm','pm.id','=','shipments.payment_mode_id')
-            ->join('shipments_journey', function ($join) use($from) {
+            ->join('shipments_journey', function ($join) {
                 $join->on('shipments_journey.shipment_id', '=', 'shipments.id')
                     ->where('shipments_journey.id', '=',
-                        DB::raw('(select max(id) from shipments_journey where shipments_journey.shipment_id = shipments.id and shipments_journey.verification = 1 and shipments_journey.created_at > "'. $from .'")'));
+                        DB::raw('(select max(id) from shipments_journey where shipments_journey.shipment_id = shipments.id and shipments_journey.verification = 1)'));
             })
             ->leftJoin('shipment_status as ss','ss.id','=','shipments_journey.shipper_status_id')
             ->leftJoin('shipment_status_reason as ssr', 'ssr.id', '=', 'shipments_journey.status_reason_id')
@@ -706,6 +647,8 @@ class ShipperDashboardController extends Controller
 //            ->where('shipments.user_id', session('user_id'))
 //            ->orwhereIn('shipments.user_id', session('sister_users'))
 //            ->groupBy('shipments.id');
+
+
 
         $shipments = $shipments->where(function ($query) {
             $query->where('shipments.user_id', session('user_id'))
@@ -725,17 +668,12 @@ class ShipperDashboardController extends Controller
             }
         }
 
-        if ($from_id && $to_id) {
-            $shipments = $shipments->where('shipments.id', '>=', $from_id)
-                ->where('shipments.id', '<=', $to_id);
+        if ($request->get('booking_from_date') && $request->get('booking_from_date')) {
+            $from = $request->get('booking_from_date');
+            $to = $request->get('booking_to_date');
+            $shipments = $shipments->whereBetween('shipments.created_at', [$from, $to]);
         }
-
-        if ($from_sj_id) {
-            $shipments = $shipments->where('shipments_journey.id', '>=', $from_sj_id);
-        }
-
         $datatable = Datatables::of($shipments)
-            ->setTotalRecords($count)
             ->editColumn('tracking_number', function ($shipments) {
                 $route = route('cod.tracking.index');
                 return "<u><a href='{$route}?tracking_number=$shipments->tracking_number' class='tracking' target='_blank'>$shipments->tracking_number</a></u>";
