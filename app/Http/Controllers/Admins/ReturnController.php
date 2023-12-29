135cc554339012ef831e19e54052c28f6ac55cb7
Mohammad <muhammad.ahmed@trax.pk>
2023-12-29 12:03:24 +0500

    test

--- app/Http/Controllers/Admins/ReturnController.php
+++ app/Http/Controllers/Admins/ReturnController.php
@@ -275,15 +275,16 @@ class ReturnController extends Controller
             $details['shipment_journey_rcp_latest'][] = isset($latestJourney->updated_at) ? $latestJourney->updated_at : '-';
         }
             
-        dd($details['shipment_journey_rcp_latest']);
-        // for ($i = 0; $i < count($details['shipment_journey_rcp_latest']); $i++) {
-        //     if($details['shipment_journey_rcp_latest'][$i] != null){
-        //         $created_at = new DateTime($details['rv_shipment_created_at'][$i]);
-        //         $updated_at = new DateTime($details['shipment_journey_rcp_latest'][$i]);
-        //         $interval = $created_at->diff($updated_at);
-        //         $averageResponseTime += $interval->s + $interval->i * 60 + $interval->h * 3600;
-        //     }
-        // }
+        if($details){
+            for ($i = 0; $i < count($details['shipment_journey_rcp_latest']); $i++) {
+                if($details['shipment_journey_rcp_latest'][$i] != null){
+                    $created_at = new DateTime($details['rv_shipment_created_at'][$i]);
+                    $updated_at = new DateTime($details['shipment_journey_rcp_latest'][$i]);
+                    $interval = $created_at->diff($updated_at);
+                    $averageResponseTime += $interval->s + $interval->i * 60 + $interval->h * 3600;
+                }
+            }
+        }
         $averageResponseTimeInSeconds = $averageResponseTime / count($details['shipment_journey_rcp_latest']);
         $averageResponseTimeInHours = $averageResponseTimeInSeconds / 3600;
 
