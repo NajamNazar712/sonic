52cdc48a30f51d12d038c1745767cec1334737e6
Mohammad <muhammad.ahmed@trax.pk>
2024-01-01 11:32:39 +0500

    NAME Changes of Seeder Booking Type

--- database/seeds/BoltUndeliveredReasonAgainstBookingTypeSeeder.php
+++ database/seeds/BoltUndeliveredReasonAgainstBookingTypeSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class BoltUndeliveredReasonAgainstBookingTypeSeeder extends Seeder
+class BoltUndeliveredReasonAgainstBookingTypesSeeder extends Seeder
 {
     /**
      * Run the database seeds.
