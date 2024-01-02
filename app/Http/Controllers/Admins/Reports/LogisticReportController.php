3053e861058aa910ed00c0de52edf503b3ea08af
Najam <muhammad.najam@trax.pk>
2024-01-02 12:56:42 +0500

    booked shipment included in logistic report

--- app/Http/Controllers/Admins/Reports/LogisticReportController.php
+++ app/Http/Controllers/Admins/Reports/LogisticReportController.php
@@ -99,9 +99,9 @@ class LogisticReportController extends Controller
         })
             ->leftJoin('shipments_journey as sj', function ($join) use ($connection, $sj_from_id) {
                 $join->on('sj.shipment_id', '=', 'shipments.id')
-                    ->where('sj.shipper_status_id', 2)
+                    ->whereIn('sj.shipper_status_id', [1,2])
                     ->where('sj.id', '=',
-                        DB::connection($connection)->raw("(select max(id) from shipments_journey where shipments_journey.shipment_id = shipments.id and shipments_journey.shipper_status_id = 2 and shipments_journey.id >= $sj_from_id)"));
+                        DB::connection($connection)->raw("(select max(id) from shipments_journey where shipments_journey.shipment_id = shipments.id and shipments_journey.shipper_status_id IN(1,2) and shipments_journey.id >= $sj_from_id)"));
             })
             ->leftJoin('shipments_journey as sjr', function ($join) use ($connection, $sj_from_id) {
                 $join->on('sjr.shipment_id', '=', 'shipments.id')
@@ -121,7 +121,7 @@ class LogisticReportController extends Controller
             })
             ->leftJoin('shipment_status_reason as ssr', 'ssr.id', '=', 'sjr.status_reason_id')
             ->select('shipments.id as shipment_id','shipments.tracking_number','shipments.order_id as order_id','shipments.tracking_number as tracking_number_link', 'shipments.consignee_name','u.name as shipper','usi.pickup_address as shipper_address','ss.name as current_status','sj.created_at as arrival_date', 'shipments.created_at as booking_date','dc.name as destination','h.name as hub', 'dr.created_at as delivered_or_returned','z.name as zone', 'dc.id as destination_city_id', 'shipments.shipper_status_id as shipment_status', 'shipments.consignee_address', 'shipments.consignee_phone_number_1', 'shipments.consignee_phone_number_2', 'si.description','si.quantity','shipments.pieces','shipments.estimated_weight', 'oc.name as origin')
-            ->whereNotIn('shipments.shipper_status_id',[1,17])
+            ->where('shipments.shipper_status_id', '!=', 17)
             ->whereIn('u.id', $special_shippers)
             ->whereBetween('sj.created_at', [$from,$to]);
    
