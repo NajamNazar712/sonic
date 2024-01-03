07791e4cc1fc7b4e01d33cc0daa037d45737d6c7
Zain <zain.hyder@trax.pk>
2024-01-03 17:40:07 +0500

    rc_flag changes

--- app/Http/Controllers/Rider/RiderAPIController.php
+++ app/Http/Controllers/Rider/RiderAPIController.php
@@ -11695,13 +11695,9 @@ class RiderAPIController extends Controller
                                         }
                                         if ($request->has('otp_entered') && $request->status_reason_id == 8) {
                                             $rider_delivery->otp_entered = $request->otp_entered;
-                                            // if ($request->otp_entered == 1) {
-                                            //     $rc_flag = true;
-                                            // }
-                                            if ($request->otp_entered != null) {
-                                                    $rc_flag = true;
-                                                }
-                                            // $rc_flag = true;
+                                            if ($request->otp_entered == 1) {
+                                                $rc_flag = true;
+                                            }
                                         }
                                         $rider_delivery->save();
 
@@ -11787,18 +11783,15 @@ class RiderAPIController extends Controller
                                         if ($rc_flag == true) {
                                             $otp_bypass = $this->otp_bypass($shipment->user_id);
                                             if ($otp_bypass) {
-                                                dd(1);
                                                 $this->auto_return_confirm($shipment->id);
                                                 ShipmentsJourneyController::add($shipment->id, 20, 20, 8, $remarks, NULL, 346, $request->delivery_note_id, NULL, 1, NULL, $rider_id, NULL, NULL, $remarks_id);
                                             } else {
-                                                dd(2);
                                                 $arr['shipment_id'] = $request->shipment_id;
                                                 $arr['delivery_note_id'] = $request->delivery_note_id;
                                                 dispatch(new ProcessAgentCallMonitoring($arr));
                                             }
                                         }
                                         if ($rc_flag == false) {
-                                                dd(3);
                                             $arr['shipment_id'] = $request->shipment_id;
                                             $arr['delivery_note_id'] = $request->delivery_note_id;
                                             dispatch(new ProcessAgentCallMonitoring($arr));
