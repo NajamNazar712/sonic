354aaf4c885b592d879d33fac64962618ec6c711
Zain <zain.hyder@trax.pk>
2024-01-05 16:28:58 +0500

    adding seeder to update shipmentstatusshipmentstatus table for rv add on statues

--- database/seeds/ShipmentStatusShipmentStatusReasonTableSeeder.php
+++ database/seeds/ShipmentStatusShipmentStatusReasonTableSeeder.php
@@ -58,21 +58,7 @@ class ShipmentStatusShipmentStatusReasonTableSeeder extends Seeder
 			array('shipment_status_id' => 15, 'shipment_status_reason_id' => 15),
 			array('shipment_status_id' => 15, 'shipment_status_reason_id' => 16),
 			array('shipment_status_id' => 15, 'shipment_status_reason_id' => 17),
-			array('shipment_status_id' => 15, 'shipment_status_reason_id' => 18),
-            array('shipment_status_id' => 8, 'shipment_status_reason_id' => 40),
-            array('shipment_status_id' => 8, 'shipment_status_reason_id' => 45),
-            array('shipment_status_id' => 12, 'shipment_status_reason_id' => 45),
-            array('shipment_status_id' => 12, 'shipment_status_reason_id' => 12),
-            array('shipment_status_id' => 12, 'shipment_status_reason_id' => 28),
-            array('shipment_status_id' => 12, 'shipment_status_reason_id' => 34),
-            array('shipment_status_id' => 12, 'shipment_status_reason_id' => 35),
-            array('shipment_status_id' => 12, 'shipment_status_reason_id' => 60),
-            array('shipment_status_id' => 12, 'shipment_status_reason_id' => 63),
-			array('shipment_status_id' => 56, 'shipment_status_reason_id' => 31),
-			array('shipment_status_id' => 56, 'shipment_status_reason_id' => 32),
-			array('shipment_status_id' => 56, 'shipment_status_reason_id' => 33),
-			array('shipment_status_id' => 56, 'shipment_status_reason_id' => 77),
-
+			array('shipment_status_id' => 15, 'shipment_status_reason_id' => 18)
         ));
     }
 }
