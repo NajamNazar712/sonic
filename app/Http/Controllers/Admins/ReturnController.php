5927b7ad22e80288ebc541a730fc3ec96649957c
Zain <zain.hyder@trax.pk>
2024-01-05 17:48:16 +0500

    debugging assign agent

--- app/Http/Controllers/Admins/ReturnController.php
+++ app/Http/Controllers/Admins/ReturnController.php
@@ -5362,7 +5362,18 @@ class ReturnController extends Controller
                 // });
 
                 // $assigned_shipment_count = count(array_chunk($assigned_shipment_count, 1));\
-                if ($already_assigned == '') {
+                // if ($already_assigned == '') {
+                if ($already_assigned <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Document</title>
+                </head>
+                <body>
+                    
+                </body>
+                </html>= '') {
                     return response()->json([
                         'status' => 1,
                         'error' => 'No Shipment Of These Tracking Numbers Are Assigned: ' . $no_zone_shipment . 
