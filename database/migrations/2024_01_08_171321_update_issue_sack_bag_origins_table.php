63dda466a3587a20e35aa072f681d1ccb48fcfc3
usamauddin <usama.uddin@trax.pk>
2024-01-08 19:46:31 +0500

    add drop column cmd in migrtion

--- database/migrations/2024_01_08_171321_update_issue_sack_bag_origins_table.php
+++ database/migrations/2024_01_08_171321_update_issue_sack_bag_origins_table.php
@@ -25,6 +25,9 @@ class UpdateIssueSackBagOriginsTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('issue_sack_bag_origins', function (Blueprint $table) {
+            $table->dropColumn('sack_destination_id');
+
+        });
     }
 }
