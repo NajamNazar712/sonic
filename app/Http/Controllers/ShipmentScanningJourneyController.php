03a73e8f8726674bdb4fdf2c7a558e608a114e17
Mohammad <muhammad.ahmed@trax.pk>
2024-01-08 16:12:07 +0500

    rider called from shipment_scanning_journey_controller

--- app/Http/Controllers/ShipmentScanningJourneyController.php
+++ app/Http/Controllers/ShipmentScanningJourneyController.php
@@ -46,16 +46,16 @@ class ShipmentScanningJourneyController extends Controller
     
     static public function shipment_scanning_area_logs($shipment_id, $auth_id, $via)
     {
-        $employee = ($via == 'app') ? Rider::find($auth_id) : Admin::find($auth_id);
+        $employee = ($via == 'rider') ? Rider::find($auth_id) : Admin::find($auth_id);
         $latestShipmentScanningId = ShipmentScanningJourney::latest('id')->first()->id;
 
         $addScanningHistoryAreaLog = new ShipmentScanningJourneyAreaLog([
             'shipment_id' => $shipment_id,
             'shipment_scanning_journey_id' => $latestShipmentScanningId,
-            'hub_id' => ($via == 'app') ? $employee->city_id : $employee->default_hub_id,
+            'hub_id' => ($via == 'rider') ? $employee->city_id : $employee->default_hub_id,
             'area_id' => optional($employee)->area_id,
-            'admin_id' => ($via == 'app') ? null : $auth_id, 
-            'rider_id' => ($via == 'app') ? $auth_id : null, 
+            'admin_id' => ($via == 'rider') ? null : $auth_id, 
+            'rider_id' => ($via == 'rider') ? $auth_id : null, 
             'location_status' => 0,
             'status' => 0,
         ]);
