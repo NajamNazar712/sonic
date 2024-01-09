8d0493f0d5df9821ab8bd66daa978b9daabffa0b
Muhammad Waqas <muhammad.waqas@trax.pk>
2024-01-09 15:32:20 +0500

    testing code revert

--- app/Console/Commands/EmailTest.php
+++ app/Console/Commands/EmailTest.php
@@ -2,10 +2,6 @@
 
 namespace App\Console\Commands;
 
-use App\Http\Controllers\NotificationsController;
-use App\Http\Models\Invoice;
-use App\Http\Models\ShipmentsJourney;
-use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
 
@@ -43,57 +39,7 @@ class EmailTest extends Command
     public function handle()
     {
 
-        $invoice = Invoice::find(4122);
-
-//        $filename = 'sonic_invoice_details_' . $->id . '.xlsx';
-
-        $details = array();
-
-        $details[] = ['S. No.', 'Tracking No.', 'Origin', 'Destination', 'Arrival Date', 'Weight (kg)', 'Weight Charges (PKR)', 'Fuel Surcharge (PKR)', 'OSA Charges (PKR)', 'Adjustment Charges (PKR)', 'Total Charges (PKR)', 'GST (PKR)', 'Invoice Amount (PKR)', 'Intercept Charges  (PKR)'];
-
-        $serial_number = 1;
-
-        foreach ($invoice->invoice_shipments as $invoice_shipment) {
-            $shipment = $invoice_shipment->shipment;
-
-            $shipment_journey = ShipmentsJourney::where('shipment_id', $shipment->id)->where('shipper_status_id', 2);
-
-            if ($shipment_journey->exists()) {
-                $date = $shipment_journey->first()->created_at;
-            } else {
-                $date = $shipment->created_at;
-            }
-
-            $date = Carbon::parse($date)->format('Y-m-d');
-
-            $row = array();
-
-            $row[] = $serial_number;
-            $row[] = $shipment->tracking_number;
-            $row[] = $shipment->pickup_address->city->name;
-            $row[] = $shipment->consignee_city->name;
-            $row[] = $shipment->created_at;
-            $row[] = $shipment->actual_weight;
-            $row[] = (($invoice_shipment->type != 2) ? $shipment->weight_charges : 0);
-            $row[] = (($invoice_shipment->type != 2) ? $shipment->fuel_surcharge : 0);
-            $row[] = (($invoice_shipment->type != 2) ? $shipment->nsa_osa_charges : 0);
-            $row[] = (($invoice_shipment->type == 2) ? $shipment->adjustment_charges : 0);
-            $row[] = $invoice_shipment->charges;
-            $row[] = $invoice_shipment->gst;
-            $row[] = $invoice_shipment->invoice_amount;
-            $row[] = $shipment->intercept_charges;
-
-            $details[] = $row;
-
-            $serial_number++;
-        }
-        print_r($details);
-
-
-
-
-
-//        Log::channel('code_test_log')->info('Noman bhai ka log in logs!');
+        Log::channel('code_test_log')->info('Noman bhai ka log in logs!');
         echo "Noman bhai ka log!";
 //        $ref = 'nothing';
 //        NotificationsController::send(219, $ref);
