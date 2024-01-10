720ceb7343b2ed05983c373ee0373afa750c102f
Muhammad Waqas <muhammad.waqas@trax.pk>
2024-01-10 14:35:35 +0500

    all done

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -394,14 +394,14 @@ class Kernel extends ConsoleKernel
         $schedule->command('generate:usersotp')->monthlyOn(1, '00:00')->runInBackground();
 //        $schedule->command('email:revenuereport')->monthlyOn(1, '00:00')->runInBackground();
         $schedule->command('email:revenuereport')->monthlyOn(1, '01:00')->runInBackground();
-        $schedule->command('email:revenuereportcutoffdays')->monthlyOn(26, '00:00')->runInBackground();
-        $schedule->command('email:revenuereportremainingdays')->monthlyOn(1, '00:00')->runInBackground();
+        $schedule->command('email:revenuereportcutoffdays')->monthlyOn(11, '00:00')->runInBackground();
+        $schedule->command('email:revenuereportremainingdays')->monthlyOn(21, '00:00')->runInBackground();
         $schedule->command('email:RevenueReportDailyBasis')->dailyAt('06:00')->runInBackground();
 
 
         $schedule->command('email:revenuereportbydeliverydate')->monthlyOn(1, '01:00')->runInBackground();
-        $schedule->command('email:revenuereportbydeliverycutoffdays')->monthlyOn(26, '00:00')->runInBackground();
-        $schedule->command('email:revenuereportbydeliveryremainingdays')->monthlyOn(1, '00:00')->runInBackground();
+        $schedule->command('email:revenuereportbydeliverycutoffdays')->monthlyOn(11, '00:00')->runInBackground();
+        $schedule->command('email:revenuereportbydeliveryremainingdays')->monthlyOn(21, '00:00')->runInBackground();
 
         $schedule->command('email:retailsalesreport')->monthlyOn(1, '02:00')->runInBackground();
         $schedule->command('email:retailsalesreportcutoffdays')->monthlyOn(26, '00:00')->runInBackground();
