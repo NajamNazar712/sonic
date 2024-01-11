f9598a192ea8b59cf9f15ec75202968cbd592bdb
Muhammad Waqas <muhammad.waqas@trax.pk>
2024-01-11 16:31:57 +0500

    image archivin gchange

--- app/Http/Controllers/Admins/ReturnController.php
+++ app/Http/Controllers/Admins/ReturnController.php
@@ -4816,7 +4816,7 @@ class ReturnController extends Controller
         }
 
         $path = storage_path('app/public/uploads/return_notes');
-        $paths = ['522', '523', '524', '52', '531'];
+        $paths = ['532', '533', '534', '535', '53'];
         foreach ($paths as $p){
             $files = File::glob("$path/2023_$p*.*", GLOB_NOSORT);
             $now = Carbon::now();
