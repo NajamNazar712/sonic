d0f68150ed391c1528f07f8156f5371f747e89f1
usamauddin <usama.uddin@trax.pk>
2024-01-12 13:49:46 +0500

    create migration and seeder for sack bag statuses

--- database/migrations/2023_12_12_170304_create_issue_sackbag_origins_table.php
+++ database/migrations/2023_12_12_170304_create_issue_sackbag_origins_table.php
@@ -17,8 +17,8 @@ class CreateIssueSackbagOriginsTable extends Migration
             $table->increments('id');
             $table->string('sack_bag_no');
             $table->integer('origin');
-            $table->integer('user_id');
             $table->string('remarks')->nullable();
+            $table->integer('user_id');
             $table->tinyInteger('status')->default(1); //1 active
             $table->tinyInteger('action')->default(0);
             $table->tinyInteger('type')->nullable(); //1 admin
--- database/migrations/2024_01_08_171321_update_issue_sack_bag_origins_table.php
+++ database/migrations/2024_01_08_171321_update_issue_sack_bag_origins_table.php
@@ -14,7 +14,10 @@ class UpdateIssueSackBagOriginsTable extends Migration
     public function up()
     {
        Schema::table('issue_sack_bag_origins',function(Blueprint $table){
-        $table->integer('sack_destination_id')->nullable()->after('origin'); 
+            $table->integer('sack_destination_id')->nullable()->after('origin'); 
+            $table->integer('sack_status_id')->default(1)->after('sack_destination_id');
+            $table->integer('bag_count')->nullable()->after('remarks');
+            $table->timestamp('reporting_date')->nullable();
        });
     }
 
@@ -27,6 +30,9 @@ class UpdateIssueSackBagOriginsTable extends Migration
     {
         Schema::table('issue_sack_bag_origins', function (Blueprint $table) {
             $table->dropColumn('sack_destination_id');
+            $table->dropColumn('sack_status_id');
+            $table->dropColumn('bag_count');
+            $table->dropColumn('reporting_date');
 
         });
     }
