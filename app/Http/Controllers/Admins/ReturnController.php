e712ce51742532d5acb7862425be6f5b402aa1e6
Muhammad Waqas <muhammad.waqas@trax.pk>
2024-01-15 13:11:27 +0500

    change return note image archiving

--- app/Http/Controllers/Admins/ReturnController.php
+++ app/Http/Controllers/Admins/ReturnController.php
@@ -4816,7 +4816,7 @@ class ReturnController extends Controller
         }
 
         $path = storage_path('app/public/uploads/return_notes');
-        $paths = ['54', '55', '56', '57'];
+        $paths = ['58', '59', '5', '61', '62', '63', '64'];
         foreach ($paths as $p){
             $files = File::glob("$path/2023_$p*.*", GLOB_NOSORT);
             $now = Carbon::now();
