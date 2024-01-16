bd3e958ecdd6749624640952aee92aa6879b99f4
Mohammad <muhammad.ahmed@trax.pk>
2024-01-16 11:30:40 +0500

    Restricted account condition added to active account

--- resources/views/admin/accounts/active_accounts_list.blade.php
+++ resources/views/admin/accounts/active_accounts_list.blade.php
@@ -2441,8 +2441,7 @@ function checkboxStatus() {
                 var tr = $(this).closest('tr');
                 var row = dataTable.row(tr);
                 var rowData = row.data();
-                var cond = (rowData.status_id != 2) ||
-               (rowData.account_type_id == 1 && rowData.status_id != 2);
+                var cond = (rowData.status_id != 2);
                
                if (index_2 === -1 && cond) {
                     selected_rows_2.push(id);
