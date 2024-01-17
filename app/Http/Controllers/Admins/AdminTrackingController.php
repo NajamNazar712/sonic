bd34f3e9b2b28e7d73c38b9bcca5c05a2e7fcf24
Najam <muhammad.najam@trax.pk>
2024-01-17 11:56:30 +0500

    dd remove

--- app/Http/Controllers/Admins/AdminTrackingController.php
+++ app/Http/Controllers/Admins/AdminTrackingController.php
@@ -955,7 +955,6 @@ class AdminTrackingController extends Controller
 
                         $details['tracking_number'] = $tracking_number;
                         $details['weight_recorded_as'] = DB::table('shipments_weight_types')->leftJoin('weight_types as wt', 'wt.id','=','shipments_weight_types.weight_type')->where('shipments_weight_types.shipment_id', $shipment->id)->pluck('wt.name')->first() ?? '';
-                        //dd($details['weight_recorded_as']);
                         if ($shipment->pod_image()->exists()) {
                             $details['pod_file'] = asset('uploads/pod_images/' . $shipment->pod_image->pod_file);
 
