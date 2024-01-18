677f8826434ff7db76b0c6f5621faccd15c83ea1
usamauddin <usama.uddin@trax.pk>
2024-01-18 17:30:31 +0500

    create general setting controller and model and table general setting

---
+++ app/Http/Controllers/Admins/Settings/GeneralSettingController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\admins\settings;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class GeneralSettingController extends Controller
+{
+    //
+}
---
+++ app/Http/Models/Admin/Settings/GeneralSetting.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\http\models\admin\settings;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GeneralSetting extends Model
+{
+    //
+}
