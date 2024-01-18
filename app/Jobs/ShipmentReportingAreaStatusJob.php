f694dad5b76167d01860416fcd4332ad1adc37b0
Mohammad <muhammad.ahmed@trax.pk>
2024-01-18 15:36:17 +0500

    Minor Condition change to ShipmentReportingAreaStatusJob and revert

--- app/Jobs/ShipmentReportingAreaStatusJob.php
+++ app/Jobs/ShipmentReportingAreaStatusJob.php
@@ -2,9 +2,8 @@
 
 namespace App\Jobs;
 
-use App\Http\Models\City;
-use App\Http\Models\ReportingLocation;
 use Illuminate\Bus\Queueable;
+use App\Http\Models\ReportingLocation;
 use Illuminate\Queue\SerializesModels;
 use App\ShipmentScanningJourneyAreaLog;
 use Illuminate\Queue\InteractsWithQueue;
@@ -38,46 +37,42 @@ class ShipmentReportingAreaStatusJob implements ShouldQueue
     {
         $latest_shipment_scanning_id = $this->latest_shipment_scanning_id;
         $shipmentScanning = ShipmentScanningJourney::find($latest_shipment_scanning_id);
-        
+        $areaLogId = ShipmentScanningJourneyAreaLog::latest('id')->first()->id;
+        $areaLog = ShipmentScanningJourneyAreaLog::find($areaLogId);
+
         if (!$shipmentScanning) {
             return;
         }
-        
-        $areaLogId = ShipmentScanningJourneyAreaLog::latest('id')->first()->id;
-        $areaLog = ShipmentScanningJourneyAreaLog::find($areaLogId);
-        
-        $cityArea = $areaLog->city_area->reporting_location ?? ReportingLocation::where('city_id',$areaLog->hub_id)->first() ?? null;
-        
-        if ($shipmentScanning && $cityArea) {
-            $latitude = $cityArea->lat ?? $cityArea->location_latitude ?? null;
-            $longitude = $cityArea->long ?? $cityArea->location_longitude ?? null;
-        
-            if (isset($shipmentScanning->latitude, $shipmentScanning->longitude, $latitude, $longitude)) {
+
+        $cityArea = $areaLog->city_area->reporting_location ?? ReportingLocation::where('city_id',$areaLog->hub_id)->first() ?? null;;
+
+        if (isset($shipmentScanning, $cityArea)) {
+            if (isset($shipmentScanning->latitude, $shipmentScanning->longitude, $cityArea->lat, $cityArea->long)) {
                 $journeyLatitude = deg2rad($shipmentScanning->latitude);
                 $journeyLongitude = deg2rad($shipmentScanning->longitude);
-                $cityLatitude = deg2rad($latitude);
-                $cityLongitude = deg2rad($longitude);
-        
+                $cityLatitude = deg2rad($cityArea->lat);
+                $cityLongitude = deg2rad($cityArea->long);
+
                 $earthRadius = 6371;
                 $longitudeDelta = $journeyLongitude - $cityLongitude;
-        
+
                 $distance = round($earthRadius * acos(
                     sin($cityLatitude) * sin($journeyLatitude) +
                     cos($cityLatitude) * cos($journeyLatitude) * cos($longitudeDelta)
                 ), 2);
-        
+
                 $distance = $distance * 1000;  // To Metre
-        
+
                 $areaLog->location_status = ($distance <= $cityArea->radius) ? 1 : 0;
+
             } else {
                 $areaLog->location_status = 0;
             }
         } else {
             $areaLog->location_status = 0;
         }
-        
+
         $areaLog->status = 1;
         $areaLog->save();
-        
     }
 }
