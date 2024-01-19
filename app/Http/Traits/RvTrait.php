ba0bffaf6ba062a49a1f7cd52a28c3689db811a2
Zain <zain.hyder@trax.pk>
2024-01-19 19:37:07 +0500

    tetsing unresposinve after 2 count

--- app/Http/Traits/RvTrait.php
+++ app/Http/Traits/RvTrait.php
@@ -443,8 +443,8 @@ trait RvTrait
     // Description:
     protected function return_confirm($request)
     {
-        dd($request->all());
         $remarks = (isset($request['remarks']) && $request['remarks'] !== null) ? $request['remarks'] : null;
+        dd($remarks);
         $parcel = Shipment::find($request->shipment_id);
         // $rv_sub_status = RvAssignAgentSubStatus::where('id', $request->rv_assign_agent_sub_status_id)->value('name');
         $rv_sub_status = RvAssignAgentSubStatus::where('id', $request->rv_assign_agent_sub_status_id)->value('name') ?? null;
