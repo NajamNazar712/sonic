e4cd04169895ca5954b9c55db8b8f1eed2c437ea
usamauddin <usama.uddin@trax.pk>
2024-01-23 13:41:31 +0500

    heading size small in ticker

--- resources/views/admin/settings/ticker.blade.php
+++ resources/views/admin/settings/ticker.blade.php
@@ -21,7 +21,7 @@
 								{{-- <div class="col-12 col-sm-12 col-md-12 col-lg-12">  value="{{ $admin_ticker->start_date ?? ''}}"--}}
 									<form id="settings_form" class="form-horizontal text-center" method="POST" action="{{ route('admin.settings.ticker.store') }}" novalidate="novalidate">
 										{{ csrf_field() }}
-										<h2>Admin Ticker</h2>
+										<h3>Admin Ticker</h3>
 										<div class="row">
 											<div class="col-md-12">
 												<div class="form-group">
@@ -67,8 +67,8 @@
 												</div>
 											</div>
 										</div>
-										<br>
-										<h2>Shipper Ticker</h2>
+										<br>		
+										<h3>Shipper Ticker</h3>
 										<div class="row">
 											<div class="col-md-12">
 												<div class="form-group">													
