a82ac60361470aab3c28e61c600074b868b42119
Zain <zain.hyder@trax.pk>
2024-01-25 14:13:40 +0500

    rv detail bug fixed

--- app/Http/Controllers/Agent/ReturnV2Controller.php
+++ app/Http/Controllers/Agent/ReturnV2Controller.php
@@ -347,7 +347,7 @@ class ReturnV2Controller extends Controller
                                     } 
                                     else {
                                         // $shipment_assign_agents = RvShipmentAssignAgent::where('shipment_id', $request->shipment_id)->where('rv_state_id', 2)->latest()->first();
-                                        $shipment_assign_agents = RvShipmentAssignAgent::where('shipment_id', $request->shipment_id)->latest()->first();
+                                        $shipment_assign_agent = RvShipmentAssignAgent::where('shipment_id', $request->shipment_id)->latest()->first();
                                         // //adding logs in rv_shipment_assign_agent_details table
                                         $rv_shipment_assign_agent_details = $this->rv_shipment_assign_agent_details($request, $shipment_assign_agent, $shipments_journey);
                                         if($rv_shipment_assign_agent_details != true){
