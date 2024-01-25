63bf1cf0c07c8683980deeb7c17f9fd1987915c7
Abdul Ghani CHishti <ghani.chisti@trax.pk>
2024-01-25 17:52:35 +0500

    working on manifest without restriction , just set a pending_bag_condition for misroute

--- app/Http/Controllers/Admins/AdminCargoManifestController.php
+++ app/Http/Controllers/Admins/AdminCargoManifestController.php
@@ -416,8 +416,6 @@ class AdminCargoManifestController extends Controller
             ->leftjoin('star_shippers as sts', 'sts.user_id', '=', 'u.id')
             ->select('z.name as zone_name','shipments.shipper_status_id', 'shipments.tracking_number', 'shipments.tracking_number as tracking', 'shipments.order_id', 'bt.booking_type as service_type', 'ss.name as status', 'oc.name as origin', 'dc.name as destination', 'u.name as shipper', 'shipments.amount', 'sm.mode as shipping_mode', 'shipments.created_at as booked_at', 'shipments_journey.created_at as arrival_at', 'shipments.booking_type_id', 'usi.poc', 'csj.created_at as current_status', 'olddc.name as old_destination', 'olddci.name as old_destination_intercept', 'crm.id as complaint', 'shipments.return_address_id', 'rc.name as return_city_name', 'ohc.name as origin_hub', 'dhc.name as destination_hub', 'olddhci.name as old_destination_intercept_hub', 'olddhc.name as old_destination_hub', 'shipments.consignee_address', 'dc.id as destination_city_id', 'sts.status as star_status')
             ->whereNotIn('shipments.id', $on_hold_shipments);
-            //->whereIn('shipments.shipper_status_id', [11,66]);
-            //dd($shipments->where('tracking_number',2021741725334)->get());
 
         if (session('role_id') != 1) {
             $shipments = $shipments->where(function ($query) {
@@ -435,11 +433,11 @@ class AdminCargoManifestController extends Controller
                     })
                     ->orWhere(function ($sub_query) {
                         $sub_query->where('shipments.shipper_status_id', 11)
-                            ->whereIn('olddc.hub_id', session('hubs'));
+                            ->whereIn('csj.city_id', session('hubs'));
                     })
                     ->orWhere(function ($sub_query) {
                         $sub_query->where('shipments.shipper_status_id', 66)
-                            ->whereIn('olddc.hub_id', session('hubs'));
+                            ->whereIn('csj.city_id', session('hubs'));
                     })
                     ->orWhere(function ($sub_query) {
                         $sub_query->where('shipments.shipper_status_id', 55)
