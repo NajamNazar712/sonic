7c9c01b406498d6013526021de482878c80a36f3
Zain <zain.hyder@trax.pk>
2024-01-25 17:37:35 +0500

    card update

--- resources/views/admin/rv_assign_shipments/index.blade.php
+++ resources/views/admin/rv_assign_shipments/index.blade.php
@@ -60,7 +60,7 @@
                                                             </div>
                                                             <div class="media-body text-white text-right">
                                                                 <h3 class="text-white">
-                                                                    <p id="received_leads" class="d-inline">{{ count($online_agents)}} /</p>
+                                                                    <p id="received_leads" class="d-inline">{{ count($online_agents)}}</p>
                                                                     <p id="received_leads" class="d-inline">({{ count($number_of_available_agents) }})</p>
                                                                 </h3>
                                                                 <span>Online/ Available Agents(out of) </span>
