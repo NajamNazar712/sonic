8178838db85bf392dc2e401a9563ed229fd2f6c3
Zain <zain.hyder@trax.pk>
2024-01-31 19:14:26 +0500

    update dn.pending_status in progress

--- app/Http/Controllers/Agent/ReturnV2Controller.php
+++ app/Http/Controllers/Agent/ReturnV2Controller.php
@@ -230,12 +230,8 @@ class ReturnV2Controller extends Controller
                                 $shipment_status = DeliveryNoteShipment::join('delivery_notes as dn', 'delivery_note_shipments.delivery_note_id', 'dn.id')
                                 ->where('delivery_note_shipments.shipment_id', '=', DB::raw('(select max(id) from delivery_note_shipments where delivery_note_shipments.shipment_id = ' . $shipment->id . ')'))
                                 ->where('dn.pending_status', 1)
-                                ->latest()->take(1)->first();
-                                $shipment_status1 = DeliveryNoteShipment::join('delivery_notes as dn', 'delivery_note_shipments.delivery_note_id', 'dn.id')
-                                ->where('delivery_note_shipments.shipment_id', '=', DB::raw('(select max(id) from delivery_note_shipments where delivery_note_shipments.shipment_id = ' . $shipment->id . ')'))
-                                ->where('dn.pending_status', 1)
                                 ->latest()->first();
-                                dd(1,$shipment_status,$shipment_status1);
+                                dd(1,$shipment_status);
         
                                 
                                 //agar pending status 1 mila to return confirm dekhae aur refusal on call na dekhae
