2aeade0d8ddcf4d3243d03a7e5546b39d09cc92e
Zain <zain.hyder@trax.pk>
2024-02-01 16:55:03 +0500

    updating agentsarnotification for resfual call shipments email debugging

--- app/Console/Commands/AgentSarNotification.php
+++ app/Console/Commands/AgentSarNotification.php
@@ -70,7 +70,7 @@ class AgentSarNotification extends Command
 
             //Combine the results for sending in single email
             $sendEmail = $sendEmails->union($sendEmailofRefusalShipments)->get();
-            dd($sendEmails);
+            dd($sendEmail);
 
             // If there are shipments that meet the conditions, send Email Notification to shipper for each shipment
 
