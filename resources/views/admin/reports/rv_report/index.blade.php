2bb815176ca3ce9d106411b70a81c963e2751f87
Zain <zain.hyder@trax.pk>
2024-02-04 18:06:37 +0500

    updated excel column name from rv status to rv action

--- resources/views/admin/reports/rv_report/index.blade.php
+++ resources/views/admin/reports/rv_report/index.blade.php
@@ -458,7 +458,7 @@
                             head.push('Action Date');
                             head.push('Action Updated By');
                             head.push('RCP Agent Updated By');
-                            head.push('RV Status');
+                            head.push('RV Action');
                             head.push('RV Status Date');
                             head.push('Current Status');
                             head.push('Current Status Date');
