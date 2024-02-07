00feae75b30958d3c79faab4f8bae61a7ca9645c
Zain <zain.hyder@trax.pk>
2024-02-07 14:15:50 +0500

    unreponsive count bug fixed

--- app/Http/Controllers/Admins/AdminReportsController.php
+++ app/Http/Controllers/Admins/AdminReportsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admins;
 use App\Http\Models\HR\Employee;
+use App\Http\Models\RvShipmentAssignAgentDetails;
 use Carbon\Carbon;
 use App\DailyVisit;
 use PHPExcel_Style_Fill;
@@ -13551,7 +13552,7 @@ class AdminReportsController extends Controller
             $reattempted = count(ShipmentsJourney::where('shipper_status_id', 13)->whereBetween('updated_at' ,[$from, $to])->get());
             $returned = count(ShipmentsJourney::where('shipper_status_id', 20)->whereBetween('updated_at' ,[$from, $to])->get());
             $on_hold = count(ShipmentsJourney::where('shipper_status_id', 9)->whereBetween('updated_at' ,[$from, $to])->get());
-            $unresponsive = count(RvShipmentAssignAgent::leftJoin('shipments as sh','rv_shipment_assign_agents.shipment_id','sh.id')->whereIn('sh.shipper_status_id', [12,65,66])->where('rv_assign_agent_status_id', 6)->where('unresponsive_count','>',0)->whereBetween('rv_shipment_assign_agents.updated_at' ,[$from, $to])->get());
+            $unresponsive = count(RvShipmentAssignAgentDetails::join('rv_shipment_assign_agents as rsaa','rsaa.id','rv_shipment_assign_agent_details.rv_shipment_assign_agent_id')->leftJoin('shipments as sh','rv_shipment_assign_agent_details.shipment_id','sh.id')->whereIn('sh.shipper_status_id', [12,65,66])->where('rv_shipment_assign_agent_details.rv_assign_agent_status_id', 6)->where('rsaa.unresponsive_count','>',0)->whereBetween('rv_shipment_assign_agent_details.updated_at' ,[$from, $to])->get());
             return response()->json(['unresponsive' => $unresponsive, 'shipper_advised_requested' => $shipper_advised_requested, 'reason_validation_required' => $reason_validation_required,
             'reattempted' => $reattempted, 'intercepted' => $intercepted,'returned' => $returned, 'on_hold' => $on_hold ]);
         }
