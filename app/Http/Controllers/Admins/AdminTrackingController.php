7d5089ffb2d02cb9bb5ea003736df6dc8c8ddcb2
Mohammad <muhammad.ahmed@trax.pk>
2024-02-08 18:14:41 +0500

    admin condition added to scanning tracking

--- app/Http/Controllers/Admins/AdminTrackingController.php
+++ app/Http/Controllers/Admins/AdminTrackingController.php
@@ -1432,39 +1432,44 @@ class AdminTrackingController extends Controller
                             ->join('shipment_scanning_journey_area_logs as ssjal', 'ssjal.shipment_scanning_journey_id', '=', 'shipment_scanning_journeys.id')
                             ->whereNotIn('shipment_scanning_journeys.id', $processed_scanning_ids);    
                         
-                        switch ($journey->shipper_status_id) {
-                            case 2:
-                                $scanning_data = $shipment_scanning_query->where('screen_location_id', 1)->latest()->first();
-                                $processed_scanning_ids[] = $scanning_data['id'];
-                                break;
-                            case 3:
-                                $scanning_data = $shipment_scanning_query->where('screen_location_id', 2)->latest()->first();
-                                $processed_scanning_ids[] = $scanning_data['id'];
-                                break;
-                            case 4:
-                                $scanning_data = $shipment_scanning_query->whereIn('screen_location_id', [20, 21])->latest()->first();
-                                $processed_scanning_ids[] = $scanning_data['id'];
-                                break;
-                            case 5:
-                                $scanning_data = $shipment_scanning_query->where('screen_location_id', 4)->latest()->first();
-                                $processed_scanning_ids[] = $scanning_data['id'];
-                                break;
-                            case 11:
-                                $scanning_data = $shipment_scanning_query->whereIn('screen_location_id', [3, 10, 20, 21])->latest()->first();
-                                $processed_scanning_ids[] = $scanning_data['id'];
-                                break;
-                            case 23:
-                                $scanning_data = $shipment_scanning_query->where('screen_location_id', 7)->latest()->first();
-                                $processed_scanning_ids[] = $scanning_data['id'];
-                                break;
-                            case 53:
-                                $scanning_data = $shipment_scanning_query->where('screen_location_id', 31)->latest()->first();
-                                $processed_scanning_ids[] = $scanning_data['id'];
-                                break;
-                            default:
+                            if($journey->admin['role_id'] != 1){
+                                switch ($journey->shipper_status_id) {
+                                    case 2:
+                                        $scanning_data = $shipment_scanning_query->where('screen_location_id', 1)->latest()->first();
+                                        $processed_scanning_ids[] = $scanning_data['id'];
+                                        break;
+                                    case 3:
+                                        $scanning_data = $shipment_scanning_query->where('screen_location_id', 2)->latest()->first();
+                                        $processed_scanning_ids[] = $scanning_data['id'];
+                                        break;
+                                    case 4:
+                                        $scanning_data = $shipment_scanning_query->whereIn('screen_location_id', [20, 21])->latest()->first();
+                                        $processed_scanning_ids[] = $scanning_data['id'];
+                                        break;
+                                    case 5:
+                                        $scanning_data = $shipment_scanning_query->where('screen_location_id', 4)->latest()->first();
+                                        $processed_scanning_ids[] = $scanning_data['id'];
+                                        break;
+                                    case 11:
+                                        $scanning_data = $shipment_scanning_query->whereIn('screen_location_id', [3, 10, 20, 21])->latest()->first();
+                                        $processed_scanning_ids[] = $scanning_data['id'];
+                                        break;
+                                    case 23:
+                                        $scanning_data = $shipment_scanning_query->where('screen_location_id', 7)->latest()->first();
+                                        $processed_scanning_ids[] = $scanning_data['id'];
+                                        break;
+                                    case 53:
+                                        $scanning_data = $shipment_scanning_query->where('screen_location_id', 31)->latest()->first();
+                                        $processed_scanning_ids[] = $scanning_data['id'];
+                                        break;
+                                    default:
+                                        $scanning_data = null;
+                                        break;
+                                }
+                            }else{
                                 $scanning_data = null;
-                                break;
-                        }
+                            }
+                        
 
                             $journey_details['area_log'] = $this->setJourneyDetails($scanning_data);
                             $journey_details['status_reason'] = ($journey->status_reason_id) ? $journey->shipment_status_reason->name : NULL;
