c7921fbbf3088fd7ba076755745a7c1f9b1185a2
Zain <zain.hyder@trax.pk>
2024-02-09 18:44:23 +0500

    debuggging

--- app/Http/Controllers/Admins/AdminReportsController.php
+++ app/Http/Controllers/Admins/AdminReportsController.php
@@ -13543,14 +13543,14 @@ class AdminReportsController extends Controller
             
             $intercepted = count(RvShipmentAssignAgentDetails::whereIn('rv_assign_agent_status_id', [3,4])->whereBetween('updated_at' ,[$from, $to])->groupBy('shipment_id')->get());
             $shipper_advised_requested = count(RvShipmentAssignAgentDetails::where('rv_assign_agent_status_id', 7)->whereBetween('updated_at' ,[$from, $to])->groupBy('shipment_id')->get());
-            $reason_validation_required = Shipment::join('shipments_journey as sj', function ($join) {
+            $reason_validation_required = count(Shipment::join('shipments_journey as sj', function ($join) {
                 $join->on('sj.shipment_id', '=', 'shipments.id')
                      ->where('sj.id', '=', DB::raw('(select max(id) from shipments_journey where shipments_journey.shipment_id = shipments.id)'));
             })
             ->where('shipments.shipper_status_id', 12)
             ->where('sj.verification', 0)
             ->whereBetween('shipments.updated_at', [$from, $to])
-            ->get();
+            ->get());
             // $reason_validation_required = count(Shipment::join('shipments_journey as sj', 'shipment.id', '',where('shipper_status_id', 12)->where('verification', 0)->whereBetween('updated_at' ,[$from, $to])->get());
             $reattempted = count(RvShipmentAssignAgentDetails::where('rv_assign_agent_status_id', 2)->whereBetween('updated_at' ,[$from, $to])->get());
             $returned = count(RvShipmentAssignAgentDetails::where('rv_assign_agent_status_id', 1)->whereBetween('updated_at' ,[$from, $to])->get());
