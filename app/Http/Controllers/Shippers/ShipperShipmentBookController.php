2047fe70d578f9fa1412acad077d8d2d95a47400
Anas <anas.mazhar@trax.pk>
2024-02-12 06:55:46 -0800

    Consigne and shipper area save add the seprate function

--- app/Http/Controllers/Shippers/ShipperShipmentBookController.php
+++ app/Http/Controllers/Shippers/ShipperShipmentBookController.php
@@ -231,10 +231,6 @@ class ShipperShipmentBookController extends Controller
             $shipment_coordinates->save();
         }
 
-        $user_shipping_info = UserShippingInfo::find($pickup_address_id);
-        self::consignee_address_area($shipment_id,$pickup_address_id,$consignee_city_id,$consignee_address);
-        self::shipper_address_area($user_shipping_info->city_id,$user_shipping_info->pickup_address,$user_shipping_info->id);
-
         //Existing Coordinates
 //        if($pieces > 1){
 //            $user = User::where('id', $user_id)->where('multipiece_status', 0);
@@ -245,6 +241,7 @@ class ShipperShipmentBookController extends Controller
 //            }
 //        }
 
+        self::addressAreaConsigneeShipper($shipment_id,$pickup_address_id,$consignee_city_id,$consignee_address);
         return $shipment_id;
     }
 
@@ -3769,9 +3766,7 @@ class ShipperShipmentBookController extends Controller
 //            }
 //        }
 
-        $user_shipping_info = UserShippingInfo::find($pickup_address_id);
-        self::consignee_address_area($shipment_id,$pickup_address_id,$consignee_city_id,$consignee_address);
-        self::shipper_address_area($user_shipping_info->city_id,$user_shipping_info->pickup_address,$user_shipping_info->id);
+        self::addressAreaConsigneeShipper($shipment_id,$pickup_address_id,$consignee_city_id,$consignee_address);
 
         return $shipment_id;
     }
@@ -7927,4 +7922,10 @@ class ShipperShipmentBookController extends Controller
 
     }
 
+    static function addressAreaConsigneeShipper($shipment_id,$pickup_address_id,$consignee_city_id,$consignee_address)
+    {
+        $user_shipping_info = UserShippingInfo::find($pickup_address_id);
+        self::consignee_address_area($shipment_id,$pickup_address_id,$consignee_city_id,$consignee_address);
+        self::shipper_address_area($user_shipping_info->city_id,$user_shipping_info->pickup_address,$user_shipping_info->id);
+    }
 }
