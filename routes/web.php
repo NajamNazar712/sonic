437c156a7daaca5f05b1b030ee4b9b66bd0468e7
Najam <muhammad.najam@trax.pk>
2024-02-13 11:56:45 +0500

    function added for mms excel booking and new template added in public/file

--- routes/web.php
+++ routes/web.php
@@ -127,7 +127,7 @@ Route::prefix('cod')->name('cod.')->group(function () {
                 Route::post('', 'Shippers\ShipperShipmentBookController@corporate_excel_store')->name('store');
 
                 Route::get('/mms', 'Shippers\ShipperShipmentBookController@corporate_excel_mms_index')->name('mms');
-                Route::post('/mms/store', 'Shippers\ShipperShipmentBookController@corporate_excel_mms_store')->name('mms.store');
+                Route::post('/mms', 'Shippers\ShipperShipmentBookController@corporate_excel_mms_store')->name('mms.store');
 
                 Route::get('/index', 'Shippers\ShipperShipmentBookController@corporate_excel_distribution_index')->name('distribution');
                 Route::post('/store', 'Shippers\ShipperShipmentBookController@corporate_excel_distribution_store')->name('distribution.store');
