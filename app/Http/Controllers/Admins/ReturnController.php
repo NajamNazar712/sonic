1ca0c7ea2f836106e2aa4a9c4dfefc933a8d6293
Zain <zain.hyder@trax.pk>
2024-02-15 14:54:39 +0500

    update error message

--- app/Http/Controllers/Admins/ReturnController.php
+++ app/Http/Controllers/Admins/ReturnController.php
@@ -5469,7 +5469,7 @@ class ReturnController extends Controller
                                 'error' => ($assigned_to_new_user != null)
                                     ? 'These Shipments are assigned to this agent successfully: ' . $assigned_shipment . 
                                     (($already_assigned_shipment != null) ? ' X No Shipment Of These Tracking Numbers Are Assigned ' . $already_assigned_shipment : '')
-                                    : 'Already Assigned' 
+                                    : 'Shipment Already Assigned' 
                             ]);
                         } 
                     }
