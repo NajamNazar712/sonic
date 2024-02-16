a8d2f9d25357ab08f053374232655a4e01766213
Mohammad <muhammad.ahmed@trax.pk>
2024-02-16 16:44:41 +0500

    Handover And Scanning Journey Location Fixed on shipper side.

--- app/Http/Controllers/AdminShipmentHandoverController.php
+++ app/Http/Controllers/AdminShipmentHandoverController.php
@@ -332,7 +332,7 @@ class AdminShipmentHandoverController extends Controller
         'hr.admin_id as from_admin_id','hor.admin_id as to_admin_id','c.name as hub',
         'handovers.shipments as shipment_count','handovers.shipments as total_shipments','hs.name as status',
         'handovers.received as received_shipments','hr.name as from_name','hor.name as to_name',
-        'handovers.from_dept_area_desg','handovers.to_dept_area_desg','handovers.received_at','handovers.created_at',
+        'handovers.from_dept_area_desg as from_dept_area_desg','handovers.to_dept_area_desg as to_dept_area_desg','handovers.received_at','handovers.created_at',
         DB::raw('(select shipments - received_shipments from handovers where handovers.id= handover_id ) as remaining'),
         DB::raw('SUM(s.pieces) as shipment_pieces'),'c_from.name as from_area','c_to.name as to_area','journey.shipper_status_id as shipper_status_id',
         'sjl.shipment_id as journey_latest_id',
@@ -361,6 +361,32 @@ class AdminShipmentHandoverController extends Controller
                   return $handover_list->from_name;
               }
             })
+
+            ->editColumn('from_dept_area_desg', function($handover_list){
+              if (isset($handover_list->from_admin_id)) {
+                 $admin = Admin::where('id', $handover_list->from_admin_id)->first();
+                 if(isset($admin->Edesignation) && isset($admin->Edesignation->department)){
+                  return $admin->Edesignation->department->name;
+                 }else{
+                  return '-';
+                 }
+              }else {
+                  return '-';
+              }            
+          })
+
+          ->editColumn('to_dept_area_desg', function($handover_list){
+            if (isset($handover_list->to_admin_id)) {
+               $admin = Admin::where('id', $handover_list->to_admin_id)->first();
+               if(isset($admin->Edesignation) && isset($admin->Edesignation->department)){
+                return $admin->Edesignation->department->name;
+               }else{
+                return '-';
+               }
+            }else {
+              return '-';
+            }            
+        })
 
             ->editColumn('to', function($handover_list) {
               if (isset($handover_list->to_admin_id)) {
