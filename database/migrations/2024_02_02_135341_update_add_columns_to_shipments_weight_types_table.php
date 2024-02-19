03fd1aba6cfaf77196138ad5980cedf41815daf7
Najam <muhammad.najam@trax.pk>
2024-02-19 13:30:32 +0500

    migration file name and class name changed and condition also added in new migration file to only add columns if not exist already

---
+++ database/migrations/2024_02_02_135341_update_add_columns_to_shipments_weight_types_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateAddColumnsToShipmentsWeightTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if(!Schema::hasColumn('shipments_weight_types', 'shipper_range_weight_charges')) {
+            Schema::table('shipments_weight_types', function (Blueprint $table) {
+                $table->decimal('shipper_range_weight_charges', 8, 2)->nullable()->default(NULL)->after('shipper_weight_charges');
+                $table->decimal('arrival_range_weight_charges', 8, 2)->nullable()->default(NULL)->after('shipper_weight_charges');
+            });
+        }
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Schema::table('shipments_weight_types', function (Blueprint $table) {
+        //     $table->dropColumn('shipper_range_weight_charges');
+        //     $table->dropColumn('arrival_range_weight_charges');
+        // });
+    }
+}
