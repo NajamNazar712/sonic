a3b059aa592d345a903086c1c5ccd04927cbf2ff
Zain <zain.hyder@trax.pk>
2024-02-27 20:03:10 +0500

    debugging

--- app/Http/Controllers/Admins/AdminReportsController.php
+++ app/Http/Controllers/Admins/AdminReportsController.php
@@ -12353,8 +12353,7 @@ class AdminReportsController extends Controller
         'rv_fakes.name as fake_status', 's_status.name as current_status', 'shipments.updated_at as current_status_date', 
         'rv_shipment_assign_agents.unresponsive_count as call_count','rv_status.name as rv_status_name','sj.updated_at as rv_status_date','rv_shipment_assign_agent_details.rv_state_id as rv_state_id', 'add.id as agent_id')
         ->where('rv_shipment_assign_agent_details.rv_state_id', '!=', 1)
-        ->groupBy('rv_shipment_assign_agent_details.shipment_id')
-        ->groupBy('rv_shipment_assign_agent_details.created_at');
+        ->groupBy('rv_shipment_assign_agent_details.shipment_id', 'rv_shipment_assign_agent_details.created_at');
             
         $datatable = Datatables::of($rv_report)
                     ->editColumn('tracking_number', function($rv_report) {
