c3a8c995e0a6f2c76b0a3cea12f38f4b0c8b41e5
Zain <zain.hyder@trax.pk>
2024-02-27 16:40:56 +0500

    debugging

--- app/Http/Traits/RvTrait.php
+++ app/Http/Traits/RvTrait.php
@@ -353,6 +353,8 @@ trait RvTrait
             $reattempt_count = BoltUndeliveredReasonMapCount::where('shipment_id', $request->shipment_id)->where('count', 3)->latest()->first();
             $reattempt_requested_shipment = Shipment::where('id', $request->shipment_id)->where('shipper_status_id', 52)->latest()->first();
 
+            dd($request->rv_assign_agent_status_id, $shipment_assign_agent->unresponsive_count);
+
             //if shipment delivery count is 3 and again status is updated to unresponsive set the shipment to return confirm
             if ($request->rv_assign_agent_status_id == 6 && $shipment_assign_agent->unresponsive_count > 0 && $reattempt_count) {
                 $shipment_assign_agent_table_columns['rv_assign_agent_status_id'] = 1; //set status to return confirm
@@ -371,7 +373,6 @@ trait RvTrait
             } 
             
             else if ($request->rv_assign_agent_status_id == 6 && $shipment_assign_agent->unresponsive_count == 2) {
-                dd(123);
                 $shipment_assign_agent_table_columns['rv_assign_agent_status_id'] = 7; //set status to Shipper Advise Requested 
                 $shipment_assign_agent_table_columns['rv_state_id'] = 2; //unassign shipment
             } 
