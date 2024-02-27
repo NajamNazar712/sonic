18820821ca135fbe420e6483f845e1830f71b4a1
Zain <zain.hyder@trax.pk>
2024-02-27 17:22:36 +0500

    debugging

--- app/Http/Controllers/Admins/AdminReportsController.php
+++ app/Http/Controllers/Admins/AdminReportsController.php
@@ -12487,7 +12487,7 @@ class AdminReportsController extends Controller
         if ($request->get('search_date_from') && $request->get('search_date_to')) {
             $from = $request->get('search_date_from');
             $to = $request->get('search_date_to');
-            $rv_report->whereBetween('action_date', [$from, $to]);
+            $rv_report->whereBetween('rv_shipment_assign_agent_details.created_at', [$from, $to]);
         }   
 
         return $datatable->make(true);
