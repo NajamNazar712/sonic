fd02afeb4303c7517aabcd97323ae36a6ac07ee9
Zain <zain.hyder@trax.pk>
2024-02-27 16:24:16 +0500

    debugging

--- app/Http/Traits/RvTrait.php
+++ app/Http/Traits/RvTrait.php
@@ -350,6 +350,36 @@ trait RvTrait
 
             //this is updating status of rvshipment assign agent row 
             $shipment_assign_agent_table_columns = $this->shipment_assign_agent_table_columns($request, $add_agent);
+            $reattempt_count = BoltUndeliveredReasonMapCount::where('shipment_id', $request->shipment_id)->where('count', 3)->latest()->first();
+            $reattempt_requested_shipment = Shipment::where('id', $request->shipment_id)->where('shipper_status_id', 52)->latest()->first();
+
+            //if shipment delivery count is 3 and again status is updated to unresponsive set the shipment to return confirm
+            if ($request->rv_assign_agent_status_id == 6 && $shipment_assign_agent->unresponsive_count > 0 && $reattempt_count) {
+                $shipment_assign_agent_table_columns['rv_assign_agent_status_id'] = 1; //set status to return confirm
+                $shipment_assign_agent_table_columns['rv_assign_agent_sub_status_id'] = null;
+                $shipment_assign_agent_table_columns['rv_state_id'] = 4; //set status as shipment completed
+            } 
+            // if current status of shipment is 52 (shipment reattempt requested) and agent has updated the status to unresponsive set the status to return confirm
+            else if ($request->rv_assign_agent_status_id == 6 && $shipment_assign_agent->unresponsive_count > 0 && $reattempt_requested_shipment) {
+                $shipment_assign_agent_table_columns['rv_assign_agent_status_id'] = 1; //set status to return confirm
+                $shipment_assign_agent_table_columns['rv_assign_agent_sub_status_id'] = null;
+                $shipment_assign_agent_table_columns['rv_state_id'] = 4; //set status as shipment completed
+            } 
+            
+            else if ($request->rv_assign_agent_status_id == 6 && $shipment_assign_agent->unresponsive_count < 2) {
+                $shipment_assign_agent_table_columns['rv_state_id'] = 2; //unassign shipment
+            } 
+            
+            else if ($request->rv_assign_agent_status_id == 6 && $shipment_assign_agent->unresponsive_count == 2) {
+                $shipment_assign_agent_table_columns['rv_assign_agent_status_id'] = 7; //set status to Shipper Advise Requested 
+                $shipment_assign_agent_table_columns['rv_state_id'] = 2; //unassign shipment
+            } 
+            
+            else if ($request->rv_assign_agent_status_id == 6 && $shipment_assign_agent->unresponsive_count == 3) {
+                $shipment_assign_agent_table_columns['rv_assign_agent_status_id'] = 1; //set status to return confirm
+                $shipment_assign_agent_table_columns['rv_assign_agent_sub_status_id'] = null;
+                $shipment_assign_agent_table_columns['rv_state_id'] = 4; //set status as shipment completed
+            }
 
             $shipment_assign_agent_table_columns['updated_type_id'] = 2; // agent type
             $shipment_assign_agent_table_columns['rv_state_id'] = 2;
