579a352c4d9fa7c6f5a3452436476197cec2771d
Anas <anas.mazhar@trax.pk>
2024-02-29 14:51:27 +0500

    Logs add

--- app/Http/Traits/LastMileAppReportTrait.php
+++ app/Http/Traits/LastMileAppReportTrait.php
@@ -8,6 +8,7 @@ use App\RiderWiseDeliveryNote;
 use App\RiderWiseDeliveryNoteShipment;
 use App\RiderWiseDeliveryNoteSummary;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
 
 trait LastMileAppReportTrait
 {
@@ -192,6 +193,8 @@ trait LastMileAppReportTrait
 
     static function countAdd($time,$check_summary)
     {   
+        
+        Log::channel('last_mile_app_report_log')->info('Before'.$check_summary);
 		if ($time <= '10:59:59') {
 			$check_summary->before_11_count = $check_summary->before_11_count + 1;
 		} elseif ($time > '10:59:59' && $time <= '11:59:59') {
@@ -221,7 +224,10 @@ trait LastMileAppReportTrait
 		} elseif ($time > '22:59:59' && $time <= '23:59:59') {
 			$check_summary->after_23_count = $check_summary->after_23_count + 1;
 		}
+        Log::channel('last_mile_app_report_log')->info('after'.$check_summary);
 		$check_summary->save();
+        Log::channel('last_mile_app_report_log')->info('save'.$check_summary);
+
     }
 
     static function countSub($time,$check_summary)
