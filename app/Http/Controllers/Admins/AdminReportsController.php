8813cc67e5842f4309d278882f5a4fb8f1f8d9da
Zain <zain.hyder@trax.pk>
2024-02-29 20:02:13 +0500

    rename alias

--- app/Http/Controllers/Admins/AdminReportsController.php
+++ app/Http/Controllers/Admins/AdminReportsController.php
@@ -12343,7 +12343,7 @@ class AdminReportsController extends Controller
                  ->where('sj.shipper_status_id', '=', 12);
         })
         ->leftjoin('shipment_status as rv_status', 'sj.shipper_status_id', 'rv_status.id')
-        ->leftjoin('shipment_status_reason as rv_reason', 'sj.shipper_status_id', 'rv_reason.id')
+        ->leftjoin('shipment_status_reason as rv_reason', 'sj.status_reason_id', 'rv_reason.id')
 
         ->leftjoin('shipments_journey as sjj', function($join) {
             $join->on('sjj.shipment_id', '=', 'shipments.id')
