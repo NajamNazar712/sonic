304b5aab451d941aa41696d75492b7f5390e65c6
Mohammad <muhammad.ahmed@trax.pk>
2024-03-01 18:16:01 +0500

    select ALL condition removed

--- resources/views/admin/lost/index.blade.php
+++ resources/views/admin/lost/index.blade.php
@@ -570,14 +570,14 @@
 
                                 if (id) {
                                     var index = $.inArray(id, selected_rows);
-                                    if ((index === -1 && approval >= 1 && cleared == 0) || (index === -1 && confirm_reattempt === 1) || (index === -1 && permission == 944 && shipment_cleared == null)) {
+                                    // if ((index === -1 && approval >= 1 && cleared != 0) || (index === -1 && confirm_reattempt === 1) || (permission == 944 && shipment_cleared == null)) {
                                         selected_rows.push(id);
                                         row.select();
                                         table.button('.reject').enable();
                                         table.button('.approve').enable();
                                         table.button('.re-attempt').enable();
                                         table.button('.confirm').enable();
-                                    }
+                                    // }
                                 }
                             }
                         });
