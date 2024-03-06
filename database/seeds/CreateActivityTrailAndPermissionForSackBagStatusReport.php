9625e64f1d1a670a02e72e8b6fcf0f83d33f7046
usamauddin <usama.uddin@trax.pk>
2024-03-06 16:00:27 +0500

    remove adminscreen list in old sackbag sseders

--- database/seeds/CreateActivityTrailAndPermissionForSackBagStatusReport.php
+++ database/seeds/CreateActivityTrailAndPermissionForSackBagStatusReport.php
@@ -22,9 +22,6 @@ class CreateActivityTrailAndPermissionForSackBagStatusReport extends Seeder
 
         ));
 
-//        $timestamp = \Carbon\Carbon::now()->format('Y-m-d H:i:s');
-//        DB::table('admins_screen_list')->insert(array(
-//            array('created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Reports > Canvas Bag Status', 'url' => 'admin.reports.sack_bag_status.index', 'permission_id' => 936),
-//        ));
+
     }
 }
