815bc904000753bd8df5ef1e8fd0bc23797d1601
Mohammad <muhammad.ahmed@trax.pk>
2024-03-11 12:03:23 +0500

    join_corrected

--- app/Http/Controllers/Admins/LostShipmentsController.php
+++ app/Http/Controllers/Admins/LostShipmentsController.php
@@ -105,7 +105,7 @@ class LostShipmentsController extends Controller
                             DB::raw('(select max(id) from shipments_journey where shipments_journey.shipment_id = shipments.id and shipments_journey.shipper_status_id = 2)'));
                 })
                 ->leftJoin('shipment_status_reason as ssr', 'ssr.id', '=', 'shipments_journey.status_reason_id')
-                ->join('lost_shipment_responsibles as lsr', 'lsr.shipment_id', '=', 'shipments.id')
+                ->leftjoin('lost_shipment_responsibles as lsr', 'lsr.shipment_id', '=', 'shipments.id')
 
 //                ->leftJoin('shipment_payment_status as sps', 'sps.id', '=', 'shipments.payment_status_id')
                 ->select('shipments.id as shId', 'shipments.tracking_number as tracking_number_link', 'shipments.tracking_number','shipments.user_id as shipper_id', 'u.name as shipper', 'oc.name as origin', 'dc.name as destination', 'h.name as hub', 'shipments.consignee_name', 'shipments.consignee_phone_number_1 as phone', 'shipments.consignee_address', 'shipments.amount', 'sm.mode as shipping_mode', 'bt.booking_type as service_type', 'ss.name as status', 'ssr.name as reason', 'shipments_journey.remarks as remarks', 'shipments_journey.created_at as status_date', 'shipments_journey.created_at as current_status_date', 'sj.created_at as arrival','shipments.payment_status_id', 'shipments.booking_type_id', 'usi.poc', 'shipments_journey.reference_1_id as reference','ad.name as marked_by', 'lsr.shipment_id as responsible_person_shipment')
