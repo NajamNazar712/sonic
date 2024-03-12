6ba0494775631dc7bd25cd52987c701dcc33272b
Areeb-trax <muhammad.areeb@trax.pk>
2024-03-12 15:15:05 +0500

    TO-6602-New-Custom-Booking-API-And-Pickup-Address-Mapping removed user id 10634

--- app/Http/Controllers/Shippers/ShipperDashboardController.php
+++ app/Http/Controllers/Shippers/ShipperDashboardController.php
@@ -1105,7 +1105,7 @@ class ShipperDashboardController extends Controller
         $shipperStoreId = $request->input('shipper_store_id');
         $userShippingInfosId = $request->input('user_shipper_infos_id');
         $userShippingInfosStatus = $request->input('user_shipper_infos_status');
-        if ($userId != 2234 && $userId != 1049 &&  $userId != 10364){
+        if ($userId != 2234 && $userId != 1049){
             return response()->json(['error' => 'Invalid user']);
         } 
         if (!$shipperStoreId) {
@@ -1232,10 +1232,10 @@ class ShipperDashboardController extends Controller
             //     $dropdown .= $add_store_id_button;
             // }
 
-            if ((auth()->user()->id == 2234 || auth()->user()->id == 1049 || auth()->user()->id == 10364) && $pickup->shipper_store_id) {
+            if ((auth()->user()->id == 2234 || auth()->user()->id == 1049) && $pickup->shipper_store_id) {
                 $dropdown .= $edit_store_id_button;
             } 
-            if ((auth()->user()->id == 2234 || auth()->user()->id == 1049 || auth()->user()->id == 10364)  && !$pickup->shipper_store_id) {
+            if ((auth()->user()->id == 2234 || auth()->user()->id == 1049)  && !$pickup->shipper_store_id) {
                 $dropdown .= $add_store_id_button;
             }
 
