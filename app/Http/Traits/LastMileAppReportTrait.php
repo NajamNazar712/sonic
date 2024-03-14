d04e37978255fb2b4ec5abe4b474d59d95715d00
Anas <anas.mazhar@trax.pk>
2024-03-14 12:54:10 +0500

    Working on it

--- app/Http/Traits/LastMileAppReportTrait.php
+++ app/Http/Traits/LastMileAppReportTrait.php
@@ -59,14 +59,17 @@ trait LastMileAppReportTrait
                  $delivery_note_data = $delivery_note_data->first();
                  
                  $check_summary = RiderWiseDeliveryNoteSummary::where('rider_id',$rider_id)->whereDate('delivery_date', $today);
-                 
                  if($check_summary->exists())
                  {
                      $check_summary = $check_summary->first();
                      $rwdnsum_id = $check_summary->id;
-                     $check_existing_shipment = RiderWiseDeliveryNoteShipment::where('shipment_id',$shipment_id)->where('rwdn_id',$delivery_note_id);
+                     $check_existing_note = RiderWiseDeliveryNote::where('delivery_note_id',$delivery_note_id);
+                     
+                     $check_existing_shipment = RiderWiseDeliveryNoteShipment::where('shipment_id',$shipment_id)->where('rwdn_id',$check_existing_note->latest()->first()->id);
+                     $this->createDeliveryNoteErrorLog($delivery_note_id,$shipment_id,'first-'.$check_existing_note->latest()->first()->id);
                      if($check_existing_shipment->exists())
                      {
+                         $this->createDeliveryNoteErrorLog($delivery_note_id,$shipment_id,'Sec-'.$check_existing_shipment->exists());
                        
                         
                         $update_delivery_note_shipment = $check_existing_shipment->first();
@@ -115,7 +118,6 @@ trait LastMileAppReportTrait
                      }
                     $check_summary->save();
                     
-                     $check_existing_note = RiderWiseDeliveryNote::where('delivery_note_id',$delivery_note_id);
                      if(!$check_existing_note->exists())
                      {
                         
