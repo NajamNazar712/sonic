0c63e943184f850c22eae2a126c89212f20ef128
usamauddin <usama.uddin@trax.pk>
2024-03-19 16:59:32 +0500

    create bridge table called:2024_03_19_164259_create_pms_admin_products_table for attached admin with product and area also create screen called admin_products

---
+++ database/migrations/2024_03_19_164259_create_pms_admin_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePmsAdminProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pms_admin_products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->nullable();
+            $table->integer('type')->default(1)->nullable(); //1-admin 2-user
+            $table->integer('product_id')->nullable();
+            $table->string('area')->nullable();
+            $table->integer('status')->default(1)->nullable();
+            $table->integer('created_by')->nullable();
+            $table->integer('update_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pms_admin_products');
+    }
+}
