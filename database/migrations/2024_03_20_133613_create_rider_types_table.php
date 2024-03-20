4b5d992c813d3d936f403098e00249d1e0e735c0
Mohammad <muhammad.ahmed@trax.pk>
2024-03-20 13:40:21 +0500

    migration and model have been made

---
+++ database/migrations/2024_03_20_133613_create_rider_types_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRiderTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rider_types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rider_types');
+    }
+}
