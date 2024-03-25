2ad6c07ecaa5d22317510236e769523a685e566e
Mohammad <muhammad.ahmed@trax.pk>
2024-03-25 15:44:32 +0500

    optimize in track actual shipment

--- app/Http/Controllers/Admins/AdminTrackingController.php
+++ app/Http/Controllers/Admins/AdminTrackingController.php
@@ -1498,7 +1498,7 @@ class AdminTrackingController extends Controller
                                     case 28:
                                         $scanning_data = $shipment_scanning_query->where('screen_location_id', 7)->latest()->first();
                                         break;
-                                    case 32:f
+                                    case 32:
                                         $scanning_data = $shipment_scanning_query->where('screen_location_id', 2)->latest()->first();
                                         break;
                                     case 33:
@@ -2480,7 +2480,6 @@ class AdminTrackingController extends Controller
                 )');
         })
         ->leftJoin('shipment_scanning_journey_area_logs as ssjal', 'ssjal.shipment_scanning_journey_id', '=', 'ssj.id')
-        ->leftJoin('shipment_scanning_journey_area_logs as ssjal_hss', 'ssjal_hss.shipment_scanning_journey_id', '=', 'ssj_hss.id')        
         ->select(['shipment_positions.tracking_number', 'shipment_positions.origin', 'shipment_positions.destination', 'shipment_positions.status', 'shipment_positions.status_at', 'shipment_positions.status_by', 'shipment_positions.screen_location', 'shipment_positions.city', 'shipment_positions.scanned_by', 'shipment_positions.scanned_at', 'shipment_positions.handover_note', 'shipment_positions.handover_created_by', 'shipment_positions.handover_created_at', 'shipment_positions.handover_from', 'shipment_positions.handover_to', 'shipment_positions.handover_received_by', 'shipment_positions.handover_received_at', 'shipment_positions.last_action','u.name as shipper_name','s.amount as cod_value','a.trax_id' ,'sj.admin_id as admin_id','s.id as shipment_id','sjl.shipment_id as journey_latest_id',
         'sjl.updated_at as journey_latest_updated_at',
         'sjl.shipper_status_id as latest_shipper_status_id','s.shipper_status_id as shipper_status_id', 'ssj.id as ssj_id',
