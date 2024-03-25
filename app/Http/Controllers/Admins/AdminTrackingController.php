69dadb99ac764d8862270fc4cc0c6ace39ceb312
Mohammad <muhammad.ahmed@trax.pk>
2024-03-25 17:14:14 +0500

    Track Actual Shipment Position Excluded

--- app/Http/Controllers/Admins/AdminTrackingController.php
+++ app/Http/Controllers/Admins/AdminTrackingController.php
@@ -2409,7 +2409,7 @@ class AdminTrackingController extends Controller
                                 $shipment_position->handover_received_by = $handover_received_by;
                                 $shipment_position->handover_received_at =  $handover_received_at != '-' ? Carbon::parse($handover_received_at)->format('Y-m-d H:i:s') : '-';
                                 $shipment_position->last_action = $last_action;
-                                $shipment_position->tracked_bfy = Auth::id();
+                                $shipment_position->tracked_by = Auth::id();
                                 $shipment_position->save();
                             }
 
@@ -2467,23 +2467,22 @@ class AdminTrackingController extends Controller
                     DB::connection('reports')->raw('(select max(id) from shipments_journey where shipments_journey.shipment_id = s.id)')
                 );
         })
-        ->leftjoin('admins as admin', 'admin.id', '=', 'journey.admin_id')
-        ->leftJoin('shipment_status_screen_locations as sssl', 'sssl.shipment_status_id', '=', 'journey.shipper_status_id')
-        ->leftJoin('shipment_scanning_journeys as ssj', function ($join) {
-            $join->on('ssj.shipment_id', '=', 'journey.shipment_id')
-                 ->whereRaw('ssj.id = (
-                    select max(id) 
-                    from shipment_scanning_journeys 
-                    where shipment_scanning_journeys.shipment_id = journey.shipment_id
-                    and shipment_scanning_journeys.screen_location_id = sssl.screen_location_id and
-                    admin.role_id != 1
-                )');
-        })
-        ->leftJoin('shipment_scanning_journey_area_logs as ssjal', 'ssjal.shipment_scanning_journey_id', '=', 'ssj.id')
+        // ->leftjoin('admins as admin', 'admin.id', '=', 'journey.admin_id')
+        // ->leftJoin('shipment_status_screen_locations as sssl', 'sssl.shipment_status_id', '=', 'journey.shipper_status_id')
+        // ->leftJoin('shipment_scanning_journeys as ssj', function ($join) {
+        //     $join->on('ssj.shipment_id', '=', 'journey.shipment_id')
+        //          ->whereRaw('ssj.id = (
+        //             select max(id) 
+        //             from shipment_scanning_journeys 
+        //             where shipment_scanning_journeys.shipment_id = journey.shipment_id
+        //             and shipment_scanning_journeys.screen_location_id = sssl.screen_location_id and
+        //             admin.role_id != 1
+        //         )');
+        // })
+        //->leftJoin('shipment_scanning_journey_area_logs as ssjal', 'ssjal.shipment_scanning_journey_id', '=', 'ssj.id'), 'ssj.id as ssj_id','ssjal.location_status as location_status'
         ->select(['shipment_positions.tracking_number', 'shipment_positions.origin', 'shipment_positions.destination', 'shipment_positions.status', 'shipment_positions.status_at', 'shipment_positions.status_by', 'shipment_positions.screen_location', 'shipment_positions.city', 'shipment_positions.scanned_by', 'shipment_positions.scanned_at', 'shipment_positions.handover_note', 'shipment_positions.handover_created_by', 'shipment_positions.handover_created_at', 'shipment_positions.handover_from', 'shipment_positions.handover_to', 'shipment_positions.handover_received_by', 'shipment_positions.handover_received_at', 'shipment_positions.last_action','u.name as shipper_name','s.amount as cod_value','a.trax_id' ,'sj.admin_id as admin_id','s.id as shipment_id','sjl.shipment_id as journey_latest_id',
         'sjl.updated_at as journey_latest_updated_at',
-        'sjl.shipper_status_id as latest_shipper_status_id','s.shipper_status_id as shipper_status_id', 'ssj.id as ssj_id',
-        'ssjal.location_status as location_status'
+        'sjl.shipper_status_id as latest_shipper_status_id','s.shipper_status_id as shipper_status_id'
 
         ])
         ->where('tracked_by', Auth::id())->groupBy('shipment_positions.shipment_id');
@@ -2548,14 +2547,14 @@ class AdminTrackingController extends Controller
                     $trax_id = '-';
                 }
                 return $trax_id;
-            })
-            ->editColumn('location_status', function ($shipment) {
-                if(isset($shipment->location_status)){
-                    return $shipment->location_status == 1 ? 'On-site' : 'Off-site';
-                }else{
-                    return '-';
-                }
             });
+            // ->editColumn('location_status', function ($shipment) {
+            //     if(isset($shipment->location_status)){
+            //         return $shipment->location_status == 1 ? 'On-site' : 'Off-site';
+            //     }else{
+            //         return '-';
+            //     }
+            // });
         return $datatables->make(true);
     }
 }
