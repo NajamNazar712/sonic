678697a82121418e2130e38491d56714ee19ec9b
Areeb-trax <muhammad.areeb@trax.pk>
2024-03-28 12:42:45 +0500

    TO-6631-Retail-Booking-Points ongoing task

--- app/Http/Controllers/Retail/RetailShipmentBookController.php
+++ app/Http/Controllers/Retail/RetailShipmentBookController.php
@@ -433,6 +433,17 @@ class RetailShipmentBookController extends Controller
             $shipper_info->shipper_address = $request->shipper_address;
             $shipper_info->city_id = $pickup_city_id;
             $shipper_info->save();
+
+            if ($request->iban_no == null || $request->iban_no == ''){
+                return redirect()->back()->with(['error' => 'Please provide the IBAN number']);
+            } else if ($request->account_no == null || $request->account_no == '') {
+                return redirect()->back()->with(['error' => 'Please provide the account number']);
+            } else if ($request->bank == null || $request->bank == ''){
+                return redirect()->back()->with(['error' => 'Please choose a bank']);
+            } else if (!$request->hasFile('cheque_image')){
+                return redirect()->back()->with(['error' => 'Please provide the cheque image']);
+            } 
+
             if ($request->hasFile('cheque_image') && $request->iban_no != null && $request->account_no != null && $request->bank != null) {
                 $shipper_info->bank_id = $request->bank;
                 $shipper_info->iban = $request->iban_no;
--- resources/views/retail/shipment/booking/index.blade.php
+++ resources/views/retail/shipment/booking/index.blade.php
@@ -127,10 +127,15 @@
                                             <input type="text" name="order_id" id="order_id" class="form-control" placeholder="Order ID">
                                         </div>
                                     </div>
-                                    <div class="form-group col-6 previous-names">
+                                    {{-- <div class="form-group col-6 previous-names">
                                         <select name="previous_name" id="previous_name" class="select2 form-control">
                                         </select>
+                                    </div> --}}
+
+                                    <div class="form-group col-6">
+                                        <a href="javascript:void(0);" id="auto_fetch_shipper" class="btn btn-sm btn-outline-success sm" disabled="disabled">Auto Fetch</a>
                                     </div>
+
                                     <div class="form-group col-6">
                                         <input type="text" name="shipper_name" id="shipper_name" class="form-control shipper_name" placeholder="Shipper Name*" data-rule-required="true" data-msg-required="Shipper Name is required">
                                     </div>
@@ -423,6 +428,35 @@
                     </div>
                        <div class="col-12 form-group">
                             <table class="table" id="consignee_table">
+
+                            </table>
+                       </div>
+                   </div>
+               </div>
+           </form>
+       </div>
+   </div>
+</div>
+
+<div class="modal fade text-left" id="AutoFetchShipper" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="AutoFetchShipper"
+    aria-hidden="true">
+   <div class="modal-dialog modal-lg" role="document">
+       <div class="modal-content">
+           <div class="modal-header bg-primary white">
+               <h4 class="modal-title white">Shipper Details</h4>
+               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                   <span aria-hidden="true">&times;</span>
+               </button>
+           </div>
+           <form id="auto_fetch_shipper_form" class="form-horizontal" novalidate="novalidate">
+               @csrf
+               <div class="modal-body">
+                   <div class="row justify-content-center">
+                    {{-- <div class="col-12 form-group">
+                        <h3 class="text-danger text-center" id="black_listed_employee">Employee is Blacklisted</h3>
+                    </div> --}}
+                       <div class="col-12 form-group">
+                            <table class="table" id="shipper_table">
 
                             </table>
                        </div>
@@ -1452,6 +1486,63 @@
             $('#AutoFetchConsignee').on('hidden.bs.modal', function () {
                 $('#consignee_table').html('');   
 
+            });
+
+            $('#auto_fetch_shipper').on('click', function(){
+                if($('input[name="shipper_phone_no"]').val().match(/\d/g) != null){
+					var length = $('input[name="shipper_phone_no"]').val().match(/\d/g).length;
+				}
+				else{
+					var length = 0;
+				}
+				if(length == 11){
+					$.ajax({
+                    url: '{!! route('retail.shipment.book.previous_names_verify') !!}',
+                    method: 'POST',
+                    data: {
+                        '_token': '{{ csrf_token() }}',
+                        'phone_number': $('input[name="shipper_phone_no"]').val(),
+                        
+                    }
+                })
+                    .done(function(data) {
+                        if(data.status == 0){
+                            toastr.error(data.message, 'Error!', {
+                            positionClass: 'toast-top-center',
+                            containerId: 'toast-top-center'
+                            });
+                        }else{
+                            // if(data.blacklist == 0){
+                            //     $('#black_listed_employee').addClass('d-none');
+                            // }else{
+                            //     $('#black_listed_employee').removeClass('d-none');
+                            // }
+                            $('#AutoFetchShipper').modal('show');
+                            var html = '';
+                            $.each(data.data, function (index, details) {
+                                    html +='<tr><td><a href="javascript:void(0)" class="btn btn-outline-success btn-sm shipper_auto_fetch_btn"><i class="ft-check"></i></a></td>';
+                                    html +='<td>'+details.shipper_name+'</td>';
+                                    html +='<td>'+details.shipper_address+'</td></tr>';
+                                });
+                                $('#shipper_table').html(html);   
+                                $('.shipper_auto_fetch_btn  ').on('click', function(){
+                                    var name = $(this).parent().next().html();
+                                    var address = $(this).parent().next().next().html();
+
+                                    $('#shipper_name').val(name);
+                                    $('#shipper_address').val(address);
+                                    $('#AutoFetchShipper').modal('hide');
+
+                                });
+                        }
+                      
+                    });
+				}
+                
+            });
+
+            $('#AutoFetchShipper').on('hidden.bs.modal', function () {
+                $('#shipper_table').html('');   
             });
 
             $('#previous_name').prepend('<option value="" selected="selected"></option>').select2({
