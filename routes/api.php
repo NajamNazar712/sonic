277d39ead56743c78d8ada684975559e974151f0
usamauddin <usama.uddin@trax.pk>
2024-03-28 12:42:15 +0500

    add api version prefix in api route

--- routes/api.php
+++ routes/api.php
@@ -356,9 +356,12 @@ Route::name('api.')->group(function () {
                 Route::post('scan_shipment', 'Rider\RiderAPIController@scan_shipment')->name('index');
             });
 
-            Route::prefix('logistic')->name('logistic.')->group(function (){
-                Route::get('data','Rider\Logistic\Api\RiderLogisticApiController@logistic_data')->name('data');
+            Route::prefix('v1')->name('v1.')->group(function (){
+                Route::prefix('logistic')->name('logistic.')->group(function (){
+                    Route::get('data','Rider\Logistic\Api\RiderLogisticApiController@logistic_data')->name('data');
+                });
             });
+
         });
 
     });
