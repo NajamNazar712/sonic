7d021f3d357ee8174ffa568bd7c47a63fdfaa8cb
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2024-04-02 17:52:00 +0500

    changes

--- app/Jobs/CountFintechCharges.php
+++ app/Jobs/CountFintechCharges.php
@@ -115,7 +115,8 @@ class CountFintechCharges implements ShouldQueue
                 ];
                 $client = new Client();
                 $response = $client->request('Post', $this->url, $options);
-                Log::channel('trax_pay')->info('s ' . json_encode($response->getBody()->getContents()));
+//                Log::channel('trax_pay')->info('s ' . json_encode($response->getBody()->getContents()));
+                Log::channel('trax_pay')->info('s ' . json_encode($request_body));
             }
 
         } catch (\Throwable $e) {
