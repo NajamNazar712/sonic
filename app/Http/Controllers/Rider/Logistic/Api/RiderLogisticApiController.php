13b1b5780c468bb514d03581bb067c399b7b28c0
usamauddin <usama.uddin@trax.pk>
2024-04-03 17:18:16 +0500

    remove dd function

--- app/Http/Controllers/Rider/Logistic/Api/RiderLogisticApiController.php
+++ app/Http/Controllers/Rider/Logistic/Api/RiderLogisticApiController.php
@@ -147,7 +147,6 @@ class RiderLogisticApiController extends Controller
             return response()->json(['status'=>0,'success'=>'Booking Completed Successfully']);
 
         }catch (\Exception $ex) {
-            dd($ex->getMessage());
             DB::rollback();
             return response()->json(['status'=>1,'error'=>'Something went wrong!']);
         }
