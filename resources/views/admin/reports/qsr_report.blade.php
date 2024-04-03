6db3f0d27fa8e17e044cacc114afa6abcdccb9df
Mohammad <muhammad.ahmed@trax.pk>
2024-04-03 16:15:33 +0500

    Sorted Corrected

--- resources/views/admin/reports/qsr_report.blade.php
+++ resources/views/admin/reports/qsr_report.blade.php
@@ -609,7 +609,7 @@
                     }
                 },
                 rowId: 'shId',
-                order: [[21, 'desc']],
+                order: [[22, 'desc']],
                 columns: [
                     {orderable: false, searchable: false, name: 'serial_number', class: 'align-middle serial_number', targets: 0, render: function (data, type, row) {return '';}},
                     {data: 'tracking_number_link', name: 'shipments.tracking_number', class: 'align-middle tracking_number_link'},
