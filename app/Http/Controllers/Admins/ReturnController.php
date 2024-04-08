1f2ef8a8877d21a379764b28d24119bb162cab93
Anas <anas.mazhar@trax.pk>
2024-04-08 15:32:39 +0500

    Condition change

--- app/Http/Controllers/Admins/ReturnController.php
+++ app/Http/Controllers/Admins/ReturnController.php
@@ -314,10 +314,10 @@ class ReturnController extends Controller
 
 
         //Pending First And Second Call Tickets Count
-        $number_of_pending_tickets = Shipment::leftJoin('rv_agent_call_histories as rvach', 'rvach.shipment_id', 'shipments.id')
-        ->where('shipments.shipper_status_id', 12)
+        $number_of_pending_tickets = Shipment::leftJoin('rv_shipment_assign_agents as rvsa', 'rvsa.shipment_id', 'shipments.id')
+        ->leftJoin('rv_agent_call_histories as rvach', 'rvach.shipment_id', 'shipments.id')
         ->selectRaw('COUNT(CASE WHEN rvach.id IS NULL THEN 1 END) AS pending_first_call_count')
-        ->selectRaw('COUNT(CASE WHEN rvsa.id IS NOT NULL OR rvsa.unresponsive_attempt_time IS NOT NULL THEN 1 END) AS pending_second_call_count')
+        ->selectRaw('COUNT(CASE WHEN rvsa.id IS NOT NULL AND rvsa.unresponsive_attempt_time IS NOT NULL THEN 1 END) AS pending_second_call_count')
         ->first();
 
         //Pending First Call
