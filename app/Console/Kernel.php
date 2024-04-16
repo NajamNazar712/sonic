785678d431de4ec1f2994308cf011bfaac1e3db9
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2024-04-16 17:07:49 +0500

    changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -291,7 +291,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('email:returnconfirmationpending')->dailyAt('10:00')->runInBackground();
         $schedule->command('email:returnconfirm')->dailyAt('15:00')->runInBackground();
         $schedule->command('email:shipmentreattempt')->dailyAt('08:00')->runInBackground();
-        $schedule->command('shipment:cancel')->dailyAt('00:00')->runInBackground();
+        //$schedule->command('shipment:cancel')->dailyAt('00:00')->runInBackground();
         $schedule->command('shipper:disable')->dailyAt('00:00')->runInBackground();
         $schedule->command('email:outstandingshipments')->dailyAt('10:00')->runInBackground();
         $schedule->command('keyaccount:dashboard')->dailyAt('4:00')->runInBackground();
