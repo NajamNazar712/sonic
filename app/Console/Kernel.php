4b1fc274a420938588261a4833f9913d26899823
Anas <anas.mazhar@trax.pk>
2024-04-19 12:12:46 +0500

    Cron job Changes status twice daily at same day

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -222,8 +222,7 @@ class Kernel extends ConsoleKernel
         // }
 
         // $schedule->command('agent:changeStatus')->everyFiveMinutes()->withoutOverlapping()->runInBackground();
-        $schedule->command('agent:changeStatus')->dailyAt('23:00')->runInBackground(); // Dailt at 9:55
-        $schedule->command('agent:changeStatus')->dailyAt('00:05')->runInBackground(); // Dailt at 9:55
+        $schedule->command('agent:changeStatus')->twiceDaily('21:55','00:10')->runInBackground(); // Dailt at 9:55
 
         //SarNotification Email Cron
         $agent_sar_settings = GlobalSettings::where('type', 'agent_sar_notification');
