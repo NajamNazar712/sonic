cbe16fbc3a19b772ddced340aa4e4b508a4c24af
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2024-04-21 16:54:27 +0500

    cron done payment and cancel shipment openm

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -292,7 +292,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('email:returnconfirmationpending')->dailyAt('10:00')->runInBackground();
         $schedule->command('email:returnconfirm')->dailyAt('15:00')->runInBackground();
         $schedule->command('email:shipmentreattempt')->dailyAt('08:00')->runInBackground();
-        //$schedule->command('shipment:cancel')->dailyAt('00:00')->runInBackground();
+        $schedule->command('shipment:cancel')->dailyAt('00:00')->runInBackground();
         $schedule->command('shipper:disable')->dailyAt('00:00')->runInBackground();
         $schedule->command('email:outstandingshipments')->dailyAt('10:00')->runInBackground();
         $schedule->command('keyaccount:dashboard')->dailyAt('4:00')->runInBackground();
@@ -371,7 +371,7 @@ class Kernel extends ConsoleKernel
 
         $schedule->command('shipmentemail:cancel')->dailyAt('8:00')->runInBackground();
 
-        $schedule->command('report:donepayment')->dailyAt('19:05')->runInBackground();
+        $schedule->command('report:donepayment')->dailyAt('17:30')->runInBackground();
         $schedule->command('report:retaildonepayment')->dailyAt('17:30')->runInBackground();
 
         $settings = GlobalSettings::where('type', 'completed_aging_report_time');
