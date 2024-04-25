7fbc963c7d0af9e5f3df2a67623f014a5aaa2d68
usamauddin <usama.uddin@trax.pk>
2024-04-25 16:48:31 +0500

    show image in booking  screen

--- resources/views/admin/logistic/edit_logistic_book.blade.php
+++ resources/views/admin/logistic/edit_logistic_book.blade.php
@@ -570,7 +570,6 @@
                                                 <div class="col-md-12" >
                                                     <div class="label_img">
                                                         @if(isset($booking_img_url))
-{{--                                                            {{dd(asset($booking_img_url))}}--}}
                                                             <img id="myimage" src="{{asset($booking_img_url)}}" >
                                                         @else
                                                             <img id="myimage" src="" alt="Booking label not available" >
