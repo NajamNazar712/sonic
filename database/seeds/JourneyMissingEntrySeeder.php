d45b9549dd7bd24d42733c5fbeff9ba6c5444a50
Anas <anas.mazhar@trax.pk>
2024-04-26 16:00:17 +0500

    Fixes seeder JourneyMIssing

--- database/seeds/JourneyMissingEntrySeeder.php
+++ database/seeds/JourneyMissingEntrySeeder.php
@@ -19,50 +19,54 @@ class JourneyMissingEntrySeeder extends Seeder
     public function run()
     {
         //
-        $shipmentId = [34691375,34690896,34689305,34677630,34571165,34690896,34690896,34691375,34689305];
+        $shipmentId = [34690896,34689305,34677630,34571165,34690896,34690896,34691375,34689305];
         
-        foreach($shipmentId as $Shipment){
-            $shipment = Shipment::find($Shipment);
+        foreach($shipmentId as $value){
+            $shipment = Shipment::find($value);
             if($shipment->shipper_status_id === 14)
             {
                 $charges = $shipment->weight_charges + $shipment->fuel_surcharge;
-                $deliveryNoteId = DeliveryNoteShipment::where('shipment_id',$Shipment)->first();
+                $deliveryNoteId = DeliveryNoteShipment::where('shipment_id',$shipment->id)->first();
                 $pending_payment = PendingPayment::where('user_id', $shipment->user_id);
+                $pending_payment_shipment = PendingPaymentShipment::where('shipment_id', $shipment->id);
                 $zone = Zone::find($shipment->pickup_address->city->zone_id);
                 $gst = ROUND($charges * $zone->gst, 2, PHP_ROUND_HALF_DOWN);
                 $payable = $shipment->amount - $gst;
-                if ($pending_payment->exists()) {
-                    $pending_payment = $pending_payment->first();
+                // if ($pending_payment->exists()) {
+                //     $pending_payment = $pending_payment->first();
     
-                    $pending_payment->total_shipments = $pending_payment->total_shipments + 1;
-                    $pending_payment->delivered_shipments = $pending_payment->delivered_shipments + 1;
+                //     $pending_payment->total_shipments = $pending_payment->total_shipments + 1;
+                //     $pending_payment->delivered_shipments = $pending_payment->delivered_shipments + 1;
     
-                    $pending_payment->save();
-                } else {
-                    $pending_payment = new PendingPayment();
+                //     $pending_payment->save();
+                // } else {
+                //     $pending_payment = new PendingPayment();
     
-                    $pending_payment->user_id = $shipment->user_id;
-                    $pending_payment->total_shipments = 1;
-                    $pending_payment->delivered_shipments = 1;
-                    $pending_payment->returned_shipments = 0;
-                    $pending_payment->adjusted_shipments = 0;
+                //     $pending_payment->user_id = $shipment->user_id;
+                //     $pending_payment->total_shipments = 1;
+                //     $pending_payment->delivered_shipments = 1;
+                //     $pending_payment->returned_shipments = 0;
+                //     $pending_payment->adjusted_shipments = 0;
     
-                    $pending_payment->save();
+                //     $pending_payment->save();
+                // }
+                if(!$pending_payment_shipment->exists())
+                {
+                    $pending_payment_shipment->pending_payment_id = $pending_payment->id;
+                    $pending_payment_shipment->shipment_id = $Shipment;
+                    $pending_payment_shipment->type = 0;
+                    $pending_payment_shipment->amount = $shipment->amount;
+                    $pending_payment_shipment->charges = $charges;
+                    $pending_payment_shipment->gst = $zone->gst;
+                    $pending_payment_shipment->payable = $payable;
+                    $pending_payment_shipment->save();
+
                 }
-                $pending_payment_shipment = new PendingPaymentShipment();
+                
 
-                $pending_payment_shipment->pending_payment_id = $pending_payment->id;
-                $pending_payment_shipment->shipment_id = $Shipment;
-                $pending_payment_shipment->type = 0;
-                $pending_payment_shipment->amount = $shipment->amount;
-                $pending_payment_shipment->charges = $charges;
-                $pending_payment_shipment->gst = $zone->gst;
-                $pending_payment_shipment->payable = $payable;
-
-                $pending_payment_shipment->save();
                 // $deliveryNoteId->status = 6;
                 // $deliveryNoteId->save();
-                ShipmentsJourneyController::add($shipment->shipment_id, $shipment->shipper_status_id, $shipment->shipper_status_id, NULL, NULL, $shipment->user_id, NULL, $deliveryNoteId->delivery_note_id);
+                ShipmentsJourneyController::add($shipment->id, $shipment->shipper_status_id, $shipment->shipper_status_id, NULL, NULL, $shipment->user_id, NULL, $deliveryNoteId->delivery_note_id);
 
             }
         }
