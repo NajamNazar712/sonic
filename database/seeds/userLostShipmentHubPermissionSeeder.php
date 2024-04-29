8940ca648ca0a3596ab978efd7756e8b8bb12a87
Mohammad <muhammad.ahmed@trax.pk>
2024-04-29 19:51:43 +0500

    seeder name corrected

--- database/seeds/userLostShipmentHubPermissionSeeder.php
+++ database/seeds/userLostShipmentHubPermissionSeeder.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class userLostShipmentHubPermissionSeeder extends Seeder
+class UserLostShipmentHubPermissionSeeder extends Seeder
 {
     /**
      * Run the database seeds.
