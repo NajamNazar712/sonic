ee28822226da83d136e36542e4f207052d2ee788
Mohammad <muhammad.ahmed@trax.pk>
2024-04-30 14:05:52 +0500

    migration added

---
+++ database/migrations/2024_04_30_134436_update_add_columns_in_leads_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateAddColumnsInLeadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('leads', function (Blueprint $table) {
+            $table->integer('average_shipment_per_week')->after('call_status');
+            $table->integer('average_parcel_cod_amount')->after('average_shipment_per_week');
+            $table->string('business_address')->after('average_parcel_cod_amount');
+            $table->string('company_name')->after('business_address');
+            $table->boolean('business_registered_status')->after('company_name');
+            $table->integer('ntn_number')->after('business_registered_status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('leads', function (Blueprint $table) {
+            $table->integer('average_shipment_per_week');
+            $table->integer('average_parcel_cod_amount');
+            $table->integer('business_address');
+            $table->integer('company_name');
+            $table->integer('business_registered_status');
+            $table->integer('ntn_number');
+        });
+    }
+}
