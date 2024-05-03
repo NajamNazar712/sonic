9e3a80a3a74954542b8eabb675c0691ddc9fc03d
Hammad Zafar <hammad.zafar@trax.pk>
2024-05-03 11:52:06 +0500

    Remarks Updated in Bulk Adjust In Payments Action

--- app/Http/Controllers/Admins/AdminFinanceController.php
+++ app/Http/Controllers/Admins/AdminFinanceController.php
@@ -1843,7 +1843,7 @@ class AdminFinanceController extends Controller
                                         ShipmentsPaymentJourneyController::add($shipment_id, 4, Auth::id());
                                     }
 
-                                    ShipmentsJourneyController::add($shipment_id, 13, 13, NULL, "Shipment Reverted", NULL, Auth::id());
+                                    ShipmentsJourneyController::add($shipment_id, 13, 13, NULL, "Shipment reverted from delivered status as it was incorrectly updated", NULL, Auth::id());
 
                                     if ($shipment->packaging_material_request == 1) {
                                         $packaging_material_shipment = PackagingMaterialRequest::where('tracking_number', $shipment->tracking_number)->first();
@@ -1994,7 +1994,7 @@ class AdminFinanceController extends Controller
                                         ShipmentsPaymentJourneyController::add($shipment_id, 4, Auth::id(), '', NULL, 1);
                                     }
 
-                                    ShipmentsJourneyController::add($shipment_id, 13, 13, NULL, "Shipment Reverted", NULL, Auth::id());
+                                    ShipmentsJourneyController::add($shipment_id, 13, 13, NULL, "Shipment reverted from delivered status as it was incorrectly updated", NULL, Auth::id());
 
                                     if ($shipment->packaging_material_request == 1) {
                                         $packaging_material_shipment = PackagingMaterialRequest::where('tracking_number', $shipment->tracking_number)->first();
@@ -2151,7 +2151,7 @@ class AdminFinanceController extends Controller
                                     ShipmentsPaymentJourneyController::add($shipment_id, 4, Auth::id());
                                 }
 
-                                ShipmentsJourneyController::add($shipment_id, 13, 13, NULL, "Shipment Reverted", NULL, Auth::id());
+                                ShipmentsJourneyController::add($shipment_id, 13, 13, NULL, "Shipment reverted from delivered status as it was incorrectly updated", NULL, Auth::id());
 
                                 if ($shipment->packaging_material_request == 1) {
                                     $packaging_material_shipment = PackagingMaterialRequest::where('tracking_number', $shipment->tracking_number)->first();
@@ -2302,7 +2302,7 @@ class AdminFinanceController extends Controller
                                     ShipmentsPaymentJourneyController::add($shipment_id, 4, Auth::id(), '', NULL, 1);
                                 }
 
-                                ShipmentsJourneyController::add($shipment_id, 13, 13, NULL, "Shipment Reverted", NULL, Auth::id());
+                                ShipmentsJourneyController::add($shipment_id, 13, 13, NULL, "Shipment reverted from delivered status as it was incorrectly updated", NULL, Auth::id());
 
                                 if ($shipment->packaging_material_request == 1) {
                                     $packaging_material_shipment = PackagingMaterialRequest::where('tracking_number', $shipment->tracking_number)->first();
