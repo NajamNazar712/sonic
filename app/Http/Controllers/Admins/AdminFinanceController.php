60f1b736b7b720516ba721d7c662cce9b0b8cea8
Areeb-trax <muhammad.areeb@trax.pk>
2024-05-09 19:11:55 +0500

    TO-6634-Shipment-Reversal-Access

--- app/Http/Controllers/Admins/AdminFinanceController.php
+++ app/Http/Controllers/Admins/AdminFinanceController.php
@@ -5344,7 +5344,7 @@ class AdminFinanceController extends Controller
                                         ShipmentsPaymentJourneyController::add($request->id, 4, Auth::id());
                                     }
 
-                                    ShipmentsJourneyController::add($request->id, 13, 13, NULL, NULL, NULL, Auth::id());
+                                    ShipmentsJourneyController::add($shipment_id, 13, 13, NULL, "Shipment reverted from delivered status as it was incorrectly updated", NULL, Auth::id());
 
                                     if ($shipment->packaging_material_request == 1) {
                                         $packaging_material_shipment = PackagingMaterialRequest::where('tracking_number', $shipment->tracking_number)->first();
