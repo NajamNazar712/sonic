e6a77537d9186a122973f7104437b23478f91111
Hammad Zafar <hammad.zafar@trax.pk>
2024-05-13 18:11:47 +0500

    bugs fixed

--- app/Http/Controllers/Agent/ReturnV2Controller.php
+++ app/Http/Controllers/Agent/ReturnV2Controller.php
@@ -118,6 +118,11 @@ class ReturnV2Controller extends Controller
         if ($admin->exists()) {
             $admin = $admin->first();
 
+            if($admin->agent_caller_type == null)
+            {
+                return response()->json(['status' => 7, 'error' => 'Contact Your Admin For Shipments']);
+            }
+
             $employee = Employee::where('phone_number', $admin->phone_number)->where('staff_category_id', 3)->where('status_id', '!=', 2);
 
             $shipment = [];
