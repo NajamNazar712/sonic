7a1b14c4f36ece29588c63ac421279bff4600c00
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2024-05-17 17:18:40 +0500

    minor fixes

--- app/Http/Controllers/Admins/V2Pickup/V2AdminPickupsController.php
+++ app/Http/Controllers/Admins/V2Pickup/V2AdminPickupsController.php
@@ -745,7 +745,6 @@ class V2AdminPickupsController extends Controller
         $id = $request->shipment_id;
         $data = V2PickupRequest::find($id);
         $data->reminder_status = 1;
-        $data->reminder_status = 1;
         $data->save();
         return ['status' => 1, 'success' => "Reminder successfully Set"];
     }
