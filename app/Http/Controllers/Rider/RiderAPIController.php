84dc7c640e2712810780371f98caa1e6bb1512b1
Anas <anas.mazhar@trax.pk>
2024-05-21 18:05:23 +0500

    parameter fixes for the mobile side

--- app/Http/Controllers/Rider/RiderAPIController.php
+++ app/Http/Controllers/Rider/RiderAPIController.php
@@ -12761,7 +12761,7 @@ class RiderAPIController extends Controller
                 foreach ($delivery_note_shipments as $delivery_note_shipment) {
                     $replacement_parcel_image = null;
                     $shipment_data = $delivery_note_shipment->shipment;
-                    $information['summary']['requests']['shipment_reattempt_marked'] = ($shipment_data && $shipment_data->shipper_status_id == 13) ? 1 : 0; // using this key for the color mark in api
+                    $information['summary']['completed']['shipment_reattempt_marked'] = ($shipment_data && $shipment_data->shipper_status_id == 13) ? 1 : 0; // using this key for the color mark in api
 
                     $shipment_id = $shipment_data->id;
                     $refusal_otp = null;
