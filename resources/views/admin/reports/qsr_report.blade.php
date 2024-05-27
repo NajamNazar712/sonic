503c783c9feb0cd4da907461b3bb31f307612c60
Mohammad <muhammad.ahmed@trax.pk>
2024-05-27 16:51:38 +0500

    qsr done new requirement

--- resources/views/admin/reports/qsr_report.blade.php
+++ resources/views/admin/reports/qsr_report.blade.php
@@ -216,8 +216,13 @@
                         <th class="border-primary border-darken-1">First Attempt Date</th>
                         <th class="border-primary border-darken-1">Rider Picked Status Date</th>
                         <th class="border-primary border-darken-1">Status</th>
-                        <th class="border-primary border-darken-1">Location Status Area</th>
-                        <th class="border-primary border-darken-1">Location Status</th>
+                        {{-- <th class="border-primary border-darken-1">Location Status Area</th>
+                        <th class="border-primary border-darken-1">Location Status</th> --}}
+
+                        <th class="border-primary border-darken-1">Last Location Screen Name</th>
+                        <th class="border-primary border-darken-1">Sub Hub</th>
+                        <th class="border-primary border-darken-1">Last Location Updated At</th>
+
                         <th class="border-primary border-darken-1">Reason</th>
                         <th class="border-primary border-darken-1">Remarks</th>
                         <th class="border-primary border-darken-1">Total Attempt</th>
@@ -667,8 +672,13 @@
                     {data: 'first_attempt_date', name: 'sjfa.created_at', as:'first_attempt_date', class: 'align-middle first_attempt_date',text:'First Attempt Date',value:'first_attempt_date',download:true},
                     {data: 'rider_picked_status_date', name: 'sjrp.created_at', as:'rider_picked_status_date', class: 'align-middle rider_picked_status_date',text:'Rider Picked Status Date',value:'rider_picked_status_date',download:true},
                     {data: 'status', name: 'ss.name', as:'status', class: 'align-middle status', text:'Status', value:'status',download:true},
-                    {data: 'scanning_city_area_name', name: 'ca_scanning.name', as:'scanning_city_area_name', class: 'align-middle scanning_city_area_name',text:'Location Status Area',value:'scanning_city_area_name',download:true},
-                    {data: 'location_status', name: 'ssjal.location_status', as:'location_status', class: 'align-middle location_status',text:'Location Status',value:'location_status',download:true},
+                    // {data: 'scanning_city_area_name', name: 'ca_scanning.name', as:'scanning_city_area_name', class: 'align-middle scanning_city_area_name',text:'Location Status Area',value:'scanning_city_area_name',download:true},
+                    // {data: 'location_status', name: 'ssjal.location_status', as:'location_status', class: 'align-middle location_status',text:'Location Status',value:'location_status',download:true},
+                    
+                    {data: 'last_location_screen_location_name', name: 'shipment_scanning_screen_locations.name', as:'last_location_screen_location_name', class: 'align-middle last_location_screen_location_name',text:'Last Location Screen Location Name',value:'last_location_screen_location_name',download:true},
+                    {data: 'ca_scanning_last_location_name', name: 'ca_scanning_last_location_name.name', as:'ca_scanning_last_location_name', class: 'align-middle ca_scanning_last_location_name',text:'Last Location Area',value:'ca_scanning_last_location_name',download:true},
+                    {data: 'last_location_updated_at', name: 'ssjal_last_location.updated_at', as:'last_location_updated_at', class: 'align-middle last_location_updated_at',text:'Last Location Updated At',value:'ssjal_last_location',download:true},
+
                     {data: 'reason', name: 'ssr.name', as:'reason', class: 'align-middle reason',text:'Reason',value:'reason',download:true},
                     {data: 'remarks', name: 'sjr.remarks', class: 'align-middle remarks',text:'Remarks',value:'remarks',download:true},
                     {data: 'total_attempt' ,name: 'total_attempt', class: 'align-middle total_attempt',text:'Total Attempt',value:'total_attempt',download:true},
