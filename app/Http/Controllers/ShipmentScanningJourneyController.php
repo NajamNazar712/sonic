dbd67d096fb1dd31b9816eef829c1ba8c41ca926
Mohammad <muhammad.ahmed@trax.pk>
2024-05-30 19:21:45 +0500

    Rider Condition When Null in shipment_scanning_area_logs function

--- app/Http/Controllers/ShipmentScanningJourneyController.php
+++ app/Http/Controllers/ShipmentScanningJourneyController.php
@@ -52,11 +52,11 @@ class ShipmentScanningJourneyController extends Controller
         if(!$employee){
             $employee = Rider::find($auth_id);
         }
-        
+
         $addScanningHistoryAreaLog = new ShipmentScanningJourneyAreaLog([
             'shipment_id' => $shipment_id,
             'shipment_scanning_journey_id' => $latestShipmentScanningId,
-            'hub_id' => ($via == 'rider') ? $employee->city_id : $employee->default_hub_id,
+            'hub_id' => isset($employee->default_hub_id) ? $employee->default_hub_id : $employee->city_id,            
             'area_id' => optional($employee)->area_id,
             'admin_id' => ($via == 'rider') ? null : $auth_id, 
             'rider_id' => ($via == 'rider') ? $auth_id : null, 
