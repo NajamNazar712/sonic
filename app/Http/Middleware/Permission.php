243379f606299e4e8fd0db69a431f95aab582204
Hammad Zafar <hammad.zafar@trax.pk>
2024-05-31 15:52:56 +0500

    permissions updated

--- app/Http/Middleware/Permission.php
+++ app/Http/Middleware/Permission.php
@@ -793,6 +793,7 @@ class Permission
             'settings.shippers.base_rate_revisions.bulk_store' => 990,
             'settings.shippers.base_rate_revisions.approval1_update' => 990,
             'settings.shippers.base_rate_revisions.approval2_update' => 990,
+            'settings.shippers.base_rate_revisions.shippers_with_rates' => 990,
 
             'settings.shippers.status_webhook.index' => 646,
             'settings.shippers.status_webhook.list' => 646,
