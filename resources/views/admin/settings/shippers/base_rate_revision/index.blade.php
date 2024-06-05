cf15c4ab341023ea78b5e82db2c8df0da4c02f69
Hammad Zafar <hammad.zafar@trax.pk>
2024-06-05 15:47:30 +0500

    .

--- resources/views/admin/settings/shippers/base_rate_revision/index.blade.php
+++ resources/views/admin/settings/shippers/base_rate_revision/index.blade.php
@@ -365,8 +365,8 @@
                     $('td:eq(0)', row).html(index + 1 + info.page * info.length);
                 },
                 initComplete: function() {
-                    // var search = $('<tr role="row" class="bg-primary bg-lighten-1 search"></tr>')
-                    //     .appendTo(this.api().table().header());
+                    var search = $('<tr role="row" class="bg-primary bg-lighten-1 search"></tr>')
+                        .appendTo(this.api().table().header());
 
                     var td =
                         '<td style="padding:5px;" class="border-primary border-lighten-2"><fieldset class="form-group m-0 position-relative has-icon-right"></fieldset></td>';
@@ -374,11 +374,11 @@
                         '<input type="text" class="form-control form-control-sm input-sm primary">';
                     var icon =
                         '<div class="form-control-position primary"><i class="la la-search"></i></div>';
-                    // var status_select =
-                    //     '<select name="status_select" id="status_select" class="status_select form-control">' +
-                    //     '<option value="1">Enabled</option>' +
-                    //     '<option value="0">Disabled</option>' +
-                    //     '</select>';
+                    var status_select =
+                        '<select name="status_select" id="status_select" class="status_select form-control">' +
+                        '<option value="1">Enabled</option>' +
+                        '<option value="0">Disabled</option>' +
+                        '</select>';
 
                     this.api().columns().every(function(column_id) {
                         var column = this;
