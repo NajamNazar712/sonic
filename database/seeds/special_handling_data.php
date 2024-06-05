1ddbdb6f1f2dc44ea6773f8598e8ae39a9ca35ea
usamauddin <usama.uddin@trax.pk>
2024-06-05 16:59:28 +0500

    special_handling_data add other handling row

--- database/seeds/special_handling_data.php
+++ database/seeds/special_handling_data.php
@@ -13,7 +13,9 @@ class special_handling_data extends Seeder
     {
         $time_stamp =  \Carbon\Carbon::now();
         \Illuminate\Support\Facades\DB::table('trax_special_handling_list')->insert(array(
-          array('handling_code'=>'Electronic','description'=>'Electronic Charges','rate'=>550,'pay_mode'=>'Cash','created_at'=>$time_stamp,'updated_at'=>$time_stamp)
+          array('handling_code'=>'Electronic','description'=>'Electronic Charges','rate'=>550,'pay_mode'=>'Cash','created_at'=>$time_stamp,'updated_at'=>$time_stamp),
+            array('handling_code'=>'Other','description'=>'Other Charges','rate'=>0,'pay_mode'=>'Cash','created_at'=>$time_stamp,'updated_at'=>$time_stamp)
+
         ));
     }
 }
