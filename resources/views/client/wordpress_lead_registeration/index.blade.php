2e8527550d01f1623c777744f5ef4411613af11f
Mohammad <muhammad.ahmed@trax.pk>
2024-06-11 15:48:13 +0500

    issue fixed

--- resources/views/client/wordpress_lead_registeration/index.blade.php
+++ resources/views/client/wordpress_lead_registeration/index.blade.php
@@ -1165,6 +1165,26 @@
                                                                         </div>
                                                                     </fieldset>
                                                                 </div>
+
+                                                                <div class="col-md-3 text-center">
+                                                                    <fieldset>
+                                                                        <div class="input-group form-group">
+                                                                            <div class="input-group-prepend">
+                                                                                <span class="input-group-text">Reverse Pickup</span>
+                                                                            </div>
+                                                                            <input type="text" class="form-control percent"
+                                                                                   data-rule-required="true"
+                                                                                   data-msg-required="This field is required"
+                                                                                   value="{{$shippingType[1][0]->reverse_pickup_charges}}"
+                                                                                   name="on_reverse_charges" readonly>
+                                                                            <div class="input-group-append">
+                                                                                <span class="input-group-text">%</span>
+                                                                            </div>
+                                                                        </div>
+                                                                    </fieldset>
+                                                                </div>
+
+
                                                                 <div class="col-md-6 text-center">
                                                                     <div class="row">
                                                                         <div class="col-3 mt-1">
@@ -1637,6 +1657,25 @@
                                                                         </div>
                                                                     </fieldset>
                                                                 </div>
+
+                                                                <div class="col-md-3 text-center">
+                                                                    <fieldset>
+                                                                        <div class="input-group form-group">
+                                                                            <div class="input-group-prepend">
+                                                                                <span class="input-group-text">Reverse Pickup</span>
+                                                                            </div>
+                                                                            <input type="text" class="form-control percent"
+                                                                                   data-rule-required="true"
+                                                                                   data-msg-required="This field is required"
+                                                                                   value="{{$shippingType[2][0]->reverse_pickup_charges}}"
+                                                                                   name="ol_reverse_charges" readonly>
+                                                                            <div class="input-group-append">
+                                                                                <span class="input-group-text">%</span>
+                                                                            </div>
+                                                                        </div>
+                                                                    </fieldset>
+                                                                </div>
+
                                                                 <div class="col-md-6 text-center">
                                                                     <div class="row">
                                                                         <div class="col-3 mt-1">
@@ -2105,6 +2144,25 @@
                                                                         </div>
                                                                     </fieldset>
                                                                 </div>
+
+                                                                <div class="col-md-3 text-center">
+                                                                    <fieldset>
+                                                                        <div class="input-group form-group">
+                                                                            <div class="input-group-prepend">
+                                                                                <span class="input-group-text">Reverse Pickup</span>
+                                                                            </div>
+                                                                            <input type="text" class="form-control percent"
+                                                                                   data-rule-required="true"
+                                                                                   data-msg-required="This field is required"
+                                                                                   value="{{$shippingType[3][0]->reverse_pickup_charges}}"
+                                                                                   name="detain_reverse_charges" readonly>
+                                                                            <div class="input-group-append">
+                                                                                <span class="input-group-text">%</span>
+                                                                            </div>
+                                                                        </div>
+                                                                    </fieldset>
+                                                                </div>
+
                                                                 <div class="col-md-6 text-center">
                                                                     <div class="row">
                                                                         <div class="col-3 mt-1">
@@ -2538,6 +2596,25 @@
                                                                         </div>
                                                                     </fieldset>
                                                                 </div>
+
+                                                                <div class="col-md-3 text-center">
+                                                                    <fieldset>
+                                                                        <div class="input-group form-group">
+                                                                            <div class="input-group-prepend">
+                                                                                <span class="input-group-text">Reverse Pickup</span>
+                                                                            </div>
+                                                                            <input type="text" class="form-control percent"
+                                                                                   data-rule-required="true"
+                                                                                   data-msg-required="This field is required"
+                                                                                   value="{{$shippingType[4][0]->reverse_pickup_charges}}"
+                                                                                   name="sameday_reverse_charges" readonly>
+                                                                            <div class="input-group-append">
+                                                                                <span class="input-group-text">%</span>
+                                                                            </div>
+                                                                        </div>
+                                                                    </fieldset>
+                                                                </div>
+
                                                                 <div class="col-md-6 text-center">
                                                                     <div class="row">
                                                                         <div class="col-3 mt-1">
