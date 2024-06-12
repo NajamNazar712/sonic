0363797d214c1e6ab40e6f188bef680a50e85db1
Najam <muhammad.najam@trax.pk>
2024-06-12 20:44:15 +0500

    functionality added for short url against tracking link to send on OFD sms

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -126,6 +126,8 @@ use App\Http\Models\ShipmentSmsLogs;
 use App\Http\Models\Survey\DisableAccountIntimationSendSurvey;
 use App\Http\Models\ShipperVerificationPinCode;
 use App\Http\Models\ShipmentsWeightType;
+use App\Http\Models\ShortUrl;
+
 
 class NotificationsController extends Controller
 {
@@ -1133,6 +1135,12 @@ class NotificationsController extends Controller
                         if (strpos($body, '[refusal_otp]') !== FALSE) {
                             $body = str_replace('[refusal_otp]', $shipment_otp->otp, $body);
                         }
+                    }
+                    //$tracking_link = self::make_short_url($shipment->tracking_number);
+                    if (strpos($body, '[tracking_link]') !== FALSE) {
+                       
+                        $tracking_link = self::make_short_url($shipment->tracking_number);
+                        $body = str_replace('[tracking_link]', $tracking_link, $body);
                     }
 
                     $sms_setting = self::sms_notification_setting(12, $shipper->id);
@@ -11566,6 +11574,57 @@ class NotificationsController extends Controller
 
         } else {
             return true;
+        }
+    }
+
+    static function make_short_url($tracking_number) {
+        
+        $site_url = "https://sonic.pk/";
+        $long_url = "https://trax.pk/tracking/?tracking_number=$tracking_number";
+		$random_param = substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil(3/strlen($x)) )),1,3);
+
+
+        $latest_id = ShortUrl::latest('id')->first();
+        if($latest_id != null) {
+            $latest_id = $latest_id->id + 1;
+        } else {
+            $latest_id = 1;
+        }
+
+        $check_record = ShortUrl::where('tracking_number', $tracking_number);
+
+        if(!$check_record->exists()) {
+
+            $check_record = new ShortUrl;
+            $check_record->long_url = $long_url;
+            $check_record->site_url = $site_url;
+            $check_record->tiny_url = $random_param . $latest_id;
+            $check_record->custom_param = 0;
+            $check_record->tracking_number = $tracking_number;
+            $check_record->source = 0; 
+            $check_record->medium = 0;
+            $check_record->description = null;  
+            $check_record->save();
+
+            $result =  $check_record->site_url . $check_record->tiny_url;
+            return $result;
+        } else {
+            
+            $check_record = $check_record->first();
+            $result =  $check_record->site_url . $check_record->tiny_url;
+            return $result;
+        }
+    }
+
+    public function get_actual_url($tiny_url) {
+       
+        $long_url = ShortUrl::where('tiny_url', $tiny_url);
+        if($long_url->exists()) {
+            $long_url = $long_url->latest()->first();
+            $long_url = $long_url->long_url;
+            return redirect()->away($long_url);
+        } else {
+            return redirect()->away('https://trax.pk');
         }
     }
 }
