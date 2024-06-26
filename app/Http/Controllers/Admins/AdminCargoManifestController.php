a88ff597d3e8398f2468008407ee89f5df0e123a
Najam <muhammad.najam@trax.pk>
2024-06-26 19:16:51 +0500

    some changes

--- app/Http/Controllers/Admins/AdminCargoManifestController.php
+++ app/Http/Controllers/Admins/AdminCargoManifestController.php
@@ -4426,7 +4426,7 @@ class AdminCargoManifestController extends Controller
                                 $bag->junction_mapping_id = null;
                                 foreach ($bag->shipment as $shipment) {
                                     $shipment_table = Shipment::find($shipment->shipment_id);
-                                    if (in_array($shipment_table->shipper_status_id, [3, 20, 21, 26, 32, 49])) {
+                                    if (in_array($shipment_table->shipper_status_id, [3, 21, 26, 32, 49])) {
                                         ShipmentsJourneyController::add($shipment->shipment_id, 11, 11, null, null, null, Auth::id(), $bag->seal_number);
                                         $shipment_table->shipper_status_id = 11;
                                         $shipment_table->consignee_status_id = 11;
@@ -5979,7 +5979,7 @@ class AdminCargoManifestController extends Controller
             DB::beginTransaction();
             Log::channel('cronJobLog')->info('cargo:check_1');
 
-        $shipment_status_array = [2,3, 20, 11,21, 26, 32, 49,68];
+        $shipment_status_array = [2,3,11,20,21,26,32,49,68];
         $shipment_ids = array_unique(explode(',', $request->shipment_ids));
         $open_box_ids = explode(',', $request->open_box_ids);
         $bag_ids = array();
