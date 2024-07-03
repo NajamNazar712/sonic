b92a0ba6c0d4541d17e35ba67eb150e4a61b623d
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2024-07-03 14:00:17 +0500

    addscreenpermission

--- database/seeds/ActivityTrailForSMSCountReport.php
+++ database/seeds/ActivityTrailForSMSCountReport.php
@@ -11,15 +11,15 @@ class ActivityTrailForSMSCountReport extends Seeder
      */
     public function run()
     {
-        DB::table('module_permissions')->insert(array(
-            array('id' => 945, 'name' => 'SMS Count Report - Permission', 'module_id' => 9),
-        ));
-
-        DB::table('activity_trail_actions')->insert(array(
-            array('id' => 753, 'screen_name' => 'SMS Count Report ', 'action'=> 'View'),
-            array('id' => 754, 'screen_name' => 'SMS Count Report ', 'action'=> 'Excel'),
-
-        ));
+//        DB::table('module_permissions')->insert(array(
+//            array('id' => 945, 'name' => 'SMS Count Report - Permission', 'module_id' => 9),
+//        ));
+//
+//        DB::table('activity_trail_actions')->insert(array(
+//            array('id' => 753, 'screen_name' => 'SMS Count Report ', 'action'=> 'View'),
+//            array('id' => 754, 'screen_name' => 'SMS Count Report ', 'action'=> 'Excel'),
+//
+//        ));
 
         $timestamp = \Carbon\Carbon::now()->format('Y-m-d H:i:s');
         DB::table('admins_screen_list')->insert(array(
