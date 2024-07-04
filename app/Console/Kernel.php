4976d2e89d44d391ba1d93b0fd0ab49c94dc9376
usamauddin <usama.uddin@trax.pk>
2024-07-04 16:32:59 +0500

    logistic email cron commit in kernal said by atif sir and add log cn issue to rider for testing

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -151,8 +151,8 @@ class Kernel extends ConsoleKernel
         'App\Console\Commands\AgentSarNotification',
         'App\Console\Commands\SackBagStatusUpdate',
         'App\Console\Commands\AutoAssignCrmAgentNew',
-        'App\Console\Commands\ShipperLogisticBookingCron',
-        'App\Console\Commands\HourlyShipperLogisticBookingEmailCron'
+//        'App\Console\Commands\ShipperLogisticBookingCron',
+//        'App\Console\Commands\HourlyShipperLogisticBookingEmailCron'
         ];
 
     /**
@@ -534,8 +534,8 @@ class Kernel extends ConsoleKernel
 		$schedule->command('email:qsrreport')->dailyAt('10:01')->runInBackground();
 		$schedule->command('email:pendingdeliveriesreport')->dailyAt('09:01')->runInBackground();
 		$schedule->command('clean:7DaysQrsPDReportStorage')->dailyAt('06:00')->runInBackground();
-        $schedule->command('logistic:shipper-bookings')->dailyAt('06:00')->runInBackground();
-        $schedule->command('hourly-logistic:shipper-bookings')->hourly()->runInBackground();
+//        $schedule->command('logistic:shipper-bookings')->dailyAt('06:00')->runInBackground();
+//        $schedule->command('hourly-logistic:shipper-bookings')->hourly()->runInBackground();
 
     }
     /**
