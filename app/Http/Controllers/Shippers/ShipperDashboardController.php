570c678b0e4d4336b523310cde9ba6a0502a5f03
Anas <anas.mazhar@trax.pk>
2024-07-08 17:11:52 +0500

    temporary fix for the percentage

--- app/Http/Controllers/Shippers/ShipperDashboardController.php
+++ app/Http/Controllers/Shippers/ShipperDashboardController.php
@@ -275,14 +275,8 @@ class ShipperDashboardController extends Controller
                 $user = User::find($shipper_id);
                 
                 $weight_charges = WeightCharge::where('user_id' , $shipper_id);
-
-                if (!isset($user->request_custom_quotation)) { // for the temporary fixes
-                    $lead_progress_setting = LeadProgressSetting::find(1);
-                    $percentage = $lead_progress_setting->percent;
-                    $color = $lead_progress_setting->color;
-
-                    $description = "Your account is $percentage% completed";
-                }elseif($user->status == 0 && !$weight_charges->exists() && (!isset($user->request_custom_quotation) || $user->request_custom_quotation != 1)){
+                
+                if($user->status == 0 && !$weight_charges->exists() && (!isset($user->request_custom_quotation) || $user->request_custom_quotation != 1)){
                     $lead_progress_setting = LeadProgressSetting::find(1);
                     $percentage = $lead_progress_setting->percent;
                     $color = $lead_progress_setting->color;
