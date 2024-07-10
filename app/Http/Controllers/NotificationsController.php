cc39f060499a89d3bda5ce45a1430910f967dfed
Mohammad <muhammad.ahmed@trax.pk>
2024-07-10 15:11:54 +0500

    email bypass check

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -267,7 +267,7 @@ class NotificationsController extends Controller
         dispatch(new ProcessOTPSMSForBotSMS($sms));
     }
 
-    static private function email($subject, $body, $to, $cc = null, $bcc = null, $from = null)
+    static private function email($subject, $body, $to, $cc = null, $bcc = null, $from = null, $id = null)
     {
         $block_email = BlockEmail::select('email')->pluck('email')->toArray();
         $filterBlockedEmails = function ($emails) use ($block_email) {
@@ -320,7 +320,11 @@ class NotificationsController extends Controller
                     $mail->bcc($bcc);
                 }
     
-                $mail->send(new Notifications($subject, $body, $from));
+                if($id = 230){
+                    $mail->sendNow(new Notifications($subject, $body, $from));
+                }else{
+                    $mail->send(new Notifications($subject, $body, $from));
+                }
             }
 
 
@@ -11171,21 +11175,20 @@ class NotificationsController extends Controller
                             $body = $notification->body; // Reset $body to its original state
                             $subject = $notification->subject;  // Reset $subject to its original state
 
-                            if (strpos($body, '[Link]') !== FALSE) {
-                                $body = str_replace('[Link]', $link, $body); // Use $body instead of $old_body
-                            }
-                            if (strpos($body, '[Company Name]') !== FALSE) {
-                                $body = str_replace('[Company Name]', $lead->company_name, $body); // Use $body instead of $old_body
-                            }
-                            if (strpos($body, '[Full Name]') !== FALSE) {
-                                $body = str_replace('[Full Name]', $lead->contact_person, $body); // Use $body instead of $old_body
-                            }
-                            if (strpos($subject, '[Company Name]') !== FALSE) {
-                                $subject = str_replace('[Company Name]', $lead->company_name, $subject);
-                            }
-                            self::email($subject, $body, $lead->email_address); // Send email with $body
-                        // }
-                    }
+                        if (strpos($body, '[Link]') !== FALSE) {
+                            $body = str_replace('[Link]', $link, $body); // Use $body instead of $old_body
+                        }
+                        if (strpos($body, '[Company Name]') !== FALSE) {
+                            $body = str_replace('[Company Name]', $lead->company_name, $body); // Use $body instead of $old_body
+                        }
+                        if (strpos($body, '[Full Name]') !== FALSE) {
+                            $body = str_replace('[Full Name]', $lead->contact_person, $body); // Use $body instead of $old_body
+                        }
+                        if (strpos($subject, '[Company Name]') !== FALSE) {
+                            $subject = str_replace('[Company Name]', $lead->company_name, $subject);
+                        }
+                        self::email($subject, $body, $lead->email_address, null, null, null, 230); // Send email with $body
+                    }                    
                 } else if ($id == 231){
                     $subject = $notification->subject;
                     $body = $notification->body;
