f1d9c83ef8d57bfc54594c4992a762a68827f376
Hammad Zafar <hammad.zafar@trax.pk>
2024-07-10 16:24:27 +0500

    added new columns in station_deposite_note_action_logs table

---
+++ database/migrations/2024_07_10_161051_add_log_columns_in_station_deposite_note_action_logs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddLogColumnsInStationDepositeNoteActionLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('station_deposite_note_action_logs', function (Blueprint $table) {
+            $table->integer('dncc_id')->nullable();
+            $table->bigInteger('dncc_amount')->nullable();
+            $table->string('action')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('station_deposite_note_action_logs', function (Blueprint $table) {
+            //
+        });
+    }
+}
