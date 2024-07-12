14fc315a2206970c51f25a3698949362af90fead
Faraz <khokhar.faraz.a@gmail.com>
2024-07-12 18:23:01 +0500

    change cities query only for domistics record from shipper_cities apis

--- app/Http/Controllers/Rider/RiderAPIController.php
+++ app/Http/Controllers/Rider/RiderAPIController.php
@@ -15234,7 +15234,7 @@ class RiderAPIController extends Controller
     {
 
        
-        $cities = City::where('status', 1);
+        $cities = City::where('status', 1)->where('business_category_id',1);
 
         if ($cities->exists()) {
             $cities = $cities->get();
