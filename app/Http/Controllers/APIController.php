db0474d2b8be1784f12a409112929ad1addf5cbd
Mohammad Ahmad <muhammad.ahmed@trax.pk>
2024-07-20 16:00:56 +0500

    check for new git config

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -9819,11 +9819,12 @@ class APIController extends Controller
 
     }
 
+
     public function trax_pk_validation($company_name =  null , $email_address = null , $phone_number = null) {
         $exists = User::where('name', $company_name)
                   ->orWhere('email', $email_address)
                   ->orWhere('phone', $phone_number)->exists();
-
+                  
         return response()->json(['exists' => $exists]);
     }
 }
