69ed94bfa4c2b789ef584c9e4b6ffd426ead92df
Hammad Zafar <hammad.zafar@trax.pk>
2024-07-22 19:01:21 +0500

    checking

--- resources/views/admin/crm/request_details.blade.php
+++ resources/views/admin/crm/request_details.blade.php
@@ -263,7 +263,7 @@
                                                     </span>
                                                                 </button>
                                                             @elseif($crm_details['status_id'] == 2)
-                                                                @if(session('role_id') == 1  || session('role_id') == 6 || $crm_details->agent['id'] == Auth::id() || in_array(184, session('permissions')) && (in_array($crm_details->shipment->pickup_address->city->hub_id, session('hubs')) || in_array($crm_details->shipment->consignee_city->hub_id, session('hubs'))))
+                                                                @if(session('role_id') == 1 || session('role_id') == 6 || $crm_details->agent['id'] == Auth::id() || in_array(184, session('permissions')) || (($tag_check['crm_request_tagging_type_id'] == 1 && $tag_check['tagged_id'] == $tag_permission) || ($tag_check['crm_request_tagging_type_id'] == 2 && $tag_check['tagged_id'] == Auth::id()) || $escalation_tagged_check == true) || (in_array(session('role_id'), [8, 9 ,10]) && (in_array($crm_details->shipment->pickup_address->city->hub_id, session('hubs')) || in_array($crm_details->shipment->consignee_city->hub_id, session('hubs')))))
                                                                     <button id="valid" type="submit"
                                                                             class="btn btn-success mr-1">
                                                         <span class="d-none d-lg-block">
