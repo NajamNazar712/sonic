3f943f1fe8a30e6dea99f0d9c203ea7611d4c487
Mohammad <muhammad.ahmed@trax.pk>
2024-07-24 17:11:42 +0500

    dynamic case nature claim in email

--- app/Console/Commands/CrmClosedReasonCron.php
+++ app/Console/Commands/CrmClosedReasonCron.php
@@ -41,10 +41,12 @@ class CrmClosedReasonCron extends Command
      */
     public function handle()
     {
+        $claim = CrmRequestCaseNature::where('name', 'Claim')->first()->id;  
+
         $closed_reason = CrmRequest::leftJoin('crm_request_feedbacks', 'crm_requests.id', '=', 'crm_request_feedbacks.crm_request_id')
         ->whereNull('crm_request_feedbacks.crm_request_id')
         ->where('crm_requests.status_id', 4)
-        ->where('crm_requests.case_nature_id', '!=', 1)
+        ->where('crm_requests.case_nature_id', '!=', $claim)
         ->whereDate('crm_requests.updated_at', now()->format('Y-m-d'))
         ->select('crm_requests.*')
         ->get();
