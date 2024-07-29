0dc0bf614568625327ee9fcae7d9acad1c7ec1e2
Mohammad <muhammad.ahmed@trax.pk>
2024-07-29 23:11:56 +0500

    empty check added on activation code

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -11182,6 +11182,7 @@ class NotificationsController extends Controller
 
                     self::email($subject, $body, $to);
                 } else if ($id == 230){
+                    dd(1);
                     $lead_ids = $reference_1_id;
 
                     if(!is_array($lead_ids)){
