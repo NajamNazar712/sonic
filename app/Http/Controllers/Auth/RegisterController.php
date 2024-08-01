d8142e852e1ccd10a5c83565b7f9d447fdc3fce2
Mohammad <muhammad.ahmed@trax.pk>
2024-08-01 12:22:49 +0500

    if $use null then ID will pick from sesssion

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -256,36 +256,36 @@ class RegisterController extends Controller
         if ($request->wordpress_lead_register != 1) {
 
             $user_attachment = new UserDocumentAttachment();
-            $user_attachment->user_id = $user->id;
+            $user_attachment->user_id = $user->id ?? session('user_id');
             $date = Carbon::now()->format('Y_m_d');
 
             if ($request->hasFile('cnic_front_image')) {
                 if ($user_attachment->cnic_front_image != NULL) {
-                    Storage::disk('public')->delete('users_attached_documents/' . $request->user_id . '/' . $user_attachment->cnic_front_image);
+                    Storage::disk('public')->delete('users_attached_documents/' . $request->user_id ?? session('user_id') . '/' . $user_attachment->cnic_front_image);
                 }
                 $filename = 'cnic_front_image_' . $date . '_' . $user->id . '.png';
                 $file = $request->file('cnic_front_image');
-                Storage::disk('public')->putFileAs('users_attached_documents/' . $user->id . '', $file, $filename);
+                Storage::disk('public')->putFileAs('users_attached_documents/' . $user->id ?? session('user_id') . '', $file, $filename);
                 $user_attachment->cnic_front_image = $filename;
             }
 
             if ($request->hasFile('cnic_back_image')) {
                 if ($user_attachment->cnic_back_image != NULL) {
-                    Storage::disk('public')->delete('users_attached_documents/' . $request->user_id . '/' . $user_attachment->cnic_back_image);
+                    Storage::disk('public')->delete('users_attached_documents/' . $request->user_id ?? session('user_id') . '/' . $user_attachment->cnic_back_image);
                 }
                 $filename = 'cnic_back_image_' . $date . '_' . $user->id . '.png';
                 $file = $request->file('cnic_back_image');
-                Storage::disk('public')->putFileAs('users_attached_documents/' . $user->id . '', $file, $filename);
+                Storage::disk('public')->putFileAs('users_attached_documents/' . $user->id ?? session('user_id') . '', $file, $filename);
                 $user_attachment->cnic_back_image = $filename;
             }
 
             if ($request->hasFile('blank_cheque_image')) {
                 if ($user_attachment->blank_cheque_image != NULL) {
-                    Storage::disk('public')->delete('users_attached_documents/' . $request->user_id . '/' . $user_attachment->blank_cheque_image);
+                    Storage::disk('public')->delete('users_attached_documents/' . $request->user_id ?? session('user_id') . '/' . $user_attachment->blank_cheque_image);
                 }
                 $filename = 'blank_cheque_image_' . $date . '_' . $user->id . '.png';
                 $file = $request->file('blank_cheque_image');
-                Storage::disk('public')->putFileAs('users_attached_documents/' . $user->id . '', $file, $filename);
+                Storage::disk('public')->putFileAs('users_attached_documents/' . $user->id ?? session('user_id') . '', $file, $filename);
                 $user_attachment->blank_cheque_image = $filename;
             }
             $user_attachment->uploaded_at = Carbon::now();
