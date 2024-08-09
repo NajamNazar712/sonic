4191c5ed4b479d465940398658c55e7f7c56449a
Najam <muhammad.najam@trax.pk>
2024-08-09 17:48:34 +0500

    replacement arrived at origin and try & buy arrived at origin statuses added to restrict user to receive shipment again as normal on quick receive bag shipment

--- app/Http/Controllers/Admins/AdminCargoManifestController.php
+++ app/Http/Controllers/Admins/AdminCargoManifestController.php
@@ -5363,11 +5363,11 @@ class AdminCargoManifestController extends Controller
                 // validate bag n shipment type
                 
                 if ($bag_type == 1) {
-                    if (in_array($shipment_status, [20, 21, 22, 23, 24, 25, 44, 47, 48,69,70,72,73,75,76])) {
+                    if (in_array($shipment_status, [20, 21, 22, 23, 24, 25, 44, 47, 48,69,70,72,73,75,76,27,33])) {
                         return ['status' => 1, 'error' => 'Tracking Number is of return type while bag type is normal !'];
                     }
                 } else {
-                    if (!in_array($shipment_status, [20, 21, 22, 23, 24, 25, 44, 47, 48,69,70,72,73,75,76])) {
+                    if (!in_array($shipment_status, [20, 21, 22, 23, 24, 25, 44, 47, 48,69,70,72,73,75,76,27,33])) {
                         return ['status' => 1, 'error' => 'Tracking Number is of normal type while bag type is return !'];
                     }
                 }
@@ -5485,7 +5485,7 @@ class AdminCargoManifestController extends Controller
 
                 // validate bag n shipment type
                 if ($bag_type == 1) {
-                    if (in_array($shipment_status, [20, 21, 22, 23, 24, 25, 44, 47, 48,69,70,72,73,75,76])) {
+                    if (in_array($shipment_status, [20, 21, 22, 23, 24, 25, 44, 47, 48,69,70,72,73,75,76,27,33])) {
                         return ['status' => 1, 'error' => 'Tracking Number is of return type while bag type is normal !'];
                     }
 
@@ -5503,7 +5503,7 @@ class AdminCargoManifestController extends Controller
                     // check that is shipment return reattempt or not end
 
                 } else {
-                    if (!in_array($shipment_status, [20, 21, 22, 23, 24, 25, 44, 47, 48,69,70,72,73,75,76])) {
+                    if (!in_array($shipment_status, [20, 21, 22, 23, 24, 25, 44, 47, 48,69,70,72,73,75,76,27,33])) {
                         return ['status' => 1, 'error' => 'Tracking Number is of normal type while bag type is return !'];
                     }
                 }
@@ -5679,7 +5679,7 @@ class AdminCargoManifestController extends Controller
             if ($shipment_status != 11) // agr status 11 h to masla h q k forwarding and return dono p same 11 lagta h ispe sochna h, lekin koshish ki h niche is resolve krne ki ($return_confirm_journey,$return_reattampt)
             {
                 if ($bag_type == 1) {
-                    if (in_array($shipment_status, [20, 21, 22, 23, 24, 25, 44, 47, 48,69,70,72,73,75,76])) {
+                    if (in_array($shipment_status, [20, 21, 22, 23, 24, 25, 44, 47, 48,69,70,72,73,75,76,27,33])) {
                         return ['status' => 1, 'error' => 'Tracking Number is of return type while bag type is normal !'];
                     }
                 } else {
@@ -5703,7 +5703,7 @@ class AdminCargoManifestController extends Controller
                     }
                     // check that is shipment return reattempt or not end
 
-                    if ((!in_array($shipment_status, [20, 21, 22, 24, 44, 47, 48,49,26,27,29,69,70,72,73,75,76])) && ($return_reattempt_flag == 1)) {
+                    if ((!in_array($shipment_status, [20, 21, 22, 24, 44, 47, 48,49,26,27,29,69,70,72,73,75,76,27,33])) && ($return_reattempt_flag == 1)) {
                         return ['status' => 1, 'error' => 'Tracking Number is of normal type while bag type is return !'];
                     }
                 }
