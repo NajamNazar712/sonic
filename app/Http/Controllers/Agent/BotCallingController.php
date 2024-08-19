c336090e1adc4e6c931f875f76564c3381b6191f
Anas <anas.mazhar@trax.pk>
2024-08-19 19:03:52 +0500

    working complted

--- app/Http/Controllers/Agent/BotCallingController.php
+++ app/Http/Controllers/Agent/BotCallingController.php
@@ -121,7 +121,7 @@ class BotCallingController extends Controller
             ];
             $shipment_assign_agent = RvShipmentAssignAgent::where('shipment_id', $findShipmentId->id)->where('rv_state_id', 1)->latest()->first();
             $request->request->add(['agent_id' => $request->admin_id, 'shipment_id' => $findShipmentId->id, 'rv_assign_agent_status_id' => $request->call_status, 'remarks' => 'bot call add the ' . $array[$request->input]['remarks'], 'rv_assign_agent_status_id' => $array[$request->input]['status_id'], 'call_count' => 1]);
-            Log::channel('cronJobLog')->info('s ' . 'bot-call- parameters'. $request);
+            Log::channel('cronJobLog')->info('s ' . 'bot-call- parameters'. json_encode($request));
 
             // if($shipment_assign_agent){
             if ($request->input > 0) {
