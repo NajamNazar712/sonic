4444cc77d2db921c77854d708eef32a138b97bfd
Anas <anas.mazhar@trax.pk>
2024-08-19 12:00:14 +0500

    Working insert

--- database/seeds/globalSettingAddOnFirstSecondCallValue.php
+++ database/seeds/globalSettingAddOnFirstSecondCallValue.php
@@ -12,7 +12,7 @@ class globalSettingAddOnFirstSecondCallValue extends Seeder
      */
     public function run()
     {
-        //
+        // 
         $timestamp = \Carbon\Carbon::now()->format('Y-m-d H:i:s');
         DB::table('global_settings')->insert(array(
             array('type' => 'second_bot_call', 'setting_value' => 15, 'created_at' => $timestamp, 'updated_at' => $timestamp),
