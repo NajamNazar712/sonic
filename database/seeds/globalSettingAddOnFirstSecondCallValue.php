fcf3584501558f08eef8937dea5f25a1ae3e0c25
Anas <anas.mazhar@trax.pk>
2024-08-19 11:51:54 +0500

    working completed

--- database/seeds/globalSettingAddOnFirstSecondCallValue.php
+++ database/seeds/globalSettingAddOnFirstSecondCallValue.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class globalSettingAddOnFirstSecondCallValue extends Seeder
 {
@@ -13,7 +14,7 @@ class globalSettingAddOnFirstSecondCallValue extends Seeder
     {
         //
         $timestamp = \Carbon\Carbon::now()->format('Y-m-d H:i:s');
-        \DB::table('global_settings')->insert(array(
+        DB::table('global_settings')->insert(array(
             array('type' => 'second_bot_call', 'setting_value' => 15, 'created_at' => $timestamp, 'updated_at' => $timestamp),
             array('type' => 'third_bot_call', 'setting_value' => 60, 'created_at' => $timestamp, 'updated_at' => $timestamp)
         ));
