ad6c8e00371dfd380bf5a2992889bef32c3ce544
Mohammad <muhammad.ahmed@trax.pk>
2024-08-20 16:50:02 +0500

    comma omitted, unique condtion removed

--- app/Http/Controllers/Admins/UserManagementController.php
+++ app/Http/Controllers/Admins/UserManagementController.php
@@ -1369,8 +1369,9 @@ class UserManagementController extends Controller
             return redirect()->back()->with(['error' => 'Zone Doesn\'t Exist!']);
         }
         
-        $zonalHubs = City::where(['zone_id' => $zone_id, 'status' => 1,])->pluck('hub_id')->unique()->toArray();
+        $zonalHubs = City::where(['zone_id' => $zone_id, 'status' => 1])->pluck('hub_id')->toArray();
         $existingHubIds = AdminHub::where('admin_id', $adminId)->pluck('hub_id')->toArray();
+
 
         $insertData = [];
         foreach ($zonalHubs as $hubId) {
