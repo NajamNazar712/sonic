0ccc6582d367ecbdb1b16623a09c8644ceacdb67
Areeb-trax <muhammad.areeb@trax.pk>
2024-08-20 14:08:56 +0500

    Seeder updated

--- database/seeds/ActivityTrailForShipmentReversalReport.php
+++ database/seeds/ActivityTrailForShipmentReversalReport.php
@@ -15,5 +15,10 @@ class ActivityTrailForShipmentReversalReport extends Seeder
         DB::table('activity_trail_actions')->insert(array(
             array('id' => 803, 'screen_name' => 'Shipment Reversal Report', 'action'=> 'View'),
         ));
+
+        $timestamp = \Carbon\Carbon::now()->format('Y-m-d H:i:s');
+        DB::table('admins_screen_list')->insert(array(
+            array('created_at' => $timestamp, 'updated_at' => $timestamp, 'name' => 'Reports > Shipment Reversal Report', 'url'=>'admin.reports.shipment_reversal_report.index', 'permission_id' => 894),           
+        ));
     }
 }
