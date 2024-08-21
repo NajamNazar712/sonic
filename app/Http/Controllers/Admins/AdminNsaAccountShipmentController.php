c4d36fab556c1b92d346d1384131ec0995040c7d
Areeb-trax <muhammad.areeb@trax.pk>
2024-08-21 14:56:23 +0500

    Updated rider id for express shippers

--- app/Http/Controllers/Admins/AdminNsaAccountShipmentController.php
+++ app/Http/Controllers/Admins/AdminNsaAccountShipmentController.php
@@ -468,7 +468,9 @@ class AdminNsaAccountShipmentController extends Controller
                                 $admin_id = Auth::id();
                             }
                             else {
-                                $rider_id = $settings->setting_value;
+                                // 16704 ----> new rider id for express shippers
+                                // $rider_id = $settings->setting_value;
+                                $rider_id = 16704;
                                 $admin_id = 50;
                             }
 
@@ -483,7 +485,6 @@ class AdminNsaAccountShipmentController extends Controller
                                 }
                             }
                         }
-
                         $order = false;
                         if ($shipments_count != 0) {
                             $note = DeliveryNote::create([
@@ -611,7 +612,8 @@ class AdminNsaAccountShipmentController extends Controller
         if ($nsa_shipments->exists()) {
             $nsa_shipments = $nsa_shipments->get();
             $settings = GlobalSettings::where('type', 'nsa_accounts')->first();
-            $rider_id = $settings->setting_value;
+            // $rider_id = $settings->setting_value;
+            $rider_id = 16704;
             $valid_shipments = array();
             $shipments_count = 0;
             $total_cod_amount = 0;
@@ -954,7 +956,8 @@ class AdminNsaAccountShipmentController extends Controller
                     if ($nsa_shipments->exists()) {
                         $nsa_shipments = $nsa_shipments->get();
                         $settings = GlobalSettings::where('type', 'nsa_accounts')->first();
-                        $rider_id = $settings->setting_value;
+                        // $rider_id = $settings->setting_value;
+                        $rider_id = 16704;
                         $valid_shipments = array();
                         $shipments_count = 0;
                         $total_cod_amount = 0;
