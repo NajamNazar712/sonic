8632cfaa8bca7004a381bee4a6367cb492ec30dd
Anas <anas.mazhar@trax.pk>
2024-08-27 13:52:30 +0500

    Substatus minor changes

--- database/seeds/BotCallAddRVAsginAgentSubStatus.php
+++ database/seeds/BotCallAddRVAsginAgentSubStatus.php
@@ -16,11 +16,11 @@ class BotCallAddRVAsginAgentSubStatus extends Seeder
         //
         $timestamp = Carbon::now()->format('Y-m-d H:i:s');
         DB::table('rv_assign_agent_sub_statuses')->insert(array(
-            array('name' => 'Not Answer', 'rv_assign_agent_status_id' => 6, 'is_active' => 1, 'created_at' => $timestamp, 'updated_at' => $timestamp),
+            array('name' => 'Consignee Disconnected', 'rv_assign_agent_status_id' => 6, 'is_active' => 1, 'created_at' => $timestamp, 'updated_at' => $timestamp),
             array('name' => 'Picked but no option selected', 'rv_assign_agent_status_id' => 6, 'is_active' => 1, 'created_at' => $timestamp, 'updated_at' => $timestamp),
-            array('name' => 'Consignee OPT to Reattempt', 'rv_assign_agent_status_id' => 2, 'is_active' => 1, 'created_at' => $timestamp, 'updated_at' => $timestamp),
-            array('name' => 'Consignee OPT to Return', 'rv_assign_agent_status_id' => 2, 'is_active' => 1, 'created_at' => $timestamp, 'updated_at' => $timestamp),
-            array('name' => 'Consignee OPT to Manual Call', 'rv_assign_agent_status_id' => 1, 'is_active' => 1, 'created_at' => $timestamp, 'updated_at' => $timestamp),
+            array('name' => 'Consignee Opt to Reattempt', 'rv_assign_agent_status_id' => 2, 'is_active' => 1, 'created_at' => $timestamp, 'updated_at' => $timestamp),
+            array('name' => 'Consignee Opt to Return', 'rv_assign_agent_status_id' => 2, 'is_active' => 1, 'created_at' => $timestamp, 'updated_at' => $timestamp),
+            array('name' => 'Consignee Opt to Manual Call', 'rv_assign_agent_status_id' => 1, 'is_active' => 1, 'created_at' => $timestamp, 'updated_at' => $timestamp),
         ));
     }
 }
