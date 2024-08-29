f9dc837fb63488c3b9ffac3f24b5d2ec2d2436d8
Anas <anas.mazhar@trax.pk>
2024-08-29 13:07:47 +0500

    Modificatioin of rvsubstatuses

--- database/seeds/BotCallAddRVAsginAgentSubStatus.php
+++ database/seeds/BotCallAddRVAsginAgentSubStatus.php
@@ -18,9 +18,9 @@ class BotCallAddRVAsginAgentSubStatus extends Seeder
         DB::table('rv_assign_agent_sub_statuses')->insert(array(
             array('name' => 'Consignee Disconnected', 'rv_assign_agent_status_id' => 6, 'is_active' => 1, 'created_at' => $timestamp, 'updated_at' => $timestamp),
             array('name' => 'Picked but no option selected', 'rv_assign_agent_status_id' => 6, 'is_active' => 1, 'created_at' => $timestamp, 'updated_at' => $timestamp),
-            array('name' => 'Consignee Opt to Reattempt', 'rv_assign_agent_status_id' => 2, 'is_active' => 1, 'created_at' => $timestamp, 'updated_at' => $timestamp),
-            array('name' => 'Consignee Opt to Return', 'rv_assign_agent_status_id' => 2, 'is_active' => 1, 'created_at' => $timestamp, 'updated_at' => $timestamp),
-            array('name' => 'Consignee Opt to Manual Call', 'rv_assign_agent_status_id' => 1, 'is_active' => 1, 'created_at' => $timestamp, 'updated_at' => $timestamp),
+            array('name' => 'Consignee opted to Reattempt', 'rv_assign_agent_status_id' => 2, 'is_active' => 1, 'created_at' => $timestamp, 'updated_at' => $timestamp),
+            array('name' => 'Consignee opted to Return (Refused)', 'rv_assign_agent_status_id' => 2, 'is_active' => 1, 'created_at' => $timestamp, 'updated_at' => $timestamp),
+            array('name' => 'Consignee opted to Manual Call', 'rv_assign_agent_status_id' => 1, 'is_active' => 1, 'created_at' => $timestamp, 'updated_at' => $timestamp),
         ));
     }
 }
