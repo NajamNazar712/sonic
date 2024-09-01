feb52137c027964bdd3e95f69cb69e592ca9ba51
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2024-09-01 12:23:22 +0500

    changes in api

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\ShipmentAdditionalCharges;
 use DB;
 use SnappyPDF;
 use Validator;
@@ -3850,7 +3851,10 @@ class APIController extends Controller
                             $details['payment_type'] = 'Delivered';
                         } else if ($done_payment_shipment->type == 1) {
                             $details['payment_type'] = 'Returned';
-                        } else {
+                        }else if ($done_payment_shipment->type == 3) {
+                            $details['payment_type'] = 'Arrival';
+                        }
+                        else {
                             $details['payment_type'] = 'Adjusted';
                         }
                         $details['payment_id'] = $done_payment_shipment->done_payment->id;
@@ -3904,19 +3908,23 @@ class APIController extends Controller
                     if (!$invoice_shipments->isEmpty()) {
                         foreach ($invoice_shipments as $invoice_shipment) {
                             $shipment = $invoice_shipment->shipment;
+                            $arrival_charges_applied = ShipmentAdditionalCharges::check_additional_charges($shipment->id,true,false,false);;
                             $details = array();
                             if ($invoice_shipment->type == 0) {
                                 $details[$shipment->tracking_number]['payment_type'] = 'Delivered';
                             } else if ($invoice_shipment->type == 1) {
                                 $details[$shipment->tracking_number]['payment_type'] = 'Returned';
-                            } else {
+                            } else if ($invoice_shipment->type == 3) {
+                                $details[$shipment->tracking_number]['payment_type'] = 'Arrival';
+                            }
+                            else {
                                 $details[$shipment->tracking_number]['payment_type'] = 'Adjusted';
                             }
-                            $details[$shipment->tracking_number]['weight_charges'] = (($account_type_id == 2 && $invoice_shipment->type != 2 && $invoice_shipment->charges != 0) ? $shipment->weight_charges : 0);
+                            $details[$shipment->tracking_number]['weight_charges'] = (($account_type_id == 2 && $invoice_shipment->type != 2 && $invoice_shipment->charges != 0 && ($invoice_shipment->type == 3 || (!$arrival_charges_applied))) ? $shipment->weight_charges : 0);
                             $details[$shipment->tracking_number]['cash_handling_charges'] = (($account_type_id == 2 && $invoice_shipment->type == 0 && $invoice_shipment->charges != 0) ? $shipment->cash_handling_charges : 0);
                             $details[$shipment->tracking_number]['insurance_charges'] = (($account_type_id == 2 && $invoice_shipment->type != 2 && $invoice_shipment->charges != 0) ? $shipment->insurance_charges : 0);
                             $details[$shipment->tracking_number]['return_charges'] = (($account_type_id == 2 && $invoice_shipment->type == 1 && $invoice_shipment->charges != 0) ? $shipment->return_charges : 0);
-                            $details[$shipment->tracking_number]['fuel_surcharge'] = (($account_type_id == 2 && $invoice_shipment->type != 2 && $invoice_shipment->charges != 0) ? $shipment->fuel_surcharge : 0);
+                            $details[$shipment->tracking_number]['fuel_surcharge'] = (($account_type_id == 2 && $invoice_shipment->type != 2 && $invoice_shipment->charges != 0 && ($invoice_shipment->type == 3 || (!$arrival_charges_applied))) ? $shipment->fuel_surcharge : 0);
                             $details[$shipment->tracking_number]['replacement_charges'] = (($account_type_id == 2 && $invoice_shipment->type != 2 && $invoice_shipment->charges != 0) ? $shipment->replacement_charges : 0);
                             $details[$shipment->tracking_number]['try_and_buy_charges'] = (($account_type_id == 2 && $invoice_shipment->type != 2 && $invoice_shipment->charges != 0) ? $shipment->try_and_buy_charges : 0);
                             $details[$shipment->tracking_number]['intercept_charges'] = (($account_type_id == 2 && $invoice_shipment->type != 2 && $invoice_shipment->charges != 0) ? $shipment->intercept_charges : 0);
@@ -3952,19 +3960,23 @@ class APIController extends Controller
                     if (!$done_payment_shipments->isEmpty()) {
                         foreach ($done_payment_shipments as $done_payment_shipment) {
                             $shipment = $done_payment_shipment->shipment;
+                            $arrival_charges_applied = ShipmentAdditionalCharges::check_additional_charges($shipment->id,true,false,false);;
                             $details = array();
                             if ($done_payment_shipment->type == 0) {
                                 $details[$shipment->tracking_number]['payment_type'] = 'Delivered';
                             } else if ($done_payment_shipment->type == 1) {
                                 $details[$shipment->tracking_number]['payment_type'] = 'Returned';
-                            } else {
+                            } else if ($done_payment_shipment->type == 3) {
+                                $details[$shipment->tracking_number]['payment_type'] = 'Arrival';
+                            }
+                            else {
                                 $details[$shipment->tracking_number]['payment_type'] = 'Adjusted';
                             }
-                            $details[$shipment->tracking_number]['weight_charges'] = (($account_type_id == 1 && $done_payment_shipment->type != 2 && $done_payment_shipment->charges != 0) ? $shipment->weight_charges : 0);
+                            $details[$shipment->tracking_number]['weight_charges'] = (($account_type_id == 1 && $done_payment_shipment->type != 2 && $done_payment_shipment->charges != 0 && ($done_payment_shipment->type == 3 || (!$arrival_charges_applied))) ? $shipment->weight_charges : 0);
                             $details[$shipment->tracking_number]['cash_handling_charges'] = (($account_type_id == 1 && $done_payment_shipment->type == 0 && $done_payment_shipment->charges != 0) ? $shipment->cash_handling_charges : 0);
                             $details[$shipment->tracking_number]['insurance_charges'] = (($account_type_id == 1 && $done_payment_shipment->type != 2 && $done_payment_shipment->charges != 0) ? $shipment->insurance_charges : 0);
                             $details[$shipment->tracking_number]['return_charges'] = (($account_type_id == 1 && $done_payment_shipment->type == 1 && $done_payment_shipment->charges != 0) ? $shipment->return_charges : 0);
-                            $details[$shipment->tracking_number]['fuel_surcharge'] = (($account_type_id == 1 && $done_payment_shipment->type != 2 && $done_payment_shipment->charges != 0) ? $shipment->fuel_surcharge : 0);
+                            $details[$shipment->tracking_number]['fuel_surcharge'] = (($account_type_id == 1 && $done_payment_shipment->type != 2 && $done_payment_shipment->charges != 0 && ($done_payment_shipment->type == 3 || (!$arrival_charges_applied))) ? $shipment->fuel_surcharge : 0);
                             $details[$shipment->tracking_number]['replacement_charges'] = (($account_type_id == 1 && $done_payment_shipment->type != 2 && $done_payment_shipment->charges != 0) ? $shipment->replacement_charges : 0);
                             $details[$shipment->tracking_number]['try_and_buy_charges'] = (($account_type_id == 1 && $done_payment_shipment->type != 2 && $done_payment_shipment->charges != 0) ? $shipment->try_and_buy_charges : 0);
                             $details[$shipment->tracking_number]['intercept_charges'] = (($account_type_id == 1 && $done_payment_shipment->type != 2 && $done_payment_shipment->charges != 0) ? $shipment->intercept_charges : 0);
--- app/ShipmentAdditionalCharges.php
+++ app/ShipmentAdditionalCharges.php
@@ -3,9 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Support\Facades\DB;
 class ShipmentAdditionalCharges extends Model
 {
+
+    protected $fillable = [
+        'shipment_id',
+        'faf_charges',
+        'arrival_charges_applied',
+        'zero_cod_discount_applied',
+        'return_cod_discount_applied',
+        'payment_type',
+    ];
     static function fetch_faf_charges($shipment_id){
         $shipment_additional_charges = ShipmentAdditionalCharges::where('shipment_id', $shipment_id)->latest()->first();
         if(!empty($shipment_additional_charges)){
@@ -29,22 +38,16 @@ class ShipmentAdditionalCharges extends Model
 
     static function additional_charges_apply($shipment_ids,$arrival = false ,$zero_cod = false,$return_discount =false){
         foreach ($shipment_ids as $shipment_id) {
-            $shipment_Additional_charges = ShipmentAdditionalCharges::where('shipment_id', $shipment_id)->first();
-            if(empty($shipment_Additional_charges)){
-                $shipment_Additional_charges = new  ShipmentAdditionalCharges();
-            }
-            $shipment_Additional_charges->shipment_id = $shipment_id;
-            if ($arrival) {
-                $shipment_Additional_charges->arrival_charges_applied = 1;
-            }
-            if ($zero_cod) {
-                $shipment_Additional_charges->zero_cod_discount_applied = 1;
-            }
-            if ($return_discount) {
-                $shipment_Additional_charges->return_cod_discount_applied = 1;
-            }
-            $shipment_Additional_charges->faf_charges = isset($shipment_Additional_charges->faf_charges) ? $shipment_Additional_charges->faf_charges : 0;
-            $shipment_Additional_charges->save();
+            // Use updateOrCreate to simplify the process
+            ShipmentAdditionalCharges::updateOrCreate(
+                ['shipment_id' => $shipment_id], // The condition to check if the record exists
+                array_merge(
+                    ['faf_charges' => DB::raw('IFNULL(faf_charges, 0)')], // Ensure faf_charges is set to 0 if null
+                    $arrival ? ['arrival_charges_applied' => 1] : [],
+                    $zero_cod ? ['zero_cod_discount_applied' => 1] : [],
+                    $return_discount ? ['return_cod_discount_applied' => 1] : []
+                )
+            );
         }
     }
 
