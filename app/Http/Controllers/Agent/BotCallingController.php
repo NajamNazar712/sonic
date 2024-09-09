2aec4f3f4bdadca22165eee261bcc38200249a12
Anas <anas.mazhar@trax.pk>
2024-09-09 13:42:08 +0500

    api post check

--- app/Http/Controllers/Agent/BotCallingController.php
+++ app/Http/Controllers/Agent/BotCallingController.php
@@ -138,8 +138,6 @@ class BotCallingController extends Controller
                 ];
 
                 $shipment_assign_agent = RvShipmentAssignAgent::where('shipment_id', $findShipmentId->id)->whereIn('rv_state_id', [1, 3])->latest()->first();
-                dd($shipment_assign_agent);
-
                 $request->request->add(['agent_id' => $request->admin_id, 'shipment_id' => $findShipmentId->id, 'rv_assign_agent_status_id' => null, 'rv_assign_agent_sub_status_id' => $array[$request->input]['call_finding_id'], 'rv_assign_agent_status_id' => $array[$request->input]['status_id'], 'call_count' => 1, 'call_to_id' => 1, 'call_status' => $array[$request->input]['call_status_type']]);
 
                 // if($shipment_assign_agent){
