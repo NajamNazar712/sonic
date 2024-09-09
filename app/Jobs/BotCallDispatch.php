f985c02ddeafcbd76d1fd54b250d2d71b454a280
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2024-09-09 18:05:00 +0500

    changes in log

--- app/Jobs/BotCallDispatch.php
+++ app/Jobs/BotCallDispatch.php
@@ -12,6 +12,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Log;
 
 class BotCallDispatch implements ShouldQueue
 {
