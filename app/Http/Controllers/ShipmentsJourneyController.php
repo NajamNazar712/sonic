798138eb4cda5d8d8834d4c6618e8154570e933f
Mohammad <muhammad.ahmed@trax.pk>
2024-09-10 16:51:01 +0500

    changed

--- app/Http/Controllers/ShipmentsJourneyController.php
+++ app/Http/Controllers/ShipmentsJourneyController.php
@@ -270,10 +270,10 @@ class ShipmentsJourneyController extends Controller
         if (in_array($shipper_status_id, [25, 31])) {
             $crm_request = CrmRequest::where('shipment_id', $shipment_id)->where('status_id', 2)->first();
             
-            if ($crm_request && $crm_request->case_nature_id == 1) { //against complaint only.
+            if ($crm_request && $crm_request->case_nature_id == 1 && $crm_request->case_nature_type_id == 37) { //against complaint only and case nature type is Delay in return id == 37
                 $shipperName = User::find(Shipment::where('id', $shipment_id)->select('user_id')->first()->user_id)->name;
                 
-                if ($crm_request->status_id == 2 && $crm_request->case_nature_type_id == 37) {//if crm request is in_process and case nature type is Delay in return id == 37
+                if ($crm_request->status_id == 2 ) {//if crm request is in_process
                     CrmRequest::where('id', $crm_request->id)->update([
                         'status_id' => 4 // Closed status
                     ]);
