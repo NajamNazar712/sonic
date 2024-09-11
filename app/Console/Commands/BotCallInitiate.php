f2469099a1d8e38f1b9cf58f0efbd78478a3c393
Anas <anas.mazhar@trax.pk>
2024-09-11 21:34:34 +0500

    fixes

--- app/Console/Commands/BotCallInitiate.php
+++ app/Console/Commands/BotCallInitiate.php
@@ -2,22 +2,17 @@
 
 namespace App\Console\Commands;
 
-use App\Http\Controllers\Admins\ReturnController;
 use App\Http\Controllers\Webhook\WebhookLogController;
 use App\Http\Models\Admin\GlobalSettings;
 use App\Http\Models\RvShipmentAssignAgent;
 use App\Http\Models\Shipment;
-use App\Jobs\BotCallDispatch;
 use App\RvCronLog;
-use App\RVDashboardDailyCount;
 use App\RvShipmentTicket;
 use Carbon\Carbon;
 use GuzzleHttp\Client;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
-use PDO;
 
-use function PHPSTORM_META\type;
 
 class BotCallInitiate extends Command
 {
