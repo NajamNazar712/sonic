5dec93702868505b4feb04c949f37889b2de0b17
Anas <anas.mazhar@trax.pk>
2024-09-13 18:25:56 +0500

    Zong api response is busy set the call_status is busy

--- app/Http/Controllers/Agent/BotCallingController.php
+++ app/Http/Controllers/Agent/BotCallingController.php
@@ -119,7 +119,7 @@ class BotCallingController extends Controller
                     0 => [
                         'status_id' => 6,
                         'call_finding_id' => ($request->call_status == 'ANSWER' ? 35 : 34),
-                        'call_status_type' => ($request->call_status == 'ANSWER' ? 'Connected' : 'Not Connected'),
+                        'call_status_type' => ($request->call_status == 'ANSWER' ? 'Connected' : ($request->call_status == 'BUSY' ? 'Busy' : 'Not Connected')),
                     ], // unresponsive
                     1 => [
                         'status_id' => 2,
