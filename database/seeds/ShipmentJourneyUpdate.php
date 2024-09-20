4699e36121159c14b70825eda10a9475c8d3b9b1
Anas <anas.mazhar@trax.pk>
2024-09-21 02:45:21 +0500

    Journey Rv shipment Assign agent bot calling adnmin is not isnerted

---
+++ database/seeds/ShipmentJourneyUpdate.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Models\RvShipmentAssignAgent;
+use App\Http\Models\ShipmentsJourney;
+use Illuminate\Database\Seeder;
+
+class ShipmentJourneyUpdate extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $rvAdminIds = RvShipmentAssignAgent::join('shipments_journey as sj', 'sj.shipment_id', '=', 'rv_shipment_assign_agents.shipment_id')
+            ->whereIn('sj.shipper_status_id', [13, 20, 65])
+            ->whereNull('sj.admin_id')
+            ->where('rv_shipment_assign_agents.agent_id', 4620)
+            ->limit(100)
+            ->pluck('sj.admin_id');
+        // Update sj.admin_id for the retrieved IDs
+        ShipmentsJourney::whereIn('id', $rvAdminIds)
+        ->update(['admin_id' => 4620]);
+    }
+}
