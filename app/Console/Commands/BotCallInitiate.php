9d1390997c216479602a6ea290df4aa66fc04869
Anas <anas.mazhar@trax.pk>
2024-09-23 21:22:05 +0500

    fixes

--- app/Console/Commands/BotCallInitiate.php
+++ app/Console/Commands/BotCallInitiate.php
@@ -56,8 +56,11 @@ class BotCallInitiate extends Command
     public function handle()
     {
         try {
-            $timeStart = '2024-09-23 12:59'.':00'; // Get the timestamp of two hours ago
-            $timeEnd = Carbon::parse(now())->format('Y-m-d H:i').':59';
+            // $timeEnd = Carbon::parse(now())->subHour(5)->format('Y-m-d H:i').':00'; // Get the timestamp of two hours ago
+            // $timeStart = Carbon::parse(now())->subMinute(60)->format('Y-m-d H:i').':59';
+            // dd($timeStart);
+            $timeStart = '2024-09-23 12:59' . ':00'; // Get the timestamp of two hours ago
+            $timeEnd = Carbon::parse(now())->subHour(2)->format('Y-m-d H:i') . ':59';
             
             // Now, re-initiate process for the retrieved shipment_ids after unresponsive one
             $shipmentSeconds = RvShipmentAssignAgent::where(['agent_id' => 4620, ['unresponsive_attempt_time', '>=',$timeStart], ['unresponsive_attempt_time', '<=', $timeEnd],'unresponsive_count' => 1, 'rv_assign_agent_status_id'=>6])->pluck('shipment_id');
