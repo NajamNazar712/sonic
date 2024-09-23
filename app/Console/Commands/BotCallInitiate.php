8f33023640b3f5d3859e37f7b4a6668ad6779d5e
Anas <anas.mazhar@trax.pk>
2024-09-23 20:21:26 +0500

    Bot Call initiate

--- app/Console/Commands/BotCallInitiate.php
+++ app/Console/Commands/BotCallInitiate.php
@@ -88,7 +88,7 @@ class BotCallInitiate extends Command
                             $response = json_decode($response);
                             
                             // WebhookLogController::shipment_status_log($shipment->user_id, $status_code, json_encode($response));
-                            Log::channel('cronJobLog')->info('s ' . 'Log after second call  with response' . $shipment->tracking_number);
+                            // Log::channel('cronJobLog')->info('s ' . 'Log after second call  with response' . $shipment->tracking_number);
                             WebhookLogController::zong_call_log($shipment->user_id,  $status_code, $shipmentId, 2, json_encode($response));
                         } else {
                             return json_encode(['status' => 0, 'message' => 'Shipment isn`t at the bot call prefernce']);
