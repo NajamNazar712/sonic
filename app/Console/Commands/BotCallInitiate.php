bfd38b3b99c4cf8a89cbc6adec5a1012251f499e
Anas <anas.mazhar@trax.pk>
2024-09-23 17:05:29 +0500

    Call count logs

--- app/Console/Commands/BotCallInitiate.php
+++ app/Console/Commands/BotCallInitiate.php
@@ -89,7 +89,7 @@ class BotCallInitiate extends Command
                             
                             WebhookLogController::shipment_status_log($shipment->user_id, $status_code, json_encode($response));
                             Log::channel('cronJobLog')->info('s ' . 'Log after second call  with response' . $shipment->tracking_number);
-                            // WebhookLogController::zong_call_log($shipment->user_id,  $status_code, $shipment->id, 2, json_encode($response));
+                            WebhookLogController::zong_call_log($shipment->user_id,  $status_code, $shipmentId, 2, json_encode($response));
                         } else {
                             return json_encode(['status' => 0, 'message' => 'Shipment isn`t at the bot call prefernce']);
                         }
@@ -122,8 +122,7 @@ class BotCallInitiate extends Command
                             $response = $response->getBody()->getContents();
                             $response = json_decode($response);
                             Log::channel('cronJobLog')->info('s ' . 'Log after call dispatched with response third-call' . json_encode($response));
-                            WebhookLogController::zong_call_log($shipment->user_id,  $status_code, $shipment->id,3, json_encode($response));
-                            WebhookLogController::shipment_status_log($shipment->user_id, $status_code, json_encode($response));
+                            WebhookLogController::zong_call_log($shipment->user_id,  $status_code, $shipmentId, 3, json_encode($response));
                         } else {
                             return json_encode(['status' => 0, 'message' => 'Shipment isn`t at the bot call prefernce']);
                         }
