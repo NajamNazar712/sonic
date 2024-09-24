55e9b6be45090af738800e3c998b69811b23c44e
Anas <anas.mazhar@trax.pk>
2024-09-24 13:28:30 +0500

    Fixes

--- app/Console/Commands/BotCallInitiate.php
+++ app/Console/Commands/BotCallInitiate.php
@@ -59,8 +59,8 @@ class BotCallInitiate extends Command
             
             $timeEnd = Carbon::parse(now())->subHour(2)->format('Y-m-d H:i'). ':59';
             // Carbon::parse(now())->subHour(2)->format('Y-m-d H:i') 
-            $timeStart = Carbon::parse($timeEnd)->subMinute(15)->format('Y-m-d H:i') . ':00'; // Get the timestamp of two hours ago
-            
+            $timeStart = Carbon::parse($timeEnd)->subMinute(14)->format('Y-m-d H:i') . ':00'; // Get the timestamp of two hours ago
+            dd($timeStart);
             // Now, re-initiate process for the retrieved shipment_ids after unresponsive one
             $shipmentSeconds = RvShipmentAssignAgent::where(['agent_id' => 4620, ['unresponsive_attempt_time', '>=', $timeStart], ['unresponsive_attempt_time', '<=', $timeEnd], 'unresponsive_count' => 1, 'rv_assign_agent_status_id' => 6])->pluck('shipment_id');
             Log::channel('cronJobLog')->info('s ' . 'Log after  shipmentSeconds call  record' . count($shipmentSeconds));
