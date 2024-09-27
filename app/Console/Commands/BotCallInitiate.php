3bc28bef123e0f47530f6f25b21377b96990862e
Anas <anas.mazhar@trax.pk>
2024-09-27 17:47:05 +0500

    Job logs

--- app/Console/Commands/BotCallInitiate.php
+++ app/Console/Commands/BotCallInitiate.php
@@ -64,7 +64,7 @@ class BotCallInitiate extends Command
             $timeStart = Carbon::parse($timeEnd)->subMinute(14)->format('Y-m-d H:i') . ':00'; // Get the timestamp of two hours ago
            
             // Now, re-initiate process for the retrieved shipment_ids after unresponsive one
-            $shipmentSeconds = RvShipmentAssignAgent::where(['agent_id' => 4620, ['unresponsive_attempt_time', '>=', '2024-09-27 15:00:00'], ['unresponsive_attempt_time', '<=', '2024-09-27 15:16:59'], 'unresponsive_count' => 1, 'rv_assign_agent_status_id' => 6])->pluck('shipment_id');
+            $shipmentSeconds = RvShipmentAssignAgent::where(['agent_id' => 4620, ['unresponsive_attempt_time', '>=', $timeStart], ['unresponsive_attempt_time', '<=', $timeEnd], 'unresponsive_count' => 1, 'rv_assign_agent_status_id' => 6])->pluck('shipment_id');
             Log::channel('botCallJobLog')->info('s ' . 'Log after  shipmentSeconds call  record' . count($shipmentSeconds) .'Start time'. $timeStart .'End Time'. $timeEnd);
             
             // dd($shipmentSeconds);
@@ -114,7 +114,7 @@ class BotCallInitiate extends Command
 
             }
             
-            $shipmentThirds = RvShipmentAssignAgent::where(['agent_id' => 4620,['unresponsive_attempt_time', '>=', '2024-09-27 10:00:00'], ['unresponsive_attempt_time', '<=', '2024-09-27 15:15:59'], 'unresponsive_count' => 2, 'rv_assign_agent_status_id' => 6])->pluck('shipment_id');
+            $shipmentThirds = RvShipmentAssignAgent::where(['agent_id' => 4620, ['unresponsive_attempt_time', '>=', $timeStart], ['unresponsive_attempt_time', '<=', $timeEnd], 'unresponsive_count' => 2, 'rv_assign_agent_status_id' => 6])->pluck('shipment_id');
             Log::channel('botCallJobLog')->info('s ' . 'Log after  shipmentThirds call  record' . count($shipmentThirds));
 
             $third_count =1;
