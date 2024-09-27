abf85c333909c37637c57fde6dd3a728019e50d7
Anas <anas.mazhar@trax.pk>
2024-09-27 16:34:43 +0500

    Bot Call second dispatch open

--- app/Console/Commands/BotCallInitiate.php
+++ app/Console/Commands/BotCallInitiate.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Webhook\WebhookLogController;
 use App\Http\Models\Admin\GlobalSettings;
 use App\Http\Models\RvShipmentAssignAgent;
 use App\Http\Models\Shipment;
+use App\Jobs\BotCallDispatchSecod;
 use App\RvCronLog;
 use App\RvShipmentTicket;
 use Carbon\Carbon;
@@ -63,7 +64,7 @@ class BotCallInitiate extends Command
            
             // Now, re-initiate process for the retrieved shipment_ids after unresponsive one
             $shipmentSeconds = RvShipmentAssignAgent::where(['agent_id' => 4620, ['unresponsive_attempt_time', '>=', $timeStart], ['unresponsive_attempt_time', '<=', $timeEnd], 'unresponsive_count' => 1, 'rv_assign_agent_status_id' => 6])->pluck('shipment_id');
-            Log::channel('cronJobLog')->info('s ' . 'Log after  shipmentSeconds call  record' . count($shipmentSeconds) .'Start time'. $timeStart .'End Time'. $timeEnd);
+            Log::channel('botCallJobLog')->info('s ' . 'Log after  shipmentSeconds call  record' . count($shipmentSeconds) .'Start time'. $timeStart .'End Time'. $timeEnd);
             
             // dd($shipmentSeconds);
             //  Now, re-initiate process for the retrieved shipment_ids after unresponsive two            
@@ -71,39 +72,40 @@ class BotCallInitiate extends Command
             // $second_count = 1;
             if (count($shipmentSeconds) > 0) {            
                 foreach($shipmentSeconds as $shipmentId){
-                    
 
-                    if (GlobalSettings::where(['type' => 'bot_call_enable_disable', 'setting_value' => 1])->exists()) {
-                        if (RvShipmentTicket::where('shipment_id', $shipmentId)->whereNull('deleted_at')->where('is_bot', 1)->exists() && Shipment::whereIn('shipper_status_id', [12, 52, 66])->where('id', $shipmentId)->exists()) {
-                            $base_uri = 'https://cap.zong.com.pk:8444/vpbx-apis/roboCalls/outboundCall';
-                            RvShipmentTicket::where('shipment_id', $shipmentId)->update(['in_progress' => 1]);
-                            $shipment = Shipment::with(['user:id,name,brand_name'])->select('user_id', 'consignee_phone_number_1', 'consignee_name', 'tracking_number', 'amount')->find($shipmentId);
-                            $post = [
-                                'vpbx_id' => '66bdfd18cb67f',
-                                'caller_id' => preg_replace("/[^a-zA-Z0-9]+/", "", $shipment->consignee_phone_number_1),
-                                'tracking_number' => $shipment->tracking_number,
-                                'cod_amount' => $shipment->amount,
-                                'brand_name' => $shipment->user->name ?? $shipment->user->brand_name,
-                                'customer_name' => $shipment->consignee_name,
-                            ];
-                            $client = new Client(['base_uri' => $base_uri, 'http_errors' => FALSE, 'connect_timeout' => 60, 'timeout' => 60, 'verify' => false]);
-                            $response = $client->post('', [
-                                    'json' => $post
-                                ]);
-                            $status_code = $response->getStatusCode();
-                            $response = $response->getBody()->getContents();
-                            $response = json_decode($response);
-                            Log::channel('botCallJobLog')->info('s ' . 'Log after second call  record' . $shipmentId);
-                            // $second_count++;    
-                            // WebhookLogController::shipment_status_log($shipment->user_id, $status_code, json_encode($response));
-                            // Log::channel('cronJobLog')->info('s ' . 'Log after second call  with response' . $shipment->tracking_number);
-                            WebhookLogController::zong_call_log($shipment->user_id,  $status_code, $shipmentId, 2, json_encode($response));
-                        } else {
-                            Log::channel('botCallJobLog')->info('s ' . 'Log after second call not recorded' . $shipmentId);
+                    dispatch(new BotCallDispatchSecod($shipmentId));
 
-                            return json_encode(['status' => 0, 'message' => 'Shipment isn`t at the bot call prefernce']);
-                        }
-                    }
+                    // if (GlobalSettings::where(['type' => 'bot_call_enable_disable', 'setting_value' => 1])->exists()) {
+                    //     if (RvShipmentTicket::where('shipment_id', $shipmentId)->whereNull('deleted_at')->where('is_bot', 1)->exists() && Shipment::whereIn('shipper_status_id', [12, 52, 66])->where('id', $shipmentId)->exists()) {
+                    //         $base_uri = 'https://cap.zong.com.pk:8444/vpbx-apis/roboCalls/outboundCall';
+                    //         RvShipmentTicket::where('shipment_id', $shipmentId)->update(['in_progress' => 1]);
+                    //         $shipment = Shipment::with(['user:id,name,brand_name'])->select('user_id', 'consignee_phone_number_1', 'consignee_name', 'tracking_number', 'amount')->find($shipmentId);
+                    //         $post = [
+                    //             'vpbx_id' => '66bdfd18cb67f',
+                    //             'caller_id' => preg_replace("/[^a-zA-Z0-9]+/", "", $shipment->consignee_phone_number_1),
+                    //             'tracking_number' => $shipment->tracking_number,
+                    //             'cod_amount' => $shipment->amount,
+                    //             'brand_name' => $shipment->user->name ?? $shipment->user->brand_name,
+                    //             'customer_name' => $shipment->consignee_name,
+                    //         ];
+                    //         $client = new Client(['base_uri' => $base_uri, 'http_errors' => FALSE, 'connect_timeout' => 60, 'timeout' => 60, 'verify' => false]);
+                    //         $response = $client->post('', [
+                    //                 'json' => $post
+                    //             ]);
+                    //         $status_code = $response->getStatusCode();
+                    //         $response = $response->getBody()->getContents();
+                    //         $response = json_decode($response);
+                    //         Log::channel('botCallJobLog')->info('s ' . 'Log after second call  record' . $shipmentId);
+                    //         // $second_count++;    
+                    //         // WebhookLogController::shipment_status_log($shipment->user_id, $status_code, json_encode($response));
+                    //         // Log::channel('cronJobLog')->info('s ' . 'Log after second call  with response' . $shipment->tracking_number);
+                    //         WebhookLogController::zong_call_log($shipment->user_id,  $status_code, $shipmentId, 2, json_encode($response));
+                    //     } else {
+                    //         Log::channel('botCallJobLog')->info('s ' . 'Log after second call not recorded' . $shipmentId);
+
+                    //         return json_encode(['status' => 0, 'message' => 'Shipment isn`t at the bot call prefernce']);
+                    //     }
+                    // }
                 }
             }
             
