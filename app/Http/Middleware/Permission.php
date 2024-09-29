b59a0e536793cf9b9d908b0d59aba2c8a5d72cbd
Anas <anas.mazhar@trax.pk>
2024-09-29 15:56:53 +0500

    Bot Call Report working completed

--- app/Http/Middleware/Permission.php
+++ app/Http/Middleware/Permission.php
@@ -1628,7 +1628,7 @@ class Permission
 
             'crm.bulk_claim.index' => 19,
 
-            'admin.reports.rvr_reattempt.index' => 1010
+            'admin.reports.bot_rvr.index' => 1010
 
         ]
     ];
