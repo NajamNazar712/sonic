c53978f486f448334342360ba9f6eacaf8a3031a
Faraz <khokhar.faraz.a@gmail.com>
2024-10-07 18:55:26 +0500

    check admin image address in logs;

--- app/Http/Controllers/Admins/Logistic/AdminLogisticBookingController.php
+++ app/Http/Controllers/Admins/Logistic/AdminLogisticBookingController.php
@@ -508,7 +508,7 @@ class AdminLogisticBookingController extends Controller
                 }
 
                 Log::channel('code_test_log')->error('batch_id = > '.$batch_id.' booking_img_url '.$booking_img_url);
-                $imageUrl = Storage::disk('s3')->url('sonic_staging/logistic_booking1/168_1728304014.png');
+                $imageUrl = Storage::disk('s3')->url('logistic_booking1/168_1728304014.png');
 
                 //return view('admin.logistic.edit_logistic_book', compact('imageUrl'));
                 return view('admin.logistic.edit_logistic_book')
