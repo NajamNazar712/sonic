4d981ad9937434a7cd0f1b4349593daa72534e21
Faraz <khokhar.faraz.a@gmail.com>
2024-10-08 13:16:32 +0500

    commit storage s3 validation if exist or not

--- app/Http/Controllers/Admins/Logistic/AdminLogisticBookingController.php
+++ app/Http/Controllers/Admins/Logistic/AdminLogisticBookingController.php
@@ -504,11 +504,10 @@ class AdminLogisticBookingController extends Controller
                 $booking_img_url=null;
                 if ($booking_img)
                 {
+                    $booking_img_url =Storage::disk('public')->url('logistic_bookings/'. $booking_img->image_name);
                     if (Storage::disk('s4')->exists('logistic_bookings/'. $booking_img->image_name)) {
                         $booking_img_url =  Storage::disk('s4')->url('logistic_bookings/'. $booking_img->image_name);
-                    } else {
-                        $booking_img_url =  Storage::disk('public')->url('logistic_bookings/'. $booking_img->image_name);
-                    }
+                    } 
                     
                 }
 
