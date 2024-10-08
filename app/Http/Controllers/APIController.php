72b7b720686eebfdcd937757351a07b0067d1800
Mohammad <muhammad.ahmed@trax.pk>
2024-10-08 10:50:11 +0500

    Limit set to 150

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -1587,7 +1587,7 @@ class APIController extends Controller
             ];
 
             foreach ($timeLimits as $range) {
-                if ($count >= $range['min'] && $count < $range['max'] && $minutesDiff < $range['limit']) {
+                if ($count >= $range['min'] && $count <= $range['max'] && $minutesDiff < $range['limit']) {
                     $timeLimit = $range['seconds'];
                     break;
                 }
