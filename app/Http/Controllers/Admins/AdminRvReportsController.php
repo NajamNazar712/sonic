ff6303c7d004d94ed5e64ac6cf630c9ad310bf59
Anas <anas.mazhar@trax.pk>
2024-10-09 19:20:43 +0500

    Rv bot log group by add

--- app/Http/Controllers/Admins/AdminRvReportsController.php
+++ app/Http/Controllers/Admins/AdminRvReportsController.php
@@ -98,7 +98,8 @@ class AdminRvReportsController extends Controller
             'azl.call_date_time as call_start_date',
             'azl.api_request as api_request',
             'azl.error as message',
-            'azl.created_at as date_time');
+            'azl.created_at as date_time')
+            ->groupBy('api_call_logs.id');
         
             $datatable = Datatables::of($rv_call_logs)
             ->editColumn('tracking_number', function ($rv_call_logs) {
