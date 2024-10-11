1ca37099477ee23b4d45dde0210066ba0b8b4bb2
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2024-10-11 16:36:59 +0500

    changes

--- database/migrations/2024_10_07_160305_apizonglogaddcolumn.php
+++ database/migrations/2024_10_07_160305_apizonglogaddcolumn.php
@@ -17,6 +17,10 @@ class Apizonglogaddcolumn extends Migration
         Schema::table('api_zong_logs', function (Blueprint $table) {
             $table->integer('shipment_id')->index()->nullable();
         });
+
+        Schema::table('api_zong_logs', function (Blueprint $table) {
+            $table->timestamp('updated_at')->nullable();
+        });
     }
 
     /**
@@ -29,6 +33,9 @@ class Apizonglogaddcolumn extends Migration
         //
         Schema::table('api_zong_logs', function (Blueprint $table) {
             $table->dropColumn('shipment_id');
+        });
+        Schema::table('api_zong_logs', function (Blueprint $table) {
+            $table->dropColumn('updated_at');
         });
     }
 }
