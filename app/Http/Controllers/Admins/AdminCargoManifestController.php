416483c11ccc37f53b502692d1d4645ac8c5b189
Anas <anas.mazhar@trax.pk>
2024-10-14 16:39:38 +0500

    Replica reproduce production

--- app/Http/Controllers/Admins/AdminCargoManifestController.php
+++ app/Http/Controllers/Admins/AdminCargoManifestController.php
@@ -3127,18 +3127,14 @@ class AdminCargoManifestController extends Controller
 
     public static function print($cargo_manifest_ids, $type = NULL)
     {
-        // $minimalHtml = '<h1>Hello World</h1>';
-        // // $pdf = FacadesSnappyPdf::loadHTML($minimalHtml)->save(public_path('reports/anas'. rand(1, 100) .'.pdf'));
-        // // return $pdf;
-        // $pdfFilePath = public_path('reports/anas'. rand(1, 100) .'.pdf');
-
-        // $pdf = FacadesSnappyPdf::loadHTML($pdfFilePath)->save($pdfFilePath);
-
-        // // Return a response to open the PDF in a new tab
-        // return Response::make(file_get_contents($pdfFilePath), 200, [
-        //     'Content-Type' => 'application/pdf',
-        //     'Content-Disposition' => 'inline; filename="' . basename($pdfFilePath) . '"',
-        // ]);
+        $minimalHtml = '<h1>Hello World</h1>';
+        $pdf = FacadesSnappyPdf::loadHTML($minimalHtml)->save(public_path('reports/anas'. rand(1, 100) .'.pdf'));
+        return Response::make(file_get_contents($pdf), 200, [
+            'Content-Type' => 'application/pdf',
+            'Content-Disposition' => 'inline; filename="' . basename($pdf) . '"',
+        ]);
+        // Return a response to open the PDF in a new tab
+       
         $generator = new \Picqer\Barcode\BarcodeGeneratorPNG();
         $html = '
                 <!doctype html>
