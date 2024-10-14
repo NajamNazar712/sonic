ea0c1003d59d0dbe9425011aea46e891a8d63484
Anas <anas.mazhar@trax.pk>
2024-10-14 18:44:17 +0500

    Test for the pdf

--- app/Http/Controllers/Admins/AdminCargoManifestController.php
+++ app/Http/Controllers/Admins/AdminCargoManifestController.php
@@ -3106,9 +3106,9 @@ class AdminCargoManifestController extends Controller
         foreach ($success_cargo_ids as $cargo_id) {
             $cargo_array = array($cargo_id);
             $cargo_array = implode(',', $cargo_array);
-            //$path = self::print($cargo_array, 1);
+            $path = self::print($cargo_array, 1);
             //dd($path);
-            //$manifest = CargoManifest::find($cargo_id);
+            $manifest = CargoManifest::find($cargo_id);
             //NotificationsController::send(148, $manifest->destination_hub_id, url('/') . '/' . 'reports/cargo_manifest_' . str_pad($manifest->id, 6, '0', STR_PAD_LEFT) . '.pdf');
         }
 
