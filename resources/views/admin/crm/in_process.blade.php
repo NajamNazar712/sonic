bfe8d93715b273fe9fa89580637fc0511d366f4f
Areeb-Trax <muhammad.areeb@trax.pk>
2024-10-14 16:56:14 +0500

    Reversions for some JS code

--- resources/views/admin/crm/in_process.blade.php
+++ resources/views/admin/crm/in_process.blade.php
@@ -666,18 +666,13 @@
 
                             table.rows().nodes().each(function(index) {
                                 var row = table.row(index);
-
-                                if ($(row.node().firstChild).hasClass('column_checkbox')) {
+                                if ($(row.node().firstChild).hasClass('select-checkbox')) {
                                     row.select();
-
                                     id = parseInt(row.id());
-
                                     var index = $.inArray(id, selected_rows);
-
                                     if (index === -1) {
                                         selected_rows.push(id);
                                     }
-
                                     table.button('.assign').enable();
                                     table.button('.close_request').enable();
                                     table.button('.in_valid').enable();
@@ -699,18 +694,13 @@
 
                             table.rows().nodes().each(function(index) {
                                 var row = table.row(index);
-
-                                if ($(row.node().firstChild).hasClass('column_checkbox')) {
+                                if ($(row.node().firstChild).hasClass('select-checkbox')) {
                                     row.deselect();
-
                                     id = parseInt(row.id());
-
                                     var index = $.inArray(id, selected_rows);
-
                                     if (index !== -1) {
                                         selected_rows.splice(index, 1);
                                     }
-
                                     if (selected_rows.length == 0) {
                                         table.button('.assign').disable();
                                         table.button('.close_request').disable();
@@ -760,17 +750,7 @@
                 rowId: 'id',
                 order: [[32, 'desc']],
                 columns: [
-                    // {data: 'id', orderable: false, searchable: false, class: 'text-center align-middle select p-1', targets: 0, render: function (data, type, row) {return '';}},
-                    { 
-                        data: 'id', 
-                        orderable: false, 
-                        searchable: false, 
-                        class: '', 
-                        targets: 0, 
-                        render: function (data, type, row) {
-                            return '<input type="checkbox" class="text-center align-middle select p-1 select-checkbox" data-id="' + row.id + '">';
-                        }
-                    },
+                    {data: 'id', orderable: false, searchable: false, class: 'text-center align-middle select p-1', targets: 0, render: function (data, type, row) {return '';}},
                     {orderable: false, searchable: false, name: 'serial_number', class: 'align-middle serial_number', targets: 0, render: function (data, type, row) {return '';}},
                     {data: 'id_padded_link', name: 'crm_requests.id', class: 'align-middle id_padded_link'},
                     {data: 'tracking_number_hyperlink', name: 's.tracking_number', class: 'align-middle tracking_number'}, // Tracking No.
@@ -816,9 +796,7 @@
 
                 ],
                 rowCallback: function(row, data, index) {
-                    // $('td:eq(0)', row).addClass('select-checkbox');
-                    $('td:eq(0)', row).addClass('text-center align-middle select p-1 column_checkbox');
-
+                    $('td:eq(0)', row).addClass('select-checkbox');
                     if ($.inArray(data.id, selected_rows) !== -1) {
                         table.row(row).select();
                     }
@@ -1288,7 +1266,7 @@
             });
             
 
-            $('#datatable tbody').on('click', 'tr td.column_checkbox', function() {
+            $('#datatable tbody').on('click', 'tr td.select-checkbox', function() {
                 var id = parseInt($(this).parent('tr').attr('id'));
 
                 var index = $.inArray(id, selected_rows);
@@ -1712,75 +1690,6 @@
                 $('#star_shippers_filter').val(1);
                 table.draw(true);
                 $('#star_shippers_filter').val(0);
-            });
-
-            // Check agent assigned or not
-            $('#datatable').on('click', '.column_checkbox', function(e) {
-                e.stopPropagation();
-                var $checkbox = $(this).find('.select-checkbox');
-                var crmRequestId = $checkbox.data('id') || $(this).closest('tr').attr('id');
-                var isChecked = !$checkbox.prop('checked');
-                $checkbox.prop('checked', isChecked);
-
-                $.ajax({
-                    type: "POST",
-                    url: "{{ route('admin.crm.check_agent') }}",
-                    data: {
-                        crm_request_id: crmRequestId,
-                        _token: '{{ csrf_token() }}'
-                    },
-                    success: function(response) {
-                        if (response.status === 1 && response.error) {
-                            scan_sound(2);
-                            toastr.error(response.error, 'Error!', {
-                                positionClass: 'toast-top-center',
-                                containerId: 'toast-top-center'
-                            });
-                            $checkbox.prop('checked', !isChecked);
-                        }
-                    },
-                    error: function() {
-                        toastr.error('An error occurred while processing your request.', 'Error!', {
-                            positionClass: 'toast-top-center',
-                            containerId: 'toast-top-center'
-                        });
-                        $checkbox.prop('checked', !isChecked);
-                    }
-                });
-            });
-
-            $('#datatable').on('click', '.select-checkbox', function(e) {
-                e.stopPropagation();
-                var $checkbox = $(this);
-                var crmRequestId = $checkbox.data('id') || $checkbox.closest('tr').attr('id');
-                var isChecked = !$checkbox.prop('checked');
-                $checkbox.prop('checked', isChecked);
-
-                $.ajax({
-                    type: "POST",
-                    url: "{{ route('admin.crm.check_agent') }}",
-                    data: {
-                        crm_request_id: crmRequestId,
-                        _token: '{{ csrf_token() }}'
-                    },
-                    success: function(response) {
-                        if (response.status === 1 && response.error) {
-                            scan_sound(2);
-                            toastr.error(response.error, 'Error!', {
-                                positionClass: 'toast-top-center',
-                                containerId: 'toast-top-center'
-                            });
-                            $checkbox.prop('checked', !isChecked);
-                        }
-                    },
-                    error: function() {
-                        toastr.error('An error occurred while processing your request.', 'Error!', {
-                            positionClass: 'toast-top-center',
-                            containerId: 'toast-top-center'
-                        });
-                        $checkbox.prop('checked', !isChecked);
-                    }
-                });
             });
         });
     </script>
