58092380edad7c9f4082604ce1c6909178dccce8
Anas <anas.mazhar@trax.pk>
2024-10-15 19:44:07 +0500

    Redirect back add

--- app/Http/Controllers/Admins/AdminFinanceController.php
+++ app/Http/Controllers/Admins/AdminFinanceController.php
@@ -7330,10 +7330,8 @@ use Illuminate\Support\Str;class AdminFinanceController extends Controller
     {
 
         $requestPendingShipmentIds = explode(',', $request->pending_payment_shipment_ids);
-        $existingShipmentIds = MakePaymentTempTable::whereIn('pending_payment_shipment_id', $requestPendingShipmentIds)->pluck('pending_payment_shipment_id')->toArray();        
-        
+        $existingShipmentIds = MakePaymentTempTable::whereIn('pending_payment_shipment_id', $requestPendingShipmentIds)->pluck('pending_payment_shipment_id')->toArray();
         $idsToInsert = array_diff($requestPendingShipmentIds, $existingShipmentIds);
-        
         $final_Array = [];
         if (!empty($idsToInsert)) {
             foreach ($idsToInsert as $pending_payment_shipment_id) {
@@ -7379,7 +7377,6 @@ use Illuminate\Support\Str;class AdminFinanceController extends Controller
                 $selected_shipments = count($pending_payment_shipment_ids);
 
                 $pending_payment = PendingPayment::find($pending_payment_id);
-               
                 if ($pending_payment) {
                     $user_bank_id = NULL;
 
@@ -7649,7 +7646,7 @@ use Illuminate\Support\Str;class AdminFinanceController extends Controller
 
             return redirect()->back()->with(['success' => 'Payment(s) has been Made.', 'print' => $done_payment_ids]);
         }else{
-            return redirect()->with(['success' => 'Given Ids Already Processed']);
+            return redirect()->back()->with(['success' => 'Given Ids Already Processed']);
         }
     }
 
