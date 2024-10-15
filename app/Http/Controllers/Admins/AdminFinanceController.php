d14d09b4ec836c685d6166bdf6ab1c41f5372895
Anas <anas.mazhar@trax.pk>
2024-10-15 15:44:08 +0500

    Make payment logs add

--- app/Http/Controllers/Admins/AdminFinanceController.php
+++ app/Http/Controllers/Admins/AdminFinanceController.php
@@ -7331,6 +7331,8 @@ use Illuminate\Support\Str;class AdminFinanceController extends Controller
 
         $requestPendingShipmentIds = explode(',', $request->pending_payment_shipment_ids);
         $existingShipmentIds = MakePaymentTempTable::whereIn('pending_payment_shipment_id', $requestPendingShipmentIds)->pluck('pending_payment_shipment_id')->toArray();
+        
+        Log::channel('cronJobLog')->info('s ' . 'ShipmentID pending payment' . Carbon::parse(now())->format('Y-m-d H:i:s') . ' shipment_id ' . $requestPendingShipmentIds);
         $idsToInsert = array_diff($requestPendingShipmentIds, $existingShipmentIds);
         $final_Array = [];
         if (!empty($idsToInsert)) {
