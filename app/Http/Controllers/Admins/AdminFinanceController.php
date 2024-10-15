578d8f990db2cd0583ed8e4e1e44a12fd6c50d71
Anas <anas.mazhar@trax.pk>
2024-10-15 17:09:29 +0500

    Rv cron log add for the payment

--- app/Http/Controllers/Admins/AdminFinanceController.php
+++ app/Http/Controllers/Admins/AdminFinanceController.php
@@ -7333,11 +7333,6 @@ use Illuminate\Support\Str;class AdminFinanceController extends Controller
         $existingShipmentIds = MakePaymentTempTable::whereIn('pending_payment_shipment_id', $requestPendingShipmentIds)->pluck('pending_payment_shipment_id')->toArray();        
         
         $idsToInsert = array_diff($requestPendingShipmentIds, $existingShipmentIds);
-        if($request->admin_id == 2471){
-            RvCronLog::create([
-                'message' => 'existingShipmentIds '. json_encode($existingShipmentIds) . 'idsToInsert '. json_encode($idsToInsert)
-            ]);
-        }
         $final_Array = [];
         if (!empty($idsToInsert)) {
             foreach ($idsToInsert as $pending_payment_shipment_id) {
