ff771f89b70bf9c01819ee53c32e1a5fa1f60a16
Areeb-Trax <muhammad.areeb@trax.pk>
2024-10-17 14:32:05 +0500

    Added newly added bag statuses

--- app/Http/Controllers/AdminShipmentHandoverController.php
+++ app/Http/Controllers/AdminShipmentHandoverController.php
@@ -329,7 +329,8 @@ class AdminShipmentHandoverController extends Controller
           /* 18, */ 20, 21, 22, 23, 24, 25, 26, 
           27, 28, 29, 30, 31, 32, 33, 34,
           35, 36, 37, 38, 44, 45, 46, 47,
-          48, /* 51, */ 56, 57
+          48, /* 51, */ 56, 57, 
+          69, 70, 72, 73, 75, 76
         ];
 
         $shipment_type = ShipmentsJourney::select('id', 'shipment_id', 'shipper_status_id')
@@ -519,7 +520,7 @@ class AdminShipmentHandoverController extends Controller
         20, 21, 22, 23, 24, 25, 26,
         27, 28, 29, 30, 31, 32, 33, 34,
         35, 36, 37, 38, 44, 45, 46, 47,
-        48, 56, 57
+        48, 56, 57, 69, 70, 72, 73, 75, 76
     ];
     
     $normal_status_ids_str = implode(',', $normal_status_ids);
@@ -1279,7 +1280,8 @@ class AdminShipmentHandoverController extends Controller
           /* 18, */ 20, 21, 22, 23, 24, 25, 26, 
           27, 28, 29, 30, 31, 32, 33, 34,
           35, 36, 37, 38, 44, 45, 46, 47,
-          48, /* 51, */ 56, 57
+          48, /* 51, */ 56, 57, 
+          69, 70, 72, 73, 75, 76
       ];
 
       $normal_status_ids_str = implode(',', $normal_status_ids);
@@ -1373,7 +1375,8 @@ class AdminShipmentHandoverController extends Controller
         /* 18, */ 20, 21, 22, 23, 24, 25, 26, 
         27, 28, 29, 30, 31, 32, 33, 34,
         35, 36, 37, 38, 44, 45, 46, 47,
-        48, /* 51, */ 56, 57
+        48, /* 51, */ 56, 57,
+        69, 70, 72, 73, 75, 76
       ];
 
       // normal shipments
