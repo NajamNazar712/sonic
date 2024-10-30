a1c51e1569ae10517e6894d269db9999739a4dde
Areeb-Trax <muhammad.areeb@trax.pk>
2024-10-30 18:44:26 +0500

    selecting the lastest handover id and checking if bag made or not

--- app/Http/Controllers/AdminShipmentHandoverController.php
+++ app/Http/Controllers/AdminShipmentHandoverController.php
@@ -1721,16 +1721,17 @@ class AdminShipmentHandoverController extends Controller
     public function check_handover_bag_receive(Request $request)
     {
       $shipent_id = Shipment::where('tracking_number', $request->tracking_number)
-      ->select(['id'])
-      ->first()->id;
+        ->select(['id'])
+        ->first()->id;
 
       $exisiting_handover_id = HandoverShipments::where('shipment_id', $shipent_id)
-      ->first()
-      ->handover_id;
+        ->orderBy('id', 'desc')
+        ->first()
+        ->handover_id;
 
       $existing_bag = Handover::where('id', $exisiting_handover_id)
-      ->select('bag_number')
-      ->first()->bag_number;
+        ->select('bag_number')
+        ->first()->bag_number;
 
       if ($existing_bag != null)
       {
