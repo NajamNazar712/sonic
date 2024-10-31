29389ee6f46b814710a56339fca260715e68e565
Anas <anas.mazhar@trax.pk>
2024-10-31 15:57:07 +0500

    changes of the condition caller_Agent_type 2

--- app/Http/Traits/RvTrait.php
+++ app/Http/Traits/RvTrait.php
@@ -1533,7 +1533,7 @@ trait RvTrait
                     }
                     else if($agent->agent_caller_type == 2)//These Agents will get shipments pending with second call only
                     {
-                        return $query->where('call_count' , '>', 0)->where('created_at', '<', Carbon::now()->subDays(2));
+                        return $query->where('call_count' , '>', 0)->whereDate('created_at','2024-10-26');
                     }
                     else
                     {
