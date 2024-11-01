fd37cb8fe84b1c2606a9f6ac2369c02c2049bc64
Anas <anas.mazhar@trax.pk>
2024-11-01 19:13:24 +0500

    Reattempt anylsis reports

--- app/Http/Controllers/Admins/AdminReportsController.php
+++ app/Http/Controllers/Admins/AdminReportsController.php
@@ -13146,7 +13146,7 @@ class AdminReportsController extends Controller
         ->join('rv_shipment_assign_agents', 'rv_shipment_assign_agents.id', 'rv_shipment_assign_agent_details.rv_shipment_assign_agent_id')
         ->join('shipments_journey as sj', function($join) {
             $join->on('sj.shipment_id', '=', 'rv_shipment_assign_agent_details.shipment_id')
-                 ->where('sj.shipper_status_id', '=', 66);
+                 ->where('sj.shipper_status_id', '=', 13);
         })
         ->leftjoin('users', 'shipments.user_id', 'users.id')
         ->leftjoin('user_shipping_infos as uso', 'shipments.pickup_address_id', 'uso.id')
