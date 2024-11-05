3f07156837837ac6356d4a4e454407d39e7add6f
usamauddin <usama.uddin@trax.pk>
2024-11-06 03:58:38 +0500

    apollo work done but migration issue in shipment_additinal_column

---
+++ database/seeds/ApolloCronJobLogsSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ApolloCronJobLogsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \Illuminate\Support\Facades\DB::table('apollo_cron_job_logs')->insert([
+            [
+                'id' => 1,
+                'job_name' => 'apollo:fetch-shipments-status',
+                'last_run_time' => null,  // Set to `null` initially or use `Carbon::now()` if needed
+                'created_at' => \Carbon\Carbon::now(),
+                'updated_at' => \Carbon\Carbon::now(),
+            ]
+        ]);
+    }
+}
