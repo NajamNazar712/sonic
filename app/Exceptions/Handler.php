9212b557878fb9aa338631b3caee55940a12edaa
Najam <muhammad.najam@trax.pk>
2024-11-07 13:42:01 +0500

    upgraded to 7 and application is working

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,7 @@
 
 namespace App\Exceptions;
 
-use Exception;
+use Throwable;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Support\Arr;
 
@@ -36,7 +36,7 @@ class Handler extends ExceptionHandler
      * @param  \Exception  $exception
      * @return void
      */
-    public function report(Exception $exception)
+    public function report(Throwable  $exception)
     {
         parent::report($exception);
     }
@@ -48,7 +48,7 @@ class Handler extends ExceptionHandler
      * @param  \Exception  $exception
      * @return \Illuminate\Http\Response
      */
-    public function render($request, Exception $exception)
+    public function render($request, Throwable $exception)
     {
         $class = get_class($exception);
         switch ($class){
