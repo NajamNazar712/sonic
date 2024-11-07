84d8b270befbcb010ed7e2b39c435431a8dfa0a9
Anas <anas.mazhar@trax.pk>
2024-11-07 17:29:59 +0500

    rv_pool_update time set old date

--- app/Console/Commands/AgentChangeStatus.php
+++ app/Console/Commands/AgentChangeStatus.php
@@ -63,11 +63,18 @@ class AgentChangeStatus extends Command
             //Query for Making Shipments Enable again in Get Tickets After their "Unresponsive" Status is Submitted.
             $shipments = RvShipmentTicket::where('in_progress', 1)->where('is_bot',0)->get();
             // ->update(['in_progress' => 0,'rv_shipment_tickets.updated_at'=> 'rv_shipment_tickets.created_ats']);
-             foreach($shipments as $shipment){
-                  $shipment->in_progress = 0;
-                  $shipment->updated_at = $shipment->created_at;
-                  $shipment->save();
-             }
+            foreach($shipments as $shipment){
+                $shipment->in_progress = 0;
+                $shipment->updated_at = $shipment->created_at;
+                $shipment->save();
+            }
+            
+            $upshipments = RvShipmentTicket::whereDate('created_at','>','2024-11-06')->where('in_progress', 0)->where('is_bot',0)->get();
+            // ->update(['in_progress' => 0,'rv_shipment_tickets.updated_at'=> 'rv_shipment_tickets.created_ats']);
+            foreach($upshipments as $shipment){
+                $shipment->updated_at = $shipment->created_at;
+                $shipment->save();
+            }
 
             //Make record of return/dashboard cards count daily to mantain history
             if(RVDashboardDailyCount::whereDate('created_at', Carbon::today())->doesntExist()) //Max 1 record should be created each day
