742b697e9d2532a03435125f6e9791f35029610e
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2024-11-11 18:50:03 +0500

    changes

--- app/Console/Commands/RevenueReportLastMonth.php
+++ app/Console/Commands/RevenueReportLastMonth.php
@@ -43,12 +43,12 @@ class RevenueReportLastMonth extends Command
         //from 1st to last of month
         $response = AdminRevenueReportsController::revenue_report_last_month(1);
         echo json_encode($response);
-
-        $response2 = AdminRevenueReportsController::revenue_report_last_month(2);
-        echo json_encode($response2);
-
-        $response3 = AdminRevenueReportsController::revenue_report_last_month(3); // Changed to 3
-        echo json_encode($response3);
+//
+//        $response2 = AdminRevenueReportsController::revenue_report_last_month(2);
+//        echo json_encode($response2);
+//
+//        $response3 = AdminRevenueReportsController::revenue_report_last_month(3); // Changed to 3
+//        echo json_encode($response3);
 
     }
 }
