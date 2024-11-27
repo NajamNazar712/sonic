d1cc948130ac5ad64172946e39cf45e6e86d2026
Mohammad <muhammad.ahmed@trax.pk>
2024-11-27 15:46:47 +0500

    message set

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -2524,7 +2524,7 @@ class APIController extends Controller
             }
         }
         $errors = [];
-
+        $validData = [];
 
         foreach ($request->data as $key2 => $row) {
             $key_inc = $key2 + 1;
@@ -2542,16 +2542,19 @@ class APIController extends Controller
                         }
                     }
                 }
+            } else {
+                $validData[] = $row;
             }
         }
-        if(!empty($errors)){
+
+        if(!empty($errors) && empty($validData)){
             return response()->json(['status' => 1, 'message' => 'Error(s) in Input', 'errors' => $errors]);
         }
 
-        if(empty($errors))
+        if(!empty($validData))
         $array_piece_quantity_count = 0;
 
-        foreach ($request->data as $key2 => $row) {
+        foreach ($validData as $key2 => $row) {
 //                if ($request->input('amount') == 0 || $request->input('amount') === null) {
 //                    $amount = $request->input('amount');
 //                    $parcel_value = $request->input('parcel_value');
@@ -3067,7 +3070,8 @@ class APIController extends Controller
             $return_array = array(
                 'status' => 0,
                 'message' => 'Shipment has been Booked!',
-                'tracking_numbers' => $tracking_number,
+                'Booked Tracking Numbers' => $tracking_number,
+                'Errors' => $errors,
                 'messages' => []
             );
 
