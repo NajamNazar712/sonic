1817c816fb4e83c09220e5ce60d77b589b8f7c77
usamauddin <usama.uddin@trax.pk>
2024-12-20 15:08:55 +0500

    cron fix

--- app/Http/Controllers/Admins/CronControllers/ApolloShipmentCronController.php
+++ app/Http/Controllers/Admins/CronControllers/ApolloShipmentCronController.php
@@ -41,14 +41,14 @@ class ApolloShipmentCronController extends Controller
         if($journeys->isNotEmpty()) {
 
             $client = new \GuzzleHttp\Client([
-                'base_uri' => env('APOLLO_API_URL'),
+                'base_uri' => rtrim(env('APOLLO_API_URL'), '/'),
                 'http_errors' => FALSE,
                 'connect_timeout' => 60,
                 'timeout' => 60
             ]);
 
             try {
-                $response = $client->post('/sonic/shipments/journeys/bulk-create', [
+                $response = $client->post('sonic/shipments/journeys/bulk-create', [
                     'json' => [
                         'journeys' => $journeys,
                     ]
