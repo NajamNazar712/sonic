436129eecdc6d11aa56179d47cad6c3f6eedd589
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2024-12-22 13:40:03 +0500

    changes

--- app/Http/Controllers/Admins/V2Pickup/V2AdminPickupsController.php
+++ app/Http/Controllers/Admins/V2Pickup/V2AdminPickupsController.php
@@ -3392,47 +3392,50 @@ class V2AdminPickupsController extends Controller
             $pickup_request_assigned_shipment = $pickup_request_assigned_shipment->first();
 
             $pickup_request = $pickup_request_assigned_shipment->pickup_request;
+            if(!empty($pickup_request)) {
+                $bookings = $pickup_request->booked - 1;
 
-            $bookings = $pickup_request->booked - 1;
-
-            $pickup_request->booked = $bookings;
-
-            $pickup_request->save();
-
-            ShipmentsPickupJourneyController::add($shipment_id, 4, null, $pickup_request->id);
-
-            $pickup_request_assigned_shipment->delete();
-
-            if ($bookings == 0) {
-                $pickup_request->status_id = 4;
+                $pickup_request->booked = $bookings;
 
                 $pickup_request->save();
 
-                if ($pickup_request->pickup_note_request) {
-                    $pickup_note = $pickup_request->pickup_note_request->pickup_note;
+                ShipmentsPickupJourneyController::add($shipment_id, 4, null, $pickup_request->id);
 
-                    if ($bookings == 0) {
-                        V2PickupNoteRequest::where('pickup_note_id', $pickup_note->id)->where('pickup_request_id', $pickup_request->id)->delete();
-                    }
+                $pickup_request_assigned_shipment->delete();
 
-                    $pickup_note_requests = V2PickupNoteRequest::where('pickup_note_id', $pickup_note->id);
+                if ($bookings == 0) {
+                    $pickup_request->status_id = 4;
 
-                    if ($pickup_note_requests->exists()) {
+                    $pickup_request->save();
+
+                    if ($pickup_request->pickup_note_request) {
+                        $pickup_note = $pickup_request->pickup_note_request->pickup_note;
+
                         if ($bookings == 0) {
-                            $pickup_note->pickups = $pickup_note->pickups - 1;
+                            V2PickupNoteRequest::where('pickup_note_id', $pickup_note->id)->where('pickup_request_id', $pickup_request->id)->delete();
                         }
-                        $pickup_note->shipments = $pickup_note->shipments - 1;
 
-                        $pickup_note->save();
-                    } else {
-                        $pickup_note->pickups = 0;
-                        $pickup_note->shipments = 0;
-                        $pickup_note->status = 1;
+                        $pickup_note_requests = V2PickupNoteRequest::where('pickup_note_id', $pickup_note->id);
 
-                        $pickup_note->save();
+                        if ($pickup_note_requests->exists()) {
+                            if ($bookings == 0) {
+                                $pickup_note->pickups = $pickup_note->pickups - 1;
+                            }
+                            $pickup_note->shipments = $pickup_note->shipments - 1;
+
+                            $pickup_note->save();
+                        } else {
+                            $pickup_note->pickups = 0;
+                            $pickup_note->shipments = 0;
+                            $pickup_note->status = 1;
+
+                            $pickup_note->save();
+                        }
                     }
                 }
             }
+
+
         }
     }
 
