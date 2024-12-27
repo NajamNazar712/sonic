b7e1c256d524d2d409b5b2c303c61330535fe446
Areeb-Trax <muhammad.areeb@trax.pk>
2024-12-27 15:21:33 +0500

    changes

--- app/Jobs/ProcessRetailShipmentBookingDB.php
+++ app/Jobs/ProcessRetailShipmentBookingDB.php
@@ -392,8 +392,6 @@ class ProcessRetailShipmentBookingDB implements ShouldQueue
                             'segment_id' => !empty($user_segments->segment_id) ? $user_segments->segment_id  : 0,
                             'sub_segment_id' => !empty( $user_segments->sub_segment_id) ?  $user_segments->sub_segment_id : 0
                         ]);
-                    } else {
-                        Log::warning('No segment information found for user ' . $user_id->user_id . ' on shipment ' . $shipment_id . ' from Retail excel upload');
                     }
                 } else {
                     Log::warning('No user found for shipment from Retail excel upload ' . $shipment_id);
