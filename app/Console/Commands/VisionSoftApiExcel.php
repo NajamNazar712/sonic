604ee7f7f5f43f823e68bde013e10fea501bb574
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2024-12-31 14:00:33 +0500

    changes

--- app/Console/Commands/VisionSoftApiExcel.php
+++ app/Console/Commands/VisionSoftApiExcel.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Http\Controllers\Admins\VisionSoftAPIController;
 use App\Http\Controllers\NotificationsController;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 
 class VisionSoftApiExcel extends Command
@@ -39,18 +40,31 @@ class VisionSoftApiExcel extends Command
      */
     public function handle()
     {
-        $cod_payment_excel = VisionSoftAPIController::cod_payable_excel();
-        $cod_receivable_excel = VisionSoftAPIController::cod_receivable_excel();
-        $links = "";
+        // Set the start and end dates for September 2024
+        $startDate = Carbon::create(2024, 9, 1)->startOfDay();
+        $endDate = Carbon::create(2024, 9, 30)->endOfDay();
 
-        if(!empty($cod_receivable_excel)) {
-            $links .= "<strong>COD Receivables: </strong> <br>" . "<a download='$cod_receivable_excel' href='$cod_receivable_excel' >$cod_receivable_excel</a>" . "<br>";
+        // Loop through each day in September 2024
+        for ($date = $startDate->copy(); $date->lte($endDate); $date->addDay()) {
+
+            $st = Carbon::create($date)->format('Y-m-d 00:00:00');
+            $end = Carbon::create($date)->format('Y-m-d 23:59:59');
+
+            $cod_payment_excel = VisionSoftAPIController::cod_payable_excel($st,$end);
+            $cod_receivable_excel = VisionSoftAPIController::cod_receivable_excel($st,$end);
+            $links = "";
+
+            if(!empty($cod_receivable_excel)) {
+                $links .= "<strong>COD Receivables: </strong> <br>" . "<a download='$cod_receivable_excel' href='$cod_receivable_excel' >$cod_receivable_excel</a>" . "<br>";
+            }
+            if(!empty($cod_payment_excel)) {
+                $links .= "<strong>COD Payable: </strong> <br>" . "<a download='$cod_payment_excel' href='$cod_payment_excel' >$cod_payment_excel</a>" . "<br>";
+            }
+            if(!empty($cod_payment_excel) || !empty($cod_receivable_excel)) {
+                NotificationsController::send(213, $links);
+            }
         }
-        if(!empty($cod_payment_excel)) {
-            $links .= "<strong>COD Payable: </strong> <br>" . "<a download='$cod_payment_excel' href='$cod_payment_excel' >$cod_payment_excel</a>" . "<br>";
-        }
-        if(!empty($cod_payment_excel) || !empty($cod_receivable_excel)) {
-            NotificationsController::send(213, $links);
-        }
+
+
     }
 }
