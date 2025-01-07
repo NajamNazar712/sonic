f6f8dedf40349d37bb4fff3f292dc98d0eae250e
Anas <anas.mazhar@trax.pk>
2025-01-07 16:11:49 +0500

    Vision file generate oct to dec

--- app/Console/Commands/visionsoftmultiple.php
+++ app/Console/Commands/visionsoftmultiple.php
@@ -42,7 +42,7 @@ class visionsoftmultiple extends Command
     {
         // Set the start and end dates for September 2024
         $startDate = Carbon::create(2024, 10, 1)->startOfDay();
-        $endDate = Carbon::create(2024, 12, 31)->endOfDay();
+        $endDate = Carbon::create(2024, 10, 31)->endOfDay();
 
         // Loop through each day in September 2024
         for ($date = $startDate->copy(); $date->lte($endDate); $date->addDay()) {
