a668898460aefc14a95c8290ca6c5ca95015a95d
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2025-01-08 12:40:16 +0500

    changes

--- database/seeders/UpdateModeulePermissionRevenueReportTableSeeder.php
+++ database/seeders/UpdateModeulePermissionRevenueReportTableSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class UpdateModulePermissionRevenueReportTableSeeder extends Seeder
+class UpdateModeulePermissionRevenueReportTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
