3b3427afc8e5e9938498560dd2220031e8fa395e
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2025-01-08 12:53:54 +0500

    changes

--- database/seeders/ApoloCronlogSeeder.php
+++ database/seeders/ApoloCronlogSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 
-class ApolloCronJobLogsSeeder extends Seeder
+class ApoloCronlogSeeder extends Seeder
 {
     /**
      * Run the database seeds.
