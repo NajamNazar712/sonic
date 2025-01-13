900ad49a6a00b594288c619e899d962a7d5a074a
Najam <muhammad.najam@trax.pk>
2025-01-13 17:00:57 +0500

    changes'

--- resources/views/client/finja_onboarding.blade.php
+++ resources/views/client/finja_onboarding.blade.php
@@ -122,9 +122,9 @@
                                     $('.card-text').html(errorList); // Append errors after the `.card` element
                                 }
                             }
-                            // else{
-                            //     window.location.href = '{{ url('cod/wallet/login') }}'
-                            // }
+                            else{
+                                window.location.href = data.redirect_url;
+                            }
                         },
                         error: function(xhr, status, error) {
                             console.error(xhr.responseText);
