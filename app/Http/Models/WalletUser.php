cee68019210d9c48b61a70274750420b8f5e2957
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2025-01-20 15:12:37 +0500

    changes

--- app/Http/Models/WalletUser.php
+++ app/Http/Models/WalletUser.php
@@ -18,7 +18,7 @@ class WalletUser extends Model
 
     public function wallet_user()
     {
-        return $this->belongsTo('App\Http\Models\Shipper\User', 'id', 'user_id');
+        return $this->belongsTo('App\Http\Models\Shipper\User');
 
     }
 }
