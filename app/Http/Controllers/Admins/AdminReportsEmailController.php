cde510ac6bbed087d85f951ce5c6a193fdffb861
Anas <anas.mazhar@trax.pk>
2025-01-21 00:48:11 +0500

    file share

--- app/Http/Controllers/Admins/AdminReportsEmailController.php
+++ app/Http/Controllers/Admins/AdminReportsEmailController.php
@@ -1398,7 +1398,7 @@ class AdminReportsEmailController extends Controller
             $to = ['anas.mazhar@trax.pk', 'aftab.qidwai@trax.pk'];
             $mail = Mail::to($to);
             $link = '<a href="' . $file_name_without_path . '" target="_blank">Report</a>';
-            $mail->send(new ReportsEmail("Done Payment Report Link",'Done Payment Report Link' . url('/') . '/' . $link,null));
+            $mail->send(new ReportsEmail("Done Payment",'Done Payment Report Link' . url('/') . '/' . $link,null));
             
             // NotificationsController::send(82, $date, url('/') . '/' . $file_name_without_path);
         }
