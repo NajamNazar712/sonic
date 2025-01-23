771f3ab6238d017b45d5ac8f3145b529a2c0d6b8
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2025-01-23 22:12:16 +0500

    changes

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -10150,23 +10150,27 @@ class APIController extends Controller
         if ($validate->fails()) {
             return response()->json(['status' => 0, 'message' => 'Error(s) in Input', 'errors' => $validate->errors()]);
         } else {
-            $shipment_id = Shipment::where('tracking_number', $request->tracking_number)->value('id');
-            $pending_payment_shipments = PendingPaymentShipment::where('shipment_id',$shipment_id)->whereIn('type',[0,1])->latest()->first();
+            $shipment = Shipment::where('tracking_number', $request->tracking_number)->first();
+            $shipment_id = $shipment->id;
+            if (!DonePaymentShipment::where('shipment_id', $shipment_id)->whereIn('type', [0, 1])->exists()) {
 
-            $finja_request_log = new FinjaRequestLog();
-            $finja_request_log->requested = json_encode($request->all());
-            $finja_request_log->ip_address = $request->ip();
-            $finja_request_log->save();
+                ShipmentAdditionalCharges::where('shipment_id', $shipment_id)->update([
+                    'wallet_charges' => $request->charges,
+                    'wallet_charges_updated_at' => Carbon::now()
+                ]);
 
-            ShipmentAdditionalCharges::where('shipment_id', $shipment_id)->update([
-                'wallet_charges' => $request->charges,
-                'wallet_charges_updated_at' => Carbon::now()
-            ]);
+                $pending_payment_shipments = PendingPaymentShipment::where('shipment_id', $shipment->id)->whereIn('type', [0, 1])->latest()->first();
+                $finja_request_log = new FinjaRequestLog();
+                $finja_request_log->requested = json_encode($request->all());
+                $finja_request_log->ip_address = $request->ip();
+                $finja_request_log->save();
 
-            if(!empty($pending_payment_shipments)){
-                AdminFinanceController::update_payment($shipment_id,$pending_payment_shipments->type);
-
-                return response()->json(['status' => 1, 'message' => 'Charges updated against this shipment.']);
+                if (!empty($pending_payment_shipments)) {
+                    AdminFinanceController::update_payment($shipment_id, $pending_payment_shipments->type);
+                    return response()->json(['status' => 1, 'message' => 'Charges updated against this shipment.']);
+                } else {
+                    return response()->json(['status' => 0, 'message' => 'Payment Already Processed', 'errors' => 'Error']);
+                }
             }else{
                 return response()->json(['status' => 0, 'message' => 'Payment Already Processed', 'errors' => 'Error']);
             }
