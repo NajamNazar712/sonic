8271a826cec4ecdb5b42bd3b4a305e04cc2b144f
Najam <muhammad.najam@trax.pk>
2025-01-24 14:22:09 +0500

    fixing

--- resources/views/client/layout/navbar.blade.php
+++ resources/views/client/layout/navbar.blade.php
@@ -32,11 +32,11 @@
                                             <h2 class="d-inline-block m-0 align-middle primary"><i class="la la-money m-0" style="color: white!important;"></i></h2>
                                         </div>
                                     </a>
-                                @else
+                                  @elseif(session('wallet_sign_up_allow'))
                                     <a class="nav-link d-inline-flex align-middle p-0" href="{{ route('cod.wallet.on_boarding') }}" target="_blank">
-                                        <div class="m-0 bg-white primary rounded custom-nav-buttons-padding">
-                                            <span class="d-inline-block d-md-none d-lg-none d-xl-inline-block align-middle font-weight-bold">Wallet</span>
-                                            <h2 class="d-inline-block m-0 align-middle primary"><i class="la la-circle-o-notch m-0"></i></h2>
+                                        <div class="m-0 bg-white primary rounded custom-nav-buttons-padding" style="background: rgb(67 118 98)!important">
+                                            <span class="d-inline-block d-md-none d-lg-none d-xl-inline-block align-middle font-weight-bold" style="color: white!important;">Wallet</span>
+                                            <h2 class="d-inline-block m-0 align-middle primary"><i class="la la-money m-0" style="color: white!important;"></i></h2>
                                         </div>
                                     </a>
                                 @endif
