2172a63d7d8845ba2f3b6c41063999bb0862add3
Anas <anas.mazhar@trax.pk>
2025-01-30 18:29:30 +0500

    AgentSar notification

--- app/Console/Commands/AgentSarNotification.php
+++ app/Console/Commands/AgentSarNotification.php
@@ -83,18 +83,18 @@ class AgentSarNotification extends Command
             //Combine the results for sending in single email
             $sendEmail = $sendEmails->union($sendEmailofRefusalShipments)->get();
             // If there are shipments that meet the conditions, send Email Notification to shipper for each shipment
-            // if ($sendEmail->isNotEmpty()) {
+            if ($sendEmail->isNotEmpty()) {
 
-            //     foreach ($sendEmail as $shipment) {
-            //         // if shipment status is unresponsive Increment the unresponsive_email_count for each shipment after sending the email
-            //         if($shipment->rv_assign_agent_status_id == 7){
-            //             $shipment->increment('unresponsive_email_count');
-            //             $shipment->unresponsive_email_time = $currentDateTime;
-            //             $shipment->save();
-            //         }
-            //     }
-            //     NotificationsController::send(220, $sendEmail);
-            // }
+                foreach ($sendEmail as $shipment) {
+                    // if shipment status is unresponsive Increment the unresponsive_email_count for each shipment after sending the email
+                    if($shipment->rv_assign_agent_status_id == 7){
+                        $shipment->increment('unresponsive_email_count');
+                        $shipment->unresponsive_email_time = $currentDateTime;
+                        $shipment->save();
+                    }
+                }
+                NotificationsController::send(220, $sendEmail);
+            }
 
             // When there is no response from the shipper within 24 hours of the "Shipper Advise Requested" status being set on the shipment, 
             // the system will automatically update the shipment status to "Return Confirm."
