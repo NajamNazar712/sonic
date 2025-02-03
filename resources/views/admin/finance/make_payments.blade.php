91406c2d39d1a4b1e9c0a8df328432f396348dda
Anas <anas.mazhar@trax.pk>
2025-02-03 12:02:19 +0500

    excel data is blank debugging

--- resources/views/admin/finance/make_payments.blade.php
+++ resources/views/admin/finance/make_payments.blade.php
@@ -673,14 +673,15 @@
                         url: '{{ route('admin.finance.make_payments.list') }}',
                         data: params,
                         success: function(result) {
+                            alert('data');
                             head = [];
 
                             head.push('S.No');
                             head.push('Shipper');
                             head.push('City');
-                            head.push('Phone No(s).');
-                            head.push('Address');
-                            head.push('Created Datetime');
+                            // head.push('Phone No(s).');
+                            // head.push('Address');
+                            // head.push('Created Datetime');
                             head.push('Total Shipments');
                             head.push('Total Pending Shipments');
                             head.push('Delivered Shipments');
@@ -703,7 +704,6 @@
                             head.push('Payment Cycle');
                             head.push('Payment Cycle Days');
                             head.push('Return Shipments Avg. Aging');
-console.log(result.data)
                             $.each(result.data, function(index, values) {
                                 row = [];
                                 
@@ -711,9 +711,9 @@ console.log(result.data)
                                 row.push(index + 1);
                                 row.push(values.shipper);
                                 row.push(values.city);
-                                row.push(values.phone_numbers);
-                                row.push(values.address);
-                                row.push(values.created_at);
+                                // row.push(values.phone_numbers);
+                                // row.push(values.address);
+                                // row.push(values.created_at);
                                 row.push(values.total_shipments);
                                 row.push(values.total_pending_shipments);
                                 row.push(values.delivered_shipments_count);
