aeb2e75aa6e6097a45cf3b3c6ea76b2092388fb6
Anas <anas.mazhar@trax.pk>
2025-02-03 12:05:46 +0500

    excel data is blank debugging

--- resources/views/admin/finance/make_payments.blade.php
+++ resources/views/admin/finance/make_payments.blade.php
@@ -673,15 +673,15 @@
                         url: '{{ route('admin.finance.make_payments.list') }}',
                         data: params,
                         success: function(result) {
-                            alert('data');
+                            console.log(result)
                             head = [];
 
                             head.push('S.No');
                             head.push('Shipper');
                             head.push('City');
-                            // head.push('Phone No(s).');
-                            // head.push('Address');
-                            // head.push('Created Datetime');
+                            head.push('Phone No(s).');
+                            head.push('Address');
+                            head.push('Created Datetime');
                             head.push('Total Shipments');
                             head.push('Total Pending Shipments');
                             head.push('Delivered Shipments');
@@ -711,9 +711,9 @@
                                 row.push(index + 1);
                                 row.push(values.shipper);
                                 row.push(values.city);
-                                // row.push(values.phone_numbers);
-                                // row.push(values.address);
-                                // row.push(values.created_at);
+                                row.push(values.phone_numbers);
+                                row.push(values.address);
+                                row.push(values.created_at);
                                 row.push(values.total_shipments);
                                 row.push(values.total_pending_shipments);
                                 row.push(values.delivered_shipments_count);
