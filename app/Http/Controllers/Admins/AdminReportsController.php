eaa39dc1e1e9b98b2814b11bb4319ed2f45e9bf8
Anas <anas.mazhar@trax.pk>
2025-02-04 15:51:21 +0500

    red highligthed isssue has been fixed

--- app/Http/Controllers/Admins/AdminReportsController.php
+++ app/Http/Controllers/Admins/AdminReportsController.php
@@ -15615,7 +15615,6 @@ class AdminReportsController extends Controller
             //     }
             // })
             ->editColumn('current_hub', function ($shipment) {
-                // error_log('pp'.print_r($shipment,true));
                 if(in_array($shipment->shipper_status_id ,[49, 3])){
                     if(in_array($shipment->cargo_status_id,[3,2,4, 7, 8, 9,6])){ //Shipment In Transit || Shipment Misrouted Forwarded concered hub change reference TO-6939
                         return $shipment->destination;
