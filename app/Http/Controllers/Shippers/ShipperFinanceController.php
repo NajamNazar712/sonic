b57bb9a653d87b13aeeca621be1fd9c75613679d
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2025-02-07 12:17:43 +0500

    changes

--- app/Http/Controllers/Shippers/ShipperFinanceController.php
+++ app/Http/Controllers/Shippers/ShipperFinanceController.php
@@ -608,7 +608,8 @@ class ShipperFinanceController extends Controller
                             $total_replacement_charges += $shipment->replacement_charges;
                             $total_try_and_buy_charges += $shipment->try_and_buy_charges;
                             $total_reverse_pickup_charges += $service_charges;
-                        } else {
+                        }
+                        if ($done_payment_shipment->type == 1) {
                             $total_return_charges += $shipment->return_charges;
                         }
 
