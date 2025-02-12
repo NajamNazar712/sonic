bad7b28ed8d15bd93c7ea4faefe3a20dbaeb65c6
Anas <anas.mazhar@trax.pk>
2025-02-12 19:44:19 +0500

    Working on completed excel to export for the shipper side add on the additional column

--- app/Http/Controllers/Shippers/ShipperFinanceController.php
+++ app/Http/Controllers/Shippers/ShipperFinanceController.php
@@ -827,7 +827,7 @@ class ShipperFinanceController extends Controller
 
         $filename = 'sonic_payment_details_' . $request->id . '.xlsx';
 
-        $details = array();  
+        $details = array();
         $details[] = ['S. No.', 'Tracking No.', 'Booking Date', 'Type', 'Order ID', 'Vendor', 'Origin', 'Consignee Name', 'Consignee Phone', 'Destination', 'Service Type', 'Weight (kg)', 'Collection Amount (PKR)', 'Weight Charges (PKR)', 'Cash Handling Charges (PKR)', 'OSA Charges (PKR)', 'Adjustments (PKR)','Total Charges (PKR)','GST','WHT','Net Retained Amount (PKR)','Net Disbursement Amount (PKR)
 '];
 
@@ -921,6 +921,7 @@ class ShipperFinanceController extends Controller
                 $row[] = (($done_payment_shipment->charges != 0) ? number_format($done_payment_shipment->wht, 2) : '0');
                 $row[] = (($done_payment_shipment->amount != 0) ? number_format($done_payment_shipment->amount - $done_payment_shipment->payable, 2) : '0');
                 $row[] = (($done_payment_shipment->payable != 0) ? number_format($done_payment_shipment->payable, 2) : '0');
+
                 $details[] = $row;
 
                 $serial_number++;
@@ -977,6 +978,7 @@ class ShipperFinanceController extends Controller
                     $total_payable += $done_payment_shipment->payable;
                 }
             }
+
             $total_columns = count($details[0]);
 
             $summary = ['Total Weight Charges' => $total_weight_charges, 'Total Cash Handling Charges' => $total_cash_handling_charges, 'Total Insurance Charges' => $total_insurance_charges, 'Total Replacement Charges' => $total_replacement_charges, 'Total Try & Buy Charges' => $total_try_and_buy_charges, 'Total Return Charges' => $total_return_charges, 'Total Fuel Surcharge' => $total_fuel_surcharge, 'Total Faf Charges' => $total_faf_charges, 'Total Intercept Charges' => $total_intercept_charges, 'Total OSA Charges' => $total_nsa_osa_charges, 'Total Charges (w/o GST)' => ($total_charges - $total_packaging_material_charges), 'Total GST' => $total_gst,'Total WHT (Deductable)' => $total_wht, 'Total Packaging Material Charges' => $total_packaging_material_charges, 'Total Adjustments' => $total_adjustments, 'IBFT Charges' => $done_payment->ibft_charges,  'Overall Charges' => ($total_charges + $total_sms_charges + $total_gst - $total_adjustments + $done_payment->ibft_charges - $total_wht)];
