176fa729b934b57789577959891966a9af69dc20
Anas <anas.mazhar@trax.pk>
2025-02-13 16:35:55 +0500

    Working on halt shipper condition add

--- app/Jobs/ProcessRvShipmentTicket.php
+++ app/Jobs/ProcessRvShipmentTicket.php
@@ -88,7 +88,6 @@ class ProcessRvShipmentTicket implements ShouldQueue
                 $rvShipmentTicket  = DB::table('rv_shipment_tickets')
                 ->where('shipment_id', $this->shipment['shipment_id'])
                 ->first();
-                Log::channel('cronJobLog')->info('s ' . 'rv_shipment_ticket data Saved'.json_encode($rvShipmentTicket));
                 
                 if($rvShipmentTicket->halt_shipper == 1){
                     RvShipmentAssignAgent::where('shipment_id',$this->shipment['shipment_id'])->update(['unresponsive_count' => 0, 'unresponsive_email_count' => 0, 'rv_state_id'=>2, 'unresponsive_email_time'=>NULL, 'unresponsive_attempt_time'=>NULL]);
