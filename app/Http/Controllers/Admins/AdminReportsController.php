23b5f9ee1d9869120ed2b7b85687d5383e4e842f
mohammad <muhammad.ahmed@trax.pk>
2025-02-25 11:40:11 +0500

    name fixed (report)

--- app/Http/Controllers/Admins/AdminReportsController.php
+++ app/Http/Controllers/Admins/AdminReportsController.php
@@ -6745,7 +6745,14 @@ class AdminReportsController extends Controller
             's.actual_weight as actual_weight',
             'ad2.name as kae',
             'crm_requests.complainant_phone as complainant_phone',
-            'crm_requests.case_nature_complainant as case_nature_complainant',
+            DB::raw("
+                    CASE 
+                        WHEN crm_requests.case_nature_complainant = 1 THEN 'Consignee' 
+                        WHEN crm_requests.case_nature_complainant IS NULL THEN '-' 
+                        ELSE 'Shipper' 
+                    END as case_nature_complainant
+                ")
+
         ];
 
         $crm = DB::connection('reports')->table('crm_requests')->leftjoin('shipments as s', 's.id', '=', 'crm_requests.shipment_id')
