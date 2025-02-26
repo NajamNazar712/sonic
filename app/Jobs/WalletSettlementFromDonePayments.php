40478b6a7941ea700e9d951325ae1a6c99a533ec
Najam <muhammad.najam@trax.pk>
2025-02-26 17:21:42 +0500

    condition in adjustment api

--- app/Jobs/WalletSettlementFromDonePayments.php
+++ app/Jobs/WalletSettlementFromDonePayments.php
@@ -109,12 +109,18 @@ class WalletSettlementFromDonePayments implements ShouldQueue
                 ];
 
             } elseif($dps->wallet_action_bid == 2) {
+                $type =  FinjaLogSettlementRecord::check_wallet_charges_type($dps->shipment_id);
+                $payable = $dps->payable;
+                if($type) {
+                    $wallet_charges = ShipmentAdditionalCharges::fetch_wallet_charges($dps->shipment_id);
+                    $payable = $dps->payable - $wallet_charges;
+                }
                 $requestPayload = [
                     "client_id" => $dps->user_id,
                     "wallet_id" => $dps->wallet_id,
                     "reference_id" => $dps->id,
                     "shipment_id" =>  $dps->tracking_number,
-                    "amount" => floatval($dps->payable),
+                    "amount" => floatval($payable),
                 ];
                 $request_nature = 'adjustment-request';
                 $response_nature = 'adjustment-response';
