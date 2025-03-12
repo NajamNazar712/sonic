7b2255b4e713abdfd3c4196df4fd4e6008c29e6e
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2025-03-12 15:24:12 +0500

    changes

--- app/Console/Commands/RerunWalletSettlement.php
+++ app/Console/Commands/RerunWalletSettlement.php
@@ -3,8 +3,9 @@
 namespace App\Console\Commands;
 
 use App\Jobs\WalletSettlementFromDonePayments;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
-
+use DB;
 class RerunWalletSettlement extends Command
 {
     /**
@@ -28,6 +29,27 @@ class RerunWalletSettlement extends Command
      */
     public function handle()
     {
-        WalletSettlementFromDonePayments::dispatchNow(1563023,  346);
+        $startTime = Carbon::now()->subDay(2)->format('Y-m-d H:i:s');
+        $endTime = Carbon::now()->subHours(6)->format('Y-m-d H:i:s');
+        $donePayments = DB::table('done_payment_shipments')
+            ->leftJoin('done_payments', 'done_payments.id', '=', 'done_payment_shipments.done_payment_id')
+            ->leftJoin('shipments as s', 's.id', '=', 'done_payment_shipments.shipment_id')
+            ->leftJoin('finja_log_settlement_records as sac', 'done_payment_shipments.shipment_id', '=', 'sac.shipment_id')
+            ->where('done_payments.status', 3)
+            ->where('done_payments.is_wallet_payment', 1)
+            ->whereBetween('done_payments.updated_at', [$startTime,$endTime])
+            ->whereIn('done_payment_shipments.wallet_action_bid', [0, 1, 2])
+            ->groupBy('done_payment_shipments.done_payment_id')
+            ->select('done_payments.*')
+            ->get();
+
+        $donePayments->chunk(5)->each(function ($chunkedShipments)  {
+            foreach ($chunkedShipments as $value){
+                WalletSettlementFromDonePayments::dispatch($value->id,  346);
+            }
+
+        });
+
+
     }
 }
