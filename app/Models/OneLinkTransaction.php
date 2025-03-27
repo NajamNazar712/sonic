d790db30c10f56c572f11e9b12bb57e3af375702
mohammad <muhammad.ahmed@trax.pk>
2025-03-27 13:07:47 +0500

    fillable added

--- app/Models/OneLinkTransaction.php
+++ app/Models/OneLinkTransaction.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class OneLinkTransaction extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'rrn',
+        'stan',
+        'date_time',
+        'message_id',
+        'original_rrn',
+        'original_stan',
+        'original_rtp_id',
+        'merchant_id',
+        'sub_dept',
+        'status',
+        'original_instructed_amount',
+        'net_amount',
+        'iban',
+        'account_title',
+        'longitude',
+        'latitude',
+        'nature_id'
+    ];
+    
 }
