3e01cbb56322642bf9d053a1c241b644f49fb5c6
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2025-04-08 16:29:33 +0500

    changes

--- routes/api.php
+++ routes/api.php
@@ -146,7 +146,7 @@ use Illuminate\Http\Request;
         Route::prefix('oneLink')->name('oneLink.')->group(function () {
             Route::post('verifyDeliveredShipmentDQRCMerchant', 'OneLinkController@verifyDeliveredShipmentDQRCMerchant')->name('verifyDeliveredShipmentDQRCMerchant');
             Route::post('notifyMerchant', 'OneLinkController@notifyMerchant')->name('notifyMerchant');
-            Route::post('paymentNotification', 'OneLinkController@paymentNotification')->name('paymentNotification');
+            Route::post('1Link/paymentNotification', 'OneLinkController@paymentNotification')->name('paymentNotification');
         });
 
         //Current
