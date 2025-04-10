4a2b4fa81621193349f48d9483a03f7009a3dbe8
Areeb-Trax <muhammad.areeb@trax.pk>
2025-04-10 14:28:58 +0500

    changes

--- app/Http/Controllers/Admins/AdminFinanceController.php
+++ app/Http/Controllers/Admins/AdminFinanceController.php
@@ -4434,7 +4434,7 @@ class AdminFinanceController extends Controller
     public function change_shipment_amount_store(Request $request)
     {
         $shipment_id = $request->input('shipment_id');
-        $amount = str_replace(',', '', $request->input('amount') ?? 0);
+        $amount = str_replace(',', '', $request->input('amount'));
         
         $shipment = Shipment::find($shipment_id);
         
@@ -4442,7 +4442,7 @@ class AdminFinanceController extends Controller
 
         $change_shipment_amount->shipment_id = $shipment->id;
         $change_shipment_amount->old_amount = $shipment->amount;
-        $change_shipment_amount->new_amount = $amount;
+        $change_shipment_amount->new_amount = $amount ?? 0;
         $change_shipment_amount->admin_id = Auth::id();
         $change_shipment_amount->remarks = $request->remarks;
         $change_shipment_amount->save();
