d7c194de5b352d9dbb84765984ce2a7d1547d050
Najam <muhammad.najam@trax.pk>
2025-04-11 18:23:42 +0500

    correction in condirion of settlement

--- app/Jobs/WalletBulkSettlementFromDonePayments.php
+++ app/Jobs/WalletBulkSettlementFromDonePayments.php
@@ -90,8 +90,9 @@ class WalletBulkSettlementFromDonePayments implements ShouldQueue
  				
                 if ($dps->wallet_action_bid == 1 && $dps->wallet_settlement_updated == 0) {
 
-                    $logCharged = !empty($dps->wallet_log_charges_updated) ? $dps->wallet_log_charges_updated : 0;
-
+                    // $logCharged = !empty($dps->wallet_log_charges_updated) ? $dps->wallet_log_charges_updated : 0;
+                    $logCharged = FinjaLogSettlementRecord::where('shipment_id', $shipmentId)->first();
+                    $logCharged = $logCharged ? $logCharged->wallet_log_charges_updated : 0;
                     $settlementPayload[$shipmentId] = [
                         "client_id" => $dps->user_id,
                         "wallet_id" => $dps->wallet_id,
@@ -99,9 +100,9 @@ class WalletBulkSettlementFromDonePayments implements ShouldQueue
                         "shipment_id" => $dps->tracking_number,
                         "amount" => $dps->type == 0 ? $dps->amount : 0,
                         "charges" => [
-                            'faf_charges' => $logCharged == 1 ? floatval($dps->faf_charges) : 0,
-                            'weight_charges' => $logCharged == 1 ? floatval($dps->weight_charges) : 0,
-                            'fuel_surcharge' => $logCharged == 1 ? floatval($dps->fuel_surcharge) : 0,
+                            'faf_charges' => $logCharged == 0 ? floatval($dps->faf_charges) : 0,
+                            'weight_charges' => $logCharged == 0 ? floatval($dps->weight_charges) : 0,
+                            'fuel_surcharge' => $logCharged == 0 ? floatval($dps->fuel_surcharge) : 0,
                             'cash_handling_charges' => $dps->type == 0 ? floatval($dps->cash_handling_charges) : 0,
                             'insurance_charges' => floatval($dps->insurance_charges),
                             'replacement_charges' => floatval($dps->replacement_charges),
