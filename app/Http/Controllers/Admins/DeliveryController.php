00c5142f3bf5f22a663fbd72303f0b590f160428
mohammad <muhammad.ahmed@trax.pk>
2025-04-14 14:47:50 +0500

    done

--- app/Http/Controllers/Admins/DeliveryController.php
+++ app/Http/Controllers/Admins/DeliveryController.php
@@ -2252,6 +2252,12 @@ class DeliveryController extends Controller
         if (DeliveryNote::where('id', $delivery_note_id)->where('pending_status', 1)->exists()) {
             return response()->json(['status' => 0, 'error' => 'Delivery note already updated']);
         }
+
+        $delivery_note = DeliveryNote::find($request->delivery_note_id);
+        if($selected_status == 14 && $delivery_note->rider->operation_rider_id == 2 && $delivery_note->rider->id != '12879'){
+            return response()->json(['status' => 0, 'error' => 'Delivered Status Only Allowed For Hold For Self Collection']);
+        }
+        
         $selected_reason = $request->selected_reason;
         $reason_for_first_attempt = array(7, 8, 35, 19, 34, 12, 27, 40);
 
