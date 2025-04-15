39b5ff45d31d2a7fa8708c52bafbb015f0d22f7f
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2025-04-15 14:58:10 +0500

    chnages

--- app/Console/Commands/UpdateShipmentAdditionalCharges.php
+++ app/Console/Commands/UpdateShipmentAdditionalCharges.php
@@ -51,7 +51,7 @@ class UpdateShipmentAdditionalCharges extends Command
 
         $array_merge = array_unique(array_merge($total_array,$total_array2));
 
-        if(count($total_array) > 0){
+        if(count($array_merge) > 0){
             ShipmentAdditionalCharges::additional_charges_apply($total_array);
         }
 
