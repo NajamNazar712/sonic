37fe9ad5aafcdadcf95e4a2379c5273d552e9c65
mohammad <muhammad.ahmed@trax.pk>
2025-04-22 19:17:01 +0500

    dd() remove

--- app/Http/Controllers/OneLinkController.php
+++ app/Http/Controllers/OneLinkController.php
@@ -173,7 +173,6 @@ class OneLinkController extends Controller
             'stan' => $stan
         ])->first();
 
-        dd($stan, $data, $existingTransaction);
         if ($existingTransaction) {
             try {
                 DB::transaction(function () use ($existingTransaction, $data, $natureId, $rrn, $subDept) {
@@ -224,7 +223,6 @@ class OneLinkController extends Controller
                     }
                 });
 
-                dd($stan);
 
                 $response = [
                     "responseCode" => "00",
