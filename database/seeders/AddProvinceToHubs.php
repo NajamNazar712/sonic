ab3610006260d98f4e6bbf3d1d9e4c6022233077
Areeb-Trax <muhammad.areeb@trax.pk>
2025-04-22 13:48:24 +0500

    added seeders for hubs

---
+++ database/seeders/AddProvinceToHubs.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Http\Models\City;
+
+class AddProvinceToHubs extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $provinceCities = [
+            1 => [ // Sindh
+                'Hyderabad',
+                'Hyderabad Allied',
+                'Mirpur Khas',
+                'Moro',
+                'Nawabshah',
+                'Karachi',
+                'Ghotki',
+                'Jacobabad',
+                'Khairpur',
+                'Larkana',
+                'Shikarpur',
+                'Sukkur',
+                'Sukkur Allied',
+            ],
+            2 => [ // Punjab
+                'Bhakkar',
+                'Chiniot',
+                'Dera Ismail Khan',
+                'Faisalabad',
+                'Faisalabad Allied',
+                'Jauharabad',
+                'Jhang',
+                'Khurrianwala',
+                'Mianwali',
+                'Sargodha',
+                'Toba Tek Singh',
+                'Daska',
+                'Gujranwala',
+                'Gujrat',
+                'Hafizabad',
+                'Jhelum',
+                'Kharian',
+                'Lalamusa',
+                'Mandi Bahauddin',
+                'Mirpur Azad Kashmir',
+                'Narowal',
+                'Pasrur',
+                'Sialkot',
+                'Wazirabad',
+                'Chakwal',
+                'Fateh Jang',
+                'Gujjar Khan',
+                'Islamabad',
+                'Kamra',
+                'Wah Cantt',
+                'Kasur',
+                'Lahore',
+                'Pattoki',
+                'Raiwind',
+                'Sheikhupura',
+                'Bahawalnagar',
+                'Bahawalpur',
+                'Burewala',
+                'Chichawatni',
+                'Chishtian',
+                'Dera Ghazi Khan',
+                'Jampur',
+                'Kamaliya',
+                'Khanewal',
+                'Layyah',
+                'Multan',
+                'Multan Allied',
+                'Muzaffar Garh',
+                'Okara',
+                'Rahim Yar Khan',
+                'Sahiwal',
+                'Vehari',
+                'Murree',
+            ],
+            3 => [ // Balochistan
+                'Quetta',
+                'Quetta Allied',
+            ],
+            4 => [ // Khyber Pakhtunkhwa
+                'Abbottabad',
+                'Gilgit',
+                'Haripur',
+                'Mansehra',
+                'Bannu',
+                'Batkhela',
+                'Charsadda',
+                'Kohat',
+                'Mardan',
+                'Nowshera',
+                'Peshawar',
+                'Peshawar Allied',
+                'Swabi',
+                'Swat',
+                'Timergarah',
+            ],
+        ];
+
+        foreach ($provinceCities as $provinceId => $cities) {
+            foreach ($cities as $cityName) {
+                City::where('name', 'LIKE', "%{$cityName}%")->update(['province_id' => $provinceId]);
+            }
+        }
+    }
+}
