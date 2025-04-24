d3eaa55cede28552f66e07d09582c42d88ce922d
mohammad <muhammad.ahmed@trax.pk>
2025-04-24 15:06:47 +0500

    check

--- app/Http/Controllers/OneLinkController.php
+++ app/Http/Controllers/OneLinkController.php
@@ -196,7 +196,6 @@ class OneLinkController extends Controller
             'rrn' => $rrn,
             'stan' => $stan
         ])->first();
-        dd($data, $existingTransaction);
 
         if ($existingTransaction) {
             try {
@@ -268,7 +267,7 @@ class OneLinkController extends Controller
             } catch (\Exception $e) {
                 $response = [
                     "responseCode" => "99",
-                    "responseDesc" => "Processing Error"
+                    "responseDesc" => $e->getMessage(),
                 ];
                 $status = 500;
             }
