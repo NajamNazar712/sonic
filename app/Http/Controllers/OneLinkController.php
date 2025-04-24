cdf022b1d3506225fd79a0eb64f6f43baa3d1b3e
mohammad <muhammad.ahmed@trax.pk>
2025-04-24 15:37:46 +0500

    response_data check added

--- app/Http/Controllers/OneLinkController.php
+++ app/Http/Controllers/OneLinkController.php
@@ -38,7 +38,7 @@ class OneLinkController extends Controller
             ->latest()
             ->first();
 
-        if (!empty($check_transaction)) {
+        if (!empty($check_transaction) && !empty($check_transaction->one_link_log->response_data)) {
             $existing_log = json_decode($check_transaction->one_link_log->response_data, true);
             $expiry_time = Carbon::parse($check_transaction->expiry_time)->format('Y-m-d\TH:i:s');
             $remaining_seconds = now()->diffInSeconds($expiry_time, false); // false = future is positive, past is negative
