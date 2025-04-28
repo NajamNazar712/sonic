64b171dcaa2955990a33f5062353f14e7cdc2765
usamauddin <usama.uddin@trax.pk>
2025-04-28 13:40:04 +0500

    test

--- app/Http/Controllers/ShipperOrderManagementApiController.php
+++ app/Http/Controllers/ShipperOrderManagementApiController.php
@@ -47,7 +47,7 @@ class ShipperOrderManagementApiController extends Controller
            ->where('shipments.user_id',$request->shipper_id)
            ->groupBy('shipments.id')
            ->orderBy('shipments.id', 'desc')
-           ->get();
+           ->count();
 
        if($order_list->isNotEmpty()) {
            return response()->json(['status' => 0 , 'message' => 'Success' ,'order_list'=>$order_list]);
