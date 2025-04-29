789a747a7bd39d1d41b9b647c3ceb10d710c0859
Anas <anas.mazhar@trax.pk>
2025-04-29 14:56:34 +0500

    shipper process webhook

--- app/Http/Controllers/Webhook/ShipmentStatusWebhookController.php
+++ app/Http/Controllers/Webhook/ShipmentStatusWebhookController.php
@@ -100,10 +100,10 @@ class ShipmentStatusWebhookController extends Controller
                     $response = $client->post('', [
                         'json' => $payload,
                     ]);
-                    // $body = $response->getBody();
+                    $body = $response->getBody();
                     // // // $body->rewind();  // Rewind the stream to ensure we're reading from the start
-                    // $responseBody = $body->getContents(); // Get the body content
-                    // Log::channel('botCallJobLog')->info("Guzzle Response Body: " . $responseBody);
+                    $responseBody = $body->getContents(); // Get the body content
+                    Log::channel('botCallJobLog')->info("Guzzle Response Body: " . $responseBody);
                     // // Log::channel('botCallJobLog')->info('s ' . 'Webhook log responseBody-body' . json_encode($response1));
                 }
                 if ($response instanceof \Psr\Http\Message\ResponseInterface) {
