46520dd585fef7da6df7c9883cc0960f6cc262c9
Areeb-Trax <muhammad.areeb@trax.pk>
2025-05-13 18:24:57 +0500

    key name updated

--- app/Http/Controllers/Admins/AdminReportsController.php
+++ app/Http/Controllers/Admins/AdminReportsController.php
@@ -17718,6 +17718,7 @@ class AdminReportsController extends Controller
                 $in_process_tat = $current_tat;
             }
 
+            $rider_name = '-';
             if ($delivery_note_shipment->exists()) {
                 $delivery_note_ids = $delivery_note_shipment->pluck('delivery_note_id')->toArray();
 
@@ -17751,29 +17752,29 @@ class AdminReportsController extends Controller
             $rowArray['shipper'] = ($rowArray['booking_type_id'] == 4) ? ($rowArray['shipper'] . ' (' . $rowArray['poc'] . ')') : $rowArray['shipper'];
 
             if (in_array($rowArray['shipper_status_id'], [68])) {
-                $rowArray['current_hub_name'] = $rowArray['misroutedCityname'];
+                $rowArray['concerned_hub_name'] = $rowArray['misroutedCityname'];
             } elseif (in_array($rowArray['shipper_status_id'], [49, 3]) && (in_array($rowArray['bag_status'], [3, 2, 4, 7, 8, 9, 6]))) {
                     //Shipment In Transit || Shipment Misrouted Forwarded concered hub change reference TO-6939
-                    $rowArray['current_hub_name'] = $rowArray['destination'];
+                    $rowArray['concerned_hub_name'] = $rowArray['destination'];
                 
             } elseif (in_array($rowArray['shipper_status_id'], [26, 73, 32, 70, 76]) && in_array($rowArray['bag_status'], [4, 7, 8, 9, 6])) {
                     //Shipment In Transit || Shipment Misrouted Forwarded concered hub change reference TO-6939
-                    $rowArray['current_hub_name'] = $rowArray['origin'];
+                    $rowArray['concerned_hub_name'] = $rowArray['origin'];
             } elseif (in_array($rowArray['shipper_status_id'], [18, 34, 23, 24, 47, 48, 2])) {
-                $rowArray['current_hub_name'] = $rowArray['origin'];
+                $rowArray['concerned_hub_name'] = $rowArray['origin'];
             } elseif (in_array($rowArray['shipper_status_id'], [54, 55, 69, 7, 4, 8])) {
-                $rowArray['current_hub_name'] = (($rowArray['intercepttype'] == 1) ?  $rowArray['intercept_city_name'] : $rowArray['destination']);
+                $rowArray['concerned_hub_name'] = (($rowArray['intercepttype'] == 1) ?  $rowArray['intercept_city_name'] : $rowArray['destination']);
             } elseif (in_array($rowArray['shipper_status_id'], [22, 21, 75]) && $rowArray['return_city'] != null) {
-                $rowArray['current_hub_name'] = $rowArray['return_city'];
+                $rowArray['concerned_hub_name'] = $rowArray['return_city'];
             }
             else {
-                if (!empty($rowArray['current_hub_id'])) {
-                    $rowArray['current_hub_name'] = $rowArray['current_hub_name'];
+                if (!empty($rowArray['concerned_hub'])) {
+                    $rowArray['concerned_hub_name'] = $rowArray['concerned_hub_name'];
                 }else{
                     if (in_array($rowArray['shipper_status_id'], [1, 2, 61])) {
-                        $rowArray['current_hub_name'] = $rowArray['origin'];
+                        $rowArray['concerned_hub_name'] = $rowArray['origin'];
                     } else {
-                        $rowArray['current_hub_name'] = $rowArray['hub'];
+                        $rowArray['concerned_hub_name'] = $rowArray['hub'];
                     }
                 }
             
