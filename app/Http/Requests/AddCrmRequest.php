d422fe131102679ad9b9f88cc6e33f0ec7b7598e
usamauddin <usama.uddin@trax.pk>
2025-05-20 16:45:06 +0500

    fix response CRM

--- app/Http/Requests/AddCrmRequest.php
+++ app/Http/Requests/AddCrmRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
 
 class AddCrmRequest extends FormRequest
 {
@@ -170,5 +172,14 @@ class AddCrmRequest extends FormRequest
             'receiving_sheet_id.gt'             => 'Receiving sheet ID must be greater than 0.',
 
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'status' => false,
+            'message' => 'Validation errors',
+            'errors' => $validator->errors()
+        ], 422));
     }
 }
