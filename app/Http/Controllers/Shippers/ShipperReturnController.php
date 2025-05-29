3908da1c284835f19d84e19db63fbbc2dcb38805
Anas <anas.mazhar@trax.pk>
2025-05-29 13:07:08 +0500

    return confirmed status again open

--- app/Http/Controllers/Shippers/ShipperReturnController.php
+++ app/Http/Controllers/Shippers/ShipperReturnController.php
@@ -744,7 +744,7 @@ class ShipperReturnController extends Controller
             ->leftJoin('shipment_status_reason as ssr', 'ssr.id', '=', 'shipments_journey.status_reason_id')
             ->select('shipments.id as shId', 'shipments.tracking_number', 'shipments.tracking_number as tracking', 'u.name as shipper', 'u.phone as shipper_phone1', 'u.phone2 as shipper_phone2', 'oc.name as origin', 'dc.name as destination', 'shipments.order_id', 'h.name as hub', 'shipments.consignee_name', 'shipments.consignee_phone_number_1', 'shipments.consignee_phone_number_2', 'shipments.consignee_address', 'shipments.amount', 'sm.mode', 'bt.booking_type as service_type', 'ss.name as status', 'ssr.id as reason_id', 'ssr.name as reason', 'shipments_journey.remarks as remarks', 'shipments_journey.created_at as status_date', 'shipments_journey.created_at as last_status_date', 'sj.created_at as arrival', 'shipments.booking_type_id', 'usi.poc', 'shipments_journey.remarks as shipper_remarks', 'shipments.shipper_status_id as current_status_id', 'shipments.nsa_osa_estimated_charges', 'shipments_journey.shipper_status_id as journey_shipper_status_id', 'dc.pickup as pickup', 'shipments.intercepted as intercepted', 'dc.id as consignee_city_id', 'shipments.shipping_mode_id')
             ->where('shipments.user_id', session('user_id'))
-//            ->where('shipments.shipper_status_id', DB::raw(20))
+           ->where('shipments.shipper_status_id', DB::raw(20))
             ->groupBy('shipments.id');
         if (session('user_type') == 2) {
             if (session('restriction') == 1) {
