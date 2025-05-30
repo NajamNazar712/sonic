1738e7f89b1039d7aeacb4c5aa82964c292b8a24
Anas <anas.mazhar@trax.pk>
2025-05-30 19:48:10 +0500

    working onit

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -197,7 +197,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('bulk:status-sharing-wallet')->withoutOverlapping()->cron('*/5 * * * *')->runInBackground();
+        $schedule->command('bulk:status-sharing-wallet')->withoutOverlapping()->everyMinute()->runInBackground();
 
         $schedule->command('create:service_ledger')->dailyAt('00:00')->runInBackground();
         // $schedule->command('job:run email 25000')->dailyAt('02:02')->runInBackground();
