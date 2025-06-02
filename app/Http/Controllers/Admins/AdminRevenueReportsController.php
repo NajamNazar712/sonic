a85d28f4464c8d7fa48d316c7e49dba5353fe95c
Najam <muhammad.najam@trax.pk>
2025-06-02 17:47:33 +0500

    dd added

--- app/Http/Controllers/Admins/AdminRevenueReportsController.php
+++ app/Http/Controllers/Admins/AdminRevenueReportsController.php
@@ -295,6 +295,7 @@ class AdminRevenueReportsController extends Controller
         Storage::disk('public')->put($filePath, $contents);
         $from = Carbon::parse($from)->toDateString();
         $to = Carbon::parse($to)->toDateString();
+        dd($filePath);
         return ['file_path' => $filePath, 'from' => $from, 'to' => $to];
     }
 
@@ -586,7 +587,7 @@ class AdminRevenueReportsController extends Controller
         Storage::disk('public')->put($filePath, $contents);
         $from = Carbon::parse($from)->toDateString();
         $to = Carbon::parse($to)->toDateString();
-
+        dd($filePath);
         return ['file_path' => $filePath, 'from' => $from, 'to' => $to];
     }
     public static function revenue_report_last_month($report_type)
