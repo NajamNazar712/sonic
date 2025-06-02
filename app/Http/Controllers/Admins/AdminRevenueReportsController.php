8dcebeb514322f77622080d23b0328f2cbb9a540
Anas <anas.mazhar@trax.pk>
2025-06-02 18:40:57 +0500

    working on debugging

--- app/Http/Controllers/Admins/AdminRevenueReportsController.php
+++ app/Http/Controllers/Admins/AdminRevenueReportsController.php
@@ -39,7 +39,7 @@ class AdminRevenueReportsController extends Controller
         }
         $from_id = DB::table('shipments_journey')->select(DB::raw('MIN(id) as id'))->where('created_at', '>=', $from)->first()->id;
         $to_id = DB::table('shipments_journey')->select(DB::raw('MAX(id) as id'))->where('created_at', '>=', $from)->where('created_at', '<=', $to)->first()->id;
-        dd($from_id, $to_id);
+        dd($from, $to);
         $connection = 'reports';
         $sales = DB::connection($connection)->table('shipments_journey as sj')
             ->join('shipments', 'shipments.id', 'sj.shipment_id')
