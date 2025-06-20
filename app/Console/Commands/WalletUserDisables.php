27e47a1bede80bd4b8c178f5f3a504a7129c324a
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2025-06-20 17:29:12 +0500

    changes

--- app/Console/Commands/WalletUserDisables.php
+++ app/Console/Commands/WalletUserDisables.php
@@ -34,7 +34,7 @@ class WalletUserDisables extends Command
         if ($this->hasArgument('user_id') && null !== $this->argument('user_id')) {
             $userId = explode(',', $this->argument('user_id'));
         } else {
-            $userId = [44309];
+            $userId = [44309,33952,27424];
         }
         $wallet_users = WalletUser::whereIn('user_id', $userId)->get();
         $success_delete = [];
