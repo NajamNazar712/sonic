c420d1b681c979974013142f9ef093cdf6947f1d
Anas <anas.mazhar@trax.pk>
2025-06-20 16:19:55 +0500

    working on debugging

--- app/Http/Controllers/Webhook/ShipmentStatusWebhookController.php
+++ app/Http/Controllers/Webhook/ShipmentStatusWebhookController.php
@@ -93,13 +93,13 @@ class ShipmentStatusWebhookController extends Controller
                     $payload['otp'] = $otp;
                 }
                 
-                if (!in_array($user_id, [30860, 12221, 5333, 13060])) {
+                if (!in_array($user_id, [30860, 12221, 5333])) {
                     $response = $client->post('', [
                         'form_params' => $payload
                     ]);
                 }
 
-                if (in_array($user_id, [30860, 12221, 5333, 13060])) {
+                if (in_array($user_id, [30860, 12221, 5333])) {
                     $response = $client->post('', [
                         'json' => $payload,
                     ]);
@@ -117,9 +117,9 @@ class ShipmentStatusWebhookController extends Controller
                 } else {
                     $status_code = 500;
                 }
-                // if ($user_id == 30860) {
-                //     Log::channel('botCallJobLog')->info('s ' . 'Webhook log Status-Code' . $status_code . "Payload" . json_encode($payload) ."User ID". $user_id);
-                // }
+                if ($user_id == 13060) {
+                    Log::channel('botCallJobLog')->info('s ' . 'Webhook log Status-Code' . $status_code . "Payload" . json_encode($payload) ."User ID". $user_id);
+                }
                 if (in_array($status_code, [200, 201, 202, 204])) {
                     break;
                 }
