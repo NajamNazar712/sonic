2face6870189848e5a9602da9ca9912d095a64ea
usamauddin <usama.uddin@trax.pk>
2025-06-27 20:02:03 +0500

    remove date cap filter on over all in process shipment

--- app/Http/Controllers/ShipperOrderManagementApiController.php
+++ app/Http/Controllers/ShipperOrderManagementApiController.php
@@ -117,8 +117,8 @@ class ShipperOrderManagementApiController extends Controller
         endif;
 
         // 1. Over all in process
-        $over_all_in_process = Shipment::whereNotIn('shipper_status_id', $shipment_status)
-            ->whereBetween('pickup_date', [$startDate, $endDate]);
+        $over_all_in_process = Shipment::whereNotIn('shipper_status_id', $shipment_status);
+//            ->whereBetween('pickup_date', [$startDate, $endDate]);
 
         if ($app_type == 2) {
             $over_all_in_process = $over_all_in_process
