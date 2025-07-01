629ebf5c8568f0caf3becc404062e8c0ad9d2ff5
Najam <muhammad.najam@trax.pk>
2025-07-01 14:38:48 +0500

    parent product changed to direct product id for wht and cod sst

--- app/Http/Controllers/Admins/AdminFinanceController.php
+++ app/Http/Controllers/Admins/AdminFinanceController.php
@@ -145,6 +145,8 @@ use Illuminate\Support\Facades\Http;
 use App\Jobs\CODAmountChangeSendToWallet;
 use App\Jobs\WalletBulkSettlementFromDonePayments;
 use App\Models\ParentProduct;
+use App\Http\Models\Product;
+
 class AdminFinanceController extends Controller
 {
     static public function sdn_action_logs($sdn_id, $status_id, $admin_id, $previous_bank_id = null, $new_bank_id = null, $previous_amount = null, $new_amount = null, $deposit_slip_image = null)
@@ -195,32 +197,41 @@ class AdminFinanceController extends Controller
 
     static public function wht($user_id, $amount, $packaging_request, $shipment_id){ 
 
-        $parent_product_id = User::where('id', $user_id)->value('parent_product_id');
-        // $arrival_date = ShipmentsJourney::where('shipment_id', $shipment_id)->where('shipper_status_id', 2)->whereDate('created_at', '>=', '2025-06-01');
-        if($parent_product_id  && $packaging_request == 0) {
+        $product_id = User::where('id', $user_id)->value('product_id');
 
-            $tax_percentage  = ParentProduct::where('id', $parent_product_id)->value('tax_percentage');
-            if($tax_percentage != 0) {
-                $tax_amount  = ($amount * floatval($tax_percentage)) / 100;
-                return $tax_amount;
-            }
-        } 
+        if($product_id) {
+
+            $parent_product_id = Product::where('id', $product_id )->value('parent_product_id');
+            // $arrival_date = ShipmentsJourney::where('shipment_id', $shipment_id)->where('shipper_status_id', 2)->whereDate('created_at', '>=', '2025-06-01');
+            if($parent_product_id  && $packaging_request == 0) {
+
+                $tax_percentage  = ParentProduct::where('id', $parent_product_id)->value('tax_percentage');
+                if($tax_percentage != 0) {
+                    $tax_amount  = ($amount * floatval($tax_percentage)) / 100;
+                    return $tax_amount;
+                }
+            } 
+        }
+        
         return 0;
     }
 
     static public function cod_sst($user_id, $amount, $packaging_request, $shipment_id){ 
 
-        $parent_product_id = User::where('id', $user_id)->value('parent_product_id');
-        // $arrival_date = ShipmentsJourney::where('shipment_id', $shipment_id)->where('shipper_status_id', 2)->whereDate('created_at', '>=', '2025-06-01');
-        if($parent_product_id && $packaging_request == 0) {
+        $product_id = User::where('id', $user_id)->value('product_id');
+        
+        if($product_id) {
 
-            $sst_percentage  = ParentProduct::where('id', $parent_product_id)->value('sst_percentage');
-            if($sst_percentage != 0) {
-                $cod_sst  = ($amount * floatval($sst_percentage)) / 100;
-                return $cod_sst;
-            }
-        } 
+            $parent_product_id = Product::where('id', $product_id )->value('parent_product_id');
+            if($parent_product_id && $packaging_request == 0) {
 
+                $sst_percentage  = ParentProduct::where('id', $parent_product_id)->value('sst_percentage');
+                if($sst_percentage != 0) {
+                    $cod_sst  = ($amount * floatval($sst_percentage)) / 100;
+                    return $cod_sst;
+                }
+            } 
+        }
         return 0;
     }
     
