088fb57a8979074d118f83f38120a67914450612
Najam <muhammad.najam@trax.pk>
2025-07-01 12:43:30 +0500

    arrival condition removed

--- app/Http/Controllers/Admins/AdminFinanceController.php
+++ app/Http/Controllers/Admins/AdminFinanceController.php
@@ -196,8 +196,8 @@ class AdminFinanceController extends Controller
     static public function wht($user_id, $amount, $packaging_request, $shipment_id){ 
 
         $parent_product_id = User::where('id', $user_id)->value('parent_product_id');
-        $arrival_date = ShipmentsJourney::where('shipment_id', $shipment_id)->where('shipper_status_id', 2)->whereDate('created_at', '>=', '2025-06-01');
-        if($parent_product_id  && $packaging_request == 0 && $arrival_date->exists()) {
+        // $arrival_date = ShipmentsJourney::where('shipment_id', $shipment_id)->where('shipper_status_id', 2)->whereDate('created_at', '>=', '2025-06-01');
+        if($parent_product_id  && $packaging_request == 0) {
 
             $tax_percentage  = ParentProduct::where('id', $parent_product_id)->value('tax_percentage');
             if($tax_percentage != 0) {
@@ -211,8 +211,8 @@ class AdminFinanceController extends Controller
     static public function cod_sst($user_id, $amount, $packaging_request, $shipment_id){ 
 
         $parent_product_id = User::where('id', $user_id)->value('parent_product_id');
-        $arrival_date = ShipmentsJourney::where('shipment_id', $shipment_id)->where('shipper_status_id', 2)->whereDate('created_at', '>=', '2025-06-01');
-        if($parent_product_id && $packaging_request == 0 && $arrival_date->exists()) {
+        // $arrival_date = ShipmentsJourney::where('shipment_id', $shipment_id)->where('shipper_status_id', 2)->whereDate('created_at', '>=', '2025-06-01');
+        if($parent_product_id && $packaging_request == 0) {
 
             $sst_percentage  = ParentProduct::where('id', $parent_product_id)->value('sst_percentage');
             if($sst_percentage != 0) {
