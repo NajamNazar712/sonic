c677020543270f4eadd553ad8285a77ad49a820d
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2025-07-03 00:30:20 +0500

    changes

--- app/Console/Commands/UpdateArrivalChargesCommand.php
+++ app/Console/Commands/UpdateArrivalChargesCommand.php
@@ -57,8 +57,8 @@ class UpdateArrivalChargesCommand extends Command
             ->where('pending_payment_shipments.type', 3)
 
             ->where(function ($query){
-                $query->whereNull('shipment_additional_charges.shipment_id')
-                    ->orWhere('shipment_additional_charges.faf_charges', 0);
+                $query->whereNull('shipment_additional_charges.shipment_id');
+//                    ->orWhere('shipment_additional_charges.faf_charges', 0);
             })
             ->whereNotNull('pending_payment_shipments.id')
             ->pluck('pending_payment_shipments.shipment_id')->toArray();
@@ -71,8 +71,8 @@ class UpdateArrivalChargesCommand extends Command
             ->where('users.account_type_id', 2)
             ->where('pending_invoice_shipments.type', 3)
             ->where(function ($query){
-                $query->whereNull('shipment_additional_charges.shipment_id')
-                    ->orWhere('shipment_additional_charges.faf_charges', 0);
+                $query->whereNull('shipment_additional_charges.shipment_id');
+//                    ->orWhere('shipment_additional_charges.faf_charges', 0);
             })
             ->whereNotNull('pending_invoice_shipments.id')
             ->pluck('pending_invoice_shipments.shipment_id')->toArray();
@@ -98,26 +98,26 @@ class UpdateArrivalChargesCommand extends Command
                 $shipment_id = $shipment->id;
                 if ($shipment->booking_type_id != 4) {
 
-                    $shipment_data = Shipment::find($shipment_id);
-                    if ($shipment_data->packaging_material_request == 0 && $shipment_data->shipment_type == 1) {
-                        if ($shipment_data->booking_type_id == 4) {
-                            ShipmentChargesController::walkin_weight($shipment_id);
-                        } else {
-                            ShipmentChargesController::weight($shipment_id);
-                            if($shipment_data->booking_type_id == 5){
-                                ShipmentChargesController::reverse_pickup($shipment_id);
-                            }
-                            if ($shipment_data->business_category_id == 1) {
-                                ShipmentChargesController::cash_handling($shipment_id);
-                                ShipmentChargesController::insurance($shipment_id);
-                                ShipmentChargesController::fuel_surcharge($shipment_id);
-                                ShipmentChargesController::faf_charges($shipment_id);
-                            } else {
-                                ShipmentChargesController::international_fuel_surcharge($shipment_id);
-                                ShipmentChargesController::international_faf_charges($shipment_id);
-                            }
-                        }
-                    }
+//                    $shipment_data = Shipment::find($shipment_id);
+//                    if ($shipment_data->packaging_material_request == 0 && $shipment_data->shipment_type == 1) {
+//                        if ($shipment_data->booking_type_id == 4) {
+//                            ShipmentChargesController::walkin_weight($shipment_id);
+//                        } else {
+//                            ShipmentChargesController::weight($shipment_id);
+//                            if($shipment_data->booking_type_id == 5){
+//                                ShipmentChargesController::reverse_pickup($shipment_id);
+//                            }
+//                            if ($shipment_data->business_category_id == 1) {
+//                                ShipmentChargesController::cash_handling($shipment_id);
+//                                ShipmentChargesController::insurance($shipment_id);
+//                                ShipmentChargesController::fuel_surcharge($shipment_id);
+//                                ShipmentChargesController::faf_charges($shipment_id);
+//                            } else {
+//                                ShipmentChargesController::international_fuel_surcharge($shipment_id);
+//                                ShipmentChargesController::international_faf_charges($shipment_id);
+//                            }
+//                        }
+//                    }
 
                     AdminFinanceController::update_payment($shipment_id, 3);
                 }
