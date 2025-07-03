6993dd36fe4b95c73080c2bbfe5059ad71f90eeb
Anas <anas.mazhar@trax.pk>
2025-07-03 14:21:56 +0500

    scheduler add export report

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -677,7 +677,7 @@ class Kernel extends ConsoleKernel
             $schedule->command('wallet_charges_update')->dailyAt($time)->runInBackground();
         }
         $schedule->command('export:shipment-report')
-            ->dailyAt('14:21')              
+            ->dailyAt('14:23')              
             ->withoutOverlapping()         // prevent simultaneous runs
             ->onOneServer()                // ensures single server execution
             ->runInBackground()            // runs non-blocking
