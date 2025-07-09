353e0ffa4691448eafcf6b0f465cd5f1500c012b
Anas <anas.mazhar@trax.pk>
2025-07-09 12:41:46 +0500

    working onit

--- app/Console/Commands/WalletUserDisables.php
+++ app/Console/Commands/WalletUserDisables.php
@@ -47,11 +47,11 @@ class WalletUserDisables extends Command
                 $wallet_user_disable = $user->replicate();
                 $wallet_user_disable->setTable('wallet_users_disables');
 
-                    $user->delete();
                 if ($wallet_user_disable->save()) {
+                    $user->delete();
                     WalletShipperSetting::where('user_id',$user->user_id)->delete();
-                }
                     $success_delete[] = $user->user_id;
+                }
             }
         }
 
