b29edad3ff04e20da5c6261240c1ad8e47b442a1
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2025-07-25 19:04:32 +0500

    changes

--- app/Http/Controllers/ShipperFinanceApiController.php
+++ app/Http/Controllers/ShipperFinanceApiController.php
@@ -29,9 +29,9 @@ class ShipperFinanceApiController extends Controller
 
 
         $shipper_payments = DB::table('done_payments')
-            ->join('users as u', 'done_payments.user_id', '=', 'u.id')
-            ->join('cities as c', 'u.city_id', '=', 'c.id')
-            ->join('done_payment_shipments as dps', 'done_payments.id', '=', 'dps.done_payment_id')
+            ->leftjoin('users as u', 'done_payments.user_id', '=', 'u.id')
+            ->leftjoin('cities as c', 'u.city_id', '=', 'c.id')
+            ->leftjoin('done_payment_shipments as dps', 'done_payments.id', '=', 'dps.done_payment_id')
             ->leftJoin('user_bank_infos as ubi', function ($join) {
                 $join->on('ubi.id', '=', 'done_payments.user_bank_info_id');
             })
@@ -56,11 +56,12 @@ class ShipperFinanceApiController extends Controller
     private function retail_payment_list($retail_id,$payment_id)
     {
 
-        $retail_payments = DB::table('retail_done_payments')->join('retail_shipper_infos as rsi', 'retail_done_payments.user_id', '=', 'rsi.id')
-            ->join('cities as c', 'rsi.city_id', '=', 'c.id')
-            ->join('retail_done_payment_shipments as rdps', 'retail_done_payments.id', '=', 'rdps.retail_done_payment_id')
-            ->join('retail_shipments as rs', 'rs.shipment_id', '=', 'rdps.shipment_id')
-            ->join('banks_lists as ub','ub.id','=','rsi.bank_id')
+        $retail_payments = DB::table('retail_done_payments')
+            ->leftjoin('retail_shipper_infos as rsi', 'retail_done_payments.user_id', '=', 'rsi.id')
+            ->leftjoin('cities as c', 'rsi.city_id', '=', 'c.id')
+            ->leftjoin('retail_done_payment_shipments as rdps', 'retail_done_payments.id', '=', 'rdps.retail_done_payment_id')
+            ->leftjoin('retail_shipments as rs', 'rs.shipment_id', '=', 'rdps.shipment_id')
+            ->leftjoin('banks_lists as ub','ub.id','=','rsi.bank_id')
              ->select('retail_done_payments.id','retail_done_payments.id as payment_id', 'rsi.id as user_id', 'rsi.shipper_name as shipper', 'c.name as city', 'rsi.shipper_phone_no as phone', 'rsi.shipper_address as address', 'retail_done_payments.total_shipments', 'retail_done_payments.delivered_shipments', 'retail_done_payments.adjusted_shipments', DB::raw('SUM(rdps.amount) as total_amount') ,DB::raw('SUM(rs.gst) as total_gst'),DB::raw('SUM(rs.wht) as total_wht'),DB::raw('SUM(rs.cod_sst) as total_cod_sst'), DB::raw('SUM(rdps.payable) as total_payable'), 'retail_done_payments.ibft_charges','ub.name as company_bank', 'retail_done_payments.reference_number', 'retail_done_payments.created_at as done_at', 'retail_done_payments.status')
             ->where('retail_done_payments.user_id',$retail_id)
             ->groupBy('retail_done_payments.id');
