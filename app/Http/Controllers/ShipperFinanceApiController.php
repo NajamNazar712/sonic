646e61f3dd79816f9cd2d5901283e7c5d402256d
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2025-07-25 19:02:35 +0500

    changes

--- app/Http/Controllers/ShipperFinanceApiController.php
+++ app/Http/Controllers/ShipperFinanceApiController.php
@@ -28,7 +28,8 @@ class ShipperFinanceApiController extends Controller
     {
 
 
-        $shipper_payments = DB::table('done_payments')->join('users as u', 'done_payments.user_id', '=', 'u.id')
+        $shipper_payments = DB::table('done_payments')
+            ->join('users as u', 'done_payments.user_id', '=', 'u.id')
             ->join('cities as c', 'u.city_id', '=', 'c.id')
             ->join('done_payment_shipments as dps', 'done_payments.id', '=', 'dps.done_payment_id')
             ->leftJoin('user_bank_infos as ubi', function ($join) {
@@ -39,13 +40,7 @@ class ShipperFinanceApiController extends Controller
                     ->where('ubi_default.default_bank', DB::raw(1));
             })
             ->leftJoin('banks_lists as ub', function ($join) {
-                $join->where(function ($sub_query) {
-                    $sub_query->whereNotNull('done_payments.user_bank_info_id')
-                        ->where('ubi.bank_name', '=', DB::raw('`ub`.`id`'));
-                })->orWhere(function ($sub_query) {
-                    $sub_query->whereNull('done_payments.user_bank_info_id')
-                        ->where('ubi_default.bank_name', '=', DB::raw('`ub`.`id`'));
-                });
+                $join->on('ub.id', '=', DB::raw('COALESCE(ubi.bank_name, ubi_default.bank_name)'));
             })
             ->leftjoin('banks_lists as b', 'done_payments.company_bank_id', '=', 'b.id')
             ->select('done_payments.id','done_payments.id as payment_id', 'u.id as user_id', 'u.name as shipper', 'c.name as city', 'u.phone', 'u.phone2', 'u.address', 'done_payments.total_shipments', 'done_payments.delivered_shipments', 'done_payments.returned_shipments', 'done_payments.adjusted_shipments', 'done_payments.arrival_shipment as arrival_shipments', DB::raw('SUM(dps.amount) as total_amount'), DB::raw('SUM(dps.charges) as total_charges'), DB::raw('SUM(dps.gst) as total_gst'),DB::raw('SUM(dps.wht) as total_wht'),DB::raw('SUM(dps.cod_sst) as total_cod_sst'), DB::raw('SUM(dps.payable) as total_payable'), 'done_payments.ibft_charges', 'ub.name as bank', 'done_payments.reference_number', 'done_payments.created_at as done_at', 'b.name as company_bank', 'done_payments.status')
