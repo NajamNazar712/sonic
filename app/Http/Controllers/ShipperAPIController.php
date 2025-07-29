ad31c473de84810e4255ee4f51550f900bd8eedc
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2025-07-29 18:25:21 +0500

    changes

--- app/Http/Controllers/ShipperAPIController.php
+++ app/Http/Controllers/ShipperAPIController.php
@@ -1566,14 +1566,14 @@ class ShipperAPIController extends Controller
         $date = Carbon::today();
         $user_id = $request->shipper_id;
         $user = User::find($user_id);
-        $booking_types = BookingType::whereNotIn('id', [4, 6])->get();
+//        $booking_types = BookingType::whereNotIn('id', [4, 6])->get();
         $user_shipping_address = UserShippingInfo::join('cities as c', 'c.id', '=', 'user_shipping_infos.city_id')
             ->where('user_shipping_infos.user_id', $user_id)->where('user_shipping_infos.status', 1)
             ->where('user_shipping_infos.hidden', 0)
-            ->select('user_shipping_infos.*', 'c.name as city_name')
+            ->select('user_shipping_infos.id','user_shipping_infos.pickup_address', 'c.name as city_name')
             ->get();
         $multi_piece = $user->multipiece_status;
-        $cities = City::where('pickup', 1)->where('status', 1)->where('business_category_id', 1)->whereNotNull('zone_id')->orderBy('name')->get();
+//        $cities = City::where('pickup', 1)->where('status', 1)->where('business_category_id', 1)->whereNotNull('zone_id')->orderBy('name')->get();
         if (in_array($user_id, [5982, 3324, 10104, 14110, 16292])) {
             $consignee_cities = City::where('status', 1)->where('business_category_id', 1)->whereNotNull('zone_id')->orderBy('name')->get();
         } else {
@@ -1593,8 +1593,8 @@ class ShipperAPIController extends Controller
         } else {
             $payment_modes = PaymentMode::whereNotIn('id', [2, 3])->get();
         }
-        $check = NonServiceArea::pluck('name')->toArray();
-        $charges_modes = ChargesModes::whereIn('id', [4])->get();
+//        $check = NonServiceArea::pluck('name')->toArray();
+//        $charges_modes = ChargesModes::whereIn('id', [4])->get();
 //        $air_waybill = ShipperAirWaybillSettings::where('user_id', $user_id);
 //        if ($air_waybill->exists()) {
 //            $air_waybill = $air_waybill->first();
@@ -1624,7 +1624,7 @@ class ShipperAPIController extends Controller
             }
         }
 
-        return response()->json(['status' => 0, 'shipping_address' => $user_shipping_address, 'user' => $user, 'multi_piece' => $multi_piece, 'cities' => $cities, 'products' => $products, 'shipping_mode_same_day_timings' => $shipping_mode_same_day_timings, 'payment_modes' => $payment_modes, 'consignee_cities' => $consignee_cities, 'check' => $check, 'charges_modes' => $charges_modes, 'date' => $date, 'air_waybill' => $air_waybill, 'omni_user' => $omni_user, 'booking_types' => $booking_types]);
+        return response()->json(['status' => 0, 'shipping_address' => $user_shipping_address, 'user' => $user, 'multi_piece' => $multi_piece, 'cities' => [], 'products' => $products, 'shipping_mode_same_day_timings' => $shipping_mode_same_day_timings, 'payment_modes' => $payment_modes, 'consignee_cities' => $consignee_cities, 'check' => [], 'charges_modes' => [], 'date' => $date, 'air_waybill' => $air_waybill, 'omni_user' => $omni_user, 'booking_types' => []]);
     }
 
     public function reimbursement_shipping_modes(Request $request)
