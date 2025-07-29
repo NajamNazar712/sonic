458bc4b39c6e8cf2dcaebeaffb84420453af7ac4
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2025-07-29 16:14:44 +0500

    changes

--- app/Http/Controllers/ShipperFinanceApiController.php
+++ app/Http/Controllers/ShipperFinanceApiController.php
@@ -46,9 +46,9 @@ class ShipperFinanceApiController extends Controller
             })
             ->leftjoin('banks_lists as b', 'done_payments.company_bank_id', '=', 'b.id')
             ->select(
-                DB::raw('GROUP_CONCAT(CASE WHEN rdps.type = 0 THEN s.tracking_number END SEPARATOR ", ") as delivered_tracking_numbers'),
-                DB::raw('GROUP_CONCAT(CASE WHEN rdps.type = 1 THEN s.tracking_number END SEPARATOR ", ") as return_tracking_numbers'),
-                DB::raw('GROUP_CONCAT(CASE WHEN rdps.type = 2 THEN s.tracking_number END SEPARATOR ", ") as adjusted_tracking_numbers'),
+                DB::raw('GROUP_CONCAT(CASE WHEN dps.type = 0 THEN TRIM(s.tracking_number) END SEPARATOR ",") as delivered_tracking_numbers'),
+                DB::raw('GROUP_CONCAT(CASE WHEN dps.type = 1 THEN TRIM(s.tracking_number) END SEPARATOR ",") as return_tracking_numbers'),
+                DB::raw('GROUP_CONCAT(CASE WHEN dps.type = 2 THEN TRIM(s.tracking_number) END SEPARATOR ",") as adjusted_tracking_numbers'),
                 'done_payments.id','done_payments.id as payment_id', 'u.id as user_id', 'u.name as shipper', 'c.name as city', 'u.phone', 'u.phone2', 'u.address', 'done_payments.total_shipments', 'done_payments.delivered_shipments', 'done_payments.returned_shipments', 'done_payments.adjusted_shipments', 'done_payments.arrival_shipment as arrival_shipments', DB::raw('SUM(dps.amount) as total_amount'), DB::raw('SUM(dps.charges) as total_charges'), DB::raw('SUM(dps.gst) as total_gst'),DB::raw('SUM(dps.wht) as total_wht'),DB::raw('SUM(dps.cod_sst) as total_cod_sst'), DB::raw('SUM(dps.payable) as total_payable'), 'done_payments.ibft_charges', 'ub.name as bank', 'done_payments.reference_number', 'done_payments.created_at as done_at', 'b.name as company_bank', 'done_payments.status')
             ->where('done_payments.user_id',$shipper_id)
             ->where('done_payments.created_at' ,'>=',Carbon::now()->subMonths(12)->startOfMonth())
@@ -86,9 +86,9 @@ class ShipperFinanceApiController extends Controller
             ->leftjoin('shipments as s', 's.id', '=', 'rs.shipment_id')
             ->leftjoin('banks_lists as ub','ub.id','=','rsi.bank_id')
              ->select(
-                 DB::raw('GROUP_CONCAT(CASE WHEN rdps.type = 0 THEN s.tracking_number END SEPARATOR ", ") as delivered_tracking_numbers'),
-                 DB::raw('GROUP_CONCAT(CASE WHEN rdps.type = 1 THEN s.tracking_number END SEPARATOR ", ") as return_tracking_numbers'),
-                 DB::raw('GROUP_CONCAT(CASE WHEN rdps.type = 2 THEN s.tracking_number END SEPARATOR ", ") as adjusted_tracking_numbers'),
+                 DB::raw('GROUP_CONCAT(CASE WHEN rdps.type = 0 THEN TRIM(s.tracking_number) END SEPARATOR ",") as delivered_tracking_numbers'),
+                 DB::raw('GROUP_CONCAT(CASE WHEN rdps.type = 1 THEN TRIM(s.tracking_number) END SEPARATOR ",") as return_tracking_numbers'),
+                 DB::raw('GROUP_CONCAT(CASE WHEN rdps.type = 2 THEN TRIM(s.tracking_number) END SEPARATOR ",") as adjusted_tracking_numbers'),
                  'retail_done_payments.id','retail_done_payments.id as payment_id', 'rsi.id as user_id', 'rsi.shipper_name as shipper', 'c.name as city', 'rsi.shipper_phone_no as phone', 'rsi.shipper_address as address', 'retail_done_payments.total_shipments', 'retail_done_payments.delivered_shipments', 'retail_done_payments.adjusted_shipments', DB::raw('SUM(rdps.amount) as total_amount') ,DB::raw('0.0 as total_gst'),DB::raw('SUM(rs.wht) as total_wht'),DB::raw('SUM(rs.cod_sst) as total_cod_sst'), DB::raw('SUM(rdps.payable) as total_payable'), 'retail_done_payments.ibft_charges','ub.name as company_bank', 'retail_done_payments.reference_number', 'retail_done_payments.created_at as done_at', 'retail_done_payments.status')
             ->where('retail_done_payments.user_id',$retail_id)
             ->where('retail_done_payments.created_at' ,'>=',Carbon::now()->subMonths(12)->startOfMonth())
