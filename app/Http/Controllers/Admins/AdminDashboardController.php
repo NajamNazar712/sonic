915c65a7c74c0f55246a48fef1d6eb067f981b95
Najam <muhammad.najam@trax.pk>
2025-07-30 14:49:20 +0500

    user id search added

--- app/Http/Controllers/Admins/AdminDashboardController.php
+++ app/Http/Controllers/Admins/AdminDashboardController.php
@@ -1343,7 +1343,7 @@ class AdminDashboardController extends Controller
             ->where('users.name', 'like', '%' . $keyword . '%');
 
         } else {
-            $shippers = User::where('name', 'like', '%' . $keyword . '%');
+            $shippers = User::where('name', 'like', '%' . $keyword . '%')->orWhere('id', 'like', '%' . $keyword . '%');
         }
         
         if($type == 'active')
