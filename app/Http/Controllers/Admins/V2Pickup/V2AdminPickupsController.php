cffa61ed301d1719e65fc7f51f5ecbe3564be97f
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2025-07-30 18:00:07 +0500

    changes

--- app/Http/Controllers/Admins/V2Pickup/V2AdminPickupsController.php
+++ app/Http/Controllers/Admins/V2Pickup/V2AdminPickupsController.php
@@ -2410,23 +2410,20 @@ class V2AdminPickupsController extends Controller
 
     public function arrival_individual_shipment_remove(Request $request)
     {
-        $shipment = DB::table('shipments')->where('id', $request->id)->first();
+        $start = microtime(true);
+        $shipment = Shipment::find($request->id);
 
         if ($shipment) {
-            $validStatuses = [1, 17, 53, 61, 62, 64];
+            if ($shipment->shipper_status_id == 1 || $shipment->shipper_status_id == 17 || $shipment->shipper_status_id == 53 || $shipment->shipper_status_id == 61 || $shipment->shipper_status_id == 62 || $shipment->shipper_status_id == 64) {
+                $shipment->actual_weight = null;
+                $shipment->length = null;
+                $shipment->breadth = null;
+                $shipment->height = null;
 
-            if (in_array($shipment->shipper_status_id, $validStatuses)) {
-                DB::table('shipments')
-                    ->where('id', $request->id)
-                    ->update([
-                        'actual_weight' => null,
-                        'length' => null,
-                        'breadth' => null,
-                        'height' => null,
-                        'updated_at' => now(), // optional: only if your table uses timestamps
-                    ]);
 
-                return ['status' => 0, 'success' => 'Shipment has been removed'];
+                $shipment->save();
+
+                return ['status' => 0, 'success' => 'Shipment has been removed', 'test' => 'Execution Time: ' . ($end - $start) . ' seconds'];
             } else {
                 return ['status' => 1, 'error' => 'Given Shipment ID has already been modified'];
             }
