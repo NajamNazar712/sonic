9c773036190eff4db5571d711a4b6021d0baeaed
usamauddin <usama.uddin@trax.pk>
2025-08-01 18:58:18 +0500

    fixed

--- resources/views/admin/settings/geocodes/index.blade.php
+++ resources/views/admin/settings/geocodes/index.blade.php
@@ -665,6 +665,7 @@
                     if(data.status){
                         table.rows().deselect();
                         table.draw(true);
+                        selected_rows=[];
                         toastr.success(data.success, 'Success!', {positionClass: 'toast-bottom-center', containerId: 'toast-bottom-center'});
                     } else{
                         toastr.error(data.error, 'Error!', {positionClass: 'toast-top-center', containerId: 'toast-top-center'});
