c051a68781adfd98daaf77a713b92ee691f4985d
Anas <anas.mazhar@trax.pk>
2025-08-03 03:36:25 +0500

    change

--- app/Console/Commands/OptimizeTable.php
+++ app/Console/Commands/OptimizeTable.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class OptimizeTable extends Command
 {
