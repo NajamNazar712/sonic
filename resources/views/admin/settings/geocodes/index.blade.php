2475ea64d5cccaaa80cd1ab2cec521720228f5e4
usamauddin <usama.uddin@trax.pk>
2025-08-04 19:18:03 +0500

    fix

--- resources/views/admin/settings/geocodes/index.blade.php
+++ resources/views/admin/settings/geocodes/index.blade.php
@@ -625,7 +625,7 @@
                     if(data.status){
                         table.rows().deselect();
                         table.draw(true);
-                        selected_rows=[];
+                        selected_rows_geocord_array=[];
                         toastr.success(data.success, 'Success!', {positionClass: 'toast-bottom-center', containerId: 'toast-bottom-center'});
                     } else{
                         toastr.error(data.error, 'Error!', {positionClass: 'toast-top-center', containerId: 'toast-top-center'});
