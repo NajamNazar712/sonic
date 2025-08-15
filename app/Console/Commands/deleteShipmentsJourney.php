1ed45933b1fb62650bef396b03fb5b9eea7a3faa
Anas <anas.mazhar@trax.pk>
2025-08-15 21:58:27 +0500

    changes

--- app/Console/Commands/deleteShipmentsJourney.php
+++ app/Console/Commands/deleteShipmentsJourney.php
@@ -107,7 +107,7 @@ class deleteShipmentsJourney extends Command
             // 3. Update shipments table
             DB::table('shipments')
                 ->join(DB::raw('(
-        SELECT shipment_id, shipper_status_id
+        SELECT shipment_id, shipper_status_id,consignee_status_id
         FROM shipments_journey
         WHERE id IN (
             SELECT MAX(id)
@@ -117,7 +117,8 @@ class deleteShipmentsJourney extends Command
     ) as latest'), 'shipments.id', '=', 'latest.shipment_id')
                 ->whereIn('shipments.id', $idsChunk)
                 ->update([
-                    'shipments.status_id' => DB::raw('latest.shipper_status_id')
+                'shipments.shipper_status_id' => DB::raw('latest.shipper_status_id'),
+                'shipments.consignee_status_id' => DB::raw('latest.consignee_status_id')
                 ]);
         }
     }
