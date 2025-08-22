5ad19e98f7b785a270bed3196e234d5d889cb098
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2025-08-22 14:33:46 +0500

    changes

--- app/Http/Controllers/Admins/AdminReportsEmailController.php
+++ app/Http/Controllers/Admins/AdminReportsEmailController.php
@@ -3361,7 +3361,7 @@ class AdminReportsEmailController extends Controller
             ->select($select)
             ->groupBy('shipments.id')
             ->get();
-        
+
         // $receive_deliveries_report_array[] = ['Quality of Service Report'];
         $sales_array['header'] = 
         [
