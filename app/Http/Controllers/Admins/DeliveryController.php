298ff0b7f0ab8eb992e8658eb93c8aea34f391a3
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2025-08-25 13:38:08 +0500

    changes

--- app/Http/Controllers/Admins/DeliveryController.php
+++ app/Http/Controllers/Admins/DeliveryController.php
@@ -326,19 +326,21 @@ class DeliveryController extends Controller
                 'last_screen_location.name as last_location_screen_location_name',
                 'ssj_last_location.entry_method as entry_method'
             )
-
+            ->when(
+                $request->get('search_date_from') && $request->get('search_date_to'),
+                function ($query) use ($request) {
+                    $from = $request->get('search_date_from');
+                    $to   = $request->get('search_date_to');
+                    $query->whereBetween('shipments.created_at', [$from, $to]);
+                }
+             )
+            ->whereIn('shipments.shipper_status_id', $status)
             ->whereRaw('IF (shipments.shipper_status_id IN (2, 49), (oc.hub_id = dc.hub_id), TRUE)')
             ->whereRaw('IF (shipments.shipper_status_id = 55, (irrh.old_consignee_city_id = irrh.new_consignee_city_id), TRUE)')
             ->whereRaw('IF (shipments.shipper_status_id = 55, (irrh.old_consignee_city_id = irrh.new_consignee_city_id), TRUE)')
-            ->whereIn('shipments.shipper_status_id', $status)
             ->whereNotNull('shipments.tracking_number')
             ->groupBy('shipments.id');
 
-        if ($request->get('search_date_from') && $request->get('search_date_to')) {
-            $from = $request->get('search_date_from');
-            $to = $request->get('search_date_to');
-            $shipments->whereBetween('shipments.created_at', [$from, $to]);
-        }
 
         if (session('role_id') != 1) {
             $shipments = $shipments->whereIn('dc.hub_id', session('hubs'));
