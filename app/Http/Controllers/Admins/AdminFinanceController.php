a87d75fa34f0392461ecb64ac5cb5e90b9582271
Anas <anas.mazhar@trax.pk>
2025-08-29 19:08:00 +0500

    changes

--- app/Http/Controllers/Admins/AdminFinanceController.php
+++ app/Http/Controllers/Admins/AdminFinanceController.php
@@ -19882,7 +19882,7 @@ class AdminFinanceController extends Controller
 
         $delivered_invoice_users = CorporateUserOnDeliveredInvoice::where('status', 1)
         ->pluck('user_id')
-        ->toArray();
+        ->toArray(); 
         dd($shipment['user_id'],$shipment->user_id);
         if(in_array($type, [3,1]) && !empty($delivered_invoice_users) && in_array($shipment->user_id , $delivered_invoice_users)) {
             return;
