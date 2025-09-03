8699478307928e3306d4920cad2560418f360b5f
mohammad <muhammad.ahmed@trax.pk>
2025-09-03 11:17:31 +0500

    done

--- app/Http/Controllers/Admins/AdminDashboardController.php
+++ app/Http/Controllers/Admins/AdminDashboardController.php
@@ -15400,15 +15400,15 @@ $zero_cod_discount = HistoryZeroCodDiscountCharges::all()->where('user_id', $id)
                             $actual_commission += $request->commission_percentage[$row_id];
                             $sales_commission_user->save();
 
-                        if($tier == 3){
-                            AccountTaggingLog::logTagging(
-                                $shipper_id,        
-                                Auth::id(),             
-                                $old_kam_id,                 
-                                $sales_commission_user->user_id, 
-                                3                
-                            );
-                        }
+                            if($tier == 3){
+                                AccountTaggingLog::logTagging(
+                                    $shipper_id,        
+                                    Auth::id(),             
+                                    $old_kam_id,                 
+                                    $sales_commission_user->user_id, 
+                                    3                
+                                );
+                            }
                         }
                     }
 
