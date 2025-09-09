45108afdb4f1aa997c4e3953dfd9ec2b06c3cea9
Anas <anas.mazhar@trax.pk>
2025-09-09 14:34:32 +0500

    chnages

--- app/Http/Controllers/Rider/RiderAPIController.php
+++ app/Http/Controllers/Rider/RiderAPIController.php
@@ -11935,7 +11935,7 @@ class RiderAPIController extends Controller
             $subReasonId = $reason->sub_id;
             $subReasonName = $reason->sub_name;
         
-            if (!isset($reasons[$reasonId])) {
+            if (!isset($reasons[$reasonId]) && $reasonId != 3) {
                 $reasons[$reasonId] = [
                     'id' => $reasonId,
                     'name' => $reasonName,
