dc0472f468d7b88db607ffba6eb024dd9ee15dea
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2025-09-12 13:32:04 +0500

    changes

--- app/Http/Controllers/Admins/AdminReportsController.php
+++ app/Http/Controllers/Admins/AdminReportsController.php
@@ -8253,14 +8253,14 @@ class AdminReportsController extends Controller
                 ->limit(1)->value('id');
 
             $sj_from_id = DB::connection($connection)->table('shipments_journey')
-                ->where('created_at', '>=', $from)
-                ->where('created_at', '<=',  $to)
+                ->where('created_at', '>=',   Carbon::parse($from)->subDay(10)->startOfDay())
+                ->where('created_at', '<=',    Carbon::parse($from)->addDay(10)->startOfDay())
                 ->orderBy('created_at', 'asc')->orderBy('id', 'asc')
                 ->limit(1)->value('id');
 
             $sj_to_id = DB::connection($connection)->table('shipments_journey')
-                ->where('created_at', '>=', $from)
-                ->where('created_at', '<=',  $to)
+                ->where('created_at', '>=',   Carbon::parse($from)->subDay(10)->startOfDay())
+                ->where('created_at', '<=',    Carbon::parse($from)->addDay(10)->startOfDay())
                 ->orderBy('created_at', 'desc')->orderBy('id', 'desc')
                 ->limit(1)->value('id');
 
