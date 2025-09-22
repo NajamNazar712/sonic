7e1ccf3cd41500208d3159c2eb5c323f0dea1c1b
Anas <anas.mazhar@trax.pk>
2025-09-22 12:47:31 +0500

    changes

--- app/Http/Controllers/Admins/AdminTrackingController.php
+++ app/Http/Controllers/Admins/AdminTrackingController.php
@@ -2784,7 +2784,7 @@ class AdminTrackingController extends Controller
             ->limit(1)->value('id');
 
         $arrived_sj_to_id = $sj_to_id;
-        $shipment_positions =   DB::connection('reports_2')->table('shipment_positions')->leftJoin('shipments as s','s.id','=','shipment_positions.shipment_id')
+        $shipment_positions =   DB::connection($connection)->table('shipment_positions')->leftJoin('shipments as s','s.id','=','shipment_positions.shipment_id')
         ->leftJoin('users as u','u.id','=','s.user_id')
         ->leftJoin('shipments_journey as sj','sj.shipment_id','=','shipment_positions.shipment_id')
         ->leftJoin('admins as a','a.id','=','sj.admin_id')
