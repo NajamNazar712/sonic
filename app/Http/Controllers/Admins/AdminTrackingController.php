ca8a9cc94b1096cbd1e1e2322264901f1593a683
Anas <anas.mazhar@trax.pk>
2025-09-22 16:46:23 +0500

    changes

--- app/Http/Controllers/Admins/AdminTrackingController.php
+++ app/Http/Controllers/Admins/AdminTrackingController.php
@@ -2770,7 +2770,7 @@ class AdminTrackingController extends Controller
             ActivityTrailController::createActivityTrailLog(Auth::id(),616);
         }
         $to   = Carbon::now()->endOfDay();
-        $from = "2025-09-15 23:59:59";
+        $from = Carbon::now()->subMonth(6)->startOfDay();
         $sj_from_id = DB::connection($connection)->table('shipments_journey')
             ->where('created_at', '>=',   Carbon::parse($from)->subDay(10)->startOfDay())
             ->where('created_at', '<=',    Carbon::parse($to)->addDay(10)->startOfDay())
