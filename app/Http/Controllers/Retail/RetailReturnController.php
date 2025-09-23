ba82d47b337b524beb8b711c2d1e499456bf109f
Anas <anas.mazhar@trax.pk>
2025-09-23 15:16:01 +0500

    changes

--- app/Http/Controllers/Retail/RetailReturnController.php
+++ app/Http/Controllers/Retail/RetailReturnController.php
@@ -77,7 +77,7 @@ class RetailReturnController extends Controller
                         DB::raw('(select consolidation_id from consolidation_shipments where consolidation_shipments.shipment_id = shipments.id)'));
             })
             ->select('rs.retail_user_id as retail_user_id','shipments.id as shId','shipments.tracking_number','shipments.tracking_number as tracking','u.name as shipper','u.phone as shipper_phone1','u.phone2 as shipper_phone2','oc.name as origin','dc.name as destination','shipments.order_id','h.name as hub','shipments.consignee_name','shipments.consignee_phone_number_1','shipments.consignee_phone_number_2','shipments.consignee_address','shipments.amount','sm.mode','bt.booking_type as service_type','ss.name as status','shipments_journey.remarks as remarks','ssr.id as reason_id','ssr.name as reason','shipments_journey.created_at as status_date','shipments_journey.created_at as last_status_date','sj.created_at as arrival', 'shipments.shipper_status_id as shipper_status_id', 'shipments_journey.shipper_status_id as journey_shipper_status_id', 'dc.pickup as pickup', 'shipments.intercepted as intercepted','shipments.nsa_osa_estimated_charges', 'consolidations.consolidation_id')
-            ->where('shipments.shipper_status_id', DB::raw(12))
+            ->where('shipments.shipper_status_id', DB::raw(65))
             ->where('rs.retail_user_id', Auth::id())
             ->whereBetween('rs.created_at', [$from, $to])
             ->groupBy('shipments.id');
