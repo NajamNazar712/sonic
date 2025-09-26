3056e31e2057ea6c204645065fe5a3df81d7762a
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2025-09-26 18:59:03 +0500

    changes

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -11670,7 +11670,7 @@ class NotificationsController extends Controller
         }
         $to = $phone_number;
         self::sms_otp($body, $to, "Consignee", $pin, 1);
-    }
+    }p
 
     static public function app_notification($id, $employee_id, $employee_type, $reference1_id, $reference2_id = NULL)
     {
@@ -11984,6 +11984,21 @@ class NotificationsController extends Controller
                     if (strpos($body, '[tracking_number]') !== FALSE) {
 
                         $body = str_replace('[tracking_number]', $tracking->tracking_number, $body);
+                    }
+                    self::push_notification($employee_id, $employee_type, $title, $body);
+                }else if ($id == 23) {
+                    $tracking = Shipment::find($reference1_id);
+                    if (strpos($body, '[tracking_number]') !== FALSE) {
+
+                        $body = str_replace('[tracking_number]', $tracking->tracking_number, $body);
+                    }
+                    if (strpos($body, '[old_amount]') !== FALSE) {
+
+                        $body = str_replace('[old_amount]', $reference2_id, $body);
+                    }
+                    if (strpos($body, '[new_amount]') !== FALSE) {
+
+                        $body = str_replace('[new_amount]', $tracking->amount, $body);
                     }
                     self::push_notification($employee_id, $employee_type, $title, $body);
                 }
