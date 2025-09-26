ff482edb8e46e997248ffb210d1d149207e8c594
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2025-09-26 19:04:51 +0500

    changes

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -11045,7 +11045,7 @@ class APIController extends Controller
 
         $allowedIps = [];
         if (config('app.env') === 'staging') {
-            $allowedIps = ['164.90.252.105','103.244.178.3','134.209.126.19'];
+            $allowedIps = ['164.90.252.105','103.244.178.3','134.209.126.19','72.255.0.55','110.93.236.91'];
         } elseif (config('app.env') === 'production') {
             $allowedIps = ['3.23.216.198', '18.118.233.146','103.244.178.3'];
         }
