c5d0716b70869ce48206ca2f64e4608482e214cc
Anas <anas.mazhar@trax.pk>
2025-09-29 14:33:07 +0500

    changes

--- app/Console/Commands/ArchiveReplacementParcel.php
+++ app/Console/Commands/ArchiveReplacementParcel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use DateTime;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
@@ -13,15 +14,24 @@ class ArchiveReplacementParcel extends Command
 
     public function handle()
     {
-        $sourcePath = storage_path('app/public/replacement_archive_final');
-        $months = ['2024-04', '2024-05', '2024-06', '2024-07', '2024-08', '2024-09'];
+        $sourcePath = storage_path('app/public/crm_claims_archieve');
+
+        // Generate months dynamically between 2023-11 and 2024-09
+        $start = new DateTime('2023-11');
+        $end   = new DateTime('2024-09');
+
+        $months = [];
+        while ($start <= $end) {
+            $months[] = $start->format('Y-m');
+            $start->modify('+1 month');
+        }
 
         foreach ($months as $month) {
             $zipFile = $sourcePath . '/' . $month . '.zip';
 
             if (File::exists($zipFile)) {
                 // Upload to S3
-                $s3Path = "sonic_storage_archieve/replacement_parcel/{$month}.zip";
+                $s3Path = "sonic_storage_archieve/crm_claims/{$month}.zip";
                 $stream = fopen($zipFile, 'r+');
                 Storage::disk('s3')->put($s3Path, $stream);
                 fclose($stream);
