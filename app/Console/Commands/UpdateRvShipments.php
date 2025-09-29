8c91ee293e08b8a91992e9e0b96525feaa3c1793
Anas <anas.mazhar@trax.pk>
2025-09-29 16:47:30 +0500

    changes

--- app/Console/Commands/UpdateRvShipments.php
+++ app/Console/Commands/UpdateRvShipments.php
@@ -108,11 +108,11 @@ class UpdateRvShipments extends Command
                 $join->on('s.id', '=', 'rst.shipment_id')
                     ->where('rst.permanent_disable', 1);
             })
-            ->join('shipments_journey as sj', 'sj.shipment_id', '=', 's.id')
-            ->where('sj.shipper_status_id', 12)
-            ->where('sj.verification', 1)
-            ->where('sj.created_at', '>=',  "2025-09-15"  . ' 00:00:00')
-            ->where('sj.updated_at', '<=',  "2025-09-21"  . ' 23:59:59')
+            // ->join('shipments_journey as sj', 'sj.shipment_id', '=', 's.id')
+            // ->where('sj.shipper_status_id', 12)
+            // ->where('sj.verification', 1)
+            // ->where('sj.created_at', '>=',  $yesterday  . ' 00:00:00')
+            // ->where('sj.updated_at', '<=',  $yesterday  . ' 23:59:59')
             ->where('s.shipper_status_id', 12)
             ->select('s.id','s.user_id','s.tracking_number')
             ->get();
