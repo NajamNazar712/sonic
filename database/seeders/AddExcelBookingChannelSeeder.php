baa59dfbc7357eccd8416d5af7f4c437e2e82d24
usamauddin <usama.uddin@trax.pk>
2025-10-01 15:12:24 +0500

    excel channel add

---
+++ database/seeders/AddExcelBookingChannelSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AddExcelBookingChannelSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('channels')->insert(array(
+            array('id' => 6, 'name' => 'Sonic Excel'),
+        ));
+    }
+}
