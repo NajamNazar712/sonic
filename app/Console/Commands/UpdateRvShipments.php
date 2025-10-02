d4db112be3a584af60d2b5ebe82328a68d4a43c2
Anas <anas.mazhar@trax.pk>
2025-10-02 17:52:04 +0500

    changes

--- app/Console/Commands/UpdateRvShipments.php
+++ app/Console/Commands/UpdateRvShipments.php
@@ -6,13 +6,16 @@ use App\Http\Controllers\ShipmentsJourneyController;
 use App\Http\Models\RvShipmentAssignAgent;
 use App\Http\Models\Shipment;
 use App\Http\Models\ShipmentsJourney;
+use App\RvShipmentTicket;
 use Illuminate\Console\Command;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 use PhpOffice\PhpSpreadsheet\IOFactory;
+use App\Http\Traits\RvTrait;
 
 class UpdateRvShipments extends Command
 {
+    use RvTrait;
     protected $signature = 'shipments:update-rv-sar {tracking_number?} {fileInput?} {--file= : Path to Excel file containing tracking numbers} ';
     protected $description = 'Update RV shipments and handle missing SAR shipments';
 
@@ -131,6 +134,91 @@ class UpdateRvShipments extends Command
 
     protected function processShipment($shipment)
     {
+        // try {
+        //     $haltShipper = RvShipmentTicket::join('shipments_journey', function ($join) use ($shipment) {
+        //         $join->on('rv_shipment_tickets.shipment_id', '=', 'shipments_journey.shipment_id')
+        //             ->where('shipments_journey.shipper_status_id', '=', 12)
+        //             ->where('shipments_journey.verification', 1)
+        //             ->where('shipments_journey.shipment_id', $shipment->id);
+        //         // ->where('shipments_journey.created_at', '>=', $date . ' 00:00:00')
+        //         // ->where('shipments_journey.updated_at', '<=', date('Y-m-d') . ' 23:59:59');
+        //     })
+        //         ->where('disabled_shipper', 1)
+        //         ->where('permanent_disable', 0)
+        //         ->where('halt_shipper', 0)
+        //         ->where('call_count', 0)
+        //         ->where('rv_shipment_tickets.shipment_id', $shipment->id)
+        //         // ->whereNotIn('shipment_status_reason_id', [27, 35])
+        //         // ->whereNULL('shipment_status_reason_id')
+        //         // ->where('rv_shipment_tickets.updated_at', '>=', $date . ' 00:00:00')
+        //         // ->where('rv_shipment_tickets.updated_at', '<=', date('Y-m-d') . ' 23:59:59')
+        //         ->select('rv_shipment_tickets.*', 'shipments_journey.id as journeyId')
+        //         ->get();
+
+
+        //     if ($haltShipper->isNotEmpty()) {
+        //         // dd($haltShipper);
+
+        //         foreach ($haltShipper->toArray() as $insertData) {
+        //             $data = [
+        //                 'agent_id' => 346, // testing purpose
+        //                 'shipment_id' => $insertData['shipment_id'],
+        //                 'shipments_journey_id' => $insertData['journeyId'],
+        //                 'rv_assign_agent_status_id' => 7,
+        //                 'rv_assign_agent_sub_status_id' => null,
+        //                 'assigned_to_type_id' => 0,
+        //                 'assigned_by' => 0,
+        //                 'rv_state_id' => 2,
+        //                 'updated_by_id' => 346
+        //             ];
+
+        //             $this->rv_shipment_assign($data);
+
+        //             ShipmentsJourneyController::add(
+        //                 $insertData['shipment_id'],
+        //                 65,
+        //                 65,
+        //                 $insertData['shipment_status_reason_id'],
+        //                 null,
+        //                 $insertData['shipment_user_id'],
+        //                 346
+        //             );
+        //         }
+
+        //         // NotificationsController::send(220, $haltShipper);
+
+        //         Shipment::whereIn('id', array_column($haltShipper->toArray(), 'shipment_id'))
+        //             ->update([
+        //                 'shipper_status_id' => 65,
+        //                 'consignee_status_id' => 65
+        //             ]);
+
+        //         RvShipmentTicket::whereIn('shipment_id', array_column($haltShipper->toArray(), 'shipment_id'))
+        //             ->update([
+        //                 'halt_shipper' => 1,
+        //                 'deleted_at' => now()
+        //             ]);
+
+        //         RvShipmentAssignAgent::whereIn('shipment_id', array_column($haltShipper->toArray(), 'shipment_id'))
+        //             // ->whereDate('created_at', $date)
+        //             ->update([
+        //                 'unresponsive_count' => 3,
+        //                 'unresponsive_email_count' => 1,
+        //                 'unresponsive_email_time' => now()
+        //             ]);
+        //            $this->info("✅ Shipment {$shipment->tracking_number} (ID: {$shipment->id}) updated successfully.");
+        //     }
+        // } catch (\Exception $e) {
+        //     // log error for debugging
+        //     echo "❌ Error: " . $e->getMessage() . PHP_EOL;
+        //     echo "File: " . $e->getFile() . " (Line " . $e->getLine() . ")" . PHP_EOL;
+
+        //     // If you also want the full trace in CMD
+        //     echo $e->getTraceAsString() . PHP_EOL;
+        //     // optional: notify via email/slack or throw again
+        //     // throw $e;
+        // }
+
         // Your logic to process each shipment
         $rvShipments = RvShipmentAssignAgent::where('shipment_id', $shipment->id);
 
