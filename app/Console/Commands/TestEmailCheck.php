e13298444562faed791fd43231d669572f68b448
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2025-10-09 18:07:46 +0500

    changes

--- app/Console/Commands/TestEmailCheck.php
+++ app/Console/Commands/TestEmailCheck.php
@@ -1091,7 +1091,7 @@ class TestEmailCheck extends Command
             $records = DB::table('done_payment_shipments')
                 ->whereIn('done_payment_id', $paymentId)
                 ->whereIn('shipment_id', $shipments)
-                ->select('id', 'wht', 'cod_sst', 'payable')
+                ->select('id', 'wht', 'cod_sst', 'payable','amount')
                 ->get();
 
             foreach ($records as $record) {
