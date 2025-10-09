0f954bdf7ef7da5e0426b964079f05c108d20035
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2025-10-09 14:20:38 +0500

    changes

--- database/migrations/2025_09_09_160436_create_lost_categories_table.php
+++ database/migrations/2025_09_09_160436_create_lost_categories_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
     {
         Schema::create('lost_categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->string('name',35)->unique();
             $table->timestamps();
         });
     }
