47199dfbd5f85a478fe702fd2db393fe853b256d
Anas <anas.mazhar@trax.pk>
2025-10-15 17:51:40 +0500

    changes

--- app/Http/Controllers/Admins/DeliveryController.php
+++ app/Http/Controllers/Admins/DeliveryController.php
@@ -395,9 +395,9 @@ class DeliveryController extends Controller
         if ($area = $request->get('search_area')) {
             $shipments = $shipments->where('caa.city_area_id', '=', $area);
         }
-        if ($request->get('excel') && $request->get('excel') == true) {
-            $datatables = DataTables::of($shipments)->skipPaging();   
-        }
+        // if ($request->get('excel') && $request->get('excel') == true) {
+        //     $datatables = DataTables::of($shipments)->skipPaging();   
+        // }
         $datatables = Datatables::of($shipments)
             ->setRowAttr([
                 'class' => function ($shipments) {
