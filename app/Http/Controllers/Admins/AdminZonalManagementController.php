19a67d808444a3aedd12232539461fbd9b6fef0f
Anas <anas.mazhar@trax.pk>
2025-10-17 12:11:17 +0500

    changes

--- app/Http/Controllers/Admins/AdminZonalManagementController.php
+++ app/Http/Controllers/Admins/AdminZonalManagementController.php
@@ -262,13 +262,6 @@ class AdminZonalManagementController extends Controller
         $zone->business_category_id = 2;
 
         $zone->save();
-        InternationalDhlZone::updateOrCreate(
-            ['zone_id' => $zone->id], // match existing record by ID
-            [
-                'zone_id' => $zone->id,
-                'zone_name' =>  strtolower(preg_replace('/\s*zone\s*/i', '', $request->name))
-            ] // update or insert data
-        );
         return redirect()->route('admin.management.zonal.index')->with(['success' => 'Zone: ' . $request->name . ' has been added!']);
     }
 
@@ -285,13 +278,7 @@ class AdminZonalManagementController extends Controller
         $zone->gst = $request->gst;
 
         $zone->save();
-        InternationalDhlZone::updateOrCreate(
-            ['zone_id' => $zone->id], // match existing record by ID
-            [
-                'zone_id' => $zone->id,
-                'zone_name' =>  strtolower(preg_replace('/\s*zone\s*/i', '', $request->name))
-            ] // update or insert data
-        );
+        
         return redirect()->route('admin.management.zonal.index')->with(['success' => 'Zone: ' . $request->name . ' has been updated!']);
     }
 
