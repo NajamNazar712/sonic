eca0ab37f928cd9f2a67d12b4da93691a6df262b
Anas <anas.mazhar@trax.pk>
2025-11-03 14:07:28 +0500

    changes

--- app/Console/Commands/UpdatePendingBanks.php
+++ app/Console/Commands/UpdatePendingBanks.php
@@ -43,38 +43,40 @@ class UpdatePendingBanks extends Command
             return Command::SUCCESS;
         }
         $processed = [];
-        foreach($banks as $bank){
-            $userBank = UserBankInfo::find($bank->bank_id);
-            $userBank->bank_name = $bank->bank_name;
-            $userBank->bank_branch = $bank->bank_branch;
-            $userBank->account_no = $bank->account_no;
-            $userBank->account_title = $bank->account_title;
-            $userBank->iban_no = $bank->iban_no;
-            $userBank->bank_city = $bank->bank_city;
-            $userBank->user_id = $bank->user_id;
-            $userBank->save();
-            $user_attachment = UserDocumentAttachment::where('user_id', $bank->user_id)->latest()->first();
-            if ($user_attachment->blank_cheque_image != NULL) {
-                Storage::disk('public')->delete('users_attached_documents/' . ($bank->user_id) . '/' . $user_attachment->blank_cheque_image);
+        if(!empty($banks)){
+            foreach($banks as $bank){
+                $userBank = UserBankInfo::find($bank->bank_id);
+                $userBank->bank_name = $bank->bank_name;
+                $userBank->bank_branch = $bank->bank_branch;
+                $userBank->account_no = $bank->account_no;
+                $userBank->account_title = $bank->account_title;
+                $userBank->iban = $bank->iban_no;
+                $userBank->city_id = $bank->bank_city;
+                $userBank->user_id = $bank->user_id;
+                $userBank->save();
+                $user_attachment = UserDocumentAttachment::where('user_id', $bank->user_id)->latest()->first();
+                if ($user_attachment->blank_cheque_image != NULL) {
+                    Storage::disk('public')->delete('users_attached_documents/' . ($bank->user_id) . '/' . $user_attachment->blank_cheque_image);
+                }
+                $user_attachment->blank_cheque_image = $bank->blank_cheque_image;
+                $user_attachment->save();
+                NotificationsController::send(250, $bank->user->email);
+                $processed[] = [
+                    'user_id' => $bank->user_id,
+                    'user_name' => $bank->user->name,
+                    'bank_name' => $userBank->bank->name ?? '-',
+                    'bank_branch' => $bank->bank_branch,
+                    'account_no' => $bank->account_no,
+                    'account_title' => $bank->account_title,
+                    'iban_no' => $bank->iban_no,
+                    'bank_city' => $userBank->city->name ?? '-',
+                    'blank_cheque_image' => $bank->blank_cheque_image ?? '-',
+                ];
+                $bank->status = 1;
+                $bank->save();
             }
-            $user_attachment->blank_cheque_image = $bank->blank_cheque_image;
-            $user_attachment->save();
-            NotificationsController::send(250, $bank->user->email);
-            $processed[] = [
-                'user_id' => $bank->user_id,
-                'user_name' => $bank->user->name,
-                'bank_name' => $userBank->bank->name ?? '-',
-                'bank_branch' => $bank->bank_branch,
-                'account_no' => $bank->account_no,
-                'account_title' => $bank->account_title,
-                'iban_no' => $bank->iban_no,
-                'bank_city' => $userBank->city->name ?? '-',
-                'blank_cheque_image' => $bank->blank_cheque_image ?? '-',
-            ];
-            $bank->status = 1;
-            $bank->save();
+            NotificationsController::send(251,  $processed);
         }
-        NotificationsController::send(251,  $processed);
         return Command::SUCCESS;
     }
 }
