f0780d1a79254272cf0bc3072045852c19779caa
usamauddin <usama.uddin@trax.pk>
2025-11-17 16:37:35 +0500

    set one day condition booking api

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -540,7 +540,7 @@ class APIController extends Controller
         $user_type = User::where('id', $user_id)->first();
 
         // sahban bhai said service type 3,5 then disabled this below condition
-            if ($request->input('amount') == 0 && Carbon::parse($user_type->activated_at)->lt(Carbon::now()->subDays(7)) && !PendingPayment::check_negative_payable($user_id, $user_type['account_type_id']) && !in_array($request->input('service_type_id',1), [3,5])) {
+            if ($request->input('amount') == 0 && Carbon::parse($user_type->activated_at)->lt(Carbon::now()->subDays(1)) && !PendingPayment::check_negative_payable($user_id, $user_type['account_type_id']) && !in_array($request->input('service_type_id',1), [3,5])) {
 
                 return response()->json([
                     'status' => 1,
