2127e05ae8535aff7934c4284fdc80fc4ac54984
Anas <anas.mazhar@trax.pk>
2025-12-09 00:54:23 +0500

    changes

--- app/Http/Traits/RvTrait.php
+++ app/Http/Traits/RvTrait.php
@@ -2064,9 +2064,10 @@ trait RvTrait
         if (GlobalSettings::where(['type' => 'bot_call_enable_disable', 'setting_value' => 1])->exists()) {
             if (RvShipmentTicket::where('shipment_id', $shipmentId)->whereNull('deleted_at')->where('is_bot', 1)->exists()) {
                 $shipment = Shipment::with(['user:id,name,brand_name'])->select('user_id', 'consignee_phone_number_1', 'consignee_name', 'tracking_number', 'amount')->find($shipmentId);
-                // if(in_array($shipment->user_id,[16344, 26249, 13060, 5553, 30230, 49055, 50475, 27425, 31794, 31902, 31538, 51353, 19943, 8732, 44233, 37631, 48558, 35049, 25244, 49028, 1049]) && $callCount == 3){
-                //    return self::botCallingThirdDataSet($shipmentId);
-                // }
+                if(in_array($shipment->user_id,[16344, 26249, 13060, 5553, 30230, 49055, 50475, 27425, 31794, 31902, 31538, 51353, 19943, 8732, 44233, 37631, 48558, 35049, 25244, 49028, 1049]) && $callCount == 3){
+                    Log::channel('botCallJobLog')->info('s ' . '31538 this is hit multiple time or not...!' );
+                   return self::botCallingThirdDataSet($shipmentId);
+                }
                 $base_uri = 'https://cap.zong.com.pk:8444/vpbx-apis/roboCalls/outboundCall';
                 RvShipmentTicket::where('shipment_id', $shipmentId)->update(['in_progress' => 1]);
 
