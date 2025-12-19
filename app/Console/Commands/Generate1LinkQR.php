82517118ed9a9dca72d1fbd1e2a7d80e3227c272
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2025-12-19 13:28:16 +0500

    changes

--- app/Console/Commands/Generate1LinkQR.php
+++ app/Console/Commands/Generate1LinkQR.php
@@ -54,7 +54,7 @@ class Generate1LinkQR extends Command
                 "dbaName" => "Sonic",
                 "merchantName" => "TRAX ONLINE PRIVATE LIMITED",
                 "iban" => "PK28MEZN0099120102951143",
-                "bankBic" => "MEZN009",
+                "bankBic" => "MEZN",
                 "merchantCategoryCode" => "4215",
                 "merchantID" => "854710236963454",
                 "postalAddress" => [
