58f7ac46278b3c80e553b35fcef7b70c55c414e5
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2025-12-19 17:50:47 +0500

    changes

--- app/Console/Commands/Generate1LinkQR.php
+++ app/Console/Commands/Generate1LinkQR.php
@@ -89,8 +89,8 @@ class Generate1LinkQR extends Command
                 "transactionType" => "064"
             ],
             "info" => [
-                "stan" => "123368",
-                "rrn" => "000049123368"
+                "stan" => "123468",
+                "rrn" => "000049123468"
             ]
         ];
 
