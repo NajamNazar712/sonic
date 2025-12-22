c341a8d15b8600139a93de2404eca7f85c0c2284
Najam <muhammad.najam@trax.pk>
2025-12-22 19:41:00 +0500

    working

--- app/Http/Controllers/Admins/ExpectedShipmentPenaltyAdjustmentController.php
+++ app/Http/Controllers/Admins/ExpectedShipmentPenaltyAdjustmentController.php
@@ -4,18 +4,26 @@ namespace App\Http\Controllers\Admins;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\ExpectedShipmentPenaltyAdjustment;
+use Yajra\Datatables\Datatables;
 
 
 class ExpectedShipmentPenaltyAdjustmentController extends Controller
 {
-    
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+
+        $this->middleware('Permission');
+    }
     public function index(Request $request) {
 
-        return view();
+        return view('admin.finance.expected_shipment_penalties');
     }
 
 
     public function list (Request $request) {
+
 
     }
 }
