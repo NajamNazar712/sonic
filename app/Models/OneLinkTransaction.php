ac5f272934c239bd0169db4f67d879b95c08fd73
Mohsin Sajjad <mohsin.sajjad@trax.pk>
2025-12-29 15:48:03 +0500

    changes

--- app/Models/OneLinkTransaction.php
+++ app/Models/OneLinkTransaction.php
@@ -35,8 +35,9 @@ class OneLinkTransaction extends Model
     public function one_link_log() {
         return $this->hasOne('App\Models\OneLinkApiLog', 'id', 'log_id');
     }
-    public function shipment() {
-        return $this->hasOne('App\Http\Models\Shipment', 'id', 'rrn');
+    public function shipment()
+    {
+        return $this->belongsTo(\App\Http\Models\Shipment::class, 'shipment_id', 'id');
     }
     
 }
