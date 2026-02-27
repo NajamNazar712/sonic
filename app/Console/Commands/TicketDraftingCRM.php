0fd662a2f9c15d8d235465110649e668b3164639
Anas <anas.mazhar@trax.pk>
2026-02-27 12:56:35 +0500

    changes

--- app/Console/Commands/TicketDraftingCRM.php
+++ app/Console/Commands/TicketDraftingCRM.php
@@ -44,7 +44,7 @@ class TicketDraftingCRM extends Command
                 ->first();
             if(!in_array($days, $rangeArray)  && in_array($list->shipment->shipper_status_id, $ticketDraftShipmentStatus)){
                 if($crmRequestCheck?->status_id != 1){
-                    CRMController::add(1, 2, 5, 1, 5322, 4, $list->shipment->id, $list->shipment->user_id, 5322, NULL);
+                    CRMController::add(1, 2, 5, 1, 5321, 4, $list->shipment->id, $list->shipment->user_id, 5321, NULL);
                 }
 
                 $list->status = 1;
