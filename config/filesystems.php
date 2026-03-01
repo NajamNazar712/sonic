b0cd14e1d687be9538a761c077fa3601a73ddcf6
Anas <anas.mazhar@trax.pk>
2026-03-01 10:50:43 +0500

    changes

--- config/filesystems.php
+++ config/filesystems.php
@@ -56,12 +56,25 @@ return [
         ],
 
         's3' => [
+            // 'driver' => 's3',
+            // 'key' => env('AWS_ACCESS_KEY_ID'),
+            // 'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            // 'region' => env('AWS_DEFAULT_REGION'),
+            // 'bucket' => env('AWS_BUCKET'),
+            // 'url' => env('AWS_URL'),
             'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
+            'key' => env('MINIO_KEY'),
+            'secret' => env('MINIO_SECRET'),
+            'region' => 'garaj-digitalpark-isb',
+            'bucket' => env('MINIO_BUCKET'),
+            'endpoint' => env('MINIO_ENDPOINT'),
+            'use_path_style_endpoint' => true,
+            'throw' => true,   // <-- important
+            'options' => [
+                'http' => [
+                    'verify' => false,  // skip SSL verification if needed
+                ],
+            ],
         ],
 
         // 'minio' => [
