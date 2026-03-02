56519468bb978fd8acc8544dbbffc7eb2c61d8bf
Anas <anas.mazhar@trax.pk>
2026-03-02 15:15:12 +0500

    changes

--- app/Http/Controllers/Admins/AdminPettyCashController.php
+++ app/Http/Controllers/Admins/AdminPettyCashController.php
@@ -609,7 +609,7 @@ class AdminPettyCashController extends Controller
         if ($file) {
             $url = Storage::url('petty_cash_statement_details/' . $reference_document);
         } else {
-            $url = Storage::disk('s3')->temporaryUrl('petty_cash_statement_images/' . $reference_document, now()->addMinutes(5));
+            $url = Storage::disk('s3')->temporaryUrl('sonic-archive/petty_cash_statement_images/' . $reference_document, now()->addMinutes(5));
         }
 
         return redirect($url);
