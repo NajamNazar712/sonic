6a4e867371ae05b5d58699505e5cab3ffdd9d11b
Najam <muhammad.najam@trax.pk>
2026-03-04 15:49:10 +0500

    fixing

--- app/Jobs/ShipmentGeoCodesTPL.php
+++ app/Jobs/ShipmentGeoCodesTPL.php
@@ -39,72 +39,73 @@ class ShipmentGeoCodesTPL implements ShouldQueue
      */
     public function handle()
     {
-        try {
-            $shipment_ids = $this->shipment_ids;
+        return true;
+        // try {
+        //     $shipment_ids = $this->shipment_ids;
 
-            // 1) Check if geocodes enabled
-            $geo_codes_enabled = GlobalSettings::where('type','geo_codes_enabled')->value('setting_value');
-            if (!$geo_codes_enabled) {
-                return;
-            }
+        //     // 1) Check if geocodes enabled
+        //     $geo_codes_enabled = GlobalSettings::where('type','geo_codes_enabled')->value('setting_value');
+        //     if (!$geo_codes_enabled) {
+        //         return;
+        //     }
 
-            // 2) Get overall limit
-            $geo_codes_max_limit = GlobalSettings::where('type','geo_codes_max_limit')->value('setting_value');
+        //     // 2) Get overall limit
+        //     $geo_codes_max_limit = GlobalSettings::where('type','geo_codes_max_limit')->value('setting_value');
 
-            // 3) Get current usage count
-            $api_count = GeoCodeApiCount::value('api_count');
+        //     // 3) Get current usage count
+        //     $api_count = GeoCodeApiCount::value('api_count');
 
-            if (!$api_count) $api_count = 0;
+        //     if (!$api_count) $api_count = 0;
 
-            // 4) Remaining API hits available
-            $remaining_hits = $geo_codes_max_limit - $api_count;
+        //     // 4) Remaining API hits available
+        //     $remaining_hits = $geo_codes_max_limit - $api_count;
 
-            // If no API hits remaining → DO NOT PROCESS
-            if ($remaining_hits <= 0) {
-                Log::channel('cronJobLog')->warning("GeoCodesAPI: LIMIT reached. No more API hits allowed.");
-                return;
-            }
+        //     // If no API hits remaining → DO NOT PROCESS
+        //     if ($remaining_hits <= 0) {
+        //         Log::channel('cronJobLog')->warning("GeoCodesAPI: LIMIT reached. No more API hits allowed.");
+        //         return;
+        //     }
 
-            // 5) Assigned hub + segment filters
-            $assigned_destination = GeoCodesAssignDestinationHubs::pluck('destination_hub_id')->toArray();
-            $assigned_sub_segments = GeoCodesAssignSubSegments::pluck('sub_segment_id')->toArray();
+        //     // 5) Assigned hub + segment filters
+        //     $assigned_destination = GeoCodesAssignDestinationHubs::pluck('destination_hub_id')->toArray();
+        //     $assigned_sub_segments = GeoCodesAssignSubSegments::pluck('sub_segment_id')->toArray();
 
 
-            if (empty($assigned_destination) || empty($assigned_sub_segments)) {
-                Log::channel('cronJobLog')->warning("GeoCodesAPI: Skipped — Destination hubs or sub-segments not assigned.");
-                return;
-            }
+        //     if (empty($assigned_destination) || empty($assigned_sub_segments)) {
+        //         Log::channel('cronJobLog')->warning("GeoCodesAPI: Skipped — Destination hubs or sub-segments not assigned.");
+        //         return;
+        //     }
 
-            $shipments = Shipment::with(['consignee_city','user'])
-                ->whereIn('id', $shipment_ids)
-                ->get();
+        //     $shipments = Shipment::with(['consignee_city','user'])
+        //         ->whereIn('id', $shipment_ids)
+        //         ->get();
 
-            $shipment_id_array = [];
+        //     $shipment_id_array = [];
 
-            foreach ($shipments as $shipment) {
+        //     foreach ($shipments as $shipment) {
 
-                $hub_id = optional($shipment->consignee_city)->hub_id;
-                $seg_id = optional($shipment->user)->sub_segment_id;
+        //         $hub_id = optional($shipment->consignee_city)->hub_id;
+        //         $seg_id = optional($shipment->user)->sub_segment_id;
 
-                if (in_array($hub_id, $assigned_destination) && in_array($seg_id, $assigned_sub_segments)) {
-                    $shipment_id_array[] = $shipment->id;
-                }
-            }
-            // LIMIT APPLY HERE
-            // if shipments > remaining API hits → trim array
-            if (count($shipment_id_array) > $remaining_hits) {
-                $shipment_id_array = array_slice($shipment_id_array, 0, $remaining_hits);
-            }
+        //         if (in_array($hub_id, $assigned_destination) && in_array($seg_id, $assigned_sub_segments)) {
+        //             $shipment_id_array[] = $shipment->id;
+        //         }
+        //     }
+        //     // LIMIT APPLY HERE
+        //     // if shipments > remaining API hits → trim array
+        //     if (count($shipment_id_array) > $remaining_hits) {
+        //         $shipment_id_array = array_slice($shipment_id_array, 0, $remaining_hits);
+        //     }
 
-            if (!empty($shipment_id_array)) {
-                GeoCodesController::tpl_geo_codes($shipment_id_array);
-            }
+        //     if (!empty($shipment_id_array)) {
+        //         GeoCodesController::tpl_geo_codes($shipment_id_array);
+        //     }
 
-        } catch (\Throwable $th) {
-            Log::channel('cronJobLog')->error("geo_codes_job: ".$th->getMessage(), [
-                'line' => $th->getLine(),
-                'file' => $th->getFile(),
-            ]);
-        }
+        // } catch (\Throwable $th) {
+        //     Log::channel('cronJobLog')->error("geo_codes_job: ".$th->getMessage(), [
+        //         'line' => $th->getLine(),
+        //         'file' => $th->getFile(),
+        //     ]);
+        // }
     }
 }
