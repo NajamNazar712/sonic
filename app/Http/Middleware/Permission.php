3c6fc9446e8d2078d1b616a04d33f5ae673dd7c3
Najam <muhammad.najam@trax.pk>
2026-03-25 11:29:11 +0500

    work in progress

--- app/Http/Middleware/Permission.php
+++ app/Http/Middleware/Permission.php
@@ -1564,6 +1564,9 @@ class Permission
             'reports.wht.index' => 1036,
             'reports.wht.list' => 1036,
 
+            'reports.negative_balance.list' => 1036,
+            'reports.negative_balance.index' => 1036,
+
             'settings.product_tax.index' => 1037,
             'settings.product_tax_logs.index' => 1039,
              'settings.geo_codes.index' => 1040,
