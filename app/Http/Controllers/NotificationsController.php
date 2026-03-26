d841ae7e15e8964074d15275a9bec58be1bdb85c
Anas <anas.mazhar@trax.pk>
2026-03-26 20:39:52 +0500

    working onit

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -323,13 +323,13 @@ class NotificationsController extends Controller
             if ($to) {
 
                 // Determine mailer based on $from
-                $fromAddress = (str_contains($from, 'return')) ? 'return@trax.pk' : 'info@trax.pk';
+                // $fromAddress = (str_contains($from, 'return')) ? 'return@trax.pk' : 'info@trax.pk';
                 $selectedMailer = 'huawei_email';
 
                 if ($id == 230) {
                     $mailable = new NotificationsDispatchNow($subject, $body);
                 } else {
-                    $mailable = new Notifications($subject, $body, $fromAddress);
+                    $mailable = new Notifications($subject, $body);
                 }
 
                 // Select mailer
