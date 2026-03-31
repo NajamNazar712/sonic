8a7b0c53e85aef394755f400980bfe5f7119aeaa
Anas <anas.mazhar@trax.pk>
2026-03-31 16:12:06 +0500

    changes

--- app/Http/Traits/CommonTrait.php
+++ app/Http/Traits/CommonTrait.php
@@ -695,9 +695,9 @@ trait CommonTrait
             $audioUrl = asset(Storage::url($riderReturn->audio_path));
           } else {
             try {
-              $audioUrl = Storage::disk('s3')->temporaryUrl(
+              $audioUrl = Storage::disk('s3')->url(
                 $riderReturn->audio_path,
-                now()->addMinutes(5)
+                // now()->addMinutes(5)
               );
             } catch (\Exception $e) {
               \Log::error('S3 Return Audio URL error: ' . $e->getMessage());
