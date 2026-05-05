585a4b55813c42f8738ecf33e8648c88199e5f4b
Anas <anas.mazhar@trax.pk>
2026-05-05 13:58:49 +0500

    working

--- app/Http/Controllers/Admins/ReturnController.php
+++ app/Http/Controllers/Admins/ReturnController.php
@@ -6381,7 +6381,7 @@ class ReturnController extends Controller
                         $img_url = asset('uploads/return_notes/' . $return->image);
                     } else {
                         $exists = Storage::disk('s3')->exists('sonic-archive/return_note_images/' . $return->image);
-                        dd();
+                        dd($exists);
                         if ($exists) {
                             $img_url = Storage::disk('s3')->temporaryUrl('sonic-archive/return_note_images/' . $return->image, now()->addMinutes(5));
                         }
