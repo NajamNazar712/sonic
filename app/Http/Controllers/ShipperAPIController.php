a362f492d257006553fa4891fdf68acd863899c1
Mohsin Sajjad <mohsin.sajjad@logiserves.com>
2026-05-25 13:50:16 +0500

    changes

--- app/Http/Controllers/ShipperAPIController.php
+++ app/Http/Controllers/ShipperAPIController.php
@@ -1418,6 +1418,11 @@ class ShipperAPIController extends Controller
         $user_id = $request->shipper_id;
         $date = Carbon::today();
         $user = User::find($user_id);
+
+        if (!$user) {
+            return response()->json(['status' => 1, 'message' => 'Account not found.'], 404);
+        }
+
 //        $booking_types = BookingType::where('id', '!=', 4)->get();
 //        $user_shipping_address = UserShippingInfo::join('cities as c', 'c.id', '=', 'user_shipping_infos.city_id')
 //            ->where('user_shipping_infos.user_id', $user_id)
